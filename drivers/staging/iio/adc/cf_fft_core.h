38544aea651a8f3f859f283eaa18f01b9bbc2ab9
Michael Hennerich <michael.hennerich@analog.com>
2012-04-24 14:29:28 +0200

    íio: adc: Add HW FFT support
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

---
+++ drivers/staging/iio/adc/cf_fft_core.h
@@ -0,0 +1,16 @@
+/*
+ * ADI-FFT Module
+ *
+ * Copyright 2012 Analog Devices Inc.
+ *
+ * Licensed under the GPL-2.
+ *
+ * http://wiki.analog.com/resources/fpga/xilinx/
+ */
+
+#ifndef ADI_FFT_H_
+#define ADI_FFT_H_
+
+int fft_calculate(dma_addr_t src, dma_addr_t dest, unsigned int size);
+
+#endif /* ADI_FFT_H_ */
