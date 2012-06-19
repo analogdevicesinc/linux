64f59bf41be58b8ca4d68c7a08e16f09bf6c91c8
Michael Hennerich <michael.hennerich@analog.com>
2012-06-19 11:55:59 +0200

    board: zynq: zc702: add xcomm related resources
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- arch/arm/mach-zynq/board_zc702.c
+++ arch/arm/mach-zynq/board_zc702.c
@@ -62,7 +62,7 @@ static struct flash_platform_data spi_flash_pdata = {
 	.name			= "serial_flash",
 	.parts			= spi_flash_partitions,
 	.nr_parts		= ARRAY_SIZE(spi_flash_partitions),
-	.type			= "sst25wf080"	
+	.type			= "sst25wf080"
 };
 
 #endif
@@ -157,7 +157,7 @@ static struct i2c_board_info __initdata si570_board_info[] = {
 };
 
 #endif /* CONFIG_SI570 */
-	
+
 #if defined(CONFIG_EEPROM_AT24)
 
 static struct i2c_board_info __initdata m24c08_board_info[] = {
@@ -170,25 +170,224 @@ static struct i2c_board_info __initdata m24c08_board_info[] = {
 
 #endif /* CONFIG_I2C_XILINX_PS && CONFIG_I2C_MUX_PCA954x */
 
-#if defined(CONFIG_SPI_SPIDEV) || defined(CONFIG_MTD_M25P80)
+#define SPIBUS_NUM_LPC		0
 
-static struct spi_board_info __initdata xilinx_spipss_0_boardinfo[] = {
-	{
-#ifdef CONFIG_SPI_SPIDEV
-		.modalias		= "spidev",
-		.platform_data		= &spi_0_pdata,
-#else 
-		.modalias		= "m25p80",
-		.platform_data		= &spi_flash_pdata,
-#endif
-		.irq			= IRQ_SPI1,
-		.max_speed_hz		= 40000000, /* max sample rate at 3V */
-		.bus_num		= 0,
-		.chip_select		= 1,
+#if defined(CONFIG_AD9523) || defined(CONFIG_AD9523_MODULE)
+#include "../../../../drivers/staging/iio/frequency/ad9523.h"
+
+struct ad9523_channel_spec ad9523_channels[] = {
+	{	/* ZD output */
+		.channel_num = 0,
+		.extended_name = "ZD_OUTPUT",
+		.divider_output_invert_en = false,
+		.sync_ignore_en = false,
+		.low_power_mode_en = false,
+		.driver_mode = LVDS_4mA,
+		.divider_phase = 0,
+		.channel_divider = 8,
+		.use_alt_clock_src = false,
+		.output_dis = false,
+	},
+	{	/* DAC CLK */
+		.channel_num = 1,
+		.extended_name = "DAC_CLK",
+		.divider_output_invert_en = false,
+		.sync_ignore_en = false,
+		.low_power_mode_en = false,
+		.driver_mode = LVPECL_8mA,
+		.divider_phase = 0,
+		.channel_divider = 2,
+	},
+	{	/* ADC CLK */
+		.channel_num = 2,
+		.extended_name = "ADC_CLK",
+		.divider_output_invert_en = false,
+		.sync_ignore_en = false,
+		.low_power_mode_en = false,
+		.driver_mode = LVDS_7mA,
+		.divider_phase = 0,
+		.channel_divider = 4,
+	},
+	{	/* DAC REF CLK */
+		.channel_num = 4,
+		.extended_name = "DAC_REF_CLK",
+		.divider_output_invert_en = false,
+		.sync_ignore_en = false,
+		.low_power_mode_en = false,
+		.driver_mode = LVDS_4mA,
+		.divider_phase = 0,
+		.channel_divider = 16,
+	},
+	{	/* TX LO REF */
+		.channel_num = 5,
+		.extended_name = "TX_LO_REF_CLK",
+		.divider_output_invert_en = false,
+		.sync_ignore_en = false,
+		.low_power_mode_en = false,
+		.driver_mode = CMOS_CONF3, /* HiZ on - */
+		.divider_phase = 0,
+		.channel_divider = 8,
+	},
+	{	/* DAC DCO */
+		.channel_num = 6,
+		.extended_name = "DAC_DCO_CLK",
+		.divider_output_invert_en = false,
+		.sync_ignore_en = false,
+		.low_power_mode_en = false,
+		.driver_mode = LVDS_7mA,
+		.divider_phase = 0,
+		.channel_divider = 2,
+	},
+	{	/* ADC SYNC */
+		.channel_num = 8,
+		.extended_name = "ADC_SYNC_CLK",
+		.divider_output_invert_en = false,
+		.sync_ignore_en = false,
+		.low_power_mode_en = false,
+		.driver_mode = CMOS_CONF3, /* HiZ on - */
+		.divider_phase = 1,
+		.channel_divider = 32,
+		.output_dis = false,
+	},
+	{	/* RX LO REF */
+		.channel_num = 9,
+		.extended_name = "RX_LO_REF_CLK",
+		.divider_output_invert_en = false,
+		.sync_ignore_en = false,
+		.low_power_mode_en = false,
+		.driver_mode = CMOS_CONF3, /* HiZ on - */
+		.divider_phase = 0,
+		.channel_divider = 8,
 	},
 };
 
+struct ad9523_platform_data ad9523_pdata_lpc = {
+	.vcxo_freq = 122880000,
+
+	/* Single-Ended Input Configuration */
+	.refa_diff_rcv_en = true,
+	.refb_diff_rcv_en = false,
+	.zd_in_diff_en = true,
+	.osc_in_diff_en = false,
+	.osc_in_cmos_neg_inp_en = true,
+
+	.refa_r_div = 0,
+	.refb_r_div = 0,
+	.pll1_feedback_div = 4,
+	.pll1_charge_pump_current_nA = 2000,
+#if defined(CONFIG_ADIXCOMM_SYNC)
+	.zero_delay_mode_internal_en = false,
+#else
+	.zero_delay_mode_internal_en = true,
 #endif
+	.osc_in_feedback_en = false,
+	.refb_cmos_neg_inp_en = true,
+	.pll1_loop_filter_rzero = 3,
+
+#if defined(CONFIG_ADIXCOMM_SYNC)
+	.ref_mode = 3, /* 3 ?*/
+#else
+	.ref_mode = 1, /* 3 ?*/
+#endif
+
+	.pll2_charge_pump_current_nA = 420000,
+	.pll2_ndiv_a_cnt = 0,
+	.pll2_ndiv_b_cnt = 3,
+	.pll2_freq_doubler_en = true,
+	.pll2_r2_div = 1,
+	.pll2_vco_diff_m1 = 3,
+	.pll2_vco_diff_m2 = 3,
+
+	.rpole2 = 0,
+	.rzero = 2,
+	.cpole1 = 2,
+	.rzero_bypass_en = false,
+
+	/* Output Channel Configuration */
+	.num_channels = ARRAY_SIZE(ad9523_channels),
+	.channels = ad9523_channels,
+	.name = "ad9523-lpc"
+};
+#endif
+
+#if defined(CONFIG_ADF4350) || defined(CONFIG_ADF4350_MODULE)
+#include "../../../../drivers/staging/iio/frequency/adf4350.h"
+static struct adf4350_platform_data adf4350_tx_pdata_lpc = {
+	.name = "adf4351-tx-lpc",
+	.clkin = 122880000,
+	.channel_spacing = 10000,
+	.r2_user_settings = ADF4350_REG2_PD_POLARITY_POS,
+			    ADF4350_REG2_CHARGE_PUMP_CURR_uA(2500),
+	.r3_user_settings = ADF4350_REG3_12BIT_CLKDIV_MODE(0),
+	.r4_user_settings = ADF4350_REG4_OUTPUT_PWR(3) |
+			    ADF4350_REG4_MUTE_TILL_LOCK_EN,
+	.gpio_lock_detect = -1,
+	.power_up_frequency = 2400000000,
+};
+
+static struct adf4350_platform_data adf4350_rx_pdata_lpc  = {
+	.name = "adf4351-rx-lpc",
+	.clkin = 122880000,
+	.channel_spacing = 10000,
+	.r2_user_settings = ADF4350_REG2_PD_POLARITY_POS,
+			    ADF4350_REG2_CHARGE_PUMP_CURR_uA(2500),
+	.r3_user_settings = ADF4350_REG3_12BIT_CLKDIV_MODE(0),
+	.r4_user_settings = ADF4350_REG4_OUTPUT_PWR(3) |
+			    ADF4350_REG4_MUTE_TILL_LOCK_EN,
+	.gpio_lock_detect = -1,
+	.power_up_frequency = 2400000000,
+};
+#endif
+
+static struct spi_board_info __initdata xilinx_spipss_0_boardinfo[] = {
+#if defined(CONFIG_AD9548) || defined(CONFIG_AD9548_MODULE)
+	{
+		.modalias = "ad9548",
+		.max_speed_hz = 10000000,     /* max spi clock (SCK) speed in HZ */
+		.bus_num = SPIBUS_NUM_LPC,
+		.chip_select = 2, /* AD9548 */
+		.mode = SPI_MODE_0 | SPI_3WIRE,
+	},
+#endif
+#if defined(CONFIG_AD9523) || defined(CONFIG_AD9523_MODULE)
+	{
+		.modalias = "ad9523",
+		.max_speed_hz = 1000000,     /* max spi clock (SCK) speed in HZ */
+		.bus_num = SPIBUS_NUM_LPC,
+		.chip_select = 3,	/* GPIO controlled SSEL */
+		.platform_data = &ad9523_pdata_lpc, /* spi_driver specific config */
+		.mode = SPI_MODE_0 | SPI_3WIRE,
+	},
+#endif
+#if defined(CONFIG_AD8366) || defined(CONFIG_AD8366_MODULE)
+	{
+		.modalias = "ad8366",
+		.max_speed_hz = 1000000,     /* max spi clock (SCK) speed in HZ */
+		.bus_num = SPIBUS_NUM_LPC,
+		.chip_select = 6,	/* GPIO controlled SSEL */
+		.platform_data = "ad8366-lpc", /* spi_driver specific config */
+		.mode = SPI_MODE_0 | SPI_3WIRE,
+	},
+#endif
+#if defined(CONFIG_ADF4350) || defined(CONFIG_ADF4350_MODULE)
+	{
+		.modalias = "adf4351",
+		.max_speed_hz = 1000000,     /* max spi clock (SCK) speed in HZ */
+		.bus_num = SPIBUS_NUM_LPC,
+		.chip_select = 4,	/* GPIO controlled SSEL */
+		.platform_data = &adf4350_rx_pdata_lpc, /* No spi_driver specific config */
+		.mode = SPI_MODE_0,
+	},
+	{
+		.modalias = "adf4351",
+		.max_speed_hz = 1000000,     /* max spi clock (SCK) speed in HZ */
+		.bus_num = SPIBUS_NUM_LPC,
+		.chip_select = 5,	/* GPIO controlled SSEL */
+		.platform_data = &adf4350_tx_pdata_lpc, /* No spi_driver specific config */
+		.mode = SPI_MODE_0,
+	},
+#endif
+};
 
 extern struct sys_timer xttcpss_sys_timer;
 
@@ -201,7 +400,7 @@ static void __init board_zc702_init(void)
 	xilinx_init_machine();
 
 #if 	defined(CONFIG_SPI_SPIDEV) || defined(CONFIG_MTD_M25P80)
-	spi_register_board_info(&xilinx_spipss_0_boardinfo[0], 
+	spi_register_board_info(&xilinx_spipss_0_boardinfo[0],
 		ARRAY_SIZE(xilinx_spipss_0_boardinfo));
 #endif
 
