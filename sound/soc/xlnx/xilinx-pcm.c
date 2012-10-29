0f8dd21d5a75da378066468228eba03333f2b4cf
Lars-Peter Clausen <lars@metafoo.de>
2012-10-29 13:49:07 +0100

    ASoC: xilinx-pcm: Reduce preallocated memory area
    We are running a little tight on DMAable memory otherwise if we have all three
    audio, video and xcomm running.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- sound/soc/xlnx/xilinx-pcm.c
+++ sound/soc/xlnx/xilinx-pcm.c
@@ -83,7 +83,7 @@ static int xlnx_pcm_new(struct snd_soc_pcm_runtime *rtd)
 
 	return snd_pcm_lib_preallocate_pages_for_all(rtd->pcm,
 		SNDRV_DMA_TYPE_DEV,
-		card->dev, 8 * 1024 * 1024,
+		card->dev, 1 * 1024 * 1024,
 		xlnx_pcm_hardware.buffer_bytes_max);
 }
 
