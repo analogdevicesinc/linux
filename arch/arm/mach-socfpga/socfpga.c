eadd899cd45dafc1d5729f6bf6fcc535f2ca93ea
Dinh Nguyen <dinguyen@altera.com>
2012-11-07 10:47:50 -0700

    FogBugz #80929:-2 Do not initialize L2 cache controller
    Follow up to FogBugz #80929.
    
    If CONFIG_CACHE_L2X0 is not selected, then there is no need to
    initialize the L2 cache controller.
    
    Signed-off-by: Dinh Nguyen <dinguyen@altera.com>
    
    Conflicts:
    
    	arch/arm/mach-socfpga/socfpga.c

--- arch/arm/mach-socfpga/socfpga.c
+++ arch/arm/mach-socfpga/socfpga.c
@@ -109,9 +109,12 @@ static void socfpga_cyclone5_restart(enum reboot_mode mode, const char *cmd)
 
 static void __init socfpga_cyclone5_init(void)
 {
+#ifdef CONFIG_CACHE_L2X0
 	l2x0_of_init(0, ~0UL);
-	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
-
+#endif
+	of_platform_populate(NULL, of_default_bus_match_table,
+		socfpga_auxdata_lookup, NULL);
+	socfpga_init_clocks();
 	enable_periphs();
 }
 
