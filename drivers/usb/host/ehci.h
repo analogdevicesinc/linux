f518f7350a83aae2b21c31452dc99554d1ec1f95
Lars-Peter Clausen <lars@metafoo.de>
2012-12-06 14:37:19 +0100

    usb: ehci: Fix ulpi type in the ehci_hcd struct
    Looks like this was missed during earlier conversions of the ZED USB fixes to
    usb_phy.
    
    Fixes the follow warnings:
    	drivers/usb/host/ehci-hcd.c: In function 'ehci_run':
    	drivers/usb/host/ehci-hcd.c:676:25: warning: initialization from incompatible pointer type
    	In file included from drivers/usb/host/ehci-hcd.c:1300:0:
    	drivers/usb/host/ehci-xilinx-usbps.c: In function 'usb_hcd_xusbps_probe':
    	drivers/usb/host/ehci-xilinx-usbps.c:204:15: warning: assignment from incompatible pointer type
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/usb/host/ehci.h
+++ drivers/usb/host/ehci.h
@@ -180,7 +180,7 @@ struct ehci_hcd {			/* one per controller */
 	 */
 	void (*start_hnp)(struct ehci_hcd *ehci);
 #ifdef CONFIG_XILINX_ZED_USB_OTG
-	struct otg_transceiver	*ulpi;
+	struct usb_phy	*ulpi;
 #endif
 #endif
 
