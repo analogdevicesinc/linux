e3479b0c92f292234f0467c347a4e1b5d6baeac8
Alan Tull <atull@altera.com>
2013-01-31 15:47:06 -0600

    FogBugz #98100: designware i2c driver add speed mode in devtree
    For Designware I2C adapter driver, add "speed-mode" property to
    devicetree.
    
    Signed-off-by: Alan Tull <atull@altera.com>

--- drivers/i2c/busses/i2c-designware-platdrv.c
+++ drivers/i2c/busses/i2c-designware-platdrv.c
@@ -152,10 +152,12 @@ static int dw_i2c_plat_probe(struct platform_device *pdev)
 {
 	struct dw_i2c_platform_data *pdata = dev_get_platdata(&pdev->dev);
 	struct dw_i2c_dev *dev;
+	struct device_node *np = pdev->dev.of_node;
 	struct i2c_adapter *adap;
 	struct resource *mem;
 	int irq, r;
 	u32 clk_freq, ht = 0;
+	int speed, speed_prop, ret;
 
 	irq = platform_get_irq(pdev, 0);
 	if (irq < 0)
@@ -218,6 +220,16 @@ static int dw_i2c_plat_probe(struct platform_device *pdev)
 	else
 		dev->master_cfg =  DW_IC_CON_MASTER | DW_IC_CON_SLAVE_DISABLE |
 			DW_IC_CON_RESTART_EN | DW_IC_CON_SPEED_FAST;
+
+	/* Get speed from device tree.  Default to fast speed. */
+	speed = DW_IC_CON_SPEED_FAST;
+	if (np) {
+		ret = of_property_read_u32(np, "speed-mode", &speed_prop);
+		if (!ret && (speed_prop == 0))
+			speed = DW_IC_CON_SPEED_STD;
+	}
+	dev->master_cfg =  DW_IC_CON_MASTER | DW_IC_CON_SLAVE_DISABLE |
+		DW_IC_CON_RESTART_EN | speed;
 
 	dev->clk = devm_clk_get(&pdev->dev, NULL);
 	if (!i2c_dw_plat_prepare_clk(dev, true)) {
