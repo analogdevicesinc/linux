a72bdc091b9283375a1f582616ac4184d5177ab5
Michael Hennerich <michael.hennerich@analog.com>
2013-02-11 12:53:47 +0100

    iio: frequency: ad9523 revise clk is_enabled handling
    don't get status from HW, it'll sleep.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/frequency/ad9523.c
+++ drivers/iio/frequency/ad9523.c
@@ -272,6 +272,7 @@ struct ad9523_outputs {
 	struct clk_hw hw;
 	struct iio_dev *indio_dev;
 	unsigned num;
+	bool is_enabled;
 };
 
 #define to_ad9523_clk_output(_hw) container_of(_hw, struct ad9523_outputs, hw)
@@ -687,6 +688,9 @@ static int ad9523_write_raw(struct iio_dev *indio_dev,
 			reg &= ~AD9523_CLK_DIST_PWR_DOWN_EN;
 		else
 			reg |= AD9523_CLK_DIST_PWR_DOWN_EN;
+
+		st->output[chan->channel].is_enabled = !!val;
+
 		break;
 	case IIO_CHAN_INFO_FREQUENCY:
 		if (val <= 0) {
@@ -780,7 +784,7 @@ static unsigned long ad9523_clk_recalc_rate(struct clk_hw *hw,
 
 static int ad9523_clk_is_enabled(struct clk_hw *hw)
 {
-	return ad9523_get_clk_attr(hw, IIO_CHAN_INFO_RAW);
+	return to_ad9523_clk_output(hw)->is_enabled;
 }
 
 static long ad9523_set_clk_attr(struct clk_hw *hw, long mask, unsigned long val)
@@ -856,7 +860,8 @@ const struct clk_ops ad9523_clk_ops = {
 	.round_rate = ad9523_clk_round_rate,
 };
 
-struct clk *ad9523_clk_register(struct iio_dev *indio_dev, unsigned num)
+struct clk *ad9523_clk_register(struct iio_dev *indio_dev, unsigned num,
+				bool is_enabled)
 {
 	struct ad9523_state *st = iio_priv(indio_dev);
 	struct clk_init_data init;
@@ -874,6 +879,7 @@ struct clk *ad9523_clk_register(struct iio_dev *indio_dev, unsigned num)
 	output->hw.init = &init;
 	output->indio_dev = indio_dev;
 	output->num = num;
+	output->is_enabled = is_enabled;
 
 	/* register the clock */
 	clk = clk_register(&st->spi->dev, &output->hw);
