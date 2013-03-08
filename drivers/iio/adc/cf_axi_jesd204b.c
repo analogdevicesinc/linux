5e508c92c9e7779d01d209da4498bfe16914de5c
Michael Hennerich <michael.hennerich@analog.com>
2013-03-08 12:41:39 +0100

    iio: adc: cf_axi_jesd204b: add support for eye scan and lane info
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/adc/cf_axi_jesd204b.c
+++ drivers/iio/adc/cf_axi_jesd204b.c
@@ -1,7 +1,7 @@
 /*
  * ADI AXI-JESD204B Interface Module
  *
- * Copyright 2012 Analog Devices Inc.
+ * Copyright 2012-2013 Analog Devices Inc.
  *
  * Licensed under the GPL-2.
  *
@@ -14,12 +14,12 @@
 #include <linux/init.h>
 #include <linux/io.h>
 #include <linux/wait.h>
-#include <linux/spi/spi.h>
 #include <linux/dma-mapping.h>
 #include <linux/dmaengine.h>
 #include <linux/delay.h>
 #include <linux/debugfs.h>
 #include <linux/uaccess.h>
+#include <linux/device.h>
 
 #include <linux/of.h>
 #include <linux/of_device.h>
@@ -30,25 +30,260 @@
 
 #include "cf_axi_jesd204b.h"
 
+#define AXI_JESD204B_ES_HSIZE	129
+#define AXI_JESD204B_ES_VSIZE	255
+
 struct jesd204b_state {
 	struct device 		*dev;
 	void __iomem		*regs;
 	struct clk 		*clk;
+	void			*buf_virt;
+	dma_addr_t		buf_phys;
+	struct bin_attribute 	bin;
+	unsigned			size;
+	int			lane;
+	int			prescale;
+	struct work_struct 	work;
+	struct completion       complete;
 };
 
 /*
  * IO accessors
  */
 
-static inline void jesd204b_write(struct jesd204b_state *st, unsigned reg, unsigned val)
+static inline void jesd204b_write(struct jesd204b_state *st,
+				  unsigned reg, unsigned val)
 {
 	iowrite32(val, st->regs + reg);
 }
 
-static inline unsigned int jesd204b_read(struct jesd204b_state *st, unsigned reg)
+static inline unsigned int jesd204b_read(struct jesd204b_state *st,
+					 unsigned reg)
 {
 	return ioread32(st->regs + reg);
 }
+
+static int jesd204b_set_lane(struct jesd204b_state *st, unsigned lane)
+{
+	unsigned stat;
+
+	stat = jesd204b_read(st, AXI_JESD204B_REG_TEST_MODE);
+	stat &= ~0x3;
+	stat |= lane & 0x3;
+	jesd204b_write(st, AXI_JESD204B_REG_TEST_MODE, stat);
+
+	return 0;
+}
+
+static int jesd204b_es(struct jesd204b_state *st, unsigned lane)
+{
+	unsigned stat;
+
+	jesd204b_set_lane(st, lane);
+
+	jesd204b_write(st, AXI_JESD204B_REG_ES_PRESCALE,
+		AXI_JESD204B_REG_ES_PRESCALE_STEP(1) |
+		AXI_JESD204B_REG_ES_PRESCALE_MAX(31) |
+		AXI_JESD204B_REG_ES_PRESCALE_MIN(st->prescale));
+
+	jesd204b_write(st, AXI_JESD204B_REG_ES_VOFFSET,
+		AXI_JESD204B_REG_ES_VOFFSET_STEP(1) |
+		AXI_JESD204B_REG_ES_VOFFSET_MAX(AXI_JESD204B_ES_VSIZE / 2) |
+		AXI_JESD204B_REG_ES_VOFFSET_MIN(-1 * AXI_JESD204B_ES_VSIZE / 2));
+
+	jesd204b_write(st, AXI_JESD204B_REG_ES_HOFFSET,
+		AXI_JESD204B_REG_ES_HOFFSET_MAX(AXI_JESD204B_ES_HSIZE / 2) |
+		AXI_JESD204B_REG_ES_HOFFSET_MIN(-1 * AXI_JESD204B_ES_HSIZE / 2));
+
+	jesd204b_write(st, AXI_JESD204B_REG_ES_HMAXMIN,
+		AXI_JESD204B_REG_ES_HMAXMIN_MAX(AXI_JESD204B_ES_HSIZE) |
+		AXI_JESD204B_REG_ES_HMAXMIN_MIN(0));
+
+	jesd204b_write(st, AXI_JESD204B_REG_ES_HSIZE, AXI_JESD204B_ES_HSIZE + 1);
+	jesd204b_write(st, AXI_JESD204B_REG_ES_HOFFSET_STP, 1);
+
+	jesd204b_write(st, AXI_JESD204B_REG_ES_START_ADDR, st->buf_phys);
+
+	jesd204b_write(st, AXI_JESD204B_REG_ES_CTRL,
+		AXI_JESD204B_REG_ES_CTRL_NORM);
+	jesd204b_write(st, AXI_JESD204B_REG_ES_CTRL,
+		AXI_JESD204B_REG_ES_CTRL_NORM |
+		AXI_JESD204B_REG_ES_CTRL_START);
+
+	do {
+		msleep(50 * ((st->prescale & 0x1F) + 1));
+		stat = jesd204b_read(st, AXI_JESD204B_REG_ES_STATUS);
+		if (stat & (AXI_JESD204B_REG_ES_STATUS_ERROR |
+			AXI_JESD204B_REG_ES_STATUS_BUF_UNF |
+			AXI_JESD204B_REG_ES_STATUS_BUF_OVF))
+			return -EIO;
+
+	} while (stat & AXI_JESD204B_REG_ES_STATUS_ES_BUSY);
+
+	return 0;
+}
+
+static void jesd204b_work_func(struct work_struct *work)
+{
+	struct jesd204b_state *st =
+		container_of(work, struct jesd204b_state, work);
+
+	int ret = jesd204b_es(st, st->lane);
+	if (ret)
+		dev_warn(st->dev, "Eye Scan failed (%d)\n", ret);
+
+	complete_all(&st->complete);
+}
+
+static ssize_t
+jesd204b_bin_read(struct file *filp, struct kobject *kobj,
+		       struct bin_attribute *bin_attr,
+		       char *buf, loff_t off, size_t count)
+{
+	struct jesd204b_state *st;
+	struct device *dev;
+	int ret = 0;
+
+	dev = container_of(kobj, struct device, kobj);
+	st = dev_get_drvdata(dev);
+
+	if (unlikely(off >= st->bin.size))
+		return 0;
+	if ((off + count) > st->bin.size)
+		count = st->bin.size - off;
+	if (unlikely(!count))
+		return count;
+
+	wait_for_completion(&st->complete);
+
+	memcpy(buf, st->buf_virt + off, count);
+
+	return ret ? : count;
+}
+
+static ssize_t jesd204b_laneinfo_read(struct device *dev,
+			struct device_attribute *attr,
+			char *buf, unsigned lane)
+{
+	struct jesd204b_state *st = dev_get_drvdata(dev);
+	int ret;
+	unsigned val;
+
+	if (!completion_done(&st->complete))
+		return -EBUSY;
+
+	jesd204b_set_lane(st, lane);
+
+	val = jesd204b_read(st, AXI_JESD204B_REG_INIT_DATA0);
+	ret = sprintf(buf,
+		"DID: %d, BID: %d, LID: %d, L: %d, SCR: %d, F: %d\n",
+		AXI_JESD204B_INIT0_DID(val),
+		AXI_JESD204B_INIT0_BID(val),
+		AXI_JESD204B_INIT0_LID(val),
+		AXI_JESD204B_INIT0_L(val),
+		AXI_JESD204B_INIT0_SCR(val),
+		AXI_JESD204B_INIT0_F(val));
+
+	val = jesd204b_read(st, AXI_JESD204B_REG_INIT_DATA1);
+	ret += sprintf(buf + ret,
+		"K: %d, M: %d, N: %d, CS: %d, S: %d, N': %d, HD: %d\n",
+		AXI_JESD204B_INIT1_K(val),
+		AXI_JESD204B_INIT1_M(val),
+		AXI_JESD204B_INIT1_N(val),
+		AXI_JESD204B_INIT1_CS(val),
+		AXI_JESD204B_INIT1_S(val),
+		AXI_JESD204B_INIT1_ND(val),
+		AXI_JESD204B_INIT1_HD(val));
+
+	val = jesd204b_read(st, AXI_JESD204B_REG_INIT_DATA2);
+	ret += sprintf(buf + ret, "FCHK: 0x%X, CF: %d\n",
+		AXI_JESD204B_INIT2_FCHK(val),
+		AXI_JESD204B_INIT2_CF(val));
+
+	val = jesd204b_read(st, AXI_JESD204B_REG_INIT_DATA3);
+	ret += sprintf(buf + ret,
+		"ADJCNT: %d, PHYADJ: %d, ADJDIR: %d, JESDV: %d, SUBCLASS: %d\n",
+		AXI_JESD204B_INIT3_ADJCNT(val),
+		AXI_JESD204B_INIT3_PHYADJ(val),
+		AXI_JESD204B_INIT3_ADJDIR(val),
+		AXI_JESD204B_INIT3_JESDV(val),
+		AXI_JESD204B_INIT3_SUBCLASSV(val));
+
+	ret += sprintf(buf + ret, "MFCNT : 0x%X\n",
+		       jesd204b_read(st, AXI_JESD204B_REG_TEST_MFCNT));
+	ret += sprintf(buf + ret, "ILACNT: 0x%X\n",
+		       jesd204b_read(st, AXI_JESD204B_REG_TEST_ILACNT));
+	ret += sprintf(buf + ret, "ERRCNT: 0x%X\n",
+		       jesd204b_read(st, AXI_JESD204B_REG_TEST_ERRCNT));
+	ret += sprintf(buf + ret, "BUFCNT: 0x%X\n",
+		       jesd204b_read(st, AXI_JESD204B_REG_BUFCNT));
+
+	return ret;
+}
+
+static ssize_t jesd204b_lane0_info_read(struct device *dev,
+			struct device_attribute *attr,
+			char *buf)
+{
+	return jesd204b_laneinfo_read(dev, attr, buf, 0);
+}
+static DEVICE_ATTR(lane0_info, S_IRUSR, jesd204b_lane0_info_read, NULL);
+
+static ssize_t jesd204b_lane1_info_read(struct device *dev,
+			struct device_attribute *attr,
+			char *buf)
+{
+	return jesd204b_laneinfo_read(dev, attr, buf, 1);
+}
+static DEVICE_ATTR(lane1_info, S_IRUSR, jesd204b_lane1_info_read, NULL);
+
+static ssize_t jesd204b_lane2_info_read(struct device *dev,
+			struct device_attribute *attr,
+			char *buf)
+{
+	return jesd204b_laneinfo_read(dev, attr, buf, 2);
+}
+static DEVICE_ATTR(lane2_info, S_IRUSR, jesd204b_lane2_info_read, NULL);
+
+static ssize_t jesd204b_lane3_info_read(struct device *dev,
+			struct device_attribute *attr,
+			char *buf)
+{
+	return jesd204b_laneinfo_read(dev, attr, buf, 3);
+}
+static DEVICE_ATTR(lane3_info, S_IRUSR, jesd204b_lane3_info_read, NULL);
+
+static ssize_t jesd204b_enable(struct device *dev,
+					 struct device_attribute *attr,
+					 const char *buf, size_t count)
+{
+	struct jesd204b_state *st = dev_get_drvdata(dev);
+
+	sscanf(buf, "%d", &st->lane);
+
+	if (!completion_done(&st->complete)) {
+		cancel_work_sync(&st->work);
+		complete_all(&st->complete);
+	}
+
+	INIT_COMPLETION(st->complete);
+	schedule_work(&st->work);
+
+	return count;
+}
+static DEVICE_ATTR(enable, S_IWUSR, NULL, jesd204b_enable);
+
+static ssize_t jesd204b_set_prescale(struct device *dev,
+					 struct device_attribute *attr,
+					 const char *buf, size_t count)
+{
+	struct jesd204b_state *st = dev_get_drvdata(dev);
+
+	sscanf(buf, "%d", &st->prescale);
+
+	return count;
+}
+static DEVICE_ATTR(prescale, S_IWUSR, NULL, jesd204b_set_prescale);
 
 
 static int __devinit jesd204b_of_probe(struct platform_device *op)
@@ -79,13 +314,9 @@ static int __devinit jesd204b_of_probe(struct platform_device *op)
 	st->clk = clk;
 	dev_set_drvdata(dev, st);
 
-	ret = clk_prepare(clk);
+	ret = clk_prepare_enable(clk);
 	if (ret < 0)
 		return ret;
-
-	ret = clk_enable(clk);
-	if (ret < 0)
-		goto err_clk_unprepare;
 
 	/* Get iospace for the device */
 	ret = of_address_to_resource(op->dev.of_node, 0, &r_mem);
@@ -126,9 +357,11 @@ static int __devinit jesd204b_of_probe(struct platform_device *op)
 		goto err_release_mem_region;
 
 	jesd204b_write(st, AXI_JESD204B_REG_CTRL,
-		       (of_property_read_bool(op->dev.of_node, "jesd,scramble_en") ?
+		       (of_property_read_bool(op->dev.of_node,
+			"jesd,scramble_en") ?
 		       AXI_JESD204B_CTRL_SCR_EN : 0) |
-		       (of_property_read_bool(op->dev.of_node, "jesd,lanesync_en") ?
+		       (of_property_read_bool(op->dev.of_node,
+			"jesd,lanesync_en") ?
 		       AXI_JESD204B_CTRL_LANESYNC_EN : 0));
 
 	jesd204b_write(st, AXI_JESD204B_REG_FRMCTRL,
@@ -139,14 +372,41 @@ static int __devinit jesd204b_of_probe(struct platform_device *op)
 		 jesd204b_read(st, AXI_JESD204B_REG_VERSION),
 		 (unsigned long long)phys_addr, st->regs);
 
+
+	st->size = AXI_JESD204B_ES_HSIZE * AXI_JESD204B_ES_VSIZE *
+		sizeof(unsigned int);
+	st->prescale = 0;
+	st->buf_virt = dma_alloc_coherent(dev, PAGE_ALIGN(st->size),
+					  &st->buf_phys, GFP_KERNEL);
+
+	memset(st->buf_virt, 0, PAGE_ALIGN(st->size));
+
+	sysfs_bin_attr_init(&st->bin);
+	st->bin.attr.name = "eye_data";
+	st->bin.attr.mode = S_IRUSR;
+	st->bin.read = jesd204b_bin_read;
+	st->bin.size = st->size;
+
+	ret = sysfs_create_bin_file(&op->dev.kobj, &st->bin);
+	if (ret)
+		goto err_release_mem_region;
+
+	device_create_file(dev, &dev_attr_enable);
+	device_create_file(dev, &dev_attr_prescale);
+	device_create_file(dev, &dev_attr_lane0_info);
+	device_create_file(dev, &dev_attr_lane1_info);
+	device_create_file(dev, &dev_attr_lane2_info);
+	device_create_file(dev, &dev_attr_lane3_info);
+
+	INIT_WORK(&st->work, jesd204b_work_func);
+	init_completion(&st->complete);
+
 	return 0;
 
 err_release_mem_region:
 	release_mem_region(phys_addr, remap_size);
 err_clk_disable:
-	clk_disable(clk);
-err_clk_unprepare:
-	clk_unprepare(clk);
+	clk_disable_unprepare(clk);
 	clk_put(clk);
 	dev_set_drvdata(dev, NULL);
 
@@ -175,8 +435,7 @@ static int __devexit jesd204b_of_remove(struct platform_device *op)
 	else
 		release_mem_region(r_mem.start, resource_size(&r_mem));
 
-	clk_disable(st->clk);
-	clk_unprepare(st->clk);
+	clk_disable_unprepare(st->clk);
 	clk_put(st->clk);
 
 	dev_set_drvdata(dev, NULL);
--- drivers/iio/adc/cf_axi_jesd204b.h
+++ drivers/iio/adc/cf_axi_jesd204b.h
@@ -37,6 +37,46 @@
  * 0x0b   0x2c   [31: 0]  test_mfcnt            Test MF count (?)
  * 0x0c   0x30   [31: 0]  test_ilacnt           Test ILA count (?)
  * 0x0d   0x34   [31: 0]  test_errcnt           Test ERROR count (?)
+ * ---------------------------------------------------------------------------
+ * 0x10   0x40   [ 1: 1]  es_mode               Eye scan data mode (see below)
+ *               [ 0: 0]  es_start              Start eye scan capture (0->1 transition)
+ *
+ * The eye scan data mode supports two fromats-
+ *   If mode is set (0x1), data consists of both sample and error counts.
+ *      data[31:16] = sample_count; and
+ *      data[15: 0] = error_count;
+ *   If mode is set (0x0), data consists only the error count and is filled as an RGB value
+ *      data[31:24] = 0x00;
+ *      data[23: 8] = error_count; and
+ *      data[ 7: 0] = 0x00;
+ *   This allows direct color coding, zero errors will be black.
+ * ---------------------------------------------------------------------------
+ * 0x11   0x44   [20:16]  prescale_step         Prescale step
+ *               [12: 8]  prescale_max          Prescale maximum
+ *               [ 4: 0]  prescale_min          Prescale minimum
+ * ---------------------------------------------------------------------------
+ * 0x12   0x48   [23:16]  voffset_step          Vertical offset (voltage) step
+ *               [15: 8]  voffset_max           Vertical offset (voltage) maximum
+ *               [ 7: 0]  voffset_min           Vertical offset (voltage) minimum
+ * ---------------------------------------------------------------------------
+ * 0x13   0x4c   [27:16]  hoffset_max           Horizontal offset (time) maximum
+ *               [11: 0]  hoffset_min           Horizontal offset (time) minimum
+ * ---------------------------------------------------------------------------
+ * 0x14   0x50   [11: 0]  hoffset_step          Horizontal offset (time) step
+ * ---------------------------------------------------------------------------
+ * 0x15   0x54   [31: 0]  start_address         Buffer start address
+ * ---------------------------------------------------------------------------
+ * 0x16   0x58   [15: 0]  hsize                 Buffer horizontal size
+ * ---------------------------------------------------------------------------
+ * 0x17   0x5c   [31:16]  hmax                  Buffer horizontal maximum
+ *               [15: 0]  hmin                  Buffer horizontal minimum
+ * ---------------------------------------------------------------------------
+ * 0x18   0x60   [ 5: 5]  ovf                   Buffer overflow (RW1C)
+ *               [ 4: 4]  unf                   Buffer underflow (RW1C)
+ *               [ 3: 3]  error                 Target Error (RW1C)
+ *               [ 2: 2]  ready                 Target Ready (RW1C)
+ *               [ 1: 1]  mb_state              MB busy (0x1) or idle (0x0)
+ *               [ 0: 0]  es_state              ES busy (0x1) or idle (0x0)
  */
 
 #ifndef ADI_JESD204B_H_
@@ -44,11 +84,11 @@
 
 /* PCORE CoreFPGA register map */
 
-#define AXI_JESD204B_REG_VERSION	0x00
+#define AXI_JESD204B_REG_VERSION		0x00
 #define AXI_JESD204B_REG_CTRL		0x04
 #define AXI_JESD204B_REG_SYSREF_TRIG	0x08
-#define AXI_JESD204B_REG_FRMCTRL	0x0C
-#define AXI_JESD204B_REG_BUFDELAY	0x10
+#define AXI_JESD204B_REG_FRMCTRL		0x0C
+#define AXI_JESD204B_REG_BUFDELAY		0x10
 #define AXI_JESD204B_REG_TEST_MODE	0x14
 #define AXI_JESD204B_REG_BUFCNT		0x18
 #define AXI_JESD204B_REG_INIT_DATA0	0x1C
@@ -58,30 +98,100 @@
 #define AXI_JESD204B_REG_TEST_MFCNT	0x2C
 #define AXI_JESD204B_REG_TEST_ILACNT	0x30
 #define AXI_JESD204B_REG_TEST_ERRCNT	0x34
+#define AXI_JESD204B_REG_ES_CTRL		0x40
+#define AXI_JESD204B_REG_ES_PRESCALE	0x44
+#define AXI_JESD204B_REG_ES_VOFFSET	0x48
+#define AXI_JESD204B_REG_ES_HOFFSET	0x4C
+#define AXI_JESD204B_REG_ES_HOFFSET_STP	0x50
+#define AXI_JESD204B_REG_ES_START_ADDR	0x54
+#define AXI_JESD204B_REG_ES_HSIZE		0x58
+#define AXI_JESD204B_REG_ES_HMAXMIN	0x5C
+#define AXI_JESD204B_REG_ES_STATUS	0x60
 
 /* AXI_JESD204B_REG_CTRL */
-#define AXI_JESD204B_CTRL_ERR_DIS	(1 << 0)
-#define AXI_JESD204B_CTRL_SYSREF_EN	(1 << 1)
-#define AXI_JESD204B_CTRL_SCR_EN	(1 << 2)
-#define AXI_JESD204B_CTRL_LANESYNC_EN	(1 << 3)
-#define AXI_JESD204B_CTRL_SYSREF_SW_EN	(1 << 4)
-#define AXI_JESD204B_CTRL_SYSREF_HW_EN	(0 << 4)
+#define AXI_JESD204B_CTRL_ERR_DIS			(1 << 0)
+#define AXI_JESD204B_CTRL_SYSREF_EN		(1 << 1)
+#define AXI_JESD204B_CTRL_SCR_EN			(1 << 2)
+#define AXI_JESD204B_CTRL_LANESYNC_EN		(1 << 3)
+#define AXI_JESD204B_CTRL_SYSREF_SW_EN		(1 << 4)
+#define AXI_JESD204B_CTRL_SYSREF_HW_EN		(0 << 4)
 
 /* AXI_JESD204B_REG_SYSREF_TRIG */
-#define AXI_JESD204B_SYSREF_TRIG	(1 << 0)
+#define AXI_JESD204B_SYSREF_TRIG			(1 << 0)
 
 /* AXI_JESD204B_REG_FRMCTRL */
-#define AXI_JESD204B_FRMCTRL_FRMCNT(x)	((x) << 8)
-#define AXI_JESD204B_FRMCTRL_BYTECNT(x)	(((x) & 0xFF) << 0)
+#define AXI_JESD204B_FRMCTRL_FRMCNT(x)		((x) << 8)
+#define AXI_JESD204B_FRMCTRL_BYTECNT(x)		(((x) & 0xFF) << 0)
 
 /* AXI_JESD204B_REG_BUFDELAY */
-#define AXI_JESD204B_BUFDELAY(x)	((x) << 0)
+#define AXI_JESD204B_BUFDELAY(x)			((x) << 0)
 
 /* AXI_JESD204B_REG_TEST_MODE */
-#define AXI_JESD204B_REG_TEST_MODE_LS(x)	((x) << 0)
-#define AXI_JESD204B_REG_TEST_MODE_EN(x)	((x) << 2)
+#define AXI_JESD204B_REG_TEST_MODE_LS(x)		((x) << 0)
+#define AXI_JESD204B_REG_TEST_MODE_EN(x)		((x) << 2)
 #define AXI_JESD204B_REG_TEST_MODE_JESD_RESET	(1 << 4)
 #define AXI_JESD204B_REG_TEST_MODE_GTX_RESET	(1 << 5)
 
+/* AXI_JESD204B_REG_INIT_DATA0 */
+#define AXI_JESD204B_INIT0_DID(x)	(((x) >> 0) & 0xFF) /* DID Device ID */
+#define AXI_JESD204B_INIT0_BID(x)	(((x) >> 8) & 0xFF) /* BID Bank ID */
+#define AXI_JESD204B_INIT0_LID(x)	(((x) >> 13) & 0x1F) /* LID Lane ID */
+#define AXI_JESD204B_INIT0_L(x)		(((x) >> 18) & 0x1F) /* Number of Lanes per Device*/
+#define AXI_JESD204B_INIT0_SCR(x)	(((x) >> 23) & 0x1) /* SCR Scrambling Enabled */
+#define AXI_JESD204B_INIT0_F(x)		(((x) >> 24) & 0xFF) /* Octets per Frame */
+
+/* AXI_JESD204B_REG_INIT_DATA1 */
+
+#define AXI_JESD204B_INIT1_K(x)		(((x) >> 0) & 0x1F) /* Frames per Multiframe */
+#define AXI_JESD204B_INIT1_M(x)		(((x) >> 5) & 0xFF) /* Converters per Device */
+#define AXI_JESD204B_INIT1_N(x)		(((x) >> 13) & 0x1F) /* Converter Resolution */
+#define AXI_JESD204B_INIT1_CS(x)		(((x) >> 18) & 0x3) /* Control Bits per Sample */
+#define AXI_JESD204B_INIT1_S(x)		(((x) >> 20) & 0x1F) /* Samples per Converter per Frame Cycle */
+#define AXI_JESD204B_INIT1_ND(x)		(((x) >> 25) & 0x1F) /* Total Bits per Sample */
+#define AXI_JESD204B_INIT1_HD(x)		(((x) >> 30) & 0x1) /* High Density Format */
+
+/* AXI_JESD204B_REG_INIT_DATA2 */
+
+#define AXI_JESD204B_INIT2_FCHK(x)	(((x) >> 16) & 0xFF) /* Checksum */
+#define AXI_JESD204B_INIT2_CF(x)		(((x) >> 24) & 0x1F) /* Control Words per Frame Cycle per Link */
+
+/* AXI_JESD204B_REG_INIT_DATA3 */
+
+#define AXI_JESD204B_INIT3_ADJCNT(x)		(((x) >> 0) & 0xF) /* ADJCNT Adjustment step count */
+#define AXI_JESD204B_INIT3_PHYADJ(x)		(((x) >> 4) & 0x1) /* PHYADJ Adjustment request */
+#define AXI_JESD204B_INIT3_ADJDIR(x)		(((x) >> 5) & 0x1) /* ADJDIR Adjustment direction */
+#define AXI_JESD204B_INIT3_JESDV(x)		(((x) >> 6) & 0x7) /* JESD204 Version */
+#define AXI_JESD204B_INIT3_SUBCLASSV(x)		(((x) >> 9) & 0x7) /* JESD204 subclass version */
+
+/* AXI_JESD204B_REG_ES_CTRL */
+#define AXI_JESD204B_REG_ES_CTRL_START		(1 << 0)
+#define AXI_JESD204B_REG_ES_CTRL_NORM		(1 << 1)
+#define AXI_JESD204B_REG_ES_CTRL_RGB		(0 << 1)
+
+/* AXI_JESD204B_REG_ES_PRESCALE */
+#define AXI_JESD204B_REG_ES_PRESCALE_STEP(x)	(((x) & 0x1F) << 16)
+#define AXI_JESD204B_REG_ES_PRESCALE_MAX(x)	(((x) & 0x1F) << 8)
+#define AXI_JESD204B_REG_ES_PRESCALE_MIN(x)	((x) & 0x1F)
+
+/* AXI_JESD204B_REG_ES_VOFFSET */
+#define AXI_JESD204B_REG_ES_VOFFSET_STEP(x)	(((x) & 0xFF) << 16)
+#define AXI_JESD204B_REG_ES_VOFFSET_MAX(x)	(((x) & 0xFF) << 8)
+#define AXI_JESD204B_REG_ES_VOFFSET_MIN(x)	(((unsigned)(x)) & 0xFF)
+
+/* AXI_JESD204B_REG_ES_HOFFSET */
+#define AXI_JESD204B_REG_ES_HOFFSET_MAX(x)	(((x) & 0xFFF) << 16)
+#define AXI_JESD204B_REG_ES_HOFFSET_MIN(x)	(((unsigned)(x)) & 0xFFF)
+
+/* AXI_JESD204B_REG_ES_HMAXMIN */
+#define AXI_JESD204B_REG_ES_HMAXMIN_MAX(x)	((x) << 16)
+#define AXI_JESD204B_REG_ES_HMAXMIN_MIN(x)	((x) & 0xFFFF)
+
+/* AXI_JESD204B_REG_ES_STATUS */
+#define AXI_JESD204B_REG_ES_STATUS_ES_BUSY	(1 << 0)
+#define AXI_JESD204B_REG_ES_STATUS_MB_BUSY	(1 << 1)
+#define AXI_JESD204B_REG_ES_STATUS_READY		(1 << 2)
+#define AXI_JESD204B_REG_ES_STATUS_ERROR		(1 << 3)
+#define AXI_JESD204B_REG_ES_STATUS_BUF_UNF	(1 << 4)
+#define AXI_JESD204B_REG_ES_STATUS_BUF_OVF	(1 << 5)
 
 #endif /* ADI_JESD204B_H_ */
