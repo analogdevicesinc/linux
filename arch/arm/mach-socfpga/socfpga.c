4e95184423a54235361d199aa21e0061319ac66b
Matthew Gerlach <mgerlach@altera.com>
2013-04-05 09:10:26 -0700

    FogBugz #103239: Intermittent loss of ethernet transmission.
    This bug does not really have anything to do with ethernet.
    The problem was a memory coherency problem between the cpu
    and a dma engine, in this case the ethernet mac.  In short,
    while dma writes by the ethernet were not getting cached in L2,
    they were getting cached in L1.  The good news is a single bit
    change to the programming of the PL310 fixes the problem.
    
    Signed-off-by: Matthew Gerlach <mgerlach@altera.com>

--- arch/arm/mach-socfpga/socfpga.c
+++ arch/arm/mach-socfpga/socfpga.c
@@ -156,7 +156,8 @@ static const char *altera_dt_match[] = {
 
 DT_MACHINE_START(SOCFPGA, "Altera SOCFPGA")
 	.l2c_aux_val	= L310_AUX_CTRL_DATA_PREFETCH |
-			L310_AUX_CTRL_INSTR_PREFETCH,
+			L310_AUX_CTRL_INSTR_PREFETCH |
+			L2C_AUX_CTRL_SHARED_OVERRIDE,
 	.l2c_aux_mask	= ~0,
 	.init_irq	= socfpga_init_irq,
 	.init_machine	= socfpga_cyclone5_init,
