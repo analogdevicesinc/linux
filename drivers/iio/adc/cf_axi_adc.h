4e4feefac7b61b44d1abf251ba1d2e3f81ef45ac
Michael Hennerich <michael.hennerich@analog.com>
2013-07-08 10:10:38 +0200

    iio:adc:cf_axi_adc_core: Update to new PCORE register map and split driver more logically
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/adc/cf_axi_adc.h
+++ drivers/iio/adc/cf_axi_adc.h
@@ -11,173 +11,134 @@
 #ifndef ADI_AXI_ADC_H_
 #define ADI_AXI_ADC_H_
 
-/* PCORE CoreFPGA register map */
+#define ADI_REG_VERSION		0x0000				/*Version and Scratch Registers */
+#define ADI_VERSION(x)		(((x) & 0xffffffff) << 0)	/* RO, Version number. */
+#define VERSION_IS(x,y,z)	((x) << 16 | (y) << 8 | (z))
+#define ADI_REG_ID		0x0004			 	/*Version and Scratch Registers */
+#define ADI_ID(x)		(((x) & 0xffffffff) << 0)   	/* RO, Instance identifier number. */
+#define ADI_REG_SCRATCH		0x0008			 	/*Version and Scratch Registers */
+#define ADI_SCRATCH(x)		(((x) & 0xffffffff) << 0)	/* RW, Scratch register. */
 
-#ifdef CONFIG_AXIADC_V2
-#define AXIADC_PCORE_VERSION		0x00
-#define AXIADC_PCORE_DMA_CHAN_SEL	0x00
-#define AXIADC_PCORE_DMA_CTRL		0x04
-#define AXIADC_PCORE_DMA_STAT		0x08
-#define AXIADC_PCORE_ADC_STAT		0x10
-#define AXIADC_PCORE_PN_ERR_CTRL		0x0C
-#define AXIADC_PCORE_ADC_CTRL		0x2C
-#define AXIADC_PCORE_IDENT		0x30
-#define AXIADC_PCORE_CA_OFFS_SCALE	0x40
-#define AXIADC_PCORE_CB_OFFS_SCALE	0x44
-#define AXIADC_PCORE_USRL_DECIM		0x48
-#define AXIADC_PCORE_USRL_DTYPE		0x4C
-#define AXIADC_PCORE_USRL_MAXCH		0x50
 
-/* AXIADC_PCORE_VERSION */
-#define AXIADC_PCORE_VERSION_IS(x,y,z)	((x) << 16 | (y) << 8 | (z))
+/* ADC COMMON */
 
-/* AXIADC_PCORE_DMA_CHAN_SEL */
-#define AXIADC_PCORE_DMA_CHAN_SEL0		(1 << 0)
-#define AXIADC_PCORE_DMA_CHAN_SEL1		(1 << 1)
-#define AXIADC_PCORE_DMA_CHAN_USRL_SEL(x)	((x) << 2)
 
-/* AXIADC_PCORE_DMA_CTRL */
-#define AXIADC_DMA_CAP_EN_V10A		(1 << 31)
-#define AXIADC_DMA_CNT_V10A(x)		(((x) & 0x3FFFFFF) << 0)
+#define ADI_REG_RSTN			0x0040
+#define ADI_RSTN				(1 << 0)
 
-#define AXIADC_DMA_CAP_EN		(1 << 31)
-#define AXIADC_DMA_CAP_STREAM		(1 << 30)
-#define AXIADC_DMA_CNT(x)		(((x) & 0x3FFFFFF) << 0)
+#define ADI_REG_CNTRL			0x0044
+#define ADI_R1_MODE			(1 << 2)
+#define ADI_DDR_EDGESEL			(1 << 1)
+#define ADI_PIN_MODE			(1 << 0)
 
-/* AXIADC_PCORE_DMA_STAT */
-#define AXIADC_DMA_STAT_BUSY		(1 << 0) /* W1C */
-#define AXIADC_DMA_STAT_OVF		(1 << 1) /* W1C */
-#define AXIADC_DMA_STAT_UNF		(1 << 2) /* W1C */
+#define ADI_REG_CLK_FREQ			0x0054
+#define ADI_CLK_FREQ(x)			(((x) & 0xFFFFFFFF) << 0)
+#define ADI_TO_CLK_FREQ(x)		(((x) >> 0) & 0xFFFFFFFF)
 
-/* AXIADC_PCORE_ADC_STAT */
-#define AXIADC_PCORE_ADC_STAT_OVR0	(1 << 0) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_OVR1	(1 << 1) /* W1C */
+#define ADI_REG_CLK_RATIO		0x0058
+#define ADI_CLK_RATIO(x)			(((x) & 0xFFFFFFFF) << 0)
+#define ADI_TO_CLK_RATIO(x)		(((x) >> 0) & 0xFFFFFFFF)
 
-#define AXIADC_PCORE_ADC_STAT_PN_OOS0	(1 << 0) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_PN_OOS1	(1 << 1) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_PN_ERR0	(1 << 16) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_PN_ERR1	(1 << 17) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_MASK	0xFFFFFFFF
+#define ADI_REG_STATUS			0x005C
+#define ADI_MUX_PN_ERR			(1 << 3)
+#define ADI_MUX_PN_OOS			(1 << 2)
+#define ADI_MUX_OVER_RANGE		(1 << 1)
+#define ADI_STATUS			(1 << 0)
 
-/* AXIADC_PCORE_ADC_STAT */
-#define AXIADC_PCORE_ADC_STAT_OVR	(1 << 0) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_PN_OOS	(1 << 1) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_PN_ERR	(1 << 2) /* W1C */
-#define AXIADC_PCORE_ADC_1C_STAT_MASK	0x7
+#define ADI_REG_DELAY_CNTRL		0x0060
+#define ADI_DELAY_SEL			(1 << 17)
+#define ADI_DELAY_RWN			(1 << 16)
+#define ADI_DELAY_ADDRESS(x)		(((x) & 0xFF) << 8)
+#define ADI_TO_DELAY_ADDRESS(x)		(((x) >> 8) & 0xFF)
+#define ADI_DELAY_WDATA(x)		(((x) & 0x1F) << 0)
+#define ADI_TO_DELAY_WDATA(x)		(((x) >> 0) & 0x1F)
 
-/* AXIADC_PCORE_PN_ERR_CTRL */
-#define AXIADC_PN23_1_EN		(1 << 1)
-#define AXIADC_PN23_0_EN		(1 << 0)
-#define AXIADC_PN9_1_EN			(0 << 1)
-#define AXIADC_PN9_0_EN			(0 << 0)
-#define AXIADC_PN23_EN			(AXIADC_PN23_0_EN | AXIADC_PN23_1_EN)
-#define AXIADC_PN9_EN			(AXIADC_PN9_0_EN | AXIADC_PN9_1_EN)
+#define ADI_REG_DELAY_STATUS		0x0064
+#define ADI_DELAY_LOCKED			(1 << 9)
+#define ADI_DELAY_STATUS			(1 << 8)
+#define ADI_DELAY_RDATA(x)		(((x) & 0x1F) << 0)
+#define ADI_TO_DELAY_RDATA(x)		(((x) >> 0) & 0x1F)
 
-/* AXIADC_PCORE_ADC_CTRL */
-#define AXIADC_INPUT_FMT_OFFSET_BIN	(1 << 3)
-#define AXIADC_INPUT_FMT_TWOS_COMPL	(0 << 3)
-#define AXIADC_SCALE_OFFSET_EN		(1 << 2)
-#define AXIADC_SIGNEXTEND		(1 << 1)
-#define AXIADC_STATUS_EN		(1 << 0)
+#define ADI_REG_DRP_CNTRL		0x0070
+#define ADI_DRP_SEL			(1 << 29)
+#define ADI_DRP_RWN			(1 << 28)
+#define ADI_DRP_ADDRESS(x)		(((x) & 0xFFF) << 16)
+#define ADI_TO_DRP_ADDRESS(x)		(((x) >> 16) & 0xFFF)
+#define ADI_DRP_WDATA(x)			(((x) & 0xFFFF) << 0)
+#define ADI_TO_DRP_WDATA(x)		(((x) >> 0) & 0xFFFF)
 
-/* AXIADC_PCORE_IDENT */
-#define AXIADC_PCORE_IDENT_SLAVE	0x1
+#define ADI_REG_DRP_STATUS		0x0074
+#define ADI_DRP_STATUS			(1 << 16)
+#define ADI_DRP_RDATA(x)			(((x) & 0xFFFF) << 0)
+#define ADI_TO_DRP_RDATA(x)		(((x) >> 0) & 0xFFFF)
 
-/* AXIADC_PCORE_C[A|B]_OFFS_SCALE */
-#define AXIADC_OFFSET(x)		(((x) & 0xFFFF) << 16)
-#define AXIADC_SCALE(x)			((x) & 0xFFFF)
+#define ADI_REG_DMA_CNTRL		0x0080
+#define ADI_DMA_STREAM			(1 << 1)
+#define ADI_DMA_START			(1 << 0)
 
-/* AXIADC_PCORE_USRL_DECIM Custom User Logic Decimation (M/N) */
+#define ADI_REG_DMA_COUNT		0x0084
+#define ADI_DMA_COUNT(x)			(((x) & 0xFFFFFFFF) << 0)
+#define ADI_TO_DMA_COUNT(x)		(((x) >> 0) & 0xFFFFFFFF)
 
-#define AXIADC_PCORE_USRL_DECIM_NUM(x)		((x) >> 16)
-#define AXIADC_PCORE_USRL_DECIM_DEN(x)		((x) & 0xFFFF)
+#define ADI_REG_DMA_STATUS		0x0088
+#define ADI_DMA_OVF			(1 << 2)
+#define ADI_DMA_UNF			(1 << 1)
+#define ADI_DMA_STATUS			(1 << 0)
 
-/* AXIADC_PCORE_USRL_DTYPE Custom User Logic Data Type */
-#define AXIADC_PCORE_USRL_DTYPE_NORM		(1 << 0)
+#define ADI_REG_DMA_BUSWIDTH		0x008C
+#define ADI_DMA_BUSWIDTH(x)		(((x) & 0xFFFFFFFF) << 0)
+#define ADI_TO_DMA_BUSWIDTH(x)		(((x) >> 0) & 0xFFFFFFFF)
 
-#else
+#define ADI_REG_USR_CNTRL_1		0x00A0
+#define ADI_USR_CHANMAX(x)		(((x) & 0xFF) << 0)
+#define ADI_TO_USR_CHANMAX(x)		(((x) >> 0) & 0xFF)
 
-#define AXIADC_PCORE_VERSION		0x00
-#define AXIADC_PCORE_DMA_CHAN_SEL	0x04
-#define AXIADC_PCORE_DMA_CTRL		0x0C
-#define AXIADC_PCORE_DMA_STAT		0x10
-#define AXIADC_PCORE_ADC_STAT		0x14
-#define AXIADC_PCORE_PN_ERR_CTRL	0x24
-#define AXIADC_PCORE_ADC_CTRL		0x2C
-#define AXIADC_PCORE_IDENT		0x30
-#define AXIADC_PCORE_CA_OFFS_SCALE	0x40
-#define AXIADC_PCORE_CB_OFFS_SCALE	0x44
-#define AXIADC_PCORE_USRL_DECIM		0x48
-#define AXIADC_PCORE_USRL_DTYPE		0x4C
-#define AXIADC_PCORE_USRL_MAXCH		0x50
+/* ADC CHANNEL */
 
-/* AXIADC_PCORE_VERSION */
-#define AXIADC_PCORE_VERSION_IS(x,y,z)	((x) << 16 | (y) << 8 | (z))
+#define ADI_REG_CHAN_CNTRL(c)		(0x0400 + (c) * 0x40)
+#define ADI_IQCOR_ENB			(1 << 9)
+#define ADI_DCFILT_ENB			(1 << 8)
+#define ADI_FORMAT_SIGNEXT		(1 << 6)
+#define ADI_FORMAT_TYPE			(1 << 5)
+#define ADI_FORMAT_ENABLE		(1 << 4)
+#define ADI_PN23_TYPE			(1 << 1)
+#define ADI_ENABLE			(1 << 0)
 
-/* AXIADC_PCORE_DMA_CHAN_SEL */
-#define AXIADC_PCORE_DMA_CHAN_SEL0		(1 << 0)
-#define AXIADC_PCORE_DMA_CHAN_SEL1		(1 << 1)
-#define AXIADC_PCORE_DMA_CHAN_USRL_SEL(x)	((x) << 2)
+#define ADI_REG_CHAN_STATUS(c)		(0x0404 + (c) * 0x40)
+#define ADI_PN_ERR			(1 << 2)
+#define ADI_PN_OOS			(1 << 1)
+#define ADI_OVER_RANGE			(1 << 0)
 
-/* AXIADC_PCORE_DMA_CTRL */
-#define AXIADC_DMA_CAP_EN_V10A		(1 << 16)
-#define AXIADC_DMA_CNT_V10A(x)		(((x) & 0xFFFF) << 0)
+#define ADI_REG_CHAN_CNTRL_1(c)		(0x0410 + (c) * 0x40)
+#define ADI_DCFILT_OFFSET(x)		(((x) & 0xFFFF) << 16)
+#define ADI_TO_DCFILT_OFFSET(x)		(((x) >> 16) & 0xFFFF)
+#define ADI_DCFILT_COEFF(x)		(((x) & 0xFFFF) << 0)
+#define ADI_TO_DCFILT_COEFF(x)		(((x) >> 0) & 0xFFFF)
 
-#define AXIADC_DMA_CAP_EN		(1 << 31)
-#define AXIADC_DMA_CAP_STREAM		(1 << 30)
-#define AXIADC_DMA_CNT(x)		(((x) & 0x3FFFFFF) << 0)
+#define ADI_REG_CHAN_CNTRL_2(c)		(0x0414 + (c) * 0x40)
+#define ADI_IQCOR_COEFF_1(x)		(((x) & 0xFFFF) << 16)
+#define ADI_TO_IQCOR_COEFF_1(x)		(((x) >> 16) & 0xFFFF)
+#define ADI_IQCOR_COEFF_2(x)		(((x) & 0xFFFF) << 0)
+#define ADI_TO_IQCOR_COEFF_2(x)		(((x) >> 0) & 0xFFFF)
 
-/* AXIADC_PCORE_DMA_STAT */
-#define AXIADC_DMA_STAT_BUSY		(1 << 0) /* W1C */
-#define AXIADC_DMA_STAT_OVF		(1 << 1) /* W1C */
-#define AXIADC_DMA_STAT_UNF		(1 << 2) /* W1C */
+#define ADI_REG_CHAN_USR_CNTRL_1(c)		(0x0420 + (c) * 0x40)
+#define ADI_USR_DATATYPE_BE			(1 << 25)
+#define ADI_USR_DATATYPE_SIGNED			(1 << 24)
+#define ADI_USR_DATATYPE_SHIFT(x)		(((x) & 0xFF) << 16)
+#define ADI_TO_USR_DATATYPE_SHIFT(x)		(((x) >> 16) & 0xFF)
+#define ADI_USR_DATATYPE_TOTAL_BITS(x)		(((x) & 0xFF) << 8)
+#define ADI_TO_USR_DATATYPE_TOTAL_BITS(x)	(((x) >> 8) & 0xFF)
+#define ADI_USR_DATATYPE_BITS(x)			(((x) & 0xFF) << 0)
+#define ADI_TO_USR_DATATYPE_BITS(x)		(((x) >> 0) & 0xFF)
 
-/* AXIADC_PCORE_ADC_STAT */
-#define AXIADC_PCORE_ADC_STAT_OVR0	(1 << 0) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_OVR1	(1 << 1) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_PN_OOS0	(1 << 2) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_PN_OOS1	(1 << 3) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_PN_ERR0	(1 << 4) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_PN_ERR1	(1 << 5) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_MASK	0x3F
+#define ADI_REG_CHAN_USR_CNTRL_2(c)		(0x0424 + (c) * 0x40)
+#define ADI_USR_DECIMATION_M(x)			(((x) & 0xFFFF) << 16)
+#define ADI_TO_USR_DECIMATION_M(x)		(((x) >> 16) & 0xFFFF)
+#define ADI_USR_DECIMATION_N(x)			(((x) & 0xFFFF) << 0)
+#define ADI_TO_USR_DECIMATION_N(x)		(((x) >> 0) & 0xFFFF)
 
-/* AXIADC_PCORE_ADC_STAT */
-#define AXIADC_PCORE_ADC_STAT_OVR	(1 << 0) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_PN_OOS	(1 << 1) /* W1C */
-#define AXIADC_PCORE_ADC_STAT_PN_ERR	(1 << 2) /* W1C */
-#define AXIADC_PCORE_ADC_1C_STAT_MASK	0x7
 
-/* AXIADC_PCORE_PN_ERR_CTRL */
-#define AXIADC_PN23_1_EN		(1 << 1)
-#define AXIADC_PN23_0_EN		(1 << 0)
-#define AXIADC_PN9_1_EN			(0 << 1)
-#define AXIADC_PN9_0_EN			(0 << 0)
-#define AXIADC_PN23_EN			(AXIADC_PN23_0_EN | AXIADC_PN23_1_EN)
-#define AXIADC_PN9_EN			(AXIADC_PN9_0_EN | AXIADC_PN9_1_EN)
 
-/* AXIADC_PCORE_ADC_CTRL */
-#define AXIADC_INPUT_FMT_OFFSET_BIN	(1 << 3)
-#define AXIADC_INPUT_FMT_TWOS_COMPL	(0 << 3)
-#define AXIADC_SCALE_OFFSET_EN		(1 << 2)
-#define AXIADC_SIGNEXTEND		(1 << 1)
-#define AXIADC_STATUS_EN		(1 << 0)
-
-/* AXIADC_PCORE_IDENT */
-#define AXIADC_PCORE_IDENT_SLAVE	0x1
-
-/* AXIADC_PCORE_C[A|B]_OFFS_SCALE */
-#define AXIADC_OFFSET(x)		(((x) & 0xFFFF) << 16)
-#define AXIADC_SCALE(x)			((x) & 0xFFFF)
-
-/* AXIADC_PCORE_USRL_DECIM Custom User Logic Decimation (M/N) */
-
-#define AXIADC_PCORE_USRL_DECIM_NUM(x)		((x) >> 16)
-#define AXIADC_PCORE_USRL_DECIM_DEN(x)		((x) & 0xFFFF)
-
-/* AXIADC_PCORE_USRL_DTYPE Custom User Logic Data Type */
-#define AXIADC_PCORE_USRL_DTYPE_NORM		(1 << 0)
-
-#endif
 /*
  * ADI High-Speed ADC common spi interface registers
  * See Application-Note AN-877
@@ -209,8 +170,9 @@
 #define TESTMODE_NEG_FULLSCALE		0x3
 #define TESTMODE_ALT_CHECKERBOARD	0x4
 #define TESTMODE_PN23_SEQ		0x5
-#define TESTMODE_PN9_SEQ		0x6
-#define TESTMODE_ONE_ZERO_TOGGLE	0x7
+#define TESTMODE_PN9_SEQ			0x6
+#define TESTMODE_ONE_ZERO_TOGGLE		0x7
+#define TESTMODE_RAMP			0xF
 
 /* ADC_REG_OUTPUT_MODE */
 #define OUTPUT_MODE_OFFSET_BINARY	0x0
@@ -306,10 +268,11 @@ enum {
 struct axiadc_chip_info {
 	char				name[8];
 	unsigned			num_channels;
-	const int			(*scale_table)[2];
+	int			(*scale_table)[2];
 	int				num_scales;
+	int				max_testmode;
 	unsigned long			max_rate;
-	struct iio_chan_spec		channel[17];
+	struct iio_chan_spec		channel[2];
 };
 
 struct axiadc_state {
@@ -317,13 +280,13 @@ struct axiadc_state {
 	struct mutex			lock;
 	struct completion		dma_complete;
 	struct dma_chan			*rx_chan;
-	const struct axiadc_chip_info	*chip_info;
+	struct iio_info			iio_info;
 	void __iomem			*regs;
 	void				*buf_virt;
 	dma_addr_t			buf_phys;
 	size_t				read_offs;
 	int				compl_stat;
-	unsigned				have_user_logic;
+	unsigned				max_usr_channel;
 	unsigned			adc_def_output_mode;
 	unsigned			ring_lenght;
 	unsigned			rcount;
@@ -333,6 +296,7 @@ struct axiadc_state {
 	unsigned char		testmode[2];
 	unsigned long 		adc_clk;
 	unsigned			dma_align;
+	struct iio_chan_spec	channels[16];
 };
 
 struct ad9361_rf_phy;
@@ -342,10 +306,32 @@ struct axiadc_converter {
 	struct clk 		*clk;
 	struct ad9361_rf_phy *phy;
 	unsigned			id;
+	unsigned			adc_output_mode;
+	unsigned 		testmode[2];
+	unsigned long 		adc_clk;
+	const struct axiadc_chip_info	*chip_info;
 	int		(*read)(struct spi_device *spi, unsigned reg);
 	int		(*write)(struct spi_device *spi,
 				 unsigned reg, unsigned val);
 	int		(*setup)(struct spi_device *spi, unsigned mode);
+
+	struct iio_chan_spec const	*channels;
+	int				num_channels;
+	const struct attribute_group	*attrs;
+	struct iio_dev 	*indio_dev;
+	int (*read_raw)(struct iio_dev *indio_dev,
+			struct iio_chan_spec const *chan,
+			int *val,
+			int *val2,
+			long mask);
+
+	int (*write_raw)(struct iio_dev *indio_dev,
+			 struct iio_chan_spec const *chan,
+			 int val,
+			 int val2,
+			 long mask);
+
+	int (*post_setup)(struct iio_dev *indio_dev);
 };
 
 
