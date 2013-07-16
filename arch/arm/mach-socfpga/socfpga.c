14dc3f821e56cb637a9c911d4439c32e52c603f6
Alan Tull <atull@altera.com>
2013-07-16 15:18:58 -0500

    FogBugz #102675: do not touch fpga bridge resets by default
    Previously we were bringing all FPGA-hps bridges out of reset
    without checking to see if the FPGA was programmed.  This patch
    changes the behavior to not bring the bridges out of reset
    or to set them to be in reset.
    
    Signed-off-by: Alan Tull <atull@altera.com>
    
    Conflicts:
    
    	arch/arm/mach-socfpga/socfpga.c

--- arch/arm/mach-socfpga/socfpga.c
+++ arch/arm/mach-socfpga/socfpga.c
@@ -137,9 +137,6 @@ static void __init enable_periphs(void)
 {
 	/* Release all peripherals from reset.*/
 	__raw_writel(0, rst_manager_base_addr + SOCFPGA_RSTMGR_MODPERRST);
-
-	/* Release all FPGA bridges from reset.*/
-	__raw_writel(0, rst_manager_base_addr + SOCFPGA_RSTMGR_BRGMODRST);
 }
 
 static void __init socfpga_map_io(void)
