b70654ea6bd6e7af1709339a0597cc5ce93dc032
Ley Foon Tan <lftan@altera.com>
2013-08-12 11:09:10 +0800

    FogBugz #143478: arch/arm: Move sysid from arch to drivers
    Removed sysid from arch/arm and read silicon ID from
    system manager for soc_id and revision.
    
    Signed-off-by: Ley Foon Tan <lftan@altera.com>

--- arch/arm/mach-socfpga/core.h
+++ arch/arm/mach-socfpga/core.h
@@ -34,14 +34,22 @@
 
 #define RSTMGR_MPUMODRST_CPU1		0x2     /* CPU1 Reset */
 
-#define SOCFPGA_SYSID_DEFAULT		0x1
 #define SOCFPGA_REVISION_DEFAULT	0x1
 
-/* Sysid register map */
-#define SYSID_ID_REG			0x0
+/*MPU Module Reset Register */
+#define RSTMGR_MPUMODRST_CPU0	0x1	/*CPU0 Reset*/
+#define RSTMGR_MPUMODRST_CPU1	0x2	/*CPU1 Reset*/
+#define RSTMGR_MPUMODRST_WDS		0x4	/*Watchdog Reset*/
+#define RSTMGR_MPUMODRST_SCUPER	0x8	/*SCU and periphs reset*/
+#define RSTMGR_MPUMODRST_L2		0x10	/*L2 Cache reset*/
+#define SOCFPGA_ID_DEFAULT		0x1
+#define SOCFPGA_REVISION_DEFAULT	0x1
 
-extern void socfpga_init_clocks(void);
-extern void socfpga_sysmgr_init(void);
+#define SYSMGR_SILICON_ID1_OFFSET 0x0
+#define SYSMGR_SILICON_ID1_REV_SHIFT 0
+#define SYSMGR_SILICON_ID1_REV_MASK 0x0000FFFF
+#define SYSMGR_SILICON_ID1_ID_SHIFT 16
+#define SYSMGR_SILICON_ID1_ID_MASK 0xFFFF0000
 
 extern void __iomem *sys_manager_base_addr;
 extern void __iomem *rst_manager_base_addr;
