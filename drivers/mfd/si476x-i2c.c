47aa8ff5e1e39d8cc2eeee990409c8bcd52309d8
Nicolin Chen <b42378@freescale.com>
2013-08-29 14:25:22 +0800

    MLK-11429-22: mfd: si476x: Fix power up failure
    cherry-pick below patch from imx_3.14.y
    ENGR00276567-4 mfd: si476x: Fix power up failure
    
    This's some logical error in power-up code, thus fix it.
    
    Signed-off-by: Nicolin Chen <b42378@freescale.com>
    (cherry picked from commit 77d97ad1bb77c0e3c60b9781a06b61d4b4667de1)
    (cherry picked from commit b656522da2685ef9a4da2229b6786d5cd0c12189)
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>

--- drivers/mfd/si476x-i2c.c
+++ drivers/mfd/si476x-i2c.c
@@ -294,7 +294,7 @@ int si476x_core_set_power_state(struct si476x_core *core,
 			 */
 			udelay(100);
 
-			err = si476x_core_start(core, false);
+			err = si476x_core_start(core, true);
 			if (err < 0)
 				goto disable_regulators;
 
@@ -303,7 +303,7 @@ int si476x_core_set_power_state(struct si476x_core *core,
 
 		case SI476X_POWER_DOWN:
 			core->power_state = next_state;
-			err = si476x_core_stop(core, false);
+			err = si476x_core_stop(core, true);
 			if (err < 0)
 				core->power_state = SI476X_POWER_INCONSISTENT;
 disable_regulators:
@@ -793,6 +793,9 @@ static int si476x_core_probe(struct i2c_client *client)
 	}
 
 	core->chip_id = id->driver_data;
+
+	/* Power down si476x first */
+	si476x_core_stop(core, true);
 
 	rval = si476x_core_get_revision_info(core);
 	if (rval < 0) {
