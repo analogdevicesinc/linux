13da0ad68b82bbc0834844b270e6008f187089f8
Peter Chen <peter.chen@freescale.com>
2013-09-16 16:31:24 +0800

    MLK-11340-26 usb: phy: add notify suspend and resume callback
    They are used to notify PHY that the controller enters suspend
    or finishes resume.
    
    Signed-off-by: Peter Chen <peter.chen@freescale.com>

--- include/linux/usb/phy.h
+++ include/linux/usb/phy.h
@@ -155,6 +155,12 @@ struct usb_phy {
 	 * manually detect the charger type.
 	 */
 	enum usb_charger_type (*charger_detect)(struct usb_phy *x);
+
+	int	(*notify_suspend)(struct usb_phy *x,
+			enum usb_device_speed speed);
+	int	(*notify_resume)(struct usb_phy *x,
+			enum usb_device_speed speed);
+
 };
 
 /* for board-specific init logic */
@@ -330,6 +336,24 @@ usb_phy_notify_disconnect(struct usb_phy *x, enum usb_device_speed speed)
 {
 	if (x && x->notify_disconnect)
 		return x->notify_disconnect(x, speed);
+	else
+		return 0;
+}
+
+static inline int usb_phy_notify_suspend
+	(struct usb_phy *x, enum usb_device_speed speed)
+{
+	if (x && x->notify_suspend)
+		return x->notify_suspend(x, speed);
+	else
+		return 0;
+}
+
+static inline int usb_phy_notify_resume
+	(struct usb_phy *x, enum usb_device_speed speed)
+{
+	if (x && x->notify_resume)
+		return x->notify_resume(x, speed);
 	else
 		return 0;
 }
