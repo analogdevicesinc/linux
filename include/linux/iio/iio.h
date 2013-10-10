d415db05cd34ccccc0881280b9bec08daeb24b39
Lars-Peter Clausen <lars@metafoo.de>
2013-10-10 11:08:18 +0200

    iio: Add output buffer support
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- include/linux/iio/iio.h
+++ include/linux/iio/iio.h
@@ -332,6 +332,11 @@ static inline bool iio_channel_has_available(const struct iio_chan_spec *chan,
 s64 iio_get_time_ns(const struct iio_dev *indio_dev);
 unsigned int iio_get_time_res(const struct iio_dev *indio_dev);
 
+enum iio_device_direction {
+	IIO_DEVICE_DIRECTION_IN,
+	IIO_DEVICE_DIRECTION_OUT,
+};
+
 /* Device operating modes */
 #define INDIO_DIRECT_MODE		0x01
 #define INDIO_BUFFER_TRIGGERED		0x02
@@ -542,6 +547,8 @@ struct iio_dev {
 	int				modes;
 	int				currentmode;
 	struct device			dev;
+
+	enum iio_device_direction	direction;
 
 	struct iio_buffer		*buffer;
 	int				scan_bytes;
