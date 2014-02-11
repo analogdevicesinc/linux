3390711035320296901d67e9eff3233583fd1182
Paul Cercueil <paul.cercueil@analog.com>
2014-02-11 17:02:11 +0100

    iio: ad5064: Explicitly configure whether to use external supply
    Previously the driver would revert to internal supply if the
    external supply couldn't be found. This had multiple problems:
    - it caused silently ignored errors when a regulator was intended
      to be supplied, but was not specified correctly.
    - if CONFIG_REGULATOR is disabled, regulator_get() will always
      return a dummy regulator, which caused a device to always use
      the external vref mode, even though there is none.
    
    This patch addresses the issue by adding a platform data structure,
    containing a boolean field use_external_ref. If the platform data
    structure is present and if that boolean is set, the external vref
    is used; otherwise the internal vref is used.
    
    In the case where an external vref is wanted but regulator_get()
    fails, the driver no longer reverts to using the internal vref,
    but returns an error instead.
    
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

--- drivers/iio/dac/ad5064.c
+++ drivers/iio/dac/ad5064.c
@@ -5,7 +5,7 @@
  * LTC2617, LTC2619, LTC2626, LTC2627, LTC2629, LTC2631, LTC2633, LTC2635
  * Digital to analog converters driver
  *
- * Copyright 2011 Analog Devices Inc.
+ * Copyright 2011, 2014 Analog Devices Inc.
  *
  * Licensed under the GPL-2.
  */
@@ -23,6 +23,8 @@
 
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
+
+#include <linux/platform_data/ad5064.h>
 
 #define AD5064_MAX_DAC_CHANNELS			8
 #define AD5064_MAX_VREFS			4
@@ -811,6 +813,7 @@ static int ad5064_set_config(struct ad5064_state *st, unsigned int val)
 static int ad5064_probe(struct device *dev, enum ad5064_type type,
 			const char *name, ad5064_write_func write)
 {
+	struct ad5064_platform_data *pdata = dev->platform_data;
 	struct iio_dev *indio_dev;
 	struct ad5064_state *st;
 	unsigned int midscale;
@@ -831,11 +834,20 @@ static int ad5064_probe(struct device *dev, enum ad5064_type type,
 	for (i = 0; i < ad5064_num_vref(st); ++i)
 		st->vref_reg[i].supply = ad5064_vref_name(st, i);
 
-	ret = devm_regulator_bulk_get(dev, ad5064_num_vref(st),
-		st->vref_reg);
-	if (ret) {
-		if (!st->chip_info->internal_vref)
+	if (pdata && pdata->use_external_ref) {
+		ret = devm_regulator_bulk_get(dev, ad5064_num_vref(st),
+					st->vref_reg);
+		if (ret)
 			return ret;
+		ret = regulator_bulk_enable(ad5064_num_vref(st), st->vref_reg);
+		if (ret)
+			return ret;
+	} else {
+		if (!st->chip_info->internal_vref) {
+			dev_err(dev, "No vref available\n");
+			return -ENXIO;
+		}
+
 		st->use_internal_vref = true;
 		ret = ad5064_set_config(st, AD5064_CONFIG_INT_VREF_ENABLE);
 		if (ret) {
@@ -843,10 +855,6 @@ static int ad5064_probe(struct device *dev, enum ad5064_type type,
 				ret);
 			return ret;
 		}
-	} else {
-		ret = regulator_bulk_enable(ad5064_num_vref(st), st->vref_reg);
-		if (ret)
-			return ret;
 	}
 
 	indio_dev->dev.parent = dev;
