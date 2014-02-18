c49aa7c1230fb676cf71b7e41062ae93782caff8
Dinh Nguyen <dinguyen@altera.com>
2014-02-18 15:58:20 -0600

    FogBugz #184646: Turn on all peripheral clocks for a system reboot
    When doing a software reboot, all peripheral clocks must get turned on for the
    L3 interconnect to work.
    
    This code is needed when doing a "reboot" from user-space and a peripheral
    clock as been gated off. Why would a peripheral clock get gated? An example
    use case would be a .ko that gets insmod and rmmod during runtime. The insmod
    would turn on the IP's clock, and the rmmod would turn off the IP's clock.
    Doing a "reboot" would cause the system to hang.
    
    Signed-off-by: Dinh Nguyen <dinguyen@altera.com>
    
    Conflicts:
    
    	arch/arm/mach-socfpga/core.h

--- arch/arm/mach-socfpga/socfpga.c
+++ arch/arm/mach-socfpga/socfpga.c
@@ -33,6 +33,7 @@ void __iomem *sys_manager_base_addr;
 void __iomem *rst_manager_base_addr;
 void __iomem *sdr_ctl_base_addr;
 unsigned long socfpga_cpu1start_addr;
+void __iomem *clkmgr_base_addr;
 
 static void __init socfpga_soc_device_init(void)
 {
@@ -110,6 +111,10 @@ void __init socfpga_sysmgr_init(void)
 	np = of_find_compatible_node(NULL, NULL, "altr,rst-mgr");
 	rst_manager_base_addr = of_iomap(np, 0);
 
+	np = of_find_compatible_node(NULL, NULL, "altr,clk-mgr");
+	clkmgr_base_addr = of_iomap(np, 0);
+	WARN_ON(!clkmgr_base_addr);
+
 	np = of_find_compatible_node(NULL, NULL, "altr,sdr-ctl");
 	sdr_ctl_base_addr = of_iomap(np, 0);
 }
@@ -128,6 +133,9 @@ static void __init socfpga_init_irq(void)
 static void socfpga_cyclone5_restart(enum reboot_mode mode, const char *cmd)
 {
 	u32 temp;
+
+	/* Turn on all periph PLL clocks */
+	writel(0xffff, clkmgr_base_addr + SOCFPGA_ENABLE_PLL_REG);
 
 	temp = readl(rst_manager_base_addr + SOCFPGA_RSTMGR_CTRL);
 
