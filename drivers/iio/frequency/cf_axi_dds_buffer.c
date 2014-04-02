795236f108f84431119dbfe407079cf02a875b8c
Lars-Peter Clausen <lars@metafoo.de>
2014-04-02 14:22:49 +0200

    iio: Cleanup cf_axi_dds
    Major cleanup of the cf_axi_dds driver:
    	* Merge cf_axi_dds_so and cf_axi_dds
    	* Isolate buffer implementation to the buffer source file
    	* Fix channel registration
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/iio/frequency/cf_axi_dds_buffer.c
+++ drivers/iio/frequency/cf_axi_dds_buffer.c
@@ -8,11 +8,10 @@
 
 #include <linux/slab.h>
 #include <linux/kernel.h>
-#include <linux/poll.h>
 #include <linux/io.h>
 #include <linux/dma-mapping.h>
 #include <linux/dmaengine.h>
-#include <linux/spi/spi.h>
+#include <linux/uaccess.h>
 
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
@@ -21,117 +20,87 @@
 #include "../../staging/iio/ring_hw.h"
 #include "cf_axi_dds.h"
 
+#include <linux/amba/xilinx_dma.h>
+
 #define VDMA_MAX_HSIZE	0xFFFF
 #define VDMA_MAX_VSIZE	0xFFF
 
+struct dds_buffer {
+	struct iio_buffer iio_buffer;
+	unsigned int length;
+	struct dma_chan *chan;
 
-static int __cf_axi_dds_hw_buffer_state_set(struct iio_dev *indio_dev, bool state);
+	void *buf_virt;
+	dma_addr_t buf_phys;
 
-static int cf_axi_dds_write_buffer(struct iio_buffer *r,
-				      size_t count, const char __user *buf)
+	struct iio_dev *indio_dev;
+	struct xilinx_dma_config dma_config;
+};
+
+static struct dds_buffer *iio_buffer_to_dds_buffer(struct iio_buffer *buf)
 {
-	struct iio_hw_buffer *hw_buffer = iio_to_hw_buf(r);
-	struct iio_dev *indio_dev = hw_buffer->private;
-	struct cf_axi_dds_state *st = iio_priv(indio_dev);
-	int ret = 0;
-
-	if (PAGE_ALIGN(count) > AXIDDS_MAX_DMA_SIZE || count % 8) {
-		ret = -EINVAL;
-		goto error_ret;
-	}
-
-	mutex_lock(&indio_dev->mlock);
-
-	if (copy_from_user (st->buf_virt, buf, count))
-		ret = -EFAULT;
-
-	st->txcount = count;
-	st->buffer_length = count;
-
-	if (iio_buffer_enabled(indio_dev)) {
-		dmaengine_terminate_all(st->tx_chan);
-		__cf_axi_dds_hw_buffer_state_set(indio_dev, 1);
-	}
-
-	mutex_unlock(&indio_dev->mlock);
-
-error_ret:
-
-	return ret < 0 ? ret : count;
+	return container_of(buf, struct dds_buffer, iio_buffer);
 }
 
-static int cf_axi_dds_buffer_get_length(struct iio_buffer *r)
+static int dds_buffer_enable(struct iio_buffer *buf, struct iio_dev *indio_dev)
 {
-	struct iio_hw_buffer *hw_buffer = iio_to_hw_buf(r);
-	struct iio_dev *indio_dev = hw_buffer->private;
+	struct dds_buffer *dds_buffer = iio_buffer_to_dds_buffer(buf);
 	struct cf_axi_dds_state *st = iio_priv(indio_dev);
+	struct dma_async_tx_descriptor *desc;
+	unsigned int x, cnt;
 
-	return st->buffer_length;
-}
+	if (!st->has_fifo_interface) {
+		cnt = dds_buffer->length;
+		x = 1;
 
-static int cf_axi_dds_buffer_set_length(struct iio_buffer *r, int length)
-{
-	struct iio_hw_buffer *hw_buffer = iio_to_hw_buf(r);
-	struct cf_axi_dds_state *st = iio_priv(hw_buffer->private);
+		do {
+			if ((cnt <= VDMA_MAX_HSIZE) && ((cnt % 8) == 0))
+				break;
 
-	st->buffer_length = length;
+			cnt = dds_buffer->length / ++x;
+
+		} while (x < VDMA_MAX_VSIZE);
+
+		if (x > VDMA_MAX_VSIZE || cnt * x != dds_buffer->length || ((cnt % 8) != 0)) {
+			dev_err(indio_dev->dev.parent, "Buffer size doesn't fit VDMA\n");
+			return -EINVAL;
+		}
+
+		dds_buffer->dma_config.vsize = x;
+		dds_buffer->dma_config.stride = dds_buffer->dma_config.hsize = cnt;
+
+		dmaengine_device_control(dds_buffer->chan, DMA_SLAVE_CONFIG,
+			(unsigned long)&dds_buffer->dma_config);
+	}
+
+	desc = dmaengine_prep_slave_single(dds_buffer->chan,
+		dds_buffer->buf_phys, dds_buffer->length, DMA_MEM_TO_DEV, 0);
+	if (!desc) {
+		dev_err(indio_dev->dev.parent, "Failed to prepare DMA descriptor\n");
+		return -ENOMEM;
+	}
+
+	dmaengine_submit(desc);
+	dma_async_issue_pending(dds_buffer->chan);
 
 	return 0;
 }
 
-static int cf_axi_dds_buffer_get_bytes_per_datum(struct iio_buffer *r)
+static int dds_buffer_disable(struct iio_buffer *buf, struct iio_dev *indio_dev)
 {
-	return r->bytes_per_datum;
+
+	struct dds_buffer *dds_buffer = iio_buffer_to_dds_buffer(buf);
+
+	dmaengine_terminate_all(dds_buffer->chan);
+
+	return 0;
 }
 
-static IIO_BUFFER_ENABLE_ATTR;
-static IIO_BUFFER_LENGTH_ATTR;
-
-static struct attribute *cf_axi_dds_buffer_attributes[] = {
-	&dev_attr_length.attr,
-	&dev_attr_enable.attr,
-	NULL,
-};
-
-static struct attribute_group cf_axi_dds_buffer_attr = {
-	.attrs = cf_axi_dds_buffer_attributes,
-	.name = "buffer",
-};
-
-static struct iio_buffer *cf_axi_dds_rb_allocate(struct iio_dev *indio_dev)
+static int dds_buffer_state_set(struct iio_dev *indio_dev, bool state)
 {
-	struct iio_buffer *buf;
-	struct iio_hw_buffer *ring;
-
-	ring = kzalloc(sizeof *ring, GFP_KERNEL);
-	if (!ring)
-		return NULL;
-
-	ring->private = indio_dev;
-	buf = &ring->buf;
-	buf->attrs = &cf_axi_dds_buffer_attr;
-	iio_buffer_init(buf);
-
-	return buf;
-}
-
-static inline void cf_axi_dds_rb_free(struct iio_buffer *r)
-{
-	kfree(iio_to_hw_buf(r));
-}
-
-static const struct iio_buffer_access_funcs cf_axi_dds_buffer_access_funcs = {
-	.write = &cf_axi_dds_write_buffer,
-	.get_length = &cf_axi_dds_buffer_get_length,
-	.set_length = &cf_axi_dds_buffer_set_length,
-	.get_bytes_per_datum = &cf_axi_dds_buffer_get_bytes_per_datum,
-};
-
-static int __cf_axi_dds_hw_buffer_state_set(struct iio_dev *indio_dev, bool state)
-{
+	struct dds_buffer *dds_buffer = iio_buffer_to_dds_buffer(indio_dev->buffer);
 	struct cf_axi_dds_state *st = iio_priv(indio_dev);
-	struct dma_async_tx_descriptor *desc;
-	unsigned tmp_reg, x, cnt;
+	unsigned tmp_reg;
 
 #if 0
 	tmp_reg = dds_read(st, CF_AXI_DDS_DMA_STAT);
@@ -147,110 +116,180 @@ static int __cf_axi_dds_hw_buffer_state_set(struct iio_dev *indio_dev, bool stat
 //		cf_axi_dds_stop(st);
 		tmp_reg |= DATA_SEL_DDS;
 		dds_write(st, ADI_REG_CNTRL_2, tmp_reg);
-		dmaengine_terminate_all(st->tx_chan);
 		return 0;
-	}
-
-	if (st->txcount == 0) {
-		return -EINVAL;
 	}
 
 	tmp_reg |= DATA_SEL_DMA;
 
 	dds_write(st, ADI_REG_CNTRL_1, 0);
-	st->enable = 0;
+	st->enable = false;
 
-	if (!st->has_fifo_interface) {
-		cnt = st->txcount;
-		x = 1;
-
-		do {
-			if ((cnt <= VDMA_MAX_HSIZE) && ((cnt % 8) == 0))
-				break;
-
-			cnt = st->txcount / ++x;
-
-		} while (x < VDMA_MAX_VSIZE);
-
-		if (x > VDMA_MAX_VSIZE || cnt * x != st->txcount || ((cnt % 8) != 0)) {
-			dev_err(indio_dev->dev.parent, "Buffer size doesn't fit VDMA\n");
-			return -EINVAL;
-		}
-
-		st->dma_config.vsize = x;
-		st->dma_config.stride = st->dma_config.hsize = cnt;
-
-		dmaengine_device_control(st->tx_chan, DMA_SLAVE_CONFIG,
-			(unsigned long)&st->dma_config);
-
-		dds_write(st, ADI_REG_VDMA_FRMCNT, st->txcount);
-	}
-
-	desc = dmaengine_prep_slave_single(st->tx_chan,
-		st->buf_phys,
-		st->txcount,
-		DMA_MEM_TO_DEV, 0);
-	if (!desc) {
-		dev_err(indio_dev->dev.parent, "Failed to prepare DMA descriptor\n");
-		return -ENOMEM;
-	} else {
-		dmaengine_submit(desc);
-		dma_async_issue_pending(st->tx_chan);
-	}
+	if (!st->has_fifo_interface)
+		dds_write(st, ADI_REG_VDMA_FRMCNT, dds_buffer->length);
 
 	dds_write(st, ADI_REG_CNTRL_2, tmp_reg);
 	dds_write(st, ADI_REG_VDMA_STATUS, ADI_VDMA_OVF | ADI_VDMA_UNF);
 	dds_write(st, ADI_REG_CNTRL_1, ADI_ENABLE);
-	st->enable = 1;
+	st->enable = true;
 
 	return 0;
-
 }
 
-static int cf_axi_dds_hw_buffer_preenable(struct iio_dev *indio_dev)
+static int dds_buffer_write(struct iio_buffer *buf, size_t count,
+	const char __user *user_buffer)
 {
-	return __cf_axi_dds_hw_buffer_state_set(indio_dev, 1);
+	struct dds_buffer *dds_buffer = iio_buffer_to_dds_buffer(buf);
+	bool enabled = iio_buffer_enabled(dds_buffer->indio_dev);
+	int ret = 0;
+
+	if (PAGE_ALIGN(count) > AXIDDS_MAX_DMA_SIZE || count % 8)
+		return -EINVAL;
+
+	if (enabled)
+		dds_buffer_disable(buf, dds_buffer->indio_dev);
+
+	if (copy_from_user(dds_buffer->buf_virt, user_buffer, count))
+		ret = -EFAULT;
+
+	dds_buffer->length = count;
+
+	if (enabled) {
+		dds_buffer_state_set(dds_buffer->indio_dev, true);
+		dds_buffer_enable(buf, dds_buffer->indio_dev);
+	}
+
+	return ret < 0 ? ret : count;
 }
 
-static int cf_axi_dds_hw_buffer_postdisable(struct iio_dev *indio_dev)
+static int dds_buffer_get_length(struct iio_buffer *buf)
 {
-	return __cf_axi_dds_hw_buffer_state_set(indio_dev, 0);
+	struct dds_buffer *dds_buffer = iio_buffer_to_dds_buffer(buf);
+
+	return dds_buffer->length;
 }
 
-static const struct iio_buffer_setup_ops cf_axi_dds_buffer_setup_ops = {
-	.preenable = &cf_axi_dds_hw_buffer_preenable,
-	.postdisable = &cf_axi_dds_hw_buffer_postdisable,
+static int dds_buffer_set_length(struct iio_buffer *buf, int length)
+{
+	struct dds_buffer *dds_buffer = iio_buffer_to_dds_buffer(buf);
+
+	dds_buffer->length = length;
+
+	return 0;
+}
+
+static int dds_buffer_get_bytes_per_datum(struct iio_buffer *buf)
+{
+	return buf->bytes_per_datum;
+}
+
+static IIO_BUFFER_ENABLE_ATTR;
+static IIO_BUFFER_LENGTH_ATTR;
+
+static struct attribute *dds_buffer_attributes[] = {
+	&dev_attr_length.attr,
+	&dev_attr_enable.attr,
+	NULL,
+};
+
+static struct attribute_group dds_buffer_attrs = {
+	.attrs = dds_buffer_attributes,
+	.name = "buffer",
+};
+
+static void dds_buffer_release(struct iio_buffer *buf)
+{
+	struct dds_buffer *dds_buffer = iio_buffer_to_dds_buffer(buf);
+	kfree(dds_buffer);
+}
+
+static const struct iio_buffer_access_funcs dds_buffer_access_funcs = {
+	.write = &dds_buffer_write,
+	.get_length = &dds_buffer_get_length,
+	.set_length = &dds_buffer_set_length,
+	.get_bytes_per_datum = &dds_buffer_get_bytes_per_datum,
+	.enable = dds_buffer_enable,
+	.disable = dds_buffer_disable,
+	.release = dds_buffer_release,
+};
+
+static int dds_buffer_preenable(struct iio_dev *indio_dev)
+{
+	return dds_buffer_state_set(indio_dev, 1);
+}
+
+static int dds_buffer_postdisable(struct iio_dev *indio_dev)
+{
+	return dds_buffer_state_set(indio_dev, 0);
+}
+
+static const struct iio_buffer_setup_ops dds_buffer_setup_ops = {
+	.preenable = &dds_buffer_preenable,
+	.postdisable = &dds_buffer_postdisable,
 };
 
 int cf_axi_dds_configure_buffer(struct iio_dev *indio_dev)
 {
 	struct cf_axi_dds_state *st = iio_priv(indio_dev);
-	indio_dev->buffer = cf_axi_dds_rb_allocate(indio_dev);
-	if (indio_dev->buffer == NULL)
+	struct device *dev = indio_dev->dev.parent;
+	struct dds_buffer *dds_buffer;
+	int ret;
+
+	if (st->dp_disable)
+		return 0;
+
+	dds_buffer = kzalloc(sizeof(*dds_buffer), GFP_KERNEL);
+	if (!dds_buffer)
 		return -ENOMEM;
+
+	dds_buffer->iio_buffer.attrs = &dds_buffer_attrs;
+	dds_buffer->iio_buffer.access = &dds_buffer_access_funcs;
+	iio_buffer_init(&dds_buffer->iio_buffer);
+	dds_buffer->indio_dev = indio_dev;
+
+	dds_buffer->chan = dma_request_slave_channel(dev, "tx");
+	if (!dds_buffer->chan) {
+		dev_err(dev, "failed to find DMA device\n");
+		ret = -ENODEV;
+		goto err_free;
+	}
+
+	dds_buffer->buf_virt = dma_alloc_coherent(indio_dev->dev.parent,
+		PAGE_ALIGN(AXIDDS_MAX_DMA_SIZE), &dds_buffer->buf_phys,
+		GFP_KERNEL);
+	if (dds_buffer->buf_virt == NULL) {
+		ret = -ENOMEM;
+		goto err_release_dma;
+	}
 
 	indio_dev->modes |= INDIO_BUFFER_HARDWARE;
 	indio_dev->direction = IIO_DEVICE_DIRECTION_OUT;
-	indio_dev->buffer->access = &cf_axi_dds_buffer_access_funcs;
-	indio_dev->setup_ops = &cf_axi_dds_buffer_setup_ops;
+	indio_dev->setup_ops = &dds_buffer_setup_ops;
 
-	st->buf_virt = dma_alloc_coherent(indio_dev->dev.parent,
-					  PAGE_ALIGN(AXIDDS_MAX_DMA_SIZE), &st->buf_phys,
-					  GFP_KERNEL);
-	if (st->buf_virt == NULL) {
-		dev_err(indio_dev->dev.parent,
-			"Failed to allocate a dma memory\n");
-		return -ENOMEM;
-	}
+	iio_device_attach_buffer(indio_dev, &dds_buffer->iio_buffer);
 
 	return 0;
+err_release_dma:
+	dma_release_channel(dds_buffer->chan);
+err_free:
+	kfree(dds_buffer);
+
+	return ret;
 }
 
 void cf_axi_dds_unconfigure_buffer(struct iio_dev *indio_dev)
 {
 	struct cf_axi_dds_state *st = iio_priv(indio_dev);
+	struct dds_buffer *dds_buffer;
 
-	dma_free_coherent(indio_dev->dev.parent, PAGE_ALIGN(AXIDDS_MAX_DMA_SIZE),
-			  st->buf_virt, st->buf_phys);
-	cf_axi_dds_rb_free(indio_dev->buffer);
+	if (st->dp_disable)
+		return;
+
+	dds_buffer = iio_buffer_to_dds_buffer(indio_dev->buffer);
+
+	dma_release_channel(dds_buffer->chan);
+
+	dma_free_coherent(indio_dev->dev.parent,
+		PAGE_ALIGN(AXIDDS_MAX_DMA_SIZE), dds_buffer->buf_virt,
+		dds_buffer->buf_phys);
+	iio_buffer_put(indio_dev->buffer);
 }
