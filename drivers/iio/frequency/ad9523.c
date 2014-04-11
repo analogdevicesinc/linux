013276a22fee48b32253f62d6b37046684f2c20b
Lars-Peter Clausen <lars@metafoo.de>
2014-04-11 12:48:13 +0200

    iio: ad9523: Fix potential buffer overflow
    The maximum number of characters to copy should obviously be the size of the
    destination buffer, not the size of the source buffer. Also use strlcpy to make
    sure that the string is zero terminated.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/iio/frequency/ad9523.c
+++ drivers/iio/frequency/ad9523.c
@@ -1455,8 +1455,8 @@ static struct ad9523_platform_data *ad9523_parse_dt(struct device *dev)
 		pdata->channels[cnt].channel_divider = tmp;
 		ret = of_property_read_string(chan_np, "adi,extended-name", &str);
 		if (ret >= 0)
-			strncpy(&pdata->channels[cnt].extended_name[0],
-				str, SPI_NAME_SIZE - 1);
+			strlcpy(pdata->channels[cnt].extended_name,
+				str, sizeof(pdata->channels[cnt].extended_name));
 
 		cnt++;
 	}
