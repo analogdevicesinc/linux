0a8886f6ad3db52292c1a7a6ffe24eecde11cf39
Li Jun <B47624@freescale.com>
2014-09-27 20:10:43 +0800

    MLK-9637-3 usb: chipidea: udc: test: handle otg_srp_reqd set feature request
    This patch adds otg_srp_reqd set feature request from A host for chipidea
    udc driver.
    
    Acked-by: Peter Chen <peter.chen@freescale.com>
    Signed-off-by: Li Jun <b47624@freescale.com>
    (cherry picked from commit 5edca27c518e8a69accb429c946c19243e156656)
    (cherry picked from commit 14ac2f1f74ce0b02e03296472e7d170c45a31f29)

--- drivers/usb/chipidea/udc.c
+++ drivers/usb/chipidea/udc.c
@@ -752,6 +752,9 @@ __acquires(ci->lock)
 {
 	int retval;
 
+	if (ci_otg_is_fsm_mode(ci))
+		ci->fsm.otg_srp_reqd = 0;
+
 	spin_unlock(&ci->lock);
 	if (ci->gadget.speed != USB_SPEED_UNKNOWN)
 		usb_gadget_udc_reset(&ci->gadget, ci->driver);
@@ -1017,6 +1020,16 @@ static int otg_a_alt_hnp_support(struct ci_hdrc *ci)
 	return isr_setup_status_phase(ci);
 }
 
+static int otg_srp_reqd(struct ci_hdrc *ci)
+{
+	if (ci_otg_is_fsm_mode(ci)) {
+		ci->fsm.otg_srp_reqd = 1;
+		return isr_setup_status_phase(ci);
+	} else {
+		return -ENOTSUPP;
+	}
+}
+
 /**
  * isr_setup_packet_handler: setup packet handler
  * @ci: UDC descriptor
@@ -1139,6 +1152,9 @@ __acquires(ci->lock)
 					ci->test_mode = tmode;
 					err = isr_setup_status_phase(
 							ci);
+					break;
+				case TEST_OTG_SRP_REQD:
+					err = otg_srp_reqd(ci);
 					break;
 				default:
 					break;
