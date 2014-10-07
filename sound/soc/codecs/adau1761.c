1c4ae3f5cd9549ad7d7cec7441abcab7175afa99
Lars-Peter Clausen <lars@metafoo.de>
2014-10-07 15:23:46 +0200

    ASoC: adau1761: Hardcode ZED board platform data
    In the absence of devicetree support for the ADAU1761 hardcode the platform
    data settings for the ZED board for now.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- sound/soc/codecs/adau1761.c
+++ sound/soc/codecs/adau1761.c
@@ -900,12 +900,23 @@ static struct snd_soc_dai_driver adau1761_dai_driver = {
 	.ops = &adau17x1_dai_ops,
 };
 
+static struct adau1761_platform_data zed_pdata = {
+	.input_differential = true,
+	.lineout_mode = ADAU1761_OUTPUT_MODE_LINE,
+	.headphone_mode = ADAU1761_OUTPUT_MODE_HEADPHONE,
+	.digmic_jackdetect_pin_mode = ADAU1761_DIGMIC_JACKDET_PIN_MODE_NONE,
+};
+
 int adau1761_probe(struct device *dev, struct regmap *regmap,
 	enum adau17x1_type type, void (*switch_mode)(struct device *dev))
 {
 	struct snd_soc_dai_driver *dai_drv;
 	const char *firmware_name;
 	int ret;
+
+	/* ZED board hack */
+	if (!dev->platform_data)
+		dev->platform_data = &zed_pdata;
 
 	if (type == ADAU1361) {
 		dai_drv = &adau1361_dai_driver;
