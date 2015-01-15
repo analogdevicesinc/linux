3f24dae76a0c7487d1f20f675dbfcb1c2f2f634a
Michael Hennerich <michael.hennerich@analog.com>
2015-01-15 13:12:16 +0100

    drivers/iio/adc/ad9361: Add RX QEC tracking slow mode option
    If there are tones reasonable close to DC / Center frequency.
    The QEC tracking can become unstable under certain conditions.
    It’ll then cause the image rejection to pop up.
    
    This can be reliably avoided by slowing down the control loop.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/adc/ad9361.h
+++ drivers/iio/adc/ad9361.h
@@ -3078,6 +3078,7 @@ struct ad9361_phy_platform_data {
 	bool			use_ext_rx_lo;
 	bool			use_ext_tx_lo;
 	bool			rx1rx2_phase_inversion_en;
+	bool			qec_tracking_slow_mode_en;
 	u8			dc_offset_update_events;
 	u8			dc_offset_attenuation_high;
 	u8			dc_offset_attenuation_low;
