2be966f9f2e18ec27c1a3ed6cd2f1d71ec228213
Paul Cercueil <paul.cercueil@analog.com>
2015-02-13 16:48:59 +0100

    iio: frequency: ad9144: Use msleep instead of mdelay
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

--- drivers/iio/frequency/ad9144.c
+++ drivers/iio/frequency/ad9144.c
@@ -84,12 +84,12 @@ static int ad9144_setup(struct cf_axi_converter *conv, struct ad9144_platform_da
 
 	// power-up and dac initialization
 
-	mdelay(5);
+	msleep(5);
 
 	ad9144_write(spi, REG_SPI_INTFCONFA, SOFTRESET_M | SOFTRESET);	// reset
 	ad9144_write(spi, REG_SPI_INTFCONFA, 0x00);	// reset
 
-	mdelay(4);
+	msleep(4);
 
 	ad9144_write(spi, 0x011, 0x00);	// dacs - power up everything
 	ad9144_write(spi, 0x080, 0x00);	// clocks - power up everything
@@ -148,7 +148,7 @@ static int ad9144_setup(struct cf_axi_converter *conv, struct ad9144_platform_da
 	ad9144_write(spi, 0x289, 0x04);	// data-rate == 10Gbps
 	ad9144_write(spi, 0x280, 0x01);	// enable serdes pll
 	ad9144_write(spi, 0x280, 0x05);	// enable serdes calibration
-	mdelay(20);
+	msleep(20);
 
 	if ((ad9144_read(spi, 0x281) & 0x01) == 0x00) {
 		dev_err(&spi->dev, "AD9144, PLL/link errors!!\n\r");
@@ -182,12 +182,12 @@ static int ad9144_setup(struct cf_axi_converter *conv, struct ad9144_platform_da
 	ad9144_write(spi, 0x0e8, 0x03);	// cal 2 dacs at once
 	ad9144_write(spi, 0x0e9, 0x01);	// single cal enable
 	ad9144_write(spi, 0x0e9, 0x03);	// single cal start
-	mdelay(10);
+	msleep(10);
 
 	for (i = 0, timeout = 30; i < 2; i++) {
 		ad9144_write(spi, 0x0e8, 1 << i); // read dac-x
 		do {
-			mdelay(1);
+			msleep(1);
 			val = ad9144_read(spi, 0x0e9);
 		} while ((val & CAL_ACTIVE) && timeout--);
 
