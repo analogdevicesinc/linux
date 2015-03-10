b601c147747bbf9c99e194a7cd384590cbdef516
Dragos Bogdan <dragos.bogdan@analog.com>
2015-03-10 15:51:52 +0200

    drivers/staging/iio/resolver/ad2s1210: According to AD2S1210 datasheet, if the serial interface is used, the 8-bit address should be latched using the rising edge of the WR/FSYNC signal.

--- drivers/staging/iio/resolver/ad2s1210.c
+++ drivers/staging/iio/resolver/ad2s1210.c
@@ -124,7 +124,7 @@ static int ad2s1210_config_read(struct ad2s1210_state *st,
 		       unsigned char address)
 {
 	struct spi_transfer xfer = {
-		.len = 2,
+		.len = 1,
 		.rx_buf = st->rx,
 		.tx_buf = st->tx,
 	};
@@ -132,12 +132,16 @@ static int ad2s1210_config_read(struct ad2s1210_state *st,
 
 	ad2s1210_set_mode(MOD_CONFIG, st);
 	st->tx[0] = address | AD2S1210_MSB_IS_HIGH;
-	st->tx[1] = AD2S1210_REG_FAULT;
 	ret = spi_sync_transfer(st->sdev, &xfer, 1);
 	if (ret < 0)
 		return ret;
 
-	return st->rx[1];
+	st->tx[0] = AD2S1210_REG_FAULT;
+	ret = spi_sync_transfer(st->sdev, &xfer, 1);
+	if (ret < 0)
+		return ret;
+
+	return st->rx[0];
 }
 
 static inline
