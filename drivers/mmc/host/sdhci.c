4e7cd2cba076d9b777bb3128e490a2df5ed7e11e
Dong Aisheng <b29396@freescale.com>
2015-04-09 16:23:24 +0800

    MLK-10629-4 mmc: sdhci: remove MMC_CAP_NEEDS_POLL
    This will cause meaningless CPU overhead by polling the card at backgroud
    if the CD is broken.
    Most board does not intend to use this function, so remove it.
    Platform driver could add it for test if needed.
    
    Signed-off-by: Dong Aisheng <b29396@freescale.com>
    
    Conflicts:
    	drivers/mmc/host/sdhci.c

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -3629,11 +3629,7 @@ int sdhci_setup_host(struct sdhci_host *host)
 	if (host->caps & SDHCI_CAN_DO_HISPD)
 		mmc->caps |= MMC_CAP_SD_HIGHSPEED | MMC_CAP_MMC_HIGHSPEED;
 
-	if ((host->quirks & SDHCI_QUIRK_BROKEN_CARD_DETECTION) &&
-	    mmc_card_is_removable(mmc) &&
-	    mmc_gpio_get_cd(host->mmc) < 0)
-		mmc->caps |= MMC_CAP_NEEDS_POLL;
-
+	/* If vqmmc regulator and no 1.8V signalling, then there's no UHS */
 	if (!IS_ERR(mmc->supply.vqmmc)) {
 		ret = regulator_enable(mmc->supply.vqmmc);
 
