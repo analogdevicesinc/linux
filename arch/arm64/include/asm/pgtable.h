ff7b5b4f4ca677f687ab5825ad5a5823152fce97
Haiying Wang <Haiying.Wang@freescale.com>
2015-04-22 13:09:47 -0400

    arm64: add support to remap kernel cacheable memory to userspace
    Signed-off-by: Haiying Wang <Haiying.Wang@freescale.com>
    Reviewed-by: Roy Pledge <roy.pledge@freescale.com>
    Reviewed-by: Stuart Yoder <stuart.yoder@freescale.com>

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -691,6 +691,9 @@ static inline void set_pud_at(struct mm_struct *mm, unsigned long addr,
 	__pgprot_modify(prot, PTE_ATTRINDX_MASK, PTE_ATTRINDX(MT_DEVICE_nGnRnE) | PTE_PXN | PTE_UXN)
 #define pgprot_writecombine(prot) \
 	__pgprot_modify(prot, PTE_ATTRINDX_MASK, PTE_ATTRINDX(MT_NORMAL_NC) | PTE_PXN | PTE_UXN)
+#define pgprot_cached(prot) \
+	__pgprot_modify(prot, PTE_ATTRINDX_MASK, PTE_ATTRINDX(MT_NORMAL) | \
+			PTE_PXN | PTE_UXN)
 #define pgprot_device(prot) \
 	__pgprot_modify(prot, PTE_ATTRINDX_MASK, PTE_ATTRINDX(MT_DEVICE_nGnRE) | PTE_PXN | PTE_UXN)
 #define pgprot_tagged(prot) \
