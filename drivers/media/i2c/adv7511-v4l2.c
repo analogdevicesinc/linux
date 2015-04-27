c6a365506fb573866b70d5c36457e28a67af7115
Dragos Bogdan <dragos.bogdan@analog.com>
2015-04-27 14:16:33 +0300

    drivers/media/i2c/adv7511: Set V4L2_SUBDEV_FL_HAS_DEVNODE for supporting direct userspace configuration.

--- drivers/media/i2c/adv7511-v4l2.c
+++ drivers/media/i2c/adv7511-v4l2.c
@@ -1795,6 +1795,7 @@ static int adv7511_probe(struct i2c_client *client, const struct i2c_device_id *
 			 client->addr << 1);
 
 	v4l2_i2c_subdev_init(sd, client, &adv7511_ops);
+	sd->flags |= V4L2_SUBDEV_FL_HAS_DEVNODE;
 	sd->internal_ops = &adv7511_int_ops;
 
 	hdl = &state->hdl;
