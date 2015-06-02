415786747e0c131dcfd5632f719a81838569eca0
Michael Hennerich <michael.hennerich@analog.com>
2015-06-02 18:27:15 +0200

    drivers/iio/adc/ad9361: Revise external LO clock handling
    Allow to re-parent TX/RX RFPLL clocks.
    Allow switching between internal and external LO dynamically.
    
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/adc/ad9361.h
+++ drivers/iio/adc/ad9361.h
@@ -3205,6 +3205,10 @@ enum ad9361_clocks {
 	T1_CLK,
 	CLKTF_CLK,
 	TX_SAMPL_CLK,
+	RX_RFPLL_INT,
+	TX_RFPLL_INT,
+	RX_RFPLL_DUMMY,
+	TX_RFPLL_DUMMY,
 	RX_RFPLL,
 	TX_RFPLL,
 	NUM_AD9361_CLKS,
@@ -3270,6 +3274,7 @@ struct refclk_scale {
 	struct clk_hw		hw;
 	struct spi_device	*spi;
 	struct ad9361_rf_phy	*phy;
+	unsigned long		rate;
 	u32			mult;
 	u32			div;
 	enum ad9361_clocks 	source;
@@ -3278,6 +3283,8 @@ struct refclk_scale {
 struct ad9361_rf_phy {
 	struct spi_device 	*spi;
 	struct clk 		*clk_refin;
+	struct clk 		*clk_ext_lo_rx;
+	struct clk 		*clk_ext_lo_tx;
 	struct clk 		*clks[NUM_AD9361_CLKS];
 	struct refclk_scale	clk_priv[NUM_AD9361_CLKS];
 	struct clk_onecell_data	clk_data;
@@ -3290,6 +3297,8 @@ struct ad9361_rf_phy {
 	u32 			ad9361_debugfs_entry_index;
 	u8 			prev_ensm_state;
 	u8			curr_ensm_state;
+	u8			cached_rx_rfpll_div;
+	u8			cached_tx_rfpll_div;
 	struct rx_gain_info rx_gain[RXGAIN_TBLS_END];
 	enum rx_gain_table_name current_table;
 	bool 			ensm_pin_ctl_en;
