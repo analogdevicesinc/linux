65d4d2ec5a2b06dc905ba2f6df4489841a9734e2
Bai Ping <b51503@freescale.com>
2015-06-03 22:30:33 +0800

    MLK-11021-03 ARM: imx6: add low power idle support on imx6ul
    Add the low power idle support on i.MX6UL. The ANATOP can enter low
    power when all PLLs are powered down. If need entering low power idle
    with LDO_2P5 and LDO_1P1 power down and other anatop module in low
    power mode, add "uart_from_osc' on command line to make sure the UART
    clk is from osc to let the PLL3  power down when entering low power idle.
    
    Signed-off-by: Bai Ping <b51503@freescale.com>
    (cherry picked from commit 5215cba66938fd09f44e61b2c7b7ae0ef0629c2f)

--- arch/arm/mach-imx/mach-imx6ul.c
+++ arch/arm/mach-imx/mach-imx6ul.c
@@ -83,10 +83,10 @@ static void __init imx6ul_init_irq(void)
 
 static void __init imx6ul_init_late(void)
 {
-	imx6sx_cpuidle_init();
-
 	if (IS_ENABLED(CONFIG_ARM_IMX6Q_CPUFREQ))
 		platform_device_register_simple("imx6q-cpufreq", -1, NULL, 0);
+
+	imx6ul_cpuidle_init();
 }
 
 static void __init imx6ul_map_io(void)
