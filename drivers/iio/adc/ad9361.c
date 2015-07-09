1156d2389d1aa5e8ed1877a7eec2407250a0c822
Michael Hennerich <michael.hennerich@analog.com>
2015-07-09 16:04:04 +0200

    iio/adc/ad9361: debugfs: bist_timing_analysis must write before read
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/adc/ad9361.c
+++ drivers/iio/adc/ad9361.c
@@ -6994,7 +6994,11 @@ static ssize_t ad9361_debugfs_read(struct file *file, char __user *userbuf,
 				rx_gain.mixer_index);
 
 	} else if (entry->cmd == DBGFS_BIST_DT_ANALYSIS) {
-		len = ad9361_dig_interface_timing_analysis(phy, buf, sizeof(buf));
+		if (entry->val)
+			len = ad9361_dig_interface_timing_analysis(phy,
+							buf, sizeof(buf));
+
+		entry->val = 0;
 	} else if (entry->cmd) {
 		val = entry->val;
 	} else
@@ -7109,6 +7113,9 @@ static ssize_t ad9361_debugfs_write(struct file *file,
 		if (ret < 0)
 			return ret;
 
+		entry->val = val;
+		return count;
+	case DBGFS_BIST_DT_ANALYSIS:
 		entry->val = val;
 		return count;
 	default:
