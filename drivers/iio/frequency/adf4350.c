33793928f0995e44f3bd960e3e6776f39d990e61
Michael Hennerich <michael.hennerich@analog.com>
2015-07-21 15:33:07 +0200

    drivers/iio/frequency/adf4350: Add device probing capabilities via MUXOUT
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/frequency/adf4350.c
+++ drivers/iio/frequency/adf4350.c
@@ -563,6 +563,7 @@ static int adf4350_probe(struct spi_device *spi)
 	memset(st->regs_hw, 0xFF, sizeof(st->regs_hw));
 
 	if (gpio_is_valid(pdata->gpio_lock_detect)) {
+		int i;
 		ret = devm_gpio_request(&spi->dev, pdata->gpio_lock_detect,
 					indio_dev->name);
 		if (ret) {
@@ -571,7 +572,50 @@ static int adf4350_probe(struct spi_device *spi)
 			goto error_disable_reg;
 		}
 		gpio_direction_input(pdata->gpio_lock_detect);
+
+		/* ADF4350/1 are write only devices, try to probe it this way */
+		for (i = 0; i < 4; i++) {
+			st->regs[ADF4350_REG2] = ADF4350_REG2_MUXOUT((i & 1) ?
+				ADF4350_MUXOUT_DVDD : ADF4350_MUXOUT_GND);
+			adf4350_sync_config(st, false);
+			ret = gpio_get_value(st->pdata->gpio_lock_detect);
+			if (ret != ((i & 1) ? 1 : 0)) {
+				ret = -ENODEV;
+				dev_err(&spi->dev, "Probe failed (muxout)");
+				goto error_disable_reg;
+			}
+		}
 	}
+
+	st->regs[ADF4350_REG2] =
+		ADF4350_REG2_DOUBLE_BUFF_EN |
+		(pdata->ref_doubler_en ? ADF4350_REG2_RMULT2_EN : 0) |
+		(pdata->ref_div2_en ? ADF4350_REG2_RDIV2_EN : 0) |
+		(pdata->r2_user_settings & (ADF4350_REG2_PD_POLARITY_POS |
+		ADF4350_REG2_LDP_6ns | ADF4350_REG2_LDF_INT_N |
+		ADF4350_REG2_CHARGE_PUMP_CURR_uA(5000) |
+		ADF4350_REG2_MUXOUT(0x7) | ADF4350_REG2_NOISE_MODE(0x3)));
+
+	st->regs[ADF4350_REG3] = pdata->r3_user_settings &
+				 (ADF4350_REG3_12BIT_CLKDIV(0xFFF) |
+				 ADF4350_REG3_12BIT_CLKDIV_MODE(0x3) |
+				 ADF4350_REG3_12BIT_CSR_EN |
+				 ADF4351_REG3_CHARGE_CANCELLATION_EN |
+				 ADF4351_REG3_ANTI_BACKLASH_3ns_EN |
+				 ADF4351_REG3_BAND_SEL_CLOCK_MODE_HIGH);
+
+	st->regs[ADF4350_REG4] =
+		ADF4350_REG4_FEEDBACK_FUND |
+		ADF4350_REG4_RF_OUT_EN |
+		(pdata->r4_user_settings &
+		(ADF4350_REG4_OUTPUT_PWR(0x3) |
+		ADF4350_REG4_AUX_OUTPUT_PWR(0x3) |
+		ADF4350_REG4_AUX_OUTPUT_EN |
+		ADF4350_REG4_AUX_OUTPUT_FUND |
+		ADF4350_REG4_MUTE_TILL_LOCK_EN));
+
+	st->regs[ADF4350_REG5] = ADF4350_REG5_LD_PIN_MODE_DIGITAL |
+				BIT(19) | BIT(20);
 
 	if (pdata->power_up_frequency) {
 		ret = adf4350_set_freq(st, pdata->power_up_frequency);
