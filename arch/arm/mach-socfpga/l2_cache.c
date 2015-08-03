f8e07be6eb4ce47d6bea4a3516dec4b75ba225d0
Dinh Nguyen <dinguyen@opensource.altera.com>
2015-08-03 16:23:49 -0500

    FogBugz #315701-2: add of_node_put for L2 ecc node
    Both of the ecc init functions need to call of_node_put once the node is
    found.
    
    Signed-off-by: Dinh Nguyen <dinguyen@opensource.altera.com>

--- arch/arm/mach-socfpga/l2_cache.c
+++ arch/arm/mach-socfpga/l2_cache.c
@@ -48,6 +48,7 @@ void socfpga_init_arria10_l2_ecc(void)
 
 	pr_alert("SOCFPGA: Success Initializing L2 cache ECC for Arria10");
 
+	of_node_put(np);
 	return;
 }
 
@@ -72,6 +73,7 @@ void socfpga_init_l2_ecc(void)
 	writel(0x01, mapped_l2_edac_addr);
 
 	pr_alert("SOCFPGA: Success Initializing L2 cache ECC");
+	of_node_put(np);
 
 	return;
 }
