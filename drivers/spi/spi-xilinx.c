494f2881aab380455f6eba9ec8e15da014e45e09
Lars-Peter Clausen <lars@metafoo.de>
2015-08-25 17:39:02 +0200

    spi: xilinx: Don't read last RX sample before it is available
    In polled mode the driver waits for the TX FIFO to become empty before it
    starts reading from the RX FIFO. The driver reads uncondtioannly as many
    samples as it thinks it should be available after the transfers have
    comepleted. The problem though is that the when the TX FIFO goes empty when
    the last transfer is started. So depending on the number of samples in the
    RX FIFO and the speed on the SPI bus it is possible that the driver tries
    to read the last RX sample before the last transfer has completed. This
    causes the core to respond with a error to the read operation which will
    cause a imprecise external abort exception and crash the kernel.
    
    This problem does not exist in interrupt driven mode since the interrupt is
    only generated when the TX FIFO is empty and the last transfer has
    completed.
    
    Since the driver is busy waiting anyway in polled mode rather than spinning
    and waiting for the TX FIFO to become empty it makes sense to just spin and
    wait for the RX FIFO to become non-empty and read RX samples from the RX
    FIFO as soon as they become available. This fixes the issue of reading data
    before it is available and is also slightly faster.
    
    Fixes: 22417352f6b7 ("spi/xilinx: Use polling mode on small transfers")
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/spi/spi-xilinx.c
+++ drivers/spi/spi-xilinx.c
@@ -162,6 +162,17 @@ static void xilinx_spi_rx(struct xilinx_spi *xspi)
 	xspi->rx_ptr += xspi->bytes_per_word;
 }
 
+static void xilinx_spi_wait_and_rx(struct xilinx_spi *xspi)
+{
+		unsigned int sr;
+
+		do {
+			sr = xspi->read_fn(xspi->regs + XSPI_SR_OFFSET);
+		} while (sr & XSPI_SR_RX_EMPTY_MASK);
+
+		xilinx_spi_rx(xspi);
+}
+
 static void xspi_init_hw(struct xilinx_spi *xspi)
 {
 	void __iomem *regs_base = xspi->regs;
@@ -272,8 +283,9 @@ static int xilinx_spi_txrx_bufs(struct spi_device *spi, struct spi_transfer *t)
 		int n_words, tx_words, rx_words;
 
 		n_words = min(remaining_words, xspi->buffer_size);
-
+		rx_words = n_words;
 		tx_words = n_words;
+
 		while (tx_words--)
 			xilinx_spi_tx(xspi);
 
@@ -284,24 +296,23 @@ static int xilinx_spi_txrx_bufs(struct spi_device *spi, struct spi_transfer *t)
 		if (use_irq) {
 			xspi->write_fn(cr, xspi->regs + XSPI_CR_OFFSET);
 			wait_for_completion(&xspi->done);
-		} else
-			while (!(xspi->read_fn(xspi->regs + XSPI_SR_OFFSET) &
-						XSPI_SR_TX_EMPTY_MASK))
-				;
 
-		/* A transmit has just completed. Process received data and
-		 * check for more data to transmit. Always inhibit the
-		 * transmitter while the Isr refills the transmit register/FIFO,
-		 * or make sure it is stopped if we're done.
-		 */
-		if (use_irq)
+			/* A transmit has just completed. Process received data and
+			 * check for more data to transmit. Always inhibit the
+			 * transmitter while the Isr refills the transmit register/FIFO,
+			 * or make sure it is stopped if we're done.
+			 */
 			xspi->write_fn(cr | XSPI_CR_TRANS_INHIBIT,
 			       xspi->regs + XSPI_CR_OFFSET);
 
-		/* Read out all the data from the Rx FIFO */
-		rx_words = n_words;
-		while (rx_words--)
-			xilinx_spi_rx(xspi);
+			/* Read out all the data from the Rx FIFO */
+			while (rx_words--)
+				xilinx_spi_rx(xspi);
+		} else {
+			/* Read data when it becomes available */
+			while (rx_words--)
+				xilinx_spi_wait_and_rx(xspi);
+		}
 
 		remaining_words -= n_words;
 	}
