b92e44862d85e0d34d019e2d0424c3e5296c12d8
Michael Hennerich <michael.hennerich@analog.com>
2015-09-02 15:02:03 +0200

    drivers/iio/adc/ad9361:Revise AD9361 allow 1RX1TX arbitrary TRX maps
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/adc/ad9361.c
+++ drivers/iio/adc/ad9361.c
@@ -762,10 +762,17 @@ int ad9361_find_opt(u8 *field, u32 size, u32 *ret_start)
 }
 EXPORT_SYMBOL(ad9361_find_opt);
 
-static int ad9361_1rx1tx_channel_map(struct ad9361_rf_phy *phy, int map, int channel)
+static int ad9361_1rx1tx_channel_map(struct ad9361_rf_phy *phy, bool tx, int channel)
 {
+	u32 map;
+
 	if (phy->pdata->rx2tx2)
 		return channel;
+
+	if (tx)
+		map = phy->pdata->rx1tx1_mode_use_tx_num;
+	else
+		map = phy->pdata->rx1tx1_mode_use_rx_num;
 
 	if (map == 2)
 		return channel + 1;
@@ -797,6 +804,38 @@ static int ad9361_reset(struct ad9361_rf_phy *phy)
 	return -ENODEV;
 }
 
+static int ad9361_en_dis_tx(struct ad9361_rf_phy *phy, u32 tx_if, u32 enable)
+{
+	if ((tx_if & enable) > 1 && spi_get_device_id(phy->spi)->driver_data ==
+		ID_AD9364 && enable)
+		return -EINVAL;
+
+	return ad9361_spi_writef(phy->spi, REG_TX_ENABLE_FILTER_CTRL,
+			TX_CHANNEL_ENABLE(tx_if), enable);
+}
+
+static int ad9361_en_dis_rx(struct ad9361_rf_phy *phy, u32 rx_if, u32 enable)
+{
+	if ((rx_if & enable) > 1 && spi_get_device_id(phy->spi)->driver_data ==
+		ID_AD9364 && enable)
+		return -EINVAL;
+
+	return ad9361_spi_writef(phy->spi, REG_RX_ENABLE_FILTER_CTRL,
+			  RX_CHANNEL_ENABLE(rx_if), enable);
+}
+
+static int ad9361_int_loopback_fix_ch_cross(struct ad9361_rf_phy *phy, bool enable)
+{
+	/* Loopback works only TX1->RX1 or RX2->RX2 */
+	if (!phy->pdata->rx2tx2 && phy->pdata->rx1tx1_mode_use_rx_num !=
+			phy->pdata->rx1tx1_mode_use_tx_num)
+		return ad9361_en_dis_tx(phy, TX_1 | TX_2,
+				enable ? phy->pdata->rx1tx1_mode_use_rx_num :
+				phy->pdata->rx1tx1_mode_use_tx_num);
+
+	return 0;
+}
+
 int ad9361_bist_loopback(struct ad9361_rf_phy *phy, unsigned mode)
 {
 	u32 sp_hd, reg;
@@ -805,16 +844,17 @@ int ad9361_bist_loopback(struct ad9361_rf_phy *phy, unsigned mode)
 
 	reg = ad9361_spi_read(phy->spi, REG_OBSERVE_CONFIG);
 
-
 	switch (mode) {
 	case 0:
 		ad9361_hdl_loopback(phy, false);
+		ad9361_int_loopback_fix_ch_cross(phy, false);
 		reg &= ~(DATA_PORT_SP_HD_LOOP_TEST_OE |
 			DATA_PORT_LOOP_TEST_ENABLE);
 		return ad9361_spi_write(phy->spi, REG_OBSERVE_CONFIG, reg);
 	case 1:
 		/* loopback (AD9361 internal) TX->RX */
 		ad9361_hdl_loopback(phy, false);
+		ad9361_int_loopback_fix_ch_cross(phy, true);
 		sp_hd = ad9361_spi_read(phy->spi, REG_PARALLEL_PORT_CONF_3);
 		if ((sp_hd & SINGLE_PORT_MODE) && (sp_hd & HALF_DUPLEX_MODE))
 			reg |= DATA_PORT_SP_HD_LOOP_TEST_OE;
@@ -827,6 +867,7 @@ int ad9361_bist_loopback(struct ad9361_rf_phy *phy, unsigned mode)
 	case 2:
 		/* loopback (FPGA internal) RX->TX */
 		ad9361_hdl_loopback(phy, true);
+		ad9361_int_loopback_fix_ch_cross(phy, false);
 		reg &= ~(DATA_PORT_SP_HD_LOOP_TEST_OE |
 			DATA_PORT_LOOP_TEST_ENABLE);
 		return ad9361_spi_write(phy->spi, REG_OBSERVE_CONFIG, reg);
@@ -1634,27 +1675,6 @@ static int ad9361_init_gain_tables(struct ad9361_rf_phy *phy)
 		SIZE_FULL_TABLE, 4);
 
 	return 0;
-}
-
-int ad9361_en_dis_tx(struct ad9361_rf_phy *phy, u32 tx_if, u32 enable)
-{
-	if ((tx_if & enable) > 1 && spi_get_device_id(phy->spi)->driver_data ==
-		ID_AD9364 && enable)
-		return -EINVAL;
-
-	return ad9361_spi_writef(phy->spi, REG_TX_ENABLE_FILTER_CTRL,
-			TX_CHANNEL_ENABLE(tx_if), enable);
-}
-EXPORT_SYMBOL(ad9361_en_dis_tx);
-
-static int ad9361_en_dis_rx(struct ad9361_rf_phy *phy, u32 rx_if, u32 enable)
-{
-	if ((rx_if & enable) > 1 && spi_get_device_id(phy->spi)->driver_data ==
-		ID_AD9364 && enable)
-		return -EINVAL;
-
-	return ad9361_spi_writef(phy->spi, REG_RX_ENABLE_FILTER_CTRL,
-			  RX_CHANNEL_ENABLE(rx_if), enable);
 }
 
 static int ad9361_gc_update(struct ad9361_rf_phy *phy)
@@ -6592,8 +6612,7 @@ static int ad9361_set_agc_mode(struct iio_dev *indio_dev,
 	struct ad9361_rf_phy *phy = iio_priv(indio_dev);
 	struct rf_gain_ctrl gc = {0};
 
-	gc.ant = ad9361_1rx1tx_channel_map(phy,
-			phy->pdata->rx1tx1_mode_use_rx_num, chan->channel + 1);
+	gc.ant = ad9361_1rx1tx_channel_map(phy, false, chan->channel + 1);
 	gc.mode = phy->agc_mode[chan->channel] = mode;
 
 	return ad9361_set_gain_ctrl_mode(phy, &gc);
@@ -6713,8 +6732,7 @@ static ssize_t ad9361_phy_rx_read(struct iio_dev *indio_dev,
 
 	mutex_lock(&indio_dev->mlock);
 
-	rssi.ant = ad9361_1rx1tx_channel_map(phy,
-			phy->pdata->rx1tx1_mode_use_rx_num, chan->channel + 1);
+	rssi.ant = ad9361_1rx1tx_channel_map(phy, false, chan->channel + 1);
 	rssi.duration = 1;
 	ret = ad9361_read_rssi(phy, &rssi);
 	val = rssi.symbol;
@@ -6804,8 +6822,7 @@ static int ad9361_phy_read_raw(struct iio_dev *indio_dev,
 	case IIO_CHAN_INFO_HARDWAREGAIN:
 		if (chan->output) {
 			ret = ad9361_get_tx_atten(phy,
-				ad9361_1rx1tx_channel_map(phy,
-				phy->pdata->rx1tx1_mode_use_tx_num,
+				ad9361_1rx1tx_channel_map(phy, true,
 			        chan->channel + 1));
 			if (ret < 0) {
 				ret = -EINVAL;
@@ -6820,7 +6837,7 @@ static int ad9361_phy_read_raw(struct iio_dev *indio_dev,
 		} else {
 			struct rf_rx_gain rx_gain = {0};
 			ret = ad9361_get_rx_gain(phy, ad9361_1rx1tx_channel_map(phy,
-			phy->pdata->rx1tx1_mode_use_rx_num, chan->channel + 1), &rx_gain);
+			false, chan->channel + 1), &rx_gain);
 			*val = rx_gain.gain_db;
 			*val2 = 0;
 		}
@@ -6909,17 +6926,14 @@ static int ad9361_phy_write_raw(struct iio_dev *indio_dev,
 			code = ((abs(val) * 1000) + (abs(val2) / 1000));
 
 
-			ch = ad9361_1rx1tx_channel_map(phy,
-					phy->pdata->rx1tx1_mode_use_tx_num,
-				        chan->channel);
+			ch = ad9361_1rx1tx_channel_map(phy, true, chan->channel);
 			ret = ad9361_set_tx_atten(phy, code, ch == 0, ch == 1,
 			        !phy->pdata->update_tx_gain_via_alert);
 		} else {
 			struct rf_rx_gain rx_gain = {0};
 			rx_gain.gain_db = val;
 			ret = ad9361_set_rx_gain(phy,
-					ad9361_1rx1tx_channel_map(phy,
-					phy->pdata->rx1tx1_mode_use_rx_num,
+					ad9361_1rx1tx_channel_map(phy, false,
 					chan->channel + 1), &rx_gain);
 		}
 		break;
