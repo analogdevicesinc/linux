e5f988eac0b87dab85ac1c06cfa3c2398a8a065d
Lars-Peter Clausen <lars@metafoo.de>
2015-10-13 16:16:19 +0200

    Revert "usb: chipidea: Add support for VBUS control with PHY"
    This reverts commit 7be1ce169687f4e3ccbfe87459dc20c12f558a3a.
    
    The commit is conceptually broken. Whether vbus needs to be controlled or
    not is not a property of the SoC in which the controller is used, but
    rather of the fabric on the PCB.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/usb/chipidea/otg_fsm.c
+++ drivers/usb/chipidea/otg_fsm.c
@@ -479,11 +479,6 @@ static void ci_otg_drv_vbus(struct otg_fsm *fsm, int on)
 				return;
 			}
 		}
-
-		if (ci->platdata->flags & CI_HDRC_PHY_VBUS_CONTROL &&
-				ci->usb_phy && ci->usb_phy->set_vbus)
-			ci->usb_phy->set_vbus(ci->usb_phy, 1);
-
 		/* Disable data pulse irq */
 		hw_write_otgsc(ci, OTGSC_DPIE, 0);
 
@@ -494,10 +489,6 @@ static void ci_otg_drv_vbus(struct otg_fsm *fsm, int on)
 			regulator_disable(ci->platdata->reg_vbus);
 		if (ci->usb_phy && ci->usb_phy->otg)
 			otg_set_vbus(ci->usb_phy->otg, false);
-
-		if (ci->platdata->flags & CI_HDRC_PHY_VBUS_CONTROL &&
-				ci->usb_phy && ci->usb_phy->set_vbus)
-			ci->usb_phy->set_vbus(ci->usb_phy, 0);
 
 		fsm->a_bus_drop = 1;
 		fsm->a_bus_req = 0;
