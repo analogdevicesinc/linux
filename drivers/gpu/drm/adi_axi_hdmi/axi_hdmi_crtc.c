bc21ef4060f7e490d0810ad6cbe05e7d911ec5f8
Andrea Merello <andrea.merello@gmail.com>
2015-11-20 15:00:05 +0100

    drm: adi_axi_hdmi: Make DMA use interleaved transfers
    This driver was tied to a old-version, not-mainlined, Xilinx VDMA driver.
    A custom configuration API was used for passing a bunch of parameters
    to the VDMA driver.
    
    By using the interleaved API most of this custom stuff is no longer needed
    and this makes also things working with the mainlined version of VDMA driver.
    
    For now the old DMA API is still supported in order to ensure compatibility
    with the old VDMA driver, otherwise users who update kernel keeping the old
    DT/driver would have things broken. The interleaved mode will be automatically
    used when available on the DMA channel.
    
    Note that, AFAICT, this works (and ever worked) only because the Xilinx VDMA
    implementation implicitly do cyclic transfers; so that making adi_axi_hdmi to
    use the standard DMA API is not enough to state it will work with any dmaengine
    driver that supports interleaved transfers. AFAIK this is odd, and there is no
    way to ask for cyclic transfers on the top of interleaved transfers (RFC).
    
    Beside that, the new VDMA driver still has a custom-API for configuration,
    but this time we need it only to flip a configuration flag.
    I suspect we indeed need to flip it only because of a bug in the VDMA driver.
    If this will turn out to be the case, and it will be fixed, then maybe
    removing this remaining custom-stuff would be possible.
    
    Signed-off-by: Andrea Merello <andrea.merello@gmail.com>
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/gpu/drm/adi_axi_hdmi/axi_hdmi_crtc.c
+++ drivers/gpu/drm/adi_axi_hdmi/axi_hdmi_crtc.c
@@ -23,13 +23,89 @@
 struct axi_hdmi_crtc {
 	struct drm_crtc drm_crtc;
 	struct dma_chan *dma;
-	struct xilinx_dma_config dma_config;
+	struct dma_interleaved_template *dma_template;
 	int mode;
 };
 
 static inline struct axi_hdmi_crtc *to_axi_hdmi_crtc(struct drm_crtc *crtc)
 {
 	return container_of(crtc, struct axi_hdmi_crtc, drm_crtc);
+}
+
+static struct dma_async_tx_descriptor
+*axi_hdmi_vdma_prep_single_desc(struct drm_crtc *crtc,
+				struct drm_gem_cma_object *obj)
+{
+	struct xilinx_dma_config dma_config;
+	size_t offset;
+	struct axi_hdmi_crtc *axi_hdmi_crtc = to_axi_hdmi_crtc(crtc);
+	struct drm_display_mode *mode = &crtc->mode;
+	struct drm_framebuffer *fb = crtc->primary->fb;
+
+	memset(&dma_config, 0, sizeof(dma_config));
+	dma_config.hsize = mode->hdisplay * fb->bits_per_pixel / 8;
+	dma_config.vsize = mode->vdisplay;
+	dma_config.stride = fb->pitches[0];
+
+	dmaengine_slave_config(axi_hdmi_crtc->dma,
+			(struct dma_slave_config *)&dma_config);
+
+	offset = crtc->x * fb->bits_per_pixel / 8 + crtc->y * fb->pitches[0];
+
+	return dmaengine_prep_slave_single(axi_hdmi_crtc->dma,
+					obj->paddr + offset,
+					mode->vdisplay * fb->pitches[0],
+					DMA_MEM_TO_DEV, 0);
+}
+
+static struct dma_async_tx_descriptor
+*axi_hdmi_vdma_prep_interleaved_desc(struct drm_crtc *crtc,
+				struct drm_gem_cma_object *obj)
+{
+	struct xilinx_vdma_config vdma_config;
+	size_t offset, hw_row_size;
+	struct axi_hdmi_crtc *axi_hdmi_crtc = to_axi_hdmi_crtc(crtc);
+	struct drm_display_mode *mode = &crtc->mode;
+	struct drm_framebuffer *fb = crtc->primary->fb;
+
+	memset(&vdma_config, 0, sizeof(vdma_config));
+	vdma_config.park = 1;
+	xilinx_vdma_channel_set_config(axi_hdmi_crtc->dma, &vdma_config);
+
+	offset = crtc->x * fb->bits_per_pixel / 8 +
+		crtc->y * fb->pitches[0];
+
+	/* Interleaved DMA is used that way:
+	 * Each interleaved frame is a row (hsize) implemented in ONE
+	 * chunk (sgl has len 1).
+	 * The number of interleaved frames is the number of rows (vsize).
+	 * The icg in used to pack data to the HW, so that the buffer len
+	 * is fb->piches[0], but the actual size for the hw is somewhat less
+	 */
+	axi_hdmi_crtc->dma_template->dir = DMA_MEM_TO_DEV;
+	axi_hdmi_crtc->dma_template->src_start = obj->paddr + offset;
+	/* sgl list have just one entry (each interleaved frame have 1 chunk) */
+	axi_hdmi_crtc->dma_template->frame_size = 1;
+	/* the number of interleaved frame, each has the size specified in sgl */
+	axi_hdmi_crtc->dma_template->numf = mode->vdisplay;
+	axi_hdmi_crtc->dma_template->src_sgl = 1;
+	axi_hdmi_crtc->dma_template->src_inc = 1;
+
+	/* vdma IP does not provide any addr to the hdmi IP, so dst_inc
+	 * and dst_sgl should make no any difference.
+	 */
+	axi_hdmi_crtc->dma_template->dst_inc = 0;
+	axi_hdmi_crtc->dma_template->dst_sgl = 0;
+
+	hw_row_size = mode->hdisplay * fb->bits_per_pixel / 8;
+	axi_hdmi_crtc->dma_template->sgl[0].size = hw_row_size;
+
+	/* the vdma driver seems to look at icg, and not src_icg */
+	axi_hdmi_crtc->dma_template->sgl[0].icg =
+		fb->pitches[0] - hw_row_size;
+
+	return dmaengine_prep_interleaved_dma(axi_hdmi_crtc->dma,
+						axi_hdmi_crtc->dma_template, 0);
 }
 
 static int axi_hdmi_crtc_update(struct drm_crtc *crtc)
@@ -39,7 +115,6 @@ static int axi_hdmi_crtc_update(struct drm_crtc *crtc)
 	struct drm_framebuffer *fb = crtc->primary->fb;
 	struct dma_async_tx_descriptor *desc;
 	struct drm_gem_cma_object *obj;
-	size_t offset;
 
 	if (!mode || !fb)
 		return -EINVAL;
@@ -51,19 +126,14 @@ static int axi_hdmi_crtc_update(struct drm_crtc *crtc)
 		if (!obj)
 			return -EINVAL;
 
-		axi_hdmi_crtc->dma_config.hsize = mode->hdisplay * fb->bits_per_pixel / 8;
-		axi_hdmi_crtc->dma_config.vsize = mode->vdisplay;
-		axi_hdmi_crtc->dma_config.stride = fb->pitches[0];
 
-		dmaengine_slave_config(axi_hdmi_crtc->dma,
-			(struct dma_slave_config *)&axi_hdmi_crtc->dma_config);
+		if (dma_has_cap(DMA_INTERLEAVE,
+					axi_hdmi_crtc->dma->device->cap_mask)) {
+			desc = axi_hdmi_vdma_prep_interleaved_desc(crtc, obj);
+		} else {
+			desc = axi_hdmi_vdma_prep_single_desc(crtc, obj);
+		}
 
-		offset = crtc->x * fb->bits_per_pixel / 8 + crtc->y * fb->pitches[0];
-
-		desc = dmaengine_prep_slave_single(axi_hdmi_crtc->dma,
-					obj->paddr + offset,
-					mode->vdisplay * fb->pitches[0],
-					DMA_MEM_TO_DEV, 0);
 		if (!desc) {
 			pr_err("Failed to prepare DMA descriptor\n");
 			return -ENOMEM;
@@ -141,6 +211,7 @@ static void axi_hdmi_crtc_destroy(struct drm_crtc *crtc)
 	struct axi_hdmi_crtc *axi_hdmi_crtc = to_axi_hdmi_crtc(crtc);
 
 	drm_crtc_cleanup(crtc);
+	kfree(axi_hdmi_crtc->dma_template);
 	kfree(axi_hdmi_crtc);
 }
 
@@ -158,6 +229,17 @@ struct drm_crtc *axi_hdmi_crtc_create(struct drm_device *dev)
 	axi_hdmi_crtc = kzalloc(sizeof(*axi_hdmi_crtc), GFP_KERNEL);
 	if (!axi_hdmi_crtc) {
 		DRM_ERROR("failed to allocate axi_hdmi crtc\n");
+		return ERR_PTR(-ENOMEM);
+	}
+
+	/* we know we'll always use only one data chunk */
+	axi_hdmi_crtc->dma_template = kzalloc(
+		sizeof(struct dma_interleaved_template) +
+		sizeof(struct data_chunk), GFP_KERNEL);
+
+	if (!axi_hdmi_crtc->dma_template) {
+		kfree(axi_hdmi_crtc);
+		DRM_ERROR("failed to allocate dma_template crtc\n");
 		return ERR_PTR(-ENOMEM);
 	}
 
