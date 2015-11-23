93b332b793df547359d780f9383a189066760bc9
Michael Hennerich <michael.hennerich@analog.com>
2015-11-23 10:35:01 +0100

    drivers/iio/adc/ad9361: Add support for GPO manual mode
    Please note: GPO manual mode conflicts with
    	     automatic ENSM slave and eLNA mode.
    
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/adc/ad9361.h
+++ drivers/iio/adc/ad9361.h
@@ -3020,6 +3020,8 @@ struct auxadc_control {
 };
 
 struct gpo_control {
+	u32 gpo_manual_mode_enable_mask;
+	bool gpo_manual_mode_en;
 	bool gpo0_inactive_state_high_en;
 	bool gpo1_inactive_state_high_en;
 	bool gpo2_inactive_state_high_en;
@@ -3296,7 +3298,7 @@ struct ad9361_rf_phy {
 	struct refclk_scale	clk_priv[NUM_AD9361_CLKS];
 	struct clk_onecell_data	clk_data;
 	struct ad9361_phy_platform_data *pdata;
-	struct ad9361_debugfs_entry debugfs_entry[175];
+	struct ad9361_debugfs_entry debugfs_entry[177];
 	struct bin_attribute 	bin;
 	struct iio_dev 		*indio_dev;
 	struct work_struct 	work;
