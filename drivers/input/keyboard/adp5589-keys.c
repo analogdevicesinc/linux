b181fbfb66b852c3beeec3e089dfc36aa725f4d0
Lars-Peter Clausen <lars@metafoo.de>
2015-11-28 15:38:19 +0100

    input: adp5589: Add basic devicetree support
    Add very basic devicetree suppport to the adp5589 allowing the device to be
    registered from devicetree.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/input/keyboard/adp5589-keys.c
+++ drivers/input/keyboard/adp5589-keys.c
@@ -1003,6 +1003,7 @@ static const struct adp5589_kpad_platform_data adp5589_default_pdata = {
 static int adp5589_probe(struct i2c_client *client,
 			 const struct i2c_device_id *id)
 {
+	unsigned int dev_id;
 	struct adp5589_kpad *kpad;
 	const struct adp5589_kpad_platform_data *pdata =
 		dev_get_platdata(&client->dev);
@@ -1024,7 +1025,21 @@ static int adp5589_probe(struct i2c_client *client,
 
 	kpad->client = client;
 
-	switch (id->driver_data) {
+	if (id) {
+		dev_id = id->driver_data;
+	} else if (client->dev.of_node) {
+		const struct of_device_id *of_id;
+
+		of_id = of_match_node(client->dev.driver->of_match_table,
+			client->dev.of_node);
+		if (!of_id)
+			return -ENODEV;
+		dev_id = (unsigned int)of_id->data;
+	} else {
+		return -ENODEV;
+	}
+
+	switch (dev_id) {
 	case ADP5585_02:
 		kpad->support_row5 = true;
 	case ADP5585_01:
@@ -1123,6 +1138,13 @@ static int adp5589_resume(struct device *dev)
 
 static SIMPLE_DEV_PM_OPS(adp5589_dev_pm_ops, adp5589_suspend, adp5589_resume);
 
+static const struct of_device_id adp5589_of_match[] = {
+	{ .compatible = "adi,adp5585", .data = (void *)ADP5585_01 },
+	{ .compatible = "adi,adp5585-02", .data = (void *)ADP5585_02 },
+	{ .compatible = "adi,adp5589", .data = (void *)ADP5589 },
+	{}
+};
+
 static const struct i2c_device_id adp5589_id[] = {
 	{"adp5589-keys", ADP5589},
 	{"adp5585-keys", ADP5585_01},
@@ -1136,6 +1158,7 @@ static struct i2c_driver adp5589_driver = {
 	.driver = {
 		.name = KBUILD_MODNAME,
 		.pm = &adp5589_dev_pm_ops,
+		.of_match_table = adp5589_of_match,
 	},
 	.probe = adp5589_probe,
 	.remove = adp5589_remove,
