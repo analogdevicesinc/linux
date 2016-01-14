f306b23578555f3334187302180ef15be1f90748
Lars-Peter Clausen <lars@metafoo.de>
2016-01-14 11:48:44 +0100

    iio: Move scan mask management to the core
    commit 43c3d79139b416edc622799c39cadbc56a78f594 m2k branch.
    
    Let the core handle the buffer scan mask management including allocation
    and channel selection. Having this handled in a central place rather than
    open-coding it all over the place will make it easier to change the
    implementation.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/iio/buffer/industrialio-buffer-cb.c
+++ drivers/iio/buffer/industrialio-buffer-cb.c
@@ -36,7 +36,7 @@ static int iio_buffer_cb_store_to(struct iio_buffer *buffer, const void *data)
 static void iio_buffer_cb_release(struct iio_buffer *buffer)
 {
 	struct iio_cb_buffer *cb_buff = buffer_to_cb_buffer(buffer);
-	kfree(cb_buff->buffer.scan_mask);
+	iio_buffer_free_scanmask(buffer);
 	kfree(cb_buff);
 }
 
@@ -53,6 +53,7 @@ struct iio_cb_buffer *iio_channel_get_all_cb(struct device *dev,
 					     void *private)
 {
 	int ret;
+	struct iio_dev *indio_dev;
 	struct iio_cb_buffer *cb_buff;
 	struct iio_channel *chan;
 
@@ -73,29 +74,25 @@ struct iio_cb_buffer *iio_channel_get_all_cb(struct device *dev,
 		goto error_free_cb_buff;
 	}
 
-	cb_buff->indio_dev = cb_buff->channels[0].indio_dev;
-	cb_buff->buffer.scan_mask
-		= kcalloc(BITS_TO_LONGS(cb_buff->indio_dev->masklength),
-			  sizeof(long), GFP_KERNEL);
-	if (cb_buff->buffer.scan_mask == NULL) {
-		ret = -ENOMEM;
+	indio_dev = cb_buff->channels[0].indio_dev;
+
+	ret = iio_buffer_alloc_scanmask(&cb_buff->buffer, indio_dev);
+	if (ret)
 		goto error_release_channels;
-	}
 	chan = &cb_buff->channels[0];
 	while (chan->indio_dev) {
-		if (chan->indio_dev != cb_buff->indio_dev) {
+		if (chan->indio_dev != indio_dev) {
 			ret = -EINVAL;
 			goto error_free_scan_mask;
 		}
-		set_bit(chan->channel->scan_index,
-			cb_buff->buffer.scan_mask);
+		iio_buffer_channel_enable(&cb_buff->buffer, chan);
 		chan++;
 	}
 
 	return cb_buff;
 
 error_free_scan_mask:
-	kfree(cb_buff->buffer.scan_mask);
+	iio_buffer_free_scanmask(&cb_buff->buffer);
 error_release_channels:
 	iio_channel_release_all(cb_buff->channels);
 error_free_cb_buff:
