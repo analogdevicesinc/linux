643a984149c0c534b892de856e6aee2106498a53
Michael Hennerich <michael.hennerich@analog.com>
2016-05-20 10:22:22 +0200

    iio:adc:ad9371: New AD9371 highly integrated RF Transceiver (Mykonos)
    Initial check in
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

---
+++ drivers/iio/adc/mykonos/mykonos_version.h
@@ -0,0 +1,22 @@
+/**
+ * \file mykonos_version.h
+ * \brief Contains the version number for the Mykonos API
+ */
+
+#ifndef MYKONOS_VERSION_H_
+#define MYKONOS_VERSION_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MYKONOS_CURRENT_SI_VERSION 1
+#define MYKONOS_CURRENT_MAJOR_VERSION 1
+#define MYKONOS_CURRENT_MINOR_VERSION 9
+#define MYKONOS_CURRENT_BUILD_VERSION 3325
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MYKONOS_VERSION_H_ */
