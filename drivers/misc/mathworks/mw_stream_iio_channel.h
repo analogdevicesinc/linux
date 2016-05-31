0ab017033fde7281a0ecf8bc96c9584af30c737f
Matthew Fornero <mfornero@mathworks.com>
2016-05-31 14:35:01 -0400

    misc: mathworks: create iio-based stream driver
    Create stream driver that implements each mm2s/s2mm channel as an iio
    device
    
    Signed-off-by: Matt Fornero <matt.fornero@mathworks.com>
    
    Conflicts:
    	drivers/misc/mathworks/Makefile

---
+++ drivers/misc/mathworks/mw_stream_iio_channel.h
@@ -0,0 +1,51 @@
+/*
+ * MathWorks Streaming Channel
+ *
+ * Copyright 2016 The MathWorks, Inc
+ *
+ * Licensed under the GPL-2.
+ */
+
+#ifndef _MW_STREAM_IIO_CHANNEL_H_
+#define _MW_STREAM_IIO_CHANNEL_H_
+
+#include <linux/device.h>
+#include <linux/iio/iio.h>
+#include "mathworks_ipcore.h"
+
+struct mw_stream_iio_channel_info {
+	enum iio_device_direction 		iio_direction;
+};
+
+enum mw_stream_iio_tlast_mode {
+	MW_STREAM_TLAST_MODE_AUTO = 0,
+	MW_STREAM_TLAST_MODE_USER_LOGIC,
+};
+
+enum mw_stream_iio_reset_tlast_mode {
+	MW_STREAM_TLAST_MODE_PREBUFFER = 0,
+	MW_STREAM_TLAST_MODE_NEVER,
+};
+
+struct mw_stream_iio_chandev {
+	struct mathworks_ipcore_dev 			*mwdev;
+	struct device							dev;
+	enum iio_device_direction 				iio_direction;
+	const char								*dmaname;
+	enum mw_stream_iio_tlast_mode			tlast_mode;
+	enum mw_stream_iio_reset_tlast_mode		reset_tlast_mode;
+	int										tlast_cntr_addr;
+	int										num_data_chan;
+};
+
+/*********************************************************
+* API Symbols
+*********************************************************/
+extern struct mathworks_ip_ops mw_stream_iio_ops;
+
+/*********************************************************
+* API functions
+*********************************************************/
+extern int mw_stream_iio_channels_probe(struct mathworks_ipcore_dev	*mwdev);
+
+#endif /* _MW_STREAM_IIO_CHANNEL_H_ */
