f880d4af2b5bd0f8090536216949c0cd400ddaee
Alan Tull <atull@opensource.altera.com>
2016-06-08 13:14:03 -0500

    FogBugz #383074-1: fpga-mgr: add fpga image information struct
    A FPGA design contained in a FPGA image file will likely have
    particulars that affect how the image is programmed to the FPGA.
    Currently the only such particular is a single flag bit
    indicating whether the image is for full or partial reconfig-
    uration.
    
    This patch adds a new 'struct  fpga_image_info', moves the
    current 'u32 flags' to it.
    
    The FPGA Manager API functions are changed, as the 'u32 flag'
    parameter is replaced with a pointer to struct fpga_image_info.
    
    Subsequent patches will add more to the struct.  The things
    that will be added for current use are timeouts for knowing how
    long to reasonably wait when shutting down/re-enabling traffic
    to a FPGA region that is reprogrammed.  That can vary widely
    with the implementation of the design that is being programmed.
    Other future additions will be to handle passing addresses for
    reprogramming relocatable FPGA images.  As use of FPGA in the
    kernel continues to develop, other specifics are likely to be
    added.
    
    Signed-off-by: Alan Tull <atull@opensource.altera.com>

--- include/linux/fpga/fpga-mgr.h
+++ include/linux/fpga/fpga-mgr.h
@@ -71,6 +71,14 @@ enum fpga_mgr_states {
 #define FPGA_MGR_EXTERNAL_CONFIG	BIT(1)
 
 /**
+ * struct fpga_image_info - information specific to a FPGA image
+ * @flags: boolean flags as defined above
+ */
+struct fpga_image_info {
+	u32 flags;
+};
+
+/**
  * struct fpga_manager_ops - ops for low level fpga manager drivers
  * @state: returns an enum value of the FPGA's state
  * @write_init: prepare the FPGA to receive confuration data
@@ -84,10 +92,12 @@ enum fpga_mgr_states {
  */
 struct fpga_manager_ops {
 	enum fpga_mgr_states (*state)(struct fpga_manager *mgr);
-	int (*write_init)(struct fpga_manager *mgr, u32 flags,
+	int (*write_init)(struct fpga_manager *mgr,
+			  struct fpga_image_info *info,
 			  const char *buf, size_t count);
 	int (*write)(struct fpga_manager *mgr, const char *buf, size_t count);
-	int (*write_complete)(struct fpga_manager *mgr, u32 flags);
+	int (*write_complete)(struct fpga_manager *mgr,
+			      struct fpga_image_info *info);
 	void (*fpga_remove)(struct fpga_manager *mgr);
 };
 
@@ -111,10 +121,11 @@ struct fpga_manager {
 
 #define to_fpga_manager(d) container_of(d, struct fpga_manager, dev)
 
-int fpga_mgr_buf_load(struct fpga_manager *mgr, u32 flags,
+int fpga_mgr_buf_load(struct fpga_manager *mgr, struct fpga_image_info *info,
 		      const char *buf, size_t count);
 
-int fpga_mgr_firmware_load(struct fpga_manager *mgr, u32 flags,
+int fpga_mgr_firmware_load(struct fpga_manager *mgr,
+			   struct fpga_image_info *info,
 			   const char *image_name);
 
 struct fpga_manager *of_fpga_mgr_get(struct device_node *node);
