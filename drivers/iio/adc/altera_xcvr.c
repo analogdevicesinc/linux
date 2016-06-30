27a3f14100bd4302997127509fccf8ec42939470
Dragos Bogdan <dragos.bogdan@analog.com>
2016-06-30 16:26:58 +0300

    drivers/iio/adc/altera_xcvr: Fix link offset

--- drivers/iio/adc/altera_xcvr.c
+++ drivers/iio/adc/altera_xcvr.c
@@ -46,26 +46,26 @@
 
 #define XCVR_REG_DEVICE_TYPE			0x00C0
 
-#define XCVR_REG_ARBITRATION(link)				(0x000 | ((link) << 18))
+#define XCVR_REG_ARBITRATION(link)				(0x000 | ((link) << 16))
 #define XCVR_ARBITRATION_MASK					0xFF
 #define XCVR_ARBITRATION_GET_AVMM				0x02
 #define XCVR_ARBITRATION_RELEASE_AVMM			0x01
 
-#define XCVR_REG_CALIB_ATX_PLL_EN(link)			(0x400 | ((link) << 18))
+#define XCVR_REG_CALIB_ATX_PLL_EN(link)			(0x400 | ((link) << 16))
 #define XCVR_CALIB_ATX_PLL_EN_MASK				0x01
 #define XCVR_CALIB_ATX_PLL_EN					0x01
 
-#define XCVR_REG_CAPAB_ATX_PLL_STAT(link)		(0xA00 | ((link) << 18))
+#define XCVR_REG_CAPAB_ATX_PLL_STAT(link)		(0xA00 | ((link) << 16))
 #define XCVR_CAPAB_ATX_PLL_CAL_BSY_MASK			0x02
 #define XCVR_CAPAB_ATX_PLL_CAL_DONE				0x00
 
-#define XCVR_REG_CALIB_PMA_EN(link)				(0x400 | ((link) << 18))
+#define XCVR_REG_CALIB_PMA_EN(link)				(0x400 | ((link) << 16))
 #define XCVR_CALIB_TX_TERM_VOD_MASK				0x20
 #define XCVR_CALIB_TX_TERM_VOD_EN				0x20
 #define XCVR_CALIB_CMU_CDR_PLL_EN_MASK			0x02
 #define XCVR_CALIB_CMU_CDR_PLL_EN				0x02
 
-#define XCVR_REG_CAPAB_PMA(link)				(0xA04 | ((link) << 18))
+#define XCVR_REG_CAPAB_PMA(link)				(0xA04 | ((link) << 16))
 #define XCVR_CAPAB_RX_CAL_BUSY_EN_MASK			0x20
 #define XCVR_CAPAB_RX_CAL_BUSY_EN				0x20
 #define XCVR_CAPAB_RX_CAL_BUSY_DIS				0x00
@@ -77,7 +77,7 @@
 #define XCVR_CAPAB_TX_CAL_BUSY_MASK				0x01
 #define XCVR_CAPAB_TX_CAL_DONE					0x00
 
-#define XCVR_REG_RATE_SWITCH_FLAG(link)			(0x598 | ((link) << 18))
+#define XCVR_REG_RATE_SWITCH_FLAG(link)			(0x598 | ((link) << 16))
 #define XCVR_RATE_SWITCH_FLAG_MASK				0x80
 #define XCVR_RATE_SWITCH_FLAG_NO_RATE_SWITCH	0x80
 
