2c0a0dff4513fd8af91d8c484c4cf239acae7180
Michal Simek <michal.simek@xilinx.com>
2016-08-23 17:21:26 +0200

    tty: serial: of_serial: Do not allocate greater space than required
    The patch fixes the problem when there is another IP on higher addresses
    which require allocation.
    For example:
    uart16550: 0x44a00000 - 0x44a10000 (reg offset: 0x1000)
    IP: 0x44a10000 - 0x44aX0000
    
    Current code is shifting base address with reg-offset value
    (0x44a01000) but the size is the same that's why requested memory
    region for ioremap is from 0x44a01000 with 0x10000 size which overlap
    the next IP where requesting memory region is failing because the part
    of region is already mapped.
    
    The patch decreases mapped size with reg-offset not to overlap selected
    memory address ranges.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/tty/serial/8250/8250_of.c
+++ drivers/tty/serial/8250/8250_of.c
@@ -102,8 +102,10 @@ static int of_platform_serial_setup(struct platform_device *ofdev,
 	port->mapsize = resource_size(&resource);
 
 	/* Check for shifted address mapping */
-	if (of_property_read_u32(np, "reg-offset", &prop) == 0)
+	if (of_property_read_u32(np, "reg-offset", &prop) == 0) {
 		port->mapbase += prop;
+		port->mapsize -= prop;
+	}
 
 	/* Compatibility with the deprecated pxa driver and 8250_pxa drivers. */
 	if (of_device_is_compatible(np, "mrvl,mmp-uart"))
