4d5e3ec31762a914e17a143dbda056e258be4204
Bai Ping <ping.bai@nxp.com>
2016-08-24 10:27:58 +0800

    MLK-13101 ARM: imx: correct the pll2's clock type on imx6sl
    On i,MX6SL, no NUM and DENUM register, so this PLL should not
    be registered as IMX_PLLV3_GENERIC type PLL, it should be
    registered as IMX_PLLV3_SYSV2.
    
    Signed-off-by: Bai Ping <ping.bai@nxp.com>

--- drivers/clk/imx/clk-imx6sl.c
+++ drivers/clk/imx/clk-imx6sl.c
@@ -215,7 +215,7 @@ static void __init imx6sl_clocks_init(struct device_node *ccm_node)
 
 	/*                                    type               name    parent_name        base         div_mask */
 	clks[IMX6SL_CLK_PLL1] = imx_clk_pllv3(IMX_PLLV3_SYS,     "pll1", "osc", base + 0x00, 0x7f);
-	clks[IMX6SL_CLK_PLL2] = imx_clk_pllv3(IMX_PLLV3_PLL2,    "pll2", "osc", base + 0x30, 0x1);
+	clks[IMX6SL_CLK_PLL2] = imx_clk_pllv3(IMX_PLLV3_GENERIC, "pll2", "osc", base + 0x30, 0x1);
 	clks[IMX6SL_CLK_PLL3] = imx_clk_pllv3(IMX_PLLV3_USB,     "pll3", "osc", base + 0x10, 0x3);
 	clks[IMX6SL_CLK_PLL4] = imx_clk_pllv3(IMX_PLLV3_AV,      "pll4", "osc", base + 0x70, 0x7f);
 	clks[IMX6SL_CLK_PLL5] = imx_clk_pllv3(IMX_PLLV3_AV,      "pll5", "osc", base + 0xa0, 0x7f);
