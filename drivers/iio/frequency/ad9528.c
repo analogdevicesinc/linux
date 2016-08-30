49313a6b5a9656c35c6cd2054fa1a9cd6f8df510
Lars-Peter Clausen <lars@metafoo.de>
2016-08-30 13:18:12 +0200

    iio: ad9523/ad9528: Fix displayed phase
    Fix the displayed phase for the ad9523/ad9528 drivers. Currently the most
    significant decimal place is dropped and all other digits are shifted one
    to the left. This is due to a multiplication by 10, which is not necessary,
    so remove it.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/iio/frequency/ad9528.c
+++ drivers/iio/frequency/ad9528.c
@@ -531,7 +531,7 @@ static int ad9528_read_raw(struct iio_dev *indio_dev,
 		code = (AD9528_CLK_DIST_DIV_PHASE_REV(ret) * 3141592) /
 			AD9528_CLK_DIST_DIV_REV(ret);
 		*val = code / 1000000;
-		*val2 = (code % 1000000) * 10;
+		*val2 = code % 1000000;
 		return IIO_VAL_INT_PLUS_MICRO;
 	default:
 		return -EINVAL;
