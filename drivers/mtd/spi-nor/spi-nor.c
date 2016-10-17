5704206727cd1206f7cfcd1eb15f287e99ad60bf
Michael Hennerich <michael.hennerich@analog.com>
2016-10-17 10:52:18 +0200

    drivers/mtd/spi-nor/spi-nor: Info print 17 byte EDID information
    found on some Micron SPI NOR Flashes.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -1657,11 +1657,13 @@ static const struct flash_info spi_nor_ids[] = {
 
 static const struct flash_info *spi_nor_read_id(struct spi_nor *nor)
 {
+#define SPI_NOR_MAX_EDID_LEN	20
+
 	int			tmp;
-	u8			id[SPI_NOR_MAX_ID_LEN];
+	u8			id[SPI_NOR_MAX_EDID_LEN];
 	const struct flash_info	*info;
 
-	tmp = nor->read_reg(nor, SPINOR_OP_RDID, id, SPI_NOR_MAX_ID_LEN);
+	tmp = nor->read_reg(nor, SPINOR_OP_RDID, id, SPI_NOR_MAX_EDID_LEN);
 	if (tmp < 0) {
 		dev_dbg(nor->dev, "error %d reading JEDEC ID\n", tmp);
 		return ERR_PTR(tmp);
@@ -1670,8 +1672,12 @@ static const struct flash_info *spi_nor_read_id(struct spi_nor *nor)
 	for (tmp = 0; tmp < ARRAY_SIZE(spi_nor_ids) - 1; tmp++) {
 		info = &spi_nor_ids[tmp];
 		if (info->id_len) {
-			if (!memcmp(info->id, id, info->id_len))
+			if (!memcmp(info->id, id, info->id_len)) {
+				if (id[0] == SNOR_MFR_MICRON)
+					dev_info(nor->dev, "SPI-NOR-UniqueID %*phN\n",
+						 SPI_NOR_MAX_EDID_LEN - info->id_len, &id[info->id_len]);
 				return &spi_nor_ids[tmp];
+			}
 		}
 	}
 	dev_err(nor->dev, "unrecognized JEDEC id bytes: %02x, %02x, %02x\n",
