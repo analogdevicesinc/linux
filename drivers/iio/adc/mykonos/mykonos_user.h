7b05c6d652ed93ed40a9dd4cea5f57abe5b7526d
Michael Hennerich <michael.hennerich@analog.com>
2016-11-11 13:35:03 +0100

    drivers/iio/adc/mykonos: Update to Mykonos API version: 1.3.1.3534
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/adc/mykonos/mykonos_user.h
+++ drivers/iio/adc/mykonos/mykonos_user.h
@@ -3,7 +3,7 @@
  *
  * \brief Contains macro definitions and global structure declarations for mykonos_user.c
  *
- * Mykonos API version: 1.3.0.3528
+ * Mykonos API version: 1.3.1.3534
  */
 
 #ifndef _MYKONOSPROFILES_H_
@@ -14,12 +14,12 @@ extern "C" {
 #endif
 
 /* turns verbose messaging on */
-#define MYKONOS_VERBOSE 0
-#define MYK_ENABLE_SPIWRITEARRAY 0
+#define MYKONOS_VERBOSE 1
+#define MYK_ENABLE_SPIWRITEARRAY 1
 
 /* 3 Bytes per SPI transaction * 341 transactions = ~1024 byte buffer size */
 /* Minimum MYK_SPIWRITEARRAY_BUFFERSIZE = 27 */
-#define MYK_SPIWRITEARRAY_BUFFERSIZE 300
+#define MYK_SPIWRITEARRAY_BUFFERSIZE 341
 /*
  *****************************************
  * Rx, ObsRx, and Sniffer gain tables
@@ -53,23 +53,23 @@ extern uint8_t SnRxGainTable [53][4];
  * Rx, Sniffer, ObsRx and Tx Profiles limits
  ********************************************
  */
-#define MIN_RX_IQRATE_KHZ   15000
-#define MAX_RX_IQRATE_KHZ   320000
+#define MIN_RX_IQRATE_KHZ   15000       /*!< Mykonos minimum IQ rate for the Rx channel, expressed in KHz */
+#define MAX_RX_IQRATE_KHZ   320000      /*!< Mykonos maximum IQ rate for the Rx channel, expressed in KHz */
 
-#define MIN_TX_IQRATE_KHZ   15000
-#define MAX_TX_IQRATE_KHZ   492000
+#define MIN_TX_IQRATE_KHZ   15000       /*!< Mykonos minimum IQ rate for the Tx channel, expressed in KHz */
+#define MAX_TX_IQRATE_KHZ   492000      /*!< Mykonos maximum IQ rate for the Tx channel, expressed in KHz */
 
-#define MIN_SNIFFER_RFBW_HZ 5000000
-#define MAX_SNIFFER_RFBW_HZ 20000000
+#define MIN_SNIFFER_RFBW_HZ 5000000     /*!< Mykonos minimum Sniffer channel bandwidth expressed in Hz */
+#define MAX_SNIFFER_RFBW_HZ 20000000    /*!< Mykonos maximum Sniffer channel bandwidth expressed in Hz */
 
-#define MIN_ORX_RFBW_HZ     5000000
-#define MAX_ORX_RFBW_HZ     250000000
+#define MIN_ORX_RFBW_HZ     5000000     /*!< Mykonos minimum Observation channel bandwidth expressed in Hz */
+#define MAX_ORX_RFBW_HZ     250000000   /*!< Mykonos maximum Observation channel bandwidth expressed in Hz */
 
-#define MIN_RX_RFBW_HZ      5000000
-#define MAX_RX_RFBW_HZ      100000000
+#define MIN_RX_RFBW_HZ      5000000     /*!< Mykonos minimum Rx channel bandwidth expressed in Hz */
+#define MAX_RX_RFBW_HZ      100000000   /*!< Mykonos maximum Rx channel bandwidth expressed in Hz */
 
-#define MIN_TX_RFBW_HZ      20000000
-#define MAX_TX_RFBW_HZ      250000000
+#define MIN_TX_RFBW_HZ      20000000    /*!< Mykonos minimum Tx channel bandwidth expressed in Hz */
+#define MAX_TX_RFBW_HZ      250000000   /*!< Mykonos maximum Tx channel bandwidth expressed in Hz */
 
 #ifdef __cplusplus
 }
