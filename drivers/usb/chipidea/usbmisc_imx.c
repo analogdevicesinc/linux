bee188567a174d7747107823ec729180aeb25be2
Peter Chen <peter.chen@nxp.com>
2016-12-06 09:41:04 +0800

    MLK-13570-4 usb: chipidea: usbmisc_imx: add imx7ulp entry
    imx7ulp non core register mapping is similar with imx7d, and the
    initialization is the same, but lacks of USB charger detection support.
    
    Signed-off-by: Peter Chen <peter.chen@nxp.com>

--- drivers/usb/chipidea/usbmisc_imx.c
+++ drivers/usb/chipidea/usbmisc_imx.c
@@ -1025,6 +1025,11 @@ static const struct usbmisc_ops imx7d_usbmisc_ops = {
 	.term_select_override = usbmisc_term_select_override,
 };
 
+static const struct usbmisc_ops imx7ulp_usbmisc_ops = {
+	.init = usbmisc_imx7d_init,
+	.set_wakeup = usbmisc_imx7d_set_wakeup,
+};
+
 static inline bool is_imx53_usbmisc(struct imx_usbmisc_data *data)
 {
 	struct imx_usbmisc *usbmisc = dev_get_drvdata(data->dev);
@@ -1223,6 +1228,10 @@ static const struct of_device_id usbmisc_imx_dt_ids[] = {
 	{
 		.compatible = "fsl,imx7d-usbmisc",
 		.data = &imx7d_usbmisc_ops,
+	},
+	{
+		.compatible = "fsl,imx7ulp-usbmisc",
+		.data = &imx7ulp_usbmisc_ops,
 	},
 	{ /* sentinel */ }
 };
