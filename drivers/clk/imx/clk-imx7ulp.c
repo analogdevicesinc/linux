786f959f88ed008f9a3d2b8b5d8f87f530ce9dcb
Yuchou Gan <yuchou.gan@nxp.com>
2016-12-21 02:09:19 +0800

    MGS-2483 Increase the gpu 2d and 3d frequency to 475M Hz on 7ULP
    Increase the gpu 2d and 3d frequency to 475M Hz on 7ULP
    
    Signed-off-by: Yuchou Gan <yuchou.gan@nxp.com>
    Date: Dec 20, 2016

--- drivers/clk/imx/clk-imx7ulp.c
+++ drivers/clk/imx/clk-imx7ulp.c
@@ -175,6 +175,10 @@ static void __init imx7ulp_clocks_init(struct device_node *scg_node)
 
 	for (i = 0; i < ARRAY_SIZE(clks_init_on); i++)
 		imx_clk_prepare_enable(clks[clks_init_on[i]]);
+        imx_clk_set_parent(clks[IMX7ULP_CLK_GPU2D], clks[IMX7ULP_CLK_APLL_PFD2]);
+        imx_clk_set_parent(clks[IMX7ULP_CLK_GPU3D], clks[IMX7ULP_CLK_APLL_PFD2]);
+        imx_clk_prepare_enable(clks[IMX7ULP_CLK_GPU3D]);
+        imx_clk_prepare_enable(clks[IMX7ULP_CLK_GPU2D]);
 
 	pr_info("i.MX7ULP clock tree init done.\n");
 }
