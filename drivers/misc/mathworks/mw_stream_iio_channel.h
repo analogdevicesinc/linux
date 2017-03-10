fdf6a6ffd187f8ded7c3e411260bcd3318d43c04
Matt Fornero <matt.fornero@mathworks.com>
2017-03-10 12:30:21 -0500

    misc: mathworks: Fix build when IIO streaming off
    If building w/o IIO streaming, especially in a tree w/o ADI's IIO
    updates, the build may fail due to missing IIO definitions and circular
    Kconfig dependencies.
    
    * Move definitions out of the shared header file and into the source
      file itself
    * Update Kconfig to remove circular dependencies

--- drivers/misc/mathworks/mw_stream_iio_channel.h
+++ drivers/misc/mathworks/mw_stream_iio_channel.h
@@ -9,43 +9,7 @@
 #ifndef _MW_STREAM_IIO_CHANNEL_H_
 #define _MW_STREAM_IIO_CHANNEL_H_
 
-#include <linux/device.h>
-#include <linux/iio/iio.h>
-#include <linux/errno.h>
 #include "mathworks_ipcore.h"
-
-struct mw_stream_iio_channel_info {
-	enum iio_device_direction 		iio_direction;
-};
-
-enum mw_stream_iio_tlast_mode {
-	MW_STREAM_TLAST_MODE_AUTO = 0,
-	MW_STREAM_TLAST_MODE_USER_LOGIC,
-};
-
-enum mw_stream_iio_reset_tlast_mode {
-	MW_STREAM_TLAST_MODE_PREBUFFER = 0,
-	MW_STREAM_TLAST_MODE_NEVER,
-};
-
-enum mw_stream_iio_reset_ip_mode {
-	MW_STREAM_RESET_IP_MODE_NONE = 0,
-	MW_STREAM_RESET_IP_MODE_ENABLE,
-	MW_STREAM_RESET_IP_MODE_DISABLE,
-	MW_STREAM_RESET_IP_MODE_ALL,
-};
-
-struct mw_stream_iio_chandev {
-	struct mathworks_ipcore_dev 			*mwdev;
-	struct device							dev;
-	enum iio_device_direction 				iio_direction;
-	const char								*dmaname;
-	enum mw_stream_iio_tlast_mode			tlast_mode;
-	enum mw_stream_iio_reset_tlast_mode		reset_tlast_mode;
-	enum mw_stream_iio_reset_ip_mode		reset_ip_mode;
-	int										tlast_cntr_addr;
-	int										num_data_chan;
-};
 
 /*********************************************************
 * API functions
