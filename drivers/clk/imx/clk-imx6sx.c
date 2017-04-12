df749ccca929df379b30f0910dcdbbabeb106ef0
Shengjiu Wang <shengjiu.wang@freescale.com>
2017-04-12 14:45:53 +0800

    MLK-14679-1: ARM: clk: spdif clock rate is too high for asrc
    spdif clock is one of the asrc clock source, which is used
    for ideal ratio mode. when set to 98.304MHz, it cause the
    divider of asrc input clock and output clock exceed the
    maximum value, and asrc driver saturate the value to maximum
    value, which will cause the ASRC's performance very bad.
    So we need to set spdif clock to a proper rate. which make asrc
    divider not exceed maximum value, at least one of divider not
    exceed maximum value.
    The target is spdif clock rate / output(or input) sample rate
    less than 1024(which is maximum divider).
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@freescale.com>

--- drivers/clk/imx/clk-imx6sx.c
+++ drivers/clk/imx/clk-imx6sx.c
@@ -618,7 +618,7 @@ static void __init imx6sx_clocks_init(struct device_node *ccm_node)
 	clk_set_rate(hws[IMX6SX_CLK_PLL4_AUDIO_DIV]->clk, 393216000);
 
 	clk_set_parent(hws[IMX6SX_CLK_SPDIF_SEL]->clk, hws[IMX6SX_CLK_PLL4_AUDIO_DIV]->clk);
-	clk_set_rate(hws[IMX6SX_CLK_SPDIF_PODF]->clk, 98304000);
+	clk_set_rate(hws[IMX6SX_CLK_SPDIF_PODF]->clk, 24576000);
 
 	clk_set_parent(hws[IMX6SX_CLK_AUDIO_SEL]->clk, hws[IMX6SX_CLK_PLL3_USB_OTG]->clk);
 	clk_set_rate(hws[IMX6SX_CLK_AUDIO_PODF]->clk, 24000000);
