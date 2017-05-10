f79d9ee2198803f9dea549ab3dbd8ddd3377bfa7
Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
2017-05-10 19:42:00 +0530

    dwc3: core: Enable GUCTL1 chicken bit 10 for fixing crc error after resume bug
    When configured in HOST mode, after issuing U3/L2 exit controller fails to
    send proper CRC checksum in CRC5 feild. Because of this behaviour
    Transaction Error is generated, resulting in reset and re-enumeration of
    usb device attached. Enabling chicken bit 10 of GUCTL1 will correct this
    problem. This patch does the same.
    
    Signed-off-by: Anurag Kumar Vulisha <anuragku@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1036,8 +1036,6 @@ static int dwc3_core_get_phy(struct dwc3 *dwc)
 			dev_err(dev, "no usb2 phy configured\n");
 			return ret;
 		}
-	} else {
-		dwc3_set_phydata(dev, dwc->usb2_generic_phy);
 	}
 
 	dwc->usb3_generic_phy = devm_phy_get(dev, "usb3-phy");
@@ -1051,8 +1049,6 @@ static int dwc3_core_get_phy(struct dwc3 *dwc)
 			dev_err(dev, "no usb3 phy configured\n");
 			return ret;
 		}
-	} else {
-		dwc3_set_phydata(dev, dwc->usb3_generic_phy);
 	}
 
 	return 0;
