e76ba92ad86d4c87a37ce52235689d7a8d0bb9c3
Robert Chiras <robert.chiras@nxp.com>
2017-05-11 16:00:15 +0300

    MLK-14800: fbdev: Refactor printed information
    Debug info should not be printed using pr_info on init. Use pr_debug
    instead.
    Also, use the driver name, since there is more than one MIPI DSI driver.
    
    Signed-off-by: Robert Chiras <robert.chiras@nxp.com>

--- drivers/video/fbdev/mxc/mipi_dsi_northwest.c
+++ drivers/video/fbdev/mxc/mipi_dsi_northwest.c
@@ -1107,7 +1107,7 @@ static int __init mipi_dsi_init(void)
 		return err;
 	}
 
-	pr_info("MIPI DSI driver module loaded\n");
+	pr_debug("MIPI DSI driver module loaded: %s\n", mipi_dsi_driver.driver.name);
 
 	return 0;
 }
