ca1a578d630affd3a6cba82492538591d99fdecc
Lars-Peter Clausen <lars@metafoo.de>
2017-06-01 14:27:33 +0200

    iio: jesd204: Add library for reconfiguring Xilinx high-speed transceiver
    The Xilinx high-speed transceivers feature a so called
    dynamic-reconfiguration-port (DRP) that can be used to reconfigure the
    operating parameters of the transceiver at runtime.
    
    Add a standalone library module that can be used to calculate and program
    the DRP settings that are relevant for JESD204 operation. It can be used by
    drivers that need to re-program the high-speed transceivers to handle the
    low level details.
    
    The module is independent of the access scheme that is used for the DRP
    registers and provides a set of drp_ops with read and write callbacks that
    need to filled in by a driver making use of the library.
    
    Based on the existing axi_adxcvr and axi_jesd204b_gt drivers.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

---
+++ drivers/iio/jesd204/xilinx_transceiver.h
@@ -0,0 +1,92 @@
+/*
+ * Xilinx High-speed tranceiver dynamic reconfiguration
+ *
+ * Copyright 2016-2017 Analog Devices Inc.
+ *
+ * Licensed under the GPL-2.
+ *
+ */
+
+#ifndef XILINX_XCVR_H
+#define XILINX_XCVR_H
+
+enum xilinx_xcvr_type {
+	XILINX_XCVR_TYPE_S7_GTX2,
+	XILINX_XCVR_TYPE_US_GTH3,
+};
+
+enum xilinx_xcvr_refclk_ppm {
+	PM_200,
+	PM_700,
+	PM_1250,
+};
+
+struct xilinx_xcvr;
+struct device;
+
+struct xilinx_xcvr_drp_ops {
+	int (*write)(struct xilinx_xcvr *xcvr, unsigned int drp_port,
+		unsigned int reg, unsigned int val);
+	int (*read)(struct xilinx_xcvr *xcvr, unsigned int drp_port,
+		unsigned int reg);
+};
+
+struct xilinx_xcvr {
+	struct device *dev;
+
+	const struct xilinx_xcvr_drp_ops *drp_ops;
+
+	enum xilinx_xcvr_type type;
+	enum xilinx_xcvr_refclk_ppm refclk_ppm;
+	unsigned int encoding;
+};
+
+#define ENC_8B10B					810
+
+struct xilinx_xcvr_cpll_config {
+	unsigned int refclk_div;
+	unsigned int fb_div_N1;
+	unsigned int fb_div_N2;
+};
+
+struct xilinx_xcvr_qpll_config {
+	unsigned int refclk_div;
+	unsigned int fb_div;
+	unsigned int band;
+};
+
+int xilinx_xcvr_configure_cdr(struct xilinx_xcvr *xcvr, unsigned int drp_port,
+	unsigned int lane_rate, unsigned int out_div, bool lpm);
+int xilinx_xcvr_configure_lpm_dfe_mode(struct xilinx_xcvr *xcvr,
+	unsigned int drp_port, bool lpm);
+
+int xilinx_xcvr_calc_cpll_config(struct xilinx_xcvr *xcvr,
+	unsigned int refclk_hz, unsigned int lanerate_khz,
+	struct xilinx_xcvr_cpll_config *conf,
+	unsigned int *out_div);
+int xilinx_xcvr_cpll_read_config(struct xilinx_xcvr *xcvr,
+	unsigned int drp_port, struct xilinx_xcvr_cpll_config *conf);
+int xilinx_xcvr_cpll_write_config(struct xilinx_xcvr *xcvr,
+	unsigned int drp_port, const struct xilinx_xcvr_cpll_config *conf);
+int xilinx_xcvr_cpll_calc_lane_rate(struct xilinx_xcvr *xcvr,
+	unsigned int refclk_hz, const struct xilinx_xcvr_cpll_config *conf,
+	unsigned int out_div);
+
+int xilinx_xcvr_calc_qpll_config(struct xilinx_xcvr *xcvr,
+	unsigned int refclk_hz, unsigned int lanerate_khz,
+	struct xilinx_xcvr_qpll_config *conf,
+	unsigned int *out_div);
+int xilinx_xcvr_qpll_read_config(struct xilinx_xcvr *xcvr,
+	unsigned int drp_port, struct xilinx_xcvr_qpll_config *conf);
+int xilinx_xcvr_qpll_write_config(struct xilinx_xcvr *xcvr,
+	unsigned int drp_port, const struct xilinx_xcvr_qpll_config *conf);
+int xilinx_xcvr_qpll_calc_lane_rate(struct xilinx_xcvr *xcvr,
+	unsigned int ref_clk_hz, const struct xilinx_xcvr_qpll_config *conf,
+	unsigned int out_div);
+
+int xilinx_xcvr_read_out_div(struct xilinx_xcvr *xcvr, unsigned int drp_port,
+	unsigned int *rx_out_div, unsigned int *tx_out_div);
+int xilinx_xcvr_write_out_div(struct xilinx_xcvr *xcvr, unsigned int drp_port,
+	int rx_out_div, int tx_out_div);
+
+#endif
