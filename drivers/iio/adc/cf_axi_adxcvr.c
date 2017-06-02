b9c8f35b53231592069c5660fb9b4c13a351907f
Lars-Peter Clausen <lars@metafoo.de>
2017-06-02 18:21:56 +0200

    iio: axi_adxcvr: Remove node field from adxcvr_state struct
    The node field of the adxcvr_state struct is only ever used in the probe
    function where the devicetree node is directly accessible. Remove the node
    field from the struct.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/iio/adc/cf_axi_adxcvr.c
+++ drivers/iio/adc/cf_axi_adxcvr.c
@@ -191,7 +191,6 @@ struct adxcvr_state {
 	struct clk_hw		out_clk_hw;
 	bool				out_clk_enabled;
 	struct work_struct	work;
-	struct device_node *node;
 	unsigned long		lane_rate;
 	bool				gth_enable;
 	bool				tx_enable;
@@ -1027,8 +1026,6 @@ static int adxcvr_parse_dt(struct adxcvr_state *st,
 	st->lpm_enable = of_property_read_bool(np,
 				"adi,use-lpm-enable");
 
-	st->node = np;
-
 	st->encoding = ENC_8B10B;
 	st->ppm = PM_200; /* TODO use clock accuracy */
 
@@ -1107,7 +1104,7 @@ static int adxcvr_probe(struct platform_device *pdev)
 		goto disable_unprepare;
 	}
 
-	clk = adxcvr_clk_register(&pdev->dev, st->node,
+	clk = adxcvr_clk_register(&pdev->dev, np,
 							  __clk_get_name(st->conv_clk), 0);
 	if (IS_ERR(clk)) {
 		ret = PTR_ERR(clk);
@@ -1115,7 +1112,7 @@ static int adxcvr_probe(struct platform_device *pdev)
 	}
 
 	if (!IS_ERR(st->out_clk))
-		of_clk_add_provider(st->node, of_clk_src_simple_get,
+		of_clk_add_provider(np, of_clk_src_simple_get,
 							st->out_clk);
 
 	st->sysref_gpio = devm_gpiod_get_optional(&pdev->dev, "sysref",
