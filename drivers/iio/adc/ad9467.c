bfed6deca6c020e02464caec519b9c31156c4a45
Lars-Peter Clausen <lars@metafoo.de>
2017-06-08 10:22:21 +0200

    iio: ad9467: Enable JESD204 link after AD9680 and lane rate are configured
    For proper glitch free link startup the order in which things are enabled
    and configured need to be considered.
    
    Before the receiver side of the link is enabled the lane rate should be
    configured since changing the lane rate while the link is active will cause
    glitches.
    
    In addition before the receiver side of the link is enabled the transmitter
    side of the AD9680 should be up and running. This is necessary so that the
    CDR on the receiver side has something to lock onto. Otherwise the CDRs
    will be freerunning and each lane will run at a different clock rate, which
    will cause a drift and very large latency variation.
    
    Also make sure that the link is only enabled once (both conv->clk and
    jesd_clk point to the same link clock).
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/iio/adc/ad9467.c
+++ drivers/iio/adc/ad9467.c
@@ -1185,12 +1185,6 @@ static int ad9680_setup(struct spi_device *spi, unsigned m, unsigned l,
 	if (IS_ERR(jesd_clk) && PTR_ERR(jesd_clk) != -ENOENT)
 		return PTR_ERR(jesd_clk);
 
-	if (!IS_ERR(jesd_clk)) {
-		ret = clk_prepare_enable(jesd_clk);
-		if (ret < 0)
-			return ret;
-	}
-
 #ifdef CONFIG_OF
 	if (spi->dev.of_node)
 		tmp = of_property_read_u32_array(
@@ -1238,10 +1232,6 @@ static int ad9680_setup(struct spi_device *spi, unsigned m, unsigned l,
 		ret |= ad9467_spi_write(spi, 0x56e, 0x00);	// low line rate mode must be disabled
 	ret |= ad9467_spi_write(spi, 0x0ff, 0x01);	// write enable
 
-	ret = clk_prepare_enable(conv->clk);
-	if (ret < 0)
-		return ret;
-
 	conv->clk = clk;
 
 	mdelay(20);
@@ -1251,6 +1241,12 @@ static int ad9680_setup(struct spi_device *spi, unsigned m, unsigned l,
 		 pll_stat & 0x80 ? "LOCKED" : "UNLOCKED");
 
 	ret = clk_set_rate(jesd_clk, lane_rate_kHz);
+	if (ret < 0)
+		return ret;
+
+	ret = clk_prepare_enable(jesd_clk);
+	if (ret < 0)
+		return ret;
 
 	conv->sample_rate_read_only = true;
 
