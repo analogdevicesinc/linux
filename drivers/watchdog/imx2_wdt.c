fd54e52c59984994db181389ae91c7f3030a9641
Octavian Purdila <octavian.purdila@nxp.com>
2017-06-21 11:01:23 +0300

    MLK-15083 watchdog: imx2_wdt: fallback to timeout reset if explicit reset fails
    If explicit reset fails fallback using the watchdog timeout. We
    already have set the timeout counter to 0, but we might need to ping
    the watchdog to load the new timeout, if a previous watchdog timeout
    value has already been set.
    
    We also decrease the time we spend waiting, to give a chance to log
    that the explicit reset failed and that we fallback to watchdog
    timeout reset.
    
    Signed-off-by: Octavian Purdila <octavian.purdila@nxp.com>

--- drivers/watchdog/imx2_wdt.c
+++ drivers/watchdog/imx2_wdt.c
@@ -92,6 +92,9 @@ static const struct watchdog_info imx2_wdt_pretimeout_info = {
 		   WDIOF_PRETIMEOUT,
 };
 
+static int imx2_wdt_ping(struct watchdog_device *wdog);
+static inline bool imx2_wdt_is_running(struct imx2_wdt_device *wdev);
+
 static int imx2_wdt_restart(struct watchdog_device *wdog, unsigned long action,
 			    void *data)
 {
@@ -106,6 +109,10 @@ static int imx2_wdt_restart(struct watchdog_device *wdog, unsigned long action,
 
 	/* Assert SRS signal */
 	regmap_write(wdev->regmap, IMX2_WDT_WCR, wcr_enable);
+
+	if (imx2_wdt_is_running(wdev))
+		imx2_wdt_ping(wdog);
+
 	/*
 	 * Due to imx6q errata ERR004346 (WDOG: WDOG SRS bit requires to be
 	 * written twice), we add another two writes to ensure there must be at
@@ -117,7 +124,9 @@ static int imx2_wdt_restart(struct watchdog_device *wdog, unsigned long action,
 	regmap_write(wdev->regmap, IMX2_WDT_WCR, wcr_enable);
 
 	/* wait for reset to assert... */
-	mdelay(500);
+	mdelay(100);
+	dev_err(wdog->parent, "failed to assert %s reset, trying with timeout\n",
+		wdev->ext_reset ? "external" : "internal");
 
 	return 0;
 }
