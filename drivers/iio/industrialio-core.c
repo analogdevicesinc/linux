1fa8772fdabbb5482d36fffeee60d65fd9392ce8
Michael Hennerich <michael.hennerich@analog.com>
2017-08-03 09:06:09 +0200

    iio: add generic data type
    Add generic channel type, "data" for use with devices that do not have
    a fixed physical unit / data type (e.g. FPGA IP cores).
    
    Signed-off-by: Matt Fornero <matt.fornero@mathworks.com>

--- drivers/iio/industrialio-core.c
+++ drivers/iio/industrialio-core.c
@@ -87,6 +87,7 @@ static const char * const iio_chan_type_name_spec[] = {
 	[IIO_GRAVITY]  = "gravity",
 	[IIO_POSITIONRELATIVE]  = "positionrelative",
 	[IIO_PHASE] = "phase",
+	[IIO_GENERIC_DATA] = "data",
 };
 
 static const char * const iio_modifier_names[] = {
