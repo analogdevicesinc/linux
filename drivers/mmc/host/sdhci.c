a913b27fa715f422ab610100b0b8bde921b0c538
Haibo Chen <haibo.chen@nxp.com>
2017-08-04 11:21:03 +0800

    MLK-16155-11 mmc: sdhci: correct the maximum timeout when enable CMDQ
    Priority use the callback set_timeout() to set the maximum timeout
    if the host has.
    
    Signed-off-by: Haibo Chen <haibo.chen@nxp.com>
    Reviewed-by: Dong Aisheng <aisheng.dong@nxp.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -713,7 +713,7 @@ static u32 sdhci_sdma_address(struct sdhci_host *host)
 static u8 sdhci_calc_timeout(struct sdhci_host *host, struct mmc_command *cmd)
 {
 	u8 count;
-	struct mmc_data *data = cmd->data;
+	struct mmc_data *data;
 	unsigned target_timeout, current_timeout;
 
 	/*
@@ -724,6 +724,12 @@ static u8 sdhci_calc_timeout(struct sdhci_host *host, struct mmc_command *cmd)
 	 */
 	if (host->quirks & SDHCI_QUIRK_BROKEN_TIMEOUT_VAL)
 		return 0xE;
+
+	/* Unspecified command, assume max */
+	if (cmd == NULL)
+		return 0xE;
+
+	data = cmd->data;
 
 	/* Unspecified timeout, assume max */
 	if (!data && !cmd->busy_timeout)
@@ -3109,7 +3115,7 @@ void sdhci_cqe_enable(struct mmc_host *mmc)
 		     SDHCI_BLOCK_SIZE);
 
 	/* Set maximum timeout */
-	sdhci_writeb(host, 0xE, SDHCI_TIMEOUT_CONTROL);
+	sdhci_set_timeout(host, NULL);
 
 	host->ier = host->cqe_ier;
 
