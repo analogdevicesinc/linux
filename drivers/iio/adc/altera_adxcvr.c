99e2b6afb4b34e10a65beee5249e8aeb127b2425
Lars-Peter Clausen <lars@metafoo.de>
2017-08-09 16:42:00 +0200

    iio: altera_adxcvr: Remove multi-link support
    The peripheral in its current iteration does non longer have support for
    multiple links. Multiple links are handled through multiple instances of
    the peripheral. Remove support for it from the driver.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/iio/adc/altera_adxcvr.c
+++ drivers/iio/adc/altera_adxcvr.c
@@ -34,28 +34,28 @@
 
 /* XCVR Registers */
 
-#define XCVR_REG_ARBITRATION(link)		(0x000 | ((link) << 16))
+#define XCVR_REG_ARBITRATION			0x000
 #define XCVR_ARBITRATION_MASK			0xFF
 #define XCVR_ARBITRATION_GET_AVMM		0x02
 #define XCVR_ARBITRATION_RELEASE_AVMM		0x01
 
-#define XCVR_REG_CALIB_ATX_PLL_EN(link)		(0x100 | ((link) << 16))
+#define XCVR_REG_CALIB_ATX_PLL_EN		0x100
 #define XCVR_CALIB_ATX_PLL_EN_MASK		0x01
 #define XCVR_CALIB_ATX_PLL_EN			0x01
 
-#define XCVR_REG_CAPAB_ATX_PLL_STAT(link)	(0x280 | ((link) << 16))
+#define XCVR_REG_CAPAB_ATX_PLL_STAT		0x280
 #define XCVR_CAPAB_ATX_PLL_CAL_BSY_MASK		0x02
 #define XCVR_CAPAB_ATX_PLL_CAL_DONE		0x00
 
 
 
-#define XCVR_REG_CALIB_PMA_EN(link)		(0x100 | ((link) << 16))
+#define XCVR_REG_CALIB_PMA_EN			0x100
 #define XCVR_CALIB_TX_TERM_VOD_MASK		0x20
 #define XCVR_CALIB_TX_TERM_VOD_EN		0x20
 #define XCVR_CALIB_CMU_CDR_PLL_EN_MASK		0x02
 #define XCVR_CALIB_CMU_CDR_PLL_EN		0x02
 
-#define XCVR_REG_CAPAB_PMA(link)		(0x281 | ((link) << 16))
+#define XCVR_REG_CAPAB_PMA			0x281
 #define XCVR_CAPAB_RX_CAL_BUSY_EN_MASK		0x20
 #define XCVR_CAPAB_RX_CAL_BUSY_EN		0x20
 #define XCVR_CAPAB_RX_CAL_BUSY_DIS		0x00
@@ -67,7 +67,7 @@
 #define XCVR_CAPAB_TX_CAL_BUSY_MASK		0x01
 #define XCVR_CAPAB_TX_CAL_DONE			0x00
 
-#define XCVR_REG_RATE_SWITCH_FLAG(link)		(0x166 | ((link) << 16))
+#define XCVR_REG_RATE_SWITCH_FLAG		0x166
 #define XCVR_RATE_SWITCH_FLAG_MASK		0x80
 #define XCVR_RATE_SWITCH_FLAG_NO_RATE_SWITCH	0x80
 
@@ -79,7 +79,6 @@ struct adxcvr_state {
 	unsigned int 		version;
 	bool			tx_en;
 	bool			rx_en;
-	u32			link_num;
 	u32			lanes_per_link;
 	u32			delay_work_seconds;
 	struct delayed_work	delayed_work;
@@ -119,7 +118,6 @@ static unsigned int adxcfg_read(struct adxcvr_state *st, unsigned lane,
 
 static int atx_pll_calib(struct adxcvr_state *st)
 {
-	unsigned link = st->link_num;
 	unsigned addr;
 	unsigned mask;
 	unsigned val;
@@ -127,21 +125,21 @@ static int atx_pll_calib(struct adxcvr_state *st)
 	unsigned read_val;
 
 	/* Get AVMM Interface */
-	addr = XCVR_REG_ARBITRATION(link);
+	addr = XCVR_REG_ARBITRATION;
 	mask = XCVR_ARBITRATION_MASK;
 	val = XCVR_ARBITRATION_GET_AVMM;
 	write_val = (atx_pll_read(st, addr) & ~mask) | (val & mask);
 	atx_pll_write(st, addr, write_val);
 
 	/* Initiate re-calibration of ATX_PLL */
-	addr = XCVR_REG_CALIB_ATX_PLL_EN(link);
+	addr = XCVR_REG_CALIB_ATX_PLL_EN;
 	mask = XCVR_CALIB_ATX_PLL_EN_MASK;
 	val = XCVR_CALIB_ATX_PLL_EN;
 	write_val = (atx_pll_read(st, addr) & ~mask) | (val & mask);
 	atx_pll_write(st, addr, write_val);
 
 	/* Release AVMM Interface to PreSICE */
-	addr = XCVR_REG_ARBITRATION(link);
+	addr = XCVR_REG_ARBITRATION;
 	mask = XCVR_ARBITRATION_MASK;
 	val = XCVR_ARBITRATION_RELEASE_AVMM;
 	write_val = (atx_pll_read(st, addr) & ~mask) | (val & mask);
@@ -150,22 +148,21 @@ static int atx_pll_calib(struct adxcvr_state *st)
 	mdelay(100);	// Wait 100ms for cal_busy to de-assert
 
 	/* Read ATX PLL calibration status from capability register */
-	addr = XCVR_REG_CAPAB_ATX_PLL_STAT(link);
+	addr = XCVR_REG_CAPAB_ATX_PLL_STAT;
 	read_val = atx_pll_read(st, addr);
 	if ((read_val & XCVR_CAPAB_ATX_PLL_CAL_BSY_MASK) ==
 			XCVR_CAPAB_ATX_PLL_CAL_DONE) {
-		dev_info(st->dev, "Link %d ATX PLL calibration OK\n", link);
+		dev_info(st->dev, "ATX PLL calibration OK\n");
 		return 0;
 	}
 	else {
-		dev_err(st->dev, "Link %d ATX PLL calibration error\n", link);
+		dev_err(st->dev, "ATX PLL calibration error\n");
 		return 1;
 	}
 }
 
 static int xcvr_calib_tx(struct adxcvr_state *st)
 {
-	unsigned link = st->link_num;
 	unsigned lane;
 	unsigned addr;
 	unsigned mask;
@@ -176,7 +173,7 @@ static int xcvr_calib_tx(struct adxcvr_state *st)
 
 	for (lane = 0; lane < st->lanes_per_link; lane++) {
 		/* Get AVMM Interface from PreSICE through arbitration register */
-		addr = XCVR_REG_ARBITRATION(link);
+		addr = XCVR_REG_ARBITRATION;
 		mask = XCVR_ARBITRATION_MASK;
 		val = XCVR_ARBITRATION_GET_AVMM;
 		write_val = (adxcfg_read(st, lane, addr) & ~mask) | (val & mask);
@@ -184,7 +181,7 @@ static int xcvr_calib_tx(struct adxcvr_state *st)
 
 		/* Perform TX termination & Vod calibration through
 		   PMA calibration enable register */
-		addr = XCVR_REG_CALIB_PMA_EN(link);
+		addr = XCVR_REG_CALIB_PMA_EN;
 		mask = XCVR_CALIB_TX_TERM_VOD_MASK;
 		val = XCVR_CALIB_TX_TERM_VOD_EN;
 		write_val = (adxcfg_read(st, lane, addr) & ~mask) | (val & mask);
@@ -192,14 +189,14 @@ static int xcvr_calib_tx(struct adxcvr_state *st)
 
 		/* Disable rx_cal_busy and enable tx_cal_busy output through
 		   capability register */
-		addr = XCVR_REG_CAPAB_PMA(link);
+		addr = XCVR_REG_CAPAB_PMA;
 		mask = XCVR_CAPAB_RX_CAL_BUSY_EN_MASK | XCVR_CAPAB_TX_CAL_BUSY_EN_MASK;
 		val = XCVR_CAPAB_RX_CAL_BUSY_DIS | XCVR_CAPAB_TX_CAL_BUSY_EN;
 		write_val = (adxcfg_read(st, lane, addr) & ~mask) | (val & mask);
 		adxcfg_write(st, lane, addr, write_val);
 
 		/* Release AVMM Interface to PreSICE */
-		addr = XCVR_REG_ARBITRATION(link);
+		addr = XCVR_REG_ARBITRATION;
 		mask = XCVR_ARBITRATION_MASK;
 		val = XCVR_ARBITRATION_RELEASE_AVMM;
 		write_val = (adxcfg_read(st, lane, addr) & ~mask) | (val & mask);
@@ -208,14 +205,14 @@ static int xcvr_calib_tx(struct adxcvr_state *st)
 		mdelay(100);	// Wait 100ms for cal_busy to de-assert
 
 		/* Read PMA calibration status from capability register */
-		addr = XCVR_REG_CAPAB_PMA(link);
+		addr = XCVR_REG_CAPAB_PMA;
 		read_val = adxcfg_read(st, lane, addr);
 		if ((read_val & XCVR_CAPAB_TX_CAL_BUSY_MASK) == XCVR_CAPAB_TX_CAL_DONE) {
-			dev_info(st->dev, "Link %d ch %d TX termination and VOD calib OK\n",
-					 link, lane);
+			dev_info(st->dev, "Lane %d TX termination and VOD calib OK\n",
+					lane);
 		} else {
-			dev_err(st->dev, "Link %d ch %d TX termination and VOD calib error\n",
-					link, lane);
+			dev_err(st->dev, "Lane %d TX termination and VOD calib error\n",
+					lane);
 			err |= 1;
 		}
 	}
@@ -225,7 +222,6 @@ static int xcvr_calib_tx(struct adxcvr_state *st)
 
 static int xcvr_calib_rx(struct adxcvr_state *st)
 {
-	unsigned link = st->link_num;
 	unsigned lane;
 	unsigned addr;
 	unsigned mask;
@@ -236,7 +232,7 @@ static int xcvr_calib_rx(struct adxcvr_state *st)
 
 	for (lane = 0; lane < st->lanes_per_link; lane++) {
 		/* Get AVMM Interface from PreSICE through arbitration register */
-		addr = XCVR_REG_ARBITRATION(link);
+		addr = XCVR_REG_ARBITRATION;
 		mask = XCVR_ARBITRATION_MASK;
 		val = XCVR_ARBITRATION_GET_AVMM;
 		write_val = (adxcfg_read(st, lane, addr) & ~mask) | (val & mask);
@@ -244,14 +240,14 @@ static int xcvr_calib_rx(struct adxcvr_state *st)
 
 		/* Perform CDR/CMU PLL and RX offset cancellation calibration through
 		   PMA calibration enable register */
-		addr = XCVR_REG_CALIB_PMA_EN(link);
+		addr = XCVR_REG_CALIB_PMA_EN;
 		mask = XCVR_CALIB_CMU_CDR_PLL_EN_MASK;
 		val = XCVR_CALIB_CMU_CDR_PLL_EN;
 		write_val = (adxcfg_read(st, lane, addr) & ~mask) | (val & mask);
 		adxcfg_write(st, lane, addr, write_val);
 
 		/* Set rate switch flag register for CDR charge pump calibration */
-		addr = XCVR_REG_RATE_SWITCH_FLAG(link);
+		addr = XCVR_REG_RATE_SWITCH_FLAG;
 		mask = XCVR_RATE_SWITCH_FLAG_MASK;
 		val = XCVR_RATE_SWITCH_FLAG_NO_RATE_SWITCH;
 		write_val = (adxcfg_read(st, lane, addr) & ~mask) | (val & mask);
@@ -259,14 +255,14 @@ static int xcvr_calib_rx(struct adxcvr_state *st)
 
 		/* Disable tx_cal_busy and enable rx_cal_busy output through
 		   capability register */
-		addr = XCVR_REG_CAPAB_PMA(link);
+		addr = XCVR_REG_CAPAB_PMA;
 		mask = XCVR_CAPAB_RX_CAL_BUSY_EN_MASK | XCVR_CAPAB_TX_CAL_BUSY_EN_MASK;
 		val = XCVR_CAPAB_RX_CAL_BUSY_EN | XCVR_CAPAB_TX_CAL_BUSY_DIS;
 		write_val = (adxcfg_read(st, lane, addr) & ~mask) | (val & mask);
 		adxcfg_write(st, lane, addr, write_val);
 
 		/* Release AVMM Interface to PreSICE */
-		addr = XCVR_REG_ARBITRATION(link);
+		addr = XCVR_REG_ARBITRATION;
 		mask = XCVR_ARBITRATION_MASK;
 		val = XCVR_ARBITRATION_RELEASE_AVMM;
 		write_val = (adxcfg_read(st, lane, addr) & ~mask) | (val & mask);
@@ -275,14 +271,14 @@ static int xcvr_calib_rx(struct adxcvr_state *st)
 		mdelay(100);	// Wait 100ms for cal_busy to de-assert
 
 		/* Read PMA calibration status from capability register */
-		addr = XCVR_REG_CAPAB_PMA(link);
+		addr = XCVR_REG_CAPAB_PMA;
 		read_val = adxcfg_read(st, lane, addr);
 		if ((read_val & XCVR_CAPAB_RX_CAL_BUSY_MASK) == XCVR_CAPAB_RX_CAL_DONE) {
-			dev_info(st->dev, "Link %d ch %d CDR/CMU PLL & RX offset calib OK\n",
-					 link, lane);
+			dev_info(st->dev, "Lane %d CDR/CMU PLL & RX offset calib OK\n",
+					 lane);
 		} else {
-			dev_err(st->dev, "Link %d ch %d CDR/CMU PLL & RX offset calib error\n",
-					link, lane);
+			dev_err(st->dev, "Lane %d CDR/CMU PLL & RX offset calib error\n",
+					lane);
 			err |= 1;
 		}
 	}
@@ -423,8 +419,6 @@ static int adxcvr_probe(struct platform_device *pdev)
 
 	st->version = adxcvr_read(st, ADXCVR_REG_VERSION);
 
-	of_property_read_u32(np, "adi,link-number",
-			     &st->link_num);
 	of_property_read_u32(np, "adi,lanes-per-link",
 			     &st->lanes_per_link);
 
