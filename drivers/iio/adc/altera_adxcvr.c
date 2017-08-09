d36e2cf36edc72e30df1fa4f102c41c400ef8f84
Lars-Peter Clausen <lars@metafoo.de>
2017-08-09 17:50:10 +0200

    iio: altera_adxcvr: Use 8-bit addresses
    The AVMM register space is a 8-bit internally, but mapped onto a 32-bit
    bus. So when reading or writing registers the address needs to be
    multiplied by 4. Currently this is included in the register definition.
    Change things so that the register definitions use the 8-bit address and do
    the multiplication in the IO accessor functions. This makes it easier to
    compare the register addresses defined in the driver to the register map
    documentation.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/iio/adc/altera_adxcvr.c
+++ drivers/iio/adc/altera_adxcvr.c
@@ -39,23 +39,23 @@
 #define XCVR_ARBITRATION_GET_AVMM		0x02
 #define XCVR_ARBITRATION_RELEASE_AVMM		0x01
 
-#define XCVR_REG_CALIB_ATX_PLL_EN(link)		(0x400 | ((link) << 16))
+#define XCVR_REG_CALIB_ATX_PLL_EN(link)		(0x100 | ((link) << 16))
 #define XCVR_CALIB_ATX_PLL_EN_MASK		0x01
 #define XCVR_CALIB_ATX_PLL_EN			0x01
 
-#define XCVR_REG_CAPAB_ATX_PLL_STAT(link)	(0xA00 | ((link) << 16))
+#define XCVR_REG_CAPAB_ATX_PLL_STAT(link)	(0x280 | ((link) << 16))
 #define XCVR_CAPAB_ATX_PLL_CAL_BSY_MASK		0x02
 #define XCVR_CAPAB_ATX_PLL_CAL_DONE		0x00
 
 
 
-#define XCVR_REG_CALIB_PMA_EN(link)		(0x400 | ((link) << 16))
+#define XCVR_REG_CALIB_PMA_EN(link)		(0x100 | ((link) << 16))
 #define XCVR_CALIB_TX_TERM_VOD_MASK		0x20
 #define XCVR_CALIB_TX_TERM_VOD_EN		0x20
 #define XCVR_CALIB_CMU_CDR_PLL_EN_MASK		0x02
 #define XCVR_CALIB_CMU_CDR_PLL_EN		0x02
 
-#define XCVR_REG_CAPAB_PMA(link)		(0xA04 | ((link) << 16))
+#define XCVR_REG_CAPAB_PMA(link)		(0x281 | ((link) << 16))
 #define XCVR_CAPAB_RX_CAL_BUSY_EN_MASK		0x20
 #define XCVR_CAPAB_RX_CAL_BUSY_EN		0x20
 #define XCVR_CAPAB_RX_CAL_BUSY_DIS		0x00
@@ -67,7 +67,7 @@
 #define XCVR_CAPAB_TX_CAL_BUSY_MASK		0x01
 #define XCVR_CAPAB_TX_CAL_DONE			0x00
 
-#define XCVR_REG_RATE_SWITCH_FLAG(link)		(0x598 | ((link) << 16))
+#define XCVR_REG_RATE_SWITCH_FLAG(link)		(0x166 | ((link) << 16))
 #define XCVR_RATE_SWITCH_FLAG_MASK		0x80
 #define XCVR_RATE_SWITCH_FLAG_NO_RATE_SWITCH	0x80
 
@@ -97,24 +97,24 @@ static unsigned int adxcvr_read(struct adxcvr_state *st, unsigned reg)
 
 static void atx_pll_write(struct adxcvr_state *st, unsigned reg, unsigned val)
 {
-	iowrite32(val, st->atx_pll_regs + reg);
+	iowrite32(val, st->atx_pll_regs + reg * 4);
 }
 
 static unsigned int atx_pll_read(struct adxcvr_state *st, unsigned reg)
 {
-	return ioread32(st->atx_pll_regs + reg);
+	return ioread32(st->atx_pll_regs + reg * 4);
 }
 
 static void adxcfg_write(struct adxcvr_state *st, unsigned lane, unsigned reg,
 	unsigned val)
 {
-	iowrite32(val, st->adxcfg_regs[lane] + reg);
+	iowrite32(val, st->adxcfg_regs[lane] + reg * 4);
 }
 
 static unsigned int adxcfg_read(struct adxcvr_state *st, unsigned lane,
 	unsigned reg)
 {
-	return ioread32(st->adxcfg_regs[lane] + reg);
+	return ioread32(st->adxcfg_regs[lane] + reg * 4);
 }
 
 static int atx_pll_calib(struct adxcvr_state *st)
