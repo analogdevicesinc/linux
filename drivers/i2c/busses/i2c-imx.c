3d4b46fe0c0c0651b78d6de185ba83e7dbab906b
Gao Pan <pandy.gao@nxp.com>
2017-08-18 18:35:11 +0800

    MLK-16203 enable runtime pm of i2c temporary when do system suspend
    When we do system suspend, the runtime pm will be disabled, but we need
    to control the PMIC to power on/off the regulator, if the runtime pm is
    disabled, if will failed to request runtime wakeup. So data transfer will
    failed.
    
    Signed-off-by: Bai Ping <ping.bai@nxp.com>
    Signed-off-by: Gao Pan <pandy.gao@nxp.com>
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
    (cherry picked from commit 93adab71408f1f162015e77715dd04ce2301d673)
    [ Aisheng: fix big conflict due to
    3a5ee18d2a32 ("i2c: imx: implement master_xfer_atomic callback") ]
    [ Aisheng: switch to NOIRQ_SYSTEM_SLEEP_PM_OPS ]
    Sign-off-by: Dong Aisheng <aisheng.dong@nxp.com>

--- drivers/i2c/busses/i2c-imx.c
+++ drivers/i2c/busses/i2c-imx.c
@@ -1242,6 +1242,7 @@ static int i2c_imx_xfer_common(struct i2c_adapter *adapter,
 	unsigned int i, temp;
 	int result;
 	bool is_lastmsg = false;
+	bool enable_runtime_pm = false;
 	struct imx_i2c_struct *i2c_imx = i2c_get_adapdata(adapter);
 
 	/* Start I2C transfer */
@@ -1327,11 +1328,20 @@ static int i2c_imx_xfer(struct i2c_adapter *adapter,
 			struct i2c_msg *msgs, int num)
 {
 	struct imx_i2c_struct *i2c_imx = i2c_get_adapdata(adapter);
+	bool enable_runtime_pm = false;
 	int result;
 
+	if (!pm_runtime_enabled(i2c_imx->adapter.dev.parent)) {
+		pm_runtime_enable(i2c_imx->adapter.dev.parent);
+		enable_runtime_pm = true;
+	}
+
 	result = pm_runtime_resume_and_get(i2c_imx->adapter.dev.parent);
-	if (result < 0)
+	if (result < 0) {
+		if (enable_runtime_pm)
+			pm_runtime_disable(i2c_imx->adapter.dev.parent);
 		return result;
+	}
 
 	result = i2c_imx_xfer_common(adapter, msgs, num, false);
 
@@ -1585,7 +1595,7 @@ static int i2c_imx_resume(struct device *dev)
 }
 
 static const struct dev_pm_ops i2c_imx_pm_ops = {
-	SYSTEM_SLEEP_PM_OPS(i2c_imx_suspend, i2c_imx_resume)
+	NOIRQ_SYSTEM_SLEEP_PM_OPS(i2c_imx_suspend, i2c_imx_resume)
 	RUNTIME_PM_OPS(i2c_imx_runtime_suspend, i2c_imx_runtime_resume, NULL)
 };
 
