58d686899e5cab2e88c2e35fb34b91690b1ed99b
Lars-Peter Clausen <lars@metafoo.de>
2017-09-01 17:55:43 +0200

    drm/bridge: adv7511: Add support for configuring the EDID I2C address
    The ADV7511 has a secondary I2C address for the EDID memory. This address
    is programmable and currently the driver sets it to an address at a fixed
    offset to the primary I2C address.
    
    This approach does not work on all platforms and can lead to an I2C address
    conflict with other devices. To avoid this allow the secondary address to
    be specified through firmware data (e.g. devicetree).
    
    This is done by replacing i2c_new_dummy() with i2c_new_secondary_device().
    It will check if a address for the secondary I2C interface is specified via
    the firmware interface and if it is use that address. If no address is
    specified it will fallback to the fixed offset address.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
+++ drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
@@ -543,15 +543,13 @@ static int adv7511_get_modes(struct adv7511 *adv7511,
 
 	/* Reading the EDID only works if the device is powered */
 	if (!adv7511->powered) {
-		regmap_update_bits(adv7511->regmap, ADV7511_REG_POWER,
-				   ADV7511_POWER_POWER_DOWN, 0);
-		if (adv7511->i2c_main->irq) {
-			regmap_write(adv7511->regmap, ADV7511_REG_INT_ENABLE(0),
-				     ADV7511_INT0_EDID_READY);
-			regmap_write(adv7511->regmap, ADV7511_REG_INT_ENABLE(1),
-				     ADV7511_INT1_DDC_ERROR);
-		}
-		adv7511->current_edid_segment = -1;
+		unsigned int edid_i2c_addr = adv7511->i2c_edid->addr << 1;
+
+		__adv7511_power_on(adv7511);
+
+		/* Reset the EDID_I2C_ADDR register as it might be cleared */
+		regmap_write(adv7511->regmap, ADV7511_REG_EDID_I2C_ADDR,
+			     edid_i2c_addr);
 	}
 
 	edid = drm_do_get_edid(connector, adv7511_get_edid_block, adv7511);
@@ -983,7 +981,6 @@ static int adv7511_probe(struct i2c_client *i2c, const struct i2c_device_id *id)
 	if (ret)
 		return ret;
 
-	regmap_write(adv7511->regmap, ADV7511_REG_EDID_I2C_ADDR, edid_i2c_addr);
 	regmap_write(adv7511->regmap, ADV7511_REG_PACKET_I2C_ADDR,
 		     main_i2c_addr - 0xa);
 	regmap_write(adv7511->regmap, ADV7511_REG_CEC_I2C_ADDR,
@@ -992,9 +989,13 @@ static int adv7511_probe(struct i2c_client *i2c, const struct i2c_device_id *id)
 	adv7511_packet_disable(adv7511, 0xffff);
 
 	adv7511->i2c_main = i2c;
-	adv7511->i2c_edid = i2c_new_dummy(i2c->adapter, edid_i2c_addr >> 1);
+	adv7511->i2c_edid = i2c_new_secondary_device(i2c, "edid",
+						     edid_i2c_addr >> 1);
 	if (!adv7511->i2c_edid)
 		return -ENOMEM;
+
+	regmap_write(adv7511->regmap, ADV7511_REG_EDID_I2C_ADDR,
+		     adv7511->i2c_edid->addr << 1);
 
 	if (adv7511->type == ADV7533) {
 		ret = adv7533_init_cec(adv7511);
