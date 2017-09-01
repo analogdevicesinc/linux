2828e5b0f81d979386ec208bc749e3868b23fbf5
Lars-Peter Clausen <lars@metafoo.de>
2017-09-01 10:56:09 +0200

    drm: adi_axi_hdmi: Convert to atomic mode setting
    The DRM framework is transitioning towards atomic mode setting and
    non-atomic mode setting has been deprecated and will be removed eventually.
    
    Update the adi_axi_hdmi driver to atomic mode setting to follow what the
    framework does.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/gpu/drm/adi_axi_hdmi/axi_hdmi_encoder.c
+++ drivers/gpu/drm/adi_axi_hdmi/axi_hdmi_encoder.c
@@ -16,7 +16,7 @@
 #include <linux/debugfs.h>
 #include <linux/clk.h>
 
-#include <drm/drmP.h>
+#include <drm/drm_atomic_helper.h>
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_encoder_slave.h>
 
@@ -216,7 +216,7 @@ static inline void axi_hdmi_debugfs_init(struct axi_hdmi_encoder *enc)
 
 #endif
 
-static void axi_hdmi_encoder_dpms(struct drm_encoder *encoder, int mode)
+static void axi_hdmi_encoder_enable(struct drm_encoder *encoder)
 {
 	struct axi_hdmi_encoder *axi_hdmi_encoder = to_axi_hdmi_encoder(encoder);
 	struct drm_connector *connector = &axi_hdmi_encoder->connector;
@@ -225,84 +225,81 @@ static void axi_hdmi_encoder_dpms(struct drm_encoder *encoder, int mode)
 	struct adv7511_video_config config;
 	struct edid *edid;
 
-	switch (mode) {
-	case DRM_MODE_DPMS_ON:
-		if (!private->clk_enabled) {
-			clk_prepare_enable(private->hdmi_clock);
-			private->clk_enabled = true;
-		}
-		writel(AXI_HDMI_RESET_ENABLE, private->base + AXI_HDMI_REG_RESET);
+	if (!private->clk_enabled) {
+		clk_prepare_enable(private->hdmi_clock);
+		private->clk_enabled = true;
+	}
+	writel(AXI_HDMI_RESET_ENABLE, private->base + AXI_HDMI_REG_RESET);
 
-		if (!connector)
-			edid = NULL;
-		else
-			edid = drm_connector_get_edid(connector);
+	if (!sfuncs)
+		return;
 
-		if (edid) {
-			config.hdmi_mode = drm_detect_hdmi_monitor(edid);
-			kfree(edid);
+	if (!connector)
+		edid = NULL;
+	else
+		edid = drm_connector_get_edid(connector);
+
+	if (edid)
+		config.hdmi_mode = drm_detect_hdmi_monitor(edid);
+	else
+		config.hdmi_mode = false;
+
+	hdmi_avi_infoframe_init(&config.avi_infoframe);
+
+	config.avi_infoframe.scan_mode = HDMI_SCAN_MODE_UNDERSCAN;
+
+	if (private->is_rgb) {
+			config.csc_enable = false;
+			config.avi_infoframe.colorspace = HDMI_COLORSPACE_RGB;
+	} else {
+		config.csc_scaling_factor = ADV7511_CSC_SCALING_4;
+		config.csc_coefficents = adv7511_csc_ycbcr_to_rgb;
+
+		if ((connector->display_info.color_formats & DRM_COLOR_FORMAT_YCRCB422) &&
+			config.hdmi_mode) {
+			config.csc_enable = false;
+			config.avi_infoframe.colorspace = HDMI_COLORSPACE_YUV422;
 		} else {
-			config.hdmi_mode = false;
+			config.csc_enable = true;
+			config.avi_infoframe.colorspace = HDMI_COLORSPACE_RGB;
 		}
+	}
 
-		hdmi_avi_infoframe_init(&config.avi_infoframe);
-
-		config.avi_infoframe.scan_mode = HDMI_SCAN_MODE_UNDERSCAN;
-
-		if (private->is_rgb) {
-				config.csc_enable = false;
-				config.avi_infoframe.colorspace = HDMI_COLORSPACE_RGB;
-		} else {
-			config.csc_scaling_factor = ADV7511_CSC_SCALING_4;
-			config.csc_coefficents = adv7511_csc_ycbcr_to_rgb;
-
-			if ((connector->display_info.color_formats & DRM_COLOR_FORMAT_YCRCB422) &&
-				config.hdmi_mode) {
-				config.csc_enable = false;
-				config.avi_infoframe.colorspace = HDMI_COLORSPACE_YUV422;
-			} else {
-				config.csc_enable = true;
-				config.avi_infoframe.colorspace = HDMI_COLORSPACE_RGB;
-			}
-		}
-
+	if (sfuncs->set_config)
 		sfuncs->set_config(encoder, &config);
-		break;
-	default:
-		writel(0, private->base + AXI_HDMI_REG_RESET);
-		if (private->clk_enabled) {
-			clk_disable_unprepare(private->hdmi_clock);
-			private->clk_enabled = false;
-		}
-		break;
+
+	if (sfuncs->dpms)
+		sfuncs->dpms(encoder, DRM_MODE_DPMS_ON);
+}
+
+static void axi_hdmi_encoder_disable(struct drm_encoder *encoder)
+{
+	struct axi_hdmi_private *private = encoder->dev->dev_private;
+	const struct drm_encoder_slave_funcs *sfuncs = get_slave_funcs(encoder);
+
+	writel(0, private->base + AXI_HDMI_REG_RESET);
+	if (private->clk_enabled) {
+		clk_disable_unprepare(private->hdmi_clock);
+		private->clk_enabled = false;
 	}
 
 	if (sfuncs && sfuncs->dpms)
-		sfuncs->dpms(encoder, mode);
-}
-
-static bool axi_hdmi_encoder_mode_fixup(struct drm_encoder *encoder,
-	const struct drm_display_mode *mode, struct drm_display_mode *adjusted_mode)
-{
-	struct drm_encoder_slave_funcs *sfuncs = get_slave_funcs(encoder);
-
-	if (sfuncs && sfuncs->mode_fixup)
-		return sfuncs->mode_fixup(encoder, mode, adjusted_mode);
-
-	return true;
+		sfuncs->dpms(encoder, DRM_MODE_DPMS_OFF);
 }
 
 static void axi_hdmi_encoder_mode_set(struct drm_encoder *encoder,
-	struct drm_display_mode *mode, struct drm_display_mode *adjusted_mode)
+	struct drm_crtc_state *crtc_state,
+	struct drm_connector_state *conn_state)
 {
 	struct drm_encoder_slave_funcs *sfuncs = get_slave_funcs(encoder);
 	struct axi_hdmi_private *private = encoder->dev->dev_private;
+	struct drm_display_mode *mode = &crtc_state->mode;
 	unsigned int h_de_min, h_de_max;
 	unsigned int v_de_min, v_de_max;
 	unsigned int val;
 
 	if (sfuncs && sfuncs->mode_set)
-		sfuncs->mode_set(encoder, mode, adjusted_mode);
+		sfuncs->mode_set(encoder, mode, &crtc_state->adjusted_mode);
 
 	h_de_min = mode->htotal - mode->hsync_start;
 	h_de_max = h_de_min + mode->hdisplay;
@@ -326,28 +323,10 @@ static void axi_hdmi_encoder_mode_set(struct drm_encoder *encoder,
 	clk_set_rate(private->hdmi_clock, mode->clock * 1000);
 }
 
-static void axi_hdmi_encoder_commit(struct drm_encoder *encoder)
-{
-	axi_hdmi_encoder_dpms(encoder, DRM_MODE_DPMS_ON);
-}
-
-static void axi_hdmi_encoder_prepare(struct drm_encoder *encoder)
-{
-	axi_hdmi_encoder_dpms(encoder, DRM_MODE_DPMS_OFF);
-}
-
-static struct drm_crtc *axi_hdmi_encoder_get_crtc(struct drm_encoder *encoder)
-{
-	return encoder->crtc;
-}
-
-static struct drm_encoder_helper_funcs axi_hdmi_encoder_helper_funcs = {
-	.dpms		= axi_hdmi_encoder_dpms,
-	.mode_fixup	= axi_hdmi_encoder_mode_fixup,
-	.mode_set	= axi_hdmi_encoder_mode_set,
-	.prepare	= axi_hdmi_encoder_prepare,
-	.commit		= axi_hdmi_encoder_commit,
-	.get_crtc	= axi_hdmi_encoder_get_crtc,
+static const struct drm_encoder_helper_funcs axi_hdmi_encoder_helper_funcs = {
+	.enable = axi_hdmi_encoder_enable,
+	.disable = axi_hdmi_encoder_disable,
+	.atomic_mode_set = axi_hdmi_encoder_mode_set,
 };
 
 static void axi_hdmi_encoder_destroy(struct drm_encoder *encoder)
@@ -360,11 +339,10 @@ static void axi_hdmi_encoder_destroy(struct drm_encoder *encoder)
 		sfuncs->destroy(encoder);
 
 	drm_encoder_cleanup(encoder);
-	encoder->dev->mode_config.num_encoder--;
 	kfree(axi_hdmi_encoder);
 }
 
-static struct drm_encoder_funcs axi_hdmi_encoder_funcs = {
+static const struct drm_encoder_funcs axi_hdmi_encoder_funcs = {
 	.destroy = axi_hdmi_encoder_destroy,
 };
 
@@ -460,10 +438,13 @@ static void axi_hdmi_connector_destroy(struct drm_connector *connector)
 }
 
 static struct drm_connector_funcs axi_hdmi_connector_funcs = {
-	.dpms		= drm_helper_connector_dpms,
-	.fill_modes	= drm_helper_probe_single_connector_modes,
-	.detect		= axi_hdmi_connector_detect,
-	.destroy	= axi_hdmi_connector_destroy,
+	.dpms = drm_atomic_helper_connector_dpms,
+	.fill_modes = drm_helper_probe_single_connector_modes,
+	.detect = axi_hdmi_connector_detect,
+	.destroy = axi_hdmi_connector_destroy,
+	.reset = drm_atomic_helper_connector_reset,
+	.atomic_duplicate_state = drm_atomic_helper_connector_duplicate_state,
+	.atomic_destroy_state = drm_atomic_helper_connector_destroy_state,
 };
 
 static int axi_hdmi_connector_init(struct drm_device *dev,
