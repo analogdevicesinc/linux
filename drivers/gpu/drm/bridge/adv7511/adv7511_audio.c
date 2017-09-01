fe80873621ac9126d052418f2deedffd942cab30
Lars-Peter Clausen <lars@metafoo.de>
2017-09-01 14:08:36 +0200

    drm/bridge: adv7511: Enable SPDIF support
    This is work-in-progress. Breaks I2S support at the moment.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/gpu/drm/bridge/adv7511/adv7511_audio.c
+++ drivers/gpu/drm/bridge/adv7511/adv7511_audio.c
@@ -120,6 +120,9 @@ int adv7511_hdmi_hw_params(struct device *dev, void *data,
 		audio_source = ADV7511_AUDIO_SOURCE_I2S;
 		i2s_format = ADV7511_I2S_FORMAT_LEFT_J;
 		break;
+	case HDMI_SPDIF:
+		audio_source = ADV7511_AUDIO_SOURCE_SPDIF;
+		break;
 	default:
 		return -EINVAL;
 	}
@@ -213,7 +216,7 @@ static const struct hdmi_codec_ops adv7511_codec_ops = {
 static const struct hdmi_codec_pdata codec_data = {
 	.ops = &adv7511_codec_ops,
 	.max_i2s_channels = 2,
-	.i2s = 1,
+	.spdif = 1,
 };
 
 int adv7511_audio_init(struct device *dev, struct adv7511 *adv7511)
