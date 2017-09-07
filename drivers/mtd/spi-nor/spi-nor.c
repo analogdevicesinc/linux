4ed510702e0c63beb1c4dc776468d39db068f32f
Michael Hennerich <michael.hennerich@analog.com>
2017-09-07 12:27:28 +0200

    mtd: spi-nor: Don't assume all STmicro/Micron chips have lock support
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -3457,8 +3457,7 @@ int spi_nor_scan(struct spi_nor *nor, const char *name,
 	mtd->_resume = spi_nor_resume;
 
 	/* NOR protection support for STmicro/Micron chips and similar */
-	if (JEDEC_MFR(info) == SNOR_MFR_MICRON ||
-			info->flags & SPI_NOR_HAS_LOCK) {
+	if (info->flags & SPI_NOR_HAS_LOCK) {
 		nor->flash_lock = stm_lock;
 		nor->flash_unlock = stm_unlock;
 		nor->flash_is_locked = stm_is_locked;
