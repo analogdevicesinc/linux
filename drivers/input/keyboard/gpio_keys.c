30178cff89baccba3f194409ba667422d697eb26
Michael Hennerich <michael.hennerich@analog.com>
2017-09-28 11:44:25 +0200

    Revert "gpio_keys: Added support to read the IRQ_FLAGS from devicetree"
    This reverts commit a59cfe9317a522c11d03924cda5a784e990cf33c.
    
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -514,7 +514,7 @@ static int gpio_keys_setup_key(struct platform_device *pdev,
 		INIT_DELAYED_WORK(&bdata->work, gpio_keys_gpio_work_func);
 
 		isr = gpio_keys_gpio_isr;
-		irqflags = button->irq_flags;
+		irqflags = IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING;
 
 	} else {
 		if (!button->irq) {
@@ -653,7 +653,7 @@ gpio_keys_get_devtree_pdata(struct device *dev)
 
 		button = &pdata->buttons[i++];
 
-		button->gpio = of_get_gpio_flags(pp, 0, &button->irq_flags);
+		button->gpio = of_get_gpio_flags(pp, 0, &flags);
 		if (button->gpio < 0) {
 			error = button->gpio;
 			if (error != -ENOENT) {
@@ -664,8 +664,7 @@ gpio_keys_get_devtree_pdata(struct device *dev)
 				return ERR_PTR(error);
 			}
 		} else {
-			button->active_low = button->irq_flags
-						& OF_GPIO_ACTIVE_LOW;
+			button->active_low = flags & OF_GPIO_ACTIVE_LOW;
 		}
 
 		button->irq = irq_of_parse_and_map(pp, 0);
