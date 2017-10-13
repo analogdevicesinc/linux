cf9241d492e6afe7fc6cf7f7be5bb8f575c20972
Dragos Bogdan <dragos.bogdan@analog.com>
2017-10-13 15:27:09 +0300

    iio: ad9528: Request both Status pins as output low GPIOs
    The AD9528 has two serial interfaces, SPI and I2C. Users can
    select either the SPI or I2C depending on the states (logic high,
    logic low) of the two logic level input pins (STATUS0/SP0 and
    STATUS1/SP1), when initial power is applied or after a RESET.
    
    Request the pins as output low GPIOs before the RESET is applied
    in order to enable the SPI interface. Set them as input GPIOs
    after the process is done so the driver can safely enable the
    chip status indicators.

--- drivers/iio/frequency/ad9528.c
+++ drivers/iio/frequency/ad9528.c
@@ -946,7 +946,7 @@ static int ad9528_setup(struct iio_dev *indio_dev)
 	if (ret < 0)
 		return ret;
 
-	
+
 	vco_freq = div_u64((unsigned long long)pdata->vcxo_freq *
 			   (pdata->pll2_freq_doubler_en ? 2 : 1) * pll2_ndiv,
 			    pdata->pll2_r1_div);
@@ -1314,6 +1314,8 @@ static int ad9528_probe(struct spi_device *spi)
 	struct ad9528_platform_data *pdata;
 	struct iio_dev *indio_dev;
 	struct ad9528_state *st;
+	struct gpio_desc *status0_gpio;
+	struct gpio_desc *status1_gpio;
 	int ret;
 
 	if (spi->dev.of_node)
@@ -1341,7 +1343,10 @@ static int ad9528_probe(struct spi_device *spi)
 			return ret;
 	}
 
-	devm_gpiod_get(&spi->dev, "status0", GPIOD_OUT_LOW);
+	status0_gpio = devm_gpiod_get_optional(&spi->dev,
+					"status0", GPIOD_OUT_LOW);
+	status1_gpio = devm_gpiod_get_optional(&spi->dev,
+					"status1", GPIOD_OUT_LOW);
 
 	st->reset_gpio = devm_gpiod_get(&spi->dev, "reset", GPIOD_OUT_LOW);
 	if (!IS_ERR(st->reset_gpio)) {
@@ -1350,6 +1355,11 @@ static int ad9528_probe(struct spi_device *spi)
 	}
 
 	mdelay(10);
+
+	if (!PTR_ERR_OR_ZERO(status0_gpio))
+		gpiod_direction_input(status0_gpio);
+	if (!PTR_ERR_OR_ZERO(status1_gpio))
+		gpiod_direction_input(status1_gpio);
 
 	spi_set_drvdata(spi, indio_dev);
 	st->spi = spi;
