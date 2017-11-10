7b77a602db5f7c501bc9e3385bb3d278ca1649e7
Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
2017-11-10 15:33:04 +0530

    usb: dwc3: Clean the logic for assigning USB control registers to phy
    This patch cleans the logic for assigning USB control registers to phy
    platform data, which are used by phy driver for setting USB PIPE CLK and
    POWER PRESENT signals.
    
    Signed-off-by: Anurag Kumar Vulisha <anuragku@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1003,8 +1003,6 @@ static int dwc3_core_get_phy(struct dwc3 *dwc)
 			dev_err(dev, "no usb2 phy configured\n");
 			return ret;
 		}
-	} else {
-		dwc3_set_phydata(dev, dwc->usb2_generic_phy);
 	}
 
 	dwc->usb3_generic_phy = devm_phy_get(dev, "usb3-phy");
@@ -1018,8 +1016,6 @@ static int dwc3_core_get_phy(struct dwc3 *dwc)
 			dev_err(dev, "no usb3 phy configured\n");
 			return ret;
 		}
-	} else {
-		dwc3_set_phydata(dev, dwc->usb3_generic_phy);
 	}
 
 	return 0;
