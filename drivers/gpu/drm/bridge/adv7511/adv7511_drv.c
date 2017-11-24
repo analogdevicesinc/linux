811707426cac349c0d4cca2d8e6dddd5bd0f19cd
Robert Chiras <robert.chiras@nxp.com>
2017-11-24 14:04:24 +0200

    MLK-16986-4: drm: bridge: adv7511: set bus_flags and bus_format
    For a proper initialization of the crtc driving the connector for this
    bridge, we need to set the bus_formats and bus_flags of the connector's
    display_info.
    
    Signed-off-by: Robert Chiras <robert.chiras@nxp.com>
    Reviewed-by: Laurentiu Palcu <laurentiu.palcu@nxp.com>

--- drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
+++ drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
@@ -583,6 +583,8 @@ static int adv7511_get_modes(struct adv7511 *adv7511,
 {
 	struct edid *edid;
 	unsigned int count;
+	u32 bus_format = MEDIA_BUS_FMT_RGB888_1X24;
+	int ret;
 
 	/* Reading the EDID only works if the device is powered */
 	if (!adv7511->powered) {
@@ -610,6 +612,14 @@ static int adv7511_get_modes(struct adv7511 *adv7511,
 	count = drm_add_edid_modes(connector, edid);
 
 	adv7511_set_config_csc(adv7511, connector, adv7511->rgb);
+
+	connector->display_info.bus_flags = DRM_BUS_FLAG_DE_LOW |
+					    DRM_BUS_FLAG_PIXDATA_NEGEDGE;
+
+	ret = drm_display_info_set_bus_formats(&connector->display_info,
+					       &bus_format, 1);
+	if (ret)
+		return ret;
 
 	return count;
 }
