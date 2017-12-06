d4f36fbd261bd716895974f4452cc98bf279ea40
Li Jun <jun.li@nxp.com>
2017-12-06 23:55:51 +0800

    MLK-17092 staging: typec: enable vbus voltage low alarm
    We use vbus low voltage alarm to start vbus discharge to meet
    timing requirement on turning off vbus for power swap from
    source to sink, per type-C port controller spec(tcpci), the
    Voltage Alarms Power status reporting is disabled by default,
    so we need enable it at tcpci init.
    
    Acked-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Li Jun <jun.li@nxp.com>

--- drivers/staging/typec/tcpci.c
+++ drivers/staging/typec/tcpci.c
@@ -516,6 +516,11 @@ static int tcpci_init(struct tcpc_dev *tcpc)
 	if (ret < 0)
 		return ret;
 
+	/* Enable Voltage Alarms Power status reporting */
+	regmap_read(tcpci->regmap, TCPC_POWER_CTRL, &reg);
+	reg &= ~TCPC_POWER_CTRL_DIS_VOL_ALARM;
+	ret = regmap_write(tcpci->regmap, TCPC_POWER_CTRL, reg);
+
 	reg = TCPC_ALERT_TX_SUCCESS | TCPC_ALERT_TX_FAILED |
 		TCPC_ALERT_TX_DISCARDED | TCPC_ALERT_RX_STATUS |
 		TCPC_ALERT_RX_HARD_RST | TCPC_ALERT_CC_STATUS |
