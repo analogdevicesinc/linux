9c328c646fa358ec4f53fdff465536d007da9d59
Fugang Duan <fugang.duan@nxp.com>
2018-01-17 15:36:39 +0800

    MLK-17400-02 gpio: max732x: add device reset support
    Add device reset for max732x driver.
    
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>
    Acked-by: Gao Pan <pandy.gao@nxp.com>
    Signed-off-by: Arulpandiyan Vadivel <arulpandiyan_vadivel@mentor.com>

--- drivers/gpio/gpio-max732x.c
+++ drivers/gpio/gpio-max732x.c
@@ -18,6 +18,7 @@
 #include <linux/interrupt.h>
 #include <linux/i2c.h>
 #include <linux/platform_data/max732x.h>
+#include <linux/reset.h>
 
 /*
  * Each port of MAX732x (including MAX7319) falls into one of the
@@ -633,6 +634,10 @@ static int max732x_probe(struct i2c_client *client)
 	if (chip == NULL)
 		return -ENOMEM;
 	chip->client = client;
+
+	ret = device_reset(&client->dev);
+	if (ret == -EPROBE_DEFER)
+		return ret;
 
 	nr_port = max732x_setup_gpio(chip, id, pdata->gpio_base);
 	chip->gpio_chip.parent = &client->dev;
