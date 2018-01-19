e1564bbf2d185646535632436fce8fbf53ee7a47
David Heidelberger <david.heidelberger@ixit.cz>
2018-01-19 12:07:27 +0200

    uapi/if_ether.h: prevent redefinition of struct ethhdr
    Backported from:
      https://github.com/openwrt/openwrt/blob/9c2ac19b032c81a454f1efbcf5681b80cad2fa39/target/linux/generic/pending-4.9/272-uapi-if_ether.h-prevent-redefinition-of-struct-ethhd.patch
    
    Musl provides its own ethhdr struct definition. Add a guard to prevent
    its definition of the appropriate musl header has already been included.
    
    Signed-off-by: John Spencer <maillist-linux@barfooze.de>
    Tested-by: David Heidelberger <david.heidelberger@ixit.cz>
    Signed-off-by: Jonas Gorski <jogo@openwrt.org>

--- include/uapi/linux/libc-compat.h
+++ include/uapi/linux/libc-compat.h
@@ -90,6 +90,14 @@
 
 #endif /* _NET_IF_H */
 
+/* musl defines the ethhdr struct itself in its netinet/if_ether.h.
+ * Glibc just includes the kernel header and uses a different guard. */
+#if defined(_NETINET_IF_ETHER_H)
+#define __UAPI_DEF_ETHHDR		0
+#else
+#define __UAPI_DEF_ETHHDR		1
+#endif
+
 /* Coordinate with libc netinet/in.h header. */
 #if defined(_NETINET_IN_H)
 
@@ -197,6 +205,9 @@
 #ifndef __UAPI_DEF_IF_NET_DEVICE_FLAGS_LOWER_UP_DORMANT_ECHO
 #define __UAPI_DEF_IF_NET_DEVICE_FLAGS_LOWER_UP_DORMANT_ECHO 1
 #endif
+
+/* Definitions for if_ether.h */
+#define __UAPI_DEF_ETHHDR 		1
 
 /* Definitions for in.h */
 #ifndef __UAPI_DEF_IN_ADDR
