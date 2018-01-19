d0d23ff7825223993d4ddbb72da8a73ddeb316f5
Laurentiu Palcu <laurentiu.palcu@nxp.com>
2018-01-19 15:15:58 +0200

    MLK-17634-6: drm: imx: dcss: add P010 drm format
    This is 10-bit per channel YUV420 semi-planar.
    
    Signed-off-by: Laurentiu Palcu <laurentiu.palcu@nxp.com>

--- include/uapi/drm/drm_fourcc.h
+++ include/uapi/drm/drm_fourcc.h
@@ -140,6 +140,7 @@ extern "C" {
 #define DRM_FORMAT_NV61		fourcc_code('N', 'V', '6', '1') /* 2x1 subsampled Cb:Cr plane */
 #define DRM_FORMAT_NV24		fourcc_code('N', 'V', '2', '4') /* non-subsampled Cr:Cb plane */
 #define DRM_FORMAT_NV42		fourcc_code('N', 'V', '4', '2') /* non-subsampled Cb:Cr plane */
+#define DRM_FORMAT_P010		fourcc_code('P', '0', '1', '0') /* 2x2 subsampled Cr:Cb plane, 10-bit per channel */
 
 /*
  * 3 plane YCbCr
