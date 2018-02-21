ce5b28c80ccd9d0a17d0f00e2d54cea9b4f4c9d6
Michael Hennerich <michael.hennerich@analog.com>
2018-02-21 14:11:32 +0100

    iio: adc: talise: Reduce verboseness
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/adc/talise/talise_user.h
+++ drivers/iio/adc/talise/talise_user.h
@@ -88,8 +88,8 @@ extern "C" {
 /* 3 Bytes per SPI transaction * 341 transactions = ~1024 byte buffer size */
 /* Minimum MYK_SPIWRITEARRAY_BUFFERSIZE = 18 */
 
-#define TALISE_VERBOSE 1
-#define TALISE_LOGGING 0xF      /*LogLevel Set to All*/
+#define TALISE_VERBOSE 0
+#define TALISE_LOGGING 0x7      /*LogLevel Set to All*/
 #define TALISE_RESET_ON_ERR  1   /*API Reset on Severe Errors*/
 
 /*
