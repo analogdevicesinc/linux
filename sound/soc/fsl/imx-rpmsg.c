69fbd0770654d2642e180b1f6e8785caa88c8a50
Shengjiu Wang <shengjiu.wang@nxp.com>
2018-03-07 11:14:53 +0800

    MLK-17156-7: ASoC: imx-rpmsg: use rpmsg codec instead the dummy
    use the rpmsg_wm8960 codec instead of the dummy codec
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@nxp.com>

--- sound/soc/fsl/imx-rpmsg.c
+++ sound/soc/fsl/imx-rpmsg.c
@@ -58,8 +58,8 @@ static int imx_rpmsg_probe(struct platform_device *pdev)
 
 	data->dai[0].name = "rpmsg hifi";
 	data->dai[0].stream_name = "rpmsg hifi";
-	data->dai[0].codec_dai_name = "snd-soc-dummy-dai";
-	data->dai[0].codec_name = "snd-soc-dummy";
+	data->dai[0].codec_dai_name = "rpmsg-wm8960-hifi";
+	data->dai[0].codec_name = "rpmsg-audio-codec";
 	data->dai[0].cpu_dai_name = dev_name(&cpu_pdev->dev);
 	data->dai[0].platform_of_node = cpu_np;
 	data->dai[0].playback_only = false;
