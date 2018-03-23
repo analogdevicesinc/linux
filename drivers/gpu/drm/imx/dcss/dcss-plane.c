b262065d8b63edc00afab450d56ca8613b45de8d
Laurentiu Palcu <laurentiu.palcu@nxp.com>
2018-03-23 17:36:33 -0500

    MLK-17703-9: drm: imx: dcss: align input and output pipe gamut and nonlinearity
    For better results, output and input pipe gamut and nonlinearity should
    match.
    
    Signed-off-by: Laurentiu Palcu <laurentiu.palcu@nxp.com>

--- drivers/gpu/drm/imx/dcss/dcss-plane.c
+++ drivers/gpu/drm/imx/dcss/dcss-plane.c
@@ -463,11 +463,11 @@ static void dcss_plane_atomic_update(struct drm_plane *plane,
 
 	ipipe_cfg.pixel_format = pixel_format;
 
-	ipipe_cfg.nl = NL_REC2084;
-	ipipe_cfg.pr = PR_FULL;
-	ipipe_cfg.g = G_REC2020;
-
 	dcss_crtc_get_opipe_cfg(state->crtc, &opipe_cfg);
+
+	ipipe_cfg.nl = opipe_cfg.nl == NL_REC2084 ? NL_REC2084 : NL_REC709;
+	ipipe_cfg.pr = PR_FULL;
+	ipipe_cfg.g = opipe_cfg.g == G_REC2020 ? G_REC2020 : G_REC709;
 
 	dcss_hdr10_setup(dcss_plane->dcss, dcss_plane->ch_num,
 			 &ipipe_cfg, &opipe_cfg);
