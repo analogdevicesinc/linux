33df3945cdd9c88730bdcaa0d526e2e9f36f7bf4
Yuchou Gan <yuchou.gan@nxp.com>
2018-03-31 01:45:52 +0800

    MGS-3786 [#ccc] Cncrease the clock rate of GPU3D/GPU2D for 7ulp B0 board
    The gpu3d/2d clock rate for 7ulp B0 board is 400M, increase it
    
    Signed-off-by: yuchou gan <yuchou.gan@nxp.com>

--- drivers/clk/imx/clk-imx7ulp.c
+++ drivers/clk/imx/clk-imx7ulp.c
@@ -208,7 +208,7 @@ static void __init imx7ulp_clocks_init(struct device_node *scg_node)
         imx_clk_set_parent(clks[IMX7ULP_CLK_GPU2D], clks[IMX7ULP_CLK_APLL_PFD2]);
         imx_clk_set_parent(clks[IMX7ULP_CLK_GPU3D], clks[IMX7ULP_CLK_APLL_PFD2]);
 
-	imx_clk_set_rate(clks[IMX7ULP_CLK_APLL_PFD2], 350000000);
+	imx_clk_set_rate(clks[IMX7ULP_CLK_APLL_PFD2], 400000000);
 
 	/* setting the rate for emmc/sd usage */
 	imx_clk_set_rate(clks[IMX7ULP_CLK_APLL_PFD1], 352800000);
