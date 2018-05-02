a7623bd6efd2b07a4220d8761683a165ad8cdf3a
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-05-02 17:04:25 +0300

    arm,arm64,microblaze: copy ADI defconfigs from master
    Similar to device-trees, these are pulled directly from the master branch.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

---
+++ arch/microblaze/kernel/platform.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2008 Michal Simek <monstr@monstr.eu>
+ *
+ * based on virtex.c file
+ *
+ * Copyright 2007 Secret Lab Technologies Ltd.
+ *
+ * This file is licensed under the terms of the GNU General Public License
+ * version 2. This program is licensed "as is" without any warranty of any
+ * kind, whether express or implied.
+ */
+
+#include <linux/init.h>
+#include <linux/of_platform.h>
+#include <asm/prom.h>
+#include <asm/setup.h>
+
+#include <linux/clk-provider.h>
+
+static struct of_device_id xilinx_of_bus_ids[] __initdata = {
+	{ .compatible = "simple-bus", },
+	{ .compatible = "xlnx,compound", },
+	{}
+};
+
+static const __initconst struct of_device_id clk_match[] = {
+	{ .compatible = "fixed-clock", .data = of_fixed_clk_setup, },
+	{ /* sentinel */ }
+};
+
+
+static int __init microblaze_device_probe(void)
+{
+	of_platform_bus_probe(NULL, xilinx_of_bus_ids, NULL);
+	of_clk_init(clk_match);
+
+	return 0;
+}
+device_initcall(microblaze_device_probe);
