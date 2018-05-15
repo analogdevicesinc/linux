946d2a810bf21dcdee4b43eee6d718ef2c5219f7
Robin Gong <yibin.gong@nxp.com>
2018-05-16 00:42:01 +0800

    MLK-18315-2: spi: spi-imx: use PIO if length is smaller than wml
    Use PIO if transfer length is smaller than wml instead of DMA mode.
    That means some spi-nor driver such as m25p32 may setup one small size
    transfer to probe device id from external spi-nor devicee by PIO mode
    instead of DMA which can avoid checking sdma firmware ready during spi-nor
    driver probe.
    
    Signed-off-by: Robin Gong <yibin.gong@nxp.com>
    Acked-by: Fugang Duan <fugang.duan@nxp.com>

--- drivers/spi/spi-imx.c
+++ drivers/spi/spi-imx.c
@@ -233,7 +233,7 @@ static bool spi_imx_can_dma(struct spi_master *master, struct spi_device *spi,
 			break;
 	}
 
-	if (i == 0)
+	if (i == 0 || transfer->len < spi_imx->devtype_data->fifo_size / 2)
 		return false;
 
 	spi_imx->wml = i;
@@ -538,11 +538,12 @@ static int mx51_ecspi_config(struct spi_device *spi)
 	if (spi_imx->devtype_data->devtype == IMX6UL_ECSPI)
 		tx_wml = spi_imx->wml / 2;
 
-	writel(MX51_ECSPI_DMA_RX_WML(spi_imx->wml) |
-		MX51_ECSPI_DMA_TX_WML(tx_wml) |
-		MX51_ECSPI_DMA_RXT_WML(spi_imx->wml) |
-		MX51_ECSPI_DMA_TEDEN | MX51_ECSPI_DMA_RXDEN |
-		MX51_ECSPI_DMA_RXTDEN, spi_imx->base + MX51_ECSPI_DMA);
+	if (spi_imx->usedma)
+		writel(MX51_ECSPI_DMA_RX_WML(spi_imx->wml) |
+			MX51_ECSPI_DMA_TX_WML(tx_wml) |
+			MX51_ECSPI_DMA_RXT_WML(spi_imx->wml) |
+			MX51_ECSPI_DMA_TEDEN | MX51_ECSPI_DMA_RXDEN |
+			MX51_ECSPI_DMA_RXTDEN, spi_imx->base + MX51_ECSPI_DMA);
 
 	return 0;
 }
