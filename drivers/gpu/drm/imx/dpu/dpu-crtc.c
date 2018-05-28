729ea38167cca78cc2d073b07f0be9e0c3343bd3
Liu Ying <victor.liu@nxp.com>
2018-05-28 13:28:34 +0800

    MLK-19413-28 drm/imx: dpu: crtc: Export more dpu crtc information from dpu-crtc.h
    This patch moves several basic structures and helpers of DPU CRTC from
    the DPU CRTC driver to dpu-crtc.h so that more DPU CRTC information can
    be exported there.
    
    Signed-off-by: Liu Ying <victor.liu@nxp.com>

--- drivers/gpu/drm/imx/dpu/dpu-crtc.c
+++ drivers/gpu/drm/imx/dpu/dpu-crtc.c
@@ -26,52 +26,10 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <video/dpu.h>
+#include "dpu-crtc.h"
 #include "dpu-kms.h"
 #include "dpu-plane.h"
 #include "imx-drm.h"
-
-struct dpu_crtc {
-	struct device		*dev;
-	struct drm_crtc		base;
-	struct imx_drm_crtc	*imx_crtc;
-	struct dpu_constframe	*cf;
-	struct dpu_disengcfg	*dec;
-	struct dpu_extdst	*ed;
-	struct dpu_framegen	*fg;
-	struct dpu_tcon		*tcon;
-	struct dpu_constframe	*aux_cf;
-	struct dpu_disengcfg	*aux_dec;
-	struct dpu_extdst	*aux_ed;
-	struct dpu_framegen	*aux_fg;
-	struct dpu_tcon		*aux_tcon;
-	struct dpu_plane	**plane;
-	unsigned int		hw_plane_num;
-	unsigned int		stream_id;
-	unsigned int		crtc_grp_id;
-	int			vbl_irq;
-	int			safety_shdld_irq;
-	int			content_shdld_irq;
-	int			dec_shdld_irq;
-
-	struct completion	safety_shdld_done;
-	struct completion	content_shdld_done;
-	struct completion	dec_shdld_done;
-};
-
-struct dpu_crtc_state {
-	struct imx_crtc_state	imx_crtc_state;
-	struct dpu_plane_state	**dpu_plane_states;
-};
-
-static inline struct dpu_crtc_state *to_dpu_crtc_state(struct imx_crtc_state *s)
-{
-	return container_of(s, struct dpu_crtc_state, imx_crtc_state);
-}
-
-static inline struct dpu_crtc *to_dpu_crtc(struct drm_crtc *crtc)
-{
-	return container_of(crtc, struct dpu_crtc, base);
-}
 
 static inline struct dpu_plane_state **
 alloc_dpu_plane_states(struct dpu_crtc *dpu_crtc)
