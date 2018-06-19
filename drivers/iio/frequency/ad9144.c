32a758f9359df47a451dde569a2e7d36b458fbc9
Lars-Peter Clausen <lars@metafoo.de>
2018-06-19 13:36:12 +0200

    iio: ad9144: Add support for both TXEN pins
    The ad9144 and its derivatives have two TXEN pins. Currently only one of
    them is requested. Make sure that both are requested otherwise the second
    TX path might stay disabled.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/iio/frequency/ad9144.c
+++ drivers/iio/frequency/ad9144.c
@@ -548,6 +548,7 @@ static int ad9144_probe(struct spi_device *spi)
 	struct ad9144_platform_data *pdata;
 	struct ad9144_state *st;
 	unsigned long lane_rate_kHz;
+	unsigned int i;
 	unsigned id;
 	int ret;
 
@@ -586,9 +587,12 @@ static int ad9144_probe(struct spi_device *spi)
 	if (IS_ERR(conv->reset_gpio))
 		return PTR_ERR(conv->reset_gpio);
 
-	conv->txen_gpio = devm_gpiod_get_optional(&spi->dev, "txen", GPIOD_OUT_HIGH);
-	if (IS_ERR(conv->txen_gpio))
-		return PTR_ERR(conv->txen_gpio);
+	for (i = 0; i < 2; i++) {
+		conv->txen_gpio[i] = devm_gpiod_get_index_optional(&spi->dev,
+			"txen", i, GPIOD_OUT_HIGH);
+		if (IS_ERR(conv->txen_gpio[i]))
+			return PTR_ERR(conv->txen_gpio[i]);
+	}
 
 	st->map = devm_regmap_init_spi(spi, &ad9144_regmap_config);
 	if (IS_ERR(st->map))
