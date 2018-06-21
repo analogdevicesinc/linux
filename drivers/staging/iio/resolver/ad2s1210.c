41d4b8ad6f6291ea5e08d58f208cdf01b9780f7c
Dragos Bogdan <dragos.bogdan@analog.com>
2018-06-21 19:23:53 +0300

    staging: iio: ad2s1210: Fix clkin initial value
    CLKIN is independent of SPI SCLK. Its default value is
    AD2S1210_DEF_CLKIN.
    
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- drivers/staging/iio/resolver/ad2s1210.c
+++ drivers/staging/iio/resolver/ad2s1210.c
@@ -741,6 +741,7 @@ static int ad2s1210_probe(struct spi_device *spi)
 	st->hysteresis = true;
 	st->mode = MOD_CONFIG;
 	st->resolution = 12;
+	st->fclkin = AD2S1210_DEF_CLKIN;
 	st->fexcit = AD2S1210_DEF_EXCIT;
 
 	ret = ad2s1210_setup_gpios(st);
@@ -762,7 +763,6 @@ static int ad2s1210_probe(struct spi_device *spi)
 	if (ret)
 		return ret;
 
-	st->fclkin = spi->max_speed_hz;
 	spi_setup(spi);
 	ad2s1210_initial(st);
 
