85bc0f08ea6d6c44204623986e848b905c19df2e
Russell King <rmk+kernel@armlinux.org.uk>
2018-07-14 16:01:06 +0100

    ASoC: hdmi-codec: fix routing
    Commit 943fa0228252 ("ASoC: hdmi-codec: Use different name for playback
    streams") broke hdmi-codec's routing between it's output "TX" widget
    and the S/PDIF or I2S streams by renaming the streams.
    
    Whether an error occurs or not is dependent on whether there is another
    widget called "Playback" registered by some other component - if there
    is, that widget will be (incorrectly) bound to the HDMI codec's "TX"
    output widget.  If we end up connecting "TX" incorrectly, it can result
    in components not being started, causing no audio output.
    
    Since the I2S and S/PDIF streams now have different names, we can't
    use a static route at component level to describe the relationship, so
    arrange to dynamically create the route when the DAI driver is probed.
    
    Fixes: 943fa0228252 ("ASoC: hdmi-codec: Use different name for playback streams")
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit d30e23d69981a4b665f5ce8711335df986576389)

--- sound/soc/codecs/hdmi-codec.c
+++ sound/soc/codecs/hdmi-codec.c
@@ -292,11 +292,6 @@ static const struct snd_soc_dapm_widget hdmi_widgets[] = {
 	SND_SOC_DAPM_OUTPUT("RX"),
 };
 
-static const struct snd_soc_dapm_route hdmi_routes[] = {
-	{ "TX", NULL, "Playback" },
-	{ "Capture", NULL, "RX", },
-};
-
 enum {
 	DAI_ID_I2S = 0,
 	DAI_ID_SPDIF,
@@ -695,9 +690,29 @@ static int hdmi_codec_pcm_new(struct snd_soc_pcm_runtime *rtd,
 	return snd_ctl_add(rtd->card->snd_card, kctl);
 }
 
+static int hdmi_dai_probe(struct snd_soc_dai *dai)
+{
+	struct snd_soc_dapm_context *dapm;
+	struct snd_soc_dapm_route route[] = {
+		{
+			.sink = "TX",
+			.source = dai->driver->playback.stream_name,
+		},
+		{
+			.sink = "Capture",
+			.source = "RX",
+		},
+	};
+
+	dapm = snd_soc_component_get_dapm(dai->component);
+
+	return snd_soc_dapm_add_routes(dapm, route, ARRAY_SIZE(route));
+}
+
 static const struct snd_soc_dai_driver hdmi_i2s_dai = {
 	.name = "i2s-hifi",
 	.id = DAI_ID_I2S,
+	.probe = hdmi_dai_probe,
 	.playback = {
 		.stream_name = "I2S Playback",
 		.channels_min = 2,
@@ -721,6 +736,7 @@ static const struct snd_soc_dai_driver hdmi_i2s_dai = {
 static const struct snd_soc_dai_driver hdmi_spdif_dai = {
 	.name = "spdif-hifi",
 	.id = DAI_ID_SPDIF,
+	.probe = hdmi_dai_probe,
 	.playback = {
 		.stream_name = "SPDIF Playback",
 		.channels_min = 2,
@@ -755,8 +771,6 @@ static const struct snd_soc_codec_driver hdmi_codec = {
 	.component_driver = {
 		.dapm_widgets		= hdmi_widgets,
 		.num_dapm_widgets	= ARRAY_SIZE(hdmi_widgets),
-		.dapm_routes		= hdmi_routes,
-		.num_dapm_routes	= ARRAY_SIZE(hdmi_routes),
 		.of_xlate_dai_id	= hdmi_of_xlate_dai_id,
 	},
 };
