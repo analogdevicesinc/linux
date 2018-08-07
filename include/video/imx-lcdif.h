5f975caae0c097e2eea189802cecd0d57217d485
Fancy Fang <chen.fang@nxp.com>
2018-08-07 18:04:54 +0800

    MLK-19152-1 gpu: imx: lcdif: realize fb horizontal crop via Pigeon Mode
    According to the LCDIF specification, the Legacy Mode does not
    support cropping function in the horizontal direction, so add
    Pigeon Mode which can support this kind of function. And when
    enable this mode, the legacy horizontal timings configuration
    should use stride value but not the active width, and related
    pigeon configuration should use the active width but not the
    stride value.
    
    Signed-off-by: Fancy Fang <chen.fang@nxp.com>
    (cherry picked from commit e6da9542693dd585972897f62748a101f5726a74)

--- include/video/imx-lcdif.h
+++ include/video/imx-lcdif.h
@@ -32,6 +32,8 @@ int  lcdif_get_bus_fmt_from_pix_fmt(struct lcdif_soc *lcdif,
 int  lcdif_set_pix_fmt(struct lcdif_soc *lcdif, u32 format);
 void lcdif_set_fb_addr(struct lcdif_soc *lcdif, int id, u32 addr);
 void lcdif_set_mode(struct lcdif_soc *lcdif, struct videomode *vmode);
+void lcdif_set_fb_hcrop(struct lcdif_soc *lcdif, u32 src_w,
+			u32 fb_w, bool crop);
 void lcdif_enable_controller(struct lcdif_soc *lcdif);
 void lcdif_disable_controller(struct lcdif_soc *lcdif);
 void lcdif_dump_registers(struct lcdif_soc *lcdif);
