c2b841df669144040f85f697c55c1c41c3041dd4
Fancy Fang <chen.fang@nxp.com>
2018-08-12 08:58:43 +0800

    MLK-19158-2 drm/imx: lcdif: improve output bus format config
    According to LCDIF specification, the input pixel data
    width and the output pixel data width can be different,
    and this conversion is done by LCDIF automatically. So
    config the output data width according to the requested
    bus format from the encoder, instead to be same with the
    input pixel data width.
    
    Signed-off-by: Fancy Fang <chen.fang@nxp.com>
    (cherry picked from commit bfd27f6d71d86a7f2fc8314f082565db3682b925)

--- include/video/imx-lcdif.h
+++ include/video/imx-lcdif.h
@@ -30,6 +30,7 @@ void lcdif_vblank_irq_clear(struct lcdif_soc *lcdif);
 int  lcdif_get_bus_fmt_from_pix_fmt(struct lcdif_soc *lcdif,
 				    uint32_t format);
 int  lcdif_set_pix_fmt(struct lcdif_soc *lcdif, u32 format);
+void lcdif_set_bus_fmt(struct lcdif_soc *lcdif, u32 bus_format);
 void lcdif_set_fb_addr(struct lcdif_soc *lcdif, int id, u32 addr);
 void lcdif_set_mode(struct lcdif_soc *lcdif, struct videomode *vmode);
 void lcdif_set_fb_hcrop(struct lcdif_soc *lcdif, u32 src_w,
