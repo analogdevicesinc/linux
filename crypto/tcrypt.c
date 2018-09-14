f389c802aa7bc3dc6cc6be2a486fe38807e9c95c
Franck LENORMAND <franck.lenormand@nxp.com>
2018-09-14 15:13:22 +0200

    MLK-19365: crypto: tcrypt: Remove testing of hmac(crc32)
    The kernel implementation of crc32 (crc32_generic.c)
    accepts a key to set a seed. It is incompatible with the
    kernel implementation of the crypto template hmac which
    does not support keyed algorithms.
    
    So it is not possible to load the algorithm hmac(crc32)
    so remove it from tcrypt.
    
    Signed-off-by: Franck LENORMAND <franck.lenormand@nxp.com>

--- crypto/tcrypt.c
+++ crypto/tcrypt.c
@@ -1316,10 +1316,6 @@ static int do_test(const char *alg, u32 type, u32 mask, int m)
 		ret += tcrypt_test("vmac(aes)");
 		break;
 
-	case 110:
-		ret += tcrypt_test("hmac(crc32)");
-		break;
-
 	case 111:
 		ret += tcrypt_test("hmac(sha3-224)");
 		break;
