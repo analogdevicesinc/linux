6e5d9784281b6c58c7cf7ac31e43d252c19b0847
Robin Gong <yibin.gong@nxp.com>
2018-09-20 18:25:32 +0800

    MLK-19734-1: sdma: add multi-fifo scripts for i.mx6
    Add multi-fifo script for i.mx6 instead of dual fifo script, because
    there is one hardware issue inside SSI ip and switch between dual fifo
    and single fifo could workaroud it, so add multi-fifo scripts.
    
    Signed-off-by: Robin Gong <yibin.gong@nxp.com>
    (cherry picked from commit 85bb6546ff8cb1fc323e568d689a1e078eaca8fa)

--- drivers/dma/imx-sdma.c
+++ drivers/dma/imx-sdma.c
@@ -1941,7 +1941,7 @@ static void sdma_issue_pending(struct dma_chan *chan)
 
 #define SDMA_SCRIPT_ADDRS_ARRAY_SIZE_V1	34
 #define SDMA_SCRIPT_ADDRS_ARRAY_SIZE_V2	38
-#define SDMA_SCRIPT_ADDRS_ARRAY_SIZE_V3	41
+#define SDMA_SCRIPT_ADDRS_ARRAY_SIZE_V3	43
 #define SDMA_SCRIPT_ADDRS_ARRAY_SIZE_V4	44
 
 static void sdma_add_scripts(struct sdma_engine *sdma,
--- include/linux/platform_data/dma-imx-sdma.h
+++ include/linux/platform_data/dma-imx-sdma.h
@@ -52,10 +52,10 @@ struct sdma_script_start_addrs {
 	s32 zcanfd_2_mcu_addr;
 	s32 zqspi_2_mcu_addr;
 	s32 mcu_2_ecspi_addr;
-	/* End of v3 array */
-	s32 mcu_2_zqspi_addr;
 	s32 mcu_2_sai_addr;
 	s32 sai_2_mcu_addr;
+	/* End of v3 array */
+	s32 mcu_2_zqspi_addr;
 	/* End of v4 array */
 };
 
