06ddc4d6c16c0df94bec6f03d2f675eef4018a89
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-09-28 12:11:38 +0300

    ci/travis: move yaml scripts into shell file
    This offers more flexibility for later, when we want to do different
    build-types based on conditionals.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

---
+++ ci/travis/run-build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -xe
+
+build_default() {
+	if [ -n "$TRAVIS_BRANCH" ]; then
+		git fetch origin +refs/heads/${TRAVIS_BRANCH}:${TRAVIS_BRANCH}
+	fi
+
+	COMMIT_RANGE=$([ "$TRAVIS_PULL_REQUEST" == "false" ] &&  echo HEAD || echo ${TRAVIS_BRANCH}..)
+
+	make ${DEFCONFIG_NAME}
+	make -j`getconf _NPROCESSORS_ONLN` $IMAGE UIMAGE_LOADADDR=0x8000
+
+	for file in $DTS_FILES; do
+		make ${DTS_PREFIX}`basename $file | sed  -e 's\dts\dtb\g'` || exit 1
+	done
+
+	scripts/checkpatch.pl --git ${COMMIT_RANGE} \
+		--ignore FILE_PATH_CHANGES \
+		--ignore LONG_LINE \
+		--ignore LONG_LINE_STRING \
+		--ignore LONG_LINE_COMMENT
+}
+
+BUILD_TYPE=default
+
+build_${BUILD_TYPE}
