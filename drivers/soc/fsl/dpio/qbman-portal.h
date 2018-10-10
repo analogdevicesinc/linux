ed7670f2f55cbcd134e55628d231e469f302e423
Horia Geantă <horia.geanta@nxp.com>
2018-10-10 15:59:55 +0300

    soc: fsl: dpio: add support for opr
    Order preservation is a feature that will be supported
    in dpni, dpseci and dpci devices.
    This is a preliminary patch for the changes to be
    introduced in the corresponding drivers.
    
    Signed-off-by: Radu Alexe <radu.alexe@nxp.com>
    Signed-off-by: Horia Geantă <horia.geanta@nxp.com>

--- drivers/soc/fsl/dpio/qbman-portal.h
+++ drivers/soc/fsl/dpio/qbman-portal.h
@@ -230,7 +230,6 @@ void qbman_eq_desc_set_fq(struct qbman_eq_desc *d, u32 fqid);
 void qbman_eq_desc_set_qd(struct qbman_eq_desc *d, u32 qdid,
 			  u32 qd_bin, u32 qd_prio);
 
-
 int qbman_orp_drop(struct qbman_swp *s, u16 orpid, u16 seqnum);
 
 void qbman_release_desc_clear(struct qbman_release_desc *d);
