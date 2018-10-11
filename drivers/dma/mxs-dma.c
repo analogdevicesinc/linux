faf31389b85598daed01920fcfb344c4cf4ac3a1
Han Xu <han.xu@nxp.com>
2018-10-11 15:32:04 -0500

    MLK-19897: dma: mxs-dma: update mxs-dma filter function to filter out the unrelated dma channels
    update mxs-dma filter function to firstly filter the dma channels only for mxs-dma, rather than checking unrelated dma chans in following code.
    
    Signed-off-by: Han Xu <han.xu@nxp.com>

--- drivers/dma/mxs-dma.c
+++ drivers/dma/mxs-dma.c
@@ -779,6 +779,9 @@ static bool mxs_dma_filter_fn(struct dma_chan *chan, void *fn_param)
 	struct mxs_dma_engine *mxs_dma = mxs_chan->mxs_dma;
 	int chan_irq;
 
+	if (strcmp(chan->device->dev->driver->name, "mxs-dma"))
+		return false;
+
 	if (!mxs_dma)
 		return false;
 
