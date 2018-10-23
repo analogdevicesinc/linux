33037f50238c4fe170a57008809026606627e7fa
Lars-Peter Clausen <lars@metafoo.de>
2018-10-23 18:24:47 +0200

    usb: dwc3: gadget: Handle dequeuing of non queued URB gracefully
    Trying to dequeue and URB that is currently not queued should be a no-op
    and be handled gracefully.
    
    Use the list field of the URB to indicate whether it is queued or not by
    setting it to the empty list when it is not queued.
    
    Handling this gracefully allows for race condition free synchronization
    between the complete callback being called to to a completed transfer and
    trying to call usb_ep_dequeue() at the same time.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -174,7 +174,7 @@ static void dwc3_gadget_del_and_unmap_request(struct dwc3_ep *dep,
 {
 	struct dwc3			*dwc = dep->dwc;
 
-	list_del(&req->list);
+	list_del_init(&req->list);
 	req->remaining = 0;
 	req->needs_extra_trb = false;
 
@@ -861,6 +861,7 @@ static struct usb_request *dwc3_gadget_ep_alloc_request(struct usb_ep *ep,
 	req->epnum	= dep->number;
 	req->dep	= dep;
 	req->status	= DWC3_REQUEST_STATUS_UNKNOWN;
+	INIT_LIST_HEAD(&req->list);
 
 	trace_dwc3_alloc_request(req);
 
@@ -1598,6 +1599,10 @@ static int dwc3_gadget_ep_dequeue(struct usb_ep *ep,
 
 	if (dep->stream_capable && timer_pending(&req->stream_timeout_timer))
 		del_timer(&req->stream_timeout_timer);
+
+	/* Not queued, nothing to do */
+	if (list_empty(&req->list))
+		goto out0;
 
 	list_for_each_entry(r, &dep->pending_list, list) {
 		if (r == req)
