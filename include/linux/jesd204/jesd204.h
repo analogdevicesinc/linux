a45b6ca9345494b3aad57fce99248a0fd5352763
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-11-27 15:29:00 +0200

    jesd204: add basic registration code/logic
    This logic handles basic registration of JESD204 devices with the JESD204
    framework. Currently the logic doesn't do much, as more stuff will be added
    along the way. This change roughly defines the types that will be used by
    to create/register a JESD204 device, and how to release/free it.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    (cherry picked from commit 8c48bd13940c7b0901ef42013254727560ca3911)

--- include/linux/jesd204/jesd204.h
+++ include/linux/jesd204/jesd204.h
@@ -9,11 +9,26 @@
 
 struct jesd204_dev;
 
+/**
+ * struct jesd204_dev_data - JESD204 device initialization data
+ */
+struct jesd204_dev_data {
+};
+
 #if IS_ENABLED(CONFIG_JESD204)
+
+struct jesd204_dev *jesd204_dev_register(struct device *dev,
+					 const struct jesd204_dev_data *init);
 
 void jesd204_dev_unregister(struct jesd204_dev *jdev);
 
 #else /* !IS_ENABLED(CONFIG_JESD204) */
+
+static inline struct jesd204_dev *jesd204_dev_register(
+		struct device *dev, const struct jesd204_dev_data *init)
+{
+	return NULL;
+}
 
 static inline void jesd204_dev_unregister(struct jesd204_dev *jdev) {}
 
