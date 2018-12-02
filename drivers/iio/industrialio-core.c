f91ea479cebcba16d415438fc16883677c345345
Andi Shyti <andi@etezian.org>
2018-12-02 20:30:31 +0200

    iio: core: check 'info' value before registering the device
    When the 'info' structure inside indio_dev is left uninitialized,
    a segmentation fault occurs.
    
    Check the 'info' value before using it and if it is equal to
    NULL, return with -EINVAL.
    
    Signed-off-by: Andi Shyti <andi@etezian.org>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/industrialio-core.c
+++ drivers/iio/industrialio-core.c
@@ -1780,6 +1780,9 @@ int __iio_device_register(struct iio_dev *indio_dev, struct module *this_mod)
 	if (ret < 0)
 		return ret;
 
+	if (!indio_dev->info)
+		return -EINVAL;
+
 	/* configure elements for the chrdev */
 	indio_dev->dev.devt = MKDEV(MAJOR(iio_devt), indio_dev->id);
 
