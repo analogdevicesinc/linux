abce893c13c0e5507b11565336ed005178061ff1
Michael Hennerich <michael.hennerich@analog.com>
2018-12-11 16:24:19 +0100

    iio: frequency: ad9172: Switch to API driver - WORK IN PROGRESS
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/frequency/ad9172.c
+++ drivers/iio/frequency/ad9172.c
@@ -1,7 +1,9 @@
 /*
- * Licensed under the GPL-2.
+ * AD9172 SPI DAC driver for AXI DDS PCORE/COREFPGA Module
  *
- * NOT A DRIVER. DO NOT USE.
+ * Copyright 2018 Analog Devices Inc.
+ *
+ * Licensed under the GPL-2.
  */
 
 #include <linux/module.h>
@@ -17,9 +19,14 @@
 #include <linux/gpio/consumer.h>
 #include <linux/regmap.h>
 
+#include <linux/clk/clkscale.h>
+
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
 #include "cf_axi_dds.h"
+
+#include "ad917x/AD917x.h"
+
 
 enum chip_id {
 	CHIPID_AD9172 = 0x72,
@@ -29,7 +36,11 @@ struct ad9172_state {
 	struct cf_axi_converter conv;
 	enum chip_id id;
 	struct regmap *map;
-	unsigned int interpolation;
+	ad917x_handle_t dac_h;
+	bool complex_mode;
+	bool iq_swap;
+	unsigned interpolation;
+
 };
 
 static const char * const clk_names[] = {
@@ -56,31 +67,226 @@ static int ad9172_write(struct spi_device *spi, unsigned reg, unsigned val)
 	return regmap_write(st->map, reg, val);
 }
 
-static int ad9172_get_clks(struct cf_axi_converter *conv)
+static int ad9172_get_temperature_code(struct cf_axi_converter *conv)
 {
-	struct clk *clk;
-	int i, ret;
-
-	for (i = 0; i < 3; i++) {
-		clk = devm_clk_get(&conv->spi->dev, clk_names[i]);
-		if (IS_ERR(clk))
-			return PTR_ERR(clk);
-
-		if (i > 0) {
-			ret = clk_prepare_enable(clk);
-			if (ret < 0)
-				return ret;
-		}
-		conv->clk[i] = clk;
-	}
 	return 0;
 }
 
 static unsigned long long ad9172_get_data_clk(struct cf_axi_converter *conv)
 {
 	struct ad9172_state *st = container_of(conv, struct ad9172_state, conv);
+	/* FIXME: Temp hack */
 
-	return clk_get_rate(conv->clk[CLK_DAC]) / st->interpolation;
+	return div_u64(2949120000ULL, st->interpolation);
+
+	//return div_u64(clk_get_rate_scaled(conv->clk[CLK_DAC], &conv->clkscale[CLK_DAC]), st->interpolation);
+}
+
+static int ad9172_setup(struct ad9172_state *st)
+{
+	struct regmap *map = st->map;
+	struct device *dev = regmap_get_device(map);
+	uint8_t revision[3] = {0,0,0};
+	adi_chip_id_t dac_chip_id;
+	jesd_param_t appJesdConfig;
+	uint8_t pll_lock_status = 0, dll_lock_stat = 0;
+	int ret;
+	u64 dac_rate_Hz;
+	unsigned long lane_rate_kHz;
+	ad917x_jesd_link_stat_t link_status;
+	ad917x_handle_t *ad917x_h = &st->dac_h;
+
+	/*Initialise DAC Module*/
+	ret = ad917x_init(ad917x_h);
+	if (ret != 0) {
+		dev_err(dev, "ad917x_init failed (%d)\n", ret);
+		return ret;
+	}
+
+	ret = ad917x_reset(ad917x_h, 0);
+	if (ret != 0) {
+		dev_err(dev, "ad917x_reset failed (%d)\n", ret);
+		return ret;
+	}
+
+	ret = ad917x_get_chip_id(ad917x_h, &dac_chip_id);
+	if (ret != 0) {
+		dev_err(dev, "ad917x_get_chip_id failed (%d)\n", ret);
+		return ret;
+	}
+
+	ret = ad917x_get_revision(ad917x_h, &revision[0], &revision[1], &revision[2]);
+	if (ret != 0)
+		return ret;
+
+	dev_info(dev, "AD916x DAC Chip ID: %d\n", dac_chip_id.chip_type);
+	dev_info(dev, "AD916x DAC Product ID: %x\n", dac_chip_id.prod_id);
+	dev_info(dev, "AD916x DAC Product Grade: %d\n", dac_chip_id.prod_grade);
+	dev_info(dev, "AD916x DAC Product Revision: %d\n", dac_chip_id.dev_revision);
+	dev_info(dev, "AD916x Revision: %d.%d.%d\n", revision[0], revision[1], revision[2]);
+
+	dac_rate_Hz = clk_get_rate(st->conv.clk[CLK_DAC]);
+
+	/* FIXME: The clock framework overflows by such numbers... */
+	dac_rate_Hz = 2949120000;
+
+	dev_info(dev, "dac_rate_Hz %llu\n",dac_rate_Hz);
+
+	ret = ad917x_set_dac_clk(ad917x_h, dac_rate_Hz, 0, dac_rate_Hz);
+	if (ret != 0) {
+		dev_err(dev, "ad917x_set_dac_clk failed (%d)\n", ret);
+		return ret;
+	}
+
+// 	ret = ad917x_set_dac_clk(ad917x_h, dac_rate_Hz * 16, 1, dac_rate_Hz);
+// 	if (ret != 0) {
+// 		dev_err(dev, "ad917x_set_dac_clk failed (%d)\n", ret);
+// 		return ret;
+// 	}
+
+	ret = ad917x_set_dac_clk_freq(ad917x_h, dac_rate_Hz);
+	if (ret != 0) {
+		dev_err(dev, "ad917x_set_dac_clk_freq failed (%d)\n", ret);
+		return ret;
+	}
+
+	/* DEBUG: route DAC clock to output, so we can meassure it */
+	ret = ad917x_set_clkout_config(ad917x_h, 1);
+	if (ret != 0) {
+		dev_err(dev, "ad917x_set_clkout_config failed (%d)\n", ret);
+		return ret;
+	}
+
+	ret = ad917x_get_dac_clk_status(ad917x_h,
+				  &pll_lock_status, &dll_lock_stat);
+	if (ret != 0) {
+		dev_err(dev, "ad917x_get_dac_clk_status failed (%d)\n", ret);
+		return ret;
+	}
+
+	dev_info(dev, "PLL lock status %x,  DLL lock status: %x\n",
+		 pll_lock_status, dll_lock_stat);
+
+
+	st->interpolation = 1;
+	//st->iq_swap = true;
+	st->conv.id = ID_AD9172;
+
+
+	ret = ad917x_jesd_config_datapath(ad917x_h, 0, 10, 1, st->interpolation);
+	if (ret != 0) {
+		dev_err(dev, "ad917x_jesd_config_datapath failed (%d)\n", ret);
+		return ret;
+	}
+	ret = ad917x_jesd_get_cfg_param(ad917x_h, &appJesdConfig);
+	if (ret != 0) {
+		dev_err(dev, "ad917x_jesd_get_cfg_param failed (%d)\n", ret);
+		return ret;
+	}
+
+	ret = ad917x_jesd_set_scrambler_enable(ad917x_h, 1);
+	if (ret != 0) {
+		dev_err(dev, "ad917x_jesd_set_scrambler_enable failed (%d)\n", ret);
+		return ret;
+	}
+
+	ret = ad917x_jesd_enable_datapath(ad917x_h, 0xFF, 0x1, 0x1);
+	if (ret != 0) {
+		dev_err(dev, "ad917x_jesd_enable_datapath failed (%d)\n", ret);
+		return ret;
+	}
+
+	ret = ad917x_jesd_set_syncoutb_enable(ad917x_h, SYNCOUTB_0, 1);
+	if (ret != 0) {
+		dev_err(dev, "ad917x_jesd_set_syncoutb_enable failed (%d)\n", ret);
+		return ret;
+	}
+
+	msleep(100);
+
+	ret = ad917x_jesd_get_pll_status(ad917x_h, &pll_lock_status);
+	if (ret != 0) {
+		dev_err(dev, "ad917x_jesd_get_pll_status failed (%d)\n", ret);
+		return ret;
+	}
+
+	dev_info(dev, "Serdes PLL %s (stat: %x)\n",
+		 ((pll_lock_status & 0x1) == 0x1) ?
+		 "Locked" : "Unlocked",  pll_lock_status);
+
+
+	lane_rate_kHz = div_u64(dac_rate_Hz * 20 * appJesdConfig.jesd_M,
+				appJesdConfig.jesd_L * st->interpolation * 1000);
+
+	ret = clk_set_rate(st->conv.clk[CLK_DATA], lane_rate_kHz);
+	if (ret < 0) {
+		dev_err(dev, "Failed to set lane rate to %lu kHz: %d\n",
+			lane_rate_kHz, ret);
+		return ret;
+	}
+
+	ret = clk_prepare_enable(st->conv.clk[CLK_DATA]);
+	if (ret) {
+		dev_err(dev, "Failed to enable JESD204 link: %d\n", ret);
+		return ret;
+	}
+
+	ad917x_jesd_set_sysref_enable(ad917x_h, 0); /* subclass 0 */
+
+	/*Enable Link*/
+	ret = ad917x_jesd_enable_link(ad917x_h, JESD_LINK_ALL, 0x1);
+	if (ret != 0) {
+		dev_err(dev, "DAC:MODE:JESD: ERROR : Enable Link failed\n");
+		return -EIO;
+	}
+
+	msleep(100);
+
+	ret = ad917x_jesd_get_link_status(ad917x_h, JESD_LINK_0, &link_status);
+	if (ret != 0) {
+		dev_err(dev, "DAC:MODE:JESD: ERROR : Get Link status failed \r\n");
+		return -EIO;
+	}
+
+	dev_info(dev, "code_grp_sync: %x \n", link_status.code_grp_sync_stat);
+	dev_info(dev, "frame_sync_stat: %x \n", link_status.frame_sync_stat);
+	dev_info(dev, "good_checksum_stat: %x \n", link_status.good_checksum_stat);
+	dev_info(dev, "init_lane_sync_stat: %x \n", link_status.init_lane_sync_stat);
+	dev_info(dev, "%d lanes @ %lu kBps\n", appJesdConfig.jesd_L, lane_rate_kHz);
+
+	regmap_write(st->map, 0x008, 0xc0);
+	regmap_write(st->map, 0x596, 0x1c);
+
+	return 0;
+}
+
+static int ad9172_get_clks(struct cf_axi_converter *conv)
+{
+	struct clk *clk;
+	int i, ret;
+
+	for (i = 0; i < ARRAY_SIZE(clk_names); i++) {
+		clk = devm_clk_get(&conv->spi->dev, clk_names[i]);
+		if (IS_ERR(clk) && PTR_ERR(clk) != -ENOENT)
+			return PTR_ERR(clk);
+
+		if (PTR_ERR(clk) == -ENOENT) {
+			/* sysref might be optional */
+			conv->clk[i] = NULL;
+			continue;
+		}
+
+		if (i > CLK_DATA) {
+			ret = clk_prepare_enable(clk);
+			if (ret < 0)
+				return ret;
+		}
+
+		of_clk_get_scale(conv->spi->dev.of_node, clk_names[i], &conv->clkscale[i]);
+		conv->clk[i] = clk;
+	}
+
+	return 0;
 }
 
 static int ad9172_read_raw(struct iio_dev *indio_dev,
@@ -88,17 +294,63 @@ static int ad9172_read_raw(struct iio_dev *indio_dev,
 			   int *val, int *val2, long m)
 {
 	struct cf_axi_converter *conv = iio_device_get_drvdata(indio_dev);
+	unsigned tmp;
 
 	switch (m) {
 	case IIO_CHAN_INFO_SAMP_FREQ:
+
 		*val = ad9172_get_data_clk(conv);
+		return IIO_VAL_INT;
+	case IIO_CHAN_INFO_CALIBBIAS:
+		*val = conv->temp_calib_code;
+		return IIO_VAL_INT;
+	case IIO_CHAN_INFO_PROCESSED:
+		if (!conv->temp_calib_code)
+			return -EINVAL;
+
+		tmp = ad9172_get_temperature_code(conv);
+
+		*val = ((tmp - conv->temp_calib_code) * 77
+			+ conv->temp_calib * 10 + 10000) / 10;
+
 		return IIO_VAL_INT;
 	}
 	return -EINVAL;
 }
 
+static int ad9172_write_raw(struct iio_dev *indio_dev,
+			    struct iio_chan_spec const *chan,
+			    int val, int val2, long mask)
+{
+	struct cf_axi_converter *conv = iio_device_get_drvdata(indio_dev);
+
+	switch (mask) {
+	case IIO_CHAN_INFO_CALIBBIAS:
+		conv->temp_calib_code = val;
+		break;
+	case IIO_CHAN_INFO_PROCESSED:
+		/*
+		 * Writing in_temp0_input with the device temperature in milli
+		 * degrees Celsius triggers the calibration.
+		 */
+		conv->temp_calib_code = ad9172_get_temperature_code(conv);
+		conv->temp_calib = val;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
 static int ad9172_prepare(struct cf_axi_converter *conv)
 {
+//	struct cf_axi_dds_state *st = iio_priv(conv->indio_dev);
+//	struct ad9172_state *ad9172 = container_of(conv, struct ad9172_state, conv);
+
+// 	/* FIXME This needs documenation */
+// 	dds_write(st, 0x428, (ad9172->complex_mode ? 0x1 : 0x0) |
+// 		  (ad9172->iq_swap ? 0x2 : 0x0 ));
 	return 0;
 }
 
@@ -110,32 +362,123 @@ static const struct regmap_config ad9172_regmap_config = {
 	.cache_type = REGCACHE_NONE,
 };
 
-#define REG_SPI_INTFCONFA                        0x000 /* Interface configuration A */
-#define REG_SPI_INTFCONFB                        0x001 /* Interface configuration B */
-#define REG_SPI_DEVCONF                          0x002 /* Device Configuration */
-#define REG_SPI_PRODIDL                          0x004 /* Product Identification Low Byte */
-#define REG_SPI_PRODIDH                          0x005 /* Product Identification High Byte */
-#define REG_SPI_CHIPGRADE                        0x006 /* Chip Grade */
-#define REG_SPI_PAGEINDX                         0x008 /* Page Pointer or Device Index */
-#define REG_SPI_DEVINDX2                         0x009 /* Secondary Device Index */
-#define REG_SPI_SCRATCHPAD                       0x00A /* Scratch Pad */
+static int delay_us(void *user_data, unsigned int us)
+{
+	usleep_range(us, (us * 110) / 100);
+	return 0;
+}
+
+static int ad9172_spi_xfer(void *user_data, uint8_t *wbuf, uint8_t *rbuf, int len)
+{
+	struct spi_device *spi = user_data;
+
+	struct spi_transfer t = {
+		.tx_buf = wbuf,
+		.rx_buf = rbuf,
+		.len = len,
+	};
+
+	return spi_sync_transfer(spi, &t, 1);
+}
+
+static ssize_t ad9172_attr_store(struct device *dev,
+					  struct device_attribute *attr,
+					  const char *buf, size_t len)
+{
+	struct iio_dev *indio_dev = dev_to_iio_dev(dev);
+	struct iio_dev_attr *this_attr = to_iio_dev_attr(attr);
+//	struct cf_axi_converter *conv = iio_device_get_drvdata(indio_dev);
+//	struct ad9172_state *st = container_of(conv, struct ad9172_state, conv);
+//	ad917x_handle_t *ad917x_h = &st->dac_h;
+	unsigned long long readin;
+	int ret;
+
+	ret = kstrtoull(buf, 10, &readin);
+	if (ret)
+		return ret;
+
+	mutex_lock(&indio_dev->mlock);
+
+	switch ((u32)this_attr->address) {
+		case 0:
+			//ret = ad917x_nco_set(ad917x_h, 0, readin, 0, 0);
+			break;
+		default:
+			ret = -EINVAL;
+	}
+
+	mutex_unlock(&indio_dev->mlock);
+
+	return ret ? ret : len;
+}
+
+static ssize_t ad9172_attr_show(struct device *dev,
+					 struct device_attribute *attr,
+					 char *buf)
+{
+	struct iio_dev *indio_dev = dev_to_iio_dev(dev);
+	struct iio_dev_attr *this_attr = to_iio_dev_attr(attr);
+//	struct cf_axi_converter *conv = iio_device_get_drvdata(indio_dev);
+//	struct ad9172_state *st = container_of(conv, struct ad9172_state, conv);
+//	ad917x_handle_t *ad917x_h = &st->dac_h;
+	int ret = 0;
+	u64 freq = 0;
+//	u16 ampl;
+
+	mutex_lock(&indio_dev->mlock);
+	switch ((u32)this_attr->address) {
+		case 0:
+			//ad917x_nco_get(ad917x_h, 0, &freq, &ampl, &ret);
+			ret = sprintf(buf, "%llu\n", freq);
+			break;
+		default:
+			ret = -EINVAL;
+	}
+	mutex_unlock(&indio_dev->mlock);
+
+	return ret;
+}
+
+static IIO_DEVICE_ATTR(out_altvoltage4_frequency_nco, S_IRUGO | S_IWUSR,
+		       ad9172_attr_show,
+		       ad9172_attr_store,
+		       0);
+
+static struct attribute *ad9172_attributes[] = {
+	&iio_dev_attr_out_altvoltage4_frequency_nco.dev_attr.attr,
+	NULL,
+};
+
+static const struct attribute_group ad9172_attribute_group = {
+	.attrs = ad9172_attributes,
+};
+
 
 static int ad9172_probe(struct spi_device *spi)
 {
 	const struct spi_device_id *dev_id = spi_get_device_id(spi);
 	struct cf_axi_converter *conv;
 	struct ad9172_state *st;
-	unsigned long lane_rate_kHz;
-	unsigned int val;
-	unsigned int id;
 	int ret;
 
 	st = devm_kzalloc(&spi->dev, sizeof(*st), GFP_KERNEL);
 	if (st == NULL)
 		return -ENOMEM;
 
-	st->id = (enum chip_id)dev_id->driver_data;
+	st->id = (enum chip_id) dev_id->driver_data;
 	conv = &st->conv;
+
+	conv->reset_gpio = devm_gpiod_get_optional(&spi->dev, "reset", GPIOD_OUT_HIGH);
+	if (IS_ERR(conv->reset_gpio))
+		return PTR_ERR(conv->reset_gpio);
+
+	conv->txen_gpio[0] = devm_gpiod_get_optional(&spi->dev, "txen0", GPIOD_OUT_HIGH);
+	if (IS_ERR(conv->txen_gpio[0]))
+		return PTR_ERR(conv->txen_gpio[0]);
+
+	conv->txen_gpio[1] = devm_gpiod_get_optional(&spi->dev, "txen1", GPIOD_OUT_HIGH);
+	if (IS_ERR(conv->txen_gpio[1]))
+		return PTR_ERR(conv->txen_gpio[1]);
 
 	st->map = devm_regmap_init_spi(spi, &ad9172_regmap_config);
 	if (IS_ERR(st->map))
@@ -144,155 +487,52 @@ static int ad9172_probe(struct spi_device *spi)
 	conv->write = ad9172_write;
 	conv->read = ad9172_read;
 	conv->setup = ad9172_prepare;
-
 	conv->get_data_clk = ad9172_get_data_clk;
+	conv->write_raw = ad9172_write_raw;
 	conv->read_raw = ad9172_read_raw;
+	conv->attrs = &ad9172_attribute_group;
 	conv->spi = spi;
 	conv->id = ID_AD9172;
 
 	ret = ad9172_get_clks(conv);
 	if (ret < 0) {
-		if (ret != -EPROBE_DEFER)
-			dev_err(&spi->dev, "Failed to get clocks\n");
-		return ret;
+		dev_err(&spi->dev, "Failed to get clocks\n");
+		goto out;
 	}
 
-	regmap_write(st->map, 0x000, 0x81);
-	regmap_write(st->map, 0x000, 0x3c);
-	regmap_write(st->map, 0x705, 0x01);
-	regmap_write(st->map, 0x091, 0x00);
-	regmap_write(st->map, 0x206, 0x01);
-	regmap_write(st->map, 0x705, 0x01);
-	regmap_write(st->map, 0x090, 0x00);
-	regmap_write(st->map, 0x253, 0x01);
-	regmap_write(st->map, 0x254, 0x01);
+	st->dac_h.user_data = spi;
+	st->dac_h.sdo = SPI_SDO;
+	st->dac_h.dev_xfer = ad9172_spi_xfer;
+	st->dac_h.delay_us = delay_us;
+	st->dac_h.tx_en_pin_ctrl = NULL;
+	st->dac_h.reset_pin_ctrl = NULL;
+	st->dac_h.syncoutb = SIGNAL_LVDS;
+	st->dac_h.sysref = COUPLING_AC;
 
-	regmap_write(st->map, 0x095, 0x01);
-	regmap_write(st->map, 0x790, 0x0f);
-	regmap_write(st->map, 0x791, 0x1f);
-
-	mdelay(1);
-	ret = regmap_read(st->map, REG_SPI_PRODIDL, &id);
-	if (ret < 0)
-		return ret;
-
-	if (id != st->id) {
-		dev_err(&spi->dev, "Unrecognized CHIP_ID 0x%X\n", id);
-		return -ENODEV;
-	}
-
-	regmap_write(st->map, REG_SPI_SCRATCHPAD, 0xAD);
-	regmap_read(st->map, REG_SPI_SCRATCHPAD, &id);
-	if (id != 0xAD)
-		return -EIO;
-
-	regmap_write(st->map, 0xc0, 0x01);
-	regmap_write(st->map, 0xdb, 0x00);
-	regmap_write(st->map, 0xdb, 0x01);
-	regmap_write(st->map, 0xdb, 0x00);
-	regmap_write(st->map, 0xc1, 0x48);
-	regmap_write(st->map, 0xc1, 0x49);
-	regmap_write(st->map, 0xc7, 0x01);
-
-	regmap_write(st->map, 0x50, 0x2a);
-	regmap_write(st->map, 0x61, 0x68);
-	regmap_write(st->map, 0x51, 0x82);
-	regmap_write(st->map, 0x51, 0x83);
-	regmap_write(st->map, 0x81, 0x03);
-
-	regmap_write(st->map, 0x100, 0x00);
-	regmap_write(st->map, 0x110, 0x0a);
-	regmap_write(st->map, 0x111, 0x11);
-	regmap_write(st->map, 0x084, 0x00);
-	regmap_write(st->map, 0x300, 0x01);
-	regmap_write(st->map, 0x475, 0x09);
-	regmap_write(st->map, 0x453, 0x87);
-	regmap_write(st->map, 0x457, 0x0f);
-	regmap_write(st->map, 0x458, 0x2f);
-	regmap_write(st->map, 0x459, 0x01);
-	regmap_write(st->map, 0x475, 0x01);
-	regmap_write(st->map, 0x300, 0x00);
-
-	regmap_write(st->map, 0x240, 0xaa);
-	regmap_write(st->map, 0x241, 0xaa);
-	regmap_write(st->map, 0x242, 0x55);
-	regmap_write(st->map, 0x243, 0x55);
-	regmap_write(st->map, 0x244, 0x1f);
-	regmap_write(st->map, 0x245, 0x1f);
-	regmap_write(st->map, 0x246, 0x1f);
-	regmap_write(st->map, 0x247, 0x1f);
-	regmap_write(st->map, 0x248, 0x1f);
-	regmap_write(st->map, 0x249, 0x1f);
-	regmap_write(st->map, 0x24a, 0x1f);
-	regmap_write(st->map, 0x24b, 0x1f);
-
-	regmap_write(st->map, 0x201, 0x00);
-	regmap_write(st->map, 0x203, 0x01);
-
-	regmap_write(st->map, 0x210, 0x16);
-	regmap_write(st->map, 0x216, 0x05);
-	regmap_write(st->map, 0x212, 0xff);
-	regmap_write(st->map, 0x212, 0x00);
-	regmap_write(st->map, 0x210, 0x87);
-	regmap_write(st->map, 0x216, 0x11);
-	regmap_write(st->map, 0x213, 0x01);
-	regmap_write(st->map, 0x213, 0x00);
-	regmap_write(st->map, 0x200, 0x00);
-	msleep(100);
-	regmap_write(st->map, 0x210, 0x86);
-	regmap_write(st->map, 0x216, 0x40);
-	regmap_write(st->map, 0x213, 0x01);
-	regmap_write(st->map, 0x213, 0x00);
-	regmap_write(st->map, 0x210, 0x86);
-	regmap_write(st->map, 0x216, 0x00);
-	regmap_write(st->map, 0x213, 0x01);
-	regmap_write(st->map, 0x213, 0x00);
-	regmap_write(st->map, 0x210, 0x86);
-	regmap_write(st->map, 0x216, 0x01);
-	regmap_write(st->map, 0x213, 0x01);
-	regmap_write(st->map, 0x213, 0x00);
-	regmap_write(st->map, 0x210, 0x87);
-	regmap_write(st->map, 0x216, 0x01);
-	regmap_write(st->map, 0x213, 0x01);
-	regmap_write(st->map, 0x213, 0x00);
-	regmap_write(st->map, 0x280, 0x05);
-	regmap_write(st->map, 0x280, 0x01);
-	regmap_write(st->map, 0x206, 0x01);
-
-	regmap_read(st->map, 0x281, &val);
-
-	dev_err(&spi->dev, "SERDES locked: %d\n", val & 1);
-
-	regmap_write(st->map, 0x306, 0x0c);
-	regmap_write(st->map, 0x307, 0x0c);
-	regmap_write(st->map, 0x03b, 0xf1);
-	regmap_write(st->map, 0x03a, 0x02);
-	msleep(10);
-	regmap_write(st->map, 0x300, 0x01);
-
-	regmap_write(st->map, 0x008, 0xc0);
-	regmap_write(st->map, 0x596, 0x1c);
-
-	st->interpolation = 1;
-
-	lane_rate_kHz = clk_get_rate(st->conv.clk[1]) / st->interpolation;
-	lane_rate_kHz = DIV_ROUND_CLOSEST(lane_rate_kHz, 200);
-	ret = clk_set_rate(conv->clk[0], lane_rate_kHz);
+	ret = ad9172_setup(st);
 	if (ret < 0) {
-		dev_err(&spi->dev, "Failed to set lane rate to %ld kHz: %d\n",
-			lane_rate_kHz, ret);
-		return ret;
-	}
-
-	ret = clk_prepare_enable(conv->clk[0]);
-	if (ret < 0) {
-		dev_err(&spi->dev, "Failed to enable JESD204 link: %d\n", ret);
-		return ret;
+		dev_err(&spi->dev, "Failed to setup device\n");
+		goto out;
 	}
 
 	spi_set_drvdata(spi, conv);
 
-	dev_dbg(&spi->dev, "Probed.\n");
+	dev_info(&spi->dev, "Probed.\n");
+	return 0;
+out:
+	return ret;
+}
+
+static int ad9172_remove(struct spi_device *spi)
+{
+	struct cf_axi_converter *conv = spi_get_drvdata(spi);
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(clk_names); i++)
+		if (conv->clk[i])
+			clk_disable_unprepare(conv->clk[i]);
+
+	spi_set_drvdata(spi, NULL);
 	return 0;
 }
 
@@ -300,6 +540,7 @@ static const struct spi_device_id ad9172_id[] = {
 	{ "ad9172", CHIPID_AD9172 },
 	{}
 };
+
 MODULE_DEVICE_TABLE(spi, ad9172_id);
 
 static struct spi_driver ad9172_driver = {
@@ -308,8 +549,12 @@ static struct spi_driver ad9172_driver = {
 		   .owner = THIS_MODULE,
 		   },
 	.probe = ad9172_probe,
+	.remove = ad9172_remove,
 	.id_table = ad9172_id,
 };
+
 module_spi_driver(ad9172_driver);
 
+MODULE_AUTHOR("Michael Hennerich <michael.hennerich@analog.com>");
+MODULE_DESCRIPTION("Analog Devices AD9172 DAC");
 MODULE_LICENSE("GPL v2");
