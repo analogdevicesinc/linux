91817e786dfd614fa005c86ac05b94f5afa086a1
Michael Hennerich <michael.hennerich@analog.com>
2018-12-13 15:44:35 +0100

    iio: frequency: ad917x: api_errors: use more meaningful errno
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/frequency/ad917x/api_errors.h
+++ drivers/iio/frequency/ad917x/api_errors.h
@@ -28,23 +28,23 @@
 /** Wrong value assigned to the \ref ad9172_handle_t.sdo */
 #define API_ERROR_SPI_SDO (-EINVAL)
 /** Handler pointer is invalid */
-#define API_ERROR_INVALID_HANDLE_PTR (-ENODEV)
+#define API_ERROR_INVALID_HANDLE_PTR (-EINVAL)
 /** Invalid pointer to the SPI xfer function assigned */
-#define API_ERROR_INVALID_XFER_PTR (-ENODEV)
+#define API_ERROR_INVALID_XFER_PTR (-EINVAL)
 /** Invalid pointer to the delayus function assigned */
-#define API_ERROR_INVALID_DELAYUS_PTR (-ENODEV)
+#define API_ERROR_INVALID_DELAYUS_PTR (-EINVAL)
 /** Invalid parameter passed */
-#define API_ERROR_INVALID_PARAM (-ENODEV)
+#define API_ERROR_INVALID_PARAM (-EINVAL)
 /** Not supported */
-#define API_ERROR_NOT_SUPPORTED (-EPERM)
+#define API_ERROR_NOT_SUPPORTED (-ENOTSUP)
 /** The VCO is out of range */
-#define API_ERROR_VCO_OUT_OF_RANGE (-ENODEV)
+#define API_ERROR_VCO_OUT_OF_RANGE (-EINVAL)
 /** FTW Acknowledge not received */
-#define API_ERROR_FTW_LOAD_ACK (-ENODEV)
+#define API_ERROR_FTW_LOAD_ACK (-EFAULT)
 /** The NCO is not enabled */
 #define API_ERROR_NCO_NOT_ENABLED (-ENODEV)
 /** Initialization sequence failed */
-#define API_ERROR_INIT_SEQ_FAIL (-ENODEV)
+#define API_ERROR_INIT_SEQ_FAIL (-EFAULT)
 /** Test failed */
 #define API_ERROR_TEST_FAILED -(ENODEV)
 /** SPI transfer error */
