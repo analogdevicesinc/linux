3becaeaa173fd13c52b18efe409b6c2466aa2bc5
Dragos Bogdan <dragos.bogdan@analog.com>
2019-01-15 15:59:52 +0000

    iio: adc: adrv9009: Add support for dual chip setup
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>
    (cherry picked from commit 22e7487622b0cd7b4e9b6c7bfff2d625aefd8848)

--- drivers/iio/frequency/cf_axi_dds.c
+++ drivers/iio/frequency/cf_axi_dds.c
@@ -1191,6 +1191,40 @@ static struct cf_axi_dds_chip_info cf_axi_dds_chip_info_ad9371 = {
 	.scan_masks = ad9361_available_scan_masks,
 };
 
+static struct cf_axi_dds_chip_info cf_axi_dds_chip_info_adrv9009_x2 = {
+	.name = "ADRV9009-X2",
+	.channel = {
+		CF_AXI_DDS_CHAN_BUF(0),
+		CF_AXI_DDS_CHAN_BUF(1),
+		CF_AXI_DDS_CHAN_BUF(2),
+		CF_AXI_DDS_CHAN_BUF(3),
+		CF_AXI_DDS_CHAN_BUF(4),
+		CF_AXI_DDS_CHAN_BUF(5),
+		CF_AXI_DDS_CHAN_BUF(6),
+		CF_AXI_DDS_CHAN_BUF(7),
+		CF_AXI_DDS_CHAN(0, 0, "TX1_I_F1"),
+		CF_AXI_DDS_CHAN(1, 0, "TX1_I_F2"),
+		CF_AXI_DDS_CHAN(2, 0, "TX1_Q_F1"),
+		CF_AXI_DDS_CHAN(3, 0, "TX1_Q_F2"),
+		CF_AXI_DDS_CHAN(4, 0, "TX2_I_F1"),
+		CF_AXI_DDS_CHAN(5, 0, "TX2_I_F2"),
+		CF_AXI_DDS_CHAN(6, 0, "TX2_Q_F1"),
+		CF_AXI_DDS_CHAN(7, 0, "TX2_Q_F2"),
+		CF_AXI_DDS_CHAN(8, 0, "TX3_I_F1"),
+		CF_AXI_DDS_CHAN(9, 0, "TX3_I_F2"),
+		CF_AXI_DDS_CHAN(10, 0, "TX3_Q_F1"),
+		CF_AXI_DDS_CHAN(11, 0, "TX3_Q_F2"),
+		CF_AXI_DDS_CHAN(12, 0, "TX4_I_F1"),
+		CF_AXI_DDS_CHAN(13, 0, "TX4_I_F2"),
+		CF_AXI_DDS_CHAN(14, 0, "TX4_Q_F1"),
+		CF_AXI_DDS_CHAN(15, 0, "TX4_Q_F2"),
+	},
+	.num_channels = 24,
+	.num_dds_channels = 16,
+	.num_buf_channels = 8,
+	.scan_masks = ad9361_2x2_available_scan_masks,
+};
+
 static struct cf_axi_dds_chip_info cf_axi_dds_chip_info_ad9364 = {
 	.name = "AD9364",
 	.channel = {
