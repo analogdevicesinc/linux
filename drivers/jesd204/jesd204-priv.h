8f03b681f5f589c0d7a9b561b7d0fe501ba44e15
Alexandru Ardelean <alexandru.ardelean@analog.com>
2019-01-22 16:55:42 +0200

    jesd204: keep track of top JESD204 topologies
    This change implements a differentiation between normal/node JESD204
    devices and top JESD204 devices.
    
    In a JESD204 topology, at least one device must be a top-level. This
    device is the device for which all the JESD204 chain/topology must
    synchronize (via this JESD204 framework) so that it can start/operate.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    (cherry picked from commit 91e1d550c5d1c48705d092cd5f3ab838c2b151d7)

--- drivers/jesd204/jesd204-priv.h
+++ drivers/jesd204/jesd204-priv.h
@@ -11,18 +11,44 @@
 #include <linux/jesd204/jesd204.h>
 
 struct jesd204_dev;
+struct jesd204_dev_top;
 
 /**
  * struct jesd204_dev - JESD204 device
  * @list		list entry for the framework to keep a list of devices
+ * @is_top		true if this device is a top device in a topology of
+ *			devices that make up a JESD204 link (typically the
+ *			device that is the ADC, DAC, or transceiver)
  * @np			reference in the device-tree for this JESD204 device
  * @ref			ref count for this JESD204 device
  */
 struct jesd204_dev {
 	struct list_head		list;
 
+	bool				is_top;
+
 	struct device_node		*np;
 	struct kref			ref;
 };
+
+/**
+ * struct jesd204_dev_top - JESD204 top device (in a JESD204 topology)
+ * @list		list entry for the framework to keep a list of top
+ *			devices (and implicitly topologies)
+ * @jdev		JESD204 device data
+ */
+struct jesd204_dev_top {
+	struct list_head		list;
+
+	struct jesd204_dev		jdev;
+};
+
+static inline struct jesd204_dev_top *jesd204_dev_top_dev(
+		struct jesd204_dev *jdev)
+{
+	if (!jdev || !jdev->is_top)
+		return NULL;
+	return container_of(jdev, struct jesd204_dev_top, jdev);
+}
 
 #endif /* _JESD204_PRIV_H_ */
