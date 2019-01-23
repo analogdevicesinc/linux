8975a722e8c56b7e270ab02d3099756cbe3ae7a4
Alexandru Ardelean <alexandru.ardelean@analog.com>
2019-01-23 10:20:12 +0200

    jesd204: create topology links during init
    This change implements reading device from the device-tree and creating
    links between devices.
    Each JESD204 device will keep a list of JESD204 device inputs and outputs.
    This way callbacks can be propagated across a group of devices that are
    linked/correlated so that a JESD204 top-level device can start operation
    after all devices in that graph have met certain requirements.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    (cherry picked from commit e48467f38717f551ff8904665822a30f1cdbfcf7)

--- drivers/jesd204/jesd204-priv.h
+++ drivers/jesd204/jesd204-priv.h
@@ -14,6 +14,37 @@ struct jesd204_dev;
 struct jesd204_dev_top;
 
 /**
+ * struct jesd204_dev_list_entry - Entry for a JESD204 device in a list
+ * @entry		list entry for a device to keep a list of devices
+ * @jdev		pointer to JESD204 device for this list entry
+ */
+struct jesd204_dev_list_entry {
+	struct list_head		entry;
+	struct jesd204_dev		*jdev;
+};
+
+/**
+ * struct jesd204_dev_con_out - Output connection of a JESD204 device
+ * @entry		list entry for a device to keep a list of connections
+ * @owner		pointer to JESD204 device to which this connection
+ *			belongs to
+ * @jdev_top		pointer to JESD204 top device, to which this connection
+ *			belongs to
+ * @dests		list of JESD204 devices this connection is connected
+ *			as input
+ * @dests_count		number of connected JESD204 devices to this output
+ * @of			device-tree reference and arguments for this connection
+ */
+struct jesd204_dev_con_out {
+	struct list_head		entry;
+	struct jesd204_dev		*owner;
+	struct jesd204_dev_top		*jdev_top;
+	struct list_head		dests;
+	unsigned int			dests_count;
+	struct of_phandle_args		of;
+};
+
+/**
  * struct jesd204_dev - JESD204 device
  * @entry		list entry for the framework to keep a list of devices
  * @is_top		true if this device is a top device in a topology of
@@ -22,6 +53,12 @@ struct jesd204_dev_top;
  * @dev			device that registers itself as a JESD204 device
  * @np			reference in the device-tree for this JESD204 device
  * @ref			ref count for this JESD204 device
+ * @inputs		array of pointers to output connections from other
+ *			devices
+ * @inputs_count	number of @inputs in the array
+ * @outputs		list of output connections that take input from this
+ *			device
+ * @outputs_count	number of @outputs in the list
  */
 struct jesd204_dev {
 	struct list_head		entry;
@@ -31,6 +68,11 @@ struct jesd204_dev {
 	struct device			*dev;
 	struct device_node		*np;
 	struct kref			ref;
+
+	struct jesd204_dev_con_out	**inputs;
+	unsigned int			inputs_count;
+	struct list_head		outputs;
+	unsigned int			outputs_count;
 };
 
 /**
