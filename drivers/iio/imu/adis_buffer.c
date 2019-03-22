4a020820c67bb6fbc0af2c00e58dd6f1da5dced6
Stefan Popa <stefan.popa@analog.com>
2019-03-22 15:33:56 +0200

    iio: imu: adis: Add delay for burst function
    Some IMU devices from ADI which support the burst read function, also
    require a write and read delay.
    
    Signed-off-by: Stefan Popa <stefan.popa@analog.com>
    (cherry picked from commit c245e857e957762a976fb95c3efbc6eab6d08134)

--- drivers/iio/imu/adis_buffer.c
+++ drivers/iio/imu/adis_buffer.c
@@ -46,9 +46,11 @@ static int adis_update_scan_mode_burst(struct iio_dev *indio_dev,
 	adis->xfer[0].tx_buf = tx;
 	adis->xfer[0].bits_per_word = 8;
 	adis->xfer[0].len = 2;
+	adis->xfer[0].delay_usecs = adis->burst->write_delay;
 	adis->xfer[1].rx_buf = adis->buffer;
 	adis->xfer[1].bits_per_word = 8;
 	adis->xfer[1].len = burst_length;
+	adis->xfer[1].delay_usecs = adis->burst->read_delay;
 
 	spi_message_init(&adis->msg);
 	spi_message_add_tail(&adis->xfer[0], &adis->msg);
