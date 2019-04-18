8b41041a1021951cd71eeef793130fd9fc7226c1
Michael Hennerich <michael.hennerich@analog.com>
2019-04-18 15:58:17 +0200

    include: dt-bindings: iio: frequency: hmc7044: Add dt-bindings include
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    (cherry picked from commit 2810d2e3881fbe34ea3f92ef1c91454b6d30ea2a)

---
+++ include/dt-bindings/iio/frequency/hmc7044.h
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * HMC7044 SPI Low Jitter Clock Generator
+ *
+ * Copyright 2019 Analog Devices Inc.
+ */
+
+#ifndef _DT_BINDINGS_IIO_FREQUENCY_HMC7044_H_
+#define _DT_BINDINGS_IIO_FREQUENCY_HMC7044_H_
+
+/*
+ * adi,pulse-generator-mode
+ */
+#define HMC7044_PULSE_GEN_LEVEL_SENSITIVE	0
+#define HMC7044_PULSE_GEN_1_PULSE		1
+#define HMC7044_PULSE_GEN_2_PULSE		2
+#define HMC7044_PULSE_GEN_4_PULSE		3
+#define HMC7044_PULSE_GEN_8_PULSE		4
+#define HMC7044_PULSE_GEN_16_PULSE		5
+#define HMC7044_PULSE_GEN_CONT_PULSE		7
+
+/*
+ * adi,driver-mode
+ */
+#define HMC7044_DRIVER_MODE_CML			0
+#define HMC7044_DRIVER_MODE_LVPECL		1
+#define HMC7044_DRIVER_MODE_LVDS		2
+#define HMC7044_DRIVER_MODE_CMOS		3
+
+/*
+ * adi,driver-impedance
+ */
+#define HMC7044_DRIVER_IMPEDANCE_DISABLE	0
+#define HMC7044_DRIVER_IMPEDANCE_100_OHM	1
+#define HMC7044_DRIVER_IMPEDANCE_50_OHM		3
+
+#endif /* _DT_BINDINGS_IIO_FREQUENCY_HMC7044_H_ */
