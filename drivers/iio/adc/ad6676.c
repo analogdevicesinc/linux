283692cffd558cbff97d8eaaab910f3868f5e795
Alexandru Ardelean <alexandru.ardelean@analog.com>
2019-05-17 17:12:43 +0300

    drivers: iio: remove adi,spi-3wire-enable & handling
    All drivers support upstream 3wire mode by using the upstream `spi-3wire`
    property. All device-trees have been updated to use this property.
    
    This change removes all usage/handling of the old/special
    adi,spi-3wire-enable.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    (cherry picked from commit 149a17ce80ebbfdb6ba0a5ef48e029be6f0ba509)

--- drivers/iio/adc/ad6676.c
+++ drivers/iio/adc/ad6676.c
@@ -69,7 +69,6 @@ struct ad6676_platform_data {
 	struct ad6676_jesd_conf jesd;
 	struct ad6676_agc_conf agc;
 	struct ad6676_shuffler_conf shuffler;
-	bool spi3wire;
 };
 
 struct ad6676_phy {
@@ -510,7 +509,7 @@ static int ad6676_setup(struct axiadc_converter *conv)
 		phy->ref_clk = clk_get_rate(clk);
 	}
 
-	ret = ad6676_reset(conv, (spi->mode & SPI_3WIRE) || pdata->spi3wire);
+	ret = ad6676_reset(conv, (spi->mode & SPI_3WIRE));
 	if (ret < 0)
 		return ret;
 
@@ -1078,8 +1077,6 @@ static struct ad6676_platform_data *ad6676_parse_dt(struct device *dev)
 	pdata = devm_kzalloc(dev, sizeof(*pdata), GFP_KERNEL);
 	if (!pdata)
 		return NULL;
-
-	pdata->spi3wire = of_property_read_bool(np, "adi,spi-3wire-enable");
 
 	pdata->base.f_adc_hz = ad6676_of_property_read_u32(np, "adi,adc-frequency-hz", 320000000UL);
 	pdata->base.fadc_fixed = of_property_read_bool(np, "adi,adc-frequency-fixed-enable");
--- include/linux/iio/frequency/ad9528.h
+++ include/linux/iio/frequency/ad9528.h
@@ -38,7 +38,6 @@ struct ad9528_channel_spec {
  * struct ad9528_platform_data - platform specific information
  *
  * @vcxo_freq: External VCXO frequency in Hz
- * @spi3wire: SPI 3-Wire mode enable;
  * @refa_en: REFA input enable.
  * @refb_en: REFB input enable.
  * @refa_diff_rcv_en: REFA differential/single-ended input selection.
@@ -78,7 +77,6 @@ struct ad9528_channel_spec {
 
 struct ad9528_platform_data {
 	unsigned long			vcxo_freq;
-	bool				spi3wire;
 
 	/* REFA / REFB input configuration */
 	bool				refa_en;
