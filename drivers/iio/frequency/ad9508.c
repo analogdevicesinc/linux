9d9150d209206271e2cc46d636bdb9d6a5ca4d22
Alexandru Ardelean <alexandru.ardelean@analog.com>
2019-05-24 13:32:20 +0300

    Revert "drivers: iio: remove adi,spi-3wire-enable & handling"
    This reverts commit 149a17ce80ebbfdb6ba0a5ef48e029be6f0ba509.
    
    This did not take into consideration the fact that the SPI framework does
    some validation and won't allow any modes/bits to be set if the controller
    does not support it.
    
    The Cadence SPI controller does not support 3wire, so this fails.
    There is a HDL wrapper that takes care of converting 3wire to 4wire. Both
    the SPI controller & the driver need to support 3wire, otherwise the SPI
    framework won't allow it.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    (cherry picked from commit a0d320a0a8adaa9ce8cccbd6d5f9efeaee140426)

--- drivers/iio/frequency/ad9508.c
+++ drivers/iio/frequency/ad9508.c
@@ -466,7 +466,7 @@ static int ad9508_setup(struct iio_dev *indio_dev)
 
 	ret = ad9508_write(indio_dev, AD9508_SERIAL_PORT_CONFIG,
 			AD9508_SER_CONF_SOFT_RESET |
-			((st->spi->mode & SPI_3WIRE) ? 0 :
+			((st->spi->mode & SPI_3WIRE || pdata->spi3wire) ? 0 :
 			 AD9508_SER_CONF_SDO_ACTIVE));
 	if (ret < 0)
 		return ret;
@@ -549,6 +549,8 @@ static struct ad9508_platform_data *ad9508_parse_dt(struct device *dev)
 	pdata = devm_kzalloc(dev, sizeof(*pdata), GFP_KERNEL);
 	if (!pdata)
 		return NULL;
+
+	pdata->spi3wire = of_property_read_bool(np, "adi,spi-3wire-enable");
 
 	strncpy(&pdata->name[0], np->name, SPI_NAME_SIZE - 1);
 
