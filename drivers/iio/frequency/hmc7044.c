34bfbb2ea066073a1d81530362c7f94da51a31a3
Michael Hennerich <michael.hennerich@analog.com>
2019-06-13 10:58:51 +0200

    iio: frequency: hmc7044: Fix PLL1 PFD calculation
    The Phase Frequency Detector (PFD) frequency is calculated as VCXO/N1.
    This bug has caused undesirable low PFD values.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    (cherry picked from commit ea0e697da69889fcd2f836b27becbb6849c29ab2)

--- drivers/iio/frequency/hmc7044.c
+++ drivers/iio/frequency/hmc7044.c
@@ -549,7 +549,7 @@ static int hmc7044_setup(struct iio_dev *indio_dev)
 				    HMC7044_N1_MAX, HMC7044_R1_MAX,
 				    &n1, &r1);
 
-	pfd1_freq = vcxo_freq / r1;
+	pfd1_freq = vcxo_freq / n1;
 	while ((pfd1_freq > HMC7044_RECOMM_FPD1) &&
 	       (n1 <= HMC7044_N1_MAX / 2) &&
 	       (r1 <= HMC7044_R1_MAX / 2)) {
