e90c9ab3c8295f17b351d6bfe67e02fcad0a4322
Alexandru Ardelean <alexandru.ardelean@analog.com>
2019-06-27 17:28:58 +0300

    jesd204: init JESD204 links info from driver
    This link information has been copied from the AD9680 driver. And the rest
    has been adapted from the JESD204 spec for future needs.
    It seems that most of this is more-or-less duplicated in other drivers as
    well.
    
    This information will be controlled by the framework mostly, and passed to
    all drivers that make up a JESD204 link (or more links).
    
    The variable names have been adapted to be a bit code-friendly. The spec
    refers to them as simple letters (L, M, F, K, etc).
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    (cherry picked from commit 34b513cd91c610704add3acf72d8abca78e97d53)

--- include/linux/jesd204/jesd204.h
+++ include/linux/jesd204/jesd204.h
@@ -9,6 +9,12 @@
 
 struct jesd204_dev;
 
+enum jesd204_sysref_mode {
+	JESD204_SYSREF_DISABLED,
+	JESD204_SYSREF_CONTINUOUS,
+	JESD204_SYSREF_ONESHOT,
+};
+
 enum jesd204_state_change_result {
 	JESD204_STATE_CHANGE_ERROR = -1,
 	JESD204_STATE_CHANGE_DEFER = 0,
@@ -17,10 +23,89 @@ enum jesd204_state_change_result {
 
 typedef int (*jesd204_cb)(struct jesd204_dev *jdev);
 
+/** struct jesd204_sysref - JESD204 configuration for SYSREF
+ * @mode			SYSREF mode (see @jesd204_sysref_mode)
+ * @capture_falling_edge	true if it should capture falling edge
+ * @valid_falling_edge		true if falling edge should be valid
+ */
+struct jesd204_sysref {
+	enum jesd204_sysref_mode	mode;
+	u8				capture_falling_edge;
+	u8				valid_falling_edge;
+};
+
+/**
+ * struct jesd204_link - JESD204 link configuration settings
+ * @sample_rate			sample rate for the link
+ * @num_lanes			number of JESD204 lanes (L)
+ * @num_converters		number of converters per link (M)
+ * @octets_per_frame		number of octets per frame (F)
+ * @frames_per_multiframe	number of frames per frame (K)
+ * @bits_per_sample		number of bits per sample (N')
+ * @converter_resolution	converter resolution (N)
+ * @jesd_version		JESD204 version (A, B or C) (JESDV)
+ * @subclass			JESD204 subclass (0,1 or 2) (SUBCLASSV)
+ * @did				device ID (DID)
+ * @bid				bank ID (BID)
+ * @scrambling			true if scrambling enabled (SCR)
+ * @high_density		true if high-density format is used (HD)
+ * @ctrl_words_per_frame_clk	number of control words per frame clock
+ *				period (CF)
+ * @ctrl_bits_per_sample	number of control bits per sample (CS)
+ * @samples_per_conv_frame	number of samples per converter per frame
+ *				cycle (S)
+ * @lane_ids			array of lane IDs (LID); note that this is an
+ *				array the size of @num_lanes
+ * @sysref			JESD204 sysref config, see @jesd204_sysref
+ * @dac_adj_resolution_steps	number of adjustment resolution steps to adjust
+ *				DAC LMFC (ADJCNT) - Subclass 2 only
+ * @dac_adj_direction		direction to adjust DAC LMFC (ADJDIR)
+ *				Subclass 2 only
+ * @dac_phase_adj		true to do phase adjustment request to DAC
+ *				Subclass 2 only
+ */
+struct jesd204_link {
+	u64 sample_rate;
+
+	u8 num_lanes;
+	u8 num_converters;
+	u8 octets_per_frame;
+	u8 frames_per_multiframe;
+
+	u8 bits_per_sample;
+
+	u8 converter_resolution;
+	u8 jesd_version;
+	u8 subclass;
+
+	u8 did;
+	u8 bid;
+
+	u8 scrambling;
+	u8 high_density;
+
+	u8 ctrl_words_per_frame_clk;
+	u8 ctrl_bits_per_sample;
+	u8 samples_per_conv_frame;
+
+	u8 *lane_ids;
+
+	struct jesd204_sysref sysref;
+
+	/* Subclass 2 only */
+	u8 dac_adj_resolution_steps;
+	u8 dac_adj_direction;
+	u8 dac_phase_adj;
+};
+
 /**
  * struct jesd204_dev_data - JESD204 device initialization data
+ * @links		JESD204 initial link configuration
+ * @num_links		number of JESD204 links
  */
 struct jesd204_dev_data {
+	const struct jesd204_link		*links;
+	unsigned int				num_links;
 };
 
 #if IS_ENABLED(CONFIG_JESD204)
