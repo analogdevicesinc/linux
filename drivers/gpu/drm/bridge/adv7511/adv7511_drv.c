cc5af7ba359624c0a98207d6d191b5f772c13dbb
Bogdan Togorean <bogdan.togorean@analog.com>
2019-07-04 10:37:21 +0300

    Revert "MLK-16347-6: gpu: drm: bridge: adv7511: Add new compatible string"
    This reverts commit 3d5353947ff9529e0a951fd57428d43aadd8e40d.

--- drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
+++ drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
@@ -1179,7 +1179,6 @@ static const struct i2c_device_id adv7511_i2c_ids[] = {
 	{ "adv7513", ADV7511 },
 #ifdef CONFIG_DRM_I2C_ADV7533
 	{ "adv7533", ADV7533 },
-	{ "adv7535", ADV7533 },
 #endif
 	{ }
 };
@@ -1191,7 +1190,6 @@ static const struct of_device_id adv7511_of_ids[] = {
 	{ .compatible = "adi,adv7513", .data = (void *)ADV7511 },
 #ifdef CONFIG_DRM_I2C_ADV7533
 	{ .compatible = "adi,adv7533", .data = (void *)ADV7533 },
-	{ .compatible = "adi,adv7535", .data = (void *)ADV7533 },
 #endif
 	{ }
 };
