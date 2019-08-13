a05fc89e4acc1f14a2b89f69b2bd11c5eff913c1
Robert Chiras <robert.chiras@nxp.com>
2019-08-13 16:08:12 +0300

    LF-2321-2: drm/mxsfb: Add defines for the rest of registers
    Some of the existing registers in this controller are not defined, but
    also not used. Add them to the register definitions, so that they can be
    easily used in future improvements or fixes.
    
    Signed-off-by: Robert Chiras <robert.chiras@nxp.com>
    Tested-by: Guido Günther <agx@sigxcpu.org>
    Reviewed-by: Laurentiu Palcu <laurentiu.palcu@nxp.com>
    [ Liu Ying: Fix conflicts to upgrade to next-20220527 ]
    Signed-off-by: Liu Ying <victor.liu@nxp.com>

--- drivers/gpu/drm/mxsfb/mxsfb_regs.h
+++ drivers/gpu/drm/mxsfb/mxsfb_regs.h
@@ -21,12 +21,20 @@
 #define LCDC_V4_NEXT_BUF		0x50
 #define LCDC_V3_CUR_BUF			0x30
 #define LCDC_V3_NEXT_BUF		0x40
+#define LCDC_TIMING			0x60
 #define LCDC_VDCTRL0			0x70
 #define LCDC_VDCTRL1			0x80
 #define LCDC_VDCTRL2			0x90
 #define LCDC_VDCTRL3			0xa0
 #define LCDC_VDCTRL4			0xb0
+#define LCDC_DVICTRL0			0xc0
+#define LCDC_DVICTRL1			0xd0
+#define LCDC_DVICTRL2			0xe0
+#define LCDC_DVICTRL3			0xf0
+#define LCDC_DVICTRL4			0x100
+#define LCDC_V4_DATA			0x180
 #define LCDC_V4_CRC_STAT		0x1a0
+#define LCDC_V3_DATA			0x1b0
 #define LCDC_V4_DEBUG0			0x1d0
 #define LCDC_V3_DEBUG0			0x1f0
 #define LCDC_AS_CTRL			0x210
@@ -35,31 +43,55 @@
 #define LCDC_AS_CLRKEYLOW		0x240
 #define LCDC_AS_CLRKEYHIGH		0x250
 
+#define REG_PUT(x, h, l) (((x) << (l)) & GENMASK(h, l))
+#define REG_GET(x, h, l) (((x) & GENMASK(h, l)) >> (l))
+
 #define CTRL_SFTRST			BIT(31)
 #define CTRL_CLKGATE			BIT(30)
+#define CTRL_SHIFT_DIR(x)		REG_PUT((x), 26, 26)
+#define CTRL_SHIFT_NUM(x)		REG_PUT((x), 25, 21)
 #define CTRL_BYPASS_COUNT		BIT(19)
 #define CTRL_VSYNC_MODE			BIT(18)
 #define CTRL_DOTCLK_MODE		BIT(17)
 #define CTRL_DATA_SELECT		BIT(16)
-#define CTRL_BUS_WIDTH_16		(0 << 10)
-#define CTRL_BUS_WIDTH_8		(1 << 10)
-#define CTRL_BUS_WIDTH_18		(2 << 10)
-#define CTRL_BUS_WIDTH_24		(3 << 10)
-#define CTRL_BUS_WIDTH_MASK		(0x3 << 10)
-#define CTRL_WORD_LENGTH_16		(0 << 8)
-#define CTRL_WORD_LENGTH_8		(1 << 8)
-#define CTRL_WORD_LENGTH_18		(2 << 8)
-#define CTRL_WORD_LENGTH_24		(3 << 8)
+#define CTRL_INPUT_SWIZZLE(x)		REG_PUT((x), 15, 14)
+#define CTRL_CSC_SWIZZLE(x)		REG_PUT((x), 13, 12)
+#define CTRL_SET_BUS_WIDTH(x)		REG_PUT((x), 11, 10)
+#define CTRL_BUS_WIDTH_MASK		REG_PUT((0x3), 11, 10)
+#define CTRL_SET_WORD_LENGTH(x)		REG_PUT((x), 9, 8)
 #define CTRL_MASTER			BIT(5)
 #define CTRL_DF16			BIT(3)
 #define CTRL_DF18			BIT(2)
 #define CTRL_DF24			BIT(1)
 #define CTRL_RUN			BIT(0)
 
+#define CTRL_BUS_WIDTH_8		CTRL_SET_BUS_WIDTH(1)
+#define CTRL_BUS_WIDTH_16		CTRL_SET_BUS_WIDTH(0)
+#define CTRL_BUS_WIDTH_18		CTRL_SET_BUS_WIDTH(2)
+#define CTRL_BUS_WIDTH_24		CTRL_SET_BUS_WIDTH(3)
+#define CTRL_WORD_LENGTH_8		CTRL_SET_WORD_LENGTH(1)
+#define CTRL_WORD_LENGTH_16		CTRL_SET_WORD_LENGTH(0)
+#define CTRL_WORD_LENGTH_18		CTRL_SET_WORD_LENGTH(2)
+#define CTRL_WORD_LENGTH_24		CTRL_SET_WORD_LENGTH(3)
+
 #define CTRL1_RECOVER_ON_UNDERFLOW	BIT(24)
 #define CTRL1_FIFO_CLEAR		BIT(21)
-#define CTRL1_SET_BYTE_PACKAGING(x)	(((x) & 0xf) << 16)
-#define CTRL1_GET_BYTE_PACKAGING(x)	(((x) >> 16) & 0xf)
+
+/*
+ * BYTE_PACKAGING
+ *
+ * This bitfield is used to show which data bytes in a 32-bit word area valid.
+ * Default value 0xf indicates that all bytes are valid. For 8-bit transfers,
+ * any combination in this bitfield will mean valid data is present in the
+ * corresponding bytes. In the 16-bit mode, a 16-bit half-word is valid only if
+ * adjacent bits [1:0] or [3:2] or both are 1. A value of 0x0 will mean that
+ * none of the bytes are valid and should not be used. For example, set the bit
+ * field value to 0x7 if the display data is arranged in the 24-bit unpacked
+ * format (A-R-G-B where A value does not have be transmitted).
+ */
+#define CTRL1_SET_BYTE_PACKAGING(x)	REG_PUT((x), 19, 16)
+#define CTRL1_GET_BYTE_PACKAGING(x)	REG_GET((x), 19, 16)
+
 #define CTRL1_CUR_FRAME_DONE_IRQ_EN	BIT(13)
 #define CTRL1_CUR_FRAME_DONE_IRQ	BIT(9)
 
@@ -70,10 +102,10 @@
 #define CTRL2_SET_OUTSTANDING_REQS_16	(0x4 << 21)
 #define CTRL2_SET_OUTSTANDING_REQS_MASK	(0x7 << 21)
 
-#define TRANSFER_COUNT_SET_VCOUNT(x)	(((x) & 0xffff) << 16)
-#define TRANSFER_COUNT_GET_VCOUNT(x)	(((x) >> 16) & 0xffff)
-#define TRANSFER_COUNT_SET_HCOUNT(x)	((x) & 0xffff)
-#define TRANSFER_COUNT_GET_HCOUNT(x)	((x) & 0xffff)
+#define TRANSFER_COUNT_SET_VCOUNT(x)	REG_PUT((x), 31, 16)
+#define TRANSFER_COUNT_GET_VCOUNT(x)	REG_GET((x), 31, 16)
+#define TRANSFER_COUNT_SET_HCOUNT(x)	REG_PUT((x), 15, 0)
+#define TRANSFER_COUNT_GET_HCOUNT(x)	REG_GET((x), 15, 0)
 
 #define VDCTRL0_ENABLE_PRESENT		BIT(28)
 #define VDCTRL0_VSYNC_ACT_HIGH		BIT(27)
@@ -84,23 +116,23 @@
 #define VDCTRL0_VSYNC_PULSE_WIDTH_UNIT	BIT(20)
 #define VDCTRL0_HALF_LINE		BIT(19)
 #define VDCTRL0_HALF_LINE_MODE		BIT(18)
-#define VDCTRL0_SET_VSYNC_PULSE_WIDTH(x) ((x) & 0x3ffff)
-#define VDCTRL0_GET_VSYNC_PULSE_WIDTH(x) ((x) & 0x3ffff)
+#define VDCTRL0_SET_VSYNC_PULSE_WIDTH(x) REG_PUT((x), 17, 0)
+#define VDCTRL0_GET_VSYNC_PULSE_WIDTH(x) REG_GET((x), 17, 0)
 
-#define VDCTRL2_SET_HSYNC_PERIOD(x)	((x) & 0x3ffff)
-#define VDCTRL2_GET_HSYNC_PERIOD(x)	((x) & 0x3ffff)
+#define VDCTRL2_SET_HSYNC_PERIOD(x)	REG_PUT((x), 15, 0)
+#define VDCTRL2_GET_HSYNC_PERIOD(x)	REG_GET((x), 15, 0)
 
 #define VDCTRL3_MUX_SYNC_SIGNALS	BIT(29)
 #define VDCTRL3_VSYNC_ONLY		BIT(28)
-#define SET_HOR_WAIT_CNT(x)		(((x) & 0xfff) << 16)
-#define GET_HOR_WAIT_CNT(x)		(((x) >> 16) & 0xfff)
-#define SET_VERT_WAIT_CNT(x)		((x) & 0xffff)
-#define GET_VERT_WAIT_CNT(x)		((x) & 0xffff)
+#define SET_HOR_WAIT_CNT(x)		REG_PUT((x), 27, 16)
+#define GET_HOR_WAIT_CNT(x)		REG_GET((x), 27, 16)
+#define SET_VERT_WAIT_CNT(x)		REG_PUT((x), 15, 0)
+#define GET_VERT_WAIT_CNT(x)		REG_GET((x), 15, 0)
 
-#define VDCTRL4_SET_DOTCLK_DLY(x)	(((x) & 0x7) << 29) /* v4 only */
-#define VDCTRL4_GET_DOTCLK_DLY(x)	(((x) >> 29) & 0x7) /* v4 only */
+#define VDCTRL4_SET_DOTCLK_DLY(x)	REG_PUT((x), 31, 29) /* v4 only */
+#define VDCTRL4_GET_DOTCLK_DLY(x)	REG_GET((x), 31, 29) /* v4 only */
 #define VDCTRL4_SYNC_SIGNALS_ON		BIT(18)
-#define SET_DOTCLK_H_VALID_DATA_CNT(x)	((x) & 0x3ffff)
+#define SET_DOTCLK_H_VALID_DATA_CNT(x)	REG_PUT((x), 17, 0)
 
 #define DEBUG0_HSYNC			BIT(26)
 #define DEBUG0_VSYNC			BIT(25)
