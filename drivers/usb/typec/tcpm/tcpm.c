68a9a0ea9b1a26df332421874edbf26c4844b731
Li Jun <jun.li@nxp.com>
2019-08-15 16:05:13 +0800

    MLK-22406 usb: typec: tcpm: support source only port with dual data
    In case source only for power, but dual data role on USB, we
    enable drp toggling for detect source(host), then keep it at
    SNK_ATTACHED state, this is mainly for get orientation.
    
    Signed-off-by: Li Jun <jun.li@nxp.com>

--- drivers/usb/typec/tcpm/tcpm.c
+++ drivers/usb/typec/tcpm/tcpm.c
@@ -4964,7 +4964,11 @@ static void run_state_machine(struct tcpm_port *port)
 		ret = tcpm_snk_attach(port);
 		if (ret < 0)
 			tcpm_set_state(port, SNK_UNATTACHED, 0);
-		else
+		else if (port->port_type == TYPEC_PORT_SRC &&
+			 port->typec_caps.data == TYPEC_PORT_DRD) {
+			tcpm_typec_connect(port);
+			tcpm_log(port, "Keep at SNK_ATTACHED for USB data.");
+		} else
 			tcpm_set_state(port, SNK_STARTUP, 0);
 		break;
 	case SNK_STARTUP:
