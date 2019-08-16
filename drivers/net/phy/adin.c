b59ac70923c176257f13b76ab030b9575fd68815
Alexandru Ardelean <alexandru.ardelean@analog.com>
2019-08-16 14:17:45 +0300

    net: phy: adin: define `features` flags
    The `features` flags defines the capabilities of the PHY. It is required in
    older kernels, but in newer ones it becomes deprecated and needs to be
    replaced with a `get_features` hook.
    
    The `get_features` hook (if undefined) pokes the PHY core regs to see what
    caps (link speeds, duplexes, etc) the PHY supports and constructs an
    internal representation of the current `features` flags.
    
    We need to define this for 4.14, or the PHYs won't work.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    (cherry picked from commit c2dee57a3ed5aef4d636fbccc5b3ac9e48a9f667)

--- drivers/net/phy/adin.c
+++ drivers/net/phy/adin.c
@@ -940,6 +940,7 @@ static struct phy_driver adin_driver[] = {
 	{
 		PHY_ID_MATCH_MODEL(PHY_ID_ADIN1200),
 		.name		= "ADIN1200",
+		.features	= PHY_BASIC_FEATURES, /* FIXME: remove this when the `get_features` hook becomes available */
 		.flags		= PHY_POLL_CABLE_TEST,
 		.probe		= adin_probe,
 		.config_init	= adin_config_init,
@@ -963,6 +964,7 @@ static struct phy_driver adin_driver[] = {
 	{
 		PHY_ID_MATCH_MODEL(PHY_ID_ADIN1300),
 		.name		= "ADIN1300",
+		.features	= PHY_GBIT_FEATURES, /* FIXME: remove this when the `get_features` hook becomes available */
 		.flags		= PHY_POLL_CABLE_TEST,
 		.probe		= adin_probe,
 		.config_init	= adin_config_init,
