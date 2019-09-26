fc7fc2ba25b4a157fbe7b21149c51a1c70a2733e
Nuno Sá <nuno.sa@analog.com>
2019-09-26 15:14:10 +0200

    iio: frequency: ad917x: Fix compilation warning
    Possible usage of uninitialized variable.
    
    Signed-off-by: Nuno Sá <nuno.sa@analog.com>
    (cherry picked from commit da7588a559d9bd6380e3ecd181ec6d54340201c1)

--- drivers/iio/frequency/ad917x/ad917x_nco_api.c
+++ drivers/iio/frequency/ad917x/ad917x_nco_api.c
@@ -263,13 +263,12 @@ int ad917x_nco_set_phase_offset(ad917x_handle_t *h,
 					const ad917x_channel_select_t channels, const uint16_t ch_po)
 {
 	int err;
-	uint8_t tmp_reg;
+	uint8_t tmp_reg = 0;
 
 	if (h == NULL)
 		return API_ERROR_INVALID_HANDLE_PTR;
 	if (dacs != 0) {
 		/* DAC PAGE */
-		tmp_reg = 0;
 		if (dacs & AD917X_DAC0)
 			tmp_reg |= AD917X_MAINDAC_PAGE_0;
 		if (dacs & AD917X_DAC1)
