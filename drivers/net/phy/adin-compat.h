2fe317385dc9fe2e325b5793a79eb1aa249e97ed
Beniamin Bia <beniamin.bia@analog.com>
2019-09-30 19:48:50 +0300

    net: phy: adin: remove duplicated functions
    Duplicated functions were removed in order to successfully compile the
    kernel.
    
    Signed-off-by: Beniamin Bia <beniamin.bia@analog.com>
    (cherry picked from commit 17134c6ae9830ceda8eaab898b6b8b5d4725e48d)

--- drivers/net/phy/adin-compat.h
+++ drivers/net/phy/adin-compat.h
@@ -10,21 +10,6 @@
 #define phydev_warn(_phydev, format, args...)	\
 	dev_warn(&_phydev->mdio.dev, format, ##args)
 
-/* __mdiobus_read & __mdiobus_write are unlocked functions in upstream kernel,
- * but we shouldn't implement them as unlocked in 4.14, as the locks that
- * protect them, don't exist; this way, it should fail with compilation error
- * and we can remove them, as we know that the kernel implements correct locking
- */
-static inline int __mdiobus_read(struct mii_bus *bus, int addr, u32 regnum)
-{
-	return mdiobus_read(bus, addr, regnum);
-}
-
-static inline int __mdiobus_write(struct mii_bus *bus, int addr,
-				  u32 regnum, u16 val)
-{
-	return mdiobus_write(bus, addr, regnum, val);
-}
 
 static int adin_read_mmd(struct phy_device *phydev, int devad, u16 regnum);
 static int adin_write_mmd(struct phy_device *phydev, int devad, u16 regnum,
@@ -64,45 +49,8 @@ static inline int phy_clear_bits_mmd(struct phy_device *phydev, int devad,
 	return phy_modify_mmd(phydev, devad, regnum, val, 0);
 }
 
-static inline int phy_modify_changed(struct phy_device *phydev, u32 regnum,
-				     u16 mask, u16 set)
-{
-	int new, ret;
-
-	ret = phy_read(phydev, regnum);
-	if (ret < 0)
-		return ret;
-
-	new = (ret & ~mask) | set;
-	if (new == ret)
-		return 0;
-
-	ret = phy_write(phydev, regnum, new);
-
-	return ret < 0 ? ret : 1;
-}
-static inline int phy_modify(struct phy_device *phydev, u32 regnum,
-			     u16 mask, u16 set)
-{
-	int ret;
-
-	ret = phy_modify_changed(phydev, regnum, mask, set);
-
-	return ret < 0 ? ret : 0;
-}
 static inline int phy_set_bits_mmd(struct phy_device *phydev, int devad,
 		u32 regnum, u16 val)
 {
 	return phy_modify_mmd(phydev, devad, regnum, 0, val);
 }
-
-static inline int phy_clear_bits(struct phy_device *phydev, u32 regnum, u16 val)
-{
-	return phy_modify(phydev, regnum, val, 0);
-}
-
-static inline int phy_set_bits(struct phy_device *phydev, u32 regnum, u16 val)
-{
-	return phy_modify(phydev, regnum, 0, val);
-}
-
