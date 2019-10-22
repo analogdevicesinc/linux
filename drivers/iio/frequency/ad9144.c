cfed7e132d7967af770e0eb36d6b075f8f732043
Marcus Comstedt <marcus.comstedt@qamcom.se>
2019-10-22 16:53:54 +0200

    ad9144: Handle adi,jesd-link-mode device-tree property
    Signed-off-by: Marcus Comstedt <marcus@mc.pp.se>
    (cherry picked from commit c36e140e4080048a72edf793147d95c1006b717c)

--- drivers/iio/frequency/ad9144.c
+++ drivers/iio/frequency/ad9144.c
@@ -77,6 +77,7 @@ struct ad9144_platform_data {
 	u8 interpolation;
 	unsigned int fcenter_shift;
 	bool spi4wire;
+	u8 jesd_link_mode;
 };
 
 struct ad9144_state {
@@ -92,6 +93,25 @@ struct ad9144_state {
 
 	unsigned int pll_frequency;
 	bool pll_enable;
+};
+
+static const struct {
+	u8 m, l, s, f, hd, n, np;
+} ad9144_jesd_modes[] = {
+	/* 00 */ {4, 8, 1, 1, 1, 16, 16},
+	/* 01 */ {4, 8, 2, 2, 0, 16, 16},
+	/* 02 */ {4, 4, 1, 2, 0, 16, 16},
+	/* 03 */ {4, 2, 1, 4, 0, 16, 16},
+	/* 04 */ {2, 4, 1, 1, 1, 16, 16},
+	/* 05 */ {2, 4, 2, 2, 0, 16, 16},
+	/* 06 */ {2, 2, 1, 2, 0, 16, 16},
+	/* 07 */ {2, 1, 1, 4, 0, 16, 16},
+	/* 08 */ {1, 4, 2, 1, 1, 16, 16},
+	/* 09 */ {1, 2, 1, 1, 1, 16, 16},
+	/* 10 */ {1, 1, 1, 2, 0, 16, 16},
+	/* 11 */ {2, 8, 2, 1, 1, 16, 16},
+	/* 12 */ {2, 4, 1, 1, 1, 16, 16},
+	/* 13 */ {2, 2, 1, 2, 0, 16, 16}
 };
 
 static const char * const clk_names[] = {
@@ -1034,6 +1054,10 @@ static struct ad9144_platform_data *ad9144_parse_dt(struct device *dev)
 	of_property_read_u32(np, "adi,frequency-center-shift", &tmp);
 	pdata->fcenter_shift = tmp;
 
+	tmp = 4;
+	of_property_read_u32(np, "adi,jesd-link-mode", &tmp);
+	pdata->jesd_link_mode = (tmp > 13 ? 4 : tmp);
+
 	/*
 	 * DO NOT copy this. It is as wrong as it gets, we have to do it to
 	 * preserve backwards compatibility with earlier versions of the driver
@@ -1210,9 +1234,9 @@ static int ad9144_probe(struct spi_device *spi)
 		goto out;
 	}
 
-	st->num_lanes = 4;
-	st->num_converters = 2;
-	st->octets_per_frame = 1;
+	st->num_lanes = ad9144_jesd_modes[pdata->jesd_link_mode].l;
+	st->num_converters = ad9144_jesd_modes[pdata->jesd_link_mode].m;
+	st->octets_per_frame = ad9144_jesd_modes[pdata->jesd_link_mode].f;
 
 	memset(&link_config, 0x00, sizeof(link_config));
 
@@ -1222,9 +1246,9 @@ static int ad9144_probe(struct spi_device *spi)
 	link_config.num_converters = st->num_converters;
 	link_config.octets_per_frame = st->octets_per_frame;
 	link_config.frames_per_multiframe = 32;
-	link_config.samples_per_frame = 1;
+	link_config.samples_per_frame = ad9144_jesd_modes[pdata->jesd_link_mode].s;
 
-	link_config.high_density = true;
+	link_config.high_density = ad9144_jesd_modes[pdata->jesd_link_mode].hd;
 	link_config.scrambling = true;
 	link_config.subclass = 1;
 	link_config.sysref.mode = AD9144_SYSREF_ONESHOT;
