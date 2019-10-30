d02afe9bc7d6fc34405a0a76e9fe8a1cbe9b7d8a
Alexandru Ardelean <alexandru.ardelean@analog.com>
2019-10-30 13:44:13 +0200

    iio: imu: adis: assign value only if return code zero in read funcs
    The inline read functions in the ADIS library don't check the return value
    of the `adis_read_reg()` function and assign the value of `tmp` regardless.
    
    Fix this by checking if return value is zero and only then assigning the
    value of `tmp`.
    
    Fixes: 57a1228a06b7a ("iio:imu:adis: Add support for 32bit registers")
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    (cherry picked from commit ab2cfd1a0c76c8a650404c3de354c2cff1e1bce0)

--- include/linux/iio/imu/adis.h
+++ include/linux/iio/imu/adis.h
@@ -273,7 +273,8 @@ static inline int adis_read_reg_16(struct adis *adis, unsigned int reg,
 	int ret;
 
 	ret = adis_read_reg(adis, reg, &tmp, 2);
-	*val = tmp;
+	if (ret == 0)
+		*val = tmp;
 
 	return ret;
 }
@@ -291,7 +292,8 @@ static inline int adis_read_reg_32(struct adis *adis, unsigned int reg,
 	int ret;
 
 	ret = adis_read_reg(adis, reg, &tmp, 4);
-	*val = tmp;
+	if (ret == 0)
+		*val = tmp;
 
 	return ret;
 }
