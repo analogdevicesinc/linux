a0a05f963158c2a985cb594e69989c5b11621a77
Alexandru Ardelean <alexandru.ardelean@analog.com>
2019-10-31 13:33:57 +0200

    iio: gyro: adis16136: check ret val for non-zero vs less-than-zero
    The ADIS library functions return zero on success, and negative values for
    error. Positive values aren't returned, but we only care about the success
    value (which is zero).
    
    This change is mostly needed so that the compiler won't make any inferences
    about some about values being potentially un-initialized. This only
    triggers after making some functions inline, because the compiler can
    better follow return paths.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    (cherry picked from commit bf4a6d3fd48a65c7c685192ba6805297e7a41d1b)

--- drivers/iio/gyro/adis16136.c
+++ drivers/iio/gyro/adis16136.c
@@ -81,19 +81,19 @@ static ssize_t adis16136_show_serial(struct file *file,
 
 	ret = adis_read_reg_16(&adis16136->adis, ADIS16136_REG_SERIAL_NUM,
 		&serial);
-	if (ret < 0)
+	if (ret)
 		return ret;
 
 	ret = adis_read_reg_16(&adis16136->adis, ADIS16136_REG_LOT1, &lot1);
-	if (ret < 0)
+	if (ret)
 		return ret;
 
 	ret = adis_read_reg_16(&adis16136->adis, ADIS16136_REG_LOT2, &lot2);
-	if (ret < 0)
+	if (ret)
 		return ret;
 
 	ret = adis_read_reg_16(&adis16136->adis, ADIS16136_REG_LOT3, &lot3);
-	if (ret < 0)
+	if (ret)
 		return ret;
 
 	len = snprintf(buf, sizeof(buf), "%.4x%.4x%.4x-%.4x\n", lot1, lot2,
@@ -117,7 +117,7 @@ static int adis16136_show_product_id(void *arg, u64 *val)
 
 	ret = adis_read_reg_16(&adis16136->adis, ADIS16136_REG_PROD_ID,
 		&prod_id);
-	if (ret < 0)
+	if (ret)
 		return ret;
 
 	*val = prod_id;
@@ -135,7 +135,7 @@ static int adis16136_show_flash_count(void *arg, u64 *val)
 
 	ret = adis_read_reg_16(&adis16136->adis, ADIS16136_REG_FLASH_CNT,
 		&flash_count);
-	if (ret < 0)
+	if (ret)
 		return ret;
 
 	*val = flash_count;
@@ -192,7 +192,7 @@ static int __adis16136_get_freq(struct adis16136 *adis16136, unsigned int *freq)
 	int ret;
 
 	ret = __adis_read_reg_16(&adis16136->adis, ADIS16136_REG_SMPL_PRD, &t);
-	if (ret < 0)
+	if (ret)
 		return ret;
 
 	*freq = 32768 / (t + 1);
@@ -232,7 +232,7 @@ static ssize_t adis16136_read_frequency(struct device *dev,
 	mutex_lock(slock);
 	ret = __adis16136_get_freq(adis16136, &freq);
 	mutex_unlock(slock);
-	if (ret < 0)
+	if (ret)
 		return ret;
 
 	return sprintf(buf, "%d\n", freq);
@@ -262,7 +262,7 @@ static int adis16136_set_filter(struct iio_dev *indio_dev, int val)
 
 	mutex_lock(slock);
 	ret = __adis16136_get_freq(adis16136, &freq);
-	if (ret < 0)
+	if (ret)
 		goto out_unlock;
 
 	for (i = ARRAY_SIZE(adis16136_3db_divisors) - 1; i >= 1; i--) {
@@ -289,11 +289,11 @@ static int adis16136_get_filter(struct iio_dev *indio_dev, int *val)
 
 	ret = __adis_read_reg_16(&adis16136->adis, ADIS16136_REG_AVG_CNT,
 				 &val16);
-	if (ret < 0)
+	if (ret)
 		goto err_unlock;
 
 	ret = __adis16136_get_freq(adis16136, &freq);
-	if (ret < 0)
+	if (ret)
 		goto err_unlock;
 
 	*val = freq / adis16136_3db_divisors[val16 & 0x07];
@@ -330,7 +330,7 @@ static int adis16136_read_raw(struct iio_dev *indio_dev,
 	case IIO_CHAN_INFO_CALIBBIAS:
 		ret = adis_read_reg_32(&adis16136->adis,
 			ADIS16136_REG_GYRO_OFF2, &val32);
-		if (ret < 0)
+		if (ret)
 			return ret;
 
 		*val = sign_extend32(val32, 31);
