analogdevicesinc/linux

commit dbfbfe0fb8de1d716cfe76aae096255436693c0e

iio: adis: Introduce timeouts structure
The adis library only allows to define a `startup_delay` which for some
devices is enough. However, other devices define different timeouts with
significantly different timings which could lead to devices to not wait
enough time or to wait a lot more than necessary (which is not
efficient). This patch introduces a new timeout struct that must be
passed into `adis_init()`. There are mainly, for now, three timeouts
used. This is also an introductory patch with the goal of refactoring
`adis_initial_startup()`. New driver's (eg: adis16480, adis16460) are
replicating code for the device initial setup. With some changes (being
this the first one) we can pass this to `adis_initial_startup()`.

Signed-off-by: Nuno Sá <nuno.sa@analog.com>
(cherry picked from commit 9020f8258466915fcc7012f68c6d2b78571c7ffa)

drivers/iio/accel/adis16209.c
@@ -244,6 +244,12 @@ static const char * const adis16209_status_error_msgs[] = {
 	[ADIS16209_STAT_POWER_LOW_BIT] = "Power supply below 3.15V",
 };
 
+static const struct adis_timeout adis16209_timeouts = {
+	.reset_ms = ADIS16209_STARTUP_DELAY,
+	.self_test_ms = ADIS16209_STARTUP_DELAY,
+	.sw_reset_ms = ADIS16209_STARTUP_DELAY,
+};
+
 static const struct adis_data adis16209_data = {
 	.read_delay = 30,
 	.msc_ctrl_reg = ADIS16209_MSC_CTRL_REG,
@@ -253,6 +259,7 @@ static const struct adis_data adis16209_data = {
 	.self_test_mask = ADIS16209_MSC_CTRL_SELF_TEST_EN,
 	.self_test_no_autoclear = true,
 	.startup_delay = ADIS16209_STARTUP_DELAY_MS,
+	.timeouts = &adis16209_timeouts,
 
 	.status_error_msgs = adis16209_status_error_msgs,
 	.status_error_mask = BIT(ADIS16209_STAT_SELFTEST_FAIL_BIT) |

drivers/iio/accel/adis16201.c
@@ -234,6 +234,12 @@ static const char * const adis16201_status_error_msgs[] = {
 	[ADIS16201_DIAG_STAT_POWER_LOW_BIT] = "Power supply below 3.15V",
 };
 
+static const struct adis_timeout adis16201_timeouts = {
+	.reset_ms = ADIS16201_STARTUP_DELAY,
+	.sw_reset_ms = ADIS16201_STARTUP_DELAY,
+	.self_test_ms = ADIS16201_STARTUP_DELAY,
+};
+
 static const struct adis_data adis16201_data = {
 	.read_delay = 20,
 	.msc_ctrl_reg = ADIS16201_MSC_CTRL_REG,
@@ -243,6 +249,7 @@ static const struct adis_data adis16201_data = {
 	.self_test_mask = ADIS16201_MSC_CTRL_SELF_TEST_EN,
 	.self_test_no_autoclear = true,
 	.startup_delay = ADIS16201_STARTUP_DELAY_MS,
+	.timeouts = &adis16201_timeouts,
 
 	.status_error_msgs = adis16201_status_error_msgs,
 	.status_error_mask = BIT(ADIS16201_DIAG_STAT_SPI_FAIL_BIT) |

drivers/iio/gyro/adis16136.c
@@ -60,6 +60,7 @@
 struct adis16136_chip_info {
 	unsigned int precision;
 	unsigned int fullscale;
+	const struct adis_timeout *timeouts;
 };
 
 struct adis16136 {
@@ -466,24 +467,6 @@ static const char * const adis16136_status_error_msgs[] = {
 	[ADIS16136_DIAG_STAT_FLASH_CHKSUM_FAIL] = "Flash checksum error",
 };
 
-static const struct adis_data adis16136_data = {
-	.diag_stat_reg = ADIS16136_REG_DIAG_STAT,
-	.glob_cmd_reg = ADIS16136_REG_GLOB_CMD,
-	.msc_ctrl_reg = ADIS16136_REG_MSC_CTRL,
-
-	.self_test_mask = ADIS16136_MSC_CTRL_SELF_TEST,
-	.startup_delay = 80,
-
-	.read_delay = 10,
-	.write_delay = 10,
-
-	.status_error_msgs = adis16136_status_error_msgs,
-	.status_error_mask = BIT(ADIS16136_DIAG_STAT_FLASH_UPDATE_FAIL) |
-		BIT(ADIS16136_DIAG_STAT_SPI_FAIL) |
-		BIT(ADIS16136_DIAG_STAT_SELF_TEST_FAIL) |
-		BIT(ADIS16136_DIAG_STAT_FLASH_CHKSUM_FAIL),
-};
-
 enum adis16136_id {
 	ID_ADIS16133,
 	ID_ADIS16135,
@@ -491,30 +474,73 @@ enum adis16136_id {
 	ID_ADIS16137,
 };
 
+static const struct adis_timeout adis16133_timeouts = {
+	.reset_ms = 75,
+	.sw_reset_ms = 75,
+	.self_test_ms = 50,
+};
+
+static const struct adis_timeout adis16136_timeouts = {
+	.reset_ms = 128,
+	.sw_reset_ms = 75,
+	.self_test_ms = 245,
+};
+
 static const struct adis16136_chip_info adis16136_chip_info[] = {
 	[ID_ADIS16133] = {
 		.precision = IIO_DEGREE_TO_RAD(1200),
 		.fullscale = 24000,
+		.timeouts = &adis16133_timeouts,
 	},
 	[ID_ADIS16135] = {
 		.precision = IIO_DEGREE_TO_RAD(300),
 		.fullscale = 24000,
+		.timeouts = &adis16133_timeouts,
 	},
 	[ID_ADIS16136] = {
 		.precision = IIO_DEGREE_TO_RAD(450),
 		.fullscale = 24623,
+		.timeouts = &adis16136_timeouts,
 	},
 	[ID_ADIS16137] = {
 		.precision = IIO_DEGREE_TO_RAD(1000),
 		.fullscale = 24609,
+		.timeouts = &adis16136_timeouts,
 	},
 };
+
+static struct adis_data *adis16136_adis_data_alloc(struct adis16136 *st)
+{
+	struct adis_data *data;
+	struct device *dev = &st->adis.spi->dev;
+
+	data = devm_kzalloc(dev, sizeof(struct adis_data), GFP_KERNEL);
+	if (!data)
+		return ERR_PTR(-ENOMEM);
+
+	data->msc_ctrl_reg = ADIS16136_REG_MSC_CTRL;
+	data->glob_cmd_reg = ADIS16136_REG_GLOB_CMD;
+	data->diag_stat_reg = ADIS16136_REG_DIAG_STAT;
+	data->self_test_mask = ADIS16136_MSC_CTRL_SELF_TEST;
+	data->startup_delay = 80;
+	data->read_delay = 10;
+	data->write_delay = 10;
+	data->status_error_msgs = adis16136_status_error_msgs;
+	data->status_error_mask = BIT(ADIS16136_DIAG_STAT_FLASH_UPDATE_FAIL) |
+				BIT(ADIS16136_DIAG_STAT_SPI_FAIL) |
+				BIT(ADIS16136_DIAG_STAT_SELF_TEST_FAIL) |
+				BIT(ADIS16136_DIAG_STAT_FLASH_CHKSUM_FAIL);
+	data->timeouts = st->chip_info->timeouts;
+
+	return data;
+}
 
 static int adis16136_probe(struct spi_device *spi)
 {
 	const struct spi_device_id *id = spi_get_device_id(spi);
 	struct adis16136 *adis16136;
 	struct iio_dev *indio_dev;
+	const struct adis_data *adis16136_data;
 	int ret;
 
 	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*adis16136));
@@ -533,7 +559,11 @@ static int adis16136_probe(struct spi_device *spi)
 	indio_dev->info = &adis16136_info;
 	indio_dev->modes = INDIO_DIRECT_MODE;
 
-	ret = adis_init(&adis16136->adis, indio_dev, spi, &adis16136_data);
+	adis16136_data = adis16136_adis_data_alloc(adis16136);
+	if (IS_ERR(adis16136_data))
+		return PTR_ERR(adis16136_data);
+
+	ret = adis_init(&adis16136->adis, indio_dev, spi, adis16136_data);
 	if (ret)
 		return ret;
 

drivers/iio/gyro/adis16260.c
@@ -333,6 +333,12 @@ static const char * const adis1620_status_error_msgs[] = {
 	[ADIS16260_DIAG_STAT_POWER_LOW_BIT] = "Power supply below 4.75",
 };
 
+static const struct adis_timeout adis16260_timeouts = {
+	.reset_ms = ADIS16260_STARTUP_DELAY,
+	.sw_reset_ms = ADIS16260_STARTUP_DELAY,
+	.self_test_ms = ADIS16260_STARTUP_DELAY,
+};
+
 static const struct adis_data adis16260_data = {
 	.write_delay = 30,
 	.read_delay = 30,
@@ -342,6 +348,7 @@ static const struct adis_data adis16260_data = {
 
 	.self_test_mask = ADIS16260_MSC_CTRL_MEM_TEST,
 	.startup_delay = ADIS16260_STARTUP_DELAY,
+	.timeouts = &adis16260_timeouts,
 
 	.status_error_msgs = adis1620_status_error_msgs,
 	.status_error_mask = BIT(ADIS16260_DIAG_STAT_FLASH_CHK_BIT) |

drivers/iio/imu/adis.c
@@ -343,19 +343,25 @@ EXPORT_SYMBOL_GPL(__adis_check_status);
 int __adis_reset(struct adis *adis)
 {
 	int ret;
+	const struct adis_timeout *timeouts = adis->data->timeouts;
 
 	ret = __adis_write_reg_8(adis, adis->data->glob_cmd_reg,
 			ADIS_GLOB_CMD_SW_RESET);
-	if (ret)
+	if (ret) {
 		dev_err(&adis->spi->dev, "Failed to reset device: %d\n", ret);
+		return ret;
+	}
 
-	return ret;
+	msleep(timeouts->sw_reset_ms);
+
+	return 0;
 }
 EXPORT_SYMBOL_GPL(__adis_reset);
 
 static int adis_self_test(struct adis *adis)
 {
 	int ret;
+	const struct adis_timeout *timeouts = adis->data->timeouts;
 
 	ret = __adis_write_reg_16(adis, adis->data->msc_ctrl_reg,
 			adis->data->self_test_mask);
@@ -365,7 +371,7 @@ static int adis_self_test(struct adis *adis)
 		return ret;
 	}
 
-	msleep(adis->data->startup_delay);
+	msleep(timeouts->self_test_ms);
 
 	ret = __adis_check_status(adis);
 
@@ -394,7 +400,6 @@ int adis_initial_startup(struct adis *adis)
 	if (ret) {
 		dev_err(&adis->spi->dev, "Self-test failed, trying reset.\n");
 		__adis_reset(adis);
-		msleep(adis->data->startup_delay);
 		ret = adis_self_test(adis);
 		if (ret) {
 			dev_err(&adis->spi->dev, "Second self-test failed, giving up.\n");
@@ -470,6 +475,11 @@ EXPORT_SYMBOL_GPL(adis_single_conversion);
 int adis_init(struct adis *adis, struct iio_dev *indio_dev,
 	struct spi_device *spi, const struct adis_data *data)
 {
+	if (!data || !data->timeouts) {
+		dev_err(&spi->dev, "No config data or timeouts not defined!\n");
+		return -EINVAL;
+	}
+
 	mutex_init(&adis->state_lock);
 	adis->spi = spi;
 	adis->data = data;

drivers/iio/imu/adis16400.c
@@ -160,6 +160,7 @@ struct adis16400_state;
 
 struct adis16400_chip_info {
 	const struct iio_chan_spec *channels;
+	const struct adis_timeout *timeouts;
 	const int num_channels;
 	const long flags;
 	unsigned int gyro_scale_micro;
@@ -933,6 +934,36 @@ static const struct iio_chan_spec adis16334_channels[] = {
 	IIO_CHAN_SOFT_TIMESTAMP(ADIS16400_SCAN_TIMESTAMP),
 };
 
+static const struct adis_timeout adis16300_timeouts = {
+	.reset_ms = ADIS16400_STARTUP_DELAY,
+	.sw_reset_ms = ADIS16400_STARTUP_DELAY,
+	.self_test_ms = ADIS16400_STARTUP_DELAY,
+};
+
+static const struct adis_timeout adis16362_timeouts = {
+	.reset_ms = 130,
+	.sw_reset_ms = 130,
+	.self_test_ms = 12,
+};
+
+static const struct adis_timeout adis16400_timeouts = {
+	.reset_ms = 170,
+	.sw_reset_ms = 170,
+	.self_test_ms = 12,
+};
+
+static const struct adis_timeout adis16445_timeouts = {
+	.reset_ms = 55,
+	.sw_reset_ms = 55,
+	.self_test_ms = 16,
+};
+
+static const struct adis_timeout adis16448_timeouts = {
+	.reset_ms = 90,
+	.sw_reset_ms = 90,
+	.self_test_ms = 45,
+};
+
 static struct adis16400_chip_info adis16400_chips[] = {
 	[ADIS16300] = {
 		.channels = adis16300_channels,
@@ -945,6 +976,7 @@ static struct adis16400_chip_info adis16400_chips[] = {
 		.temp_offset = 25000000 / 140000, /* 25 C = 0x00 */
 		.set_freq = adis16400_set_freq,
 		.get_freq = adis16400_get_freq,
+		.timeouts = &adis16300_timeouts,
 	},
 	[ADIS16334] = {
 		.channels = adis16334_channels,
@@ -968,6 +1000,7 @@ static struct adis16400_chip_info adis16400_chips[] = {
 		.flags = ADIS16400_NO_BURST | ADIS16400_HAS_SLOW_MODE,
 		.set_freq = adis16400_set_freq,
 		.get_freq = adis16400_get_freq,
+		.timeouts = &adis16300_timeouts,
 	},
 	[ADIS16360] = {
 		.channels = adis16350_channels,
@@ -980,6 +1013,7 @@ static struct adis16400_chip_info adis16400_chips[] = {
 		.temp_offset = 25000000 / 136000, /* 25 C = 0x00 */
 		.set_freq = adis16400_set_freq,
 		.get_freq = adis16400_get_freq,
+		.timeouts = &adis16300_timeouts,
 	},
 	[ADIS16362] = {
 		.channels = adis16350_channels,
@@ -992,6 +1026,7 @@ static struct adis16400_chip_info adis16400_chips[] = {
 		.temp_offset = 25000000 / 136000, /* 25 C = 0x00 */
 		.set_freq = adis16400_set_freq,
 		.get_freq = adis16400_get_freq,
+		.timeouts = &adis16362_timeouts,
 	},
 	[ADIS16364] = {
 		.channels = adis16350_channels,
@@ -1004,6 +1039,7 @@ static struct adis16400_chip_info adis16400_chips[] = {
 		.temp_offset = 25000000 / 136000, /* 25 C = 0x00 */
 		.set_freq = adis16400_set_freq,
 		.get_freq = adis16400_get_freq,
+		.timeouts = &adis16362_timeouts,
 	},
 	[ADIS16367] = {
 		.channels = adis16350_channels,
@@ -1016,6 +1052,7 @@ static struct adis16400_chip_info adis16400_chips[] = {
 		.temp_offset = 25000000 / 136000, /* 25 C = 0x00 */
 		.set_freq = adis16400_set_freq,
 		.get_freq = adis16400_get_freq,
+		.timeouts = &adis16300_timeouts,
 	},
 	[ADIS16400] = {
 		.channels = adis16400_channels,
@@ -1027,6 +1064,7 @@ static struct adis16400_chip_info adis16400_chips[] = {
 		.temp_offset = 25000000 / 140000, /* 25 C = 0x00 */
 		.set_freq = adis16400_set_freq,
 		.get_freq = adis16400_get_freq,
+		.timeouts = &adis16400_timeouts,
 	},
 	[ADIS16445] = {
 		.channels = adis16445_channels,
@@ -1040,6 +1078,7 @@ static struct adis16400_chip_info adis16400_chips[] = {
 		.temp_offset = 31000000 / 73860, /* 31 C = 0x00 */
 		.set_freq = adis16334_set_freq,
 		.get_freq = adis16334_get_freq,
+		.timeouts = &adis16445_timeouts,
 	},
 	[ADIS16448] = {
 		.channels = adis16448_channels,
@@ -1053,6 +1092,7 @@ static struct adis16400_chip_info adis16400_chips[] = {
 		.temp_offset = 31000000 / 73860, /* 31 C = 0x00 */
 		.set_freq = adis16334_set_freq,
 		.get_freq = adis16334_get_freq,
+		.timeouts = &adis16448_timeouts,
 	}
 };
 
@@ -1081,35 +1121,6 @@ static const char * const adis16400_status_error_msgs[] = {
 	[ADIS16400_DIAG_STAT_POWER_LOW] = "Power supply below 4.75V",
 };
 
-static const struct adis_data adis16400_data = {
-	.msc_ctrl_reg = ADIS16400_MSC_CTRL,
-	.glob_cmd_reg = ADIS16400_GLOB_CMD,
-	.diag_stat_reg = ADIS16400_DIAG_STAT,
-
-	.read_delay = 50,
-	.write_delay = 50,
-
-	.self_test_mask = ADIS16400_MSC_CTRL_MEM_TEST,
-	.startup_delay = ADIS16400_STARTUP_DELAY,
-
-	.status_error_msgs = adis16400_status_error_msgs,
-	.status_error_mask = BIT(ADIS16400_DIAG_STAT_ZACCL_FAIL) |
-		BIT(ADIS16400_DIAG_STAT_YACCL_FAIL) |
-		BIT(ADIS16400_DIAG_STAT_XACCL_FAIL) |
-		BIT(ADIS16400_DIAG_STAT_XGYRO_FAIL) |
-		BIT(ADIS16400_DIAG_STAT_YGYRO_FAIL) |
-		BIT(ADIS16400_DIAG_STAT_ZGYRO_FAIL) |
-		BIT(ADIS16400_DIAG_STAT_ALARM2) |
-		BIT(ADIS16400_DIAG_STAT_ALARM1) |
-		BIT(ADIS16400_DIAG_STAT_FLASH_CHK) |
-		BIT(ADIS16400_DIAG_STAT_SELF_TEST) |
-		BIT(ADIS16400_DIAG_STAT_OVERFLOW) |
-		BIT(ADIS16400_DIAG_STAT_SPI_FAIL) |
-		BIT(ADIS16400_DIAG_STAT_FLASH_UPT) |
-		BIT(ADIS16400_DIAG_STAT_POWER_HIGH) |
-		BIT(ADIS16400_DIAG_STAT_POWER_LOW),
-};
-
 static void adis16400_setup_chan_mask(struct adis16400_state *st)
 {
 	const struct adis16400_chip_info *chip_info = st->variant;
@@ -1124,11 +1135,49 @@ static void adis16400_setup_chan_mask(struct adis16400_state *st)
 	}
 }
 
+static struct adis_data *adis16400_adis_data_alloc(struct adis16400_state *st)
+{
+	struct adis_data *data;
+	struct device *dev = &st->adis.spi->dev;
+
+	data = devm_kzalloc(dev, sizeof(struct adis_data), GFP_KERNEL);
+	if (!data)
+		return ERR_PTR(-ENOMEM);
+
+	data->msc_ctrl_reg = ADIS16400_MSC_CTRL;
+	data->glob_cmd_reg = ADIS16400_GLOB_CMD;
+	data->diag_stat_reg = ADIS16400_DIAG_STAT;
+	data->read_delay = 50;
+	data->write_delay = 50;
+	data->self_test_mask = ADIS16400_MSC_CTRL_MEM_TEST;
+	data->startup_delay = ADIS16400_STARTUP_DELAY;
+	data->status_error_msgs = adis16400_status_error_msgs;
+	data->status_error_mask = BIT(ADIS16400_DIAG_STAT_ZACCL_FAIL) |
+				BIT(ADIS16400_DIAG_STAT_YACCL_FAIL) |
+				BIT(ADIS16400_DIAG_STAT_XACCL_FAIL) |
+				BIT(ADIS16400_DIAG_STAT_XGYRO_FAIL) |
+				BIT(ADIS16400_DIAG_STAT_YGYRO_FAIL) |
+				BIT(ADIS16400_DIAG_STAT_ZGYRO_FAIL) |
+				BIT(ADIS16400_DIAG_STAT_ALARM2) |
+				BIT(ADIS16400_DIAG_STAT_ALARM1) |
+				BIT(ADIS16400_DIAG_STAT_FLASH_CHK) |
+				BIT(ADIS16400_DIAG_STAT_SELF_TEST) |
+				BIT(ADIS16400_DIAG_STAT_OVERFLOW) |
+				BIT(ADIS16400_DIAG_STAT_SPI_FAIL) |
+				BIT(ADIS16400_DIAG_STAT_FLASH_UPT) |
+				BIT(ADIS16400_DIAG_STAT_POWER_HIGH) |
+				BIT(ADIS16400_DIAG_STAT_POWER_LOW);
+	data->timeouts = st->variant->timeouts;
+
+	return data;
+}
+
 static int adis16400_probe(struct spi_device *spi)
 {
 	struct adis16400_state *st;
 	struct iio_dev *indio_dev;
 	int ret;
+	const struct adis_data *adis16400_data;
 
 	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
 	if (indio_dev == NULL)
@@ -1155,7 +1204,11 @@ static int adis16400_probe(struct spi_device *spi)
 			st->adis.burst->extra_len = sizeof(u16);
 	}
 
-	ret = adis_init(&st->adis, indio_dev, spi, &adis16400_data);
+	adis16400_data = adis16400_adis_data_alloc(st);
+	if (IS_ERR(adis16400_data))
+		return PTR_ERR(adis16400_data);
+
+	ret = adis_init(&st->adis, indio_dev, spi, adis16400_data);
 	if (ret)
 		return ret;
 

drivers/iio/imu/adis16460.c
@@ -383,6 +383,12 @@ static const char * const adis16460_status_error_msgs[] = {
 	[ADIS16460_DIAG_STAT_FLASH_UPT] = "Flash update failure",
 };
 
+static const struct adis_timeout adis16460_timeouts = {
+	.reset_ms = 225,
+	.sw_reset_ms = 225,
+	.self_test_ms = 10,
+};
+
 static const struct adis_data adis16460_data = {
 	.diag_stat_reg = ADIS16460_REG_DIAG_STAT,
 	.glob_cmd_reg = ADIS16460_REG_GLOB_CMD,
@@ -398,6 +404,7 @@ static const struct adis_data adis16460_data = {
 		BIT(ADIS16460_DIAG_STAT_SPI_COMM) |
 		BIT(ADIS16460_DIAG_STAT_FLASH_UPT),
 	.enable_irq = adis16460_enable_irq,
+	.timeouts = &adis16460_timeouts,
 };
 
 static int adis16460_probe(struct spi_device *spi)

drivers/iio/imu/adis16475.c
@@ -77,6 +77,7 @@ struct adis16475_clks {
 struct adis16475_chip_info {
 	const struct iio_chan_spec *channels;
 	const struct adis16475_clks *clks;
+	const struct adis_timeout *timeouts;
 	u32 num_channels;
 	u32 gyro_max_val;
 	u32 gyro_max_scale;
@@ -85,8 +86,6 @@ struct adis16475_chip_info {
 	u32 temp_scale;
 	u32 int_clk;
 	u16 max_dec;
-	u16 reset_ms;
-	u16 self_test_ms;
 	u8 num_clks;
 	bool has_burst32;
 };
@@ -557,6 +556,18 @@ static const struct adis16475_clks adis16475_ext_clks[] = {
 	{ "pulse-sync", ADIS16475_CLK_PULSE, 1000, 2100 },
 };
 
+static const struct adis_timeout adis16475_timeouts = {
+	.reset_ms = 200,
+	.sw_reset_ms = 200,
+	.self_test_ms = 20,
+};
+
+static const struct adis_timeout adis1650x_timeouts = {
+	.reset_ms = 260,
+	.sw_reset_ms = 260,
+	.self_test_ms = 30,
+};
+
 static const struct adis16475_chip_info adis16475_chip_info[] = {
 	[ADIS16475_1] = {
 		.num_channels = ARRAY_SIZE(adis16475_channels),
@@ -568,8 +579,7 @@ static const struct adis16475_chip_info adis16475_chip_info[] = {
 		.temp_scale = 100,
 		.int_clk = 2000,
 		.max_dec = 1999,
-		.reset_ms = 200,
-		.self_test_ms = 20,
+		.timeouts = &adis16475_timeouts,
 		.clks = adis16475_ext_clks,
 		.num_clks = ARRAY_SIZE(adis16475_ext_clks),
 	},
@@ -583,8 +593,7 @@ static const struct adis16475_chip_info adis16475_chip_info[] = {
 		.temp_scale = 100,
 		.int_clk = 2000,
 		.max_dec = 1999,
-		.reset_ms = 200,
-		.self_test_ms = 20,
+		.timeouts = &adis16475_timeouts,
 		.clks = adis16475_ext_clks,
 		.num_clks = ARRAY_SIZE(adis16475_ext_clks),
 	},
@@ -598,8 +607,7 @@ static const struct adis16475_chip_info adis16475_chip_info[] = {
 		.temp_scale = 100,
 		.int_clk = 2000,
 		.max_dec = 1999,
-		.reset_ms = 200,
-		.self_test_ms = 20,
+		.timeouts = &adis16475_timeouts,
 		.clks = adis16475_ext_clks,
 		.num_clks = ARRAY_SIZE(adis16475_ext_clks),
 	},
@@ -613,8 +621,7 @@ static const struct adis16475_chip_info adis16475_chip_info[] = {
 		.temp_scale = 100,
 		.int_clk = 2000,
 		.max_dec = 1999,
-		.reset_ms = 200,
-		.self_test_ms = 20,
+		.timeouts = &adis16475_timeouts,
 		.clks = adis16475_ext_clks,
 		.num_clks = ARRAY_SIZE(adis16475_ext_clks),
 	},
@@ -628,8 +635,7 @@ static const struct adis16475_chip_info adis16475_chip_info[] = {
 		.temp_scale = 100,
 		.int_clk = 2000,
 		.max_dec = 1999,
-		.reset_ms = 200,
-		.self_test_ms = 20,
+		.timeouts = &adis16475_timeouts,
 		.clks = adis16475_ext_clks,
 		.num_clks = ARRAY_SIZE(adis16475_ext_clks),
 	},
@@ -643,8 +649,7 @@ static const struct adis16475_chip_info adis16475_chip_info[] = {
 		.temp_scale = 100,
 		.int_clk = 2000,
 		.max_dec = 1999,
-		.reset_ms = 200,
-		.self_test_ms = 20,
+		.timeouts = &adis16475_timeouts,
 		.clks = adis16475_ext_clks,
 		.num_clks = ARRAY_SIZE(adis16475_ext_clks),
 	},
@@ -658,8 +663,7 @@ static const struct adis16475_chip_info adis16475_chip_info[] = {
 		.temp_scale = 100,
 		.int_clk = 2000,
 		.max_dec = 1999,
-		.reset_ms = 260,
-		.self_test_ms = 30,
+		.timeouts = &adis1650x_timeouts,
 		.clks = adis16475_ext_clks,
 		/* pulse sync not supported */
 		.num_clks = ARRAY_SIZE(adis16475_ext_clks) - 1,
@@ -675,8 +679,7 @@ static const struct adis16475_chip_info adis16475_chip_info[] = {
 		.temp_scale = 100,
 		.int_clk = 2000,
 		.max_dec = 1999,
-		.reset_ms = 260,
-		.self_test_ms = 30,
+		.timeouts = &adis1650x_timeouts,
 		.clks = adis16475_ext_clks,
 		/* pulse sync not supported */
 		.num_clks = ARRAY_SIZE(adis16475_ext_clks) - 1,
@@ -692,8 +695,7 @@ static const struct adis16475_chip_info adis16475_chip_info[] = {
 		.temp_scale = 100,
 		.int_clk = 2000,
 		.max_dec = 1999,
-		.reset_ms = 260,
-		.self_test_ms = 30,
+		.timeouts = &adis1650x_timeouts,
 		.clks = adis16475_ext_clks,
 		/* pulse sync not supported */
 		.num_clks = ARRAY_SIZE(adis16475_ext_clks) - 1,
@@ -709,8 +711,7 @@ static const struct adis16475_chip_info adis16475_chip_info[] = {
 		.temp_scale = 100,
 		.int_clk = 2000,
 		.max_dec = 1999,
-		.reset_ms = 260,
-		.self_test_ms = 30,
+		.timeouts = &adis1650x_timeouts,
 		.clks = adis16475_ext_clks,
 		/* pulse sync not supported */
 		.num_clks = ARRAY_SIZE(adis16475_ext_clks) - 1,
@@ -726,8 +727,7 @@ static const struct adis16475_chip_info adis16475_chip_info[] = {
 		.temp_scale = 100,
 		.int_clk = 2000,
 		.max_dec = 1999,
-		.reset_ms = 260,
-		.self_test_ms = 30,
+		.timeouts = &adis1650x_timeouts,
 		.clks = adis16475_ext_clks,
 		/* pulse sync not supported */
 		.num_clks = ARRAY_SIZE(adis16475_ext_clks) - 1,
@@ -743,8 +743,7 @@ static const struct adis16475_chip_info adis16475_chip_info[] = {
 		.temp_scale = 100,
 		.int_clk = 2000,
 		.max_dec = 1999,
-		.reset_ms = 260,
-		.self_test_ms = 30,
+		.timeouts = &adis1650x_timeouts,
 		.clks = adis16475_ext_clks,
 		/* pulse sync not supported */
 		.num_clks = ARRAY_SIZE(adis16475_ext_clks) - 1,
@@ -807,27 +806,6 @@ static struct adis_burst adis16475_burst = {
 	.extra_len = 3 * sizeof(u16),
 	.read_delay = 5,
 	.write_delay = 5,
-};
-
-static const struct adis_data adis16475_data = {
-	.msc_ctrl_reg = ADIS16475_REG_MSG_CTRL,
-	.glob_cmd_reg = ADIS16475_REG_GLOB_CMD,
-	.diag_stat_reg = ADIS16475_REG_DIAG_STAT,
-
-	.cs_change_delay = 16,
-	.read_delay = 5,
-	.write_delay = 5,
-
-	.status_error_msgs = adis16475_status_error_msgs,
-	.status_error_mask = BIT(ADIS16475_DIAG_STAT_DATA_PATH) |
-		BIT(ADIS16475_DIAG_STAT_FLASH_MEM) |
-		BIT(ADIS16475_DIAG_STAT_SPI) |
-		BIT(ADIS16475_DIAG_STAT_STANDBY) |
-		BIT(ADIS16475_DIAG_STAT_SENSOR) |
-		BIT(ADIS16475_DIAG_STAT_MEMORY) |
-		BIT(ADIS16475_DIAG_STAT_CLK),
-
-	.enable_irq = adis16475_enable_irq
 };
 
 static u16 adis16475_validate_crc(const u8 *buffer, const u16 crc,
@@ -1112,17 +1090,18 @@ static int adis16475_check_state(struct iio_dev *indio_dev)
 	struct adis16475 *st = iio_priv(indio_dev);
 	u16 prod_id;
 	u32 device_id;
+	const struct adis_timeout *timeouts = st->info->timeouts;
 
 	ret = __adis_reset(&st->adis);
 	if (ret)
 		return ret;
 
-	msleep(st->info->reset_ms);
+	msleep(timeouts->reset_ms);
 	ret = __adis_write_reg_16(&st->adis, ADIS16475_REG_GLOB_CMD, BIT(2));
 	if (ret)
 		return ret;
 
-	msleep(st->info->self_test_ms);
+	msleep(timeouts->self_test_ms);
 	ret = __adis_check_status(&st->adis);
 	if (ret)
 		return ret;
@@ -1143,12 +1122,42 @@ static int adis16475_check_state(struct iio_dev *indio_dev)
 	return 0;
 }
 
+static struct adis_data *adis16475_adis_data_alloc(struct adis16475 *st)
+{
+	struct adis_data *data;
+	struct device *dev = &st->adis.spi->dev;
+
+	data = devm_kzalloc(dev, sizeof(struct adis_data), GFP_KERNEL);
+	if (!data)
+		return ERR_PTR(-ENOMEM);
+
+	data->msc_ctrl_reg = ADIS16475_REG_MSG_CTRL;
+	data->glob_cmd_reg = ADIS16475_REG_GLOB_CMD;
+	data->diag_stat_reg = ADIS16475_REG_DIAG_STAT;
+	data->cs_change_delay = 16;
+	data->read_delay = 5;
+	data->write_delay = 5;
+	data->status_error_msgs = adis16475_status_error_msgs;
+	data->status_error_mask = BIT(ADIS16475_DIAG_STAT_DATA_PATH) |
+				BIT(ADIS16475_DIAG_STAT_FLASH_MEM) |
+				BIT(ADIS16475_DIAG_STAT_SPI) |
+				BIT(ADIS16475_DIAG_STAT_STANDBY) |
+				BIT(ADIS16475_DIAG_STAT_SENSOR) |
+				BIT(ADIS16475_DIAG_STAT_MEMORY) |
+				BIT(ADIS16475_DIAG_STAT_CLK);
+	data->enable_irq = adis16475_enable_irq;
+	data->timeouts = st->info->timeouts;
+
+	return data;
+}
+
 static int adis16475_probe(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev;
 	struct adis16475 *st;
 	struct gpio_desc *desc;
 	const struct spi_device_id *id = spi_get_device_id(spi);
+	const struct adis_data *adis16475_data;
 	int ret;
 
 	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
@@ -1159,7 +1168,11 @@ static int adis16475_probe(struct spi_device *spi)
 	st->info = &adis16475_chip_info[id->driver_data];
 	spi_set_drvdata(spi, indio_dev);
 
-	ret = adis_init(&st->adis, indio_dev, spi, &adis16475_data);
+	adis16475_data = adis16475_adis_data_alloc(st);
+	if (IS_ERR(adis16475_data))
+		return PTR_ERR(adis16475_data);
+
+	ret = adis_init(&st->adis, indio_dev, spi, adis16475_data);
 	if (ret)
 		return ret;
 

commit 5f4ad722b96d2a485baac30ee380f22c51cdc478

iio: adis16209: Fix undefined macro
This driver has some differences from the master version (which is still
on staging) leading to a build failure. In master
`ADIS16209_STARTUP_DELAY_MS` is still `ADIS16209_STARTUP_DELAY`.

Fixes: dbfbfe0fb8de ("iio: adis: Introduce timeouts structure")
Signed-off-by: Nuno Sá <nuno.sa@analog.com>

drivers/iio/accel/adis16209.c
@@ -245,9 +245,9 @@ static const char * const adis16209_status_error_msgs[] = {
 };
 
 static const struct adis_timeout adis16209_timeouts = {
-	.reset_ms = ADIS16209_STARTUP_DELAY,
-	.self_test_ms = ADIS16209_STARTUP_DELAY,
-	.sw_reset_ms = ADIS16209_STARTUP_DELAY,
+	.reset_ms = ADIS16209_STARTUP_DELAY_MS,
+	.self_test_ms = ADIS16209_STARTUP_DELAY_MS,
+	.sw_reset_ms = ADIS16209_STARTUP_DELAY_MS,
 };
 
 static const struct adis_data adis16209_data = {