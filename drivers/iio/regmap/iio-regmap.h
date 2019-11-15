577f44b430b98d85de48c6e3eabfbd8bbb43f0c8
Alexandru Tachici <alexandru.tachici@analog.com>
2019-11-15 17:38:52 +0200

    iio: iio-regmap: added regmap configuration
    The iio-regmap will use the regmap framework for
    i2c/spi reading/writing in order to simplify the implementation.
    
    The regmap_config parameters (addr size and val size) are
    now read from the device tree.
    
    Signed-off-by: Alexandru Tachici <alexandru.tachici@analog.com>
    (cherry picked from commit c97188bc2967e61ea7a70cf772bd2be273f4ce0d)

--- drivers/iio/regmap/iio-regmap.h
+++ drivers/iio/regmap/iio-regmap.h
@@ -8,8 +8,11 @@
 #ifndef _IIO_REGMAP_H_
 #define _IIO_REGMAP_H_
 
+struct device;
 struct regmap;
+struct regmap_config;
 
+struct regmap_config *iio_regmap_read_config(struct device *dev);
 int iio_regmap_probe(struct device *dev, struct regmap *regmap,
 		     const char *name);
 
