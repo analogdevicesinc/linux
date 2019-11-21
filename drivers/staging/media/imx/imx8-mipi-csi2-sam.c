21d62e0688679462c4511c21522d20918d457743
Guoniu.zhou <guoniu.zhou@nxp.com>
2019-11-21 14:54:53 +0800

    LF-79: staging: media: imx: replace s_parm/g_parm with s/g_frame_interval
    Replace s_parm/g_parm with s_frame_interval/g_frame_interval because
    s_parm/g_parm was remove in latest V4L2 framework, more detail, please
    refer to commit: 8180b4f4f589ee34d698d30ae6ad7042bf7ff1a0
    
    we need to use s_frame_interval/g_frame_interval to set/get new property
    of subdev.
    
    Signed-off-by: Guoniu.zhou <guoniu.zhou@nxp.com>
    Reviewed-by: Robby.cai <robby.cai@nxp.com>

--- drivers/staging/media/imx/imx8-mipi-csi2-sam.c
+++ drivers/staging/media/imx/imx8-mipi-csi2-sam.c
@@ -1016,7 +1016,8 @@ static int mipi_csis_s_rx_buffer(struct v4l2_subdev *mipi_sd, void *buf,
 	return 0;
 }
 
-static int mipi_csis_s_parm(struct v4l2_subdev *mipi_sd, struct v4l2_streamparm *a)
+static int mipi_csis_s_frame_interval(struct v4l2_subdev *mipi_sd,
+				struct v4l2_subdev_frame_interval *interval)
 {
 	struct csi_state *state = mipi_sd_to_csi_state(mipi_sd);
 	struct media_pad *source_pad;
@@ -1036,10 +1037,11 @@ static int mipi_csis_s_parm(struct v4l2_subdev *mipi_sd, struct v4l2_streamparm 
 		return -EINVAL;
 	}
 
-	return v4l2_subdev_call(sen_sd, video, s_parm, a);
+	return v4l2_subdev_call(sen_sd, video, s_frame_interval, interval);
 }
 
-static int mipi_csis_g_parm(struct v4l2_subdev *mipi_sd, struct v4l2_streamparm *a)
+static int mipi_csis_g_frame_interval(struct v4l2_subdev *mipi_sd,
+				struct v4l2_subdev_frame_interval *interval)
 {
 	struct csi_state *state = mipi_sd_to_csi_state(mipi_sd);
 	struct media_pad *source_pad;
@@ -1059,7 +1061,7 @@ static int mipi_csis_g_parm(struct v4l2_subdev *mipi_sd, struct v4l2_streamparm 
 		return -EINVAL;
 	}
 
-	return v4l2_subdev_call(sen_sd, video, g_parm, a);
+	return v4l2_subdev_call(sen_sd, video, g_frame_interval, interval);
 }
 
 static int mipi_csis_enum_framesizes(struct v4l2_subdev *mipi_sd,
@@ -1135,8 +1137,8 @@ static struct v4l2_subdev_video_ops mipi_csis_video_ops = {
 	.s_rx_buffer = mipi_csis_s_rx_buffer,
 	.s_stream = mipi_csis_s_stream,
 
-	.s_parm = mipi_csis_s_parm,
-	.g_parm = mipi_csis_g_parm,
+	.g_frame_interval = mipi_csis_g_frame_interval,
+	.s_frame_interval = mipi_csis_s_frame_interval,
 };
 
 static const struct v4l2_subdev_pad_ops mipi_csis_pad_ops = {
