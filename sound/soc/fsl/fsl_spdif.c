c93004c230f4429d6a61b0a7384b7ce05bee41bf
Shengjiu Wang <shengjiu.wang@nxp.com>
2019-11-22 15:46:20 +0800

    LF-106: ASoC: fsl_spdif: request BUS_FREQ_HIGH
    request BUS_FREQ_HIGH
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>

--- sound/soc/fsl/fsl_spdif.c
+++ sound/soc/fsl/fsl_spdif.c
@@ -15,6 +15,7 @@
 #include <linux/of.h>
 #include <linux/regmap.h>
 #include <linux/pm_runtime.h>
+#include <linux/busfreq-imx.h>
 
 #include <sound/asoundef.h>
 #include <sound/dmaengine_pcm.h>
@@ -1679,6 +1680,8 @@ static int fsl_spdif_runtime_suspend(struct device *dev)
 			&spdif_priv->regcache_srpc);
 	regcache_cache_only(spdif_priv->regmap, true);
 
+	release_bus_freq(BUS_FREQ_HIGH);
+
 	for (i = 0; i < STC_TXCLK_SRC_MAX; i++)
 		clk_disable_unprepare(spdif_priv->txclk[i]);
 
@@ -1714,6 +1717,8 @@ static int fsl_spdif_runtime_resume(struct device *dev)
 		if (ret)
 			goto disable_tx_clk;
 	}
+
+	request_bus_freq(BUS_FREQ_HIGH);
 
 	regcache_cache_only(spdif_priv->regmap, false);
 	regcache_mark_dirty(spdif_priv->regmap);
