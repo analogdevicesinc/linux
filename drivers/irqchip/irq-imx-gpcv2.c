199377930d35fdef9c2eb84439f0688191fa6f2f
Anson Huang <Anson.Huang@nxp.com>
2019-11-26 16:38:26 +0800

    LF-125 irqchip: irq-imx-gpcv2: Fix build error of nosmp
    Fix below build error when CONFIG_SMP is NOT selected:
    
    drivers/irqchip/irq-imx-gpcv2.c: In function "imx_gpcv2_wake_request_fixup":
    drivers/irqchip/irq-imx-gpcv2.c:123:2: error: implicit declaration of function
    "set_smp_cross_call" [-Werror=implicit-function-declaration]
      123 |  set_smp_cross_call(imx_gpcv2_raise_softirq);
          |  ^~~~~~~~~~~~~~~~~~
    cc1: some warnings being treated as errors
    make[3]: *** [drivers/irqchip/irq-imx-gpcv2.o] Error 1
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>
    Reviewed-by: Robin Gong <yibin.gong@nxp.com>

--- drivers/irqchip/irq-imx-gpcv2.c
+++ drivers/irqchip/irq-imx-gpcv2.c
@@ -85,20 +85,26 @@ static struct syscore_ops imx_gpcv2_syscore_ops = {
 	.resume		= gpcv2_wakeup_source_restore,
 };
 
+#ifdef CONFIG_ARM64
 static void (*__gic_v3_smp_cross_call)(const struct cpumask *, unsigned int);
+#endif
 
+#ifdef CONFIG_SMP
 static void imx_gpcv2_raise_softirq(const struct cpumask *mask,
 					  unsigned int irq)
 {
 	struct arm_smccc_res res;
 
+#ifdef CONFIG_ARM64
 	/* call the hijacked smp cross call handler */
 	__gic_v3_smp_cross_call(mask, irq);
+#endif
 
 	/* now call into EL3 and take care of the wakeup */
 	arm_smccc_smc(FSL_SIP_GPC, FSL_SIP_CONFIG_GPC_CORE_WAKE,
 			*cpumask_bits(mask), 0, 0, 0, 0, 0, &res);
 }
+#endif
 
 static void imx_gpcv2_wake_request_fixup(void)
 {
@@ -119,8 +125,10 @@ static void imx_gpcv2_wake_request_fixup(void)
 	__gic_v3_smp_cross_call = __smp_cross_call;
 #endif
 
+#ifdef CONFIG_SMP
 	/* register our workaround handler for smp cross call */
 	set_smp_cross_call(imx_gpcv2_raise_softirq);
+#endif
 
 	iomux_gpr = syscon_regmap_lookup_by_compatible("fsl,imx6q-iomuxc-gpr");
 	if (!IS_ERR(iomux_gpr))
