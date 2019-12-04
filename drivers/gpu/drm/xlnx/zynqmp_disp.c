Xilinx/linux-xlnx

commit c4b7d6c966afe784e090fa66d7a7b126c2273a0b

drm: xlnx: zynqmp_disp: Skip the modeset for same fb
Since the async update creates a separate atomic mode set, there can be
back to back atomic modeset with same fb. If the mode set is applied
twice, it ends up submitting 2 dma transactions, and it results in
jitter. This fixes it by adding the check in plane atomic update,
and using plane mode set in async update.

Signed-off-by: Hyun Kwon <hyun.kwon@xilinx.com>
Tested-by: Preetesh Parekh <preetesh.parekh@xilinx.com>

drivers/gpu/drm/xlnx/zynqmp_disp.c
@@ -2676,6 +2676,9 @@ zynqmp_disp_plane_atomic_update(struct drm_plane *plane,
 	if (!plane->state->crtc || !plane->state->fb)
 		return;
 
+	if (plane->state->fb == old_state->fb)
+		return;
+
 	if (old_state->fb &&
 	    old_state->fb->format->format != plane->state->fb->format->format)
 		zynqmp_disp_plane_disable(plane);
@@ -2712,11 +2715,14 @@ static void
 zynqmp_disp_plane_atomic_async_update(struct drm_plane *plane,
 				      struct drm_plane_state *new_state)
 {
-	struct drm_plane_state *old_state =
-		drm_atomic_get_old_plane_state(new_state->state, plane);
+	int ret;
 
 	if (plane->state->fb == new_state->fb)
 		return;
+
+	if (plane->state->fb &&
+	    plane->state->fb->format->format != new_state->fb->format->format)
+		zynqmp_disp_plane_disable(plane);
 
 	 /* Update the current state with new configurations */
 	drm_atomic_set_fb_for_plane(plane->state, new_state->fb);
@@ -2731,7 +2737,19 @@ zynqmp_disp_plane_atomic_async_update(struct drm_plane *plane,
 	plane->state->src_h = new_state->src_h;
 	plane->state->state = new_state->state;
 
-	zynqmp_disp_plane_atomic_update(plane, old_state);
+	ret = zynqmp_disp_plane_mode_set(plane, plane->state->fb,
+					 plane->state->crtc_x,
+					 plane->state->crtc_y,
+					 plane->state->crtc_w,
+					 plane->state->crtc_h,
+					 plane->state->src_x >> 16,
+					 plane->state->src_y >> 16,
+					 plane->state->src_w >> 16,
+					 plane->state->src_h >> 16);
+	if (ret)
+		return;
+
+	zynqmp_disp_plane_enable(plane);
 }
 
 static const struct drm_plane_helper_funcs zynqmp_disp_plane_helper_funcs = {

analogdevicesinc/linux

commit 751dedc87ad2bdd613a36b40710244525c80d6fb

drm: xlnx: zynqmp_disp: Remove fb check in atomic update callback
The setup in which we are seeing is this, is on ZynqMP (also ZCU102) with a
monitor connected via DisplayPort and the following Xorg.conf:
```
cat /etc/X11/xorg.conf
Section "Device"
  Identifier "myfb"
  Driver "fbdev"
  Option "fbdev" "/dev/fb0"
EndSection
```
It seems that checking the `fb` pointer disallows the new mode to be set,
and the screen stays blank when the OS boots into a desktop session.
The symptoms seem to be that, on boot the console is displayed, but when
the desktop server initializes a desktop session, the screen goes blank.

It does look like an old state could point to the same DRM framebuffer
struct/info, so it probably doesn't make sense to do that check.

It's also unsure whether this brings back the old issue that the original
patch tried to fix.

Fixes: c4b7d6c966afe ("drm: xlnx: zynqmp_disp: Skip the modeset for same fb")
Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

drivers/gpu/drm/xlnx/zynqmp_disp.c
@@ -2676,9 +2676,6 @@ zynqmp_disp_plane_atomic_update(struct drm_plane *plane,
 	if (!plane->state->crtc || !plane->state->fb)
 		return;
 
-	if (plane->state->fb == old_state->fb)
-		return;
-
 	if (old_state->fb &&
 	    old_state->fb->format->format != plane->state->fb->format->format)
 		zynqmp_disp_plane_disable(plane);