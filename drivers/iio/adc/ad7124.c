237a377beb7ba2d99495c9c5a143e7e9a58fb3a8
Alexandru Tachici <alexandru.tachici@analog.com>
2019-12-05 19:01:55 +0200

    iio: adc: ad7124: allow multiple-channels enabled
    This patch allows the user to enable multiple channels.
    ad_sigma_delta will not allow the user to enable multiple
    channels unless there are multiple sequencer slots available.
    The number of slots available is specified in the num_slots
    of the ad_sigma_delta struct.
    
    The ad7124 chip supports up to 16 channels enabled. When all
    channels are enabled, an internal sequencer will perform
    a conversion on each channel sequentially.
    
    Signed-off-by: Alexandru Tachici <alexandru.tachici@analog.com>

--- drivers/iio/adc/ad7124.c
+++ drivers/iio/adc/ad7124.c
@@ -22,6 +22,8 @@
 #include <linux/iio/adc/ad_sigma_delta.h>
 #include <linux/iio/sysfs.h>
 
+#define AD7124_SEQUENCER_SLOTS		8
+
 /* AD7124 registers */
 #define AD7124_COMMS			0x00
 #define AD7124_STATUS			0x00
@@ -886,6 +888,7 @@ static int ad7124_probe(struct spi_device *spi)
 	st->chip_info = info;
 
 	ad_sd_init(&st->sd, indio_dev, spi, &ad7124_sigma_delta_info);
+	st->sd.num_slots = AD7124_SEQUENCER_SLOTS;
 
 	indio_dev->name = st->chip_info->name;
 	indio_dev->modes = INDIO_DIRECT_MODE;
