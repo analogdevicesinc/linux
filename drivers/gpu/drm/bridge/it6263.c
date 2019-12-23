59bbf82998ca4f757092cb05abe567fab65b8872
Dong Aisheng <aisheng.dong@nxp.com>
2019-12-24 02:54:53 +0800

    LF-665-13: gpu: drm: bridge: it6263: update headfile due to API change
    API changes:
    4e98f871bcff ("drm: delete drmP.h + drm_os_linux.h")
    
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>

--- drivers/gpu/drm/bridge/it6263.c
+++ drivers/gpu/drm/bridge/it6263.c
@@ -12,7 +12,9 @@
  * for more details.
  */
 
-#include <drm/drmP.h>
+#include <drm/drm_vblank.h>
+#include <drm/drm_bridge.h>
+#include <drm/drm_drv.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_edid.h>
