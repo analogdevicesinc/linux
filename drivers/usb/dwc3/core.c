004eddb7b4c9ca4c57639795b77d0c176a3c5d80
Piyush Mehta <piyush.mehta@xilinx.com>
2020-01-23 19:51:17 +0530

    usb: dwc3: Add support for masking phy reset signal
    Versal usb controller drives phy reset signal during
    Soft Reset or Core Reset to issue reset to the ULPI
    phy. The ULPI phy needs to be resetted only at the time
    of initialization. This patch updates the code to provide
    a function for masking the phy reset signal from reaching
    the ULPI phy. This function is called only after the phy
    initialization is done.
    
    Signed-off-by: Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Piyush Mehta <piyush.mehta@xilinx.com>
    State: pending

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1020,6 +1020,9 @@ int dwc3_core_init(struct dwc3 *dwc)
 	if (ret)
 		goto err0a;
 
+	if (dwc->mask_phy_rst)
+		dwc3_mask_phy_reset(dwc->dev, TRUE);
+
 	dwc3_core_setup_global_control(dwc);
 	dwc3_core_num_eps(dwc);
 
@@ -1469,6 +1472,8 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 				"snps,enable_guctl1_ipd_quirk");
 	dwc->dis_metastability_quirk = device_property_read_bool(dev,
 				"snps,dis_metastability_quirk");
+	dwc->mask_phy_rst = device_property_read_bool(dev,
+				"snps,mask_phy_reset");
 
 	dwc->dis_split_quirk = device_property_read_bool(dev,
 				"snps,dis-split-quirk");
