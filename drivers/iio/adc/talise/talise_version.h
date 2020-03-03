b7a4fa4407b0166f6bb64cf14c3bf06d6a8cb68b
Alexandru Ardelean <alexandru.ardelean@analog.com>
2020-03-03 15:34:38 +0200

    iio: adc: adr9009,talise: add driver
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

---
+++ drivers/iio/adc/talise/talise_version.h
@@ -0,0 +1,24 @@
+/**
+ * \file talise_version.h
+ * \brief Contains the version number for the Talise API
+ *
+ * Talise API version: 3.6.0.5
+ */
+
+#ifndef _TALISEVERSION_H_
+#define _TALISEVERSION_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TAL_CURRENT_SI_VERSION 3
+#define TAL_CURRENT_MAJOR_VERSION 6
+#define TAL_CURRENT_MINOR_VERSION 0
+#define TAL_CURRENT_BUILD_VERSION 5
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
