2852d2e50013206cccda6f089c29687f116e1863
Cristian Pop <cristian.pop@analog.com>
2020-05-11 08:49:26 +0300

    iio: dac: ad5766: Add span support.
    This will allow to select between eight available output ranges based on
    offset and scale attributes.
    
    Signed-off-by: Cristian Pop <cristian.pop@analog.com>
    (cherry picked from commit cce1f63cddaea8d5a0c6db92e83e76872a0444a6)

--- drivers/iio/dac/ad5766.c
+++ drivers/iio/dac/ad5766.c
@@ -36,21 +36,23 @@ enum ad5766_type {
 	ID_AD5767,
 };
 
-#define AD576x_CHANNEL(_chan, _bits) {				\
-	.type = IIO_VOLTAGE,					\
-	.indexed = 1,						\
-	.output = 1,						\
-	.channel = (_chan),					\
-	.address = (_chan),					\
-	.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),		\
-	.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_OFFSET) |	\
-		BIT(IIO_CHAN_INFO_SCALE),			\
-	.scan_type = {						\
-		.sign = 'u',					\
-		.realbits = (_bits),				\
-		.storagebits = 16,				\
-		.shift = 16 - (_bits),				\
-	},							\
+#define AD576x_CHANNEL(_chan, _bits) {					\
+	.type = IIO_VOLTAGE,						\
+	.indexed = 1,							\
+	.output = 1,							\
+	.channel = (_chan),						\
+	.address = (_chan),						\
+	.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),			\
+	.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_OFFSET) |		\
+		BIT(IIO_CHAN_INFO_SCALE),				\
+	.info_mask_shared_by_type_available =				\
+		BIT(IIO_CHAN_INFO_OFFSET) | BIT(IIO_CHAN_INFO_SCALE),	\
+	.scan_type = {							\
+		.sign = 'u',						\
+		.realbits = (_bits),					\
+		.storagebits = 16,					\
+		.shift = 16 - (_bits),					\
+	},								\
 }
 
 #define DECLARE_AD576x_CHANNELS(_name, _bits)			\
@@ -73,6 +75,18 @@ const struct iio_chan_spec _name[] = {				\
 	AD576x_CHANNEL(15, (_bits)),				\
 }
 
+enum ad5766_voltage_range {
+	AD5766_VOLTAGE_RANGE_M20V_0V,
+	AD5766_VOLTAGE_RANGE_M16V_to_0V,
+	AD5766_VOLTAGE_RANGE_M10V_to_0V,
+	AD5766_VOLTAGE_RANGE_M12V_to_14V,
+	AD5766_VOLTAGE_RANGE_M16V_to_10V,
+	AD5766_VOLTAGE_RANGE_M10V_to_6V,
+	AD5766_VOLTAGE_RANGE_M5V_to_5V,
+	AD5766_VOLTAGE_RANGE_M10V_to_10V,
+	AD5766_VOLTAGE_RANGE_MAX,
+};
+
 /**
  * struct ad5766_chip_info - chip specific information
  * @num_channels:	number of channels
@@ -90,6 +104,10 @@ struct ad5766_chip_info {
  * @lock:		Mutex lock
  * @chip_info:		Chip model specific constants
  * @gpio_reset:		Reset gpio
+ * @crt_range:		Current selected output range
+ * @cached_offset:	Cached range coresponding to the selected offset
+ * @scale_avail:	Scale available table
+ * @offset_avail:	Offest available table
  * @data:		Spi transfer buffers
  */
 
@@ -98,11 +116,55 @@ struct ad5766_state {
 	struct mutex			lock;
 	const struct ad5766_chip_info 	*chip_info;
 	struct gpio_desc		*gpio_reset;
+	enum ad5766_voltage_range	crt_range;
+	enum ad5766_voltage_range	cached_offset;
+	s32		scale_avail[AD5766_VOLTAGE_RANGE_MAX][2];
+	s32		offset_avail[AD5766_VOLTAGE_RANGE_MAX][2];
 	union {
 		u32	d32;
 		u16	w16[2];
 		u8	b8[4];
 	} data[3] ____cacheline_aligned;
+};
+
+struct ad5766_span_tbl {
+	int		min;
+	int		max;
+};
+
+static const struct ad5766_span_tbl ad5766_span_tbl[] = {
+	[AD5766_VOLTAGE_RANGE_M20V_0V] = {
+		.min = -20,
+		.max = 0,
+	},
+	[AD5766_VOLTAGE_RANGE_M16V_to_0V] = {
+		.min = -16,
+		.max = 0,
+	},
+	[AD5766_VOLTAGE_RANGE_M10V_to_0V] = {
+		.min = -10,
+		.max = 0,
+	},
+	[AD5766_VOLTAGE_RANGE_M12V_to_14V] = {
+		.min = -12,
+		.max = 14,
+	},
+	[AD5766_VOLTAGE_RANGE_M16V_to_10V] = {
+		.min = -16,
+		.max = 10,
+	},
+	[AD5766_VOLTAGE_RANGE_M10V_to_6V] = {
+		.min = -10,
+		.max = 6,
+	},
+	[AD5766_VOLTAGE_RANGE_M5V_to_5V] = {
+		.min = -5,
+		.max = 5,
+	},
+	[AD5766_VOLTAGE_RANGE_M10V_to_10V] = {
+		.min = -10,
+		.max = 10,
+	},
 };
 
 static int ad5766_write_raw(struct iio_dev *indio_dev,
@@ -117,9 +179,15 @@ static int ad5766_read_raw(struct iio_dev *indio_dev,
 			   int *val2,
 			   long m);
 
+static int ad5766_read_avail(struct iio_dev *indio_dev,
+				struct iio_chan_spec const *chan,
+				const int **vals, int *type, int *length,
+				long mask);
+
 static const struct iio_info ad5766_info = {
 	.read_raw = ad5766_read_raw,
 	.write_raw = ad5766_write_raw,
+	.read_avail = ad5766_read_avail,
 };
 
 static DECLARE_AD576x_CHANNELS(ad5766_channels, 16);
@@ -135,12 +203,6 @@ static const struct ad5766_chip_info ad5766_chip_infos[] = {
 		.channels = ad5767_channels,
 	},
 };
-
-static void _ad5766_get_span_range(int *min, int *max)
-{
-	*min = -5;
-	*max = 5;
-}
 
 static int _ad5766_spi_write(struct ad5766_state *st,
 			     u8 command,
@@ -163,6 +225,28 @@ static int ad5766_write(struct iio_dev *indio_dev, u8 dac, u16 data)
 	mutex_unlock(&st->lock);
 
 	return ret;
+}
+
+static void ad5766_init_scale_tables(struct ad5766_state *st)
+{
+	int i;
+	s32 denom;
+	s64 offset;
+	u64 scale;
+	u8 realbits = st->chip_info->channels[0].scan_type.realbits;
+
+	for (i = 0; i < AD5766_VOLTAGE_RANGE_MAX; i++) {
+		offset = (1 << realbits) * ad5766_span_tbl[i].min;
+		denom = ad5766_span_tbl[i].max - ad5766_span_tbl[i].min;
+		offset = div_s64(offset * 1000000, denom);
+		st->offset_avail[i][0] = div_s64(offset, 1000000);
+		div_s64_rem(offset, 1000000, &st->offset_avail[i][1]);
+
+		scale = ad5766_span_tbl[i].max - ad5766_span_tbl[i].min;
+		scale = div_u64((scale * 1000000000), (1 << realbits));
+		st->scale_avail[i][0] = (int)div_u64(scale, 1000000);
+		div_s64_rem(scale, 1000000, &st->scale_avail[i][1]);
+	}
 }
 
 static int ad5766_reset(struct ad5766_state *st)
@@ -189,12 +273,69 @@ static int ad5766_reset(struct ad5766_state *st)
 	return 0;
 }
 
+static int ad5766_default_setup(struct ad5766_state *st,
+	enum ad5766_voltage_range range)
+{
+	int ret;
+
+	/* Always issue a software reset before writing to the span register. */
+	ret = ad5766_reset(st);
+	if (ret)
+		return ret;
+
+	ret = _ad5766_spi_write(st, AD5766_CMD_SPAN_REG, range);
+	if (ret)
+		return ret;
+
+	st->crt_range = range;
+	st->cached_offset = range;
+
+	return 0;
+}
+
+static int ad5766_set_offset(struct ad5766_state *st, int val, int val2)
+{
+	int i;
+	s32 (*tbl)[AD5766_VOLTAGE_RANGE_MAX][2] = &(st->offset_avail);
+
+	for (i = 0; i < AD5766_VOLTAGE_RANGE_MAX; i++) {
+		if ((*tbl)[i][0] == val && (*tbl)[i][1] == val2) {
+			st->cached_offset = i;
+			return 0;
+		}
+	}
+
+	return -EINVAL;
+}
+
+static int ad5766_set_scale(struct ad5766_state *st, int val, int val2)
+{
+	int i;
+	enum ad5766_voltage_range offset_idx = st->cached_offset;
+	s32 (*offset_tbl)[AD5766_VOLTAGE_RANGE_MAX][2] = &(st->offset_avail);
+	s32 (*scale_tbl)[AD5766_VOLTAGE_RANGE_MAX][2] = &(st->scale_avail);
+
+	for (i = 0; i < AD5766_VOLTAGE_RANGE_MAX; i++) {
+		if ((*scale_tbl)[i][0] != val || (*scale_tbl)[i][1] != val2)
+			continue;
+
+		if ((*offset_tbl)[i][0] != (*offset_tbl)[offset_idx][0] ||
+			(*offset_tbl)[i][1] != (*offset_tbl)[offset_idx][1])
+			continue;
+
+		return ad5766_default_setup(st, i);
+	}
+
+	return -EINVAL;
+}
+
 static int ad5766_write_raw(struct iio_dev *indio_dev,
 			    struct iio_chan_spec const *chan,
 			    int val,
 			    int val2,
 			    long info)
 {
+	struct ad5766_state *st = iio_priv(indio_dev);
 	const int max_val = (1 << chan->scan_type.realbits);
 
 	switch (info) {
@@ -202,12 +343,14 @@ static int ad5766_write_raw(struct iio_dev *indio_dev,
 		if (val >= max_val || val < 0)
 			return -EINVAL;
 		val <<= chan->scan_type.shift;
-		break;
+		return ad5766_write(indio_dev, chan->address, val);
+	case IIO_CHAN_INFO_OFFSET:
+		return ad5766_set_offset(st, val, val2);
+	case IIO_CHAN_INFO_SCALE:
+		return ad5766_set_scale(st, val, val2);
 	default:
 		return -EINVAL;
 	}
-
-	return ad5766_write(indio_dev, chan->address, val);
 }
 
 static int _ad5766_spi_read(struct ad5766_state *st, u8 dac, int *val)
@@ -252,13 +395,42 @@ static int ad5766_read(struct iio_dev *indio_dev, u8 dac, int *val)
 	return ret;
 }
 
+
+static int ad5766_read_avail(struct iio_dev *indio_dev,
+				 struct iio_chan_spec const *chan,
+				 const int **vals, int *type, int *length,
+				 long mask)
+{
+	struct ad5766_state *st = iio_priv(indio_dev);
+
+	switch (mask) {
+	case IIO_CHAN_INFO_SCALE:
+		*vals = (int *)st->scale_avail;
+		*type = IIO_VAL_INT_PLUS_MICRO;
+		/* Values are stored in a 2D matrix  */
+		*length = AD5766_VOLTAGE_RANGE_MAX * 2;
+
+		return IIO_AVAIL_LIST;
+	case IIO_CHAN_INFO_OFFSET:
+		*vals = (int *)st->offset_avail;
+		*type = IIO_VAL_INT_PLUS_MICRO;
+		/* Values are stored in a 2D matrix  */
+		*length = AD5766_VOLTAGE_RANGE_MAX * 2;
+
+		return IIO_AVAIL_LIST;
+	default:
+		return -EINVAL;
+	}
+}
+
 static int ad5766_read_raw(struct iio_dev *indio_dev,
 			   struct iio_chan_spec const *chan,
 			   int *val,
 			   int *val2,
 			   long m)
 {
-	int ret, min, max;
+	int ret;
+	struct ad5766_state *st = iio_priv(indio_dev);
 
 	switch (m) {
 	case IIO_CHAN_INFO_RAW:
@@ -269,30 +441,19 @@ static int ad5766_read_raw(struct iio_dev *indio_dev,
 		return IIO_VAL_INT;
 
 	case IIO_CHAN_INFO_SCALE:
-		_ad5766_get_span_range(&min, &max);
-		*val = max - min;
-		*val2 = indio_dev->channels->scan_type.realbits;
-		
-		return IIO_VAL_FRACTIONAL_LOG2;
+		*val = st->scale_avail[st->crt_range][0];
+		*val2 = st->scale_avail[st->crt_range][1];
+
+		return IIO_VAL_INT_PLUS_MICRO;
 
 	case IIO_CHAN_INFO_OFFSET:
-		_ad5766_get_span_range(&min, &max);
-		*val = min;
-		
-		return IIO_VAL_INT;
+		*val = st->offset_avail[st->crt_range][0];
+		*val2 = st->offset_avail[st->crt_range][1];
+
+		return IIO_VAL_INT_PLUS_MICRO;
+	default:
+		return -EINVAL;
 	}
-	return -EINVAL;
-}
-
-static int ad5766_setup(struct ad5766_state *st)
-{
-	int ret;
-
-	ret = ad5766_reset(st);
-	if (ret < 0)
-		return ret;
-
-	return 0;
 }
 
 static int ad5766_probe(struct spi_device *spi)
@@ -323,9 +484,11 @@ static int ad5766_probe(struct spi_device *spi)
 	indio_dev->modes = INDIO_DIRECT_MODE;
 
 	st->gpio_reset = devm_gpiod_get_optional(&st->spi->dev, "reset",
-							GPIOD_OUT_HIGH);
+						GPIOD_OUT_HIGH);
 
-	ret = ad5766_setup(st);
+	ad5766_init_scale_tables(st);
+
+	ret = ad5766_default_setup(st, AD5766_VOLTAGE_RANGE_M5V_to_5V);
 	if (ret)
 		return ret;
 
