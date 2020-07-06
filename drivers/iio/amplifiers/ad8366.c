d73bbe777b5ae7421a823a6968104518fd69a153
Alexandru Ardelean <alexandru.ardelean@analog.com>
2020-07-06 11:58:17 +0300

    iio: amplifiers: ad8366: add support for HMC271
    The part is obsolete. Won't be upstreamed.
    But we still maintain it in the ADI tree.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- drivers/iio/amplifiers/ad8366.c
+++ drivers/iio/amplifiers/ad8366.c
@@ -5,6 +5,7 @@
  *   AD8366 Dual-Digital Variable Gain Amplifier (VGA)
  *   ADA4961 BiCMOS RF Digital Gain Amplifier (DGA)
  *   ADL5240 Digitally controlled variable gain amplifier (VGA)
+ *   HMC271 1 dB LSB GaAs MMIC 5-BIT Control Digital Attenuator
  *   HMC1119 0.25 dB LSB, 7-Bit, Silicon Digital Attenuator
  *
  * Copyright 2012-2019 Analog Devices Inc.
@@ -28,6 +29,7 @@ enum ad8366_type {
 	ID_AD8366,
 	ID_ADA4961,
 	ID_ADL5240,
+	ID_HMC271,
 	ID_HMC1119,
 };
 
@@ -64,6 +66,10 @@ static struct ad8366_info ad8366_infos[] = {
 		.gain_min = -11500,
 		.gain_max = 20000,
 	},
+	[ID_HMC271] = {
+		.gain_min = -31000,
+		.gain_max = 0,
+	},
 	[ID_HMC1119] = {
 		.gain_min = -31750,
 		.gain_max = 0,
@@ -89,6 +95,9 @@ static int ad8366_write(struct iio_dev *indio_dev,
 		break;
 	case ID_ADL5240:
 		st->data[0] = (ch_a & 0x3F);
+		break;
+	case ID_HMC271:
+		st->data[0] = bitrev8(ch_a & 0x1F) >> 3;
 		break;
 	case ID_HMC1119:
 		st->data[0] = ch_a;
@@ -126,6 +135,9 @@ static int ad8366_read_raw(struct iio_dev *indio_dev,
 			break;
 		case ID_ADL5240:
 			gain = 20000 - 31500 + code * 500;
+			break;
+		case ID_HMC271:
+			gain = -31000 + code * 1000;
 			break;
 		case ID_HMC1119:
 			gain = -1 * code * 250;
@@ -175,6 +187,9 @@ static int ad8366_write_raw(struct iio_dev *indio_dev,
 		break;
 	case ID_ADL5240:
 		code = ((gain - 500 - 20000) / 500) & 0x3F;
+		break;
+	case ID_HMC271:
+		code = ((gain - 1000) / 1000) & 0x1F;
 		break;
 	case ID_HMC1119:
 		code = (abs(gain) / 250) & 0x7F;
@@ -261,6 +276,7 @@ static int ad8366_probe(struct spi_device *spi)
 		break;
 	case ID_ADA4961:
 	case ID_ADL5240:
+	case ID_HMC271:
 	case ID_HMC1119:
 		st->reset_gpio = devm_gpiod_get_optional(&spi->dev, "reset", GPIOD_OUT_HIGH);
 		if (IS_ERR(st->reset_gpio)) {
@@ -316,6 +332,7 @@ static const struct spi_device_id ad8366_id[] = {
 	{"ad8366",  ID_AD8366},
 	{"ada4961", ID_ADA4961},
 	{"adl5240", ID_ADL5240},
+	{"hmc271",  ID_HMC271},
 	{"hmc1119", ID_HMC1119},
 	{}
 };
