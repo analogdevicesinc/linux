4059954e45468bbee45afc2d778232ac840a560b
Dragos Bogdan <dragos.bogdan@analog.com>
2020-07-20 12:53:20 +0300

    [media] adv7511: add support for async probing
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- drivers/media/i2c/adv7511-v4l2.c
+++ drivers/media/i2c/adv7511-v4l2.c
@@ -2417,6 +2417,11 @@ static int adv7511_probe(struct i2c_client *client, const struct i2c_device_id *
 #endif
 	v4l2_info(sd, "%s found @ 0x%x (%s)\n", client->name,
 			  client->addr << 1, client->adapter->name);
+
+	err = v4l2_async_register_subdev(sd);
+	if (err)
+		goto err_unreg_pktmem;
+
 	return 0;
 
 err_unreg_pktmem:
@@ -2451,6 +2456,7 @@ static int adv7511_remove(struct i2c_client *client)
 	i2c_unregister_device(state->i2c_cec);
 	i2c_unregister_device(state->i2c_pktmem);
 	destroy_workqueue(state->work_queue);
+	v4l2_async_unregister_subdev(sd);
 	v4l2_device_unregister_subdev(sd);
 	media_entity_cleanup(&sd->entity);
 	v4l2_ctrl_handler_free(sd->ctrl_handler);
