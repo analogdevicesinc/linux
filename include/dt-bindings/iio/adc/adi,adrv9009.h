97c86d3fe7d89fae98de0c5bef6b0edb01d33355
Michael Hennerich <michael.hennerich@analog.com>
2020-07-21 14:39:38 +0300

    dt-bindings: adrv9009: add bindings constants for driver
    These will be used in device-trees and the 9009 driver.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>"
    (cherry picked from commit 0a2ffb3b8742d856017a5538897800d09e3a156e)

---
+++ include/dt-bindings/iio/adc/adi,adrv9009.h
@@ -0,0 +1,12 @@
+/*
+ * This header provides constants for ADRV9009
+ */
+
+#ifndef _DT_BINDINGS_IIO_ADC_ADRV9009_H
+#define _DT_BINDINGS_IIO_ADC_ADRV9009_H
+
+#define DEFRAMER_LINK_TX	0
+#define FRAMER_LINK_RX		1
+#define FRAMER_LINK_ORX		2
+
+#endif
