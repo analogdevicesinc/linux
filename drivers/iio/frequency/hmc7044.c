d7280b3bfe50d0fa06d828cd60c52ecd82de1193
Michael Hennerich <michael.hennerich@analog.com>
2020-07-30 09:42:16 +0200

    iio: frequency: hmc7044: Add option to limit max SYSREF frequency
    Often it's not desired to use directly the LMFC LMEC frequency
    due to frequency spurs they may introduce.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    (cherry picked from commit 350c80726ffabce1fa561905af74f5dc13698d1a)

--- drivers/iio/frequency/hmc7044.c
+++ drivers/iio/frequency/hmc7044.c
@@ -318,6 +318,7 @@ struct hmc7044 {
 	struct jesd204_dev 		*jdev;
 	u32				jdev_lmfc_lemc_rate;
 	u32				jdev_lmfc_lemc_gcd;
+	u32				jdev_max_sysref_freq;
 	bool				is_sysref_provider;
 	bool				hmc_two_level_tree_sync_en;
 };
@@ -1292,6 +1293,10 @@ static int hmc7044_parse_dt(struct device *dev,
 
 	hmc->hmc_two_level_tree_sync_en = of_property_read_bool(np, "adi,hmc-two-level-tree-sync-en");
 
+	hmc->jdev_max_sysref_freq = INT_MAX;
+	of_property_read_u32(np, "adi,jesd204-max-sysref-frequency-hz",
+			     &hmc->jdev_max_sysref_freq);
+
 	hmc->rf_reseeder_en =
 		!of_property_read_bool(np, "adi,rf-reseeder-disable");
 
@@ -1664,6 +1669,9 @@ static int hmc7044_jesd204_link_pre_setup(struct jesd204_dev *jdev,
 		return JESD204_STATE_CHANGE_DONE;
 
 	dev_dbg(dev, "%s:%d link_num %u\n", __func__, __LINE__, lnk->link_id);
+
+	while (hmc->jdev_lmfc_lemc_gcd > hmc->jdev_max_sysref_freq)
+		hmc->jdev_lmfc_lemc_gcd >>= 1;
 
 	/* Program the output channels */
 	for (i = 0; i < hmc->num_channels; i++) {
