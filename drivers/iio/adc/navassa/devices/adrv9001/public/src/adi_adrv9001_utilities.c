d71140e9b87220772cd6306215c9a36b2ccb8959
Nuno Sá <nuno.sa@analog.com>
2020-10-06 16:44:57 +0200

    iio: adrv9002: api: Fix RX gain table load API
    Do not compare the previous gain index on the first loop iteration
    since there's still nothing to compare.
    
    Signed-off-by: Nuno Sá <nuno.sa@analog.com>
    (cherry picked from commit f01517f7bbd3206ec16552f388eaf756ba036f8f)

--- drivers/iio/adc/navassa/devices/adrv9001/public/src/adi_adrv9001_utilities.c
+++ drivers/iio/adc/navassa/devices/adrv9001/public/src/adi_adrv9001_utilities.c
@@ -155,7 +155,7 @@ int32_t adi_adrv9001_Utilities_ArmImage_Load(adi_adrv9001_Device_t *device, cons
     return recoveryAction;
 }
 
-int32_t adi_adrv9001_Utilities_StreamImage_Load(adi_adrv9001_Device_t *device, const char *streamImagePath) 
+int32_t adi_adrv9001_Utilities_StreamImage_Load(adi_adrv9001_Device_t *device, const char *streamImagePath)
 {
 
     int32_t recoveryAction = ADI_COMMON_ACT_NO_ACTION;
@@ -196,7 +196,7 @@ int32_t adi_adrv9001_Utilities_StreamImage_Load(adi_adrv9001_Device_t *device, c
     return recoveryAction;
 }
 
-int32_t adi_adrv9001_Utilities_RxGainTable_Load(adi_adrv9001_Device_t *device, const char *rxGainTablePath, uint32_t rxChannelMask) 
+int32_t adi_adrv9001_Utilities_RxGainTable_Load(adi_adrv9001_Device_t *device, const char *rxGainTablePath, uint32_t rxChannelMask)
 {
     uint8_t maxGainIndex = 0;
     uint8_t prevGainIndex = 0;
@@ -208,7 +208,6 @@ int32_t adi_adrv9001_Utilities_RxGainTable_Load(adi_adrv9001_Device_t *device, c
     static adi_adrv9001_RxGainTableRow_t rxGainTableRowBuffer[ADI_ADRV9001_RX_GAIN_TABLE_SIZE_ROWS];
     static const uint8_t NUM_COLUMNS = 7;
 
-    static const uint8_t MIN_GAIN_INDEX_RX = 183;
     static const uint8_t MIN_GAIN_INDEX_ORX = 2;
     static const uint8_t MAX_GAIN_INDEX_ORX = 14;
 
@@ -218,7 +217,7 @@ int32_t adi_adrv9001_Utilities_RxGainTable_Load(adi_adrv9001_Device_t *device, c
     ADI_ENTRY_PTR_EXPECT(device, rxGainTablePath);
 
     /*Loop until the gain table end is reached or no. of lines scanned exceeds maximum*/
-    while (lineCount <  ADI_ADRV9001_RX_GAIN_TABLE_SIZE_ROWS) 
+    while (lineCount <  ADI_ADRV9001_RX_GAIN_TABLE_SIZE_ROWS)
     {
         returnTableEntry = adi_hal_RxGainTableEntryGet(device->common.devHalInfo,
                                                        rxGainTablePath,
@@ -242,7 +241,7 @@ int32_t adi_adrv9001_Utilities_RxGainTable_Load(adi_adrv9001_Device_t *device, c
 
         rxGainTableRowBuffer[lineCount].adcTiaGain = ((adcControl << 1) | tiaControl);
 
-        if (gainIndex < MIN_GAIN_INDEX_RX)
+        if (gainIndex < ADI_ADRV9001_RX_GAIN_INDEX_MIN)
         {
             if ((gainIndex < MIN_GAIN_INDEX_ORX) ||
                 (gainIndex > MAX_GAIN_INDEX_ORX))
@@ -251,15 +250,18 @@ int32_t adi_adrv9001_Utilities_RxGainTable_Load(adi_adrv9001_Device_t *device, c
             }
         }
 
-        /*Check that gain indices are arranged in ascending order*/
-        if (prevGainIndex != (gainIndex - 1))
+        if (lineCount != 0)
         {
-            ADI_ERROR_REPORT(&device->common,
-                             ADI_COMMON_ERRSRC_API,
-                             ADI_COMMON_ERR_INV_PARAM,
-                             ADI_COMMON_ACT_ERR_CHECK_PARAM,
-                             gainIndex,
-                             "Gain indices not arranged in ascending order in Rx Gain Table file");
+            /*Check that gain indices are arranged in ascending order*/
+            if (prevGainIndex != (gainIndex - 1))
+            {
+                ADI_ERROR_REPORT(&device->common,
+                                 ADI_COMMON_ERRSRC_API,
+                                 ADI_COMMON_ERR_INV_PARAM,
+                                 ADI_COMMON_ACT_ERR_CHECK_PARAM,
+                                 gainIndex,
+                                 "Gain indices not arranged in ascending order in Rx Gain Table file");
+            }
         }
 
         prevGainIndex = gainIndex;
@@ -268,7 +270,7 @@ int32_t adi_adrv9001_Utilities_RxGainTable_Load(adi_adrv9001_Device_t *device, c
 
     maxGainIndex = prevGainIndex;
     ADI_EXPECT(adi_adrv9001_Rx_GainTable_Write, device, rxChannelMask, maxGainIndex, &rxGainTableRowBuffer[0], lineCount);
-    
+
     ADI_API_RETURN(device);
 }
 
@@ -289,7 +291,7 @@ int32_t adi_adrv9001_Utilities_TxAttenTable_Load(adi_adrv9001_Device_t *device, 
     ADI_ENTRY_PTR_EXPECT(device, txAttenTablePath);
 
     /*Loop until the atten table end is reached or no. of lines scanned exceeds maximum*/
-    while (lineCount < ADRV9001_TX_ATTEN_TABLE_MAX) 
+    while (lineCount < ADRV9001_TX_ATTEN_TABLE_MAX)
     {
         returnTableEntry = adi_hal_TxAttenTableEntryGet(device->common.devHalInfo,
                                                         txAttenTablePath,
@@ -338,6 +340,6 @@ int32_t adi_adrv9001_Utilities_TxAttenTable_Load(adi_adrv9001_Device_t *device, 
     tableSize = attenIndex - minAttenIndex + 1;
 
     ADI_EXPECT(adi_adrv9001_Tx_AttenuationTable_Write, device, txChannelMask, minAttenIndex, &txAttenTableRowBuffer[0], tableSize);
-    
+
     ADI_API_RETURN(device);
 }
