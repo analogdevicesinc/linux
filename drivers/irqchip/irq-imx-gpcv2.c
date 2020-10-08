11f4b7386f4859fbd47f922a06cf0959b3bbbac9
Abel Vesa <abel.vesa@nxp.com>
2020-10-08 13:41:23 +0300

    LF-2438 drivers: irqchip: irq-imx-gpcv2: Remove the smp_cross_call reference
    Since commit 5cebfd2d47c214 "arm64: Kill __smp_cross_call and co",
    the workaround for ERR11171 for i.MX8MQ needs a different approach.
    Remove all the smp_cross_call references and make sure the workaround
    is happening only on 8MQ and if the TF-A supports it.
    
    Signed-off-by: Abel Vesa <abel.vesa@nxp.com>

--- drivers/irqchip/irq-imx-gpcv2.c
+++ drivers/irqchip/irq-imx-gpcv2.c
@@ -87,20 +87,14 @@ static struct syscore_ops imx_gpcv2_syscore_ops = {
 	.resume		= gpcv2_wakeup_source_restore,
 };
 
-#ifdef CONFIG_ARM64
-static void (*__gic_v3_smp_cross_call)(const struct cpumask *, unsigned int);
-#endif
-
 #ifdef CONFIG_SMP
-static void imx_gpcv2_raise_softirq(const struct cpumask *mask,
+void imx_gpcv2_raise_softirq(const struct cpumask *mask,
 					  unsigned int irq)
 {
 	struct arm_smccc_res res;
 
-#ifdef CONFIG_ARM64
-	/* call the hijacked smp cross call handler */
-	__gic_v3_smp_cross_call(mask, irq);
-#endif
+	if (!err11171)
+		return;
 
 	/* now call into EL3 and take care of the wakeup */
 	arm_smccc_smc(FSL_SIP_GPC, FSL_SIP_CONFIG_GPC_CORE_WAKE,
@@ -118,19 +112,10 @@ static void imx_gpcv2_wake_request_fixup(void)
 
 	if (res.a0) {
 		pr_warn("irq-imx-gpcv2: EL3 does not support FSL_SIP_CONFIG_GPC_CORE_WAKE, disabling cpuidle.\n");
+		err11171 = false;
 		disable_cpuidle();
 		return;
 	}
-
-#ifdef CONFIG_ARM64
-	/* hijack the already registered smp cross call handler */
-	__gic_v3_smp_cross_call = __smp_cross_call;
-#endif
-
-#ifdef CONFIG_SMP
-	/* register our workaround handler for smp cross call */
-	set_smp_cross_call(imx_gpcv2_raise_softirq);
-#endif
 
 	iomux_gpr = syscon_regmap_lookup_by_compatible("fsl,imx6q-iomuxc-gpr");
 	if (!IS_ERR(iomux_gpr))
