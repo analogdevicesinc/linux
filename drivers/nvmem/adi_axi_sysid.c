6cfcafbcd3d87483985a6bda8062a9346dda7fdd
sarpadi <sergiu.arpadi@analog.com>
2020-10-21 00:57:09 +0300

    nvmem: adi_axi_sysid: Add git branch field
    Sysid memory now contains a git branch field which needs to be taken
    into account by the driver; driver has different validate functions
    depending on version of core and header
    
    Signed-off-by: Sergiu Arpadi <sergiu.arpadi@analog.com>

--- drivers/nvmem/adi_axi_sysid.c
+++ drivers/nvmem/adi_axi_sysid.c
@@ -14,11 +14,14 @@
 #include <linux/nvmem-provider.h>
 #include <linux/fpga/adi-axi-common.h>
 
-#define AXI_SYSID_WORD_SIZE	sizeof(u32)
-
-
+#define AXI_SYSID_WORD_SIZE	 	sizeof(u32)
 #define AXI_SYSID_REG_ROM_ADDR_WIDTH	0x40
 #define AXI_SYSID_ROM_OFFSET		0x800
+
+enum {
+	AXI_SYSID_HEADER_V1,
+	AXI_SYSID_HEADER_V2,
+};
 
 struct axi_sysid {
 	void __iomem *base;
@@ -38,8 +41,17 @@ struct sysid_header_v1 {
 	u32 crc;
 } __packed;
 
-
 struct build_info_header_v1 {
+	s8 git_hash[44];
+	s8 git_clean_chk[4];
+	s8 vadj_chk[4];
+	s8 epoch[12];
+	u8 padding[4];
+	u32 crc;
+} __packed;
+
+struct build_info_header_v1_1 {
+	s8 git_branch[28];
 	s8 git_hash[44];
 	s8 git_clean_chk[4];
 	s8 vadj_chk[4];
@@ -99,38 +111,59 @@ static char *axi_sysid_get_str(struct axi_sysid *st, int this)
 	return NULL;
 }
 
-static int axi_sysid_validate(struct platform_device *pdev,
-			      struct axi_sysid *st)
+static int axi_sysid_validate_v1_1(struct platform_device *pdev,
+				   struct axi_sysid *st,
+				   struct build_info_header_v1_1 *build)
 {
 	struct sysid_header_v1 *header;
-	struct build_info_header_v1 *build;
 	char custom_info[48];
 	struct tm tm;
 	time64_t t;
 	int ret;
 
-	st->mem = devm_kzalloc(&pdev->dev, st->size, GFP_KERNEL);
-	if (!st->mem)
-		return -ENOMEM;
+	if (axi_sysid_checksum((u8 *)build, sizeof(struct build_info_header_v1_1))) {
+		dev_err(&pdev->dev, "verfify build header checksum failed\n");
+		return -EFAULT;
+	}
 
-	axi_sysid_read(st, 0, st->mem, st->size);
 	header = (struct sysid_header_v1 *) st->mem;
 
-	if (axi_sysid_checksum((u8 *)st->mem, sizeof(struct sysid_header_v1))) {
-		dev_err(&pdev->dev, "verfify header checksum failed\n");
-		return -EFAULT;
-	}
+	ret = sscanf(build->epoch, "%12lld", &t);
+	if (ret != 1)
+		return -EINVAL;
 
-	if (header->version != st->info->header_version) {
-		dev_err(&pdev->dev,
-			"system ID header version mismatch. Expected %d, Reported %d\n",
-			st->info->header_version, header->version);
-		return -EFAULT;
-	}
+	time64_to_tm(t, 0, &tm);
 
-	build = (struct build_info_header_v1 *) &st->mem[
-		header->build_info_offs];
+	if (axi_sysid_get_str(st, header->custom_info_offs))
+		sprintf(custom_info, "%s%s%s", " [", axi_sysid_get_str(st, header->custom_info_offs), "]");
+	else
+		custom_info[0] = 0;
 
+	dev_info(&pdev->dev,
+		"[%s]%s on [%s] git branch <%.28s> git <%.40s> %s [%ld-%02d-%02d %02d:%02d:%02d] UTC\n",
+		axi_sysid_get_str(st, header->board_info_offs),
+		custom_info,
+		axi_sysid_get_str(st, header->product_info_offs),
+		build->git_branch,
+		build->git_hash,
+		(build->git_clean_chk[0] == 't') ? "clean" : "dirty",
+		tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
+		tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+	return 0;
+}
+
+static int axi_sysid_validate_v1(struct platform_device *pdev,
+				 struct axi_sysid *st,
+				 struct build_info_header_v1 *build)
+{
+	struct sysid_header_v1 *header;
+	char custom_info[48];
+	struct tm tm;
+	time64_t t;
+	int ret;
+
+	header = (struct sysid_header_v1 *) st->mem;
 	if (axi_sysid_checksum((u8 *)build,
 		sizeof(struct build_info_header_v1))) {
 		dev_err(&pdev->dev, "verfify build header checksum failed\n");
@@ -161,6 +194,49 @@ static int axi_sysid_validate(struct platform_device *pdev,
 	return 0;
 }
 
+static int axi_sysid_validate(struct platform_device *pdev,
+			      struct axi_sysid *st)
+{
+	struct build_info_header_v1_1 *build_v1_1;
+	struct build_info_header_v1 *build_v1;
+	struct sysid_header_v1 *header;
+	u32 *build_raw;
+
+	st->mem = devm_kzalloc(&pdev->dev, st->size, GFP_KERNEL);
+	if (!st->mem)
+		return -ENOMEM;
+
+	axi_sysid_read(st, 0, st->mem, st->size);
+	header = (struct sysid_header_v1 *) st->mem;
+
+	if (axi_sysid_checksum((u8 *)st->mem, sizeof(struct sysid_header_v1))) {
+		dev_err(&pdev->dev, "verfify header checksum failed\n");
+		return -EFAULT;
+	}
+
+	if (header->version != st->info->header_version) {
+		dev_err(&pdev->dev,
+			"system ID header version mismatch. Expected %d, Reported %d\n",
+			st->info->header_version, header->version);
+		return -EFAULT;
+	}
+
+	build_raw = &st->mem[header->build_info_offs];
+
+	switch (header->version) {
+	case AXI_SYSID_HEADER_V2:
+		build_v1_1 = (struct build_info_header_v1_1 *) build_raw;
+
+		return axi_sysid_validate_v1_1(pdev, st, build_v1_1);
+	case AXI_SYSID_HEADER_V1:
+		build_v1 = (struct build_info_header_v1 *) build_raw;
+
+		return axi_sysid_validate_v1(pdev, st, build_v1);
+	default:
+		return -EINVAL;
+	}
+}
+
 static struct nvmem_config axi_sysid_nvmem_config = {
 	.name = "system-id",
 	.read_only = true,
@@ -170,29 +246,30 @@ static struct nvmem_config axi_sysid_nvmem_config = {
 	.reg_read = axi_sysid_read,
 };
 
-static const struct axi_sysid_core_info version_1_0_0_info = {
-	.version = ADI_AXI_PCORE_VER(1, 0, 'a'),
-	.header_version = 1,
+static const struct axi_sysid_core_info version_1_0_0_info[] = {
+	{
+		.version = ADI_AXI_PCORE_VER(1, 0, 'a'),
+		.header_version = 1,
+	},
+	{
+		.version = ADI_AXI_PCORE_VER(1, 1, 'a'),
+		.header_version = 2,
+	},
+	{}
 };
-
-static const struct of_device_id axi_sysid_of_match[] = {
-	{ .compatible = "adi,axi-sysid-1.00.a",
-		.data = (void *)&version_1_0_0_info},
-	{},
-};
-MODULE_DEVICE_TABLE(of, axi_sysid_of_match);
 
 static int axi_sysid_probe(struct platform_device *pdev)
 {
-	const struct of_device_id *id;
+	const struct axi_sysid_core_info *info;
 	struct nvmem_device *nvmem;
 	struct axi_sysid *st;
 	struct resource *res;
 	u32 version;
+	int i;
 
-	id = of_match_node(axi_sysid_of_match, pdev->dev.of_node);
-	if (!id)
-		return -EINVAL;
+	info = device_get_match_data(&pdev->dev);
+	if (!info)
+		return -ENODEV;
 
 	st = devm_kzalloc(&pdev->dev, sizeof(*st), GFP_KERNEL);
 	if (!st)
@@ -203,16 +280,17 @@ static int axi_sysid_probe(struct platform_device *pdev)
 	if (IS_ERR(st->base))
 		return PTR_ERR(st->base);
 
-	st->info = id->data;
-
 	version = axi_sysid_ioread(st, ADI_AXI_REG_VERSION);
-	if (ADI_AXI_PCORE_VER_MAJOR(version) !=
-		ADI_AXI_PCORE_VER_MAJOR(st->info->version)) {
+	for (i = 0; info[i].version; i++) {
+		if (version == info[i].version) {
+			st->info = &info[i];
+			break;
+		}
+	}
+
+	if (!st->info) {
 		dev_err(&pdev->dev,
-			"Major version mismatch. Expected %d.%.2d.%c, Reported %d.%.2d.%c\n",
-			ADI_AXI_PCORE_VER_MAJOR(st->info->version),
-			ADI_AXI_PCORE_VER_MINOR(st->info->version),
-			ADI_AXI_PCORE_VER_PATCH(st->info->version),
+			"Unsupported version %d.%.2d.%c\n",
 			ADI_AXI_PCORE_VER_MAJOR(version),
 			ADI_AXI_PCORE_VER_MINOR(version),
 			ADI_AXI_PCORE_VER_PATCH(version));
@@ -245,6 +323,12 @@ static int axi_sysid_remove(struct platform_device *pdev)
 
 	return 0;
 }
+
+static const struct of_device_id axi_sysid_of_match[] = {
+	{ .compatible = "adi,axi-sysid-1.00.a", .data = version_1_0_0_info},
+	{}
+};
+MODULE_DEVICE_TABLE(of, axi_sysid_of_match);
 
 static struct platform_driver axi_sysid_driver = {
 	.probe	= axi_sysid_probe,
