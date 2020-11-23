ea036783f17aa8ad74f1e79156140be6c8f38b1f
Robin Gong <yibin.gong@nxp.com>
2020-11-23 23:17:44 +0800

    LF-2939-1: input: keyboard: snvs_pwrkey: drop checking minor_rev
    Use reading property instead of checking SNVS_HPVIDR1_REG register to know
    snvs IP version which has been fixed 'only interrupt report on release' as
    commit 1a26c920717a ("Input: snvs_pwrkey - send key events for i.MX6 S, DL
    and Q"), because not all soc strictly program the IP version into
    SNVS_HPVIDR1_REG. So change it to read from property 'emulate-press' in dts.
    
    Signed-off-by: Robin Gong <yibin.gong@nxp.com>
    Reviewed-by: Anson Huang <anson.huang@nxp.com>

--- drivers/input/keyboard/snvs_pwrkey.c
+++ drivers/input/keyboard/snvs_pwrkey.c
@@ -20,7 +20,6 @@
 #include <linux/mfd/syscon.h>
 #include <linux/regmap.h>
 
-#define SNVS_HPVIDR1_REG	0xBF8
 #define SNVS_LPSR_REG		0x4C	/* LP Status Register */
 #define SNVS_LPCR_REG		0x38	/* LP Control Register */
 #define SNVS_HPSR_REG		0x14
@@ -41,7 +40,7 @@ struct pwrkey_drv_data {
 	struct clk *clk;
 	struct timer_list check_timer;
 	struct input_dev *input;
-	u8 minor_rev;
+	bool  emulate_press;
 };
 
 static void imx_imx_snvs_check_for_events(struct timer_list *t)
@@ -107,7 +106,7 @@ static irqreturn_t imx_snvs_pwrkey_interrupt(int irq, void *dev_id)
 
 	regmap_read(pdata->snvs, SNVS_LPSR_REG, &lp_status);
 	if (lp_status & SNVS_LPSR_SPO) {
-		if (pdata->minor_rev == 0) {
+		if (pdata->emulate_press) {
 			/*
 			 * The first generation i.MX6 SoCs only sends an
 			 * interrupt on button release. To mimic power-key
@@ -147,7 +146,6 @@ static int imx_snvs_pwrkey_probe(struct platform_device *pdev)
 	struct device_node *np;
 	struct clk *clk;
 	int error;
-	u32 vid;
 
 	/* Get SNVS register Page */
 	np = pdev->dev.of_node;
@@ -181,8 +179,7 @@ static int imx_snvs_pwrkey_probe(struct platform_device *pdev)
 	if (pdata->irq < 0)
 		return -EINVAL;
 
-	regmap_read(pdata->snvs, SNVS_HPVIDR1_REG, &vid);
-	pdata->minor_rev = vid & 0xff;
+	pdata->emulate_press = of_property_read_bool(np, "emulate-press");
 
 	pdata->clk = devm_clk_get(&pdev->dev, "snvs");
 	if (IS_ERR(pdata->clk)) {
