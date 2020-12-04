ccf70555f55888508f33e661454bcace5a062987
Michael Hennerich <michael.hennerich@analog.com>
2020-12-04 09:21:15 +0100

    iio: jesd204: axi_adxcvr: Add dt-bindings include and use it
    Remove redundant adi,use-cpll-enable property since its clearly
    covered by adi,sys-clk-select.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

---
+++ include/dt-bindings/jesd204/adxcvr.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2020 Analog Devices, Inc.
+ */
+
+#ifndef _DT_BINDINGS_JESD204_ADXCVR_H
+#define _DT_BINDINGS_JESD204_ADXCVR_H
+
+/*
+ * adi,sys-clk-select
+ */
+#define XCVR_CPLL		0 /* CPLL: GTHE3, GTHE4, GTYE4, GTXE2 */
+#define XCVR_QPLL1		2 /* QPLL1: GTHE3, GTHE4, GTYE4 */
+#define XCVR_QPLL		3 /* QPLL0: GTHE3, GTHE4, GTYE4, GTXE2 */
+
+/*
+ * adi,out-clk-select
+ */
+#define XCVR_OUTCLK_PCS		1
+#define XCVR_OUTCLK_PMA		2
+#define XCVR_REFCLK		3
+#define XCVR_REFCLK_DIV2	4
+
+#endif /* _DT_BINDINGS_JESD204_ADXCVR_H */
+
