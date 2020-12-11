15185d761d75cf4cfd25cd58ff3642e0ced18d82
Michael Hennerich <michael.hennerich@analog.com>
2020-12-11 16:34:47 +0100

    iio: jesd204: axi_adxcvr: Un-managed QPLL unlocked return -EPROBE_DEFER
    In case the un-managed QPLL doesn't lock, it's likly not yet configured.
    In the unmanaged case defer probe until it succeeds.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    (cherry picked from commit dce063759f2770176953120fc5b66fb2c592811d)

--- drivers/iio/jesd204/axi_adxcvr.c
+++ drivers/iio/jesd204/axi_adxcvr.c
@@ -204,6 +204,12 @@ static int adxcvr_status_error(struct device *dev)
 	} while ((timeout--) && (status == 0));
 
 	if (!status) {
+		if (!st->qpll_enable && !st->cpll_enable) {
+			dev_err(dev, "%s %s: Not ready defer probe",
+				adxcvr_sys_clock_sel_names[st->sys_clk_sel],
+				st->tx_enable ? "TX" : "RX");
+			return -EPROBE_DEFER;
+		}
 		dev_err(dev, "%s %s Error: %x",
 			adxcvr_sys_clock_sel_names[st->sys_clk_sel],
 			st->tx_enable ? "TX" : "RX", status);
