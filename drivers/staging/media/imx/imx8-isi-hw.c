6d1f19918ba867cc8db2c6b24e8ecd997b3cc1eb
Guoniu.zhou <guoniu.zhou@nxp.com>
2020-12-22 17:19:11 +0800

    MLK-24982: media: isi: disable ISI channel clock after stream off
    Disable ISI channel clock after stream off
    
    Signed-off-by: Guoniu.zhou <guoniu.zhou@nxp.com>
    Reviewed-by: Robby Cai <robby.cai@nxp.com>

--- drivers/staging/media/imx/imx8-isi-hw.c
+++ drivers/staging/media/imx/imx8-isi-hw.c
@@ -619,7 +619,7 @@ void mxc_isi_channel_deinit(struct mxc_isi_dev *mxc_isi)
 	mxc_isi_channel_sw_reset(mxc_isi);
 
 	/* deinit channel clk first */
-	val = (CHNL_CTRL_CLK_EN_ENABLE << CHNL_CTRL_CLK_EN_OFFSET);
+	val = (CHNL_CTRL_CLK_EN_DISABLE << CHNL_CTRL_CLK_EN_OFFSET);
 	writel(val, mxc_isi->regs + CHNL_CTRL);
 
 	if (mxc_isi->chain_buf && mxc_isi->chain)
