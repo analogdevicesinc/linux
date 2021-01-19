7572979fd1864cd8051370695338943fba5a26a5
Alexandru Ardelean <alexandru.ardelean@analog.com>
2021-01-19 11:33:36 +0200

    drivers: fix implicit fallthrough warnings/errors
    We treat warnings as errors wherever we can.
    The RPi branch has plenty of custom/imported drivers that produce
    implicit-fallthrough warnings.
    This change fixes them.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- drivers/net/wireless/realtek/rtl8192cu/core/rtw_mlme_ext.c
+++ drivers/net/wireless/realtek/rtl8192cu/core/rtw_mlme_ext.c
@@ -736,6 +736,7 @@ void mgt_dispatcher(_adapter *padapter, union recv_frame *precv_frame)
 			else
 				ptable->func = &OnAuthClient;
 			//pass through
+			/* fall-through */
 		case WIFI_ASSOCREQ:
 		case WIFI_REASSOCREQ:
 			_mgt_dispatcher(padapter, ptable, precv_frame);	
