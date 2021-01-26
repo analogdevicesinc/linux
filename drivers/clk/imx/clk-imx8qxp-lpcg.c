ad36c65e9eb75f33ffa04f36edd204545b90c45e
Dong Aisheng <aisheng.dong@nxp.com>
2021-01-26 10:55:07 +0800

    clk: imx: imx8qxp: support LPCG bit-offset instead of clock-indice
    Back to support LPCG bit-offset property instead of clock indice.
    
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>

--- drivers/clk/imx/clk-imx8qxp-lpcg.c
+++ drivers/clk/imx/clk-imx8qxp-lpcg.c
@@ -17,20 +17,6 @@
 
 #define IMX_LPCG_MAX_CLKS	8
 
-static struct clk_hw *imx_lpcg_of_clk_src_get(struct of_phandle_args *clkspec,
-					      void *data)
-{
-	struct clk_hw_onecell_data *hw_data = data;
-	unsigned int idx = clkspec->args[0] / 4;
-
-	if (idx >= hw_data->num) {
-		pr_err("%s: invalid index %u\n", __func__, idx);
-		return ERR_PTR(-EINVAL);
-	}
-
-	return hw_data->hws[idx];
-}
-
 static int imx8qxp_lpcg_clk_probe(struct platform_device *pdev)
 {
 	struct device_node *np = pdev->dev.of_node;
@@ -40,8 +26,8 @@ static int imx8qxp_lpcg_clk_probe(struct platform_device *pdev)
 	struct clk_hw_onecell_data *clk_data;
 	struct clk_hw **clk_hws;
 	void __iomem *base;
+	bool autogate;
 	int count;
-	int idx;
 	int ret;
 	int i;
 
@@ -49,47 +35,41 @@ static int imx8qxp_lpcg_clk_probe(struct platform_device *pdev)
 	if (IS_ERR(base))
 		return PTR_ERR(base);
 
-	count = of_property_count_u32_elems(np, "clock-indices");
+	count = of_property_count_u32_elems(np, "bit-offset");
 	if (count < 0) {
 		dev_err(&pdev->dev, "failed to count clocks\n");
 		return -EINVAL;
 	}
 
-	/*
-	 * A trick here is that we set the num of clks to the MAX instead
-	 * of the count from clock-indices because one LPCG supports up to
-	 * 8 clock outputs which each of them is fixed to 4 bits. Then we can
-	 * easily get the clock by clk-indices (bit-offset) / 4.
-	 * And the cost is very limited few pointers.
-	 */
-
-	clk_data = devm_kzalloc(&pdev->dev, struct_size(clk_data, hws,
-				IMX_LPCG_MAX_CLKS), GFP_KERNEL);
+	clk_data = devm_kzalloc(&pdev->dev, struct_size(clk_data, hws, count),
+				GFP_KERNEL);
 	if (!clk_data)
 		return -ENOMEM;
 
-	clk_data->num = IMX_LPCG_MAX_CLKS;
+	clk_data->num = count;
 	clk_hws = clk_data->hws;
 
-	ret = of_property_read_u32_array(np, "clock-indices", bit_offset,
-					 count);
+	ret = of_property_read_u32_array(np, "bit-offset", bit_offset,
+					 clk_data->num);
 	if (ret < 0) {
-		dev_err(&pdev->dev, "failed to read clock-indices\n");
+		dev_err(&pdev->dev, "failed to read clocks bit-offset\n");
 		return -EINVAL;
 	}
 
-	ret = of_clk_parent_fill(np, parent_names, count);
-	if (ret != count) {
+	ret = of_clk_parent_fill(np, parent_names, clk_data->num);
+	if (ret != clk_data->num) {
 		dev_err(&pdev->dev, "failed to get clock parent names\n");
-		return count;
+		return -EINVAL;
 	}
 
 	ret = of_property_read_string_array(np, "clock-output-names",
-					    output_names, count);
-	if (ret != count) {
+					    output_names, clk_data->num);
+	if (ret != clk_data->num) {
 		dev_err(&pdev->dev, "failed to read clock-output-names\n");
 		return -EINVAL;
 	}
+
+	autogate = of_property_read_bool(np, "hw-autogate");
 
 	pm_runtime_get_noresume(&pdev->dev);
 	pm_runtime_set_active(&pdev->dev);
@@ -97,27 +77,26 @@ static int imx8qxp_lpcg_clk_probe(struct platform_device *pdev)
 	pm_runtime_use_autosuspend(&pdev->dev);
 	pm_runtime_enable(&pdev->dev);
 
-	for (i = 0; i < count; i++) {
-		idx = bit_offset[i] / 4;
-		if (idx >= IMX_LPCG_MAX_CLKS) {
+	for (i = 0; i < clk_data->num; i++) {
+		if (bit_offset[i] > 31) {
 			dev_warn(&pdev->dev, "invalid bit offset of clock %d\n",
 				 i);
 			ret = -EINVAL;
 			goto unreg;
 		}
 
-		clk_hws[idx] = imx_clk_lpcg_scu_dev(&pdev->dev, output_names[i],
-						    parent_names[i], 0, base,
-						    bit_offset[i], false);
-		if (IS_ERR(clk_hws[idx])) {
+		clk_hws[i] = imx_clk_lpcg_scu_dev(&pdev->dev, output_names[i],
+						  parent_names[i], 0, base,
+						  bit_offset[i], autogate);
+		if (IS_ERR(clk_hws[i])) {
 			dev_warn(&pdev->dev, "failed to register clock %d\n",
-				 idx);
-			ret = PTR_ERR(clk_hws[idx]);
+				 i);
+			ret = PTR_ERR(clk_hws[i]);
 			goto unreg;
 		}
 	}
 
-	ret = devm_of_clk_add_hw_provider(&pdev->dev, imx_lpcg_of_clk_src_get,
+	ret = devm_of_clk_add_hw_provider(&pdev->dev, of_clk_hw_onecell_get,
 					  clk_data);
 	if (ret)
 		goto unreg;
@@ -129,9 +108,8 @@ static int imx8qxp_lpcg_clk_probe(struct platform_device *pdev)
 
 unreg:
 	while (--i >= 0) {
-		idx = bit_offset[i] / 4;
-		if (clk_hws[idx])
-			imx_clk_lpcg_scu_unregister(clk_hws[idx]);
+		if (clk_hws[i])
+			imx_clk_lpcg_scu_unregister(clk_hws[i]);
 	}
 
 	pm_runtime_disable(&pdev->dev);
