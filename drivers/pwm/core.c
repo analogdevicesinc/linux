cb9260cee0a33a7dba35e6582e525a1f05aed4c3
Sergiu Cuciurean <sergiu.cuciurean@analog.com>
2021-03-29 18:26:03 +0300

    drivers: pwm: core: Add offset support
    Some hardware supports delaying the start of a PWM signal. This commit
    adds support for optional PWM offset(delay) that will be applied to a
    specific channel.
    
    In a case where a pwm chip needs two or more outputs to respect a
    certain timing, the offset parameter will come in handy.
    
    This offset can be seen as a phase shift or a time delay, relative to
    the first PWM pulse.
    
    Signed-off-by: Sergiu Cuciurean <sergiu.cuciurean@analog.com>
    (cherry picked from commit b2c4ca6bf594d7cb4aab16351b0505f5c74cb7d9)

--- drivers/pwm/core.c
+++ drivers/pwm/core.c
@@ -464,6 +464,7 @@ int pwm_apply_state(struct pwm_device *pwm, const struct pwm_state *state)
 	if (state->period == pwm->state.period &&
 	    state->duty_cycle == pwm->state.duty_cycle &&
 	    state->polarity == pwm->state.polarity &&
+	    state->offset == pwm->state.offset &&
 	    state->enabled == pwm->state.enabled)
 		return 0;
 
