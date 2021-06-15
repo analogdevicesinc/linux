b2ede066d2da2d5a1a583ea1def1168e924bdb9b
Nuno Sá <nuno.sa@analog.com>
2021-06-15 10:45:04 +0200

    iio: buffer: fix channel {en|dis}ablement
    Make sure that the complete buffer scan mask gets cleared before
    computing a new one. For that we need to multiply the number of elements
    by the element size.
    
    Fixes: 81d00795b1537 ("iio: Track enabled channels on a per channel basis")
    Signed-off-by: Nuno Sá <nuno.sa@analog.com>
    (cherry picked from commit c8816799bec95dafff8ed7838b605b6269b81a52)

--- drivers/iio/inkern.c
+++ drivers/iio/inkern.c
@@ -866,7 +866,8 @@ void iio_buffer_channel_enable(struct iio_buffer *buffer,
 
 	set_bit(chan->channel_index, buffer->channel_mask);
 
-	memset(buffer->scan_mask, 0, BITS_TO_LONGS(chan->indio_dev->masklength));
+	memset(buffer->scan_mask, 0,
+	       BITS_TO_LONGS(chan->indio_dev->masklength) * sizeof(*buffer->scan_mask));
 	for_each_set_bit(ch, buffer->channel_mask, chan->indio_dev->num_channels)
 		set_bit(chan->indio_dev->channels[ch].scan_index, buffer->scan_mask);
 }
@@ -879,7 +880,8 @@ void iio_buffer_channel_disable(struct iio_buffer *buffer,
 
 	clear_bit(chan->channel_index, buffer->channel_mask);
 
-	memset(buffer->scan_mask, 0, BITS_TO_LONGS(chan->indio_dev->masklength));
+	memset(buffer->scan_mask, 0,
+	       BITS_TO_LONGS(chan->indio_dev->masklength) * sizeof(*buffer->scan_mask));
 	for_each_set_bit(ch, buffer->channel_mask, chan->indio_dev->num_channels)
 		set_bit(chan->indio_dev->channels[ch].scan_index, buffer->scan_mask);
 }
