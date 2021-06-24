98557d45125b34b713a1f5096a96cfa1bff61acd
Antoniu Miclaus <antoniu.miclaus@analog.com>
2021-06-24 11:49:07 +0300

    drivers:iio:frequency: remove unused header
    Remove `sysfs.h` includes after removal of the custom IIO device
    attributes.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>
    (cherry picked from commit dc36510dae3e5a04556d7816c32220449dd7fa0e)

--- drivers/iio/frequency/admv1014.c
+++ drivers/iio/frequency/admv1014.c
@@ -20,8 +20,6 @@
 #include <linux/regulator/consumer.h>
 #include <linux/spi/spi.h>
 
-#include <linux/iio/sysfs.h>
-
 /* ADMV1014 Register Map */
 #define ADMV1014_REG_SPI_CONTROL		0x00
 #define ADMV1014_REG_ALARM			0x01
