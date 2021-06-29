01d6aa2d6142c80d808f80a49ade7870bc98ee1d
Richard Gong <richard.gong@intel.com>
2021-06-29 11:17:40 -0500

    firmware: stratix10-svc: update for getting SHA2 digest
    Update to support getting SHA2 digest.
    
    Signed-off-by: Richard Gong <richard.gong@intel.com>
    Signed-off-by: Siew Chin Lim <elly.siew.chin.lim@intel.com>

--- include/linux/firmware/intel/stratix10-smc.h
+++ include/linux/firmware/intel/stratix10-smc.h
@@ -972,4 +972,55 @@ INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_FPGA_CONFIG_COMPLETED_WRITE)
 #define INTEL_SIP_SMC_FCS_AES_CRYPTO_FINALIZE \
         INTEL_SIP_SMC_STD_CALL_VAL(INTEL_SIP_SMC_FUNCID_FCS_AES_CRYPTO_FINALIZE)
 
+/**
+ * Request INTEL_SIP_SMC_FCS_GET_DIGEST_INIT
+ * Sync call to request the SHA-2 hash digest on a blob
+ *
+ * Call register usage:
+ * a0 INTEL_SIP_SMC_FCS_GET_DIGEST_INIT
+ * a1 session ID
+ * a2 context ID
+ * a3 key UID
+ * a4 size of crypto parameter data
+ * a5 the crypto parameter
+ *      3:0     SHA opeation mode
+ *      7:4     digist size
+ *      63:8    not used
+ * a6-a7 not used
+ *
+ * Return status:
+ * a0 INTEL_SIP_SMC_STATUS_OK, INTEL_SIP_SMC_STATUS_NOT_SUPPORTED or
+ *    INTEL_SIP_SMC_STATUS_ERROR
+ * a1 mailbox errors if a0 is INTEL_SIP_SMC_STATUS_ERROR
+ * a2-a3 not used
+ */
+#define INTEL_SIP_SMC_FUNCID_FCS_GET_DIGEST_INIT 119
+#define INTEL_SIP_SMC_FCS_GET_DIGEST_INIT \
+        INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_FCS_GET_DIGEST_INIT)
+
+/**
+ * Request INTEL_SIP_SMC_FCS_GET_DIGEST_FINALIZE
+ * Sync call to request the SHA-2 hash digest on a blob
+ *
+ * Call register usage:
+ * a0 INTEL_SIP_SMC_FCS_GET_DIGEST_FINALIZE
+ * a1 session ID
+ * a2 context ID
+ * a3 physical address of source
+ * a4 size of source
+ * a5 physical address of destation
+ * a6 size of destation
+ * a7 not used
+ *
+ * Return status:
+ * a0 INTEL_SIP_SMC_STATUS_OK, INTEL_SIP_SMC_STATUS_NOT_SUPPORTED or
+ *    INTEL_SIP_SMC_STATUS_ERROR
+ * a1 mailbox errors if a0 is INTEL_SIP_SMC_STATUS_ERROR
+ * a2 physical address of response data
+ * a3 size of response data
+ */
+#define INTEL_SIP_SMC_FUNCID_FCS_GET_DIGEST_FINALIZE 121
+#define INTEL_SIP_SMC_FCS_GET_DIGEST_FINALIZE \
+        INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_FCS_GET_DIGEST_FINALIZE)
+
 #endif
