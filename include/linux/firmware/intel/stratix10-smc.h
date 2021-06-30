48df9811c48148d2ec2ff991ea1413b25415d072
Richard Gong <richard.gong@intel.com>
2021-06-30 08:44:14 -0500

    firmware: stratix10-svc: update for ECDSA hash signing
    Update to support ECDSA hash signing.
    
    Signed-off-by: Richard Gong <richard.gong@intel.com>
    Signed-off-by: Siew Chin Lim <elly.siew.chin.lim@intel.com>

--- include/linux/firmware/intel/stratix10-smc.h
+++ include/linux/firmware/intel/stratix10-smc.h
@@ -1077,4 +1077,54 @@ INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_FPGA_CONFIG_COMPLETED_WRITE)
 #define INTEL_SIP_SMC_FCS_MAC_VERIFY_FINALIZE \
         INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_FCS_MAC_VERIFY_FINALIZE)
 
+/**
+ * Request INTEL_SIP_SMC_FCS_ECDSA_HASH_SIGNING_INIT
+ * Sync call to sends digital signature signing request on a data blob
+ *
+ * Call register usage:
+ * a0 INTEL_SIP_SMC_FCS_ECDSA_HASH_SIGNING_INIT
+ * a1 session ID
+ * a2 context ID
+ * a3 key UID
+ * a4 size of crypto parameter data
+ * a5 size of crypto parameter data
+ *      3:0     ECC algoritim
+ *      63:4    not used
+ * a6-a7 not used
+ *
+ * Return status:
+ * a0 INTEL_SIP_SMC_STATUS_OK, INTEL_SIP_SMC_STATUS_NOT_SUPPORTED or
+ *    INTEL_SIP_SMC_STATUS_ERROR
+ * a1 mailbox errors if a0 is INTEL_SIP_SMC_STATUS_ERROR
+ * a2-a3 not used
+ */
+#define INTEL_SIP_SMC_FUNCID_FCS_ECDSA_HASH_SIGNING_INIT 125
+#define INTEL_SIP_SMC_FCS_ECDSA_HASH_SIGNING_INIT \
+        INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_FCS_ECDSA_HASH_SIGNING_INIT)
+
+/**
+ * Request INTEL_SIP_SMC_FCS_ECDSA_HASH_SIGNING_FINALIZE
+ * Sync call to sends digital signature signing request on a data blob
+ *
+ * Call register usage:
+ * a0 INTEL_SIP_SMC_FCS_ECDSA_HASH_SIGNING_FINALIZE
+ * a1 session ID
+ * a2 context ID
+ * a3 physical address of source
+ * a4 size of source
+ * a5 physical address of destation
+ * a6 size of destation
+ * a7 not used
+ *
+ * Return status:
+ * a0 INTEL_SIP_SMC_STATUS_OK, INTEL_SIP_SMC_STATUS_NOT_SUPPORTED or
+ *    INTEL_SIP_SMC_STATUS_ERROR
+ * a1 mailbox errors if a0 is INTEL_SIP_SMC_STATUS_ERROR
+ * a2 physical address of response data
+ * a3 size of response data
+ */
+#define INTEL_SIP_SMC_FUNCID_ECDSA_HASH_SIGNING_FINALIZE 127
+#define INTEL_SIP_SMC_FCS_ECDSA_HASH_SIGNING_FINALIZE \
+        INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_ECDSA_HASH_SIGNING_FINALIZE)
+
 #endif
--- include/linux/firmware/intel/stratix10-svc-client.h
+++ include/linux/firmware/intel/stratix10-svc-client.h
@@ -204,6 +204,10 @@ struct stratix10_svc_chan;
  * @COMMAND_FCS_CRYPTO_MAC_VERIFY (INIT and FINALIZE): check the integrity
  * and authenticity of a blob, return status is SVC_STATUS_OK or
  * SVC_STATUS_ERROR
+ *
+ * @COMMAND_FCS_CRYPTO_ECDSA_HASH_SIGNING (INIT and FINALIZE): send
+ * digital signature signing request on a data blob, return status is
+ * SVC_STATUS_OK or SVC_STATUS_ERROR
  */
 enum stratix10_svc_command_code {
 	/* for FPGA */
@@ -252,6 +256,8 @@ enum stratix10_svc_command_code {
 	COMMAND_FCS_CRYPTO_GET_DIGEST_FINALIZE,
 	COMMAND_FCS_CRYPTO_MAC_VERIFY_INIT,
 	COMMAND_FCS_CRYPTO_MAC_VERIFY_FINALIZE,
+	COMMAND_FCS_CRYPTO_ECDSA_HASH_SIGNING_INIT,
+	COMMAND_FCS_CRYPTO_ECDSA_HASH_SIGNING_FINALIZE,
 	/* for generic mailbox send command */
 	COMMAND_MBOX_SEND_CMD = 100,
 	/* Non-mailbox SMC Call */
