8d5393cd0c8da5e5a412bc23e7b1ce7f98dcc656
Haibo Chen <haibo.chen@nxp.com>
2021-07-20 16:29:03 +0800

    LF-4184-2 input: goodix: do not update touch device firmware
    The touch device firmware must get from vendor, we don't have any,
    so just don't update touch firmware here to save time for module probe.
    
    Reviewed-by: Clark Wang <xiaoning.wang@nxp.com>
    Signed-off-by: Haibo Chen <haibo.chen@nxp.com>

--- drivers/input/touchscreen/goodix.c
+++ drivers/input/touchscreen/goodix.c
@@ -1020,7 +1020,7 @@ retry_get_irq_gpio:
 	default:
 		if (ts->gpiod_int && ts->gpiod_rst) {
 			ts->reset_controller_at_probe = true;
-			ts->load_cfg_from_disk = true;
+			ts->load_cfg_from_disk = false;
 			ts->irq_pin_access_method = IRQ_PIN_ACCESS_GPIO;
 		}
 	}
@@ -1498,9 +1498,11 @@ static int goodix_resume(struct device *dev)
 		if (error)
 			return error;
 
-		error = goodix_send_cfg(ts, ts->config, ts->chip->config_len);
-		if (error)
-			return error;
+		if (ts->load_cfg_from_disk) {
+			error = goodix_send_cfg(ts, ts->config, ts->chip->config_len);
+			if (error)
+				return error;
+		}
 	}
 
 	error = goodix_request_irq(ts);
