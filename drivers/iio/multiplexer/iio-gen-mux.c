3a15b6428b869aae0fbd17fb56cf74d693a762e4
Michael Hennerich <michael.hennerich@analog.com>
2021-08-04 11:13:05 +0200

    iio: multiplexer: iio-gen-mux: Add optional CCF clock MUX support
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/multiplexer/iio-gen-mux.c
+++ drivers/iio/multiplexer/iio-gen-mux.c
@@ -17,14 +17,103 @@
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
 
+#include <linux/clk-provider.h>
+#include <linux/clk.h>
+
 
 struct gen_mux_state {
-	struct	mutex		lock; /* protect sensor state */
+	struct device		*dev;
+	struct mutex		lock; /* protect sensor state */
 	struct mux_control	*mux;
 	unsigned int		sel;
 	const char		**strings;
 	unsigned int		max_states;
+
+	struct clk_hw		hw;
+	const char 		**parent_names;
 };
+
+static u8 clk_gpio_mux_get_parent(struct clk_hw *hw)
+{
+	struct gen_mux_state *st = container_of(hw, struct gen_mux_state, hw);
+
+	dev_dbg(st->dev, "%s:%d: %d", __func__, __LINE__, st->sel);
+
+	return st->sel;
+}
+
+static int clk_gpio_mux_set_parent(struct clk_hw *hw, u8 index)
+{
+	struct gen_mux_state *st = container_of(hw, struct gen_mux_state, hw);
+
+	dev_dbg(st->dev, "%s:%d: %d index %d",
+		__func__, __LINE__, st->sel, index);
+
+	return 0;
+}
+
+static int clk_gpio_mux_determine_rate(struct clk_hw *hw,
+				      struct clk_rate_request *req)
+{
+	struct gen_mux_state *st = container_of(hw, struct gen_mux_state, hw);
+	struct clk_hw *parent = clk_hw_get_parent_by_index(hw, st->sel);
+
+	dev_dbg(st->dev, "%s:%d: %lu", __func__, __LINE__, req->rate);
+
+	req->best_parent_hw = parent;
+	req->best_parent_rate = req->rate;
+
+	return 0;
+}
+const struct clk_ops clk_gen_mux_ops = {
+	.get_parent = clk_gpio_mux_get_parent,
+	.set_parent = clk_gpio_mux_set_parent,
+	.determine_rate = clk_gpio_mux_determine_rate,
+};
+
+static int gen_mux_clk_driver_probe(struct platform_device *pdev,
+	struct gen_mux_state *st)
+{
+	struct device_node *node = pdev->dev.of_node;
+	unsigned int num_parents;
+	struct clk_init_data init = {};
+	struct clk_hw *hw;
+	const char *clk_name = node->name;
+	int ret;
+
+	num_parents = of_clk_get_parent_count(node);
+
+	if (num_parents) {
+		st->parent_names = devm_kcalloc(&pdev->dev, num_parents,
+					    sizeof(char *), GFP_KERNEL);
+		if (!st->parent_names)
+			return -ENOMEM;
+
+		of_clk_parent_fill(node, st->parent_names, num_parents);
+	} else {
+		return 0;
+	}
+
+	of_property_read_string(node, "clock-output-names", &clk_name);
+
+	init.name = clk_name;
+	init.ops = &clk_gen_mux_ops;
+	init.flags = 0;
+	init.parent_names = st->parent_names;
+	init.num_parents = num_parents;
+
+	st->hw.init = &init;
+
+	hw = &st->hw;
+
+	ret = devm_clk_hw_register(&pdev->dev, hw);
+
+	if (IS_ERR(hw->clk))
+		return PTR_ERR(hw->clk);
+
+	return of_clk_add_provider(node, of_clk_src_simple_get, hw->clk);
+}
+
 
 static ssize_t gen_mux_store(struct device *dev,
 				struct device_attribute *attr,
@@ -44,6 +133,11 @@ static ssize_t gen_mux_store(struct device *dev,
 		st->sel = index;
 		mux_control_deselect(st->mux);
 	}
+
+	if (st->parent_names)
+		clk_set_parent(st->hw.clk,
+			__clk_lookup(st->parent_names[index]));
+
 	mutex_unlock(&st->lock);
 
 	dev_dbg(indio_dev->dev.parent, "setting up the mux to %d\n", index);
@@ -118,6 +212,7 @@ static int gen_mux_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	st = iio_priv(indio_dev);
+	st->dev = &pdev->dev;
 
 	st->mux = devm_mux_control_get(&pdev->dev, NULL);
 	if (IS_ERR(st->mux)) {
@@ -137,6 +232,9 @@ static int gen_mux_probe(struct platform_device *pdev)
 
 	ret = of_property_read_string_array(pdev->dev.of_node,
 		"mux-state-names", st->strings, st->max_states);
+	if (ret < 0)
+		return ret;
+
 	mutex_init(&st->lock);
 
 	//indio_dev->name = "iio-gen-mux";
@@ -144,6 +242,10 @@ static int gen_mux_probe(struct platform_device *pdev)
 	indio_dev->info = &gen_mux_info;
 	indio_dev->dev.parent = &pdev->dev;
 	indio_dev->modes = INDIO_DIRECT_MODE;
+
+	ret = gen_mux_clk_driver_probe(pdev, st);
+	if (ret)
+		return ret;
 
 	return devm_iio_device_register(&pdev->dev, indio_dev);
 }
