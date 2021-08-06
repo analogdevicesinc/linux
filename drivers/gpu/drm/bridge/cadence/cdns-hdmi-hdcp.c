ad2e0aaf792899aa5c23cf0803eaa01925d1f2e9
Sandor Yu <Sandor.yu@nxp.com>
2021-08-06 11:05:12 +0800

    LF-4270-3: drm: bridge: cdns: correct copyright description
    Correct copyright description with the format:
    Copyright <year range> NXP description.
    
    Signed-off-by: Sandor Yu <Sandor.yu@nxp.com>
    Reviewed-by: Robby Cai <robby.cai@nxp.com>

--- drivers/gpu/drm/bridge/cadence/cdns-hdmi-hdcp.c
+++ drivers/gpu/drm/bridge/cadence/cdns-hdmi-hdcp.c
@@ -1,7 +1,7 @@
 /*
  * Cadence HDMI HDCP driver
  *
- * Copyright (C) 2021 NXP Semiconductor, Inc.
+ * Copyright 2021 NXP
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
