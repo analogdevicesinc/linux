64769288fbb777f2979b2209f3f3045fddeb8ccc
Sandor Yu <Sandor.yu@nxp.com>
2021-08-06 11:00:40 +0800

    LF-4270-1: staging: media: imx: correct hdmirx copyright description
    Correct copyright description with the format:
    Copyright <year range> NXP
    
    Signed-off-by: Sandor Yu <Sandor.yu@nxp.com>
    Reviewed-by: Robby Cai <robby.cai@nxp.com>

--- drivers/staging/media/imx/hdmirx/cdns-hdmirx-hw.c
+++ drivers/staging/media/imx/hdmirx/cdns-hdmirx-hw.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2020 NXP Semiconductor, Inc.
+ * Copyright 2018-2020 NXP
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
