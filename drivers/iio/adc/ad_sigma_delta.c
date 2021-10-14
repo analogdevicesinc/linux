9fd00f78c42ba412b6582c1c083b45128484dc4b
Nuno Sá <nuno.sa@analog.com>
2021-10-14 16:30:42 +0200

    iio: ad_sigma_delta: sync with master
    There are changes between 5.10.63 and 5.10 that, after the rebase, left
    the driver in broken state. This change syncs the driver with what we
    have in the master branch.
    
    The only change in question is a bugfix that landed on 5.10.63:
    
    "iio: ad_sigma_delta: Don't put SPI transfer buffer on the stack
    
    commit 0fb6ee8d0b5e90b72f870f76debc8bd31a742014 upstream."
    
    The plan is to backport this to the master branch (as it is a bugfix)
    and get this here via the CI sync job.
    
    Signed-off-by: Nuno Sá <nuno.sa@analog.com>

--- drivers/iio/adc/ad_sigma_delta.c
+++ drivers/iio/adc/ad_sigma_delta.c
@@ -58,7 +58,7 @@ EXPORT_SYMBOL_GPL(ad_sd_set_comm);
 int ad_sd_write_reg(struct ad_sigma_delta *sigma_delta, unsigned int reg,
 	unsigned int size, unsigned int val)
 {
-	uint8_t *data = sigma_delta->tx_buf;
+	uint8_t *data = sigma_delta->data;
 	struct spi_transfer t = {
 		.tx_buf		= data,
 		.len		= size + 1,
@@ -152,22 +152,22 @@ int ad_sd_read_reg(struct ad_sigma_delta *sigma_delta,
 {
 	int ret;
 
-	ret = ad_sd_read_reg_raw(sigma_delta, reg, size, sigma_delta->rx_buf);
+	ret = ad_sd_read_reg_raw(sigma_delta, reg, size, sigma_delta->data);
 	if (ret < 0)
 		goto out;
 
 	switch (size) {
 	case 4:
-		*val = get_unaligned_be32(sigma_delta->rx_buf);
+		*val = get_unaligned_be32(sigma_delta->data);
 		break;
 	case 3:
-		*val = get_unaligned_be24(sigma_delta->rx_buf);
+		*val = get_unaligned_be24(&sigma_delta->data[0]);
 		break;
 	case 2:
-		*val = get_unaligned_be16(sigma_delta->rx_buf);
+		*val = get_unaligned_be16(sigma_delta->data);
 		break;
 	case 1:
-		*val = sigma_delta->rx_buf[0];
+		*val = sigma_delta->data[0];
 		break;
 	default:
 		ret = -EINVAL;
@@ -482,7 +482,6 @@ static irqreturn_t ad_sd_trigger_handler(int irq, void *p)
 	struct iio_poll_func *pf = p;
 	struct iio_dev *indio_dev = pf->indio_dev;
 	struct ad_sigma_delta *sigma_delta = iio_device_get_drvdata(indio_dev);
-	uint8_t *data = sigma_delta->rx_buf;
 	unsigned int reg_size;
 	int ret;
 
