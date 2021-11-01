e6926e30a20412f8e4216d591e61c0f42eb7c399
Liu Ying <victor.liu@nxp.com>
2021-11-01 14:52:08 +0800

    MLK-25760-3 drm/bridge: nwl-dsi: Do mode set in ->atomic_pre_enable()
    A previous patch introduced the modeset_done flag to avoid two consecutive
    modeset operations from happening(thus, DPHY can be configured correctly)
    if the user does the following system suspend/resume test:
    
    DPMS off(disable) -> suspend -> resume(modeset) -> DPMS on(modeset, enable)
    
    However, it doesn't help with cases where consecutive system suspend/resume
    operations happen like below, because the DSI controller and DPHY configurations
    done in ->mode_set() are lost in the suspend operation(s) later on:
    
    DPMS off(disable) -> suspend -> resume(modeset) -> suspend ->
    resume(modeset skipped) -> ... -> DPMS on(modeset skipped, enable)
    
    To fix all the issues, this patch does mode set operation ->atomic_pre_enable()
    which happens in pairs with ->atomic_post_disable().  The RM68200 panel
    driver patch to postpone panel configuration to panel_funcs->enable() is
    ready beforehand, so it's safe to postpone the mode set operation for the
    DSI controller and DPHY.
    
    Reported-by: Zhang Bo <bo.zhang@nxp.com>
    Cc: Sandor Yu <Sandor.yu@nxp.com>
    Cc: Robert Chiras <robert.chiras@nxp.com>
    Tested-by: Zhang Bo <bo.zhang@nxp.com>
    Reviewed-by: Sandor Yu <Sandor.yu@nxp.com>
    Signed-off-by: Liu Ying <victor.liu@nxp.com>
    [ Liu Ying: Fix conflicts to upgrade to next-20220315 ]
    Signed-off-by: Liu Ying <victor.liu@nxp.com>

--- drivers/gpu/drm/bridge/nwl-dsi.c
+++ drivers/gpu/drm/bridge/nwl-dsi.c
@@ -153,7 +153,6 @@ struct nwl_dsi {
 
 	struct nwl_dsi_transfer *xfer;
 	bool use_dcss;
-	bool modeset_done;
 };
 
 static const struct regmap_config nwl_dsi_regmap_config = {
@@ -736,7 +735,7 @@ static irqreturn_t nwl_dsi_irq_handler(int irq, void *data)
 	return IRQ_HANDLED;
 }
 
-static int nwl_dsi_mode_set(struct nwl_dsi *dsi)
+static int nwl_dsi_enable(struct nwl_dsi *dsi)
 {
 	struct device *dev = dsi->dev;
 	union phy_configure_opts *phy_cfg = &dsi->phy_cfg;
@@ -870,8 +869,6 @@ nwl_dsi_bridge_atomic_post_disable(struct drm_bridge *bridge,
 		clk_disable_unprepare(dsi->lcdif_clk);
 
 	pm_runtime_put(dsi->dev);
-
-	dsi->modeset_done = false;
 }
 
 static int nwl_dsi_get_dphy_params(struct nwl_dsi *dsi,
@@ -935,15 +932,6 @@ static int nwl_dsi_bridge_atomic_check(struct drm_bridge *bridge,
 		adjusted->flags |= (DRM_MODE_FLAG_NHSYNC | DRM_MODE_FLAG_NVSYNC);
 	}
 
-	/*
-	 * Do a full modeset if crtc_state->active is changed to be true.
-	 * This ensures our ->mode_set() is called to get the DSI controller
-	 * and the PHY ready to send DCS commands, when only the connector's
-	 * DPMS is brought out of "Off" status.
-	 */
-	if (crtc_state->active_changed && crtc_state->active)
-		crtc_state->mode_changed = true;
-
 	return 0;
 }
 
@@ -957,9 +945,6 @@ nwl_dsi_bridge_mode_set(struct drm_bridge *bridge,
 	union phy_configure_opts new_cfg;
 	unsigned long phy_ref_rate;
 	int ret;
-
-	if (dsi->modeset_done)
-		return;
 
 	DRM_DEV_DEBUG_DRIVER(dsi->dev, "Setting mode:\n");
 	drm_mode_debug_printmodeline(adjusted_mode);
@@ -988,8 +973,16 @@ nwl_dsi_bridge_mode_set(struct drm_bridge *bridge,
 	DRM_DEV_DEBUG_DRIVER(dev, "PHY at ref rate: %lu\n", phy_ref_rate);
 	/* Save the new desired phy config */
 	memcpy(&dsi->phy_cfg, &new_cfg, sizeof(new_cfg));
+}
 
-	if (pm_runtime_resume_and_get(dev) < 0)
+static void
+nwl_dsi_bridge_atomic_pre_enable(struct drm_bridge *bridge,
+				 struct drm_bridge_state *old_bridge_state)
+{
+	struct nwl_dsi *dsi = bridge_to_dsi(bridge);
+	int ret;
+
+	if (pm_runtime_resume_and_get(dsi->dev) < 0)
 		return;
 
 	dsi->pdata->dpi_reset(dsi, true);
@@ -1019,26 +1012,33 @@ nwl_dsi_bridge_mode_set(struct drm_bridge *bridge,
 	/* Step 1 from DSI reset-out instructions */
 	ret = dsi->pdata->pclk_reset(dsi, false);
 	if (ret < 0) {
-		DRM_DEV_ERROR(dev, "Failed to deassert PCLK: %d\n", ret);
+		DRM_DEV_ERROR(dsi->dev, "Failed to deassert PCLK: %d\n", ret);
 		goto runtime_put;
 	}
 
 	/* Step 2 from DSI reset-out instructions */
-	nwl_dsi_mode_set(dsi);
+	nwl_dsi_enable(dsi);
 
 	/* Step 3 from DSI reset-out instructions */
 	ret = dsi->pdata->mipi_reset(dsi, false);
 	if (ret < 0) {
-		DRM_DEV_ERROR(dev, "Failed to deassert DSI: %d\n", ret);
+		DRM_DEV_ERROR(dsi->dev, "Failed to deassert DSI: %d\n", ret);
 		goto runtime_put;
 	}
 
-	dsi->modeset_done = true;
+	/*
+	 * We need to force call enable for the panel here, in order to
+	 * make the panel initialization execute before our call to
+	 * bridge_enable, where we will enable the DPI and start streaming
+	 * pixels on the data lanes.
+	 */
+	drm_atomic_bridge_chain_enable(dsi->panel_bridge,
+				       old_bridge_state->base.state);
 
 	return;
 
 runtime_put:
-	pm_runtime_put_sync(dev);
+	pm_runtime_put_sync(dsi->dev);
 }
 
 static void
