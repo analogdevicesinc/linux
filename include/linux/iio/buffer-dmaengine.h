a4ff35a6407674fbc909756ff5d38f7d6012d4e8
Paul Cercueil <paul@crapouillou.net>
2021-11-10 11:03:10 +0000

    iio: buffer-dmaengine: Support specifying buffer direction
    Update the devm_iio_dmaengine_buffer_setup() function to support
    specifying the buffer direction.
    
    Update the iio_dmaengine_buffer_submit() function to handle input
    buffers as well as output buffers.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- include/linux/iio/buffer-dmaengine.h
+++ include/linux/iio/buffer-dmaengine.h
@@ -7,6 +7,8 @@
 #ifndef __IIO_DMAENGINE_H__
 #define __IIO_DMAENGINE_H__
 
+#include <linux/iio/buffer.h>
+
 struct iio_dev;
 struct iio_dma_buffer_block;
 struct iio_dma_buffer_ops;
@@ -15,10 +17,12 @@ struct device;
 
 int devm_iio_dmaengine_buffer_setup(struct device *dev,
 				    struct iio_dev *indio_dev,
-				    const char *channel);
+				    const char *channel,
+				    enum iio_buffer_direction dir);
 int devm_iio_dmaengine_buffer_setup_with_ops(struct device *dev,
 					     struct iio_dev *indio_dev,
 					     const char *channel,
+					     enum iio_buffer_direction dir,
 					     const struct iio_dma_buffer_ops *ops);
 
 int iio_dmaengine_buffer_submit_block(struct iio_dma_buffer_queue *queue,
