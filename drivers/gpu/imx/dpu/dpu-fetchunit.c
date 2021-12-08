496e04ec1f85f3741dd37d081274f679e5c8f4cf
Liu Ying <victor.liu@nxp.com>
2021-12-08 15:21:44 +0800

    LF-5053 gpu: imx: dpu: fetchunit: Fix a potential integer overflow in fetchunit_set_baseaddress()
    This patch fixes a potential unintentional integer overflow in
    fetchunit_set_baseaddress() by applying a cast during calculation.
    
    This fixes Coverity issue: CID 16251658.
    
    Cc: Sandor Yu <Sandor.yu@nxp.com>
    Reviewed-by: Sandor Yu <Sandor.yu@nxp.com>
    Signed-off-by: Liu Ying <victor.liu@nxp.com>

--- drivers/gpu/imx/dpu/dpu-fetchunit.c
+++ drivers/gpu/imx/dpu/dpu-fetchunit.c
@@ -155,7 +155,7 @@ void fetchunit_set_baseaddress(struct dpu_fetchunit *fu, unsigned int width,
 							  baddr, nonzero_mod);
 
 		/* consider PRG y offset to calculate buffer address */
-		baddr += (y_offset % mt_h) * stride;
+		baddr += (dma_addr_t)(y_offset % mt_h) * stride;
 	}
 
 	mutex_lock(&fu->mutex);
