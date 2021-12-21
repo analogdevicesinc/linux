72d66d2680e47e0e0ba23aeb3ce3670c186906e4
Iulia Moldovan <iulia.moldovan@analog.com>
2021-12-21 14:56:20 +0000

    ad9783_dev: Add write of dac_dds_sel from HDL
    Signed-off-by: Iulia Moldovan <iulia.moldovan@analog.com>

--- drivers/iio/frequency/ad9783.c
+++ drivers/iio/frequency/ad9783.c
@@ -33,6 +33,24 @@ struct ad9783_phy {
 	struct ad9783_platform_data *pdata;
 };
 
+// similar to drivers/iio/frequency/m2k-dac.c: struct m2k_dac
+struct ad9783_dac {
+	void __iomem *regs;
+	struct clk *clk;
+
+	struct cf_axi_dds_state *dds;
+
+	struct mutex lock;
+
+	strct iio_dev *ch_indio_dev[2];
+}
+
+// similar to struct m2k_dac_ch
+struct ad9783_dac_ch {
+	struct m2k_dac *dac;
+	unsigned int num;
+}
+
 static const char ad9783_op_modes[2][16] = {"normal-baseband", "mix-mode"};
 
 enum ad9783_iio_dev_attr {
@@ -78,7 +96,6 @@ static int ad9783_write(struct spi_device *spi, unsigned reg, unsigned val)
 static int ad9783_setup(struct cf_axi_converter *conv)
 {
 	struct spi_device *spi = conv->spi;
-	int repeat, status;
 
 	/* Configure for the 4-wire SPI mode (bit 7) with MSB (bit 6). */
 	/* Software reset (bit 5) to default SPI values. */
@@ -91,31 +108,6 @@ static int ad9783_setup(struct cf_axi_converter *conv)
 	ad9783_write(spi, REG_SMP, 0x8); // 7 is the value from datasheet
 
 	return 0;
-	/* Set the common-mode voltage of DACCLK_P and DACCLK_N inputs */
-	//ad9783_write(spi, REG_CROSS_CNT1, CROSS_CNT1_CLKP_OFFSET(0xF));
-	//ad9783_write(spi, REG_CROSS_CNT2, CROSS_CNT2_CLKN_OFFSET(0xF));
-
-	/* Configure the Mu controller. */
-	//ad9783_write(spi, REG_PHS_DET, PHS_DET_CMP_BST | PHS_DET_PHS_DET_AUTO_EN);
-	//ad9783_write(spi, REG_MU_DUTY, MU_DUTY_MU_DUTY_AUTO_EN);
-	//ad9783_write(spi, REG_MU_CNT2, MU_CNT2_SRCH_MODE(2) | MU_CNT2_SET_PHS(4));
-	//ad9783_write(spi, REG_MU_CNT3, MU_CNT3_MUDEL(0x6C));
-
-	//for (repeat = 0; repeat < 3; repeat++) {
-	//	ad9783_write(spi, REG_MU_CNT4,
-	//	MU_CNT4_SEARCH_TOL | MU_CNT4_RETRY | MU_CNT4_GUARD(0xB));
-	//	ad9783_write(spi, REG_MU_CNT1, MU_CNT1_GAIN(1));
-	//	/* Enable the Mu controller search and track mode. */
-	//	ad9783_write(spi, REG_MU_CNT1, MU_CNT1_GAIN(1) | MU_CNT1_ENABLE);
-	//	mdelay(10);
-	//	status = ad9783_read(spi, REG_MU_STAT1);
-	//	if (status == MU_STAT1_MU_LKD)
-	//		return 0;
-	//}
-
-	//dev_err(&spi->dev, "Mu lock failure\n\r");
-
-	//return -1;
 }
 
 static int ad9783_set_fsc(struct cf_axi_converter *conv, u16 fsc_ua)
@@ -343,6 +335,126 @@ static struct ad9783_platform_data *ad9783_parse_dt(struct device *dev)
 }
 #endif
 
+/* Function to check if the registers that contain the result sum are equal to
+ * the known values
+ * */
+static bool ad9783_equal_bist_regs(struct spi_device *spi)
+{
+	int reg_1b, reg_1c;
+	int reg_1d, reg_1e;
+
+	// prepare BIST for reading operation; bring the sums into the registers
+	ad9783_write(spi, REG_BIST_CTRL, 0xC0);
+
+	reg_1b = ad9783_read(spi, REG_BIST_RES_1_LOW);
+	reg_1c = ad9783_read(spi, REG_BIST_RES_1_HIGH);
+	reg_1d = ad9783_read(spi, REG_BIST_RES_2_LOW);
+	reg_1e = ad9783_read(spi, REG_BIST_RES_2_HIGH);
+
+	return ((reg_1b == 0x8) && (reg_1c == 0x82)
+	&& (reg_1b == reg_1d) && (reg_1c == reg_1e));
+}
+
+/*
+ * Function that calibrates the device by trying multiple configurations for
+ * the Timing Adjust register (or SAMP_DLY[4:0] found at 0x05).
+ * */
+static int ad9783_prepare_bist(struct spi_device *spi,
+			       struct iio_dev *indio_dev)
+{
+	struct ad9783_dac_ch *ch = iio_priv(indio_dev);
+	struct ad9783_dac * ad9783_dac = ch->dac;
+
+	ad9783_write(spi, REG_DATA_CTRL, 0x80);
+	ad9783_write(spi, REG_SETUP_HOLD, 0x0);
+	//ad9783_write(spi, REG_SMP, 0x3);
+
+	//
+	ad9783_write(spi, REG_BIST_CTRL, 0x0);
+
+	// dac_dds_sel to send 0's
+	cf_axi_dds_datasel(ad9783_dac->dds, 0, DATA_SEL_ZERO);
+
+	// enable BIST
+	ad9783_write(spi, REG_BIST_CTRL, 0x20);
+	ad9783_write(spi, REG_BIST_CTRL, 0x0);
+	ad9783_write(spi, REG_BIST_CTRL, 0x80);
+
+	// dac_dds_sel to send PN23
+	cf_axi_dds_datasel(ad9783_dac->dds, 0, DATA_SEL_PNXX);
+
+	//ad9783_write(spi, REG_BIST_CTRL, 0xC0);
+	return 0;
+}
+
+static int ad9783_calibrate(struct cf_axi_converter *conv,
+			    struct iio_dev *ad9783)
+{
+	pr_err("\n\n\n----------> Intru in calibrare <----------\n\n\n");
+	struct spi_device *spi = conv->spi;
+	int smp_dly;
+	int reg_1b, reg_1c;
+	int reg_1d, reg_1e;
+	bool found = false;
+
+	// initially hardcoded
+	smp_dly = 3;
+	ad9783_prepare_bist(spi, ad9783_dev);
+
+	do {
+		while (!ad9783_equal_bist_regs(spi)) {
+			smp_dly++;
+			ad9783_write(spi, REG_SMP, smp_dly);
+			ad9783_prepare_bist(spi, ad9783_dev);
+		}
+
+		// if they are equal, then
+		for (int i = 0; i < 100; i++) {
+			ad9783_prepare_bist(spi, ad9783_dev);
+
+			if (!ad9783_equal_bist_regs(spi)) {
+				calibrated = false;
+				i = 1001;
+			}
+		}
+
+		if (calibrated) {
+			found = true;
+		}
+	} while (!found);
+
+	pr_err("\n\n\n----------> Found SMP_DLY=%d <----------\n\n\n", smp_dly);
+/*
+	bool calibrated = false;
+
+	while (!calibrated) {
+		smp_dly = ad9783_read(spi, REG_SMP);
+		// enable BIST read; prepare sums in the assigned registers
+	        ad9783_write(spi, REG_BIST_CTRL, 0xC0);
+		reg_1b = ad9783_read(spi, REG_BIST_RES_1_LOW);
+		reg_1c = ad9783_read(spi, REG_BIST_RES_1_HIGH);
+		reg_1d = ad9783_read(spi, REG_BIST_RES_2_LOW);
+		reg_1e = ad9783_read(spi, REG_BIST_RES_2_HIGH);
+
+		if ((reg_1b == 0x8) && (reg_1c == 0x82)
+		&& (reg_1b == reg_1d) && (reg_1c == reg_1e)) {
+			// rulez de 1000 de ori sa ma asigur ca nu crapa pe
+			// varianta asta, si daca e ok, atunci pot zice ca e calibrat
+
+			for (int i = 0; i < 1000; i++) {
+				ad9783_check_interface();
+			}
+		}
+		else {
+			smp_dly++;
+			ad9783_write(spi, REG_SMP, smp_dly);
+			calibrated = false;
+		}
+	}
+	return 0;
+*/
+}
+
 static int ad9783_probe(struct spi_device *spi)
 {
 	struct cf_axi_converter *conv;
@@ -411,9 +523,15 @@ static int ad9783_probe(struct spi_device *spi)
 
 	conv->clk[CLK_DAC] = clk;
 
-	ret = ad9783_setup(conv);
+	//ret = ad9783_setup(conv);
+
+	// AD9783 device related variables
+	struct iio_dev *indio_dev;
+
+
+	ret = ad9783_calibrate(conv);
 	if (ret < 0) {
-		dev_err(&spi->dev, "Failed to setup device\n");
+		dev_err(&spi->dev, "Failed to calibrate device\n");
 		goto out;
 	}
 
