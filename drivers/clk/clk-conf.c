228be6fbf0ef53434bc03da307f8b009c22ab6ae
Alexandru Tachici <alexandru.tachici@analog.com>
2022-01-06 18:45:09 +0200

    clk: clk-conf: allow user to setup an n-shot from DT
    Allow the user to setup an N-shot directly from DT by
    specifying the number of continuous pulses same as
    setting the clock rate.
    
    Signed-off-by: Alexandru Tachici <alexandru.tachici@analog.com>
    (cherry picked from commit e51ed899e04717dd8d53794222a0e6b35293ae75)

--- drivers/clk/clk-conf.c
+++ drivers/clk/clk-conf.c
@@ -116,6 +116,55 @@ static int __set_clk_rates(struct device_node *node, bool clk_supplier)
 	return 0;
 }
 
+static int __set_clk_nshot(struct device_node *node, bool clk_supplier)
+{
+	struct of_phandle_args clkspec;
+	struct property	*prop;
+	const __be32 *cur;
+	int rc, index = 0;
+	struct clk *clk;
+	u32 nshot;
+
+	of_property_for_each_u32(node, "assigned-clock-nshot", prop, cur, nshot) {
+		if (nshot) {
+			rc = of_parse_phandle_with_args(node, "assigned-clocks",
+							"#clock-cells",	index, &clkspec);
+			if (rc < 0) {
+				/* skip empty (null) phandles */
+				if (rc == -ENOENT)
+					continue;
+
+				return rc;
+			}
+
+			if (clkspec.np == node && !clk_supplier) {
+				of_node_put(clkspec.np);
+				return 0;
+			}
+
+			clk = of_clk_get_from_provider(&clkspec);
+			if (IS_ERR(clk)) {
+				if (PTR_ERR(clk) != -EPROBE_DEFER)
+					pr_err("clk: couldn't get clock %d for %pOF\n", index,
+					       node);
+				of_node_put(clkspec.np);
+				return PTR_ERR(clk);
+			}
+
+			rc = clk_set_nshot(clk, nshot);
+			if (rc < 0)
+				pr_warn("clk: couldn't set %s clk nshot to %u (%d), current nshot: %u\n",
+					__clk_get_name(clk), nshot, rc, clk_get_nshot(clk));
+
+			clk_put(clk);
+			of_node_put(clkspec.np);
+		}
+		index++;
+	}
+
+	return 0;
+}
+
 /**
  * of_clk_set_defaults() - parse and set assigned clocks configuration
  * @node: device node to apply clock settings for
@@ -139,6 +188,10 @@ int of_clk_set_defaults(struct device_node *node, bool clk_supplier)
 	if (rc < 0)
 		return rc;
 
-	return __set_clk_rates(node, clk_supplier);
+	rc = __set_clk_rates(node, clk_supplier);
+	if (rc < 0)
+		return rc;
+
+	return __set_clk_nshot(node, clk_supplier);
 }
 EXPORT_SYMBOL_GPL(of_clk_set_defaults);
