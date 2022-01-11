cee24549ba138f39784b7158570f41bacdfb044b
Laszlo Nagy <laszlo.nagy@analog.com>
2022-01-11 10:21:41 +0000

    iio: jesd204: axi_jesd204_rx: Move lane clock enable
    Lane clock enable triggers the physical layer reset de-assertion.
    This must happen only when the transmit side of the link is out of reset
    and sends valid characters that the receive side CDR circuitry can lock
    on.
    Assumption is that this happens in the clock enable phase or earlier
    of the transmit side of the link.
    
    Signed-off-by: Laszlo Nagy <laszlo.nagy@analog.com>
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    (cherry picked from commit 9bad1d35b461b3ad7bfd813e76dbd6ef3f100d36)

--- drivers/iio/jesd204/axi_jesd204_rx.c
+++ drivers/iio/jesd204/axi_jesd204_rx.c
@@ -912,14 +912,13 @@ static int axi_jesd204_rx_jesd204_clks_enable(struct jesd204_dev *jdev,
 	struct axi_jesd204_rx *jesd = dev_get_drvdata(dev);
 	int ret;
 
-	dev_dbg(dev, "%s:%d link_num %u reason %s\n", __func__, __LINE__, lnk->link_id, jesd204_state_op_reason_str(reason));
+	dev_dbg(dev, "%s:%d link_num %u reason %s\n", __func__, __LINE__,
+		lnk->link_id, jesd204_state_op_reason_str(reason));
 
 	switch (reason) {
 	case JESD204_STATE_OP_REASON_INIT:
 		break;
 	case JESD204_STATE_OP_REASON_UNINIT:
-		if (__clk_is_enabled(jesd->lane_clk)) /* REVIST */
-			clk_disable_unprepare(jesd->lane_clk);
 		if (__clk_is_enabled(jesd->device_clk))
 			clk_disable_unprepare(jesd->device_clk);
 		if (!IS_ERR(jesd->link_clk)) {
@@ -947,13 +946,6 @@ static int axi_jesd204_rx_jesd204_clks_enable(struct jesd204_dev *jdev,
 		}
 	}
 
-	ret = clk_prepare_enable(jesd->lane_clk);
-		if (ret) {
-		dev_err(dev, "%s: Link%u enable lane clock failed (%d)\n",
-			__func__, lnk->link_id, ret);
-		return ret;
-	}
-
 	return JESD204_STATE_CHANGE_DONE;
 }
 
@@ -963,8 +955,10 @@ static int axi_jesd204_rx_jesd204_link_enable(struct jesd204_dev *jdev,
 {
 	struct device *dev = jesd204_dev_to_device(jdev);
 	struct axi_jesd204_rx *jesd = dev_get_drvdata(dev);
+	int ret;
 
-	dev_dbg(dev, "%s:%d link_num %u reason %s\n", __func__, __LINE__, lnk->link_id, jesd204_state_op_reason_str(reason));
+	dev_dbg(dev, "%s:%d link_num %u reason %s\n", __func__, __LINE__,
+		lnk->link_id, jesd204_state_op_reason_str(reason));
 
 	switch (reason) {
 	case JESD204_STATE_OP_REASON_INIT:
@@ -974,10 +968,22 @@ static int axi_jesd204_rx_jesd204_link_enable(struct jesd204_dev *jdev,
 			cancel_delayed_work_sync(&jesd->watchdog_work);
 		else
 			disable_irq(jesd->irq);
+
 		writel_relaxed(0x1, jesd->base + JESD204_RX_REG_LINK_DISABLE);
+
+		if (__clk_is_enabled(jesd->lane_clk))
+			clk_disable_unprepare(jesd->lane_clk);
+
 		return JESD204_STATE_CHANGE_DONE;
 	default:
 		return JESD204_STATE_CHANGE_DONE;
+	}
+
+	ret = clk_prepare_enable(jesd->lane_clk);
+	if (ret) {
+		dev_err(dev, "%s: Link%u enable lane clock failed (%d)\n",
+			__func__, lnk->link_id, ret);
+		return ret;
 	}
 
 	writel_relaxed(0x3, jesd->base + JESD204_RX_REG_SYSREF_STATUS);
