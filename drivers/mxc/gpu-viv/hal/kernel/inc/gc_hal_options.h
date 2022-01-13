ecd6149c6f6fd2891d27234b8da4693aeb6a71ec
Xianzhong <xianzhong.li@nxp.com>
2022-01-13 15:39:21 +0800

    LF-5214 [#imx-2759] disable GPU command switch to avoid 8ULP hang
    GPU power management involve clock/power gating and command start/stop,
    the problem is caused by repeated GPU command start/stop quickly(<1ms).
    
    this patch disabled separate GPU clock and command switch from idle state,
    GPU clock and command still can gate off along with power off together.
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>
    Signed-off-by: Jiyu Yang <jiyu.yang@nxp.com>

--- drivers/mxc/gpu-viv/hal/kernel/inc/gc_hal_options.h
+++ drivers/mxc/gpu-viv/hal/kernel/inc/gc_hal_options.h
@@ -463,7 +463,7 @@ This define enables the use of VM for gckCommand and fence buffers.
         otherwise GPU will enter gcvPOWER_IDLE.
 */
 #ifndef gcdPOWER_SUSPEND_WHEN_IDLE
-#   define gcdPOWER_SUSPEND_WHEN_IDLE          1
+#   define gcdPOWER_SUSPEND_WHEN_IDLE          0
 #endif
 
 #ifndef gcdFPGA_BUILD
