9564a24c7cd72d0d8b4f3acab7091ec28ae1b0a0
Haibo Chen <haibo.chen@nxp.com>
2022-01-14 17:02:40 +0800

    LF-5346-1 iio: accel: add fxls8974cf support
    fxls8974cf is similar with fxls8962af, the only difference is the
    device id change to 0x86.
    
    Signed-off-by: Haibo Chen <haibo.chen@nxp.com>
    Reviewed-by: Clark Wang <xiaoning.wang@nxp.com>

--- drivers/iio/accel/fxls8962af-i2c.c
+++ drivers/iio/accel/fxls8962af-i2c.c
@@ -30,6 +30,7 @@ static int fxls8962af_probe(struct i2c_client *client)
 static const struct i2c_device_id fxls8962af_id[] = {
 	{ "fxls8962af", fxls8962af },
 	{ "fxls8964af", fxls8964af },
+	{ "fxls8974cf", fxls8974cf },
 	{}
 };
 MODULE_DEVICE_TABLE(i2c, fxls8962af_id);
@@ -37,6 +38,7 @@ MODULE_DEVICE_TABLE(i2c, fxls8962af_id);
 static const struct of_device_id fxls8962af_of_match[] = {
 	{ .compatible = "nxp,fxls8962af" },
 	{ .compatible = "nxp,fxls8964af" },
+	{ .compatible = "nxp,fxls8974cf" },
 	{}
 };
 MODULE_DEVICE_TABLE(of, fxls8962af_of_match);
