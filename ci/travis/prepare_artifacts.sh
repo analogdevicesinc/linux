e6ef91a176f34b2a7bdcf435cc64e10e38d8d537
Raluca Chis <raluca.chis@analog.com>
2022-03-17 08:36:47 +0200

    CI: change artifacts structure
    The structure of artifacts deployed to Artifactory and SWDownloads was
    changed and to deployed artifacts was added an archive
    rpi_modules.tar.gz containing modules files.
    
    Signed-off-by: Raluca Chis <raluca.chis@analog.com>
    (cherry picked from commit 0760fe9c263294b5b9181179712fc4d42981c2eb)

--- ci/travis/prepare_artifacts.sh
+++ ci/travis/prepare_artifacts.sh
@@ -1,8 +1,7 @@
-#!/bin/bash -e
 # SPDX-License-Identifier: (GPL-1.0-only OR BSD-2-Clause)
+#!/bin/bash -e
 
 timestamp=$(date +%Y_%m_%d-%H_%M)
-bcm_types='bcm2709 bcm2711 bcmrpi'
 
 #prepare the structure of the folder containing artifacts
 artifacts_structure() {
@@ -11,40 +10,54 @@ artifacts_structure() {
 
 	GIT_SHA=$(git rev-parse --short HEAD)
 	GIT_SHA_DATE=$(git show -s --format="%ci" ${GIT_SHA} | sed -e "s/ \|\:/-/g")
-	echo "git_sha=${GIT_SHA}" >> ${timestamp}/properties.txt
-	echo "git_sha_date=${GIT_SHA_DATE}" >> ${timestamp}/properties.txt
+	echo "git_sha=${GIT_SHA}" >> ${timestamp}/rpi_git_properties.txt
+	echo "git_sha_date=${GIT_SHA_DATE}" >> ${timestamp}/rpi_git_properties.txt
 
-	for bcm in $bcm_types; do
-		cd adi_${bcm}_defconfig
-		mkdir overlays
+	typeBCM=( "bcm2709" "bcm2711" "bcmrpi" )
+	typeKERNEL=( "kernel7" "kernel7l" "kernel" )
+	for index in "${!typeBCM[@]}"; do
+		cd adi_"${typeBCM[$index]}"_defconfig
+		mkdir overlays modules
 		mv ./*.dtbo ./overlays
-		if [ "${bcm}" = "bcm2709" ]; then
-			mv ./zImage ./kernel7.img
-		elif [ "${bcm}" = "bcm2711" ]; then
-			mv ./zImage ./kernel7l.img
-		elif [ "${bcm}" = "bcmrpi" ]; then
-			mv ./zImage ./kernel.img
-		fi
+		tar -xf rpi_modules.tar.gz -C modules
+		rm rpi_modules.tar.gz
+		mv ./zImage ./"${typeKERNEL[$index]}".img
 		cd ../
-		mv ./adi_${bcm}_defconfig ./${timestamp}/adi_${bcm}_defconfig
+		cp -r ./adi_"${typeBCM[$index]}"_defconfig/* ./${timestamp}
 	done
+	tar -C ${SOURCE_DIRECTORY}/${timestamp}/modules -cvf ${SOURCE_DIRECTORY}/${timestamp}/rpi_modules.tar.gz .
+	rm -r ${SOURCE_DIRECTORY}/${timestamp}/modules
 }
 
 #upload artifacts to Artifactory
 artifacts_artifactory() {
 	cd ${SOURCE_DIRECTORY}
-	python /root/myagent/_work/1/s/ci/travis/upload_to_artifactory.py --base_path="${ARTIFACTORY_PATH}" --server_path="linux_rpi/${BUILD_SOURCEBRANCHNAME}" --local_path="./${timestamp}" --token="${ARTIFACTORY_TOKEN}"
+	python /root/myagent/_work/1/s/ci/travis/upload_to_artifactory.py --base_path="${ARTIFACTORY_PATH}" \
+		--server_path="linux_rpi/${BUILD_SOURCEBRANCHNAME}" --local_path="./${timestamp}" --token="${ARTIFACTORY_TOKEN}"
 }
 
 #archive artifacts and upload to SWDownloads
 artifacts_swdownloads() {
 	cd ${SOURCE_DIRECTORY}/${timestamp}
 	chmod 600 ${KEY_FILE}
-	for bcm in $bcm_types; do
-		tar -zcvf adi_${bcm}_defconfig.tar adi_${bcm}_defconfig
-		scp -2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o HostKeyAlgorithms=+ssh-dss \
-		    -i ${KEY_FILE} -r ${SOURCE_DIRECTORY}/${timestamp}/adi_${bcm}_defconfig.tar ${DEST_SERVER}
-	done
+	scp -2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o HostKeyAlgorithms=+ssh-dss \
+		-i ${KEY_FILE} -r *.tar.gz ${DEST_SERVER}
+	md5_modules=($(md5sum rpi_modules.tar.gz| cut -d ' ' -f 1))
+
+	rm rpi_modules.tar.gz rpi_git_properties.txt
+	tar -C ${PWD} -cvf rpi_latest_boot.tar.gz *
+	md5_boot=($(md5sum rpi_latest_boot.tar.gz| cut -d ' ' -f 1))
+	scp -2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o HostKeyAlgorithms=+ssh-dss \
+	    -i ${KEY_FILE} -r rpi_latest_boot.tar.gz ${DEST_SERVER}
+
+	echo "boot_date=${timestamp}" >> rpi_archives_properties.txt
+	echo "https://swdownloads.analog.com/cse/linux_rpi/${BUILD_SOURCEBRANCHNAME}/rpi_modules.tar.gz" >> rpi_archives_properties.txt
+	echo "https://swdownloads.analog.com/cse/linux_rpi/${BUILD_SOURCEBRANCHNAME}/rpi_latest_boot.tar.gz" >> rpi_archives_properties.txt
+	echo "checksum_modules=${md5_modules}" >> rpi_archives_properties.txt
+	echo "checksum_boot_files=${md5_boot}" >> rpi_archives_properties.txt
+
+	scp -2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o HostKeyAlgorithms=+ssh-dss \
+                -i ${KEY_FILE} -r rpi_archives_properties.txt ${DEST_SERVER}
 }
 
 artifacts_${1}
