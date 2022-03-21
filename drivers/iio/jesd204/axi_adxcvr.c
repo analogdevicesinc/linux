4534d416efbccf61d406370048c7f925474f2c95
Dragos Bogdan <dragos.bogdan@analog.com>
2022-03-21 23:25:52 +0200

    iio: jesd204: axi_adxcvr: Use devm_clk_get_optional() for conv2
    Unlike devm_clk_get(), that returns -ENOENT when there is no clock
    producer, devm_clk_get_optional() returns NULL. Since in this case
    this is the exact expected behavior, use devm_clk_get_optional()
    instead.
    
    There are no functional changes, but the code will be simplified.
    
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>
    (cherry picked from commit a1dc4f0014d640020f277d2926ca7d858094116f)

--- drivers/iio/jesd204/axi_adxcvr.c
+++ drivers/iio/jesd204/axi_adxcvr.c
@@ -1024,12 +1024,9 @@ static int adxcvr_probe(struct platform_device *pdev)
 	 * Otional CPLL/QPLL REFCLK from a difference source
 	 * which rate and state must be in sync with the conv clk
 	 */
-	st->conv2_clk = devm_clk_get(&pdev->dev, "conv2");
-	if (IS_ERR(st->conv2_clk)) {
-		if (PTR_ERR(st->conv2_clk) != -ENOENT)
-			return PTR_ERR(st->conv2_clk);
-		st->conv2_clk = NULL;
-	}
+	st->conv2_clk = devm_clk_get_optional(&pdev->dev, "conv2");
+	if (IS_ERR(st->conv2_clk))
+		return PTR_ERR(st->conv2_clk);
 
 	st->lane_rate_div40_clk = devm_clk_get(&pdev->dev, "div40");
 	if (IS_ERR(st->lane_rate_div40_clk)) {
