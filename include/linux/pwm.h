c966a885d93c8667aacb72a335c5215a801c28b9
Sergiu Cuciurean <sergiu.cuciurean@analog.com>
2022-04-18 22:27:28 +0300

    include: linux: pwm.h: Fix pwm_apply_args
    This commit appends the phase specification to pwm_apply_args().
    
    Fixes: b2c4ca6bf594 ("drivers: pwm: core: Add offset support").
    Signed-off-by: Sergiu Cuciurean <sergiu.cuciurean@analog.com>
    (cherry picked from commit 6dda68a3579578eb2666fa22965cdd6f20705602)

--- include/linux/pwm.h
+++ include/linux/pwm.h
@@ -581,6 +581,7 @@ static inline void pwm_apply_args(struct pwm_device *pwm)
 	state.enabled = false;
 	state.polarity = pwm->args.polarity;
 	state.period = pwm->args.period;
+	state.phase = pwm->args.phase;
 
 	pwm_apply_state(pwm, &state);
 }
