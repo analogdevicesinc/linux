9e9668dd19e2db5815477c78f2ddcaa04241505e
Michael Hennerich <michael.hennerich@analog.com>
2022-04-27 13:39:48 +0200

    iio/jesd204/axi_jesd204_[rx|tx]: link running increase retry count
    This increases the retry count and gives the link a bit more time
    to enter DATA before we return error.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/jesd204/axi_jesd204_rx.c
+++ drivers/iio/jesd204/axi_jesd204_rx.c
@@ -1013,7 +1013,7 @@ static int axi_jesd204_rx_jesd204_link_running(struct jesd204_dev *jdev,
 	struct device *dev = jesd204_dev_to_device(jdev);
 	struct axi_jesd204_rx *jesd = dev_get_drvdata(dev);
 	unsigned int link_status;
-	int retry = 10;
+	int retry = 20;
 
 	dev_dbg(dev, "%s:%d link_num %u reason %s\n", __func__, __LINE__,
 		lnk->link_id, jesd204_state_op_reason_str(reason));
