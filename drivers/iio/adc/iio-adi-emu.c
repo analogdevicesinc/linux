1cd0aea59e70e2d21b8fe729e91085ac33e4811f
Dragos Bogdan <dragos.bogdan@analog.com>
2022-05-27 11:06:49 +0300

    iio: adc: adi-emu: Start defining the channels specifications
    iio:device0: iio-adi-emu
    	2 channels found:
    		voltage0:  (input)
    		1 channel-specific attributes found:
    			attr  0: raw value: 0
    		voltage1:  (input)
    		1 channel-specific attributes found:
    			attr  0: raw value: 1
    	1 device-specific attributes found:
    			attr  0: en value: 0
    	No trigger on this device
    
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- drivers/iio/adc/iio-adi-emu.c
+++ drivers/iio/adc/iio-adi-emu.c
@@ -9,7 +9,62 @@
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
 
+static int adi_emu_read_raw(struct iio_dev *indio_dev,
+			    struct iio_chan_spec const *chan,
+			    int *val,
+			    int *val2,
+			    long mask)
+{
+	switch (mask) {
+	case IIO_CHAN_INFO_ENABLE:
+		*val = 0;
+		return IIO_VAL_INT;
+	case IIO_CHAN_INFO_RAW:
+		if (chan->channel)
+			*val = 1;
+		else
+			*val = 0;
+		return IIO_VAL_INT;
+	}
+
+	return -EINVAL;
+}
+
+static int adi_emu_write_raw(struct iio_dev *indio_dev,
+			     struct iio_chan_spec const *chan,
+			     int val,
+			     int val2,
+			     long mask)
+{
+	switch (mask) {
+	case IIO_CHAN_INFO_ENABLE:
+		return 0;
+	}
+
+	return -EINVAL;
+}
+
 static const struct iio_info adi_emu_info = {
+	.read_raw = &adi_emu_read_raw,
+	.write_raw = &adi_emu_write_raw,
+};
+
+static const struct iio_chan_spec adi_emu_channels[] = {
+	{
+		.type = IIO_VOLTAGE,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+		.output = 0,
+		.indexed = 1,
+		.channel = 0,
+	}, {
+		.type = IIO_VOLTAGE,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+		.output = 0,
+		.indexed = 1,
+		.channel = 1,
+	}
 };
 
 static int adi_emu_probe(struct spi_device *spi)
@@ -21,6 +76,8 @@ static int adi_emu_probe(struct spi_device *spi)
 		return -ENOMEM;
 
 	indio_dev->name = "iio-adi-emu";
+	indio_dev->channels = adi_emu_channels;
+	indio_dev->num_channels = ARRAY_SIZE(adi_emu_channels);
 	indio_dev->info = &adi_emu_info;
 
 	return devm_iio_device_register(&spi->dev, indio_dev);
