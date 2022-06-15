cddcad30afb50574a7f969bce012ee6b55ab31b4
Sergiu Cuciurean <sergiu.cuciurean@analog.com>
2022-06-15 09:48:36 +0300

    drivers: usb: chipidea: Let the PHY set VBUS
    In the PHY datasheet, the typical application for Host or OTG
    implies a regulator controlled by the PHY's CPEN port.
    This change removes the Xilinx's fix leftovers and sets VBUS
    in the correct way.
    
    Signed-off-by: Sergiu Cuciurean <sergiu.cuciurean@analog.com>

--- drivers/usb/chipidea/otg_fsm.c
+++ drivers/usb/chipidea/otg_fsm.c
@@ -473,8 +473,8 @@ static void ci_otg_drv_vbus(struct otg_fsm *fsm, int on)
 		}
 
 		if (ci->platdata->flags & CI_HDRC_PHY_VBUS_CONTROL &&
-				ci->usb_phy && ci->usb_phy->set_vbus)
-			ci->usb_phy->set_vbus(ci->usb_phy, 1);
+		    ci->usb_phy)
+			ci->usb_phy->otg->set_vbus(ci->usb_phy->otg, 1);
 
 		/* Disable data pulse irq */
 		hw_write_otgsc(ci, OTGSC_DPIE, 0);
@@ -486,8 +486,8 @@ static void ci_otg_drv_vbus(struct otg_fsm *fsm, int on)
 			regulator_disable(ci->platdata->reg_vbus);
 
 		if (ci->platdata->flags & CI_HDRC_PHY_VBUS_CONTROL &&
-				ci->usb_phy && ci->usb_phy->set_vbus)
-			ci->usb_phy->set_vbus(ci->usb_phy, 0);
+		    ci->usb_phy)
+			ci->usb_phy->otg->set_vbus(ci->usb_phy->otg, 0);
 
 		fsm->a_bus_drop = 1;
 		fsm->a_bus_req = 0;
