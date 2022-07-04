3b696f5c0a6b46edc423650a9b929a9e9b3c9d5d
Dragos Bogdan <dragos.bogdan@analog.com>
2022-07-04 17:16:37 +0300

    iio: adc: ad9081: Remove "adi_utils.h" include
    Not available but not used either.
    
    Fixes:
       fatal error: adi_utils.h: No such file or directory
    
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- drivers/iio/adc/ad9081/adi_ad9081_jesd.c
+++ drivers/iio/adc/ad9081/adi_ad9081_jesd.c
@@ -16,7 +16,6 @@
 /*============= I N C L U D E S ============*/
 #include "adi_ad9081_config.h"
 #include "adi_ad9081_hal.h"
-#include "adi_utils.h"
 
 #define NELEMS(x) (sizeof(x) / sizeof((x)[0]))
 /*============= C O D E ====================*/
