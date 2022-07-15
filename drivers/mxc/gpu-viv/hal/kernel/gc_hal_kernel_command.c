c08b76e23a6eae1fbe21a4a5a08441f44fcf81d7
yuan.tian <yuan.tian@nxp.com>
2022-07-15 12:04:28 +0800

    MGS-6739 [#imx-2760] 0030-CL498981-KERNEL-SPACE-dev64x-fix-the-dump-command-bu
    dev64x: fix the dump command
     buffer address dismatch
    
    Signed-off-by: Ke Feng <ke.feng@verisilicon.com>
    
    Signed-off-by: yuan.tian <yuan.tian@nxp.com>

--- drivers/mxc/gpu-viv/hal/kernel/gc_hal_kernel_command.c
+++ drivers/mxc/gpu-viv/hal/kernel/gc_hal_kernel_command.c
@@ -4393,7 +4393,7 @@ gckCOMMAND_DumpExecutingBuffer(
         gcmkPRINT("Command buffer around 0x%08X:", gpuAddress);
 
         /* Align to 4096. */
-        offset &= 0xfffff000;
+        offset -= (gpuAddress & 0x00000fff);
         gpuAddress &= 0xfffff000;
 
         /* Dump max 4096 bytes. */
