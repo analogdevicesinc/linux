42e83743fcf9f0c06b76cb0a906cb978f0a96a83
Peter Xu <peterx@redhat.com>
2022-08-11 12:13:31 -0400

    mm/swap: cache swap migration A/D bits support
    Introduce a variable swap_migration_ad_supported to cache whether the arch
    supports swap migration A/D bits.
    
    Here one thing to mention is that SWP_MIG_TOTAL_BITS will internally
    reference the other macro MAX_PHYSMEM_BITS, which is a function call on
    x86 (constant on all the rest of archs).
    
    It's safe to reference it in swapfile_init() because when reaching here
    we're already during initcalls level 4 so we must have initialized 5-level
    pgtable for x86_64 (right after early_identify_cpu() finishes).
    
    - start_kernel
      - setup_arch
        - early_cpu_init
          - get_cpu_cap --> fetch from CPUID (including X86_FEATURE_LA57)
          - early_identify_cpu --> clear X86_FEATURE_LA57 (if early lvl5 not enabled (USE_EARLY_PGTABLE_L5))
      - arch_call_rest_init
        - rest_init
          - kernel_init
            - kernel_init_freeable
              - do_basic_setup
                - do_initcalls --> calls swapfile_init() (initcall level 4)
    
    This should slightly speed up the migration swap entry handlings.
    
    Link: https://lkml.kernel.org/r/20220811161331.37055-8-peterx@redhat.com
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Cc: Alistair Popple <apopple@nvidia.com>
    Cc: Andi Kleen <andi.kleen@intel.com>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Huang Ying <ying.huang@intel.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: "Kirill A . Shutemov" <kirill@shutemov.name>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Nadav Amit <nadav.amit@gmail.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/swapfile.h
+++ include/linux/swapfile.h
@@ -10,5 +10,7 @@ extern struct swap_info_struct *swap_info[];
 extern unsigned long generic_max_swapfile_size(void);
 /* Maximum swapfile size supported for the arch (not inclusive). */
 extern unsigned long swapfile_maximum_size;
+/* Whether swap migration entry supports storing A/D bits for the arch */
+extern bool swap_migration_ad_supported;
 
 #endif /* _LINUX_SWAPFILE_H */
