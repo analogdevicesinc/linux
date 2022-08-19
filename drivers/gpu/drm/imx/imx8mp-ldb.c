811b231c8d8c3462bf46c7035339529ee9dc7c82
Liu Ying <victor.liu@nxp.com>
2022-08-19 13:30:56 +0800

    LF-6946-1 drm/imx: imx8mp-ldb: Initialize encoder only for enabled LVDS channel
    We should not initialize encoders for both LVDS channels and then
    cleanup encoder for disabled LVDS channel, because the encoder index
    of the second LVDS channel(enabled in device tree) would be reused
    by an upcoming encoder if the first LVDS channel is disabled in device
    tree. So, ensure LVDS channel is enabled in device tree first, and then
    initialize encoder for it.
    
    Reported-by: Arun Baskar Gnanapragasam <arunbaskar.gnanapragasam@nxp.com>
    Cc: Sandor Yu <Sandor.yu@nxp.com>
    Reviewed-by: Sandor Yu <Sandor.yu@nxp.com>
    Signed-off-by: Liu Ying <victor.liu@nxp.com>

--- drivers/gpu/drm/imx/imx8mp-ldb.c
+++ drivers/gpu/drm/imx/imx8mp-ldb.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright 2020 NXP
+ * Copyright 2020,2022 NXP
  */
 
 #include <linux/clk.h>
@@ -259,7 +259,14 @@ imx8mp_ldb_bind(struct device *dev, struct device *master, void *data)
 	if (IS_ERR(imx8mp_ldb->clk_root))
 		return PTR_ERR(imx8mp_ldb->clk_root);
 
-	for (i = 0; i < LDB_CH_NUM; i++) {
+	for_each_child_of_node(np, child) {
+		ret = of_property_read_u32(child, "reg", &i);
+		if (ret || i < 0 || i > 1)
+			return -EINVAL;
+
+		if (!of_device_is_available(child))
+			continue;
+
 		encoder[i] = &imx8mp_ldb->channel[i].encoder;
 
 		drm_encoder_helper_add(encoder[i],
@@ -316,10 +323,9 @@ get_phy:
 
 	for (i = 0; i < LDB_CH_NUM; i++) {
 		ldb_ch = &imx8mp_ldb->channel[i].base;
-		if (!ldb_ch->is_valid) {
-			drm_encoder_cleanup(encoder[i]);
+
+		if (!ldb_ch->is_valid)
 			continue;
-		}
 
 		ret = imx_drm_encoder_parse_of(drm, encoder[i], ldb_ch->child);
 		if (ret)
