610d2f7f3969f414d2042693b32e1b6c9a914aa0
Alice Guo <alice.guo@nxp.com>
2022-09-09 15:37:16 +0800

    LF-7056-1 thermal: qoriq: add i.MX93 TMU support
    The procedure to program the calibration table of i.MX93 TMU:
    1. disable monitoring mode
    2. configure TCMCFG
    3. write TTCFGR with TTCFGR[CAL_PT] = n
    4. write TSCFGR with the sensor value of the calibration point n
    5. write TTRCRn associated with the calibration point n
    Need to repeat steps 3—5 for all calibration points from the calibration
    table provided by the TMU device node.
    
    When TRITSR[V] = 1 and TRITSR[TP5] = 1, need to add 0.5K to
    TRITSR[TEMP].
    
    Squashed:
    qoriq_thermal: No need to program site adjustment register
    LF-7417 thermal: qoriq: fix the bug that TMU does not work on lx2
    LF-8337 thermal: qoriq: configure the monitoring site select according to actual use
    LF-9443 thermal: qoriq: Disable tmu central module
    LF-10683-01 thermal: imx: Remove the redundant code used by imx93 tmu
    
    Signed-off-by: Alice Guo <alice.guo@nxp.com>
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>
    Signed-off-by: Pankit Garg <pankit.garg@nxp.com>
    Acked-by: Jason Liu <jason.hui.liu@nxp.com>

--- drivers/thermal/qoriq_thermal.c
+++ drivers/thermal/qoriq_thermal.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 //
 // Copyright 2016 Freescale Semiconductor, Inc.
+// Copyright 2022 NXP
 
 #include <linux/clk.h>
 #include <linux/err.h>
@@ -18,14 +19,17 @@
 #define SITES_MAX		16
 #define TMR_DISABLE		0x0
 #define TMR_ME			0x80000000
+#define TMR_CMD			BIT(29)
 #define TMR_ALPF		0x0c000000
 #define TMR_ALPF_V2		0x03000000
 #define TMTMIR_DEFAULT	0x0000000f
 #define TIER_DISABLE	0x0
 #define TEUMR0_V2		0x51009c00
+#define TEUMR0_V21		0x55010c00
 #define TMSARA_V2		0xe
 #define TMU_VER1		0x1
 #define TMU_VER2		0x2
+#define TMU_VER93		0x3
 
 #define REGS_TMR	0x000	/* Mode Register */
 #define TMR_DISABLE	0x0
@@ -231,6 +235,9 @@ static void qoriq_tmu_init_device(struct qoriq_tmu_data *data)
 
 	if (data->ver == TMU_VER1) {
 		regmap_write(data->regmap, REGS_TMTMIR, TMTMIR_DEFAULT);
+	} else if (data->ver == TMU_VER93) {
+		regmap_write(data->regmap, REGS_V2_TMTMIR, TMTMIR_DEFAULT);
+		regmap_write(data->regmap, REGS_V2_TEUMR(0), TEUMR0_V21);
 	} else {
 		regmap_write(data->regmap, REGS_V2_TMTMIR, TMTMIR_DEFAULT);
 		regmap_write(data->regmap, REGS_V2_TEUMR(0), TEUMR0_V2);
@@ -330,6 +337,9 @@ static int qoriq_tmu_probe(struct platform_device *pdev)
 	}
 	data->ver = (ver >> 8) & 0xff;
 
+	if (of_find_compatible_node(NULL, NULL, "fsl,imx93-tmu"))
+		data->ver = TMU_VER93;
+
 	qoriq_tmu_init_device(data);	/* TMU initialization */
 
 	ret = qoriq_tmu_calibration(dev, data);	/* TMU calibration */
@@ -356,6 +366,12 @@ static int qoriq_tmu_suspend(struct device *dev)
 	if (ret)
 		return ret;
 
+	if (data->ver == TMU_VER93) {
+		ret = regmap_update_bits(data->regmap, REGS_TMR, TMR_CMD, TMR_CMD);
+		if (ret)
+			return ret;
+	}
+
 	clk_disable_unprepare(data->clk);
 
 	return 0;
@@ -370,6 +386,12 @@ static int qoriq_tmu_resume(struct device *dev)
 	if (ret)
 		return ret;
 
+	if (data->ver == TMU_VER93) {
+		ret = regmap_update_bits(data->regmap, REGS_TMR, TMR_CMD, 0);
+		if (ret)
+			return ret;
+	}
+
 	/* Enable monitoring */
 	return regmap_update_bits(data->regmap, REGS_TMR, TMR_ME, TMR_ME);
 }
@@ -380,6 +402,7 @@ static DEFINE_SIMPLE_DEV_PM_OPS(qoriq_tmu_pm_ops,
 static const struct of_device_id qoriq_tmu_match[] = {
 	{ .compatible = "fsl,qoriq-tmu", },
 	{ .compatible = "fsl,imx8mq-tmu", },
+	{ .compatible = "fsl,imx93-tmu", },
 	{},
 };
 MODULE_DEVICE_TABLE(of, qoriq_tmu_match);
