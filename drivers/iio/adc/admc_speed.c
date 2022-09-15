2cb6ca984aa84fbb97f5448bab3ef97cb2e81317
Nuno Sá <nuno.sa@analog.com>
2022-09-15 16:49:08 +0200

    iio: adc: admc_speed: move to device managed probe
    Use device managed APIs during probe so that there's no need to have
    '.remove()' hook on the driver.
    
    Signed-off-by: Nuno Sá <nuno.sa@analog.com>

--- drivers/iio/adc/admc_speed.c
+++ drivers/iio/adc/admc_speed.c
@@ -109,8 +109,8 @@ static int axiadc_configure_ring_stream(struct iio_dev *indio_dev,
 	if (dma_name == NULL)
 		dma_name = "rx";
 
-	buffer = iio_dmaengine_buffer_alloc(indio_dev->dev.parent, dma_name,
-			&axiadc_dma_buffer_ops, indio_dev);
+	buffer = devm_iio_dmaengine_buffer_alloc(indio_dev->dev.parent, dma_name,
+						 &axiadc_dma_buffer_ops, indio_dev);
 	if (IS_ERR(buffer))
 		return PTR_ERR(buffer);
 
@@ -118,11 +118,6 @@ static int axiadc_configure_ring_stream(struct iio_dev *indio_dev,
 	iio_device_attach_buffer(indio_dev, buffer);
 
 	return 0;
-}
-
-static void axiadc_unconfigure_ring_stream(struct iio_dev *indio_dev)
-{
-	iio_dmaengine_buffer_free(indio_dev->buffer);
 }
 
 static int axiadc_reg_access(struct iio_dev *indio_dev,
@@ -231,28 +226,13 @@ static int axiadc_probe(struct platform_device *pdev)
 	if (ret < 0)
 		return ret;
 
-	ret = iio_device_register(indio_dev);
+	ret = devm_iio_device_register(&pdev->dev, indio_dev);
 	if (ret)
-		goto err_unconfigure_ring;
+		return ret;
 
 	dev_info(&pdev->dev, "ADI AIM (0x%X) at 0x%08llX mapped to 0x%p, probed ADC %s as %s\n",
 		 st->pcore_version, (unsigned long long)mem->start, st->regs,
 		 chip_info->name, axiadc_read(st, ADI_AXI_REG_ID) ? "SLAVE" : "MASTER");
-
-	return 0;
-
-err_unconfigure_ring:
-	axiadc_unconfigure_ring_stream(indio_dev);
-
-	return ret;
-}
-
-static int axiadc_remove(struct platform_device *pdev)
-{
-	struct iio_dev *indio_dev = platform_get_drvdata(pdev);
-
-	iio_device_unregister(indio_dev);
-	axiadc_unconfigure_ring_stream(indio_dev);
 
 	return 0;
 }
@@ -271,7 +251,6 @@ static struct platform_driver axiadc_of_driver = {
 		.of_match_table = axiadc_of_match,
 	},
 	.probe	  = axiadc_probe,
-	.remove	 = axiadc_remove,
 };
 
 module_platform_driver(axiadc_of_driver);
