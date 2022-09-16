4d42fc27e8d1b37bb7046cace3d7f8cafe815dcc
Nuno Sá <nuno.sa@analog.com>
2022-09-16 10:28:31 +0200

    iio: buffer: dmaengine: make iio_dmaengine_buffer_alloc() private
    Now that all users are using the device managed variant, we can turn
    iio_dmaengine_buffer_alloc() private and move a bit closer to what we
    have upstream.
    
    Naturally the same applies to iio_dmaengine_buffer_free().
    
    Signed-off-by: Nuno Sá <nuno.sa@analog.com>
    (cherry picked from commit 21a88cb29186148047515e1ec844ef237386bcf9)

--- include/linux/iio/buffer-dmaengine.h
+++ include/linux/iio/buffer-dmaengine.h
@@ -16,12 +16,6 @@ struct iio_dma_buffer_queue;
 int iio_dmaengine_buffer_submit_block(struct iio_dma_buffer_queue *queue,
 	struct iio_dma_buffer_block *block, int direction);
 void iio_dmaengine_buffer_abort(struct iio_dma_buffer_queue *queue);
-void iio_dmaengine_buffer_free(struct iio_buffer *buffer);
-
-struct iio_buffer *iio_dmaengine_buffer_alloc(struct device *dev,
-					      const char *channel,
-					      const struct iio_dma_buffer_ops *ops,
-					      void *data);
 
 struct iio_buffer *devm_iio_dmaengine_buffer_alloc(struct device *dev,
 						   const char *channel,
