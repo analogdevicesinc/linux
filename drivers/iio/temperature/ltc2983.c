c773cc0b9d9fe8a2a584ddde5337e79966dac828
Cosmin Tanislav <cosmin.tanislav@analog.com>
2022-10-20 11:31:09 +0300

    Revert "iio: temp: ltc2983: Fix alignment for DMA safety"
    This reverts commit 182a99c0c0b7b375e62a0369163fbb33d447e583.

--- drivers/iio/temperature/ltc2983.c
+++ drivers/iio/temperature/ltc2983.c
@@ -228,11 +228,11 @@ struct ltc2983_data {
 	u8 iio_channels;
 	bool suspend;
 	/*
-	 * DMA (thus cache coherency maintenance) may require the
+	 * DMA (thus cache coherency maintenance) requires the
 	 * transfer buffers to live in their own cache lines.
 	 * Holds the converted temperature
 	 */
-	__be32 temp __aligned(IIO_DMA_MINALIGN);
+	__be32 temp ____cacheline_aligned;
 	__be32 chan_val;
 	__be32 eeprom_key;
 };
