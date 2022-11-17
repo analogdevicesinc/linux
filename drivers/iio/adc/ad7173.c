c9c5bb191acccb8bbebdb8a65abd396320098deb
dumitruceclan <dumitru.ceclan@analog.com>
2022-11-17 11:06:24 +0200

    iio: adc: ad7173: Fix input buffers not enabled
    Fixed enabling input buffers by default.
    Reading voltage inputs require input buffers to be enabled, not all chips from this family have them enabled by default. AD7176-2 does not have buffers.
    
    Signed-off-by: Ceclan Dumitru-Ioan <dumitru.ceclan@analog.com>

--- drivers/iio/adc/ad7173.c
+++ drivers/iio/adc/ad7173.c
@@ -419,6 +419,7 @@ static struct ad7173_state *ad_sigma_delta_to_ad7173(struct ad_sigma_delta *sd)
 static int ad7173_prepare_channel(struct ad_sigma_delta *sd, unsigned int slot,
 	const struct iio_chan_spec *chan)
 {
+	struct ad7173_state *st = ad_sigma_delta_to_ad7173(sd);
 	unsigned int config;
 
 	if (st->reg)
@@ -428,6 +429,9 @@ static int ad7173_prepare_channel(struct ad_sigma_delta *sd, unsigned int slot,
 
 	if (chan->differential)
 		config |= AD7173_SETUP_BIPOLAR;
+
+	if (st->info->id != AD7176_ID)
+		config |= AD7173_SETUP_AIN_BUF;
 
 	return ad_sd_write_reg(sd, AD7173_REG_SETUP(slot), 2, config);
 }
