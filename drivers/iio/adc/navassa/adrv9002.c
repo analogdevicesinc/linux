analogdevicesinc/linux

commit add8026d42254f4ff12715acb317d59287b5dae4

iio: adrv9002: support RX compensated gain tables
Now that the RX gain table type is being provided by the loaded profile,
we can properly support it. Hence, depending on the configured gain
table selected in the profile, we load the chosen one at setup time.
The table type is also being reported when reading the profile_config
attribute.

Signed-off-by: Nuno Sá <nuno.sa@analog.com>

drivers/iio/adc/navassa/adrv9002.c
@@ -2656,6 +2656,7 @@ static int adrv9002_digital_init(struct adrv9002_rf_phy *phy)
 		struct adrv9002_rx_chan *rx = &phy->rx_channels[c];
 		struct adrv9002_tx_chan *tx = &phy->tx_channels[c];
 		adi_adrv9001_RxProfile_t *p = &rx_cfg[c].profile;
+		const char *rx_table;
 
 		if (rx->orx_en || tx->channel.enabled) {
 			ret = adi_adrv9001_Utilities_RxGainTable_Load(phy->adrv9001, ADI_ORX,
@@ -2672,9 +2673,10 @@ static int adrv9002_digital_init(struct adrv9002_rf_phy *phy)
 		if (!rx->channel.enabled)
 			continue;
 
-		ret = adi_adrv9001_Utilities_RxGainTable_Load(phy->adrv9001, ADI_RX,
-							      "RxGainTable.csv", rx->channel.number,
-							      &p->lnaConfig, t_type);
+		rx_table = p->gainTableType ? "RxGainTable_GainCompensated.csv" : "RxGainTable.csv";
+		ret = adi_adrv9001_Utilities_RxGainTable_Load(phy->adrv9001, ADI_RX, rx_table,
+							      rx->channel.number, &p->lnaConfig,
+							      t_type);
 		if (ret)
 			return adrv9002_dev_err(phy);
 	}
@@ -4281,6 +4283,11 @@ const char *const mcs[] = {
 	"Enabled RFPLL Phase"
 };
 
+const char *const rx_gain_type[] = {
+	"Correction",
+	"Compensated"
+};
+
 static ssize_t adrv9002_profile_bin_read(struct file *filp, struct kobject *kobj,
 					 struct bin_attribute *bin_attr, char *buf, loff_t pos,
 					 size_t count)
@@ -4301,6 +4308,8 @@ static ssize_t adrv9002_profile_bin_read(struct file *filp, struct kobject *kobj
 		       "RX2 LO: %s\n"
 		       "TX1 LO: %s\n"
 		       "TX2 LO: %s\n"
+		       "RX1 Gain Table Type: %s\n"
+		       "RX2 Gain Table Type: %s\n"
 		       "RX Channel Mask: 0x%x\n"
 		       "TX Channel Mask: 0x%x\n"
 		       "Duplex Mode: %s\n"
@@ -4310,6 +4319,8 @@ static ssize_t adrv9002_profile_bin_read(struct file *filp, struct kobject *kobj
 		       clks->clkPllVcoFreq_daHz * 10ULL, clks->armPowerSavingClkDiv,
 		       lo_maps[clks->rx1LoSelect], lo_maps[clks->rx2LoSelect],
 		       lo_maps[clks->tx1LoSelect], lo_maps[clks->tx2LoSelect],
+		       rx_gain_type[rx->rxChannelCfg[ADRV9002_CHANN_1].profile.gainTableType],
+		       rx_gain_type[rx->rxChannelCfg[ADRV9002_CHANN_2].profile.gainTableType],
 		       rx->rxInitChannelMask, tx->txInitChannelMask, duplex[sys->duplexMode],
 		       sys->fhModeOn, mcs[sys->mcsMode], ssi[phy->ssi_type]);
 

commit f334128fea1b2405771bab8033d54d4c77ea100f

iio: adc: adrv9002: properly set gain table type
The correct table type (compensated vs correction) must be set when
calling adi_adrv9001_Utilities_RxGainTable_Load() and
adi_adrv9001_Utilities_RxGainTable_Load().

Fixes: add8026d4225 ("iio: adrv9002: support RX compensated gaintables")
Signed-off-by: Nuno Sá <nuno.sa@analog.com>

drivers/iio/adc/navassa/adrv9002.c
@@ -2726,16 +2726,6 @@ static int adrv9002_digital_init(struct adrv9002_rf_phy *phy)
 	int ret;
 	u8 tx_mask = 0;
 	int c;
-	/*
-	 * There's still no way of getting the gain table type from the profile. We
-	 * always get the correction one (which was the one we were using already).
-	 * There were some talks and this might change in future versions of the DDAPI so,
-	 * let's force this to correction for now and wait a few release cycles for
-	 * proper support. If we do not get it, we might just add a devicetree attribute
-	 * or some runtime sysfs attr. Not ideal but we won't have any choice if we can't
-	 * get this info from the profile.
-	 */
-	adi_adrv9001_RxGainTableType_e t_type = ADI_ADRV9001_RX_GAIN_CORRECTION_TABLE;
 	adi_adrv9001_RxChannelCfg_t *rx_cfg = phy->curr_profile->rx.rxChannelCfg;
 
 	ret = adi_adrv9001_arm_AhbSpiBridge_Enable(phy->adrv9001);
@@ -2777,7 +2767,16 @@ static int adrv9002_digital_init(struct adrv9002_rf_phy *phy)
 		struct adrv9002_rx_chan *rx = &phy->rx_channels[c];
 		struct adrv9002_tx_chan *tx = &phy->tx_channels[c];
 		adi_adrv9001_RxProfile_t *p = &rx_cfg[c].profile;
+		adi_adrv9001_RxGainTableType_e t_type;
 		const char *rx_table;
+
+		if (p->gainTableType) {
+			rx_table = "RxGainTable_GainCompensated.csv";
+			t_type = ADI_ADRV9001_RX_GAIN_COMPENSATION_TABLE;
+		} else {
+			rx_table = "RxGainTable.csv";
+			t_type = ADI_ADRV9001_RX_GAIN_CORRECTION_TABLE;
+		}
 
 		if (rx->orx_en || tx->channel.enabled) {
 			ret = adi_adrv9001_Utilities_RxGainTable_Load(phy->adrv9001, ADI_ORX,
@@ -2794,7 +2793,6 @@ static int adrv9002_digital_init(struct adrv9002_rf_phy *phy)
 		if (!rx->channel.enabled)
 			continue;
 
-		rx_table = p->gainTableType ? "RxGainTable_GainCompensated.csv" : "RxGainTable.csv";
 		ret = adi_adrv9001_Utilities_RxGainTable_Load(phy->adrv9001, ADI_RX, rx_table,
 							      rx->channel.number, &p->lnaConfig,
 							      t_type);