ac7c9ee51b9ae009a64a0802a2fc01aaa846326d
Ramona Bolboaca <ramona.bolboaca@analog.com>
2022-11-24 10:25:26 +0200

    iio: adis: add '__adis_enable_irq()' implementation Add '__adis_enable_irq()' implementation which is the unlocked version of 'adis_enable_irq()'. Call '__adis_enable_irq()' instead of 'adis_enable_irq()' from '__adis_intial_startup()' to keep the expected unlocked functionality.
    This fix is needed to remove a deadlock for all devices which are
    using 'adis_initial_startup()'. The deadlock occurs because the
    same mutex is acquired twice, without releasing it.
    The mutex is acquired once inside 'adis_initial_startup()', before
    calling '__adis_initial_startup()', and once inside
    'adis_enable_irq()', which is called by '__adis_initial_startup()'.
    The deadlock is removed by calling '__adis_enable_irq()', instead of
    'adis_enable_irq()' from within '__adis_initial_startup()'.
    
    Fixes: b600bd7eb3335 ("iio: adis: do not disabe IRQs in 'adis_init()'")
    Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>
    Reviewed-by: Nuno Sá <nuno.sa@analog.com>
    Link: https://lore.kernel.org/r/20221122082757.449452-2-ramona.bolboaca@analog.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/imu/adis.c
+++ drivers/iio/imu/adis.c
@@ -274,34 +274,33 @@ EXPORT_SYMBOL(adis_debugfs_reg_access);
 #endif
 
 /**
- * adis_enable_irq() - Enable or disable data ready IRQ
+ * __adis_enable_irq() - Enable or disable data ready IRQ (unlocked)
  * @adis: The adis device
  * @enable: Whether to enable the IRQ
  *
  * Returns 0 on success, negative error code otherwise
  */
-int adis_enable_irq(struct adis *adis, bool enable)
+int __adis_enable_irq(struct adis *adis, bool enable)
 {
-	int ret = 0;
+	int ret;
 	uint16_t msc;
 
-	mutex_lock(&adis->state_lock);
 
-	if (adis->data->enable_irq) {
-		ret = adis->data->enable_irq(adis, enable);
-		goto out_unlock;
-	} else if (adis->data->unmasked_drdy) {
+	if (adis->data->enable_irq)
+		return adis->data->enable_irq(adis, enable);
+
+	if (adis->data->unmasked_drdy) {
 		if (enable)
 			enable_irq(adis->spi->irq);
 		else
 			disable_irq(adis->spi->irq);
 
-		goto out_unlock;
+		return 0;
 	}
 
 	ret = __adis_read_reg_16(adis, adis->data->msc_ctrl_reg, &msc);
 	if (ret)
-		goto out_unlock;
+		return ret;
 
 	msc |= ADIS_MSC_CTRL_DATA_RDY_POL_HIGH;
 	msc &= ~ADIS_MSC_CTRL_DATA_RDY_DIO2;
@@ -310,13 +309,9 @@ int adis_enable_irq(struct adis *adis, bool enable)
 	else
 		msc &= ~ADIS_MSC_CTRL_DATA_RDY_EN;
 
-	ret = __adis_write_reg_16(adis, adis->data->msc_ctrl_reg, msc);
-
-out_unlock:
-	mutex_unlock(&adis->state_lock);
-	return ret;
+	return __adis_write_reg_16(adis, adis->data->msc_ctrl_reg, msc);
 }
-EXPORT_SYMBOL(adis_enable_irq);
+EXPORT_SYMBOL(__adis_enable_irq);
 
 /**
  * __adis_check_status() - Check the device for error conditions (unlocked)
@@ -447,7 +442,7 @@ int __adis_initial_startup(struct adis *adis)
 	 * with 'IRQF_NO_AUTOEN' anyways.
 	 */
 	if (!adis->data->unmasked_drdy)
-		adis_enable_irq(adis, false);
+		__adis_enable_irq(adis, false);
 
 	if (!adis->data->prod_id_reg)
 		return 0;
