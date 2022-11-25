0e2f2f6b86ea388c7e4b2818fcda8fd4e35e457a
Sandor Yu <Sandor.yu@nxp.com>
2022-11-25 10:53:54 +0800

    LF-7783-2: drm: imx: lcdifv3: remove lcdif3 specific code
    hdmimix specific clocks are managed by imx8mp-hdmi-blk-ctrl.
    remove lcdif3 specific code for hdmi.
    lcidf3 could reuse the driver of compatible = "fsl,imx8mp-lcdif1".
    
    Signed-off-by: Sandor Yu <Sandor.yu@nxp.com>
    Reviewed-by: Robby Cai <robby.cai@nxp.com>

--- drivers/gpu/imx/lcdifv3/lcdifv3-common.c
+++ drivers/gpu/imx/lcdifv3/lcdifv3-common.c
@@ -46,7 +46,6 @@ struct lcdifv3_soc_pdata {
 	bool hsync_invert;
 	bool vsync_invert;
 	bool de_invert;
-	bool hdmimix;
 };
 
 struct lcdifv3_platform_reg {
@@ -65,27 +64,18 @@ static struct lcdifv3_soc_pdata imx8mp_lcdif1_pdata = {
 	.hsync_invert = false,
 	.vsync_invert = false,
 	.de_invert    = false,
-	.hdmimix     = false,
 };
 
 static struct lcdifv3_soc_pdata imx8mp_lcdif2_pdata = {
 	.hsync_invert = false,
 	.vsync_invert = false,
 	.de_invert    = true,
-	.hdmimix      = false,
 };
 
-static struct lcdifv3_soc_pdata imx8mp_lcdif3_pdata = {
-	.hsync_invert = false,
-	.vsync_invert = false,
-	.de_invert    = false,
-	.hdmimix     = true,
-};
 static const struct of_device_id imx_lcdifv3_dt_ids[] = {
 	{ .compatible = "fsl,imx93-lcdif", },
 	{ .compatible = "fsl,imx8mp-lcdif1", .data = &imx8mp_lcdif1_pdata, },
 	{ .compatible = "fsl,imx8mp-lcdif2", .data = &imx8mp_lcdif2_pdata, },
-	{ .compatible = "fsl,imx8mp-lcdif3", .data = &imx8mp_lcdif3_pdata,},
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, imx_lcdifv3_dt_ids);
@@ -522,42 +512,6 @@ long lcdifv3_pix_clk_round_rate(struct lcdifv3_soc *lcdifv3,
 }
 EXPORT_SYMBOL(lcdifv3_pix_clk_round_rate);
 
-static int hdmimix_lcdif3_setup(struct lcdifv3_soc *lcdifv3)
-{
-	struct device *dev = lcdifv3->dev;
-	int ret;
-
-	struct clk_bulk_data clocks[] = {
-		{ .id = "mix_apb" },
-		{ .id = "mix_axi" },
-		{ .id = "xtl_24m" },
-		{ .id = "mix_pix" },
-		{ .id = "lcdif_apb" },
-		{ .id = "lcdif_axi" },
-		{ .id = "lcdif_pdi" },
-		{ .id = "lcdif_pix" },
-		{ .id = "lcdif_spu" },
-		{ .id = "noc_hdmi"  },
-	};
-
-	/* power up hdmimix lcdif and nor */
-	ret = device_reset(dev);
-	if (ret)
-		dev_warn(dev, "No hdmimix sub reset found\n");
-	if (ret == -EPROBE_DEFER)
-		return ret;
-
-	/* enable lpcg of hdmimix lcdif and nor */
-	ret = devm_clk_bulk_get_optional(dev, ARRAY_SIZE(clocks), clocks);
-	if (ret < 0)
-		return ret;
-	ret = clk_bulk_prepare_enable(ARRAY_SIZE(clocks), clocks);
-	if (ret < 0)
-		return ret;
-
-	return 0;
-}
-
 static int platform_remove_device_fn(struct device *dev, void *data)
 {
 	struct platform_device *pdev = to_platform_device(dev);
@@ -686,18 +640,8 @@ static int imx_lcdifv3_probe(struct platform_device *pdev)
 	struct device_node *np = dev->of_node;
 	struct lcdifv3_soc *lcdifv3;
 	struct resource *res;
-	const struct of_device_id *of_id;
-	const struct lcdifv3_soc_pdata *soc_pdata;
 
 	dev_dbg(dev, "%s: probe begin\n", __func__);
-
-	of_id = of_match_device(imx_lcdifv3_dt_ids, dev);
-	if (!of_id) {
-		dev_err(&pdev->dev, "OF data missing\n");
-		return -EINVAL;
-	}
-
-	soc_pdata = of_id->data;
 
 	lcdifv3 = devm_kzalloc(dev, sizeof(*lcdifv3), GFP_KERNEL);
 	if (!lcdifv3) {
@@ -748,14 +692,6 @@ static int imx_lcdifv3_probe(struct platform_device *pdev)
 	imx_lcdifv3_of_parse_thres(lcdifv3);
 
 	platform_set_drvdata(pdev, lcdifv3);
-
-	if (soc_pdata && soc_pdata->hdmimix) {
-		ret = hdmimix_lcdif3_setup(lcdifv3);
-		if (ret < 0) {
-			dev_err(dev, "hdmimix lcdif3 setup failed\n");
-			return ret;
-		}
-	}
 
 	atomic_set(&lcdifv3->rpm_suspended, 0);
 	pm_runtime_enable(dev);
