db4c55c20b7f3bb9f7122e29ccbc470951a5ea0e
Han Xu <han.xu@nxp.com>
2022-12-07 10:00:07 -0600

    iio: accel: add the new entry in driver for fxls8967af
    Add this new device entry in the driver id table.
    
    Signed-off-by: Han Xu <han.xu@nxp.com>

--- drivers/iio/accel/fxls8962af-i2c.c
+++ drivers/iio/accel/fxls8962af-i2c.c
@@ -30,6 +30,7 @@ static int fxls8962af_probe(struct i2c_client *client)
 static const struct i2c_device_id fxls8962af_id[] = {
 	{ "fxls8962af", fxls8962af },
 	{ "fxls8964af", fxls8964af },
+	{ "fxls8967af", fxls8967af },
 	{ "fxls8974cf", fxls8974cf },
 	{}
 };
@@ -38,6 +39,7 @@ MODULE_DEVICE_TABLE(i2c, fxls8962af_id);
 static const struct of_device_id fxls8962af_of_match[] = {
 	{ .compatible = "nxp,fxls8962af" },
 	{ .compatible = "nxp,fxls8964af" },
+	{ .compatible = "nxp,fxls8967af" },
 	{ .compatible = "nxp,fxls8974cf" },
 	{}
 };
