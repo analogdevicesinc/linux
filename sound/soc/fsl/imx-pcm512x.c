15d80d2c192590e61145f8c5b5577880645108a2
Adrian Alonso <adrian.alonso@nxp.com>
2023-01-04 09:57:04 -0600

    LF-7658-2: sound: soc: fsl: imx pcm512x add pcm186x adc support
    Add pcm186x adc support found on hifiberry dacplusadcpro
    audio hats
    
    Signed-off-by: Adrian Alonso <adrian.alonso@nxp.com>
    Reviewed-by: Shengjiu Wang <shengjiu.wang@nxp.com>
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>

--- sound/soc/fsl/imx-pcm512x.c
+++ sound/soc/fsl/imx-pcm512x.c
@@ -25,6 +25,7 @@
 
 #include "fsl_sai.h"
 #include "../codecs/pcm512x.h"
+#include "../codecs/pcm186x.h"
 
 #define DAC_CLK_EXT_44K 22579200UL
 #define DAC_CLK_EXT_48K 24576000UL
@@ -33,6 +34,7 @@ struct imx_pcm512x_data {
 	struct simple_util_priv *priv;
 	struct gpio_desc *mute_gpio;
 	bool dac_sclk;
+	bool adc_pluspro;
 	bool dac_pluspro;
 	bool dac_led_status;
 	bool dac_gain_limit;
@@ -63,6 +65,143 @@ static const u32 pcm512x_rates[] = {
 	44100, 48000, 64000, 88200, 96000,
 	176400, 192000, 384000,
 };
+
+static const unsigned int pcm186x_adc_input_channel_sel_value[] = {
+	0x00, 0x01, 0x02, 0x03, 0x10
+};
+
+static const char * const pcm186x_adcl_input_channel_sel_text[] = {
+	"No Select",
+	"VINL1[SE]", /* Default for ADCL */
+	"VINL2[SE]",
+	"VINL2[SE] + VINL1[SE]",
+	"{VIN1P, VIN1M}[DIFF]"
+};
+
+static const char * const pcm186x_adcr_input_channel_sel_text[] = {
+	"No Select",
+	"VINR1[SE]", /* Default for ADCR */
+	"VINR2[SE]",
+	"VINR2[SE] + VINR1[SE]",
+	"{VIN2P, VIN2M}[DIFF]"
+};
+
+static const struct soc_enum pcm186x_adc_input_channel_sel[] = {
+	SOC_VALUE_ENUM_SINGLE(PCM186X_ADC1_INPUT_SEL_L, 0,
+			      PCM186X_ADC_INPUT_SEL_MASK,
+			      ARRAY_SIZE(pcm186x_adcl_input_channel_sel_text),
+			      pcm186x_adcl_input_channel_sel_text,
+			      pcm186x_adc_input_channel_sel_value),
+	SOC_VALUE_ENUM_SINGLE(PCM186X_ADC1_INPUT_SEL_R, 0,
+			      PCM186X_ADC_INPUT_SEL_MASK,
+			      ARRAY_SIZE(pcm186x_adcr_input_channel_sel_text),
+			      pcm186x_adcr_input_channel_sel_text,
+			      pcm186x_adc_input_channel_sel_value),
+};
+
+static const unsigned int pcm186x_mic_bias_sel_value[] = {
+	0x00, 0x01, 0x11
+};
+
+static const char * const pcm186x_mic_bias_sel_text[] = {
+	"Mic Bias off",
+	"Mic Bias on",
+	"Mic Bias with Bypass Resistor"
+};
+
+static const struct soc_enum pcm186x_mic_bias_sel[] = {
+	SOC_VALUE_ENUM_SINGLE(PCM186X_MIC_BIAS_CTRL, 0,
+			      GENMASK(4, 0),
+			      ARRAY_SIZE(pcm186x_mic_bias_sel_text),
+			      pcm186x_mic_bias_sel_text,
+			      pcm186x_mic_bias_sel_value),
+};
+
+static const unsigned int pcm186x_gain_sel_value[] = {
+	0xe8, 0xe9, 0xea, 0xeb, 0xec, 0xed, 0xee, 0xef,
+	0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7,
+	0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff,
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+	0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+	0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+	0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
+	0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
+	0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f,
+	0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+	0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f,
+	0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47,
+	0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f,
+	0x50
+};
+
+static const char * const pcm186x_gain_sel_text[] = {
+	"-12.0dB", "-11.5dB", "-11.0dB", "-10.5dB", "-10.0dB", "-9.5dB",
+	"-9.0dB", "-8.5dB", "-8.0dB", "-7.5dB", "-7.0dB", "-6.5dB",
+	"-6.0dB", "-5.5dB", "-5.0dB", "-4.5dB", "-4.0dB", "-3.5dB",
+	"-3.0dB", "-2.5dB", "-2.0dB", "-1.5dB", "-1.0dB", "-0.5dB",
+	"0.0dB", "0.5dB", "1.0dB", "1.5dB", "2.0dB", "2.5dB",
+	"3.0dB", "3.5dB", "4.0dB", "4.5dB", "5.0dB", "5.5dB",
+	"6.0dB", "6.5dB", "7.0dB", "7.5dB", "8.0dB", "8.5dB",
+	"9.0dB", "9.5dB", "10.0dB", "10.5dB", "11.0dB", "11.5dB",
+	"12.0dB", "12.5dB", "13.0dB", "13.5dB", "14.0dB", "14.5dB",
+	"15.0dB", "15.5dB", "16.0dB", "16.5dB", "17.0dB", "17.5dB",
+	"18.0dB", "18.5dB", "19.0dB", "19.5dB", "20.0dB", "20.5dB",
+	"21.0dB", "21.5dB", "22.0dB", "22.5dB", "23.0dB", "23.5dB",
+	"24.0dB", "24.5dB", "25.0dB", "25.5dB", "26.0dB", "26.5dB",
+	"27.0dB", "27.5dB", "28.0dB", "28.5dB", "29.0dB", "29.5dB",
+	"30.0dB", "30.5dB", "31.0dB", "31.5dB", "32.0dB", "32.5dB",
+	"33.0dB", "33.5dB", "34.0dB", "34.5dB", "35.0dB", "35.5dB",
+	"36.0dB", "36.5dB", "37.0dB", "37.5dB", "38.0dB", "38.5dB",
+	"39.0dB", "39.5dB", "40.0dB"
+};
+
+static const struct soc_enum pcm186x_gain_sel[] = {
+	SOC_VALUE_ENUM_SINGLE(PCM186X_PGA_VAL_CH1_L, 0, 0xff,
+			      ARRAY_SIZE(pcm186x_gain_sel_text),
+			      pcm186x_gain_sel_text,
+			      pcm186x_gain_sel_value),
+	SOC_VALUE_ENUM_SINGLE(PCM186X_PGA_VAL_CH1_R, 0, 0xff,
+			      ARRAY_SIZE(pcm186x_gain_sel_text),
+			      pcm186x_gain_sel_text,
+			      pcm186x_gain_sel_value),
+};
+
+static const struct snd_kcontrol_new pcm1863_snd_controls_card[] = {
+	SOC_ENUM("ADC Left Input", pcm186x_adc_input_channel_sel[0]),
+	SOC_ENUM("ADC Right Input", pcm186x_adc_input_channel_sel[1]),
+	SOC_ENUM("ADC Mic Bias", pcm186x_mic_bias_sel),
+	SOC_ENUM("PGA Gain Left", pcm186x_gain_sel[0]),
+	SOC_ENUM("PGA Gain Right", pcm186x_gain_sel[1]),
+};
+
+static int pcm1863_add_controls(struct snd_soc_component *component)
+{
+	snd_soc_add_component_controls(component, pcm1863_snd_controls_card,
+				       ARRAY_SIZE(pcm1863_snd_controls_card));
+	return 0;
+}
+
+static int imx_pcm186x_dai_init(struct snd_soc_pcm_runtime *rtd)
+{
+	struct snd_soc_dai *adc_dai = snd_soc_rtd_to_codec(rtd, 1);
+	struct snd_soc_component *adc = adc_dai->component;
+	int ret;
+
+	ret = pcm1863_add_controls(adc);
+	if (ret) {
+		dev_err(rtd->dev, "Failed to add pcm1863 controls: %d\n", ret);
+		return ret;
+	}
+
+	ret = snd_soc_dai_set_fmt(adc_dai, SND_SOC_DAIFMT_CBS_CFS |
+				  SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF);
+	if (ret) {
+		dev_err(rtd->dev, "Failed set set pcm1863 dai format: %d\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
 
 static int imx_pcm512x_select_ext_clk(struct snd_soc_component *comp,
 				      int ext_osc)
@@ -107,6 +246,12 @@ static int imx_pcm512x_dai_init(struct snd_soc_pcm_runtime *rtd)
 		ret = snd_soc_limit_volume(card, "Digital Playback Volume", 207);
 		if (ret)
 			dev_warn(card->dev, "failed to set volume limit\n");
+	}
+
+	if (data->adc_pluspro) {
+		ret = imx_pcm186x_dai_init(rtd);
+		if (ret)
+			dev_warn(card->dev, "failed pcm186x dai init\n");
 	}
 
 	return 0;
@@ -183,7 +328,7 @@ static int imx_pcm512x_hw_params(struct snd_pcm_substream *substream,
 	unsigned int channels = params_channels(params);
 	unsigned int width = params_width(params);
 	unsigned long mclk_freq;
-	int ret, i;
+	int ret;
 
 	/* set MCLK freq */
 	if (data->dac_pluspro && data->dac_sclk) {
@@ -210,12 +355,25 @@ static int imx_pcm512x_hw_params(struct snd_pcm_substream *substream,
 				mclk_freq, ret);
 	}
 
-	for_each_rtd_codec_dais(rtd, i, codec_dai) {
-		ret = snd_soc_dai_set_bclk_ratio(codec_dai, (channels * width));
-		if (ret) {
-			dev_err(card->dev, "failed to set codec dai bclk ratio\n");
+	ret = snd_soc_dai_set_bclk_ratio(codec_dai, (channels * width));
+	if (ret) {
+		dev_err(card->dev, "failed to set codec dai bclk ratio\n");
+		return ret;
+	}
+
+	ret = snd_soc_dai_set_bclk_ratio(cpu_dai, (channels * width));
+	if (ret) {
+		dev_err(card->dev, "failed to set codec dai bclk ratio\n");
+		return ret;
+	}
+
+	if (data->adc_pluspro) {
+		struct snd_soc_dai_driver *drv = codec_dai->driver;
+		const struct snd_soc_dai_ops *ops = drv->ops;
+
+		ret = ops->hw_params(substream, params, codec_dai);
+		if (ret)
 			return ret;
-		}
 	}
 
 	dev_dbg(card->dev, "mclk_freq: %lu, bclk ratio: %u\n",
@@ -244,10 +402,22 @@ static int imx_pcm512x_startup(struct snd_pcm_substream *substream)
 	if (ret)
 		return ret;
 
+
 	if (data->dac_led_status) {
-		snd_soc_component_update_bits(comp, PCM512x_GPIO_EN, 0x08, 0x08);
-		snd_soc_component_update_bits(comp, PCM512x_GPIO_OUTPUT_4, 0x0f, 0x02);
-		snd_soc_component_update_bits(comp, PCM512x_GPIO_CONTROL_1, 0x08, 0x08);
+		if (!substream->stream) {
+			snd_soc_component_update_bits(comp, PCM512x_GPIO_EN, 0x08, 0x08);
+			snd_soc_component_update_bits(comp, PCM512x_GPIO_OUTPUT_4, 0x0f, 0x02);
+			snd_soc_component_update_bits(comp, PCM512x_GPIO_CONTROL_1, 0x08, 0x08);
+		} else {
+			if (data->adc_pluspro) {
+				struct snd_soc_dai *adc_dai = snd_soc_rtd_to_codec(rtd, 1);
+				struct snd_soc_component *adc = adc_dai->component;
+
+				snd_soc_component_update_bits(adc, PCM186X_GPIO_IN_OUT, 0x40, 0x40);
+				snd_soc_component_write(adc, PCM186X_GPIO3_2_CTRL, 0x00);
+				snd_soc_component_write(adc, PCM186X_GPIO3_2_DIR_CTRL, 0x04);
+			}
+		}
 	}
 
 	if (data->dac_sclk) {
@@ -278,8 +448,18 @@ static void imx_pcm512x_shutdown(struct snd_pcm_substream *substream)
 	struct snd_soc_dai *codec_dai = snd_soc_rtd_to_codec(rtd, 0);
 	struct snd_soc_component *comp = codec_dai->component;
 
-	if (data->dac_led_status)
-		snd_soc_component_update_bits(comp, PCM512x_GPIO_CONTROL_1, 0x08, 0x00);
+	if (data->dac_led_status) {
+		if (!substream->stream)
+			snd_soc_component_update_bits(comp, PCM512x_GPIO_CONTROL_1, 0x08, 0x00);
+		else {
+			if (data->adc_pluspro) {
+				struct snd_soc_dai *adc_dai = snd_soc_rtd_to_codec(rtd, 1);
+				struct snd_soc_component *adc = adc_dai->component;
+
+				snd_soc_component_update_bits(adc, PCM186X_GPIO_IN_OUT, 0x40, 0x00);
+			}
+		}
+	}
 
 	if (data->dac_sclk && data->dac_pluspro)
 		imx_pcm512x_select_ext_clk(comp, DAC_CLK_INT);
@@ -334,7 +514,6 @@ static int imx_asoc_card_parse_dt(struct snd_soc_card *card,
 	}
 
 	num_links = of_get_child_count(dev->of_node);
-
 	card->dai_link = devm_kcalloc(dev, num_links, sizeof(*link), GFP_KERNEL);
 	if (!card->dai_link) {
 		dev_err(dev, "failed to allocate memory for dai_link\n");
@@ -478,6 +657,8 @@ static int imx_pcm512x_parse_dt(struct imx_pcm512x_data *data)
 		of_property_read_bool(np, "dac,led_status");
 	data->dac_sclk =
 		of_property_read_bool(np, "dac,sclk");
+	data->adc_pluspro =
+		of_property_read_bool(np, "adc,pluspro");
 
 	return 0;
 }
@@ -505,10 +686,8 @@ static int imx_pcm512x_probe(struct platform_device *pdev)
 	card->dev = &pdev->dev;
 
 	ret = imx_pcm512x_parse_dt(data);
-	if (ret) {
-		dev_err(&pdev->dev, "failed to parse sound card dts\n");
+	if (ret)
 		return ret;
-	}
 
 	for (i = 0; i < card->num_links; i++) {
 		card->dai_link->ops = &imx_pcm512x_ops;
