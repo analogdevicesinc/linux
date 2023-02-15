2de9f5a0c8380004758d7993ddfe75c682eaf5e8
Paul Pop <paul.pop@analog.com>
2023-02-15 13:42:05 +0200

    cn0584: updates on ad3552r driver and cn0584 devicetree

--- drivers/iio/dac/axi-ad3552r.c
+++ drivers/iio/dac/axi-ad3552r.c
@@ -17,12 +17,43 @@
 #include <linux/delay.h>
 #include <linux/clk.h>
 
+enum ad35525_source {
+	AD3552R_ADC,
+	AD3552R_DMA,
+	AD3552R_RAMP
+};
+
+enum ad35525_out_range {
+	AD3552R_0_2_5,
+	AD3552R_0_5,
+	AD3552R_0_10,
+	AD3552R_5_5,
+	AD3552R_10_10
+};
+
+
 struct axi_ad3552r_priv {
-	struct gpio_desc	*reset_gpio;
-	void __iomem		*regs;
-	struct clk		*ref_clk;
-	bool			ddr;
+	struct gpio_desc *reset_gpio;
+	void __iomem *regs;
+	struct clk *ref_clk;
+	bool ddr;
+	bool single_channel;
+	enum ad35525_source input_source;
+	enum ad35525_out_range output_range;
 	bool enable;
+};
+
+static const char * const input_source[] = {
+	[AD3552R_ADC]   = "adc_input",
+	[AD3552R_DMA]   = "dma_input",
+	[AD3552R_RAMP]  = "ramp_input"
+};
+static const char * const output_range[] = {
+	[AD3552R_0_2_5] = "0/2.5V",
+	[AD3552R_0_5]   = "0/5V",
+	[AD3552R_0_10]  = "0/10V",
+	[AD3552R_5_5]   = "-5/+5V",
+	[AD3552R_10_10] = "-10/+10V"
 };
 
 void axi_ad3552r_write(struct axi_ad3552r_priv *priv, u32 reg, u32 val)
@@ -35,34 +66,53 @@ u32 axi_ad3552r_read(struct axi_ad3552r_priv *priv, u32 reg)
 	return ioread32(priv->regs + reg);
 }
 
-void axi_ad3552r_spi_write_8b(struct axi_ad3552r_priv *priv, u32 reg, u32 val, bool ddr_sdr_n)
-{
-	axi_ad3552r_write(priv, 0x64, (reg << 24) | (val << 16));
-	if (ddr_sdr_n) {
-		axi_ad3552r_write(priv, 0x6c, 0x5);
+void axi_ad3552r_spi_write_8b(struct axi_ad3552r_priv *priv, u32 reg, u32 val, bool sdr_ddr_n)
+{  	u32 read_val;
+	read_val = axi_ad3552r_read(priv, 0x8c);
+	axi_ad3552r_write(priv, 0x84, val << 16);
+	axi_ad3552r_write(priv, 0x8c, (reg << 24) | (read_val & 0x0000ffff));
+
+	if (sdr_ddr_n) {
+		priv->ddr = false;
+		read_val = axi_ad3552r_read(priv, 0x8c);
+		axi_ad3552r_write(priv, 0x48, 0x14000);
+		axi_ad3552r_write(priv, 0x8c, read_val  | 0x00000001);
 		mdelay(100);
-		axi_ad3552r_write(priv, 0x6c, 0x0);
+		read_val = axi_ad3552r_read(priv, 0x8c);
+		axi_ad3552r_write(priv, 0x8c, read_val & 0xfffffffe);
 	} else {
-		axi_ad3552r_write(priv, 0x6c, 0x4);
+		priv->ddr = true;
+		read_val = axi_ad3552r_read(priv, 0x8c);
+		axi_ad3552r_write(priv, 0x48, 0x04000);
+		axi_ad3552r_write(priv, 0x8c, read_val  | 0x00000001);
 		mdelay(100);
-		axi_ad3552r_write(priv, 0x6c, 0x0);
+		read_val = axi_ad3552r_read(priv, 0x8c);
+		axi_ad3552r_write(priv, 0x8c, read_val & 0xfffffffe);
 	}
 }
 
-void axi_ad3552r_spi_write_16b(struct axi_ad3552r_priv *priv, u32 reg, u32 val, bool ddr_sdr_n)
+void axi_ad3552r_spi_write_16b(struct axi_ad3552r_priv *priv, u32 reg, u32 val, bool sdr_ddr_n)
 {
-	axi_ad3552r_write(priv, 0x64, (reg << 24) | (val << 8));
-	if (ddr_sdr_n) {
-		axi_ad3552r_write(priv, 0x6c, 0x7);
+	u32 read_val;
+
+	axi_ad3552r_write(priv, 0x84, val << 8);
+	axi_ad3552r_write(priv, 0x8c, reg << 24);
+	read_val = axi_ad3552r_read(priv, 0x8c);
+
+	if(sdr_ddr_n) {
+		priv->ddr = false;
+		axi_ad3552r_write(priv, 0x48, 0x10000);
+		axi_ad3552r_write(priv, 0x8c, read_val | 0x00000001);
 		mdelay(100);
-		axi_ad3552r_write(priv, 0x6c, 0x0);
+		axi_ad3552r_write(priv, 0x8c, read_val & 0xffff0000);
 	} else {
-		axi_ad3552r_write(priv, 0x6c, 0x6);
+		priv->ddr = true;
+		axi_ad3552r_write(priv, 0x48, 0x00000);
+		axi_ad3552r_write(priv, 0x8c, read_val | 0x00000001);
 		mdelay(100);
-		axi_ad3552r_write(priv, 0x6c, 0x0);
+		axi_ad3552r_write(priv, 0x8c, read_val & 0xffff0000);
 	}
 }
-
 static int axi_ad3552r_read_raw(struct iio_dev *indio_dev,
 			    struct iio_chan_spec const *chan,
 			    int *val,
@@ -74,18 +124,27 @@ static int axi_ad3552r_read_raw(struct iio_dev *indio_dev,
 	switch (mask) {
 	case IIO_CHAN_INFO_SAMP_FREQ:
 		if (priv->ddr)
-			*val = clk_get_rate(priv->ref_clk) / 8;
+			if(priv->single_channel)
+				*val = clk_get_rate(priv->ref_clk) / 4;
+			else
+				*val = clk_get_rate(priv->ref_clk) / (4 * 2);
 		else
-			*val = clk_get_rate(priv->ref_clk) / 4;
+			if(priv->single_channel)
+				*val = clk_get_rate(priv->ref_clk) / 8;
+			else
+				*val = clk_get_rate(priv->ref_clk) / (8 * 2);
 		return IIO_VAL_INT;
 	case IIO_CHAN_INFO_ENABLE:
 		*val = priv->enable;
 		return IIO_VAL_INT;
 	case IIO_CHAN_INFO_RAW:
-		if (chan->channel)
-			*val = 1;
-		else
-			*val = 0;
+		if (chan->channel){
+			axi_ad3552r_spi_write_16b(priv, 0xac, 0x00, 1);
+			*val = axi_ad3552r_read(priv,0x80);
+		} else {
+			axi_ad3552r_spi_write_16b(priv, 0xaa, 0x00, 1);
+			*val = axi_ad3552r_read(priv,0x80);
+		}
 		return IIO_VAL_INT;
 	}
 
@@ -103,26 +162,11 @@ static int axi_ad3552r_write_raw(struct iio_dev *indio_dev,
 	switch (mask) {
 	case IIO_CHAN_INFO_SAMP_FREQ:
 		return 0;
-	case IIO_CHAN_INFO_ENABLE:
-		priv->enable = val;
-		if (val) {
-
-			// Start transfer ddr
-			axi_ad3552r_spi_write_8b(priv, 0x14, 0x05,0);
-			priv->ddr = true;
-			mdelay(100);
-
-			axi_ad3552r_write(priv, 0x64, 0x2c000000);
-			axi_ad3552r_write(priv, 0x6c, 0x0d);
-			mdelay(100);
-
-		} else {
-			axi_ad3552r_write(priv, 0x6c, 0x00);
-			axi_ad3552r_spi_write_8b(priv, 0x14, 0x04,1);
-			priv->ddr = false;
-			mdelay(100);
-		}
-		return 0;
+	case IIO_CHAN_INFO_RAW:
+		if (chan->channel)
+			axi_ad3552r_spi_write_16b(priv, 0x2c, (u32)val , 1);
+		else
+			axi_ad3552r_spi_write_16b(priv, 0x2a, (u32)val , 1);
 	}
 
 	return -EINVAL;
@@ -145,22 +189,176 @@ static int axi_ad3552r_reg_access(struct iio_dev *indio_dev,
 	return 0;
 }
 
-static const struct iio_info axi_ad3552r_info = {
-	.read_raw = &axi_ad3552r_read_raw,
-	.write_raw = &axi_ad3552r_write_raw,
-	.debugfs_reg_access = &axi_ad3552r_reg_access,
+static int ad3552r_set_output_range(struct iio_dev *indio_dev,
+				const struct iio_chan_spec *chan,
+				unsigned int mode)
+{
+
+	struct axi_ad3552r_priv *priv = iio_priv(indio_dev);
+
+	priv->output_range = mode;
+
+	switch (mode){
+	case 0:
+	    // Enable 0/2.5V range
+	    axi_ad3552r_spi_write_8b(priv, 0x19, 0x00, 1);
+		mdelay(100);
+		break;
+	case 1:
+	    // Enable 0/5V range
+	    axi_ad3552r_spi_write_8b(priv, 0x19, 0x11, 1);
+		mdelay(100);
+		break;
+	case 2:
+		// Enable 0/10V range
+	    axi_ad3552r_spi_write_8b(priv, 0x19, 0x22, 1);
+		mdelay(100);
+		break;
+	case 3:
+		// Enable +-5V range
+		axi_ad3552r_spi_write_8b(priv, 0x19, 0x33, 1);
+		mdelay(100);
+		break;
+	case 4:
+		// Enable +-10V range
+		axi_ad3552r_spi_write_8b(priv, 0x19, 0x44, 1);
+		mdelay(100);
+		break;
+	default:
+	    // Enable +-10V range
+	    axi_ad3552r_spi_write_8b(priv, 0x19, 0x44, 1);
+		mdelay(100);
+		break;
+	}
+	return 0;
+}
+
+static int ad3552r_get_output_range(struct iio_dev *indio_dev,
+				const struct iio_chan_spec *chan)
+{
+	struct axi_ad3552r_priv *priv = iio_priv(indio_dev);
+	u32 val;
+	int ret;
+	axi_ad3552r_spi_write_8b(priv, 0x99, 0x00, 1);
+	mdelay(100);
+	val = axi_ad3552r_read(priv,0x80);
+
+	switch (val){
+	case 0x44:
+		ret = 4;
+		break;
+	case 0x33:
+		ret = 3;
+		break;
+	case 0x22:
+		ret = 2;
+		break;
+	case 0x11:
+		ret = 1;
+		break;
+	case 0x00:
+		ret = 0;
+		break;
+	default:
+		ret = 4;
+		break;
+	}
+	return ret;
+}
+
+static int ad3552r_set_input_source(struct iio_dev *indio_dev,
+				const struct iio_chan_spec *chan,
+				unsigned int mode)
+{
+
+	struct axi_ad3552r_priv *priv = iio_priv(indio_dev);
+
+	priv->input_source = mode;
+
+	switch (mode){
+	case 0:
+		axi_ad3552r_write(priv, 0x418, 0x08);
+		axi_ad3552r_write(priv, 0x458, 0x08);
+		break;
+	case 1:
+		axi_ad3552r_write(priv, 0x418, 0x02);
+		axi_ad3552r_write(priv, 0x458, 0x02);
+		break;
+	case 2:
+		axi_ad3552r_write(priv, 0x418, 0x0b);
+		axi_ad3552r_write(priv, 0x458, 0x0b);
+		break;
+	default:
+		axi_ad3552r_write(priv, 0x418, 0x02);
+		axi_ad3552r_write(priv, 0x458, 0x02);
+		break;
+	}
+
+	return 0;
+}
+
+static int ad3552r_get_input_source(struct iio_dev *indio_dev,
+				const struct iio_chan_spec *chan)
+{
+	struct axi_ad3552r_priv *priv = iio_priv(indio_dev);
+    u32 val;
+    int ret;
+	val = axi_ad3552r_read(priv,0x418);
+	switch (val){
+	case 0x8:
+		ret = 0;
+		break;
+	case 0x2:
+		ret = 1;
+		break;
+	case 0xb:
+		ret = 2;
+		break;
+	default:
+		ret = 0;
+		break;
+	}
+
+	return ret;
+}
+
+static const struct iio_enum ad35525_source_enum = {
+	.items = input_source,
+	.num_items = ARRAY_SIZE(input_source),
+	.get = ad3552r_get_input_source,
+	.set = ad3552r_set_input_source,
 };
+
+static const struct iio_enum ad35525_output_enum = {
+	.items = output_range,
+	.num_items = ARRAY_SIZE(output_range),
+	.get = ad3552r_get_output_range,
+	.set = ad3552r_set_output_range,
+};
+
+
+
+
+static const struct iio_chan_spec_ext_info ad3552r_ext_info[] = {
+
+	IIO_ENUM("input_source", IIO_SHARED_BY_ALL, &ad35525_source_enum),
+	IIO_ENUM_AVAILABLE_SHARED("input_source", IIO_SHARED_BY_ALL, &ad35525_source_enum),
+	IIO_ENUM("output_range", IIO_SHARED_BY_ALL, &ad35525_output_enum),
+	IIO_ENUM_AVAILABLE_SHARED("output_range", IIO_SHARED_BY_ALL, &ad35525_output_enum),
+	{ },
+};
+
 
 static const struct iio_chan_spec axi_ad3552r_channels[] = {
 	{
 		.type = IIO_VOLTAGE,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE) |
-					   BIT(IIO_CHAN_INFO_SAMP_FREQ),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_SAMP_FREQ),
 		.output = 1,
 		.indexed = 1,
 		.channel = 0,
 		.scan_index = 0,
+		.ext_info = ad3552r_ext_info,
 		.scan_type = {
 			.sign = 'u',
 			.realbits = 16,
@@ -171,11 +369,11 @@ static const struct iio_chan_spec axi_ad3552r_channels[] = {
 	}, {
 		.type = IIO_VOLTAGE,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 		.output = 1,
 		.indexed = 1,
 		.channel = 1,
 		.scan_index = 1,
+		.ext_info = ad3552r_ext_info,
 		.scan_type = {
 			.sign = 'u',
 			.realbits = 16,
@@ -186,19 +384,80 @@ static const struct iio_chan_spec axi_ad3552r_channels[] = {
 	}
 };
 
-static int axi_ad3552r_buffer_preenable(struct iio_dev *indio_dev)
-{
+/*
+ * ad7298_update_scan_mode() setup the spi transfer buffer for the new scan mask
+ */
+static int axi_ad3552r_update_scan_mode(struct iio_dev *indio_dev,
+	const unsigned long *active_scan_mask)
+{  
 	struct axi_ad3552r_priv *priv = iio_priv(indio_dev);
+	u32 read_val;
+	if(!test_bit(0, active_scan_mask) && test_bit(1, active_scan_mask)){
+		priv->single_channel = true;
+		read_val = axi_ad3552r_read(priv, 0x8c);
+		// Stream length
+		axi_ad3552r_spi_write_8b(priv, 0x0e, 0x02, 1);
+		mdelay(100);
+		// DDR configure
+		axi_ad3552r_spi_write_8b(priv, 0x14, 0x05, 1);
 
-	axi_ad3552r_write(priv, 0x048, 0x10);
-	axi_ad3552r_write(priv, 0x418, 0x02);
-	axi_ad3552r_write(priv, 0x458, 0x02);
+		read_val = axi_ad3552r_read(priv, 0x8c);
+		axi_ad3552r_write(priv, 0x8c, (read_val & 0x0000ffff) | 0x2c000000);
 
+	} else if(test_bit(0, active_scan_mask) && !test_bit(1, active_scan_mask)) {
+		priv->single_channel = true;
+		read_val = axi_ad3552r_read(priv, 0x8c);
+		// Stream length
+		axi_ad3552r_spi_write_8b(priv, 0x0e, 0x02, 1);
+		mdelay(100);
+		// DDR configure
+		axi_ad3552r_spi_write_8b(priv, 0x14, 0x05, 1);
+
+		read_val = axi_ad3552r_read(priv, 0x8c);
+		axi_ad3552r_write(priv, 0x8c, (read_val & 0x0000ffff) | 0x2a000000);
+	} else {
+		priv->single_channel = false;
+		read_val = axi_ad3552r_read(priv, 0x8c);
+		// Stream length
+		axi_ad3552r_spi_write_8b(priv, 0x0e, 0x04, 1);
+		mdelay(100);
+		// DDR configure
+		axi_ad3552r_spi_write_8b(priv, 0x14, 0x05, 1);
+
+		read_val = axi_ad3552r_read(priv, 0x8c);
+		axi_ad3552r_write(priv, 0x8c, (read_val & 0x0000ffff) | 0x2c000000);
+	}
+    priv->ddr = true;
+	return 0;
+}
+
+
+static const struct iio_info axi_ad3552r_info = {
+	.read_raw = &axi_ad3552r_read_raw,
+	.write_raw = &axi_ad3552r_write_raw,
+	.debugfs_reg_access = &axi_ad3552r_reg_access,
+	.update_scan_mode = axi_ad3552r_update_scan_mode,
+};
+
+static int axi_ad3552r_buffer_postenable(struct iio_dev *indio_dev)
+{  
+	u32 read_val;
+	struct axi_ad3552r_priv *priv = iio_priv(indio_dev);
+	priv->ddr = true;
+	axi_ad3552r_write(priv, 0x48, 0x00010);
+    // Start stream transfer ddr 
+	read_val = axi_ad3552r_read(priv, 0x8c);
+	axi_ad3552r_write(priv, 0x8c, read_val | 0x00000003);
 	return 0;
 }
 
 static int axi_ad3552r_buffer_postdisable(struct iio_dev *indio_dev)
-{
+{	
+  struct axi_ad3552r_priv *priv = iio_priv(indio_dev);
+    u32 read_val;	
+	read_val = axi_ad3552r_read(priv, 0x8c);
+	axi_ad3552r_write(priv, 0x8c, read_val & 0xfffffffc);
+	axi_ad3552r_spi_write_8b(priv, 0x14, 0x04,0);
 	return 0;
 }
 
@@ -209,7 +468,7 @@ static int axi_ad3552r_dma_buffer_submit(struct iio_dma_buffer_queue *queue,
 }
 
 static const struct iio_buffer_setup_ops axi_ad3552r_buffer_setup_ops = {
-	.preenable = axi_ad3552r_buffer_preenable,
+	.postenable  = axi_ad3552r_buffer_postenable,
 	.postdisable = axi_ad3552r_buffer_postdisable,
 };
 
@@ -225,6 +484,7 @@ static int axi_ad3552r_probe(struct platform_device *pdev)
 	struct iio_buffer *buffer;
 	struct resource *mem;
 	int ret;
+
 
 	indio_dev = devm_iio_device_alloc(&pdev->dev, sizeof(*priv));
 	if (!indio_dev)
@@ -258,21 +518,11 @@ static int axi_ad3552r_probe(struct platform_device *pdev)
 	axi_ad3552r_write(priv, 0x40, 0x03);
 
 	// External Vref + Idump
-    	axi_ad3552r_spi_write_8b(priv, 0x15, 0x00,0);
+	axi_ad3552r_spi_write_8b(priv, 0x15, 0x00, 1);
 	mdelay(100);
 	// Stream mode enable
-    	axi_ad3552r_spi_write_8b(priv, 0x0f, 0x84,0);
+	axi_ad3552r_spi_write_8b(priv, 0x0f, 0x84, 1);
 	mdelay(100);
-	// Stream length
-    	axi_ad3552r_spi_write_8b(priv, 0x0e, 0x04,0);
-	mdelay(100);
-    	// Enable +-10V range
-    	axi_ad3552r_spi_write_8b(priv, 0x19, 0x44,0);
-	mdelay(100);
-	axi_ad3552r_write(priv, 0x418, 0x0b);
-	axi_ad3552r_write(priv, 0x458, 0x0b);
-
-
 	priv->enable = false;
 
 	indio_dev->name = pdev->dev.of_node->name;
