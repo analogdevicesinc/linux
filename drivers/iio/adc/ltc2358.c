2c96064c9705bae7ab199752489483e7cdc8cb30
Kim Seer Paller <kimseer.paller@analog.com>
2023-02-15 22:56:13 +0000

    iio:adc: add ltc2358 initial support
    Add driver implementation for LTC2358 CMOS Mode.
    
    The LTC2358 is a Buffered Quad, 200ksps/Ch Differential
    ±10.24V ADC with 30VP-P Common Mode Range.
    
    Signed-off-by: Kim Seer Paller <kimseer.paller@analog.com>

--- drivers/iio/adc/ltc2358.c
+++ drivers/iio/adc/ltc2358.c
@@ -4,6 +4,9 @@
  *
  * Copyright 2023 Analog Devices Inc.
  */
+
+#include <asm/unaligned.h>
+#include <linux/cache.h>
 #include <linux/device.h>
 #include <linux/err.h>
 #include <linux/iio/iio.h>
@@ -16,26 +19,24 @@
 
 #define LTC2358_BYTES_PER_CH	3
 #define LTC2358_CHANNEL_MSK     GENMASK(2, 0)
+#define LTC2358_NUM_CH          7
+#define LTC2358_NUM_CONFIG	7
 #define T_CONVH			40
 #define T_QUIET			20
+#define CACHE_LINE_SIZE		L1_CACHE_BYTES
+#define LTC2358_CREATE_CONFIG_WORD(channel, config_number, config_word)		\
+	(*(config_word) |= ((u32)((config_number) & LTC2358_CHANNEL_MSK)	\
+			<< ((channel) * LTC2358_BYTES_PER_CH)))
 
 struct ltc2358_state {
 	struct spi_device	*spi;
-	struct mutex		lock;
 };
 
-static void ltc2358_create_config_word(u8 channel, u8 config_number,
-				       u32 *config_word)
-{
-	*config_word |= (u32)(config_number & LTC2358_CHANNEL_MSK)
-			<< (channel * LTC2358_BYTES_PER_CH);
-}
-
 static int ltc2358_read(struct ltc2358_state *adc, u32 config_word,
-			u8 data_array[24], u8 channel, int *val)
+			u8 *data_array, u8 channel, int *val)
 {
 	int ret;
-	u8 tx_buff[24] = {0};
+	u8 tx_buff[24] __aligned(CACHE_LINE_SIZE) = {0};
 
 	struct spi_transfer xfer = {
 		.tx_buf = tx_buff,
@@ -52,94 +53,90 @@ static int ltc2358_read(struct ltc2358_state *adc, u32 config_word,
 		}
 	};
 
-	tx_buff[2] = config_word & 0xFF;
-	tx_buff[1] = (config_word >> 8) & 0xFF;
-	tx_buff[0] = config_word >> 16;
+	put_unaligned_be24(config_word, tx_buff);
 
 	ret = spi_sync_transfer(adc->spi, &xfer, 1);
-	if (ret < 0)
+	if (ret)
 		return ret;
 
-	*val = (data_array[channel * LTC2358_BYTES_PER_CH] << 16 |
-		data_array[channel * LTC2358_BYTES_PER_CH + 1] << 8 |
-		data_array[channel * LTC2358_BYTES_PER_CH + 2]);
+	*val = get_unaligned_be24(&data_array[channel * LTC2358_BYTES_PER_CH]);
 
 	return 0;
 }
 
 static int ltc2358_read_raw(struct iio_dev *indio_dev,
-			    struct iio_chan_spec const *chan,
-			    int *val, int *val2, long info)
+			    struct iio_chan_spec const *chan, int *val,
+			    int *val2, long info)
 {
 	struct ltc2358_state *adc = iio_priv(indio_dev);
-	u8 data_array[24];
+	u8 data_array[24] __aligned(CACHE_LINE_SIZE);
 	u32 config_word = 0;
 	int ret;
 
-	if (info == IIO_CHAN_INFO_RAW) {
-		mutex_lock(&adc->lock);
-		ret = ltc2358_read(adc, config_word, data_array,
-				   chan->channel, val);
-		mutex_unlock(&adc->lock);
-		if (ret < 0)
+	switch (info) {
+	case IIO_CHAN_INFO_RAW:
+		ret = ltc2358_read(adc, config_word, data_array, chan->channel, val);
+		if (ret)
 			return ret;
-	} else
+		break;
+
+	default:
 		return -EINVAL;
+	}
 
 	return IIO_VAL_INT;
 }
 
-static const struct iio_chan_spec ltc2358_channels[] = {
-	{
-		.type = IIO_VOLTAGE,
-		.indexed = 1,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW)
+#define LTC2358_CHAN(name)						\
+	struct iio_chan_spec name = {					\
+		.type = IIO_VOLTAGE,					\
+		.indexed = 1,						\
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),		\
 	}
-};
 
 static const struct iio_info ltc2358_info = {
 	.read_raw = ltc2358_read_raw,
 };
 
-static int ltc2358_parse_dt(struct ltc2358_state *adc,
-			    struct iio_dev *indio_dev)
+static int ltc2358_parse_channels(struct ltc2358_state *adc,
+				  struct iio_dev *indio_dev)
 {
 	struct iio_chan_spec *ltc2358_channels;
-	struct fwnode_handle *fwnode;
+	struct device *dev = &adc->spi->dev;
 	struct fwnode_handle *child;
 	unsigned int num_ch, idx;
 	int chan_idx = 0;
 	u32 config_word = 0;
 	int config_number, ret;
 
-	fwnode = dev_fwnode(indio_dev->dev.parent);
-	num_ch = device_get_child_node_count(indio_dev->dev.parent);
-	if (num_ch == 0)
+	num_ch = device_get_child_node_count(dev);
+	if (!num_ch)
 		return -EINVAL;
 
-	ltc2358_channels = devm_kzalloc(indio_dev->dev.parent,
-					num_ch * sizeof(struct iio_chan_spec),
+	ltc2358_channels = devm_kcalloc(dev, num_ch, sizeof(*ltc2358_channels),
 					GFP_KERNEL);
 
-	fwnode_for_each_child_node(fwnode, child) {
-		ret = fwnode_property_read_u32(child, "reg", &idx);
-		if (ret < 0 || idx > 7) {
+	device_for_each_child_node(dev, child) {
+		LTC2358_CHAN(chan);
+		ret = device_property_read_u32(dev, "reg", &idx);
+		if (ret < 0 || idx > LTC2358_NUM_CH) {
 			fwnode_handle_put(child);
-			pr_err("Invalid channel index %d.", idx);
+			dev_err(dev, "Invalid channel index %d.", idx);
 			return -EINVAL;
 		}
 
-		ltc2358_channels->channel = idx;
+		chan.channel = idx;
 
-		ret = fwnode_property_read_u32(child, "config-number", &config_number);
-		if (ret < 0 || config_number > 7) {
+		ret = device_property_read_u32(dev, "config-number", &config_number);
+		if (ret < 0 || config_number > LTC2358_NUM_CONFIG) {
 			fwnode_handle_put(child);
-			pr_err("Invalid config number %d.", config_number);
+			dev_err(dev, "Invalid config number %d.", config_number);
 			return -EINVAL;
 		}
 
-		ltc2358_create_config_word(chan_idx, config_number, &config_word);
+		LTC2358_CREATE_CONFIG_WORD(idx, config_number, &config_word);
 
+		ltc2358_channels[chan_idx] = chan;
 		chan_idx++;
 	}
 
@@ -162,33 +159,15 @@ static int ltc2358_probe(struct spi_device *spi)
 	adc = iio_priv(indio_dev);
 	adc->spi = spi;
 
-	indio_dev->dev.parent = &spi->dev;
 	indio_dev->name = "ltc2358";
 	indio_dev->modes = INDIO_DIRECT_MODE;
 	indio_dev->info = &ltc2358_info;
 
-	ret = ltc2358_parse_dt(adc, indio_dev);
-	if (ret < 0)
+	ret = ltc2358_parse_channels(adc, indio_dev);
+	if (ret)
 		return ret;
-
-	ret = spi_setup(spi);
-	if (ret < 0)
-		return ret;
-
-	spi_set_drvdata(spi, indio_dev);
 
 	return devm_iio_device_register(&spi->dev, indio_dev);
-}
-
-static int ltc2358_remove(struct spi_device *spi)
-{
-	struct ltc2358_state *adc;
-	struct iio_dev *indio_dev;
-
-	indio_dev = spi_get_drvdata(spi);
-	adc = iio_priv(indio_dev);
-
-	return 0;
 }
 
 static const struct of_device_id ltc2358_of_match[] = {
@@ -199,7 +178,7 @@ MODULE_DEVICE_TABLE(of, ltc2358_of_match);
 
 static struct spi_device_id ltc2358[] = {
 	{"ltc2358", 0},
-	{ },
+	{ }
 };
 MODULE_DEVICE_TABLE(spi, ltc2358);
 
@@ -209,8 +188,7 @@ static struct spi_driver ltc2358_driver = {
 		.of_match_table = ltc2358_of_match,
 	},
 	.probe = ltc2358_probe,
-	.remove = ltc2358_remove,
-	.id_table = ltc2358
+	.id_table = ltc2358,
 };
 module_spi_driver(ltc2358_driver);
 
