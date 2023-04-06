b2091a8bed3369723c9f431c5f440b8e138ea152
Sandor Yu <Sandor.yu@nxp.com>
2023-04-06 16:16:19 +0800

    LF-8746: drm: bridge: cdns_hdmi: move hdmi_type init to sink config
    The patch fix 1920x1080p60 video mode fail to work in Ubuntu rootfs
    when the video mode change from 4kp60.
    
    Ubuntu will not call get_modes function to check
    the video modes before mode_set, and the variable of hdmi_type
    will keep in MODE_HDMI_2_0 if video mode change from 4Kp60
    that cause 1920x1080p60 could not work.
    
    hdmi_sink_config function will be called in every mode_set.
    Move hdmi type init to hdmi_sink_config function to fix the issue.
    
    Signed-off-by: Sandor Yu <Sandor.yu@nxp.com>
    Reviewed-by: Robby Cai <robby.cai@nxp.com>

--- drivers/gpu/drm/bridge/cadence/cdns-hdmi-core.c
+++ drivers/gpu/drm/bridge/cadence/cdns-hdmi-core.c
@@ -34,18 +34,20 @@
 
 static void hdmi_sink_config(struct cdns_mhdp_device *mhdp)
 {
-	struct drm_scdc *scdc = &mhdp->connector.base.display_info.hdmi.scdc;
+	struct drm_display_info *display = &mhdp->connector.base.display_info;
 	u8 buff = 0;
 
-	/* return if hdmi work in DVI mode */
-	if (mhdp->hdmi.hdmi_type == MODE_DVI)
-		return;
-
-	/* check sink support SCDC or not */
-	if (scdc->supported != true) {
-		DRM_INFO("Sink Not Support SCDC\n");
+	/* check sink type (HDMI or DVI) */
+	if (!display->is_hdmi) {
+		mhdp->hdmi.hdmi_type = MODE_DVI;
 		return;
 	}
+
+	mhdp->hdmi.hdmi_type = MODE_HDMI_1_4;
+
+	/* check HDMI2.0 sink */
+	if (!display->hdmi.scdc.supported)
+		return;
 
 	if (mhdp->hdmi.char_rate > 340000) {
 		/*
@@ -54,7 +56,7 @@ static void hdmi_sink_config(struct cdns_mhdp_device *mhdp)
 		 */
 		buff = SCDC_TMDS_BIT_CLOCK_RATIO_BY_40 | SCDC_SCRAMBLING_ENABLE;
 		mhdp->hdmi.hdmi_type = MODE_HDMI_2_0;
-	} else  if (scdc->scrambling.low_rates) {
+	} else  if (display->hdmi.scdc.scrambling.low_rates) {
 		/*
 		 * Enable scrambling and HDMI2.0 when scrambling capability of sink
 		 * be indicated in the HF-VSDB LTE_340Mcsc_scramble bit
@@ -304,8 +306,6 @@ static int cdns_hdmi_connector_get_modes(struct drm_connector *connector)
 			 edid->header[6], edid->header[7]);
 		drm_connector_update_edid_property(connector, edid);
 		num_modes = drm_add_edid_modes(connector, edid);
-		mhdp->hdmi.hdmi_type = drm_detect_hdmi_monitor(edid) ?
-						MODE_HDMI_1_4 : MODE_DVI;
 		kfree(edid);
 	}
 
