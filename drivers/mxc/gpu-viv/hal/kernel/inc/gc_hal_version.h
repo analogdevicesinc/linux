8df0ffc8a7f643aea64cdac8708ac9152ece7cdf
Yuan Tian <yuan.tian@nxp.com>
2023-04-14 17:19:08 +0800

    MGS-7121 [#imx-2976] 0156-KERNEL-SPACE-Update-version-for-patch_6.4.11.p1_2023
    Update version for
     patch_6.4.11.p1_20230412
    
    Signed-off-by: Yuan Tian <yuan.tian@nxp.com>

--- drivers/mxc/gpu-viv/hal/kernel/inc/gc_hal_version.h
+++ drivers/mxc/gpu-viv/hal/kernel/inc/gc_hal_version.h
@@ -62,9 +62,9 @@
 
 #define gcvVERSION_PATCH        11
 
-#define gcvVERSION_BUILD     608260
+#define gcvVERSION_BUILD     658245
 
-#define gcvVERSION_STRING    "6.4.11.p1.608260"
+#define gcvVERSION_STRING    "6.4.11.p1.658245"
 
 #endif /* __gc_hal_version_h_ */
 
