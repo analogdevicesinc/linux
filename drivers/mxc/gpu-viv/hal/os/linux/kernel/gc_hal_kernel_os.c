0e2e44e1c5995e95d0fb9c6090566c6dad8a2782
Guangliu Ding <guangliu.ding@nxp.com>
2023-06-21 16:40:01 +0800

    LF-9412 [#imx-3177] [L6.4] Update virt_to_pfn usage
    virt_to_phys() and virt_to_pfn() are converted to static inline
    functions. To be able to proceed, patch mm to use (void *) as argument
    to affected functions in all instances.
    
    https://lore.kernel.org/linux-mm/20220701160004.2ffff4e5ab59a55499f4c736@linux-foundation.org/
    
    So we covert vaddr from unsigned long to void *.
    
    Signed-off-by: Guangliu Ding <guangliu.ding@nxp.com>

--- drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_os.c
+++ drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_os.c
@@ -403,7 +403,11 @@ _QueryProcessPageTable(IN gctPOINTER Logical, OUT gctPHYS_ADDR_T *Address)
         /* vmalloc area. */
         *Address = page_to_phys(vmalloc_to_page(Logical)) | offset;
         return gcvSTATUS_OK;
-    } else if (virt_addr_valid(logical)) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 4, 0)
+    } else if (virt_addr_valid((void *)logical)) {
+#else
+	} else if (virt_addr_valid(logical)) {
+#endif
         /* Kernel logical address. */
         *Address = virt_to_phys(Logical);
         return gcvSTATUS_OK;
