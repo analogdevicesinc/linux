2e55546b887bd6149bd72672200374fe97d871f9
Guoniu.zhou <guoniu.zhou@nxp.com>
2023-08-01 15:42:05 +0800

    LF-9695: media: csi-sam: fix typo issue
    Fix typo issue in Samsung MIPI CSI-2 driver.
    
    Signed-off-by: Guoniu.zhou <guoniu.zhou@nxp.com>
    Reviewed-by: Robby Cai <robby.cai@nxp.com>

--- drivers/staging/media/imx/imx8-mipi-csi2-sam.c
+++ drivers/staging/media/imx/imx8-mipi-csi2-sam.c
@@ -1785,9 +1785,9 @@ static int mipi_csis_imx8mp_gclk_get(struct csi_state *state)
 
 	state->csi_aclk = devm_clk_get_optional(dev, "media_blk_csi_aclk");
 	if (IS_ERR(state->csi_aclk)) {
-		if (PTR_ERR(state->csi_pclk) != -EPROBE_DEFER)
+		if (PTR_ERR(state->csi_aclk) != -EPROBE_DEFER)
 			dev_err(dev, "Failed to get media csi aclk\n");
-		return PTR_ERR(state->csi_pclk);
+		return PTR_ERR(state->csi_aclk);
 	}
 
 	return 0;
