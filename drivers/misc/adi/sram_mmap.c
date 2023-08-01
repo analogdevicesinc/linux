85f90800c40840d3e1a879a01d2338a9ca2f5460
Greg Malysa <greg.malysa@timesys.com>
2023-08-01 15:50:38 -0400

    drivers: misc: adi: sram: Make SRAM compatible with vmap
    The previous sram mmap driver used io_remap_pfn_range to map SRAM into
    userspace. However this cannot be later used with an AF_XDP socket for
    the umem buffer, because it creates a mapping with VM_IO that is not
    backed by an instance of struct page.
    
    This replaces the driver with one that uses a cooperating reserved
    memory node to register SRAM as part of system memory and reserve it so
    that it can later be mapped into userspace, used with AF_XDP, etc. but
    will not be unexpectedly assigned to userspace otherwise.
    
    The memory is also suitably mapped for use with POSIX IPC or any other
    use which requires mapping it back into kernel space via get_user_pages,
    and caching is enabled as usual for regular memory mapped into
    userspace.

--- drivers/misc/adi/sram_mmap.c
+++ drivers/misc/adi/sram_mmap.c
@@ -14,6 +14,7 @@
 #include <linux/fs.h>
 #include <linux/mm.h>
 #include <linux/module.h>
+#include <linux/of_reserved_mem.h>
 #include <linux/uaccess.h>
 #include <linux/slab.h>
 #include <linux/of.h>
@@ -24,84 +25,100 @@
 
 #define SRAM_MMAP_DRV_NAME		"sram_mmap"
 
+#define MAX_SRAM_REGIONS 4
+struct sram_region_info {
+	struct reserved_mem *rmem;
+	struct device *dev;
+};
+
+static struct sram_region_info sram_regions[MAX_SRAM_REGIONS];
+static int next_region = 0;
+
+static struct reserved_mem *find_sram_mem(struct device *dev, int *id) {
+	int i;
+
+	if (!dev) {
+		pr_err("NULL device to find_sram_mem\n");
+		return NULL;
+	}
+
+	for (i = 0; i < MAX_SRAM_REGIONS; ++i) {
+		if (sram_regions[i].dev == dev) {
+			*id = i;
+			return sram_regions[i].rmem;
+		}
+	}
+
+	dev_err(dev, "Unable to find SRAM reservation!\n");
+	return NULL;
+}
+
 struct adi_sram_mmap {
 	struct miscdevice miscdev;
-	struct gen_pool *sram_pool;
+	struct device *dev;
+	struct page *start;
+	struct reserved_mem *rmem;
 };
 
-struct mmap_private_data {
-	struct gen_pool *pool;
-	unsigned long vaddr;
-};
-
-static void mmap_open(struct vm_area_struct *vma)
-{
-	struct mmap_private_data *pdata = vma->vm_private_data;
-	size_t sram_size = vma->vm_end - vma->vm_start;
-
-	/* Alloc the virtual address from specific sram_pool */
-	pdata->vaddr = gen_pool_alloc(pdata->pool, sram_size);
-	if (!pdata->vaddr)
-		pr_err("Failed to alloc memory from sram pool!\n");
+int sram_set_page_dirty(struct page *page) {
+	/* do nothing but avoid using __set_page_dirty_buffers which would
+	 * actually mark the page as dirty and cause a warning later */
+	return 0;
 }
 
-static void mmap_close(struct vm_area_struct *vma)
-{
-	struct mmap_private_data *pdata = vma->vm_private_data;
-	size_t sram_size = vma->vm_end - vma->vm_start;
-
-	gen_pool_free(pdata->pool, pdata->vaddr, sram_size);
-	kfree(pdata);
-}
-
-const struct vm_operations_struct sram_mmap_vm_ops = {
-	.open =     mmap_open,
-	.close =    mmap_close,
+struct address_space_operations sram_aops = {
+	.set_page_dirty = sram_set_page_dirty,
 };
 
+/**
+ * For now ignore pgoff supplied by the user and start mapping at
+ * the start of SRAM
+ */
 static int sram_mmap(struct file *fp, struct vm_area_struct *vma)
 {
 	struct adi_sram_mmap *sram = container_of(fp->private_data,
 				struct adi_sram_mmap, miscdev);
-	struct mmap_private_data *pdata;
 	size_t sram_size = vma->vm_end - vma->vm_start;
-	unsigned long paddr;
+	struct page *start_page;
+	int pages, i;
 	int ret = 0;
 
-	/*  Allocate private pdata */
-	pdata = kzalloc(sizeof(*pdata), GFP_KERNEL);
-	if (!pdata)
+	if ((vma->vm_pgoff * PAGE_SIZE) + sram_size > sram->rmem->size) {
+		dev_err(sram->dev, "Tried to map 0x%zx@0x%zx, only 0x%zx available\n",
+			sram_size, vma->vm_pgoff * PAGE_SIZE, (size_t)sram->rmem->size);
 		return -ENOMEM;
-
-	pdata->pool = sram->sram_pool;
-	vma->vm_private_data = pdata;
-	vma->vm_ops = &sram_mmap_vm_ops;
-	vma->vm_ops->open(vma);
-
-	if (!pdata->vaddr) {
-		ret = -EAGAIN;
-		goto out_free;
 	}
 
-	paddr = gen_pool_virt_to_phys(pdata->pool, pdata->vaddr);
-
-	vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
-
-	if (io_remap_pfn_range(vma, vma->vm_start,
-				__phys_to_pfn(paddr), sram_size,
-				vma->vm_page_prot)) {
-		pr_err("Unable to mmap sram\n");
-		ret = -EAGAIN;
-		goto out_free;
+	if (sram_size % PAGE_SIZE) {
+		dev_err(sram->dev, "Requested mapping is not a multiple of page size, requested 0x%lx bytes\n", sram_size);
+		return -EINVAL;
 	}
-	pr_info("sram mmaped 0x%lx : 0x%lx successfully!\n",
-		paddr, paddr + sram_size);
 
+	fp->f_mapping->a_ops = &sram_aops;
+	vma->vm_page_prot = __pgprot_modify(vma->vm_page_prot, PTE_ATTRINDX_MASK,
+		PTE_ATTRINDX(MT_NORMAL) | PTE_PXN | PTE_UXN);
+	vma->vm_private_data = sram;
+	vma->vm_ops = NULL;
+
+	start_page = sram->start;
+	pages = sram_size / PAGE_SIZE;
+	for (i = 0; i < pages; ++i) {
+		struct page *page = start_page + vma->vm_pgoff + i;
+
+		if (!page->mapping)
+			page->mapping = fp->f_mapping;
+
+		ret = vm_insert_page(vma, vma->vm_start + (i*PAGE_SIZE), page);
+		if (ret) {
+			dev_err(sram->dev, "Failed to map page to userspace\n");
+			return ret;
+		}
+	}
+
+	dev_info(sram->dev, "Mapped 0x%zx : 0x%zx successfully\n",
+		(size_t) (sram->rmem->base + vma->vm_pgoff * PAGE_SIZE),
+		(size_t) (sram->rmem->base + vma->vm_pgoff * PAGE_SIZE + sram_size));
 	return 0;
-
-out_free:
-	kfree(pdata);
-	return ret;
 }
 
 static const struct file_operations sram_fops = {
@@ -112,43 +129,58 @@ static const struct of_device_id adi_sram_mmap_of_match[] = {
 	{ .compatible = "adi,sram-mmap" },
 	{ },
 };
-MODULE_DEVICE_TABLE(of, adi_sram_mmap_of_match);
+MODULE_DEVICE_TABLE(of,adi_sram_mmap_of_match);
 
 static int adi_sram_mmap_probe(struct platform_device *pdev)
 {
 	int ret = 0;
 	struct adi_sram_mmap *sram;
-	const struct of_device_id *match;
 	struct device *dev;
+	struct page *page;
+	struct reserved_mem *mem;
+	int pages, i, id;
 
 	dev = &pdev->dev;
 
-	/* Allocate sram device data */
+	ret = of_reserved_mem_device_init(dev);
+	if (ret) {
+		dev_err(dev, "Unable to configure SRAM reserved memory\n");
+		return ret;
+	}
+
+	mem = find_sram_mem(dev, &id);
+	if (!mem) {
+		dev_err(dev, "SRAM MMAP requires adi,sram-access reserved memory, please check your device tree\n");
+		return -ENOENT;
+	}
+
+	page = pfn_to_page(PFN_DOWN(mem->base));
+	pages = mem->size / PAGE_SIZE;
+
+	// Grab reference to page so they're never freed back into the allocator
+	for (i = 0; i < pages; ++i) {
+		set_page_count(page+i, 1);
+	}
+
 	sram = devm_kzalloc(dev, sizeof(*sram), GFP_KERNEL);
-	if (!sram)
+	if (!sram) {
+		dev_err(dev, "Unable to allocate sram device data\n");
 		return -ENOMEM;
-
-	match = of_match_device(of_match_ptr(adi_sram_mmap_of_match), &pdev->dev);
-	if (!match) {
-		pr_err("No sram mmap device defined in dts file\n");
-		return -ENODEV;
-	}
-	sram->sram_pool = of_gen_pool_get(dev->of_node, "adi,sram", 0);
-	if (!sram->sram_pool) {
-		pr_err("Unable to get sram pool!\n");
-		return -ENODEV;
 	}
 
+	sram->dev = dev;
+	sram->start = page;
+	sram->rmem = mem;
 	dev_set_drvdata(&pdev->dev, sram);
 
 	sram->miscdev.minor = MISC_DYNAMIC_MINOR;
-	sram->miscdev.name = SRAM_MMAP_DRV_NAME;
+	sram->miscdev.name = kasprintf(GFP_KERNEL, "%s%d", SRAM_MMAP_DRV_NAME, id);
 	sram->miscdev.fops = &sram_fops;
 	sram->miscdev.parent = dev;
 
 	ret = misc_register(&sram->miscdev);
 	if (ret < 0)
-		pr_err("Failed to register sram mmap misc device\n");
+		dev_err(dev, "Faied to register sram mmap misc device\n");
 
 	return ret;
 }
@@ -156,7 +188,6 @@ static int adi_sram_mmap_probe(struct platform_device *pdev)
 static int adi_sram_mmap_remove(struct platform_device *pdev)
 {
 	struct adi_sram_mmap *sram = dev_get_drvdata(&pdev->dev);
-
 	misc_deregister(&sram->miscdev);
 
 	return 0;
@@ -170,6 +201,50 @@ static struct platform_driver adi_sram_mmap_driver = {
 		.of_match_table = of_match_ptr(adi_sram_mmap_of_match),
 	},
 };
+
+static int rmem_sram_init(struct reserved_mem *rmem, struct device *dev) {
+	struct sram_region_info *info = rmem->priv;
+	info->dev = dev;
+	return 0;
+}
+
+static void rmem_sram_release(struct reserved_mem *rmem, struct device *dev) {
+	struct sram_region_info *info = rmem->priv;
+	info->dev = NULL;
+}
+
+static const struct reserved_mem_ops rmem_sram_ops = {
+	.device_init = rmem_sram_init,
+	.device_release = rmem_sram_release,
+};
+
+static int __init rmem_sram_setup(struct reserved_mem *rmem) {
+	if (next_region >= MAX_SRAM_REGIONS) {
+		pr_err("Cannot allocate more SRAM regions--increase MAX_SRAM_REGIONS\n");
+		return -EINVAL;
+	}
+
+	if (rmem->base & (PAGE_SIZE-1)) {
+		pr_err("sram region starting at 0x%px is not page aligned!\n", (void *)rmem->base);
+		return -EINVAL;
+	}
+
+	if (rmem->size & (PAGE_SIZE-1)) {
+		pr_err("sram region starting at 0x%px is not a multiple of the page size (requested 0x%llx bytes)\n",
+			(void *)rmem->base, rmem->size);
+		return -EINVAL;
+	}
+
+	rmem->ops = &rmem_sram_ops;
+	rmem->priv = &sram_regions[next_region];
+	sram_regions[next_region].rmem = rmem;
+	next_region += 1;
+
+	pr_info("Reserved memory: SRAM at %pa, size %ld KiB\n",
+		&rmem->base, (unsigned long) (rmem->size / SZ_1K));
+	return 0;
+}
+RESERVEDMEM_OF_DECLARE(adi_sram, "adi,sram-access", rmem_sram_setup);
 
 module_platform_driver(adi_sram_mmap_driver);
 MODULE_DESCRIPTION("SRAM mmap misc driver for ADI processor on-chip memory");
