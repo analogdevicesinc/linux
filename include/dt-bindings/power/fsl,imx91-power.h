b2c1c48dddb49f49b9586cad964fb19bc63b4a5a
Ye Li <ye.li@nxp.com>
2023-08-02 10:29:24 +0800

    LF-11975-1 arm64: dts: Add iMX91 DTSi and header files
    Add iMX91 DTSi file for SoC and relevant header files for pinfunc,
    clock and power.
    
    Squashed:
    TBS: LF-12379 arm64: dts: imx91: change edma node property on imx91
    
    Signed-off-by: Luke Wang <ziniu.wang_1@nxp.com>
    Signed-off-by: Wei Fang <wei.fang@nxp.com>
    Signed-off-by: Guoniu.zhou <guoniu.zhou@nxp.com>
    Signed-off-by: Pengfei Li <pengfei.li_1@nxp.com>
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by: Haibo Chen <haibo.chen@nxp.com>
    [ Robby: cleanup isi node ]
    Signed-off-by: Robby Cai <robby.cai@nxp.com>

---
+++ include/dt-bindings/power/fsl,imx91-power.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: (GPL-2.0 OR MIT) */
+/*
+ *  Copyright 2024 NXP
+ */
+
+#ifndef __DT_BINDINGS_IMX91_POWER_H__
+#define __DT_BINDINGS_IMX91_POWER_H__
+
+#define IMX91_MEDIABLK_PD_MIPI_DSI		0
+#define IMX91_MEDIABLK_PD_MIPI_CSI		1
+#define IMX91_MEDIABLK_PD_PXP			2
+#define IMX91_MEDIABLK_PD_LCDIF			3
+#define IMX91_MEDIABLK_PD_ISI			4
+
+#endif
