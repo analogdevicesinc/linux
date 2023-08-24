cf1b771212df7875c4b79827d5da7d7421ce1a5d
Bogdan Togorean <bogdan.togorean@analog.com>
2023-08-24 09:21:42 +0300

    drivers: media: i2c: maxim-serdes: max_aggregator.c: First ports source and last sinks
    Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

--- drivers/media/i2c/maxim-serdes/max_aggregator.c
+++ drivers/media/i2c/maxim-serdes/max_aggregator.c
@@ -56,17 +56,17 @@ static int max_aggregator_probe(struct platform_device *pdev)
 	if (ret)
 		return ret;
 
-	npads = max_aggregator->nsinks + max_aggregator->nsources;
+	npads = max_aggregator->nsources + max_aggregator->nsinks;
 	max_aggregator->pads = devm_kzalloc(&pdev->dev, npads * sizeof(*max_aggregator->pads),
 					    GFP_KERNEL);
 	if (!max_aggregator->pads)
 		return -ENOMEM;
 
-	for (i = 0; i < max_aggregator->nsinks; ++i)
-		max_aggregator->pads[i].flags = MEDIA_PAD_FL_SINK;
+	for (i = 0; i < max_aggregator->nsources; ++i)
+		max_aggregator->pads[i].flags = MEDIA_PAD_FL_SOURCE;
 
 	for (; i < npads; ++i)
-		max_aggregator->pads[i].flags = MEDIA_PAD_FL_SOURCE;
+		max_aggregator->pads[i].flags = MEDIA_PAD_FL_SINK;
 
 	subdev = &max_aggregator->subdev;
 	v4l2_subdev_init(subdev, &max_aggregator_ops);
