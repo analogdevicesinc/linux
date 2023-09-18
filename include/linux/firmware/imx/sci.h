81586f1bd1b0987e84dd7e43d079678c20746cad
Dong Aisheng <aisheng.dong@nxp.com>
2023-09-18 18:11:36 +0800

    LF-10339-12 firmware: imx: add back IMX_SC_IRQ_X macro definitions
    The local patch was partially upstreamed and was dropped.
    But we still need the reset of those IMX_SC_IRQ_X macro definitions,
    otherwise we will meet build break.
    
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>

--- include/linux/firmware/imx/sci.h
+++ include/linux/firmware/imx/sci.h
@@ -18,8 +18,29 @@
 #include <linux/firmware/imx/svc/rm.h>
 #include <linux/firmware/imx/svc/seco.h>
 
-#define IMX_SC_IRQ_GROUP_WAKE       3U /* Wakeup interrupts */
+#define IMX_SC_IRQ_NUM_GROUP            9
+
+#define IMX_SC_IRQ_GROUP_TEMP           0   /* Temp interrupts */
+#define IMX_SC_IRQ_GROUP_WDOG           1   /* Watchdog interrupts */
+#define IMX_SC_IRQ_GROUP_RTC            2   /* RTC interrupts */
+#define IMX_SC_IRQ_GROUP_WAKE           3   /* Wakeup interrupts */
+#define IMX_SC_IRQ_GROUP_SYSCTR         4   /* System counter interrupts */
+#define IMX_SC_IRQ_GROUP_REBOOTED       5   /* Partition reboot complete */
+#define IMX_SC_IRQ_GROUP_REBOOT         6   /* Partition reboot starting */
+#define IMX_SC_IRQ_GROUP_OFFED          7   /* Partition off complete */
+#define IMX_SC_IRQ_GROUP_OFF            8   /* Partition off starting */
+
+#define IMX_SC_IRQ_RTC               BIT(0)    /* RTC interrupt */
+#define IMX_SC_IRQ_WDOG              BIT(0)    /* Watch Dog interrupt */
+#define IMX_SC_IRQ_SYSCTR            BIT(0)    /* System Counter interrupt */
+#define IMX_SC_IRQ_BUTTON            BIT(0)    /* Button interrupt */
+#define IMX_SC_IRQ_PAD               BIT(1)    /* Pad wakeup */
+#define IMX_SC_IRQ_USR1              BIT(2)    /* User defined 1 */
+#define IMX_SC_IRQ_USR2              BIT(3)    /* User defined 2 */
+#define IMX_SC_IRQ_BC_PAD            BIT(4)    /* Pad wakeup (broadcast to all partitions) */
+#define IMX_SC_IRQ_SW_WAKE           BIT(5)    /* Software requested wake */
 #define IMX_SC_IRQ_SECVIO            BIT(6)    /* Security violation */
+#define IMX_SC_IRQ_V2X_RESET         BIT(7)    /* V2X reset */
 
 #if IS_ENABLED(CONFIG_IMX_SCU)
 int imx_scu_enable_general_irq_channel(struct device *dev);
