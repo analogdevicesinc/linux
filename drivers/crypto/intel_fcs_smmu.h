aa6b0a32e0e1eec3cf5d4c0f0265d2a2699f3adf
Austin Zhang <austin.zhang@intel.com>
2023-09-19 04:58:25 +0800

    HSD #15010087323-3 :intel_fcs: Add SMMU support for FCS largeblob encryption
    Existing FCS largeblob encryption operations uses SVC shared memory
    pool which is limited to 32MB and a maximum of 4MB can be allocated
    per buffer from the SVC shared memory pool. For large plaintext
    files the number of transactions increases and the time taken to
    complete the operation increases.
    
    SMMU is used to overcome this limitation by allocating a chunk of
    virtually contiguous buffer and mapping the physical address of the
    buffer into a range of addresses accessible by the SDM using a SMMU
    translation table. This allows the data to appear contiguous virtually
    but is physically scattered. The mapped buffer will be returned to
    the FCS client to be populated with the plaintext data. The driver
    will then send the virtual starting address of the mapped buffer
    based on the translation table to SDM. When SDM attempts to read/write
    to the address the SMMU TBU will translate the virtual address to the
    physical address based on the SMMU translation table.
    
    Update makefile to build added file intel_fcs_smmu.c.
    
    Add initialization sequence to program SMMU registers associated to
    SDM SMMU TBU. SMMU TBU connected to the SDM is operating as a secure
    device hence the registers associated to the stream ID and context
    bank is programmed configured manually during fcs driver probe.
    
    Add new implementation based on existing fcs operations that supports
    largeblob with SMMU support. These implementations will use the
    staticaly allocated buffer that is mapped to the SMMU translation
    table instead of the SVC buffer. These implementations uses
    asynchronous mailbox send commands to prevent timing issues
    when processing large plaintext files.
    
    Add checking to only enable SMMU when ATF and SDM firmware versions
    are updated with SMMU enablement support. This is prevent backward
    compatibility issues when users did not update the firmware versions.
    
    Update fcs ioctl with commands for SMMU supported operations.
    
    Signed-off-by: Adrian Ng Ho Yin <adrian.ho.yin.ng@intel.com>

---
+++ drivers/crypto/intel_fcs_smmu.h
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2022, Intel Corporation
+ */
+
+#include <linux/arm-smccc.h>
+#include <linux/bitfield.h>
+#include <linux/completion.h>
+#include <linux/delay.h>
+#include <linux/firmware.h>
+#include <linux/fs.h>
+#include <linux/hw_random.h>
+#include <linux/kobject.h>
+#include <linux/miscdevice.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/of.h>
+#include <linux/of_platform.h>
+#include <linux/platform_device.h>
+#include <linux/firmware/intel/stratix10-svc-client.h>
+#include <linux/string.h>
+#include <linux/slab.h>
+#include <linux/sysfs.h>
+#include <linux/uaccess.h>
+#include <linux/dma-mapping.h>
+#include <linux/vmalloc.h>
+#include <asm/cacheflush.h>
+
+#include <uapi/linux/intel_fcs-ioctl.h>
+
+#ifndef INTEL_FCS_SMMU_H
+#define INTEL_FCS_SMMU_H
+
+extern uint64_t smmu_sdm_l3_def_table[512];
+extern uint64_t smmu_sdm_el3_l1_table[512];
+extern uint64_t smmu_sdm_el3_l2_table[512];
+extern uint64_t *l2_table;
+extern uint64_t *l1_table;
+extern uint64_t *l3_tables[512];
+
+struct intel_fcs_priv {
+	struct stratix10_svc_chan *chan;
+	struct stratix10_svc_client client;
+	struct completion completion;
+	struct mutex lock;
+	struct miscdevice miscdev;
+	unsigned int status;
+	void *kbuf;
+	unsigned int size;
+	unsigned int cid_low;
+	unsigned int cid_high;
+	unsigned int sid;
+	struct hwrng rng;
+};
+
+int smmu_program_reg(struct intel_fcs_priv *priv, uint32_t reg_add, uint32_t reg_value);
+int smmu_read_reg(struct intel_fcs_priv *priv, uint32_t reg_add);
+void invalidate_smmu_tlb_entries(struct intel_fcs_priv *priv);
+void intel_fcs_smmu_init(struct intel_fcs_priv *priv);
+void context_bank_disable(struct intel_fcs_priv *priv);
+void context_bank_enable(struct intel_fcs_priv *priv);
+void fill_l3_table(uint64_t phys, int l2_idx, int l3_idx);
+
+#endif
