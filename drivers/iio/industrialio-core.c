47437375c22c22f8759115b0a25ccc18b3ffcc20
Ramona Bolboaca <ramona.bolboaca@analog.com>
2023-09-18 13:19:52 +0300

    iio: Add IIO_DELTA_VELOCITY channel type
    The delta velocity is defined as a piece-wise integration of
    acceleration data. The delta velocity represents the linear velocity
    change between two consecutive measurements and it
    is measured in m / s (meters per second).
    
    In order to track the total linear velocity change during a desired
    period of time, simply sum-up the delta velocity samples acquired
    during that time.
    
    IIO currently does not offer a suitable channel type for this
    type of measurements hence this patch adds it.
    
    Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>
    Reviewed-by: Nuno Sa <nuno.sa@analog.com>
    Link: https://lore.kernel.org/r/20230808075059.645525-3-ramona.bolboaca@analog.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    (cherry picked from commit 83d9e616822ebd785cc142cc31cf878cdff82748)

--- drivers/iio/industrialio-core.c
+++ drivers/iio/industrialio-core.c
@@ -88,6 +88,7 @@ static const char * const iio_chan_type_name_spec[] = {
 	[IIO_PHASE] = "phase",
 	[IIO_MASSCONCENTRATION] = "massconcentration",
 	[IIO_DELTA_ANGL] = "deltaangl",
+	[IIO_DELTA_VELOCITY] = "deltavelocity",
 	[IIO_GENERIC_DATA] = "data",
 	[IIO_FLAGS] = "flags",
 };
