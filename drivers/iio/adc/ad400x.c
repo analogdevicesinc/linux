d36748dbfdf56a7503e979e8ba641b7f598211dd
Marcelo Schmitt <marcelo.schmitt@analog.com>
2023-10-07 18:08:46 -0300

    iio: adc: ad400x: Extract PWM enable logic into function
    Encapsulate PWM enable/disable logic into a separate function for reuse.
    
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- drivers/iio/adc/ad400x.c
+++ drivers/iio/adc/ad400x.c
@@ -449,11 +449,19 @@ static const struct spi_device_id ad400x_id[] = {
 };
 MODULE_DEVICE_TABLE(spi, ad400x_id);
 
+static int ad400x_enable_cnv_trigger(struct ad400x_state *st, bool enable)
+{
+	struct pwm_state cnv_state;
+
+	pwm_get_state(st->cnv_trigger, &cnv_state);
+	cnv_state.enabled = enable;
+	return pwm_apply_state(st->cnv_trigger, &cnv_state);
+}
+
 static int ad400x_buffer_postenable(struct iio_dev *indio_dev)
 {
 	struct ad400x_state *st = iio_priv(indio_dev);
 	int ret;
-	struct pwm_state conv_state;
 
 	memset(&st->spi_transfer, 0, sizeof(st->spi_transfer));
 	st->spi_transfer.rx_buf = (void *)-1;
@@ -471,25 +479,19 @@ static int ad400x_buffer_postenable(struct iio_dev *indio_dev)
 
 	spi_engine_offload_enable(st->spi, true);
 
-	pwm_get_state(st->cnv_trigger, &conv_state);
-	conv_state.enabled = true;
-	ret = pwm_apply_state(st->cnv_trigger, &conv_state);
-	return 0;
+	return ad400x_enable_cnv_trigger(st, true);
 }
 
 static int ad400x_buffer_postdisable(struct iio_dev *indio_dev)
 {
 	struct ad400x_state *st = iio_priv(indio_dev);
-	struct pwm_state conv_state;
-	int ret;
 
 	spi_engine_offload_enable(st->spi, false);
 
-	pwm_get_state(st->cnv_trigger, &conv_state);
-	conv_state.enabled = false;
-	ret = pwm_apply_state(st->cnv_trigger, &conv_state);
 	st->bus_locked = false;
-	return spi_bus_unlock(st->spi->master);
+	spi_bus_unlock(st->spi->master);
+
+	return ad400x_enable_cnv_trigger(st, false);
 }
 
 static int hw_submit_block(struct iio_dma_buffer_queue *queue,
