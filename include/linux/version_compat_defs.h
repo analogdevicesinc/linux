9355a6fa0dc4b204ed1e13b672ca89f129202204
Jiyu Yang <jiyu.yang@nxp.com>
2023-10-12 20:17:38 +0800

    MGS-7377 [#ccc] fix redefination in 6.1 android
    Signed-off-by: Jiyu Yang <jiyu.yang@nxp.com>

--- include/linux/version_compat_defs.h
+++ include/linux/version_compat_defs.h
@@ -229,8 +229,7 @@ static inline void dma_fence_set_error_helper(
 }
 
 #include <linux/mm.h>
-#if !((KERNEL_VERSION(6, 3, 0) <= LINUX_VERSION_CODE) || \
-      ((KERNEL_VERSION(6, 1, 25) <= LINUX_VERSION_CODE) && defined(__ANDROID_COMMON_KERNEL__)))
+#if (KERNEL_VERSION(6, 1, 25) > LINUX_VERSION_CODE)
 static inline void vm_flags_set(struct vm_area_struct *vma, vm_flags_t flags)
 {
 	vma->vm_flags |= flags;
