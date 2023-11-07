653a2ab262a1555fa462892fa055f0ae72d91033
George Mois <george.mois@analog.com>
2023-11-07 16:03:37 +0200

    iio: adc: Add Madura API
    Add MAdura API 6.4.0.14.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    Signed-off-by: George Mois <george.mois@analog.com>
    (cherry picked from commit 1be380acb0acfc5968eee0a1c99fd3970b41cf2f)

---
+++ drivers/iio/adc/madura/common/adi_common_user.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/**
+* \file adi_common_user.h
+*
+* User modifiable defines
+*
+* \brief Contains ADI Transceiver common hal types.
+*
+* ADI common lib Version: 0.0.1.1
+*/
+
+/**
+* Copyright 2015 - 2020 Analog Devices Inc.
+* Released under the API license, for more information
+* see the "LICENSE.pdf" file in this zip file.
+*/
+
+#ifndef _ADI_COMMON_USER_H_
+#define _ADI_COMMON_USER_H_
+
+#include <linux/kernel.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef ADI_COMMON_HAL_SPIARRAYSIZE
+    #define ADI_COMMON_HAL_SPIARRAYSIZE 256
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif  /* _ADI_COMMON_USER_H_ */
