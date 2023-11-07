66b50a1aaf5da5ba09c6d91f67a21b0a2be6dc5b
George Mois <george.mois@analog.com>
2023-11-07 16:05:20 +0200

    drivers: iio: adc: Add ADRV902x support
    The ADRV9025/26/29 are a highly integrated, radio frequency (RF)
    agile transceivers offering four independently controlled transmitters,
    dedicated observation receiver inputs for monitoring each transmitter
    channel, four independently controlled receivers,
    integrated synthesizers, and digital signal processing functions
    providing a complete transceiver solution. The device provides the
    performance demanded by cellular infrastructure applications,
    such as small cell base station radios, macro 3G/4G/5G systems,
    and massive multiple in/multiple out (MIMO) base stations.
    
    Note:
     * DPD support has been omitted in this initial version.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    Signed-off-by: George Mois <george.mois@analog.com>
    (cherry picked from commit 1f8bf379a2236003bb995822d9a05344f215a65c)

---
+++ include/dt-bindings/iio/adc/adi,adrv9025.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * This header provides constants for ADRV9025/26/29
+ *
+ * Copyright 2023 Analog Devices Inc.
+ */
+
+#ifndef _DT_BINDINGS_IIO_ADC_ADRV9025_H
+#define _DT_BINDINGS_IIO_ADC_ADRV9025_H
+
+ /*
+  * JESD204-FSM defines
+  */
+
+#define DEFRAMER0_LINK_TX	0
+#define DEFRAMER1_LINK_TX	1
+#define FRAMER0_LINK_RX		2
+#define FRAMER1_LINK_RX		3
+#define FRAMER2_LINK_RX		4
+
+#endif
