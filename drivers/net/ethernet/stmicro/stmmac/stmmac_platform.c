531e0a57587c0da00862f08a72e7f134b13bd2ad
Boon Khai Ng <boon.khai.ng@intel.com>
2023-11-17 15:04:19 +0800

    net: stmmac: stmmac_platform: replace the vlan_fail_q_en field with a flag
    Port over the changes of replacing the vlan_fail_q_en field with flag.
    refer from @c1b8df8.
    
    Signed-off-by: Boon Khai Ng <boon.khai.ng@intel.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -554,7 +554,7 @@ stmmac_probe_config_dt(struct platform_device *pdev, u8 *mac)
 		/*VLAN filter failed queue state */
 		if (of_property_read_bool(np, "snps,vlan-fail-q-en")) {
 			dev_info(&pdev->dev, "VLAN filter failed queue\n");
-			plat->vlan_fail_q_en = true;
+			plat->flags |= STMMAC_FLAG_VLAN_FAIL_Q_EN;
 			plat->vlan_fail_q = plat->rx_queues_to_use - 1;
 		}
 	}
