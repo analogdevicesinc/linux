f6c88baab3f10e55900a99df34b6ef9d51629b2b
Alin-Tudor Sferle <Alin-Tudor.Sferle@analog.com>
2023-11-22 12:14:14 +0200

    drivers: pwm: Add pwm_gen support
    Signed-off-by: Alin-Tudor Sferle <Alin-Tudor.Sferle@analog.com>

--- drivers/pwm/core.c
+++ drivers/pwm/core.c
@@ -567,7 +567,9 @@ int pwm_apply_state(struct pwm_device *pwm, const struct pwm_state *state)
 	    state->duty_cycle == pwm->state.duty_cycle &&
 	    state->polarity == pwm->state.polarity &&
 	    state->enabled == pwm->state.enabled &&
-	    state->usage_power == pwm->state.usage_power)
+	    state->usage_power == pwm->state.usage_power &&
+	    state->phase == pwm->state.phase &&
+	    state->time_unit == pwm->state.time_unit)
 		return 0;
 
 	err = chip->ops->apply(chip, pwm, state);
@@ -643,6 +645,7 @@ int pwm_adjust_config(struct pwm_device *pwm)
 		state.duty_cycle = 0;
 		state.period = pargs.period;
 		state.polarity = pargs.polarity;
+		state.time_unit = pargs.time_unit;
 
 		return pwm_apply_state(pwm, &state);
 	}
@@ -1104,6 +1107,15 @@ struct pwm_device *devm_fwnode_pwm_get(struct device *dev,
 EXPORT_SYMBOL_GPL(devm_fwnode_pwm_get);
 
 #ifdef CONFIG_DEBUG_FS
+
+const char *pwm_time_unit_strings[] = {
+	[PWM_UNIT_SEC] = "s",
+	[PWM_UNIT_MSEC] = "ms",
+	[PWM_UNIT_USEC] = "us",
+	[PWM_UNIT_NSEC] = "ns",
+	[PWM_UNIT_PSEC] = "ps",
+};
+
 static void pwm_dbg_show(struct pwm_chip *chip, struct seq_file *s)
 {
 	unsigned int i;
@@ -1113,6 +1125,8 @@ static void pwm_dbg_show(struct pwm_chip *chip, struct seq_file *s)
 		struct pwm_state state;
 
 		pwm_get_state(pwm, &state);
+		if (!state.time_unit)
+			state.time_unit = PWM_UNIT_NSEC;
 
 		seq_printf(s, " pwm-%-3d (%-20.20s):", i, pwm->label);
 
@@ -1122,8 +1136,12 @@ static void pwm_dbg_show(struct pwm_chip *chip, struct seq_file *s)
 		if (state.enabled)
 			seq_puts(s, " enabled");
 
-		seq_printf(s, " period: %llu ns", state.period);
-		seq_printf(s, " duty: %llu ns", state.duty_cycle);
+		seq_printf(s, " period: %llu %s", state.period,
+			   pwm_time_unit_strings[state.time_unit]);
+		seq_printf(s, " duty: %llu %s", state.duty_cycle,
+			   pwm_time_unit_strings[state.time_unit]);
+		seq_printf(s, " phase: %llu %s", state.phase,
+			   pwm_time_unit_strings[state.time_unit]);
 		seq_printf(s, " polarity: %s",
 			   state.polarity ? "inverse" : "normal");
 
