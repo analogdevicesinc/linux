d26f927d5dc6c67a955fbba2d4a1f57eeb7c45c9
Jiwei Fu <jiwei.fu@nxp.com>
2023-11-29 17:21:06 +0800

    LF-10551-2: drivers: Introduce a new driver for NXP neutron NPU
    NXP Neutron Neural Processing Unit (NPU) is a highly scalable accelerator
    providing machine learning (ML) acceleration.
    
    The Neutron NPU device consists of following components:
    
     - RISC-V microcontroller: running the firmware.
     - DataMover: the DMA engine used to move data between host DDR and TCM on Neutron side.
     - Neutron-Core: does the actual MAC compute for the inference job. Not controlled by host, programmed by RISC-V.
     - Mbox: mail box registers used to exchange message between host and Neutron.
    
    This patch includes only besic functionality:
    
     - Device registration.
     - Neutron firmware boot via remote-rproc inferface.
     - Provide open, close, read and other interfaces for user space.
    
    The driver depends on IMX_NEUTRON_REMOTEPROC which is used to load Neutron NPU firmware via
    remote-proc framework.
    
    Signed-off-by: Jiwei Fu <jiwei.fu@nxp.com>
    Acked-by: Peng Fan <peng.fan@nxp.com>

---
+++ drivers/staging/neutron/neutron_device.c
@@ -0,0 +1,289 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2023 NXP
+ */
+/****************************************************************************/
+
+#include <linux/dma-mapping.h>
+#include <linux/bitmap.h>
+#include <linux/interrupt.h>
+#include <linux/mutex.h>
+#include <linux/fs.h>
+#include <linux/module.h>
+#include <linux/io.h>
+#include <linux/cdev.h>
+#include <linux/of.h>
+#include <linux/clk.h>
+#include <linux/of_address.h>
+#include <linux/remoteproc.h>
+#include <linux/delay.h>
+
+#include "neutron_device.h"
+
+/****************************************************************************/
+
+#define NEUTRON_FIRMW_NAME     "NeutronFirmware.elf"
+#define NEUTRON_LOG_SIZE    (0x1000)
+
+#define APPSTATUS_MASK_INFDONE      BIT(0)
+#define APPSTATUS_MASK_INFHALTED    BIT(1)
+#define APPSTATUS_MASK_INFBUFFHALF  BIT(2)
+#define APPSTATUS_MASK_INFOUTHALF   BIT(3)
+#define APPSTATUS_MASK_MBOX         BIT(4)
+#define APPSTATUS_MASK_FAULTCAUSE   (0x3f << 16)
+
+#define APPCTRL_MBWR_UPDATED        (0xF807)
+#define appctrl_get_mbwr(val)       (((val) & 0xFFFF0000) >> 16)
+
+/****************************************************************************/
+
+/**
+ * wait_until_neutron_ready - Wait until neutron is ready.
+ *
+ * @time_ms: time to wait before timing out in milliseconds.
+ *
+ * Returns:
+ * 0 is not ready after the @timeout elapsed.
+ * else the remaining time_ms if neutron is ready.
+ */
+static bool wait_until_neutron_ready(struct neutron_device *ndev, int time_ms)
+{
+	int i, val;
+	/* APPCTRL[31:16] register will be set to 0xF807 after neutron is startup */
+	for (i = 0; i < time_ms; i++) {
+		val = readl(ndev->reg_base + APPCTRL);
+		if (appctrl_get_mbwr(val) == APPCTRL_MBWR_UPDATED)
+			return time_ms - i;
+		usleep_range(1000, 2000);
+	}
+
+	/* time out and not ready */
+	return 0;
+}
+
+struct rproc *neutron_get_rproc(struct neutron_device *ndev)
+{
+	phandle rproc_phandle;
+	struct rproc *rproc;
+
+	if (!ndev || !ndev->dev)
+		return NULL;
+
+	if (!ndev->rproc) {
+		if (of_property_read_u32(ndev->dev->of_node, "fsl,neutron-rproc",
+					 &rproc_phandle)) {
+			dev_err(ndev->dev, "could not get rproc phandle\n");
+			return NULL;
+		}
+
+		rproc = rproc_get_by_phandle(rproc_phandle);
+		if (!rproc) {
+			dev_err(ndev->dev, "could not get rproc handle\n");
+			return NULL;
+		}
+		ndev->rproc = rproc;
+
+		return rproc;
+	}
+	return ndev->rproc;
+}
+
+int neutron_rproc_boot(struct neutron_device *ndev, const char *fw_name)
+{
+	struct rproc *rproc;
+	int ret = 0;
+
+	rproc = neutron_get_rproc(ndev);
+	if (IS_ERR(rproc))
+		return -ENODEV;
+
+	/* Block other threads until neutron firmware is ready */
+	mutex_lock(&ndev->mutex);
+	if (atomic_read(&rproc->power) == 0) {
+		ret = rproc_set_firmware(rproc, fw_name ? fw_name : NEUTRON_FIRMW_NAME);
+		if (ret) {
+			dev_err(ndev->dev, "could not set firmware: %s\n", fw_name);
+			goto out;
+		}
+		ret = rproc_boot(rproc);
+		if (ret) {
+			dev_err(ndev->dev, "could not boot a remote processor\n");
+			goto out;
+		}
+		/* Continue and assume boot neutron manually */
+		if (!wait_until_neutron_ready(ndev, 100))
+			dev_err(ndev->dev, "failed: neutron is not ready, timeout\n");
+	}
+out:
+	mutex_unlock(&ndev->mutex);
+
+	return ret;
+}
+
+int neutron_rproc_shutdown(struct neutron_device *ndev)
+{
+	struct rproc *rproc;
+
+	rproc = neutron_get_rproc(ndev);
+	if (IS_ERR(rproc))
+		return -ENODEV;
+
+	return rproc_shutdown(rproc);
+}
+
+static int neutron_open(struct inode *inode,
+			struct file *file)
+{
+	struct neutron_device *ndev =
+		container_of(inode->i_cdev, struct neutron_device, cdev);
+	struct rproc *rproc;
+	int head, ret = 0;
+	bool is_iomem = true;
+
+	ret = neutron_rproc_boot(ndev, NEUTRON_FIRMW_NAME);
+	if (ret)
+		return ret;
+
+	rproc = ndev->rproc;
+	head = readl(ndev->reg_base + HEAD);
+
+	file->private_data = ndev;
+	dev_dbg(ndev->dev, "Device open. file=0x%pK\n", file);
+
+	/* 0x44000 is the LOG buffer address for neutron */
+	if (!ndev->logger.start_addr)
+		ndev->logger.start_addr = rproc_da_to_va(rproc, 0x44000, 0x1000, &is_iomem);
+
+	if (!ndev->logger.end_addr)
+		ndev->logger.end_addr = ndev->logger.start_addr + NEUTRON_LOG_SIZE;
+
+	ndev->logger.end_of_data = ndev->logger.start_addr;
+	ndev->logger.last_to_console  = ndev->logger.start_addr + head;
+
+	return nonseekable_open(inode, file);
+}
+
+/* function to read neutron log */
+static ssize_t neutron_read(struct file *file, char __user *buf,
+			    size_t count, loff_t *f_pos)
+{
+	struct neutron_device *ndev = file->private_data;
+	struct neutron_log_buffer *data = &ndev->logger;
+	size_t bytes = 0;
+	char c_data;
+	int head, tail;
+
+	/* Read logPtr and calculate log size*/
+	tail = readl(ndev->reg_base + TAIL);
+	head = readl(ndev->reg_base + HEAD);
+
+	data->end_of_data = data->start_addr + tail;
+
+	while (count != bytes && (data->end_of_data != data->last_to_console)) {
+		/* Console is end */
+		if (data->last_to_console == data->end_addr)
+			data->last_to_console = data->start_addr;
+		/* Read char data */
+		c_data =  *data->last_to_console;
+		if (copy_to_user(&buf[bytes], &c_data, 1))
+			return -EFAULT;
+		if (++bytes > NEUTRON_LOG_SIZE)
+			break;
+		++data->last_to_console;
+	}
+
+	return bytes;
+}
+
+static const struct file_operations ndev_fops = {
+	.owner		= THIS_MODULE,
+	.open		= &neutron_open,
+	.read		= &neutron_read,
+};
+
+int neutron_dev_clk_get(struct neutron_device *ndev)
+{
+	int ret;
+
+	if (!ndev || !ndev->dev)
+		return ret;
+
+	ret = devm_clk_bulk_get_all(ndev->dev, &ndev->clks);
+	if (ret < 0) {
+		dev_warn(ndev->dev, "unable to get clocks: %d\n", ret);
+		ret = 0;
+	}
+	ndev->num_clks = ret;
+
+	ret = clk_bulk_prepare_enable(ndev->num_clks, ndev->clks);
+	if (ret)
+		dev_err(ndev->dev, "failed to enable clock\n");
+
+	return ret;
+}
+
+int neutron_dev_init(struct neutron_device *ndev,
+		     struct device *dev, int irq,
+		    struct class *class, dev_t devt)
+{
+	struct device *sysdev;
+	int ret = -1;
+
+	ndev->dev = dev;
+	ndev->class = class;
+	ndev->devt = devt;
+	ndev->irq = irq;
+
+	mutex_init(&ndev->mutex);
+
+	if (ndev->irq < 0)
+		goto destroy_mutex;
+
+	ret = neutron_dev_clk_get(ndev);
+	if (ret)
+		goto destroy_mutex;
+
+	dma_set_mask_and_coherent(ndev->dev, DMA_BIT_MASK(32));
+
+	cdev_init(&ndev->cdev, &ndev_fops);
+	ndev->cdev.owner = THIS_MODULE;
+
+	ret = cdev_add(&ndev->cdev, ndev->devt, 1);
+	if (ret) {
+		dev_err(ndev->dev, "Failed to add character device.\n");
+		goto destroy_mutex;
+	}
+
+	sysdev = device_create(ndev->class, NULL, ndev->devt, ndev,
+			       "neutron%d", MINOR(ndev->devt));
+	if (IS_ERR(sysdev)) {
+		dev_err(ndev->dev, "Failed to create device.\n");
+		ret = PTR_ERR(sysdev);
+		goto del_cdev;
+	}
+
+	dev_info(ndev->dev,
+		 "created neutron device, name=%s\n", dev_name(sysdev));
+
+	return 0;
+
+del_cdev:
+	cdev_del(&ndev->cdev);
+put_clk:
+	clk_bulk_disable_unprepare(ndev->num_clks, ndev->clks);
+destroy_mutex:
+	mutex_destroy(&ndev->mutex);
+
+	dev_err(dev, "Failed to init neutron device.\n");
+	return ret;
+}
+
+void neutron_dev_deinit(struct neutron_device *ndev)
+{
+	clk_bulk_disable_unprepare(ndev->num_clks, ndev->clks);
+	mutex_destroy(&ndev->mutex);
+	device_destroy(ndev->class, ndev->cdev.dev);
+	cdev_del(&ndev->cdev);
+	pr_info("neutron device is removed\n");
+}
+
