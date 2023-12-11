6a2428d3da1abecceeac2bd900612e7a0849cfb2
Xianzhong Li <xianzhong.li@nxp.com>
2023-12-11 10:33:14 +0800

    MGS-7524 [#imx-3234] Enable USC reset for i.MX8MQ/QXP GPU only
    Use blit engine and secuirty feature to determine i.MX8MQ/QXP GPU type,
    Both chips have CVE-2023-4969 security issue as they support OpenCL local memory with on-chip USC memory.
    
    Fixed this security issue by toggling the GPU security mode to trigger a USC reset during GPU context switch,
    VSI validated the second OpenCL process can only read 0s from the USC memory after context switch.
    
    Signed-off-by: Xianzhong Li <xianzhong.li@nxp.com>

--- drivers/mxc/gpu-viv/hal/kernel/gc_hal_kernel_command.c
+++ drivers/mxc/gpu-viv/hal/kernel/gc_hal_kernel_command.c
@@ -2994,6 +2994,7 @@ gckCOMMAND_Commit(IN gckCOMMAND Command, IN gcsHAL_SUBCOMMIT *SubCommit,
 #if gcdCONTEXT_SWITCH_FORCE_USC_RESET
         if (Command->currContext != context
             && Command->kernel->hardware->type == gcvHARDWARE_3D
+            && gckHARDWARE_IsFeatureAvailable(Command->kernel->hardware, gcvFEATURE_BLT_ENGINE)
             && gckHARDWARE_IsFeatureAvailable(Command->kernel->hardware, gcvFEATURE_SECURITY))
             switchSecurityMode = gcvTRUE;
 #endif
