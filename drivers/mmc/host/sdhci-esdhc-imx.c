3a2e1787a3c440f2354363b4ed1a4dbf23e02eda
Luke Wang <ziniu.wang_1@nxp.com>
2024-01-04 10:53:00 +0800

    LF-10854: mmc: sdhci-esdhc-imx: improve imx8mq emmc/sd read performance
    Compared with kernel 6.1, imx8mq eMMC/SD read performance drops by about
    30% with kernel 6.6.
    
    The eMMC/SD read thread will be put to sleep until the hardware completes
    data transfer. Normally, the read thread will be woken up immediately
    when the data transfer is completed. However, due to a known ic bug, if
    imx8mq is in cpuidle, it will take a long time (about 500us) to exit
    cpuidle. As a result, the read thread cannot immediately read the next
    data block, affecting the read performance.
    
    Kernel 6.6 uses EEVDF as the new scheduler, which affects cpu scheduling
    and cpuidle behavior. With kernel 6.6, the cpu which the read thread
    resides has a greater probability in cpuidle (about 80%), while with
    kernel 6.1, the probability is only about 20-30%. For other platforms,
    this does not have a significant impact on read performance because the
    cpuidle exit time is very short (for example, imx93 is about 60us). But
    for imx8mq, this results in longer waits for the thread to be woken up
    while reading eMMC/SD, which drops performance.
    
    So for imx8mq, use the ESDHC_FLAG_PMQOS flag to request the cpu latency
    QoS constraint. This can prevent entering cpuidle during data transfer.
    
    Signed-off-by: Luke Wang <ziniu.wang_1@nxp.com>
    Reviewed-by: Haibo Chen <haibo.chen@nxp.com>

--- drivers/mmc/host/sdhci-esdhc-imx.c
+++ drivers/mmc/host/sdhci-esdhc-imx.c
@@ -351,6 +351,15 @@ static struct esdhc_soc_data usdhc_imx8mm_data = {
 			| ESDHC_FLAG_BUSFREQ,
 };
 
+static struct esdhc_soc_data usdhc_imx8mq_data = {
+	.flags = ESDHC_FLAG_USDHC | ESDHC_FLAG_STD_TUNING
+			| ESDHC_FLAG_HAVE_CAP1 | ESDHC_FLAG_HS200
+			| ESDHC_FLAG_HS400 | ESDHC_FLAG_PMQOS
+			| ESDHC_FLAG_STATE_LOST_IN_LPMODE
+			| ESDHC_FLAG_BROKEN_AUTO_CMD23
+			| ESDHC_FLAG_BUSFREQ,
+};
+
 static struct esdhc_soc_data usdhc_s32v234_data = {
 	.flags = ESDHC_FLAG_USDHC,
 };
@@ -399,6 +408,7 @@ static const struct of_device_id imx_esdhc_dt_ids[] = {
 	{ .compatible = "fsl,imx7ulp-usdhc", .data = &usdhc_imx7ulp_data, },
 	{ .compatible = "fsl,imx8qxp-usdhc", .data = &usdhc_imx8qxp_data, },
 	{ .compatible = "fsl,imx8mm-usdhc", .data = &usdhc_imx8mm_data, },
+	{ .compatible = "fsl,imx8mq-usdhc", .data = &usdhc_imx8mq_data, },
 	{ .compatible = "fsl,imxrt1050-usdhc", .data = &usdhc_imxrt1050_data, },
 	{ .compatible = "nxp,s32g2-usdhc", .data = &usdhc_s32g2_data, },
 	{ .compatible = "fsl,s32v234-usdhc", .data = &usdhc_s32v234_data, },
