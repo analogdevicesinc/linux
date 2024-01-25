analogdevicesinc/linux

commit fde5597b0dddceacc97896c61f821f7b71e85fbb

spi: axi-spi-engine: Add watchdog timer
If there is an issue with the axi-spi-engine hardware a scheduled transfer
might never be completed and spi_sync() will block forever.

Add a watchdog timer that will abort a transfer after 5 seconds after it
has been started. This will potentially leave the hardware in a broken
state, but it allows software to recover and allow to better diagnose the
underlying issue.

Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

drivers/spi/spi-axi-spi-engine.c
@@ -13,6 +13,7 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/spi/spi.h>
+#include <linux/timer.h>
 
 #define SPI_ENGINE_VERSION_MAJOR(x)	((x >> 16) & 0xff)
 #define SPI_ENGINE_VERSION_MINOR(x)	((x >> 8) & 0xff)
@@ -113,6 +114,8 @@ struct spi_engine {
 	unsigned int completed_id;
 
 	unsigned int int_enable;
+
+	struct timer_list watchdog_timer;
 };
 
 static void spi_engine_program_add_cmd(struct spi_engine_program *p,
@@ -439,6 +442,18 @@ static bool spi_engine_read_rx_fifo(struct spi_engine *spi_engine)
 	return spi_engine->rx_length != 0;
 }
 
+static void spi_engine_complete_message(struct spi_master *master, int status)
+{
+	struct spi_engine *spi_engine = spi_master_get_devdata(master);
+	struct spi_message *msg = spi_engine->msg;
+
+	kfree(spi_engine->p);
+	msg->status = status;
+	msg->actual_length = msg->frame_length;
+	spi_engine->msg = NULL;
+	spi_finalize_current_message(master);
+}
+
 static irqreturn_t spi_engine_irq(int irq, void *devid)
 {
 	struct spi_master *master = devid;
@@ -475,13 +490,11 @@ static irqreturn_t spi_engine_irq(int irq, void *devid)
 	if (pending & SPI_ENGINE_INT_SYNC) {
 		if (spi_engine->msg &&
 		    spi_engine->completed_id == spi_engine->sync_id) {
-			struct spi_message *msg = spi_engine->msg;
 
-			kfree(spi_engine->p);
-			msg->status = 0;
-			msg->actual_length = msg->frame_length;
-			spi_engine->msg = NULL;
-			spi_finalize_current_message(master);
+			del_timer(&spi_engine->watchdog_timer);
+
+			spi_engine_complete_message(master, 0);
+
 			disable_int |= SPI_ENGINE_INT_SYNC;
 		}
 	}
@@ -497,6 +510,20 @@ static irqreturn_t spi_engine_irq(int irq, void *devid)
 	return IRQ_HANDLED;
 }
 
+static void spi_engine_timeout(unsigned long data)
+{
+	struct spi_master *master = (struct spi_master *)data;
+	struct spi_engine *spi_engine = spi_master_get_devdata(master);
+
+
+	spin_lock(&spi_engine->lock);
+	if (spi_engine->msg) {
+		dev_err(&master->dev, "Timeout occured while waiting for transfer to complete. Hardware is probably broken.\n");
+		spi_engine_complete_message(master, -ETIMEDOUT);
+	}
+	spin_unlock(&spi_engine->lock);
+}
+
 static int spi_engine_transfer_one_message(struct spi_master *master,
 	struct spi_message *msg)
 {
@@ -505,6 +532,8 @@ static int spi_engine_transfer_one_message(struct spi_master *master,
 	unsigned int int_enable = 0;
 	unsigned long flags;
 	size_t size;
+
+	del_timer_sync(&spi_engine->watchdog_timer);
 
 	p_dry.length = 0;
 	spi_engine_compile_message(spi_engine, msg, true, &p_dry);
@@ -542,6 +571,8 @@ static int spi_engine_transfer_one_message(struct spi_master *master,
 		spi_engine->base + SPI_ENGINE_REG_INT_ENABLE);
 	spi_engine->int_enable = int_enable;
 	spin_unlock_irqrestore(&spi_engine->lock, flags);
+
+	mod_timer(&spi_engine->watchdog_timer, jiffies + 5*HZ);
 
 	return 0;
 }
@@ -622,6 +653,9 @@ static int spi_engine_probe(struct platform_device *pdev)
 	master->max_speed_hz = clk_get_rate(spi_engine->ref_clk) / 2;
 	master->transfer_one_message = spi_engine_transfer_one_message;
 	master->num_chipselect = 8;
+
+	setup_timer(&spi_engine->watchdog_timer, spi_engine_timeout,
+		(unsigned long)master);
 
 	ret = spi_register_master(master);
 	if (ret)

commit 179b29d17d4d9b8904ab20e73bb30c5499d5cc70

spi: spi-axi-spi-engine: setup the timer before IRQ enable
The watchdog timer was being activated after enabling the interrupts and
that could actually lead for the message to be transferred before we get
to call mod_timer() which would lead to a spurious soft interrupt. In
extreme cases (like playing with overlays to load + unload devices)
this could lead to a panic splat as the timer callback would be called
with the spi engine device gone already.

Fixes: fde5597b0ddd ("spi: axi-spi-engine: Add watchdog timer")
Signed-off-by: Nuno Sa <nuno.sa@analog.com>

drivers/spi/spi-axi-spi-engine.c
@@ -628,6 +628,8 @@ static int spi_engine_transfer_one_message(struct spi_master *master,
 		return -ENOMEM;
 	spi_engine_compile_message(spi_engine, msg, false, p);
 
+	mod_timer(&spi_engine->watchdog_timer, jiffies + 5 * HZ);
+
 	spin_lock_irqsave(&spi_engine->lock, flags);
 	spi_engine->sync_id = (spi_engine->sync_id + 1) & 0xff;
 	spi_engine_program_add_cmd(p, false,
@@ -655,8 +657,6 @@ static int spi_engine_transfer_one_message(struct spi_master *master,
 		spi_engine->base + SPI_ENGINE_REG_INT_ENABLE);
 	spi_engine->int_enable = int_enable;
 	spin_unlock_irqrestore(&spi_engine->lock, flags);
-
-	mod_timer(&spi_engine->watchdog_timer, jiffies + 5*HZ);
 
 	return 0;
 }