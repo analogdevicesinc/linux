f908d4e33fbbb09e3e89d81b73a83a11d316219b
Dong Aisheng <aisheng.dong@nxp.com>
2024-01-26 13:44:38 +0800

    fec_uio: fix the parameter of stmmac_fpe_send_mpacket
    Cope with upstream change:
    commit 37e4b8df27bc ("net: stmmac: fix FPE events losing")
    
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>

--- drivers/net/ethernet/freescale/fec_uio.c
+++ drivers/net/ethernet/freescale/fec_uio.c
@@ -263,7 +263,7 @@ static void stmmac_fpe_link_state_handle(struct stmmac_priv *priv, bool is_up)
 	bool *hs_enable = &fpe_cfg->hs_enable;
 
 	if (is_up && *hs_enable) {
-		stmmac_fpe_send_mpacket(priv, priv->ioaddr, MPACKET_VERIFY);
+		stmmac_fpe_send_mpacket(priv, priv->ioaddr, fpe_cfg, MPACKET_VERIFY);
 	} else {
 		*lo_state = FPE_STATE_OFF;
 		*lp_state = FPE_STATE_OFF;
