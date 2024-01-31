80a63dd78e0d8d5bc03b6c9ba7a84f730c01fc0b
Marcelo Schmitt <marcelo.schmitt@analog.com>
2024-01-31 01:38:20 -0300

    iio: adc: ad400x: Allow users to set ADC read offset
    Differential ADCs can work as differential or as single ended ADCs if
    IN- is grounded or fixed to at some voltage level (as in ADAQ4003
    datasheet Figure 43 [1] or as in Figure 2b of SAR input types [2]).
    If setting some fixed level to IN-, users may want to set _offset so the
    IIO formula ((_raw + _offset) * _scale) can provide accurate values for
    single-ended to differential ADC input configuration.
    Allow users to set ADC read offset.
    Also make the default read offset zero which is probably the offset that fits
    the most common use cases of differential and pseudo differential ADCs.
    
    [1]: https://www.analog.com/media/en/technical-documentation/data-sheets/adaq4003.pdf
    [2]: https://www.analog.com/media/en/technical-documentation/product-selector-card/2pb_sarinputtypesfb.pdf
    [3]: https://git.kernel.org/pub/scm/linux/kernel/git/jic23/iio.git/tree/Documentation/ABI/testing/sysfs-bus-iio?h=testing#n453
    
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>
    (cherry picked from commit ef8c9376b313935a31b6b9f43e2a2158b9cc5b30)

--- drivers/iio/adc/ad400x.c
+++ drivers/iio/adc/ad400x.c
@@ -175,6 +175,7 @@ struct ad400x_state {
 	bool high_z_mode;
 
 	unsigned int num_bits;
+	int read_offset;
 
 	/*
 	 * DMA (thus cache coherency maintenance) requires the
@@ -375,8 +376,7 @@ static int ad400x_read_raw(struct iio_dev *indio_dev,
 		*val = ad400x_get_sampling_freq(st);
 		return IIO_VAL_INT;
 	case IIO_CHAN_INFO_OFFSET:
-		*val = -(1 << chan->scan_type.realbits);
-
+		*val = st->read_offset;
 		return IIO_VAL_INT;
 	default:
 		break;
@@ -413,9 +413,14 @@ static int ad400x_write_raw(struct iio_dev *indio_dev,
 			    struct iio_chan_spec const *chan,
 			    int val, int val2, long info)
 {
+	struct ad400x_state *st = iio_priv(indio_dev);
+
 	switch (info) {
 	case IIO_CHAN_INFO_SAMP_FREQ:
 		return ad400x_set_sampling_freq(indio_dev, val);
+	case IIO_CHAN_INFO_OFFSET:
+		st->read_offset = val;
+		return 0;
 	default:
 		return -EINVAL;
 	}
