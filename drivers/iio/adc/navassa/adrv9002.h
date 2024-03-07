fb93a5c3283d2b66af5964bd51ab68f1ab4070d4
Nuno Sa <nuno.sa@analog.com>
2024-03-07 14:07:45 +0100

    iio: adc: adrv9002: fixup tx2 power level
    There's a very odd issue where the tx2 power is significantly higher than
    tx1. The reason is far from being clear but it looks somehow to be related
    with tuning and the SSI delays. Some workarounds tested were:
      * Issuing a sync (reg 0x44) on the DDS;
      * Re-enabling the DDS core.
    
    Both options had to be done after tuning but they were only half fixing
    the issue. Meaning that TX2 power decreased to a level closer to TX1 but
    still around 6dbs higher. Hence, what seems to really fix the issue is to
    read the TX SSI status on the device side and with testdata set to
    FIXED_PATTERN. Somehow that is making hdl happy. Another thing that was
    noted was that doing this at every calibration point (after configuring the
    delays), on TX2, lead to more reliable tuning results (more noticeable on
    the LTE40 profile).
    
    Obviously, this is an awful workaround and we need to understand the root
    cause of the issue and properly fix things. Hopefully this won't one those
    things where "we fix it later" means never!
    
    Signed-off-by: Nuno Sa <nuno.sa@analog.com>
    (cherry picked from commit 7ebe866977d44f97fac7e1766702d6a676517978)
    (cherry picked from commit f582615c0fa2e1524cfeeab20ec3d172262f3546)

--- drivers/iio/adc/navassa/adrv9002.h
+++ drivers/iio/adc/navassa/adrv9002.h
@@ -293,6 +293,7 @@ int adrv9002_intf_test_cfg(const struct adrv9002_rf_phy *phy, const int chann, c
 int adrv9002_check_tx_test_pattern(const struct adrv9002_rf_phy *phy, const int chann);
 int adrv9002_intf_change_delay(const struct adrv9002_rf_phy *phy, const int channel, u8 clk_delay,
 			       u8 data_delay, const bool tx);
+int adrv9002_tx2_fixup(const struct adrv9002_rf_phy *phy);
 /* phy lock must be held before entering the API */
 int adrv9002_channel_to_state(const struct adrv9002_rf_phy *phy, struct adrv9002_chan *chann,
 			      const adi_adrv9001_ChannelState_e state, const bool cache_state);
