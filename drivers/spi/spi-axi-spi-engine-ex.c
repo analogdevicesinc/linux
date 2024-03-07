a9b649f1967b4bae407334365535847b3d372355
David Lechner <dlechner@baylibre.com>
2024-03-07 16:17:00 -0600

    spi: axi-spi-engine-ex: port offload functions
    This ports the existing out-of-tree offload functions from
    spi-axi-spi-engine to spi-axi-spi-engine-ex. API is the same. Function
    names contain "ex" to avoid conflicts with the existing driver.
    
    Signed-off-by: David Lechner <dlechner@baylibre.com>
    (cherry picked from commit 5d577354ab1d3e91b8b399c72a97ea47b6e2170c)

--- drivers/spi/spi-axi-spi-engine-ex.c
+++ drivers/spi/spi-axi-spi-engine-ex.c
@@ -33,10 +33,19 @@
 #define SPI_ENGINE_REG_SDI_DATA_FIFO		0xe8
 #define SPI_ENGINE_REG_SDI_DATA_FIFO_PEEK	0xec
 
+#define SPI_ENGINE_REG_OFFLOAD_CTRL(x)		(0x100 + 0x20 * (x))
+#define SPI_ENGINE_REG_OFFLOAD_STATUS(x)	(0x104 + 0x20 * (x))
+#define SPI_ENGINE_REG_OFFLOAD_RESET(x)		(0x108 + 0x20 * (x))
+#define SPI_ENGINE_REG_OFFLOAD_CMD_MEM(x)	(0x110 + 0x20 * (x))
+#define SPI_ENGINE_REG_OFFLOAD_SDO_MEM(x)	(0x114 + 0x20 * (x))
+
 #define SPI_ENGINE_INT_CMD_ALMOST_EMPTY		BIT(0)
 #define SPI_ENGINE_INT_SDO_ALMOST_EMPTY		BIT(1)
 #define SPI_ENGINE_INT_SDI_ALMOST_FULL		BIT(2)
 #define SPI_ENGINE_INT_SYNC			BIT(3)
+
+#define SPI_ENGINE_OFFLOAD_CTRL_ENABLE		BIT(0)
+#define SPI_ENGINE_OFFLOAD_STATUS_ENABLED	BIT(0)
 
 #define SPI_ENGINE_CONFIG_CPHA			BIT(0)
 #define SPI_ENGINE_CONFIG_CPOL			BIT(1)
@@ -285,6 +294,90 @@ static void spi_engine_compile_message(struct spi_message *msg, bool dry,
 		spi_engine_program_add_cmd(p, dry,
 			SPI_ENGINE_CMD_WRITE(SPI_ENGINE_CMD_REG_CLK_DIV, 0));
 }
+
+bool spi_engine_ex_offload_supported(struct spi_device *spi)
+{
+	if (strcmp(spi->controller->dev.parent->driver->name, "spi-engine-ex") != 0)
+		return false;
+
+	return true;
+}
+EXPORT_SYMBOL_GPL(spi_engine_ex_offload_supported);
+
+void spi_engine_ex_offload_enable(struct spi_device *spi, bool enable)
+{
+	struct spi_controller *host = spi->controller;
+	struct spi_engine *spi_engine = spi_controller_get_devdata(host);
+	unsigned int reg;
+
+	reg = readl(spi_engine->base + SPI_ENGINE_REG_OFFLOAD_CTRL(0));
+
+	if (enable)
+		reg |= SPI_ENGINE_OFFLOAD_CTRL_ENABLE;
+	else
+		reg &= ~SPI_ENGINE_OFFLOAD_CTRL_ENABLE;
+
+	writel(reg, spi_engine->base + SPI_ENGINE_REG_OFFLOAD_CTRL(0));
+}
+EXPORT_SYMBOL_GPL(spi_engine_ex_offload_enable);
+
+int spi_engine_ex_offload_load_msg(struct spi_device *spi,
+	struct spi_message *msg)
+{
+	struct spi_controller *host = spi->controller;
+	struct spi_engine *spi_engine = spi_controller_get_devdata(host);
+	struct spi_engine_program *p = msg->opt_state;
+	struct spi_transfer *xfer;
+	void __iomem *cmd_addr;
+	void __iomem *sdo_addr;
+	unsigned int i;
+
+	/*
+	 * Ensure that __spi_validate_msg() has been run on the msg and that
+	 * msg->opt_state contains the compiled program.
+	 */
+	if (!msg->pre_optimized) {
+		dev_err(&spi->dev, "requires optimized message\n");
+		return -EINVAL;
+	}
+
+	/* clear the offload FIFO memories */
+	writel(1, spi_engine->base + SPI_ENGINE_REG_OFFLOAD_RESET(0));
+	writel(0, spi_engine->base + SPI_ENGINE_REG_OFFLOAD_RESET(0));
+
+	cmd_addr = spi_engine->base + SPI_ENGINE_REG_OFFLOAD_CMD_MEM(0);
+	sdo_addr = spi_engine->base + SPI_ENGINE_REG_OFFLOAD_SDO_MEM(0);
+
+	/* TODO: validate that we don't exceed the offload CMD FIFO size */
+	for (i = 0; i < p->length; i++)
+		writel(p->instructions[i], cmd_addr);
+
+	/* TODO: validate that we don't exceed the offload SDO FIFO size */
+	list_for_each_entry(xfer, &msg->transfers, transfer_list) {
+		if (!xfer->tx_buf)
+			continue;
+
+		if (xfer->bits_per_word <= 8) {
+			const u8 *buf = xfer->tx_buf;
+
+			for (i = 0; i < xfer->len; i++)
+				writel_relaxed(buf[i], sdo_addr);
+		} else if (xfer->bits_per_word <= 16) {
+			const u16 *buf = (const u16 *)xfer->tx_buf;
+
+			for (i = 0; i < xfer->len / 2; i++)
+				writel_relaxed(buf[i], sdo_addr);
+		} else {
+			const u32 *buf = (const u32 *)xfer->tx_buf;
+
+			for (i = 0; i < xfer->len / 4; i++)
+				writel_relaxed(buf[i], sdo_addr);
+		}
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(spi_engine_ex_offload_load_msg);
 
 static void spi_engine_xfer_next(struct spi_message *msg,
 	struct spi_transfer **_xfer)
