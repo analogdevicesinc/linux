2947497b46ad753d385129e7d04ad7247e26b276
George Mois <george.mois@analog.com>
2024-03-12 10:25:29 +0200

    iio: adc: adrv904x: Add Koror API
    Add Koror API 2.10.0.4.
    
    Signed-off-by: George Mois <george.mois@analog.com>

---
+++ drivers/iio/adc/adrv904x/devices/adrv904x/public/include/adi_adrv904x_cpu_error_codes_types.h
@@ -0,0 +1,31 @@
+/**
+ * Disclaimer Legal Disclaimer
+ * Copyright 2019 - 2021 Analog Devices Inc.
+ * Released under the ADRV904X API license, for more information
+ * see the "LICENSE.PDF" file in this zip file.
+ */
+/**
+ * \file adi_adrv904x_cpu_error_codes_types.h
+ *
+ * \brief   Contains CPU error code definitions
+ *
+ * \details Contains CPU error code definitions
+ *
+ * ADRV904X API Version: 2.10.0.4
+ */
+
+#ifndef __ADI_ADRV904X_CPU_ERROR_CODES_TYPES_H__
+#define __ADI_ADRV904X_CPU_ERROR_CODES_TYPES_H__
+
+#ifdef __KERNEL__
+#include <linux/kernel.h>
+#else
+#include <stdint.h>
+#endif
+
+/* Typedef and enumeration for CPU system-wide error codes */
+typedef uint32_t adi_adrv904x_CpuErrorCode_t;
+
+#endif /* __ADI_ADRV904X_CPU_ERROR_CODES_TYPES_H__ */
+
+
