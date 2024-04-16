371a0801be0e75379845d3b521caa6b543c42dae
George Mois <george.mois@analog.com>
2024-04-16 16:52:32 +0300

    koror: adi_library_types.h: Add Linux types
    Add constructs to be used in the Linux kernel.
    
    Signed-off-by: George Mois <george.mois@analog.com>

--- drivers/iio/adc/adrv904x/platforms/adi_library_types.h
+++ drivers/iio/adc/adrv904x/platforms/adi_library_types.h
@@ -13,7 +13,12 @@
 #define _ADI_LIBRARY_TYPES_H_
 
 #ifdef __KERNEL__
+#include <linux/int_log.h>
 #include <linux/kernel.h>
+#include <linux/limits.h>
+#include <linux/ctype.h>
+#include <linux/types.h>
+#include <linux/slab.h>
 #include <linux/time.h>
 #else
 #include <stddef.h>
@@ -27,16 +32,39 @@
 #include <time.h>
 #include <limits.h>
 #include <ctype.h>
+#endif
+
+#ifdef __KERNEL__
+typedef time64_t time_t;
+#define INT16_MAX                                       S16_MAX
+#define INT16_MIN                                       S16_MIN
+#define UINT16_MAX                                      U16_MAX
+#define UINT32_MAX                                      U32_MAX
+
+#define PRIX32                                          "lX"
+#define PRIu32                                          "u"
+#define PRId64                                          "d"
+#define PRIX64                                          "llX"
+#define time_t                                          time64_t
+#endif
 
 /* stddef.h */
 #define ADI_LIBRARY_OFFSETOF                            offsetof
 
 /* stdio.h */
+#ifndef __KERNEL__
 #define ADI_LIBRARY_PRINTF                              printf
+#else
+#define ADI_LIBRARY_PRINTF                              printk
+#endif
 #define ADI_LIBRARY_FPRINTF                             fprintf
+#ifndef __KERNEL__
 #define ADI_LIBRARY_SPRINTF                             sprintf
+#endif
 #define ADI_LIBRARY_SNPRINTF                            snprintf
+#ifndef __KERNEL__
 #define ADI_LIBRARY_VPRINTF                             vprintf
+#endif
 #define ADI_LIBRARY_VSNPRINTF                           vsnprintf
 #define ADI_LIBRARY_FFLUSH                              fflush
 #define ADI_LIBRARY_FSEEK                               fseek
@@ -47,21 +75,30 @@
 #define ADI_LIBRARY_FCLOSE                              fclose
 #define ADI_LIBRARY_FTELL                               ftell
 #define ADI_LIBRARY_FERROR                              ferror
+#ifndef __KERNEL__
 #define ADI_LIBRARY_SETVBUF                             setvbuf
+#endif
 
 /* stdlib.h */
+#ifndef __KERNEL__
 #define ADI_LIBRARY_CALLOC                              calloc
 #define ADI_LIBRARY_FREE                                free
 #define ADI_LIBRARY_RAND                                rand
 #define ADI_LIBRARY_EXIT                                exit
 #define ADI_LIBRARY_ABS                                 abs
+#else
+#define ADI_LIBRARY_FREE                                kfree
+#define ADI_LIBRARY_CALLOC                              __adrv904x_calloc
+#endif
 
 /* stdarg.h */
 #define ADI_LIBRARY_VA_START                            va_start
 #define ADI_LIBRARY_VA_END                              va_end
 
 /* math.h*/
+#ifndef __KERNEL__
 #define ADI_LIBRARY_CEIL                                ceil
+#endif
 
 /* string.h */
 #define ADI_LIBRARY_MEMSET                              memset
@@ -98,7 +135,11 @@
 #define ADI_LIBRARY_MKTIME                              mktime
 #define ADI_LIBRARY_CTIME                               ctime
 #define ADI_LIBRARY_NANOSLEEP                           nanosleep
+#ifndef __KERNEL__
 #define ADI_LIBRARY_CLOCK                               clock
+#else
+#define ADI_LIBRARY_CLOCK                               ktime_get_ns
+#endif
 #define ADI_LIBRARY_CLOCKS_PER_SEC                      CLOCKS_PER_SEC
 #define ADI_LIBRARY_GMTIME                              gmtime
 
