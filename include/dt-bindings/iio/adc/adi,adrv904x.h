782dad5d173f994e5bd803c12f35cb3a1dee152c
George Mois <george.mois@analog.com>
2024-04-17 14:47:33 +0300

    iio: adc: adrv904x: Add ADRV9040 support
    Note:
     * DPD support has been omitted in this initial version.
    
    Signed-off-by: George Mois <george.mois@analog.com>

---
+++ include/dt-bindings/iio/adc/adi,adrv904x.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * This header provides constants for ADRV904X
+ *
+ * Copyright 2024 Analog Devices Inc.
+ */
+
+#ifndef _DT_BINDINGS_IIO_ADC_ADRV904X_H
+#define _DT_BINDINGS_IIO_ADC_ADRV904X_H
+
+ /*
+  * JESD204-FSM defines
+  */
+
+#define DEFRAMER0_LINK_TX	0
+#define DEFRAMER1_LINK_TX	1
+#define FRAMER0_LINK_RX		2
+#define FRAMER1_LINK_RX		3
+#define FRAMER2_LINK_RX		4
+
+#endif
