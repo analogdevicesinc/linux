85ec9c75507e4c62e16d49b421f63ca1d4489f85
Richard Zhu <hongxing.zhu@nxp.com>
2024-05-10 14:29:04 +0800

    PCI: dwc: Fix resume failure if no EP is connected at some platforms.
    The dw_pcie_suspend_noirq() function currently returns success directly
    if no endpoint (EP) device is connected. However, on some platforms, power
    loss occurs during suspend, causing dw_resume() to do nothing in this case.
    This results in a system halt because the DWC controller is not initialized
    after power-on during resume.
    
    Change call to deinit() in suspend and init() at resume regardless of
    whether there are EP device connections or not. It is not harmful to
    perform deinit() and init() again for the no power-off case, and it keeps
    the code simple and consistent in logic.
    
    Fixes: 4774faf854f5 ("PCI: dwc: Implement generic suspend/resume functionality")
    Signed-off-by: Richard Zhu <hongxing.zhu@nxp.com>

--- drivers/pci/controller/dwc/pcie-designware-host.c
+++ drivers/pci/controller/dwc/pcie-designware-host.c
@@ -927,23 +927,23 @@ int dw_pcie_suspend_noirq(struct dw_pcie *pci)
 	if (dw_pcie_readw_dbi(pci, offset + PCI_EXP_LNKCTL) & PCI_EXP_LNKCTL_ASPM_L1)
 		return 0;
 
-	if (dw_pcie_get_ltssm(pci) <= DW_PCIE_LTSSM_DETECT_ACT)
-		return 0;
+	if (dw_pcie_get_ltssm(pci) > DW_PCIE_LTSSM_DETECT_ACT) {
+		/* Only send out PME_TURN_OFF when PCIE link is up */
+		if (pci->pp.ops->pme_turn_off)
+			pci->pp.ops->pme_turn_off(&pci->pp);
+		else
+			ret = dw_pcie_pme_turn_off(pci);
 
-	if (pci->pp.ops->pme_turn_off)
-		pci->pp.ops->pme_turn_off(&pci->pp);
-	else
-		ret = dw_pcie_pme_turn_off(pci);
+		if (ret)
+			return ret;
 
-	if (ret)
-		return ret;
-
-	ret = read_poll_timeout(dw_pcie_get_ltssm, val, val == DW_PCIE_LTSSM_L2_IDLE,
-				PCIE_PME_TO_L2_TIMEOUT_US/10,
-				PCIE_PME_TO_L2_TIMEOUT_US, false, pci);
-	if (ret) {
-		dev_err(pci->dev, "Timeout waiting for L2 entry! LTSSM: 0x%x\n", val);
-		return ret;
+		ret = read_poll_timeout(dw_pcie_get_ltssm, val, val == DW_PCIE_LTSSM_L2_IDLE,
+					PCIE_PME_TO_L2_TIMEOUT_US/10,
+					PCIE_PME_TO_L2_TIMEOUT_US, false, pci);
+		if (ret) {
+			dev_err(pci->dev, "Timeout waiting for L2 entry! LTSSM: 0x%x\n", val);
+			return ret;
+		}
 	}
 
 	if (pci->pp.ops->deinit)
