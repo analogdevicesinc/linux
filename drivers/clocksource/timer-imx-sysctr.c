8d72ccbb6e1cd0939a16b40a575a9679ba9feb0a
Peng Fan <peng.fan@nxp.com>
2024-05-28 10:43:59 +0800

    LF-12085 clocksource/drivers/imx-sysctr: fix i.MX95 support
    With gplay test[1] together with system suspend/resume, linux sometimes
    stuck with all 6 A55 cores in cpuidle. It is the broadcast timer not
    trigger interrupt, although set_next_event set the experied time.
    
    In set_next_event:
    write CMP_CR to disable CMP EVENT
    write CMP_HI
    write CMP_LO
    write CMP_CR to enable CMP EVENT (a)
    After step (a) CMP_HI value was unexpected changed to a very large value.
    So broadcast timer interrupt not trigger in time.
    
    Per HW team:
    CR  101100  -> Writing to CR can end up updating to HI/LO or address 0x0
    HI  100100  -> Writing to HI can end up updating LO or 0x0.
    LO  100000  -> Writing to LO can end up updating 0x0.
    register address 0x0 is write ignored, so no need handle it.
    
    The fix for errata ERR052268 is:
    before the upper step (a), disable sysctr interrupt
    after upper step a:
    write CMP_HI, in case step (a) wrongly updates CMP_HI
    write CMP_LO, in case step (a) or updating CMP_HI wrongly updates CMP_LO
    read back CMP_HI and CMP_LO to check, but this should always be correct
    value.
    enable sysctr interrupt.
    
    Because step (a) may wrongly update CMP_HI and CMP_LO, so need disable
    interrupt to avoid interrupt firing immediately.
    
    [1]:
     gst-launch-1.0 videotestsrc ! waylandsink window-width=800
    window-height=600 > /dev/null &
     while true; do glmark2-es2-wayland > /dev/null; done &
    
    Reviewed-by: Ranjani Vaidyanathan <ranjani.vaidyanathan@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- drivers/clocksource/timer-imx-sysctr.c
+++ drivers/clocksource/timer-imx-sysctr.c
@@ -26,19 +26,49 @@
 
 #define SYS_CTR_CLK_DIV		0x3
 
+#define SYS_CTR_IMX95		BIT(0)
+
 struct sysctr_private {
 	u32 cmpcr;
 	u32 lo_off;
 	u32 hi_off;
+	u32 flag;
 };
+
+static inline bool sysctr_is_imx95(struct sysctr_private *priv)
+{
+	return priv->flag & SYS_CTR_IMX95 ? true : false;
+}
+
+static void sysctr_timer_read_write(void __iomem *addr, u32 mask, u32 val, int count)
+{
+	u32 i = 0;
+
+	while ((readl(addr) & mask) != val) {
+		writel(val, addr);
+		count--;
+		if (count <= 0) {
+			pr_err("%s:%p:%x write failed, retry: %u\n",
+			       __func__, (addr), val, i++);
+			count = 1000;
+		}
+	}
+}
 
 static void sysctr_timer_enable(struct clock_event_device *evt, bool enable)
 {
 	struct timer_of *to = to_timer_of(evt);
 	struct sysctr_private *priv = to->private_data;
 	void __iomem *base = timer_of_base(to);
+	u32 val;
 
-	writel(enable ? priv->cmpcr | SYS_CTR_EN : priv->cmpcr, base + CMPCR);
+	val = enable ? priv->cmpcr | SYS_CTR_EN : priv->cmpcr;
+	writel(val, base + CMPCR);
+
+	if (!sysctr_is_imx95(priv))
+		return;
+
+	sysctr_timer_read_write(base + CMPCR, val, val, 1000);
 }
 
 static void sysctr_irq_acknowledge(struct clock_event_device *evt)
@@ -71,6 +101,7 @@ static int sysctr_set_next_event(unsigned long delta,
 				 struct clock_event_device *evt)
 {
 	struct timer_of *to = to_timer_of(evt);
+	struct sysctr_private *priv = to->private_data;
 	void __iomem *base = timer_of_base(to);
 	u32 cmp_hi, cmp_lo;
 	u64 next;
@@ -87,7 +118,18 @@ static int sysctr_set_next_event(unsigned long delta,
 	writel_relaxed(cmp_hi, base + CMPCV_HI);
 	writel_relaxed(cmp_lo, base + CMPCV_LO);
 
+	if (sysctr_is_imx95(priv))
+		disable_irq_nosync(evt->irq);
+
 	sysctr_timer_enable(evt, true);
+
+	if (!sysctr_is_imx95(priv))
+		return 0;
+
+	sysctr_timer_read_write(base + CMPCV_HI, GENMASK(31, 0), cmp_hi, 1000);
+	sysctr_timer_read_write(base + CMPCV_LO, GENMASK(31, 0), cmp_lo, 1000);
+
+	enable_irq(evt->irq);
 
 	return 0;
 }
@@ -197,6 +239,7 @@ static int __init sysctr_timer_imx95_init(struct device_node *np)
 	priv = to_sysctr.private_data;
 	priv->lo_off = CNTCV_LO_IMX95;
 	priv->hi_off = CNTCV_HI_IMX95;
+	priv->flag = SYS_CTR_IMX95;
 
 	clockevents_config_and_register(&to_sysctr.clkevt,
 					timer_of_rate(&to_sysctr),
