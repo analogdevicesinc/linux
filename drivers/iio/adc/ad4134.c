42efc71c7b0a60c4a0c9ecd9b6cd9305e51b593d
Ioan-daniel Pop <Pop.Ioan-daniel@analog.com>
2024-07-02 16:58:13 -0300

    iio: adc: ad4134: Tweak PWM duty cycle
    From: Ioan-daniel Pop <Pop.Ioan-daniel@analog.com>
    
    Tweak _ad4134_set_odr().
    
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>
    Signed-off-by: Ioan-daniel Pop <Pop.Ioan-daniel@analog.com>

--- drivers/iio/adc/ad4134.c
+++ drivers/iio/adc/ad4134.c
@@ -137,7 +137,7 @@ static int _ad4134_set_odr(struct ad4134_state *st, unsigned int odr)
 	 * tODR_HIGH_TIME = 3 * tDIGCLK
 	 * See datasheet page 10, Table 3. Data Interface Timing with Gated DCLK.
 	 */
-	state.duty_cycle = DIV_ROUND_CLOSEST_ULL(PICO * 6, st->sys_clk_rate);
+	state.duty_cycle = DIV_ROUND_CLOSEST_ULL(PICO * 6, st->sys_clk_rate) - 1;
 	state.period = DIV_ROUND_CLOSEST_ULL(PICO, odr);
 	state.time_unit = PWM_UNIT_PSEC;
 
