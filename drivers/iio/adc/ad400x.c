f042b95aea2017303a45d6baf0af121771095b3a
Uwe Kleine-König <u.kleine-koenig@baylibre.com>
2024-07-11 16:21:24 +0200

    iio: adc: ad400x: Simplify period calculation
    Instead of assigning the value `ref_clk_period_ps * target` assign
    `DIV_ROUND_CLOSEST_ULL(1000000000000, freq)` which apart from rounding
    loss is the same value:
    
    	  ref_clk_period_ps * target
    	= DIV_ROUND_CLOSEST_ULL(1000000000000, st->ref_clk_rate) * DIV_ROUND_CLOSEST_ULL(st->ref_clk_rate, freq)
    	≅ 1000000000000 / st->ref_clk_rate * st->ref_clk_rate / freq
    	= 1000000000000 / freq
    
    With freq = 1800000 and st->ref_clk_rate = 166666665 (which I think are
    reasonable assumptions) the exact result is 555555.5555555555. The
    previous calculation yielded 558000, while now it gives 555556.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@baylibre.com>

--- drivers/iio/adc/ad400x.c
+++ drivers/iio/adc/ad400x.c
@@ -192,16 +192,15 @@ static int ad400x_get_sampling_freq(struct ad400x_state *st)
 
 static int __ad400x_set_sampling_freq(struct ad400x_state *st, int freq)
 {
-	unsigned long long target, ref_clk_period_ps;
+	unsigned long long ref_clk_period_ps;
 	struct pwm_state cnv_state;
 
 	/* Sync up PWM state and prepare for pwm_apply_state(). */
 	pwm_init_state(st->cnv_trigger, &cnv_state);
 
-	target = DIV_ROUND_CLOSEST_ULL(st->ref_clk_rate, freq);
 	ref_clk_period_ps = DIV_ROUND_CLOSEST_ULL(1000000000000,
 						  st->ref_clk_rate);
-	cnv_state.period = ref_clk_period_ps * target;
+	cnv_state.period = DIV_ROUND_CLOSEST_ULL(1000000000000, freq);
 	cnv_state.duty_cycle = ref_clk_period_ps;
 	cnv_state.time_unit = PWM_UNIT_PSEC;
 	return pwm_apply_state(st->cnv_trigger, &cnv_state);
