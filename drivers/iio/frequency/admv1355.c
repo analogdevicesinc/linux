10e17d2294269d29b2461918fe5ed79bbe4706d5
George Mois <george.mois@analog.com>
2024-08-28 12:16:00 +0300

    drivers: iio: frequency: admv1355.c: Set filters
    Add filter setting logic depending on LO.
    
    Signed-off-by: George Mois <george.mois@analog.com>

--- drivers/iio/frequency/admv1355.c
+++ drivers/iio/frequency/admv1355.c
@@ -5,14 +5,15 @@
  * Copyright (C) 2024 Analog Devices, Inc.
  */
 
-#include <linux/module.h>
 #include <linux/gpio/consumer.h>
+#include <linux/clk/clkscale.h>
+#include <linux/notifier.h>
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
+#include <linux/module.h>
 #include <linux/regmap.h>
-#include <linux/notifier.h>
+#include <linux/units.h>
 #include <linux/clk.h>
-#include <linux/clk/clkscale.h>
 
 #define ADMV1355_REG_PRODUCT_ID_LSB	0x004
 
@@ -61,11 +62,135 @@ static const struct iio_info admv1355_info = {
 	.debugfs_reg_access = &admv1355_debug_reg_access,
 };
 
+static int admv1355_set_filters(struct admv1355_priv *priv, u64 rate_khz)
+{
+	u8 lo_trap_filter_7_0;
+	u8 lo_trap_filter_9_8;
+	u8 lo_doubler_band;
+	u16 lo_trap_filter;
+	u8 lo_x4_filter;
+	u8 lo_x3_filter;
+	u8 lo_x1_filter;
+	int ret;
+
+	// LO X4 FILTER REG0800
+	// LO X3 FILTER REG0801
+	// LO X1 FILTER REG0802
+	// LO TRAP FILTER 7:0 REG0804
+	// LO TRAP FILTER 9:8 REG0805
+	// LO DOUBLED BAND REG080C
+	if (rate_khz < (10000 * HZ_PER_KHZ)) {
+		lo_x4_filter = 0x08;
+		lo_x3_filter = 0x1C;
+		lo_x1_filter = 0x1F;
+		lo_trap_filter = 0x318;
+		lo_doubler_band = 0x1F;
+	} else if (rate_khz >= (10000 * HZ_PER_KHZ) && rate_khz < (12000 * HZ_PER_KHZ)) {
+		lo_x4_filter = 0x00;
+		lo_x3_filter = 0x1C;
+		lo_x1_filter = 0x1F;
+		lo_trap_filter = 0x318;
+		lo_doubler_band = 0x1D;
+	} else if (rate_khz >= (12000 * HZ_PER_KHZ) && rate_khz < (14000 * HZ_PER_KHZ)) {
+		lo_x4_filter = 0x00;
+		lo_x3_filter = 0x07;
+		lo_x1_filter = 0x0A;
+		lo_trap_filter = 0x3AA;
+		lo_doubler_band = 0x04;
+	} else if (rate_khz >= (14000 * HZ_PER_KHZ) && rate_khz < (14500 * HZ_PER_KHZ)) {
+		lo_x4_filter = 0x05;
+		lo_x3_filter = 0x07;
+		lo_x1_filter = 0x0A;
+		lo_trap_filter = 0x3AA;
+		lo_doubler_band = 0x08;
+	} else if (rate_khz >= (14500 * HZ_PER_KHZ) && rate_khz < (15000 * HZ_PER_KHZ)) {
+		lo_x4_filter = 0x05;
+		lo_x3_filter = 0x07;
+		lo_x1_filter = 0x0A;
+		lo_trap_filter = 0x1A3;
+		lo_doubler_band = 0x08;
+	} else if (rate_khz >= (15000 * HZ_PER_KHZ) && rate_khz < (17000 * HZ_PER_KHZ)) {
+		lo_x4_filter = 0x05;
+		lo_x3_filter = 0x05;
+		lo_x1_filter = 0x05;
+		lo_trap_filter = 0x1A3;
+		lo_doubler_band = 0x08;
+	} else if (rate_khz >= (17000 * HZ_PER_KHZ) && rate_khz < (18000 * HZ_PER_KHZ)) {
+		lo_x4_filter = 0x05;
+		lo_x3_filter = 0x04;
+		lo_x1_filter = 0x01;
+		lo_trap_filter = 0x1A3;
+		lo_doubler_band = 0x08;
+	} else if (rate_khz >= (18000 * HZ_PER_KHZ) && rate_khz < (18500 * HZ_PER_KHZ)) {
+		lo_x4_filter = 0x07;
+		lo_x3_filter = 0x04;
+		lo_x1_filter = 0x01;
+		lo_trap_filter = 0x1A3;
+		lo_doubler_band = 0x80;
+	} else if (rate_khz >= (18500 * HZ_PER_KHZ) && rate_khz < (19000 * HZ_PER_KHZ)) {
+		lo_x4_filter = 0x07;
+		lo_x3_filter = 0x04;
+		lo_x1_filter = 0x01;
+		lo_trap_filter = 0x1A7;
+		lo_doubler_band = 0x80;
+	}else if (rate_khz >= (19000 * HZ_PER_KHZ) && rate_khz < (21000 * HZ_PER_KHZ)) {
+		lo_x4_filter = 0x07;
+		lo_x3_filter = 0x03;
+		lo_x1_filter = 0x01;
+		lo_trap_filter = 0x1A7;
+		lo_doubler_band = 0x80;
+	} else if (rate_khz >= (21000 * HZ_PER_KHZ) && rate_khz < (24000 * HZ_PER_KHZ)) {
+		lo_x4_filter = 0x07;
+		lo_x3_filter = 0x02;
+		lo_x1_filter = 0x00;
+		lo_trap_filter = 0x1E7;
+		lo_doubler_band = 0x80;
+	} else {
+		lo_x4_filter = 0x07;
+		lo_x3_filter = 0x00;
+		lo_x1_filter = 0x00;
+		lo_trap_filter = 0x1E7;
+		lo_doubler_band = 0xC0;
+	}
+
+	lo_trap_filter_7_0 = lo_trap_filter & 0xff;
+	lo_trap_filter_9_8 = (lo_trap_filter >> 8) & 0x03;
+
+	ret = regmap_write(priv->regmap, ADMV1355_REG0800, lo_x4_filter);
+	if (ret)
+		return ret;
+
+	ret = regmap_write(priv->regmap, ADMV1355_REG0801, lo_x3_filter);
+	if (ret)
+		return ret;
+
+	ret = regmap_write(priv->regmap, ADMV1355_REG0802, lo_x1_filter);
+	if (ret)
+		return ret;
+
+	ret = regmap_write(priv->regmap, ADMV1355_REG0804, lo_trap_filter_7_0);
+	if (ret)
+		return ret;
+
+	ret = regmap_update_bits(priv->regmap, ADMV1355_REG0805, 0x03, lo_trap_filter_9_8);
+	if (ret)
+		return ret;
+
+	ret = regmap_write(priv->regmap, ADMV1355_REG080C, lo_doubler_band);
+	if (ret)
+		return ret;
+
+	return 0;
+}
+
 static int admv1355_update(struct admv1355_priv *priv)
 {
+	int ret;
 	u64 rate = clk_get_rate_scaled(priv->lo_input, &priv->clkscale);
 
-	pr_err("Frequency changed. New rate: %llu.\n", rate);
+	ret = admv1355_set_filters(priv, rate);
+	if (ret)
+		pr_err("Cannot apply settings for LO rate %llu.\n", rate);
 
 	return 0;
 }
@@ -111,7 +236,8 @@ static int admv1355_setup(struct iio_dev *indio_dev)
 	unsigned int product_id;
 	int ret;
 
-	ret = regmap_write(priv->regmap, 0x000, 0x18);
+	// Set SPI 4 wire
+	ret = regmap_write(priv->regmap, ADMV1355_REG0000, 0x18);
 	if (ret)
 		return ret;
 
@@ -122,68 +248,59 @@ static int admv1355_setup(struct iio_dev *indio_dev)
 
 	pr_err("%s: PRODUCT_ID: 0x%X\n", priv->spi->dev.of_node->name, product_id);
 
+	// Power down unused components
+	ret = regmap_write(priv->regmap, ADMV1355_REG013E, 0x02);
+	if (ret)
+		return ret;
+
+	// Disable filter table
+	ret = regmap_write(priv->regmap, ADMV1355_REG0202, 0x00);
+	if (ret)
+		return ret;
+
+	// Disable filter load
+	ret = regmap_write(priv->regmap, ADMV1355_REG0208, 0x00);
+	if (ret)
+		return ret;
+
+	// Enable gain table bypass
+	ret = regmap_write(priv->regmap, ADMV1355_REG028A, 0x01);
+	if (ret)
+		return ret;
+
+	// DSA1 and DSA2 bypass values
+	ret = regmap_write(priv->regmap, ADMV1355_REG028B, 0x00);
+	if (ret)
+		return ret;
+
+	// LPF state determined by SPI
+	ret = regmap_write(priv->regmap, ADMV1355_REG02A0, 0x10);
+	if (ret)
+		return ret;
+
+	// HPF determined by SPI, cutoff frequenct 0x3F (reset value)
+	ret = regmap_write(priv->regmap, ADMV1355_REG02A1, 0xBF);
+	if (ret)
+		return ret;
+
+	// Set GPIO for gain table
+	ret = regmap_write(priv->regmap, ADMV1355_REG0601, 0x08);
+	if (ret)
+		return ret;
+
 return 0;
-	ret = regmap_write(priv->regmap, 0x13E, 0x02);
+// Setting direct RF band (0 - low, 1 - high)
+	ret = regmap_update_bits(priv->regmap, ADMV1355_REG0805, 0x80, 0);
 	if (ret)
 		return ret;
 
-	ret = regmap_write(priv->regmap, 0x202, 0x00);
+// Setting LO band, mixer band, LO sideband and direct LO Phase
+	ret = regmap_write(priv->regmap, ADMV1355_REG0806, 0xF0);
 	if (ret)
 		return ret;
 
-	ret = regmap_write(priv->regmap, 0x208, 0x00);
-	if (ret)
-		return ret;
-
-	ret = regmap_write(priv->regmap, 0x28A, 0x01);
-	if (ret)
-		return ret;
-
-	ret = regmap_write(priv->regmap, 0x28B, 0x00);
-	if (ret)
-		return ret;
-
-	ret = regmap_write(priv->regmap, 0x2A0, 0x10);
-	if (ret)
-		return ret;
-
-	ret = regmap_write(priv->regmap, 0x2A1, 0xBF);
-	if (ret)
-		return ret;
-
-	ret = regmap_write(priv->regmap, 0x601, 0x08);
-	if (ret)
-		return ret;
-
-	ret = regmap_write(priv->regmap, 0x800, 0x00);
-	if (ret)
-		return ret;
-
-	ret = regmap_write(priv->regmap, 0x801, 0x1C);
-	if (ret)
-		return ret;
-
-	ret = regmap_write(priv->regmap, 0x802, 0x1F);
-	if (ret)
-		return ret;
-
-	ret = regmap_write(priv->regmap, 0x804, 0x18);
-	if (ret)
-		return ret;
-
-	ret = regmap_write(priv->regmap, 0x805, 0xC3);
-	if (ret)
-		return ret;
-
-	ret = regmap_write(priv->regmap, 0x806, 0xF0);
-	if (ret)
-		return ret;
-
-	ret = regmap_write(priv->regmap, 0x80A, 0x00);
-	if (ret)
-		return ret;
-
-	ret = regmap_write(priv->regmap, 0x80C, 0x1D);
+// Set direct IF gain
+	ret = regmap_write(priv->regmap, ADMV1355_REG080A, 0x00);
 	if (ret)
 		return ret;
 
@@ -205,6 +322,7 @@ static int admv1355_probe(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev;
 	struct admv1355_priv *priv;
+	u64 rate = 0;
 	int ret;
 
 	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*priv));
@@ -258,6 +376,14 @@ static int admv1355_probe(struct spi_device *spi)
 	ret = admv1355_setup(indio_dev);
 	if (ret)
 		return ret;
+
+	rate = clk_get_rate_scaled(priv->lo_input, &priv->clkscale);
+
+	ret = admv1355_set_filters(priv, rate);
+	if (ret) {
+		pr_err("Cannot apply settings for LO rate %llu.\n", rate);
+		return ret;
+	}
 
 	//indio_dev->name = "admv1355";
 	indio_dev->name = spi->dev.of_node->name;
