769251b9df66c480af829a8013837e4f55baea37
Antoniu Miclaus <antoniu.miclaus@analog.com>
2024-09-02 11:46:35 +0300

    ltc4296: update init function and overlay
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- drivers/hwmon/ltc4296.c
+++ drivers/hwmon/ltc4296.c
@@ -182,6 +182,12 @@
 
 #define RTESTLOAD				200 /*(ohm)*/
 
+#define T_RSTL_NOM		9000
+#define T_MSP			2000
+
+#define SCCP_TYPE_MASK         0xF000
+#define SCCP_CLASS_TYPE_MASK   0x0FFF
+
 enum ltc4296_state_dev {
 	LTC_UNLOCKED = 0,
 	LTC_LOCKED
@@ -293,6 +299,53 @@ static int ltc4296_spoe_vol_range_mv[12][2] = {
 	{9600, 15000}    /* APL Class A oldAPL    */
 };
 
+const u8 class_compatibility[16][16] = {
+//PD PSE
+// 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+/*0*/  {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+/*1*/  {0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+/*2*/  {0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+/*3*/  {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+/*4*/  {0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+/*5*/  {0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+/*6*/  {0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+/*7*/  {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+/*8*/  {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+/*9*/  {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+/*10*/  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+/*11*/  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+/*12*/  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+/*13*/  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+/*14*/  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+/*15*/  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+};
+
+/* Top 4 bits give the SCCP Types
+ *                              A,   B,   C,   D,   E
+*/
+const u8 sccp_types[5] = { 0xE, 0xD, 0xB, 0x7, 0xC };
+
+/* Bottom 12 bits give the SCCP class*/
+#define SCCP_CLASS_SIZE 16
+u16 sccp_classes[SCCP_CLASS_SIZE] = {
+    0x3FE, // class 0
+    0x3FD, // class 1
+    0x3FB, // class 2
+    0x3F7, // class 3
+    0x3F7, // class 4
+    0x3EF, // class 5
+    0x3DF, // class 6
+    0x3BF, // class 7
+    0x37F, // class 8
+    0x2FF, // class 9
+    0x001, // class 10 /* Supported by D2Z Board*/
+    0x002, // class 11 /* Supported by D2Z Board*/
+    0x003, // class 12 /* Supported by D2Z Board*/
+    0x004, // class 13 /* Supported by D2Z Board*/
+    0x005, // class 14 /* Supported by D2Z Board*/
+    0x006  // class 15 /* Supported by D2Z Board*/
+};
+
 static u8 set_port_vout[LTC4296_MAX_PORTS] = {0x04, 0x06, 0x08, 0x0A, 0x0C};
 
 struct ltc4296_vi {
@@ -303,10 +356,262 @@ struct ltc4296_vi {
 };
 
 struct ltc4296_state {
+	struct gpio_desc *sccpo;
+	struct gpio_desc *sccpi;
 	struct spi_device *spi;
 	struct ltc4296_vi ltc4296_vi;
 	u8 data[5];
 };
+
+static u8 get_CRC(u8* buf)
+{
+	u8 byte, bit;
+	u8 x3,x4,x7, in;
+
+	u8 crc = 0;
+	for(byte=0; byte<2; byte++)
+	{
+		for(bit=0; bit<8; bit++)
+		{
+			/* save some bits before shifting register */
+			x3 = (crc>>3) & 0x01;
+			x4 = (crc>>4) & 0x01;
+			x7 = (crc>>7) & 0x01;
+			in = (buf[byte] >> bit) & 0x01;
+			in ^= x7;
+			/* shift the register */
+			crc  = (crc<<1) | in;
+
+			/* clear bits 4 & 5 */
+			crc &= ~(0x30);
+
+			/* replace bits with xor of 'in' and prev bit */
+			u8 temp = x3 ^ in;
+			crc |= (temp<<4);
+			temp = x4 ^ in;
+			crc |= (temp<<5);
+		}
+	}
+	return crc;
+}
+
+u8 READ_LINE(struct ltc4296_state *st)
+{
+	volatile u32 bit=0;
+
+	bit = gpiod_get_value(st->sccpi);
+
+	return bit;
+}
+
+void PULL_DOWN_LINE(struct ltc4296_state *st)
+{
+	gpiod_set_value(st->sccpo, 1);
+}
+
+void RELEASE_LINE(struct ltc4296_state *st)
+{
+	gpiod_set_value(st->sccpo, 0);
+}
+
+void write_bit(struct ltc4296_state *st, u8 bit)
+{
+	PULL_DOWN_LINE(st);
+	if (bit)
+	{
+		fsleep(300);
+		RELEASE_LINE(st);
+		 fsleep(2150);//T_WRITESLOT-T_REC-T_W1L = 2.15
+	}
+	else
+	{
+		fsleep(2450); //TW0L + 0.45 = 2.45
+		RELEASE_LINE(st);
+	}
+
+	/*Recovery time after every bit transmit */
+	 fsleep(320);//0.32
+	return;
+}
+
+void transmit_byte(struct ltc4296_state *st, u8 tx_byte)
+{
+	u8 bit_pos = 0;
+	while (bit_pos < 8)
+	{
+		u8 bit = (tx_byte>>bit_pos) & 0x01;
+		write_bit(st, bit);
+		bit_pos++;
+	}
+	return;
+}
+
+u8 read_bit(struct ltc4296_state *st)
+{
+	u8 bit;
+
+	PULL_DOWN_LINE(st);
+	fsleep(300); //T_W1L =0.3
+
+	RELEASE_LINE(st);
+
+	fsleep(700); //T_MSR-T_W1L = 1.225-0.3 = 700
+
+	bit = READ_LINE(st);
+
+	fsleep(2000); //T_READSLOT-T_MSR = 3-1 =2
+
+	fsleep(320); //T_REC
+	return bit;
+}
+
+void receive_response(struct ltc4296_state *st, u8* buf)
+{
+	volatile u8 rx_byte = 0;
+	volatile u8 bytes_rxd=0;
+	volatile u8 bit_pos = 0;
+    	u8 sccp_buf[3] = {0, 0, 0};
+
+	while (bytes_rxd < 3)
+	{
+		rx_byte = 0;
+		bit_pos = 0;
+		while(bit_pos < 8)
+		{
+			u8 bit = read_bit(st);
+			rx_byte |= (bit<<bit_pos);
+			bit_pos++;
+		}
+		sccp_buf[bytes_rxd] = rx_byte;
+		bytes_rxd++;
+		fsleep(5000);
+	}
+	buf[0]=sccp_buf[0];
+	buf[1]=sccp_buf[1];
+	buf[2]=sccp_buf[2];
+	return;
+}
+
+enum adi_ltc_result sccp_reset_pulse(struct ltc4296_state *st)
+{
+	u8 level=0;
+	//u32 i=0,j,k;
+	enum adi_ltc_result ret= ADI_LTC_SCCP_PD_PRESENT;
+
+	/* check if the line is high before reset pulse */
+	if(!READ_LINE(st))
+	{
+		return ADI_LTC_SCCP_PD_LINE_NOT_HIGH;
+	}
+
+	/* assert pulse */
+	PULL_DOWN_LINE(st);
+
+	/* check to make sure line is actually getting pulled down (protect pull down fet) */
+	fsleep(3000);
+
+	if(READ_LINE(st))
+	{
+		/* release because fet must be pulling down against stronger source than a classification v source */
+		RELEASE_LINE(st);
+		return ADI_LTC_SCCP_PD_LINE_NOT_LOW;
+	}
+
+	fsleep(T_RSTL_NOM-3000);
+
+	RELEASE_LINE(st);
+
+	fsleep(T_MSP);
+
+	/* look for presence pulse */
+    	level = READ_LINE(st);
+
+	fsleep(4000);
+
+	if(level == 1)
+		ret = ADI_LTC_SCCP_PD_NOT_PRESENT;
+	else if(level == 0)
+		ret = ADI_LTC_SCCP_PD_PRESENT;
+
+	return ret;
+}
+
+enum adi_ltc_result sccp_read_write_pd(struct ltc4296_state *st, u8 addr, u8 cmd, u8* buf)
+{
+	enum adi_ltc_result ret;
+
+	ret = sccp_reset_pulse(st);
+	if(ret == ADI_LTC_SCCP_PD_NOT_PRESENT)
+		return ADI_LTC_SCCP_PD_NOT_PRESENT; //PD is not present
+	else if(ret == ADI_LTC_SCCP_PD_LINE_NOT_LOW)
+		return ADI_LTC_SCCP_PD_LINE_NOT_LOW;
+	else if(ret == ADI_LTC_SCCP_PD_LINE_NOT_HIGH)
+		return ADI_LTC_SCCP_PD_LINE_NOT_HIGH;
+
+	fsleep(5000);
+	transmit_byte(st, addr);
+	fsleep(5000);
+	transmit_byte(st, cmd);
+	fsleep(5000);
+	receive_response(st, buf);
+
+	/* Check if the received data from PD is valid */
+	if (get_CRC(buf) != buf[2] )
+	{
+		printk("PD CRC Error, CRC is 0x%x \n\r", buf[2]);
+		return ADI_LTC_SCCP_PD_CRC_FAILED; /* Wrong CRC*/
+	}
+	return ADI_LTC_SCCP_PD_PRESENT;
+}
+
+enum adi_ltc_result sccp_is_pd(struct ltc4296_state *st, u8 pseClass, u16 sccpResponseData, u8 *pdClass)
+{
+	u8 i,sccpType;
+	u16 val, pdType=0;
+	enum adi_ltc_result ret;
+
+	/* bottom 12 bits of CLASS_TYPE_INFO codes for PD power classes 0 through 15 */
+	val = (sccpResponseData & SCCP_CLASS_TYPE_MASK);
+
+	/*we support classes 10- Classes15*/
+	for(i=0; i<SCCP_CLASS_SIZE; i++)
+	{
+		if(sccp_classes[i] == val)
+		{
+			pdType = i;
+			break;
+		}
+	}
+
+	/* Check for SCCP type; MSB 4 bits */
+	sccpType = ( (sccpResponseData & SCCP_TYPE_MASK) >> 12);
+	if(sccpType != sccp_types[4])
+	{
+		*pdClass = i;
+		return ADI_LTC_SCCP_PD_CLASS_NOT_SUPPORTED;
+	}
+
+	/* PD Type supported are Class10 to Class15*/
+	if( (pdType < 10) || (pdType > 15) )
+	{
+		/* The PD class is not supported by the D2Z board*/
+		*pdClass = i;
+		return ADI_LTC_SCCP_PD_CLASS_NOT_SUPPORTED;
+	}
+
+	if( class_compatibility[pdType][pseClass+10] == 1)
+	{
+		ret = ADI_LTC_SCCP_PD_CLASS_COMPATIBLE;
+		*pdClass = i;
+	}
+	else
+	{
+		ret = ADI_LTC_SCCP_PD_CLASS_NOT_SUPPORTED;
+		*pdClass = i;
+	}
+    return ret;
+}
+
 
 static u8 ltc4296_get_pec_byte(u8 data, u8 seed)
 {
@@ -1032,25 +1337,55 @@ static int ltc4296_init(struct ltc4296_state *st)
 		return -EINVAL;
 	}
 
-	ret = ltc4296_port_prebias(st, LTC_PORT0, LTC_CFG_APL_MODE);
-	if (ret)
-		return ret;
+	printk("Device unlocked.\n");
 
-	ret = ltc4296_port_prebias(st, LTC_PORT1, LTC_CFG_APL_MODE);
-	if (ret)
-		return ret;
+	// ret = ltc4296_port_prebias(st, LTC_PORT0, LTC_CFG_APL_MODE);
+	// if (ret)
+	// 	return ret;
 
-	ret = ltc4296_port_prebias(st, LTC_PORT2, LTC_CFG_APL_MODE);
-	if (ret)
-		return ret;
+	// ret = ltc4296_port_en(st, LTC_PORT0);
+	// if (ret)
+	// 	return ret;
 
-	ret = ltc4296_port_prebias(st, LTC_PORT3, LTC_CFG_APL_MODE);
-	if (ret)
-		return ret;
+	// fsleep(10000);
 
-	ret = ltc4296_port_prebias(st, LTC_PORT4, LTC_CFG_APL_MODE);
-	if (ret)
-		return ret;
+	// ltc4296_port_prebias(st, LTC_PORT1, LTC_CFG_APL_MODE);
+	// if (ret)
+	// 	return ret;
+
+	// ltc4296_port_en(st, LTC_PORT1);
+	// if (ret)
+	// 	return ret;
+
+	// fsleep(10000);
+
+	// ret = ltc4296_port_prebias(st, LTC_PORT2, LTC_CFG_APL_MODE);
+	// if (ret)
+	// 	return ret;
+
+	// ret = ltc4296_port_en(st, LTC_PORT2);
+	// if (ret)
+	// 	return ret;
+
+	// fsleep(10000);
+
+	// ret = ltc4296_port_prebias(st, LTC_PORT3, LTC_CFG_APL_MODE);
+	// if (ret)
+	// 	return ret;
+
+	// ret = ltc4296_port_en(st, LTC_PORT3);
+	// if (ret)
+	// 	return ret;
+
+	// fsleep(10000);
+
+	// ret = ltc4296_port_prebias(st, LTC_PORT4, LTC_CFG_APL_MODE);
+	// if (ret)
+	// 	return ret;
+
+	// ret = ltc4296_port_en(st, LTC_PORT4);
+	// if (ret)
+	// 	return ret;
 
 	return 0;
 }
@@ -1203,15 +1538,32 @@ static int ltc4296_probe(struct spi_device *spi)
 
 	st->spi = spi;
 
+	printk("Probing ltc4296\n");
+
+	st->sccpo = devm_gpiod_get(&st->spi->dev, "adi,sccpo",
+					  GPIOD_OUT_LOW);
+	if (IS_ERR(st->sccpo))
+		return PTR_ERR(st->sccpo);
+
+	st->sccpi = devm_gpiod_get(&st->spi->dev, "adi,sccpi",
+					  GPIOD_IN);
+	if (IS_ERR(st->sccpi))
+		return PTR_ERR(st->sccpi);
+
 	ret = ltc4296_init(st);
 	if (ret)
 		return ret;
 
-	hwmon_dev = devm_hwmon_device_register_with_info(&spi->dev, spi->dev.driver->name,
-							 st, &ltc4296_chip_info,
-							 NULL);
+	ret = sccp_reset_pulse(st);
+	printk("ltc4296 reset pulse return value: %d", ret);
 
-	return PTR_ERR_OR_ZERO(hwmon_dev);
+	return 0;
+
+	// hwmon_dev = devm_hwmon_device_register_with_info(&spi->dev, spi->dev.driver->name,
+	// 						 st, &ltc4296_chip_info,
+	// 						 NULL);
+
+	// return PTR_ERR_OR_ZERO(hwmon_dev);
 }
 
 static const struct spi_device_id ltc4296_id[] = {
