e8a5e20a4068ddfae328f982b42d4dc0063a89a8
Nikita Shubin <nikita.shubin@maquefel.me>
2024-09-09 11:10:58 +0300

    pwm: ep93xx: drop legacy pinctrl
    Drop legacy gpio request/free since we are using
    pinctrl for this now.
    
    Signed-off-by: Nikita Shubin <nikita.shubin@maquefel.me>
    Acked-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Acked-by: Thierry Reding <thierry.reding@gmail.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Origin: v6.12-rc1~34^2~15, commit:a48ac3dc569771c18fcafbc8351d820cc343c54a
    (cherry picked from commit 46afb62dc9ed2f8c5c09e567a12966b6bd42267e)

--- include/linux/soc/cirrus/ep93xx.h
+++ include/linux/soc/cirrus/ep93xx.h
@@ -11,8 +11,6 @@ struct platform_device;
 #define EP93XX_CHIP_REV_E2	7
 
 #ifdef CONFIG_ARCH_EP93XX
-int ep93xx_pwm_acquire_gpio(struct platform_device *pdev);
-void ep93xx_pwm_release_gpio(struct platform_device *pdev);
 int ep93xx_ide_acquire_gpio(struct platform_device *pdev);
 void ep93xx_ide_release_gpio(struct platform_device *pdev);
 int ep93xx_keypad_acquire_gpio(struct platform_device *pdev);
@@ -22,8 +20,6 @@ void ep93xx_i2s_release(void);
 unsigned int ep93xx_chip_revision(void);
 
 #else
-static inline int ep93xx_pwm_acquire_gpio(struct platform_device *pdev) { return 0; }
-static inline void ep93xx_pwm_release_gpio(struct platform_device *pdev) {}
 static inline int ep93xx_ide_acquire_gpio(struct platform_device *pdev) { return 0; }
 static inline void ep93xx_ide_release_gpio(struct platform_device *pdev) {}
 static inline int ep93xx_keypad_acquire_gpio(struct platform_device *pdev) { return 0; }
