66de4d70ab72aef7312323c9c1f575a1d1a60628
Vladimir Oltean <vladimir.oltean@nxp.com>
2024-10-15 17:33:28 +0300

    net: sdk_fman: make FrmReplicGroupTryLock() and FrmReplicGroupUnlock() static
    Avoid compiler warnings due to the fact that these are not used outside
    of fm_replic.c, yet are not static.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/net/ethernet/freescale/sdk_fman/Peripherals/FM/Pcd/fm_replic.c
+++ drivers/net/ethernet/freescale/sdk_fman/Peripherals/FM/Pcd/fm_replic.c
@@ -643,7 +643,7 @@ void  FrmReplicGroupUpdateOwner(t_Handle                   h_ReplicGroup,
     }
 }
 
-t_Error FrmReplicGroupTryLock(t_Handle h_ReplicGroup)
+static t_Error FrmReplicGroupTryLock(t_Handle h_ReplicGroup)
 {
     t_FmPcdFrmReplicGroup *p_ReplicGroup = (t_FmPcdFrmReplicGroup *)h_ReplicGroup;
 
@@ -655,7 +655,7 @@ t_Error FrmReplicGroupTryLock(t_Handle h_ReplicGroup)
     return ERROR_CODE(E_BUSY);
 }
 
-void FrmReplicGroupUnlock(t_Handle h_ReplicGroup)
+static void FrmReplicGroupUnlock(t_Handle h_ReplicGroup)
 {
     t_FmPcdFrmReplicGroup *p_ReplicGroup = (t_FmPcdFrmReplicGroup *)h_ReplicGroup;
 
