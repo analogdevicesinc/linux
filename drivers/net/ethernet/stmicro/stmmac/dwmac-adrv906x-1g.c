ae12e5f411d6871b88a7d3b7fbbb5792f03fa291
Daniel Mateu <daniel.mateu@analog.com>
2024-10-25 08:40:04 -0400

    net: adrv906x-1g: Add RMII support
    Define pinctrl for RMII
    Fix MAC RX clock source selection
    
    Signed-off-by: Daniel Mateu <daniel.mateu@analog.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-adrv906x-1g.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-adrv906x-1g.c
@@ -14,7 +14,14 @@
 #include "stmmac_platform.h"
 
 #define EMAC_1G_CG_ENABLE                       BIT(0)
-#define EMAC_1G_YODA_MASK                       GENMASK(19, 3)
+#define EMAC_1G_OSC_CLK_DIV_MASK                GENMASK(19, 13)
+#define EMAC_1G_CLK_DIV_MASK                    GENMASK(12, 6)
+#define EMAC_1G_PHY_INTF_SEL_I_MASK             GENMASK(5, 3)
+#define EMAC_1G_OSC_CLK_DIV_OFF                 13
+#define EMAC_1G_CLK_DIV_OFF                     6
+#define EMAC_1G_PHY_INTF_SEL_I_OFF              3
+#define EMAC_1G_PHY_INTF_SEL_I_RMII             4
+#define EMAC_1G_PHY_INTF_SEL_I_RGMII            1
 
 #define ETH1G_DEVCLK_MASK                       GENMASK(13, 6)
 #define ETH1G_DEVCLK_DIV_FUND                   BIT(6)
@@ -24,112 +31,159 @@
 #define ETH1G_DEVCLK_DIV_RB                     BIT(11)
 #define ETH1G_DEVCLK_BUFFER_ENABLE              BIT(12)
 #define ETH1G_DEVCLK_BUFFER_TERM_ENABLE         BIT(13)
-#define ETH1G_DEVCLK_DEFAULT_VAL                (ETH1G_DEVCLK_DIV_FUND |         \
-						 ETH1G_DEVCLK_DIV_KILLCLK |      \
-						 ETH1G_DEVCLK_DIV_MCS_RESET |    \
-						 ETH1G_DEVCLK_DIV_RATIO |        \
-						 ETH1G_DEVCLK_DIV_RB |           \
+#define ETH1G_DEVCLK_DEFAULT_VAL                (ETH1G_DEVCLK_DIV_FUND |       \
+						 ETH1G_DEVCLK_DIV_KILLCLK |    \
+						 ETH1G_DEVCLK_DIV_MCS_RESET |  \
+						 ETH1G_DEVCLK_DIV_RATIO |      \
+						 ETH1G_DEVCLK_DIV_RB |         \
 						 ETH1G_DEVCLK_BUFFER_ENABLE)
 
 #define ETH1G_REFCLK_MASK                       BIT(17)
 #define ETH1G_REFCLK_REFPATH_PD                 0 /* BIT(17) */
 #define ETH1G_REFCLK_DEFAULT_VAL                ETH1G_REFCLK_REFPATH_PD
 
-#define BASE_CLK_SPEED_50MHZ                    50
-#define BASE_CLK_SPEED_125MHZ                   125
-#define BASE_CLK_SPEED_250MHZ                   250
+#define HZ_TO_MHZ(freq)                         (freq * 1000 * 1000)
+#define CLK_2_5MHZ                              HZ_TO_MHZ(2.5)
+#define CLK_25MHZ                               HZ_TO_MHZ(25)
+#define CLK_50MHZ                               HZ_TO_MHZ(50)
+#define CLK_125MHZ                              HZ_TO_MHZ(125)
 
 struct adrv906x_priv_data {
 	struct stmmac_priv *stm_priv;
-	unsigned int base_clk_speed;
+	uint32_t base_clk_speed;
 	void __iomem *clk_div_base;
+	phy_interface_t phy_interface;
 };
 
 static char *macaddr;
 module_param(macaddr, charp, 0644);
 MODULE_PARM_DESC(macaddr, "set dev mac addresse via kernel module parameter");
 
-static void adrv906x_dwmac_mac_speed(void *priv, unsigned int speed)
+static int adrv906x_dwmac_set_clk_dividers(void *priv, unsigned int speed, bool force_reconfig)
 {
-	struct adrv906x_priv_data *sam_priv = (struct adrv906x_priv_data *)priv;
+	struct adrv906x_priv_data *adrv_priv = (struct adrv906x_priv_data *)priv;
+	ulong rate;
 	u32 reg;
+	uint32_t osc_div;
+	uint32_t rmii_div;
 
-	// Disable clock
-	reg = ioread32(sam_priv->clk_div_base);
-	reg |= EMAC_1G_CG_ENABLE;
-	iowrite32(reg, sam_priv->clk_div_base);
-
-	// Set PHY iface (RGMII | RMII) and clock divider
-	switch (sam_priv->stm_priv->plat->phy_interface) {
-	case PHY_INTERFACE_MODE_RMII:
-		if (sam_priv->base_clk_speed == BASE_CLK_SPEED_125MHZ) {
-			dev_err(sam_priv->stm_priv->device,
-				"phy mode RMII - invalid clock speed");
-			return;
-		}
-
-		reg &= ~EMAC_1G_YODA_MASK;
-		reg |= 4 << 3;
-		if (speed == SPEED_10) {
-			if (sam_priv->base_clk_speed == BASE_CLK_SPEED_50MHZ)
-				reg |= (19 << 6);
-			if (sam_priv->base_clk_speed == BASE_CLK_SPEED_250MHZ)
-				reg |= (99 << 6) + (4 << 13);
-		}
-		if (speed == SPEED_100) {
-			if (sam_priv->base_clk_speed == BASE_CLK_SPEED_50MHZ)
-				reg |= (1 << 6);
-			if (sam_priv->base_clk_speed == BASE_CLK_SPEED_250MHZ)
-				reg |= (9 << 6) + (4 << 13);
-		}
-		if (speed == SPEED_1000) {
-			dev_err(sam_priv->stm_priv->device,
-				"phy mode RMII - 1G not supported");
-			return;
-		}
-		break;
-	case PHY_INTERFACE_MODE_RGMII:
-		reg &= ~EMAC_1G_YODA_MASK;
-		reg |= 1 << 3;
-		if (speed == SPEED_10) {
-			if (sam_priv->base_clk_speed == BASE_CLK_SPEED_50MHZ)
-				reg |= (19 << 13);
-			if (sam_priv->base_clk_speed == BASE_CLK_SPEED_125MHZ)
-				reg |= (49 << 13);
-			if (sam_priv->base_clk_speed == BASE_CLK_SPEED_250MHZ)
-				reg |= (99 << 13);
-		}
-		if (speed == SPEED_100) {
-			if (sam_priv->base_clk_speed == BASE_CLK_SPEED_50MHZ)
-				reg |= (1 << 13);
-			if (sam_priv->base_clk_speed == BASE_CLK_SPEED_125MHZ)
-				reg |= (4 << 13);
-			if (sam_priv->base_clk_speed == BASE_CLK_SPEED_250MHZ)
-				reg |= (9 << 13);
-		}
-		if (speed == SPEED_1000) {
-			if (sam_priv->base_clk_speed == BASE_CLK_SPEED_50MHZ) {
-				dev_err(sam_priv->stm_priv->device,
-					"phy mode RGMII - invalid clock speed");
-				return;
-			}
-
-			if (sam_priv->base_clk_speed == BASE_CLK_SPEED_125MHZ)
-				reg |= 0;
-			if (sam_priv->base_clk_speed == BASE_CLK_SPEED_250MHZ)
-				reg |= (1 << 13);
-		}
-		break;
-	default:
-		dev_err(sam_priv->stm_priv->device,
-			"phy mode not supported");
-		return;
+	/* Required clock freq depends on the link speed */
+	switch (speed) {
+	case SPEED_10:   rate = CLK_2_5MHZ; break;
+	case SPEED_100:  rate = CLK_25MHZ; break;
+	case SPEED_1000: rate = CLK_125MHZ; break;
+	default: pr_err("Invalid link speed"); return -1;
 	}
-	iowrite32(reg, sam_priv->clk_div_base);
 
-	// Re-enable clock
+	/* Sanity checks */
+	if (((adrv_priv->base_clk_speed) % rate) != 0) {
+		pr_err("Unable to get MAC clock");
+		return -1;
+	}
+
+	if ((adrv_priv->phy_interface == PHY_INTERFACE_MODE_RMII) &&
+	    ((adrv_priv->base_clk_speed % CLK_50MHZ) != 0)) {
+		pr_err("Unable to get RMII PHY clock (50 MHz)");
+		return -1;
+	}
+
+	if ((adrv_priv->phy_interface == PHY_INTERFACE_MODE_RMII) &&
+	    (speed == SPEED_1000)) {
+		pr_err("RMII does not support 1000 Mbs");
+		return -1;
+	}
+
+	if ((adrv_priv->phy_interface != PHY_INTERFACE_MODE_RMII) &&
+	    (adrv_priv->phy_interface != PHY_INTERFACE_MODE_RGMII)) {
+		pr_err("MAC-PHY Interface (%d) not supported", adrv_priv->phy_interface);
+		return -1;
+	}
+
+	/* Compute clock dividers */
+	if (adrv_priv->phy_interface == PHY_INTERFACE_MODE_RMII) {
+		/* input_clk   _|-> OSC_CLK_DIV (50 MHz) ------> PHY core clock (REF_CLK)
+		 * (ie 250MHz)  |-> RMII_CLK_DIV (2.5|25 MHz) -> Tx_clk and Rx_clk to GMAC
+		 *                  (based on PHY link 10|100)
+		 */
+		osc_div = (adrv_priv->base_clk_speed / CLK_50MHZ) - 1;
+		rmii_div = ((adrv_priv->base_clk_speed) / (rate)) - 1;
+	} else if (adrv_priv->phy_interface == PHY_INTERFACE_MODE_RGMII) {
+		/*
+		 * input_clk     -> OSC_CLK_DIV (2.5|25|125 MHz) -> Tx_clk to PHY and Tx_clk to GMAC
+		 * (ie 250MHz)      (based on PHY link 10|100|1000)
+		 *
+		 * Note: Rx_clk to GMAC IP is provided by the external PHY
+		 * Note: RMII_CLK_DIV does not apply
+		 * Note: PHY core clock is provided by a crystal circuitry
+		 */
+		osc_div = ((adrv_priv->base_clk_speed) / rate) - 1;
+		rmii_div = 0;
+	}
+
+	/* Do not reconfigure clock dividers in RMII if they have not changed
+	 *
+	 * Workaround: Adrv906x clocks divider block require to stop all the
+	 * clocks before updating their value (even though only one of them
+	 * needs to be updated). For RMII, one of these clocks is the PHY core
+	 * clock (REF_CLK). Linux general driver flow does not expect that PHY
+	 * core clock is interrupted at any time. This leads to a wrong behaviour
+	 * (link up and down all the time). Flow:
+	 * - MAC detects link is up (reading PHY register)
+	 * - MAC reconfigure clocks (according to Link Speed)
+	 * - Stopping the clock in the previous step results in a link down
+	 *   indication from the PHY chip (and a new autonegotiation process)
+	 * - MAC sets the link down again
+	 * - A bit later, autoneg was complete and link is up again and the
+	 *   story repeats once and again.
+	 *
+	 * The fix is not to reconfigure clocks if their value remains the same.
+	 * This prevents the endless loop in the second iteration.
+	 *
+	 * Note: PHY core clock in RGMII is not generated by Adrv906x, so this
+	 *       issue does not apply.
+	 * Note: During initalization, reconfiguration is forced. This enables
+	 *       the 50 MHz PHY core clock (RMII case)
+	 */
+	if (!force_reconfig && adrv_priv->phy_interface == PHY_INTERFACE_MODE_RMII) {
+		uint32_t curr_osc_div;
+		uint32_t curr_rmii_div;
+
+		reg = ioread32(adrv_priv->clk_div_base);
+		curr_osc_div = (reg & EMAC_1G_OSC_CLK_DIV_MASK) >> EMAC_1G_OSC_CLK_DIV_OFF;
+		curr_rmii_div = (reg & EMAC_1G_CLK_DIV_MASK) >> EMAC_1G_CLK_DIV_OFF;
+
+		if ((osc_div == curr_osc_div) && (rmii_div == curr_rmii_div))
+			return 0;
+	}
+
+	/* Disable clock */
+	reg = ioread32(adrv_priv->clk_div_base);
+	reg |= EMAC_1G_CG_ENABLE;
+	iowrite32(reg, adrv_priv->clk_div_base);
+
+	/* Set clock divider */
+	if (adrv_priv->phy_interface == PHY_INTERFACE_MODE_RMII) {
+		reg &= ~EMAC_1G_PHY_INTF_SEL_I_MASK;
+		reg |= EMAC_1G_PHY_INTF_SEL_I_RMII << EMAC_1G_PHY_INTF_SEL_I_OFF;
+	} else if (adrv_priv->phy_interface == PHY_INTERFACE_MODE_RGMII) {
+		reg &= ~EMAC_1G_PHY_INTF_SEL_I_MASK;
+		reg |= EMAC_1G_PHY_INTF_SEL_I_RGMII << EMAC_1G_PHY_INTF_SEL_I_OFF;
+	}
+	reg &= ~(EMAC_1G_OSC_CLK_DIV_MASK | EMAC_1G_CLK_DIV_MASK);
+	reg |= (osc_div << EMAC_1G_OSC_CLK_DIV_OFF) |
+	       (rmii_div << EMAC_1G_CLK_DIV_OFF);
+	iowrite32(reg, adrv_priv->clk_div_base);
+
+	/* Re-enable clock */
 	reg &= ~EMAC_1G_CG_ENABLE;
-	iowrite32(reg, sam_priv->clk_div_base);
+	iowrite32(reg, adrv_priv->clk_div_base);
+
+	return 0;
+}
+
+static void adrv906x_dwmac_fix_mac_speed(void *priv, unsigned int speed)
+{
+	adrv906x_dwmac_set_clk_dividers(priv, speed, false);
 }
 
 void adrv906x_dwmac_link_update_info(struct net_device *ndev)
@@ -169,7 +223,7 @@ static int dwmac_adrv906x_probe(struct platform_device *pdev)
 {
 	struct plat_stmmacenet_data *plat_dat;
 	struct stmmac_resources stmmac_res;
-	struct adrv906x_priv_data *sam_priv;
+	struct adrv906x_priv_data *adrv_priv;
 	struct device *dev = &pdev->dev;
 	struct device_node *clk_div_np;
 	struct net_device *ndev;
@@ -179,8 +233,8 @@ static int dwmac_adrv906x_probe(struct platform_device *pdev)
 	bool term_en;
 	int ret;
 
-	sam_priv = devm_kzalloc(dev, sizeof(*sam_priv), GFP_KERNEL);
-	if (!sam_priv)
+	adrv_priv = devm_kzalloc(dev, sizeof(*adrv_priv), GFP_KERNEL);
+	if (!adrv_priv)
 		return -ENOMEM;
 
 	ret = stmmac_get_platform_resources(pdev, &stmmac_res);
@@ -219,12 +273,12 @@ static int dwmac_adrv906x_probe(struct platform_device *pdev)
 		return -EINVAL;
 	}
 
-	sam_priv->base_clk_speed = clk_get_rate(plat_dat->stmmac_clk) / 1000000;
-	dev_info(&pdev->dev, "base clock speed %d MHz", sam_priv->base_clk_speed);
+	adrv_priv->base_clk_speed = clk_get_rate(plat_dat->stmmac_clk);
+	dev_info(&pdev->dev, "base clock speed %d MHz", adrv_priv->base_clk_speed / (HZ_TO_MHZ(1)));
 
 	of_property_read_u32_index(clk_div_np, "reg", 0, &addr);
 	of_property_read_u32_index(clk_div_np, "reg", 1, &len);
-	sam_priv->clk_div_base = devm_ioremap(&pdev->dev, addr, len);
+	adrv_priv->clk_div_base = devm_ioremap(&pdev->dev, addr, len);
 
 	of_property_read_u32_index(clk_div_np, "ctrl_reg", 0, &addr);
 	of_property_read_u32_index(clk_div_np, "ctrl_reg", 1, &len);
@@ -234,8 +288,8 @@ static int dwmac_adrv906x_probe(struct platform_device *pdev)
 
 	adrv906x_clk_buffer_enable(clk_ctrl_base, term_en);
 
-	plat_dat->bsp_priv = sam_priv;
-	plat_dat->fix_mac_speed = adrv906x_dwmac_mac_speed;
+	plat_dat->bsp_priv = adrv_priv;
+	plat_dat->fix_mac_speed = adrv906x_dwmac_fix_mac_speed;
 
 	/* Custom initialisation (if needed) */
 	if (plat_dat->init) {
@@ -244,12 +298,22 @@ static int dwmac_adrv906x_probe(struct platform_device *pdev)
 			goto err_remove_config_dt;
 	}
 
+
+	adrv_priv->phy_interface = plat_dat->phy_interface;
+	/* Configure clock distribution (depends on the phy interface type).
+	 * Enable Link-speed-related clocks to arbitrary value
+	 * Enable PHY core clock to 50 MHz (only for RMII)
+	 */
+	ret = adrv906x_dwmac_set_clk_dividers(adrv_priv, SPEED_100, true);
+	if (ret)
+		goto err_remove_config_dt;
+
 	ret = stmmac_dvr_probe(&pdev->dev, plat_dat, &stmmac_res);
 	if (ret)
 		goto err_exit;
 
 	ndev = platform_get_drvdata(pdev);
-	sam_priv->stm_priv = netdev_priv(ndev);
+	adrv_priv->stm_priv = netdev_priv(ndev);
 
 	return 0;
 
@@ -260,7 +324,7 @@ err_remove_config_dt:
 	if (pdev->dev.of_node)
 		stmmac_remove_config_dt(pdev, plat_dat);
 
-	devm_kfree(dev, sam_priv);
+	devm_kfree(dev, adrv_priv);
 	return ret;
 }
 
