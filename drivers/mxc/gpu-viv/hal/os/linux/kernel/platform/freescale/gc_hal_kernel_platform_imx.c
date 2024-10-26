29d4f7e40d2079ed3a4b49e0c47988c13df6c863
jiajia Qian <jiajia.qian@nxp.com>
2024-10-26 16:01:27 +0800

    MGS-7909 [#imx-3183] 0102-CL990561-KERNEL-SPACE-22q2-IMX-3270-Update-platform-
    Update
     platform related code to support 8mq multi power domains. [Ying.Zhang]
    
    Signed-off-by: IPD_SCM <IPD_SCM@verisilicon.com>
    
    Signed-off-by: jiajia Qian <jiajia.qian@nxp.com>

--- drivers/mxc/gpu-viv/hal/os/linux/kernel/platform/freescale/gc_hal_kernel_platform_imx.c
+++ drivers/mxc/gpu-viv/hal/os/linux/kernel/platform/freescale/gc_hal_kernel_platform_imx.c
@@ -1091,6 +1091,10 @@ static inline int get_power_imx8_subsystem(struct device *pdev)
     if (core_node)
         of_node_put(core_node);
 
+#ifdef CONFIG_PM
+    pm_runtime_enable(pdev);
+#endif
+
     return 0;
 }
 
@@ -1342,10 +1346,10 @@ free_priv(void)
 }
 
 static int set_clock(int gpu, int enable);
-static int set_power(int gpu, int enable);
+static int set_power(struct device *pdev, int gpu, int enable);
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 0)
-static void imx6sx_optimize_qosc_for_GPU(void)
+static void imx6sx_optimize_qosc_for_GPU(struct device *pdev)
 {
     struct device_node *np;
     void __iomem *src_base;
@@ -1357,7 +1361,7 @@ static void imx6sx_optimize_qosc_for_GPU(void)
     src_base = of_iomap(np, 0);
     WARN_ON(!src_base);
 
-    set_power(gcvCORE_MAJOR, 1);
+    set_power(pdev, gcvCORE_MAJOR, 1);
     set_clock(gcvCORE_MAJOR, 1);
 
     writel_relaxed(0, src_base); /* Disable clkgate & soft_rst */
@@ -1367,7 +1371,7 @@ static void imx6sx_optimize_qosc_for_GPU(void)
     writel_relaxed(0x0f000822, src_base+0x1400+0xe0); /* Set Read QoS 8 for gpu */
 
     set_clock(gcvCORE_MAJOR, 0);
-    set_power(gcvCORE_MAJOR, 0);
+    set_power(pdev, gcvCORE_MAJOR, 0);
 
     return;
 }
@@ -1536,7 +1540,7 @@ static inline int get_power(struct device *pdev)
 #endif
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 0)
-    imx6sx_optimize_qosc_for_GPU();
+    imx6sx_optimize_qosc_for_GPU(pdev);
 #endif
 
     return 0;
@@ -1575,7 +1579,7 @@ static inline int get_power_ls(struct device *pdev)
     return 0;
 }
 
-static inline void put_power(void)
+static inline void put_power(struct device *pdev)
 {
     int core = 0;
     struct gpu_clk *imx_clk = NULL;
@@ -1614,6 +1618,11 @@ static inline void put_power(void)
         }
 #endif
     }
+
+#if defined(IMX_GPU_SUBSYSTEM) && defined(CONFIG_PM)
+    if (pdev->of_node && use_imx_gpu_subsystem)
+        pm_runtime_disable(pdev);
+#endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 5, 0)
     if (priv->gpu_regulator) {
@@ -1655,7 +1664,7 @@ static inline void put_power_ls(void)
 #endif
 }
 
-static inline int set_power(int gpu, int enable)
+static inline int set_power(struct device *pdev, int gpu, int enable)
 {
 #ifdef CONFIG_PM
     struct imx_priv* priv = &imxPriv;
@@ -1682,6 +1691,10 @@ static inline int set_power(int gpu, int enable)
 
 #ifdef CONFIG_PM
         pm_runtime_get_sync(priv->pmdev[gpu]);
+#ifdef IMX_GPU_SUBSYSTEM
+        if (pdev->of_node && use_imx_gpu_subsystem)
+            pm_runtime_get_sync(pdev);
+#endif
         if(priv->pm_qos_core == gpu) {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 7, 0)
             cpu_latency_qos_add_request(&priv->pm_qos, 0);
@@ -1754,6 +1767,10 @@ static inline int set_power(int gpu, int enable)
             clk_unprepare(clk_ahb);
 #ifdef CONFIG_PM
         pm_runtime_put_sync(priv->pmdev[gpu]);
+#ifdef IMX_GPU_SUBSYSTEM
+        if (pdev->of_node && use_imx_gpu_subsystem)
+            pm_runtime_put_sync(pdev);
+#endif
         if (priv->pm_qos_core == gpu) {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 7, 0)
             cpu_latency_qos_remove_request(&priv->pm_qos);
@@ -1928,13 +1945,6 @@ _GetPower(
 {
     int ret;
 
-#if defined(IMX_GPU_SUBSYSTEM) && defined(CONFIG_PM)
-    if (Platform->device->dev.of_node && use_imx_gpu_subsystem) {
-        pm_runtime_enable(&Platform->device->dev);
-        pm_runtime_get_sync(&Platform->device->dev);
-    }
-#endif
-
     if (is_layerscape)
         ret = get_power_ls(&Platform->device->dev);
     else
@@ -1954,14 +1964,7 @@ _PutPower(
     if (is_layerscape)
         put_power_ls();
     else
-        put_power();
-
-#if defined(IMX_GPU_SUBSYSTEM) && defined(CONFIG_PM)
-    if (Platform->device->dev.of_node && use_imx_gpu_subsystem) {
-        pm_runtime_put_sync(&Platform->device->dev);
-        pm_runtime_disable(&Platform->device->dev);
-    }
-#endif
+        put_power(&Platform->device->dev);
 
     return gcvSTATUS_OK;
 }
@@ -1975,8 +1978,8 @@ _SetPower(
     gctBOOL Enable
     )
 {
-    return set_power((int)GPU, Enable) ? gcvSTATUS_GENERIC_IO
-                : gcvSTATUS_OK;
+    return set_power(&Platform->device->dev, (int)GPU, Enable) ?
+           gcvSTATUS_GENERIC_IO : gcvSTATUS_OK;
 }
 
 static gceSTATUS
