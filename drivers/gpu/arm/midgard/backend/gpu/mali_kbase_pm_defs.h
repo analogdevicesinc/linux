b5eefa1bbb4aad5a3a40b602a34a3d0c7e0fb65c
Chingling Wang <chingling.wang@nxp.com>
2024-10-28 14:03:01 -0700

    MGS-7869 gpu: fix gpu utilization overflow
    u32 type will make busy_time and idle_time overflow
    which resulting crazy output in kernle debugfs
    dvfs_utilization output data
    
    Signed-off-by: Chingling Wang <chingling.wang@nxp.com>
    Reviewed-by: Prabhu Sundararaj <prabhu.sundararaj@nxp.com>
    Reviewed-by: Jiyu Yang <jiyu.yang@nxp.com>

--- drivers/gpu/arm/midgard/backend/gpu/mali_kbase_pm_defs.h
+++ drivers/gpu/arm/midgard/backend/gpu/mali_kbase_pm_defs.h
@@ -171,10 +171,10 @@ enum kbase_pm_runtime_suspend_abort_reason {
  *           with 2 (2x256ns).
  */
 struct kbasep_pm_metrics {
-	u32 time_busy;
-	u32 time_idle;
+	u64 time_busy;
+	u64 time_idle;
 #if MALI_USE_CSF
-	u32 time_in_protm;
+	u64 time_in_protm;
 #else
 	u32 busy_cl[2];
 	u32 busy_gl;
--- drivers/gpu/arm/midgard/mali_kbase_dvfs_debugfs.c
+++ drivers/gpu/arm/midgard/mali_kbase_dvfs_debugfs.c
@@ -39,10 +39,11 @@ static int kbasep_dvfs_utilization_debugfs_show(struct seq_file *file, void *dat
 
 	CSTD_UNUSED(data);
 #if MALI_USE_CSF
-	seq_printf(file, "busy_time: %u idle_time: %u protm_time: %u\n",
+	seq_printf(file, "busy_time: %llu idle_time: %llu protm_time: %llu\n",
 		   kbdev->pm.backend.metrics.values.time_busy,
 		   kbdev->pm.backend.metrics.values.time_idle,
 		   kbdev->pm.backend.metrics.values.time_in_protm);
+
 #else
 	seq_printf(file, "busy_time: %u idle_time: %u\n",
 		   kbdev->pm.backend.metrics.values.time_busy,
