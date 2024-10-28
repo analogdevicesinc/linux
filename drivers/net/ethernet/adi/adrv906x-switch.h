e3306cc46b472923dc4a1c36b61f0f9946aaabba
sheng wang <sheng.wang@analog.com>
2024-10-28 18:11:34 -0400

    TPGSWE-15013: Add EAPOL and ESMC trapping to port 2

--- drivers/net/ethernet/adi/adrv906x-switch.h
+++ drivers/net/ethernet/adi/adrv906x-switch.h
@@ -14,6 +14,7 @@
 
 #define SWITCH_MAX_PORT_NUM                             3
 #define SWITCH_MAX_PCP_PLANE_NUM                        4
+#define SWITCH_CPU_PORT                                 2
 
 #define SECONDS_TO_WAIT                                 2
 #define ADRV906X_NET_DEV_WAIT                           100     /* msecs */
@@ -25,8 +26,8 @@
 #define SWITCH_PORT_PCP_REGEN                           0x0010
 #define SWITCH_PORT_TRAP_PTP                            0x0084
 #define SWITCH_PORT_PCP2IPV                             0x008c
-#define SWITCH_PTP_ENABLE_BIT                           24
-#define SWITCH_PTP_DSTPORT_START_BIT                    16
+#define SWITCH_TRAP_ENABLE                              BIT(24)
+#define SWITCH_TRAP_DSTPORT_CPU                         BIT(16 + SWITCH_CPU_PORT)
 #define SWITCH_DSA_TX_ENABLE_BIT                        17
 #define SWITCH_DSA_RX_ENABLE_BIT                        16
 #define SWITCH_MAC_LEARN_EN                             2
@@ -39,6 +40,11 @@
 #define   SWITCH_MAS_OP_CTRL_FLUSH_MAC_TABLE            BIT(2)
 #define SWITCH_MAS_PORT_MASK1                           0x0004
 #define SWITCH_MAS_PORT_MASK2                           0x0008
+#define SWITCH_MAS_FDB_MAC_INSERT_1                     0x000c
+#define SWITCH_MAS_FDB_MAC_INSERT_2                     0x0010
+#define   SWITCH_INSERT_VALID                           BIT(0)
+#define   SWITCH_INSERT_STATIC                          BIT(1)
+#define   SWITCH_INSERT_OVERWRITE                       BIT(3)
 #define SWITCH_MAS_VLAN_ID                              0x0014
 #define SWITCH_MAS_SOFT_RESET                           0x0020
 #define   ALL_EX_MAE                                    BIT(0)
@@ -80,7 +86,8 @@ struct adrv906x_eth_switch {
 	struct switch_isr_args isr_post_args;
 };
 
-int adrv906x_switch_register_irqs(struct adrv906x_eth_switch *es, struct device_node *eth_switch_np);
+int adrv906x_switch_register_irqs(struct adrv906x_eth_switch *es,
+				  struct device_node *eth_switch_np);
 int adrv906x_switch_register_attr(struct adrv906x_eth_switch *es);
 void adrv906x_switch_unregister_attr(struct adrv906x_eth_switch *es);
 int adrv906x_switch_probe(struct adrv906x_eth_switch *es, struct platform_device *pdev,
