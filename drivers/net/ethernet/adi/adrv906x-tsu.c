3babea2431d8402d31788c5a0a1a0955d4510be0
Jie Zhang <jie.zhang@analog.com>
2024-10-30 13:02:31 -0400

    net: adrv906x-net: Calculate TSU static PHY delays
    This implements calculation of TSU static PHY delays for TX and RX.
    
    Signed-off-by: Jie Zhang <jie.zhang@analog.com>

--- drivers/net/ethernet/adi/adrv906x-tsu.c
+++ drivers/net/ethernet/adi/adrv906x-tsu.c
@@ -6,15 +6,82 @@
 #include <linux/kernel.h>
 #include <linux/of_address.h>
 #include <linux/platform_device.h>
-#include <linux/phy.h>
+#include <linux/clk.h>
 #include <linux/io.h>
 #include "adrv906x-tsu.h"
-#include "adrv906x-net.h"
 
-void adrv906x_tsu_set_phy_delay(void __iomem *base, u32 phy_delay_tx, u32 phy_delay_rx)
+void adrv906x_tsu_calculate_phy_delay(struct adrv906x_tsu *tsu, int speed,
+				      bool rs_fec_enabled, u32 bit_slip,
+				      u32 buf_delay_tx, u32 buf_delay_rx)
 {
-	iowrite32(phy_delay_tx, base + ADRV906X_TSU_STATIC_PHY_DELAY_TX);
-	iowrite32(phy_delay_rx, base + ADRV906X_TSU_STATIC_PHY_DELAY_RX);
+	u32 rx_pcs, rx_pcs_pipeline, rx_pcs_decode, rx_pcs_gearbox, rx_rs_fec;
+	u32 tx_pcs, tx_pcs_pipeline, tx_pcs_decode, tx_pcs_gearbox, tx_rs_fec;
+	u32 t_div66, t_div64;
+	u32 tod_cdc_delay;
+
+	t_div66 = (speed == SPEED_25000) ? T_DIV66_25G : T_DIV66_10G;
+	t_div64 = (speed == SPEED_25000) ? T_DIV64_25G : T_DIV64_10G;
+
+	/* ToD_cdc_delay = 1.5 * 1 / hsdig_clk + (cdc_delay + 3.0) * T_div66 */
+
+	tod_cdc_delay = (1000000000ULL << 16) / tsu->hsdig_clk_rate;
+	tod_cdc_delay = tod_cdc_delay * 3 / 2;
+	tod_cdc_delay += (adrv906x_tod_cfg_cdc_delay + 3) * t_div66;
+
+	/* Calculate Rx_static_phy_delay */
+
+	/* Rx_pcs = Rx_pcs_pipeline + Rx_pcs_decode + Rx_pcs_gearbox + Rx_rs_fec
+	 *
+	 * Rx_pcs_pipeline = 2 * T_div66 + 2 * T_div64
+	 * Rx_pcs_decode   = 6 * T_div66 - bit_slip / 66 * T_div66
+	 * Rx_pcs_gearbox  = floor(fine_buf_receive_delay) * T_div66
+	 * if RS-FEC is used
+	 *   Rx_rs_fec       = 234 * T_div66
+	 */
+
+	rx_pcs_pipeline = 2 * t_div66 + 2 * t_div64;
+	rx_pcs_decode = 6 * t_div66 - bit_slip * t_div66 / 66;
+	rx_pcs_gearbox = buf_delay_rx * t_div66;
+	rx_rs_fec = rs_fec_enabled ? 234 * t_div66 : 0;
+
+	rx_pcs = rx_pcs_pipeline + rx_pcs_decode + rx_pcs_gearbox + rx_rs_fec;
+
+	/* Rx_static_phy_delay = Rx_pcs - ToD_cdc_delay - PCB_delay */
+
+	if (rx_pcs > tod_cdc_delay + tsu->pcb_delay_rx)
+		tsu->phy_delay_rx = rx_pcs - tod_cdc_delay - tsu->pcb_delay_rx;
+	else
+		tsu->phy_delay_rx = 0;
+
+	/* Calculate Tx_static_phy_delay */
+
+	/* Tx_pcs = Tx_pcs_pipeline + Tx_pcs_decode + Tx_pcs_gearbox + Tx_rs_fec
+	 *
+	 * Tx_pcs_pipeline = 2 * T_div66 + T_div64
+	 * Tx_pcs_decode   = 4 * T_div66
+	 * Tx_pcs_gearbox  = floor(fine_buf_transmit_delay) * T_div66
+	 * if RS-FEC is used
+	 *   Tx_rs_fec       = 17 * T_div66
+	 */
+
+	tx_pcs_pipeline = 2 * t_div66 + t_div64;
+	tx_pcs_decode = 4 * t_div66;
+	tx_pcs_gearbox = buf_delay_tx * t_div66;
+	tx_rs_fec = rs_fec_enabled ? 17 * t_div66 : 0;
+
+	tx_pcs = tx_pcs_pipeline + tx_pcs_decode + tx_pcs_gearbox + tx_rs_fec;
+
+	/* Tx_static_phy_delay = Tx_pcs + ToD_cdc_delay + PCB_delay */
+
+	tsu->phy_delay_tx = tx_pcs + tod_cdc_delay + tsu->pcb_delay_tx;
+}
+
+void adrv906x_tsu_set_phy_delay(struct adrv906x_tsu *tsu)
+{
+	void __iomem *base = tsu->base;
+
+	iowrite32(tsu->phy_delay_tx, base + ADRV906X_TSU_STATIC_PHY_DELAY_TX);
+	iowrite32(tsu->phy_delay_rx, base + ADRV906X_TSU_STATIC_PHY_DELAY_RX);
 }
 
 void adrv906x_tsu_set_ptp_timestamping_mode(void __iomem *base)
@@ -30,13 +97,13 @@ void adrv906x_tsu_set_ptp_timestamping_mode(void __iomem *base)
 	iowrite32(val, base + ADRV906X_TSU_TIMESTAMPING_MODE);
 }
 
-void adrv906x_tsu_set_speed(struct adrv906x_tsu *tsu, struct phy_device *phydev)
+void adrv906x_tsu_set_speed(struct adrv906x_tsu *tsu, int speed)
 {
 	u32 mode, val;
 	void __iomem *base;
 
 	base = tsu->base;
-	mode = (phydev->speed == SPEED_25000) ? ADRV906X_CORE_SPEED_25G : ADRV906X_CORE_SPEED_10G;
+	mode = (speed == SPEED_25000) ? ADRV906X_CORE_SPEED_25G : ADRV906X_CORE_SPEED_10G;
 
 	val = ioread32(base + ADRV906X_TSU_TIMESTAMPING_MODE);
 	val &= ~ADRV906X_CORE_SPEED;
@@ -45,20 +112,20 @@ void adrv906x_tsu_set_speed(struct adrv906x_tsu *tsu, struct phy_device *phydev)
 	iowrite32(val, base + ADRV906X_TSU_TIMESTAMPING_MODE);
 }
 
-int adrv906x_tsu_setup(struct platform_device *pdev, struct adrv906x_tsu *tsu, struct device_node *eth_port_np)
+int adrv906x_tsu_setup(struct platform_device *pdev, struct adrv906x_tsu *tsu,
+		       struct device_node *eth_port_np)
 {
 	struct device *dev = &pdev->dev;
+	struct clk *hsdig_clk;
 	u32 val, frac_val, reg, len;
-	int ret, port_id;
+	int ret;
 
-	ret = of_property_read_u32(eth_port_np, "id", &port_id);
-	if (ret < 0) {
-		dev_err(dev, "dt: id missing");
-		return -ENODEV;
-	} else if (port_id >= MAX_NETDEV_NUM) {
-		dev_err(dev, "dt: id %d  is out of range", port_id);
-		return -ENODEV;
+	hsdig_clk = devm_get_clk_from_child(dev, eth_port_np, "hsdig_clk");
+	if (IS_ERR(hsdig_clk)) {
+		dev_err(dev, "cannot get 'hsdig_clk'");
+		return PTR_ERR(hsdig_clk);
 	}
+	tsu->hsdig_clk_rate = clk_get_rate(hsdig_clk);
 
 	of_property_read_u32_index(eth_port_np, "reg", 6, &reg);
 	of_property_read_u32_index(eth_port_np, "reg", 7, &len);
@@ -67,35 +134,34 @@ int adrv906x_tsu_setup(struct platform_device *pdev, struct adrv906x_tsu *tsu, s
 	if (IS_ERR(tsu->base))
 		return PTR_ERR(tsu->base);
 
-	ret = of_property_read_u32(eth_port_np, "static-phy-delay-tx-ns", &val);
-	if (ret < 0)
-		dev_warn(dev, "dt: static-phy-delay-tx-ns missing, using 0");
+	ret = of_property_read_u32(eth_port_np, "adi,pcb-delay-tx-ns", &val);
+	if (ret < 0 || val > 0xffff)
+		dev_warn(dev, "dt: adi,pcb-delay-tx-ns missing or invalid, using 0");
 
-	ret = of_property_read_u32(eth_port_np, "static-phy-delay-tx-frac-ns", &frac_val);
-	if (ret < 0) {
-		dev_warn(dev, "dt: static-phy-delay-tx-frac-ns missing, using 0");
+	ret = of_property_read_u32(eth_port_np, "adi,pcb-delay-tx-frac-ns", &frac_val);
+	if (ret < 0 || val > 0xffff) {
+		dev_warn(dev, "dt: adi,pcb-delay-tx-frac-ns missing or invalid, using 0");
 		frac_val = 0;
 	}
 
-	tsu->phy_delay_tx = (val << 16) | (frac_val & 0xFFFF);
-	dev_info(dev, "tsu static phy delay tx 0x%08x", tsu->phy_delay_tx);
+	tsu->pcb_delay_tx = (val << 16) | (frac_val & 0xFFFF);
+	dev_info(dev, "tsu pcb delay tx 0x%08x", tsu->pcb_delay_tx);
 
-	ret = of_property_read_u32(eth_port_np, "static-phy-delay-rx-ns", &val);
-	if (ret < 0) {
-		dev_warn(dev, "dt: static-phy-delay-rx-ns missing, using 0");
+	ret = of_property_read_u32(eth_port_np, "adi,pcb-delay-rx-ns", &val);
+	if (ret < 0 || val > 0xffff) {
+		dev_warn(dev, "dt: adi,pcb-delay-rx-ns missing or invalid, using 0");
 		val = 0;
 	}
 
-	ret = of_property_read_u32(eth_port_np, "static-phy-delay-rx-frac-ns", &frac_val);
-	if (ret < 0) {
-		dev_warn(dev, "dt: static-phy-delay-rx-frac-ns missing, using 0");
+	ret = of_property_read_u32(eth_port_np, "adi,pcb-delay-rx-frac-ns", &frac_val);
+	if (ret < 0 || val > 0xffff) {
+		dev_warn(dev, "dt: adi,pcb-delay-rx-frac-ns missing or invalid, using 0");
 		frac_val = 0;
 	}
 
-	tsu->phy_delay_rx = (val << 16) | (frac_val & 0xFFFF);
-	dev_info(dev, "tsu static phy delay rx 0x%08x", tsu->phy_delay_rx);
+	tsu->pcb_delay_rx = (val << 16) | (frac_val & 0xFFFF);
+	dev_info(dev, "tsu pcb delay rx 0x%08x", tsu->pcb_delay_rx);
 
-	adrv906x_tsu_set_phy_delay(tsu->base, tsu->phy_delay_tx, tsu->phy_delay_rx);
 	adrv906x_tsu_set_ptp_timestamping_mode(tsu->base);
 
 	return 0;
--- drivers/net/ethernet/adi/adrv906x-tsu.h
+++ drivers/net/ethernet/adi/adrv906x-tsu.h
@@ -23,15 +23,35 @@
 #define   ADRV906X_PTP_TIMESTAMPING_MODE_ONE_STEP        0x00000001     /* One-step */
 #define   ADRV906X_PTP_TIMESTAMPING_MODE_TRANSP          0x00000002     /* Transparent Clock */
 
+/* T_div66 is PCS RX output clock period, its [31:16] is nanosecond part
+ * its [15:0] is the fractional nanosecond part
+ * So it can be calculated as (1,000,000,000 * 0x10000) / frequency
+ * where frequency is 156.25MHz for 10G, 390.625MHz for 25G
+ */
+#define T_DIV66_10G                                      419430         /* 0x66666 */
+#define T_DIV66_25G                                      167772         /* 0x28f5c */
+
+/* T_div64 is PCS RX input clock period. T_div64 = T_div66 * 64 / 66 */
+#define T_DIV64_10G                                      406720         /* 0x634c0 */
+#define T_DIV64_25G                                      162688         /* 0x27b80 */
+
+extern int adrv906x_tod_cfg_cdc_delay;
+
 struct adrv906x_tsu {
 	void __iomem *base;
+	unsigned long hsdig_clk_rate;
+	u32 pcb_delay_tx;
+	u32 pcb_delay_rx;
 	u32 phy_delay_tx;
 	u32 phy_delay_rx;
 };
 
-void adrv906x_tsu_set_speed(struct adrv906x_tsu *tsu, struct phy_device *phydev);
+void adrv906x_tsu_set_speed(struct adrv906x_tsu *tsu, int speed);
 int adrv906x_tsu_setup(struct platform_device *pdev, struct adrv906x_tsu *tsu, struct device_node *eth_port_np);
-void adrv906x_tsu_set_phy_delay(void __iomem *base, u32 phy_delay_tx, u32 phy_delay_rx);
+void adrv906x_tsu_calculate_phy_delay(struct adrv906x_tsu *tsu, int speed,
+				      bool rs_fec_enabled, u32 bit_slip,
+				      u32 buf_delay_tx, u32 buf_delay_rx);
+void adrv906x_tsu_set_phy_delay(struct adrv906x_tsu *tsu);
 void adrv906x_tsu_set_ptp_timestamping_mode(void __iomem *base);
 
 #endif /* __ADRV906X_TSU_H__ */
