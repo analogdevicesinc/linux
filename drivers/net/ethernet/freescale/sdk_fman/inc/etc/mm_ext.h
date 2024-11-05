f39c1633c62bfa308f7c2c42b3384242378c07cd
Vladimir Oltean <vladimir.oltean@nxp.com>
2024-11-05 14:12:55 +0200

    net: sdk_fman: remove unused Memory Manager functions
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/net/ethernet/freescale/sdk_fman/inc/etc/mm_ext.h
+++ drivers/net/ethernet/freescale/sdk_fman/inc/etc/mm_ext.h
@@ -118,17 +118,6 @@ t_Error     MM_Init(t_Handle *h_MM, uint64_t base, uint64_t size);
 uint64_t    MM_Get(t_Handle h_MM, uint64_t size, uint64_t alignment, char *name);
 
 /**************************************************************************//**
- @Function      MM_GetBase
-
- @Description   Gets the base address of the required MM objects.
-
- @Param[in]     h_MM - Handle to the MM object.
-
- @Return        base address of the block.
-*//***************************************************************************/
-uint64_t    MM_GetBase(t_Handle h_MM);
-
-/**************************************************************************//**
  @Function      MM_GetForce
 
  @Description   Force memory allocation.
@@ -147,45 +136,6 @@ uint64_t    MM_GetBase(t_Handle h_MM);
  @Return        base address of an allocated block, ILLEGAL_BASE if can't allocate a block.
 *//***************************************************************************/
 uint64_t    MM_GetForce(t_Handle h_MM, uint64_t base, uint64_t size, char *name);
-
-/**************************************************************************//**
- @Function      MM_GetForceMin
-
- @Description   Allocates a block of memory according to the given size, the alignment and minimum base address.
-
-                The Alignment argument tells from which
-                free list allocate a block of memory. 2^alignment indicates
-                the alignment that the base address of the allocated block
-                should have. So, the only values 1, 2, 4, 8, 16, 32 and 64
-                are available for the alignment argument.
-                The minimum baser address forces the location of the block
-                to be from a given address onward.
-                The routine passes through the specific free list of free
-                blocks and seeks for the first base address equal or smaller
-                than the required minimum address and end address larger than
-                than the required base + its size - i.e. that may contain
-                the required block.
-                After the block is found and data is allocated, it calls
-                the internal MM_CutFree routine to update all free lists
-                do not include a just allocated block. Of course, each
-                free list contains a free blocks with the same alignment.
-                It is also creates a busy block that holds
-                information about an allocated block.
-
- @Param[in]     h_MM        - Handle to the MM object.
- @Param[in]     size        - Size of the MM.
- @Param[in]     alignment   - Index as a power of two defines a required
-                              alignment (in bytes); Should be 1, 2, 4, 8, 16, 32 or 64
- @Param[in]     min         - The minimum base address of the block.
- @Param[in]     name        - Name that specifies an allocated block.
-
- @Return        base address of an allocated block,ILLEGAL_BASE if can't allocate a block.
-*//***************************************************************************/
-uint64_t    MM_GetForceMin(t_Handle h_MM,
-                           uint64_t size,
-                           uint64_t alignment,
-                           uint64_t min,
-                           char     *name);
 
 /**************************************************************************//**
  @Function      MM_Put
@@ -209,40 +159,6 @@ uint64_t    MM_GetForceMin(t_Handle h_MM,
 uint64_t    MM_Put(t_Handle h_MM, uint64_t base);
 
 /**************************************************************************//**
- @Function      MM_PutForce
-
- @Description   Releases a block of memory of the required size from the required base address.
-
-                First, it calls to MM_CutBusy routine
-                to cut a free block from the busy list. And then, calls to
-                MM_AddFree routine to add the free block to the free lists.
-
- @Param[in]     h_MM    - Handle to the MM object.
- @Param[in]     base    - Base address of of a block to free.
- @Param[in]     size    - Size of a block to free.
-
- @Return        The number of bytes released, 0 on failure.
-*//***************************************************************************/
-uint64_t    MM_PutForce(t_Handle h_MM, uint64_t base, uint64_t size);
-
-/**************************************************************************//**
- @Function      MM_Add
-
- @Description   Adds a new memory block for memory allocation.
-
-                When a new memory block is initialized and added to the
-                memory list, it calls to MM_AddFree routine to add the
-                new free block to the free lists.
-
- @Param[in]     h_MM    - Handle to the MM object.
- @Param[in]     base    - Base address of the memory block.
- @Param[in]     size    - Size of the memory block.
-
- @Return        E_OK on success, otherwise returns an error code.
-*//***************************************************************************/
-t_Error     MM_Add(t_Handle h_MM, uint64_t base, uint64_t size);
-
-/**************************************************************************//**
  @Function      MM_Dump
 
  @Description   Prints results of free and busy lists.
@@ -259,38 +175,6 @@ void        MM_Dump(t_Handle h_MM);
  @Param[in]     h_MM - Handle of the MM object.
 *//***************************************************************************/
 void        MM_Free(t_Handle h_MM);
-
-/**************************************************************************//**
- @Function      MM_GetMemBlock
-
- @Description   Returns base address of the memory block specified by the index.
-
-                If index is 0, returns base address
-                of the first memory block, 1 - returns base address
-                of the second memory block, etc.
-                Note, those memory blocks are allocated by the
-                application before MM_Init or MM_Add and have to
-                be released by the application before or after invoking
-                the MM_Free routine.
-
- @Param[in]     h_MM    - Handle to the MM object.
- @Param[in]     index   - Index of the memory block.
-
- @Return        valid base address or ILLEGAL_BASE if no memory block specified by the index.
-*//***************************************************************************/
-uint64_t    MM_GetMemBlock(t_Handle h_MM, int index);
-
-/**************************************************************************//**
- @Function      MM_InRange
-
- @Description   Checks if a specific address is in the memory range of the passed MM object.
-
- @Param[in]     h_MM    - Handle to the MM object.
- @Param[in]     addr    - The address to be checked.
-
- @Return        TRUE if the address is in the address range of the block, FALSE otherwise.
-*//***************************************************************************/
-bool        MM_InRange(t_Handle h_MM, uint64_t addr);
 
 /**************************************************************************//**
  @Function      MM_GetFreeMemSize
