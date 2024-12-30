analogdevicesinc/linux

commit 7e7216d725647bef14a23e119e42adcd7854976b

iio: beamformer: adar1000: add spi_device_id table
This prevents the warning message "SPI driver has no spi_device_id for..."
when registering the driver. More importantly, it makes sure that
module autoloading works as spi relies on spi: modaliases and not of.

Fixes: b2316a2ae75e ("iio: adc: adar1000: Initial driver version")
Signed-off-by: Nuno Sa <nuno.sa@analog.com>

drivers/iio/beamformer/adar1000.c
@@ -2632,12 +2632,19 @@ static const struct of_device_id adar1000_of_match[] = {
 };
 MODULE_DEVICE_TABLE(of, adar1000_of_match);
 
+static const struct spi_device_id adar1000_id[] = {
+	{ "adar1000" },
+	{ }
+}
+MODULE_DEVICE_TABLE(spi, adar1000_id_table);
+
 static struct spi_driver adar1000_driver = {
 	.driver = {
 		.name	= "adar1000",
 		.of_match_table = adar1000_of_match,
 	},
-	.probe		= adar1000_probe,
+	.probe = adar1000_probe,
+	.id_table = adar1000_id,
 };
 module_spi_driver(adar1000_driver);
 

commit 090742a4224e75cbd094322041853c0222c8c36d

iio: beamformer: adar1000: Fix a build failure
The compiler is unhappy when the driver is configured as a module:

	In file included from drivers/iio/beamformer/adar1000.c:9:
	include/linux/module.h:244:1: error: expected ‘,’ or ‘;’ before ‘extern’
	  244 | extern typeof(name) __mod_##type##__##name##_device_table               \
	      | ^~~~~~
	drivers/iio/beamformer/adar1000.c:2639:1: note: in expansion of macro ‘MODULE_DEVICE_TABLE’
	 2639 | MODULE_DEVICE_TABLE(spi, adar1000_id_table);
	      | ^~~~~~~~~~~~~~~~~~~

and when the ; is added dies again with:

	In file included from drivers/iio/beamformer/adar1000.c:9:
	drivers/iio/beamformer/adar1000.c:2639:26: error: ‘adar1000_id_table’ undeclared here (not in a function); did you mean ‘adar1000_read_enable’?
	 2639 | MODULE_DEVICE_TABLE(spi, adar1000_id_table);
	      |                          ^~~~~~~~~~~~~~~~~
	include/linux/module.h:244:15: note: in definition of macro ‘MODULE_DEVICE_TABLE’
	  244 | extern typeof(name) __mod_##type##__##name##_device_table               \
	      |               ^~~~
	include/linux/module.h:244:21: error: ‘__mod_spi__adar1000_id_table_device_table’ aliased to undefined symbol ‘adar1000_id_table’
	  244 | extern typeof(name) __mod_##type##__##name##_device_table               \
	      |                     ^~~~~~
	drivers/iio/beamformer/adar1000.c:2639:1: note: in expansion of macro ‘MODULE_DEVICE_TABLE’
	 2639 | MODULE_DEVICE_TABLE(spi, adar1000_id_table);
	      | ^~~~~~~~~~~~~~~~~~~

Fixes: 7e7216d72564 ("iio: beamformer: adar1000: add spi_device_id table")
Signed-off-by: Uwe Kleine-König <u.kleine-koenig@baylibre.com>

drivers/iio/beamformer/adar1000.c
@@ -2635,8 +2635,8 @@ MODULE_DEVICE_TABLE(of, adar1000_of_match);
 static const struct spi_device_id adar1000_id[] = {
 	{ "adar1000" },
 	{ }
-}
-MODULE_DEVICE_TABLE(spi, adar1000_id_table);
+};
+MODULE_DEVICE_TABLE(spi, adar1000_id);
 
 static struct spi_driver adar1000_driver = {
 	.driver = {