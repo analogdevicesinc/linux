43d21d0e7eb4b8cced5d859aee4f87fcc0440915
Nuno Sa <nuno.sa@analog.com>
2025-01-15 17:19:58 +0000

    iio: adc: adrv9002: Update API to 68.14.10
    The SDK is taken as is which means that compilation will likely fail.
    
    The folloing changes were squashed in the new SDK (need to be sent to
    the BU):
    
    * 9d9cde6a64b4 ("iio: adc: adrv9002: radio: turn radio functions private")
    * 67b6d36a286d ("iio: adc: adrv9002: radio: fix pin mode")
    
    Signed-off-by: Nuno Sa <nuno.sa@analog.com>
    (cherry picked from commit 3c53527979ed094ce534feafc4654dff30134e0a)

--- drivers/iio/adc/navassa/devices/adrv9001/public/include/adi_adrv9001_version.h
+++ drivers/iio/adc/navassa/devices/adrv9001/public/include/adi_adrv9001_version.h
@@ -19,7 +19,7 @@ extern "C" {
 #endif
 
 /* Auto-generated version number - DO NOT MANUALLY EDIT */
-#define ADI_ADRV9001_CURRENT_VERSION "68.13.7"
+#define ADI_ADRV9001_CURRENT_VERSION "68.14.10"
 
 #ifdef __cplusplus
 }
