804ad827aab024c6d9fe00e2d9d797741c55a393
Guillaume Ranquet <granquet@baylibre.com>
2025-01-27 14:59:32 +0100

    iio: introduce the FAULT event type
    Add a new event type to describe an hardware failure.
    
    Reviewed-by: Nuno Sa <nuno.sa@analog.com>
    Signed-off-by: Guillaume Ranquet <granquet@baylibre.com>
    Reviewed-by: David Lechner <dlechner@baylibre.com>
    Link: https://patch.msgid.link/20250127-ad4111_openwire-v5-1-ef2db05c384f@baylibre.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    (cherry picked from commit c8cfa4588b9a03a47eb4c0545f27078f74107a5a)

--- include/uapi/linux/iio/types.h
+++ include/uapi/linux/iio/types.h
@@ -116,6 +116,7 @@ enum iio_event_type {
 	IIO_EV_TYPE_CHANGE,
 	IIO_EV_TYPE_MAG_REFERENCED,
 	IIO_EV_TYPE_GESTURE,
+	IIO_EV_TYPE_FAULT,
 };
 
 enum iio_event_direction {
@@ -125,6 +126,7 @@ enum iio_event_direction {
 	IIO_EV_DIR_NONE,
 	IIO_EV_DIR_SINGLETAP,
 	IIO_EV_DIR_DOUBLETAP,
+	IIO_EV_DIR_FAULT_OPENWIRE,
 };
 
 #endif /* _UAPI_IIO_TYPES_H_ */
