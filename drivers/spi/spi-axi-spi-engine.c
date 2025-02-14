b5552774b2d56fb726aaf391657b8f767511fa8c
David Lechner <dlechner@baylibre.com>
2025-02-14 10:41:29 -0600

    arch: arm : dts: rename adi,axi-spi-engine to legacy
    To prepare for backporting the upstream SPI offload work, we need to
    rename the ADI tree's AXI SPI engine compatible string to avoid
    conflicts with the upstream driver.
    
    Call it "adi,legacy-axi-spi-engine-1.00.a" to make it clear this is
    the one that should eventually be replaced by the version that got
    accepted upstream.
    
    Signed-off-by: David Lechner <dlechner@baylibre.com>
    (cherry picked from commit d472520b1f45d1ab313ec58d7417d4a25dce7522)

--- drivers/spi/spi-axi-spi-engine.c
+++ drivers/spi/spi-axi-spi-engine.c
@@ -778,7 +778,7 @@ static int spi_engine_remove(struct platform_device *pdev)
 }
 
 static const struct of_device_id spi_engine_match_table[] = {
-	{ .compatible = "adi,axi-spi-engine-1.00.a" },
+	{ .compatible = "adi,legacy-axi-spi-engine-1.00.a" },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, spi_engine_match_table);
