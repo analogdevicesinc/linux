b05d5f14cda63d71a2950280bd32ca03fb509203
Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
2025-02-20 10:57:02 +0100

    gpiolib: introduce gpio_chip setters that return values
    Add new variants of the set() and set_multiple() callbacks that have
    integer return values allowing to indicate failures to users of the GPIO
    consumer API. Until we convert all GPIO providers treewide to using
    them, they will live in parallel to the existing ones.
    
    Make sure that providers cannot define both. Prefer the new ones and
    only use the old ones as fallback.
    
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Uwe Kleine-König <u.kleine-koenig@baylibre.com>
    Link: https://lore.kernel.org/r/20250220-gpio-set-retval-v2-5-bc4cfd38dae3@linaro.org
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

--- include/linux/gpio/driver.h
+++ include/linux/gpio/driver.h
@@ -346,6 +346,10 @@ struct gpio_irq_chip {
  *	stores them in "bits", returns 0 on success or negative error
  * @set: assigns output value for signal "offset"
  * @set_multiple: assigns output values for multiple signals defined by "mask"
+ * @set_rv: assigns output value for signal "offset", returns 0 on success or
+ *          negative error value
+ * @set_multiple_rv: assigns output values for multiple signals defined by
+ *                   "mask", returns 0 on success or negative error value
  * @set_config: optional hook for all kinds of settings. Uses the same
  *	packed config format as generic pinconf.
  * @to_irq: optional hook supporting non-static gpiod_to_irq() mappings;
@@ -441,6 +445,12 @@ struct gpio_chip {
 	void			(*set_multiple)(struct gpio_chip *gc,
 						unsigned long *mask,
 						unsigned long *bits);
+	int			(*set_rv)(struct gpio_chip *gc,
+					  unsigned int offset,
+					  int value);
+	int			(*set_multiple_rv)(struct gpio_chip *gc,
+						   unsigned long *mask,
+						   unsigned long *bits);
 	int			(*set_config)(struct gpio_chip *gc,
 					      unsigned int offset,
 					      unsigned long config);
