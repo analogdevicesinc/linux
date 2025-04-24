194cc195838c1fe8d3b362e2df3c71dc9c45f568
Andreea Andrisan <Andreea.Andrisan@analog.com>
2025-04-24 14:51:40 +0300

    test sw

--- ci/travis/prepare_artifacts_rpi.sh
+++ ci/travis/prepare_artifacts_rpi.sh
@@ -7,27 +7,27 @@ GIT_SHA_DATE=$(git show -s --format=%cd --date=format:'%Y-%m-%d %H:%M' ${GIT_SHA
 
 #prepare the structure of the folder containing artifacts
 artifacts_structure() {
-	cd ${SOURCE_DIRECTORY}
+	#cd ${SOURCE_DIRECTORY}
 	mkdir ${timestamp}
 
-	echo "git_branch=${BUILD_SOURCEBRANCHNAME}" >> ${timestamp}/rpi_git_properties.txt
-	echo "git_sha=${GIT_SHA}" >> ${timestamp}/rpi_git_properties.txt
-	echo "git_sha_date=${GIT_SHA_DATE}" >> ${timestamp}/rpi_git_properties.txt
+	echo "git_branch=${BUILD_SOURCEBRANCHNAME}" >> ${timestamp}/rpi_archives_properties.txt
+	echo "git_sha=${GIT_SHA}" >> ${timestamp}/rpi_archives_properties.txt
+	echo "git_sha_date=${GIT_SHA_DATE}" >> ${timestamp}/rpi_archives_properties.txt
 
-	typeBCM=( "bcm2709" "bcm2711" "bcmrpi" )
-	typeKERNEL=( "kernel7" "kernel7l" "kernel" )
-	for index in "${!typeBCM[@]}"; do
-		cd adi_"${typeBCM[$index]}"_defconfig
-		mkdir overlays modules
-		mv ./*.dtbo ./overlays
-		tar -xf rpi_modules.tar.gz -C modules
-		rm rpi_modules.tar.gz
-		mv ./zImage ./"${typeKERNEL[$index]}".img
-		cd ../
-		cp -r ./adi_"${typeBCM[$index]}"_defconfig/* ./${timestamp}
-	done
-	tar -C ${SOURCE_DIRECTORY}/${timestamp}/modules -czvf ${SOURCE_DIRECTORY}/${timestamp}/rpi_modules.tar.gz .
-	rm -r ${SOURCE_DIRECTORY}/${timestamp}/modules
+	# typeBCM=( "bcm2709" "bcm2711" "bcmrpi" )
+	# typeKERNEL=( "kernel7" "kernel7l" "kernel" )
+	# for index in "${!typeBCM[@]}"; do
+	# 	cd adi_"${typeBCM[$index]}"_defconfig
+	# 	mkdir overlays modules
+	# 	mv ./*.dtbo ./overlays
+	# 	tar -xf rpi_modules.tar.gz -C modules
+	# 	rm rpi_modules.tar.gz
+	# 	mv ./zImage ./"${typeKERNEL[$index]}".img
+	# 	cd ../
+	# 	cp -r ./adi_"${typeBCM[$index]}"_defconfig/* ./${timestamp}
+	# done
+	# tar -C ${SOURCE_DIRECTORY}/${timestamp}/modules -czvf ${SOURCE_DIRECTORY}/${timestamp}/rpi_modules.tar.gz .
+	# rm -r ${SOURCE_DIRECTORY}/${timestamp}/modules
 }
 
 #upload artifacts to Artifactory
@@ -42,28 +42,29 @@ artifacts_artifactory() {
 #archive artifacts and upload to SWDownloads
 artifacts_swdownloads() {
 	artifacts_structure
-	cd ${SOURCE_DIRECTORY}/${timestamp} || exit 1
+	cd ${timestamp} || exit 1
 	chmod 600 ${KEY_FILE}
+	echo ">>>>>>>>>>>>>>>1"
 	scp -2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o HostKeyAlgorithms=+ssh-dss \
-		-i ${KEY_FILE} -r *.tar.gz ${DEST_SERVER}/${BUILD_SOURCEBRANCHNAME}
+		-v -i ${KEY_FILE} -r rpi_archives_properties.txt ${DEST_SERVER}/rpi-6.6.y
 	md5_modules=($(md5sum rpi_modules.tar.gz| cut -d ' ' -f 1))
+	echo ">>>>>>>>>>>>>>>2"
+	# rm rpi_modules.tar.gz rpi_git_properties.txt
+	# tar -C ${PWD} -czvf rpi_latest_boot.tar.gz *
+	# md5_boot=($(md5sum rpi_latest_boot.tar.gz| cut -d ' ' -f 1))
+	# scp -2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o HostKeyAlgorithms=+ssh-dss \
+	#     -i ${KEY_FILE} -r rpi_latest_boot.tar.gz ${DEST_SERVER}/${BUILD_SOURCEBRANCHNAME}
 
-	rm rpi_modules.tar.gz rpi_git_properties.txt
-	tar -C ${PWD} -czvf rpi_latest_boot.tar.gz *
-	md5_boot=($(md5sum rpi_latest_boot.tar.gz| cut -d ' ' -f 1))
-	scp -2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o HostKeyAlgorithms=+ssh-dss \
-	    -i ${KEY_FILE} -r rpi_latest_boot.tar.gz ${DEST_SERVER}/${BUILD_SOURCEBRANCHNAME}
+	# echo "git_branch=${BUILD_SOURCEBRANCHNAME}" >> rpi_archives_properties.txt
+	# echo "https://swdownloads.analog.com/cse/linux_rpi/${BUILD_SOURCEBRANCHNAME}/rpi_modules.tar.gz" >> rpi_archives_properties.txt
+	# echo "https://swdownloads.analog.com/cse/linux_rpi/${BUILD_SOURCEBRANCHNAME}/rpi_latest_boot.tar.gz" >> rpi_archives_properties.txt
+	# echo "checksum_modules=${md5_modules}" >> rpi_archives_properties.txt
+	# echo "checksum_boot_files=${md5_boot}" >> rpi_archives_properties.txt
+	# echo "git_sha=${GIT_SHA}" >> rpi_archives_properties.txt
+	# echo "git_sha_date=${GIT_SHA_DATE}" >> rpi_archives_properties.txt
 
-	echo "git_branch=${BUILD_SOURCEBRANCHNAME}" >> rpi_archives_properties.txt
-	echo "https://swdownloads.analog.com/cse/linux_rpi/${BUILD_SOURCEBRANCHNAME}/rpi_modules.tar.gz" >> rpi_archives_properties.txt
-	echo "https://swdownloads.analog.com/cse/linux_rpi/${BUILD_SOURCEBRANCHNAME}/rpi_latest_boot.tar.gz" >> rpi_archives_properties.txt
-	echo "checksum_modules=${md5_modules}" >> rpi_archives_properties.txt
-	echo "checksum_boot_files=${md5_boot}" >> rpi_archives_properties.txt
-	echo "git_sha=${GIT_SHA}" >> rpi_archives_properties.txt
-	echo "git_sha_date=${GIT_SHA_DATE}" >> rpi_archives_properties.txt
-
-	scp -2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o HostKeyAlgorithms=+ssh-dss \
-                -i ${KEY_FILE} -r rpi_archives_properties.txt ${DEST_SERVER}/${BUILD_SOURCEBRANCHNAME}
+	# scp -2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o HostKeyAlgorithms=+ssh-dss \
+  #               -i ${KEY_FILE} -r rpi_archives_properties.txt ${DEST_SERVER}/${BUILD_SOURCEBRANCHNAME}
 }
 
 artifacts_${1}
