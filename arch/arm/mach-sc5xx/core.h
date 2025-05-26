bae7711da2d56e472b735d2d56f81751476d6d25
UtsavAgarwalADI <utsav.agarwal@analog.com>
2025-05-26 12:01:12 +0100

    arm: mach-sc5xx: Adding init for sc5xx boards
    Adding machine specific initialization
    
    Signed-off-by: UtsavAgarwalADI <utsav.agarwal@analog.com>

---
+++ arch/arm/mach-sc5xx/core.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * (C) Copyright 2022 - Analog Devices, Inc.
+ *
+ * Written and/or maintained by Timesys Corporation
+ *
+ * Contact: Nathan Barrett-Morrison <nathan.morrison@timesys.com>
+ * Contact: Greg Malysa <greg.malysa@timesys.com>
+ *
+ */
+
+#ifndef ARCH_SC5XX_CORE_H
+#define ARCH_SC5XX_CORE_H
+
+#include <linux/init.h>
+
+void __init sc5xx_init_ethernet(void);
+
+#endif
