117071e356855468ca70c5c06c691ff8e7c5a5ae
UtsavAgarwalADI <utsav.agarwal@analog.com>
2025-07-15 15:52:39 +0100

    musb: adi: Adding adsp musb glue layer
    Adding glue layer for adsp platforms sc57x/sc58x.
    
    This is enables support for the following:
    
    * usbboot: Mount and boot into file systems contained on USB devices by
    switching OTG to host mode.
    
    * usb gadget audio: Utilize linux's USB gadget for streaming audio from
    another device by switching OTG to device mode.
    
    Signed-off-by: Utsav Agarwal <utsav.agarwal@analog.com>

---
+++ drivers/usb/musb/adi.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * (C) Copyright 2022 - Analog Devices, Inc.
+ *
+ * Written by Timesys Corporation
+ * Maintained by Analog Devices, Inc.
+ *
+ * Contact: Analog Devices, Inc <adsp-linux@analog.com>
+ *
+ */
+
+#ifndef __ADI_H__
+#define __ADI_H__
+
+#define REG_USB_VBUS_CTL	0x380
+#define REG_USB_ID_CTL		0x382
+#define REG_USB_PHY_CTL		0x394
+#define REG_USB_PLL_OSC		0x398
+#define REG_USB_UTMI_CTL	0x39c
+
+#endif
