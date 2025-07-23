b06b4e0efa04f5309102c7a65f0dfe97aeb1432a
Jonathan Santos <Jonathan.Santos@analog.com>
2025-07-23 18:55:30 -0300

    spi: Set bits for all of device chipselects into cs_index_mask\
    The cs_index_mask field of struct spi_device is meant to hold the
    chipselect(s) that the driver needs to use from the chipselect array.
    Though, device registration was setting only one CS number into
    cs_index_mask, preventing devices from having multiple CS lines.
    Properly set cs_index_mask with all CS numbers provided from device tree
    reg property.
    
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>
    Signed-off-by: Jonathan Santos <Jonathan.Santos@analog.com>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -2602,7 +2602,8 @@ static int of_spi_parse_dt(struct spi_controller *ctlr, struct spi_device *spi,
 	 * By default spi->chip_select[0] will hold the physical CS number,
 	 * so set bit 0 in spi->cs_index_mask.
 	 */
-	spi->cs_index_mask = BIT(0);
+	for (idx = 0; idx < rc; idx++)
+		spi->cs_index_mask |= BIT(cs[idx]);
 
 	/* Device speed */
 	if (!of_property_read_u32(nc, "spi-max-frequency", &value))
