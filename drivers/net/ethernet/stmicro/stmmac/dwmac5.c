altera-opensource/linux-socfpga

HSD #16020988342: net: stmmac: Fix FPE handshaking flow
In the current FPE handshaking flow, FPE is enabled for local only
the link partner sends Verify mPacket and the local responds with a
Response mPacket. i.e., FPE is only enabled if lo_state of FPE
handshaking flow enters FPE_STATE_ENTERING_ON. But this state is
not relevant for the local.

802.3br mentions the MAC merge sublayer receiver always operates the
same way regardless of whether the preemption is active or not. In
other words, the receiver is always ready for preemption. DWMAC IP
receive side pre-emption is always active. Pre-emption capability is
activated independently on each end. So for enabling transmit side
pre-emption, no need to wait for Verify mPacket from the link partner
Response mPacket from the link partner is enough.

Also seperated FPE receive and transmit configuration as pre-emption
is activated independently on each end. Also pMAC is always ready
for receive pre-emptible traffic.

Signed-off-by: Rohan G Thomas <rohan.g.thomas@intel.com>

drivers/net/ethernet/stmicro/stmmac/dwmac5.c
@@ -710,34 +710,37 @@ void dwmac5_est_irq_status(void __iomem *ioaddr, struct net_device *dev,
 	}
 }
 
-void dwmac5_fpe_configure(void __iomem *ioaddr, u32 num_txq, u32 num_rxq,
-			  u32 txqpec, bool enable)
+void dwmac5_fpe_tx_configure(void __iomem *ioaddr, u32 num_txq, u32 txqpec,
+			  bool enable)
 {
 	u32 txqmask = (1 << num_txq) - 1;
 	u32 value;
 
-	if (!enable) {
+	if (enable) {
+		value = readl(ioaddr + MTL_FPE_CTRL_STS);
+		value &= ~(txqmask << PEC_SHIFT);
+		value |= (txqpec << PEC_SHIFT);
+		writel(value, ioaddr + MTL_FPE_CTRL_STS);
+
 		value = readl(ioaddr + MAC_FPE_CTRL_STS);
-
-		value &= ~EFPE;
-
+		value |= EFPE;
 		writel(value, ioaddr + MAC_FPE_CTRL_STS);
-		return;
 	}
+	else {
+		value = readl(ioaddr + MAC_FPE_CTRL_STS);
+		value &= ~EFPE;
+		writel(value, ioaddr + MAC_FPE_CTRL_STS);
+	}
+}
+
+void dwmac5_fpe_rx_configure(void __iomem *ioaddr, u32 num_rxq)
+{
+	u32 value;
 
 	value = readl(ioaddr + GMAC_RXQ_CTRL1);
 	value &= ~GMAC_RXQCTRL_FPRQ;
 	value |= (num_rxq - 1) << GMAC_RXQCTRL_FPRQ_SHIFT;
 	writel(value, ioaddr + GMAC_RXQ_CTRL1);
-
-	value = readl(ioaddr + MTL_FPE_CTRL_STS);
-	value &= ~(txqmask << PEC_SHIFT);
-	value |= (txqpec << PEC_SHIFT);
-	writel(value, ioaddr + MTL_FPE_CTRL_STS);
-
-	value = readl(ioaddr + MAC_FPE_CTRL_STS);
-	value |= EFPE;
-	writel(value, ioaddr + MAC_FPE_CTRL_STS);
 }
 
 int dwmac5_fpe_irq_status(void __iomem *ioaddr, struct net_device *dev)

analogdevicesinc/linux

drivers/net/ethernet/stmicro/stmmac/dwmac5.c
@@ -710,22 +710,26 @@ void dwmac5_est_irq_status(void __iomem *ioaddr, struct net_device *dev,
 	}
 }
 
-void dwmac5_fpe_configure(void __iomem *ioaddr, struct stmmac_fpe_cfg *cfg,
-			  u32 num_txq, u32 num_rxq, u32 txqpec, bool enable)
+void dwmac5_fpe_tx_configure(void __iomem *ioaddr, struct stmmac_fpe_cfg *cfg,
+			  u32 num_txq, u32 txqpec, bool enable)
 {
-	u32 value;
-
 	if (enable) {
 		cfg->fpe_csr = EFPE;
 		cfg->fpe_csr = (txqpec << PEC_SHIFT);
-		value = readl(ioaddr + GMAC_RXQ_CTRL1);
-		value &= ~GMAC_RXQCTRL_FPRQ;
-		value |= (num_rxq - 1) << GMAC_RXQCTRL_FPRQ_SHIFT;
-		writel(value, ioaddr + GMAC_RXQ_CTRL1);
 	} else {
 		cfg->fpe_csr = 0;
 	}
 	writel(cfg->fpe_csr, ioaddr + MAC_FPE_CTRL_STS);
+}
+
+void dwmac5_fpe_rx_configure(void __iomem *ioaddr, u32 num_rxq)
+{
+	u32 value;
+
+	value = readl(ioaddr + GMAC_RXQ_CTRL1);
+	value &= ~GMAC_RXQCTRL_FPRQ;
+	value |= (num_rxq - 1) << GMAC_RXQCTRL_FPRQ_SHIFT;
+	writel(value, ioaddr + GMAC_RXQ_CTRL1);
 }
 
 int dwmac5_fpe_irq_status(void __iomem *ioaddr, struct net_device *dev)