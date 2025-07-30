a52b0cdae7605b947c571bc3e3d3215312196045
Jorge Marques <jorge.marques@analog.com>
2025-07-30 13:51:24 +0200

    fixup! drivers: misc: adi-axi-hsci: Add Analog Devices AXI HSCI interface driver
    Use kernel types, add declarations.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- drivers/misc/adi-axi-hsci.h
+++ drivers/misc/adi-axi-hsci.h
@@ -10,33 +10,33 @@
 #ifndef ADI_AXI_HSCI_H_
 #define ADI_AXI_HSCI_H_
 
+#include <linux/types.h>
+#include <linux/device.h>
+
 struct axi_hsci_state;
 
 #ifdef CONFIG_ADI_AXI_HSCI
 
-int axi_hsci_manual_linkup(struct axi_hsci_state *st, uint8_t enable,
-			   uint16_t link_up_signal_bits);
-
-int axi_hsci_auto_linkup(struct axi_hsci_state *st, uint8_t enable,
-			 u8 hscim_mosi_clk_inv, uint8_t hscim_miso_clk_inv);
-
-int axi_hsci_readm(struct axi_hsci_state *st, const uint8_t tx_data[],
-		   u8 rx_data[], uint32_t num_tx_rx_bytes,
-		   u8 addr_len,
-		   u8 data_len,
-		   uint32_t stream_len);
-
-int axi_hsci_writem(struct axi_hsci_state *st, const uint8_t tx_data[],
-		    u32 num_tx_rx_bytes,
-		    u8 addr_len,
-		    u8 data_len,
-		    uint32_t stream_len);
-
-struct axi_hsci_state *devm_axi_hsci_get_optional(struct device *source);
-
 void axi_hsci_silent(struct axi_hsci_state *st, bool enable);
 
-int axi_hsci_alink_tbl_get(struct axi_hsci_state *st, u16 *hscim_alink_table);
+int axi_hsci_alink_tbl_get(struct axi_hsci_state *st,
+			   u16 *hscim_alink_table);
+
+int axi_hsci_manual_linkup(struct axi_hsci_state *st, u8 enable,
+			   u16 link_up_signal_bits);
+
+int axi_hsci_auto_linkup(struct axi_hsci_state *st, u8 enable,
+			 u8 hscim_mosi_clk_inv, u8 hscim_miso_clk_inv);
+
+int axi_hsci_readm(struct axi_hsci_state *st, const u8 tx_data[], u8 rx_data[],
+		   u32 num_tx_rx_bytes, u8 addr_len, u8 data_len,
+		   u32 stream_len);
+
+int axi_hsci_writem(struct axi_hsci_state *st, const u8 tx_data[],
+		    u32 num_tx_rx_bytes, u8 addr_len, u8 data_len,
+		    u32 stream_len);
+
+struct axi_hsci_state *devm_axi_hsci_get_optional(struct device *source);
 
 #else
 
@@ -52,24 +52,24 @@ static inline int axi_hsci_alink_tbl_get(struct axi_hsci_state *st,
 
 static inline int axi_hsci_manual_linkup(struct axi_hsci_state *st,
 					 u8 enable,
-					 uint16_t link_up_signal_bits)
+					 u16 link_up_signal_bits)
 {
 	return -ENODEV;
 }
 
 static inline int axi_hsci_auto_linkup(struct axi_hsci_state *st,
 				       u8 enable,
-				       u8 hscim_mosi_clk_inv, uint8_t hscim_miso_clk_inv)
+				       u8 hscim_mosi_clk_inv, u8 hscim_miso_clk_inv)
 {
 	return -ENODEV;
 }
 
 static inline int axi_hsci_readm(struct axi_hsci_state *st,
 				 const u8 tx_data[],
-				 u8 rx_data[], uint32_t num_tx_rx_bytes,
+				 u8 rx_data[], u32 num_tx_rx_bytes,
 				 u8 addr_len,
 				 u8 data_len,
-				 uint32_t stream_len)
+				 u32 stream_len)
 {
 	return -ENODEV;
 }
@@ -79,7 +79,7 @@ static inline int axi_hsci_writem(struct axi_hsci_state *st,
 				  u32 num_tx_rx_bytes,
 				  u8 addr_len,
 				  u8 data_len,
-				  uint32_t stream_len)
+				  u32 stream_len)
 {
 	return -ENODEV;
 }
