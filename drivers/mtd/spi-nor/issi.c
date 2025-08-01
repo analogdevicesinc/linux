6cc52e51a86babd6295bda4b35d2615dd8694dfe
Philip Molloy <philip.molloy@analog.com>
2025-08-01 19:34:12 +0200

    [ADI] mtd: spi-nor: issi: add support for is25lp01g
    Link: https://github.com/Xilinx/linux-xlnx/commit/4152de17bc53
    (cherry picked from commit 4152de17bc534f890546bc100008996b0eb3a70f)
    Signed-off-by: Philip Molloy <philip.molloy@analog.com>

--- drivers/mtd/spi-nor/issi.c
+++ drivers/mtd/spi-nor/issi.c
@@ -111,6 +111,13 @@ static const struct flash_info issi_nor_parts[] = {
 		.size = SZ_64M,
 		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES,
 	}, {
+		.id = SNOR_ID(0x9d, 0x60, 0x1b),
+		.name = "is25lp01g",
+		.size = SZ_128M,
+		.flags = SPI_NOR_HAS_LOCK,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
+		.fixup_flags = SPI_NOR_4B_OPCODES,
+	}, {
 		.id = SNOR_ID(0x9d, 0x70, 0x16),
 		.name = "is25wp032",
 		.size = SZ_4M,
