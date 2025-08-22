4b47db660451e4ee732383429a2ba3402dfb52a2
AndrDragomir <andrei.dragomir@analog.com>
2025-08-22 12:13:22 +0300

    adrv903x: Replace default profile and add ORX
    Signed-off-by: AndrDragomir <andrei.dragomir@analog.com>

--- drivers/iio/adc/ad_adc.c
+++ drivers/iio/adc/ad_adc.c
@@ -637,8 +637,10 @@ static const struct of_device_id adc_of_match[] = {
 				.data = &adrv9002_rx_chip_info },
 	{ .compatible = "adi,axi-adrv9006-rx2-1.0",
 				.data = &adrv9002_rx_chip_info },
+	{ .compatible = "adi,axi-adrv903x-obs-1.0",
+				.data = &obs_rx_chip_info },
 	{ .compatible = "adi,axi-adc-tpl-so-10.0.a",
-		.data = &obs_rx_chip_info },
+				.data = &obs_rx_chip_info },
 	{ /* end of list */ },
 };
 MODULE_DEVICE_TABLE(of, adc_of_match);
