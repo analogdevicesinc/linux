f8dc92d7a48744151929461e00ca5eb30dd4ffc2
Jorge Marques <jorge.marques@analog.com>
2025-09-03 17:33:28 +0200

    ci: Containerfile: Bump base image version
    opensuse/leap/16.0 is now in the container registry, and adding the repo
    is not necessary anymore. Bump gcc to 15 as well.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- container/install-compilers.sh
+++ container/install-compilers.sh
@@ -2,18 +2,15 @@
 
 set -e
 
-zypper addrepo -c http://download.opensuse.org/distribution/leap/16.0/repo/oss leap-16.0-repo-oss
-zypper refresh
-
 #core, linux, linux gcc, linux clang, arm, aarch64, linux checks
 zypper install -y --no-recommends \
     git \
     libncurses6 swig bc u-boot-tools flex bison tar kmod xz gawk diffutils \
-    gcc14 libelf1 libelf-devel \
+    gcc15 libelf1 libelf-devel \
     clang19 llvm19 lld19 \
-    cross-arm-gcc14 \
-    cross-aarch64-gcc14-bootstrap \
-    sparse coccinelle ocaml ocaml-findlib cppcheck python311 python311-pip python311-devel
+    cross-arm-gcc15 \
+    cross-aarch64-gcc15 \
+    sparse coccinelle ocaml ocaml-findlib cppcheck python313 python313-pip python313-devel
 
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 50
-update-alternatives --install /usr/bin/cpp cpp /usr/bin/cpp-14 50
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-15 50
+update-alternatives --install /usr/bin/cpp cpp /usr/bin/cpp-15 50
