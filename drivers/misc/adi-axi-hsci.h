4ee6ce7c8c1bfb4b3f55f8f945093b1f132cb4ab
Jorge Marques <jorge.marques@analog.com>
2025-09-08 18:52:50 +0200

    misc: adi-axi-hsci: fix as a module
    Needs to be tristate, and the fallback defines are not useful for
    moudules.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- drivers/misc/adi-axi-hsci.h
+++ drivers/misc/adi-axi-hsci.h
@@ -15,8 +15,6 @@
 
 struct axi_hsci_state;
 
-#ifdef CONFIG_ADI_AXI_HSCI
-
 void axi_hsci_silent(struct axi_hsci_state *st, bool enable);
 
 int axi_hsci_alink_tbl_get(struct axi_hsci_state *st,
@@ -37,59 +35,5 @@ int axi_hsci_writem(struct axi_hsci_state *st, const u8 tx_data[],
 		    u32 stream_len);
 
 struct axi_hsci_state *devm_axi_hsci_get_optional(struct device *source);
-
-#else
-
-static inline void axi_hsci_silent(struct axi_hsci_state *st, bool enable)
-{
-}
-
-static inline int axi_hsci_alink_tbl_get(struct axi_hsci_state *st,
-					 u16 *hscim_alink_table)
-{
-	return -ENODEV;
-}
-
-static inline int axi_hsci_manual_linkup(struct axi_hsci_state *st,
-					 u8 enable,
-					 u16 link_up_signal_bits)
-{
-	return -ENODEV;
-}
-
-static inline int axi_hsci_auto_linkup(struct axi_hsci_state *st,
-				       u8 enable,
-				       u8 hscim_mosi_clk_inv, u8 hscim_miso_clk_inv)
-{
-	return -ENODEV;
-}
-
-static inline int axi_hsci_readm(struct axi_hsci_state *st,
-				 const u8 tx_data[],
-				 u8 rx_data[], u32 num_tx_rx_bytes,
-				 u8 addr_len,
-				 u8 data_len,
-				 u32 stream_len)
-{
-	return -ENODEV;
-}
-
-static inline int axi_hsci_writem(struct axi_hsci_state *st,
-				  const u8 tx_data[],
-				  u32 num_tx_rx_bytes,
-				  u8 addr_len,
-				  u8 data_len,
-				  u32 stream_len)
-{
-	return -ENODEV;
-}
-
-static inline struct
-axi_hsci_state *devm_axi_hsci_get_optional(struct device *source)
-{
-	return NULL;
-}
-
-#endif /* CONFIG_ADI_AXI_HSCI */
 
 #endif /* ADI_AXI_HSCI_H_ */
