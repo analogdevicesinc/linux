54db82bbfeccf3cb9b04042ab16ec7ee3219ede3
Caleb Ethridge <caleb.ethridge@analog.com>
2025-10-15 09:59:37 -0400

    mtd: spi-nor: issi: Fix entry for IS25LP512
    The LP512 requires the same fixups that the LP256 does, but
    the changes were lost on a kernel upgrade. Updating the entry
    fixes the SC594 being unable to probe the QSPI.
    
    Signed-off-by: Caleb Ethridge <caleb.ethridge@analog.com>

--- drivers/mtd/spi-nor/issi.c
+++ drivers/mtd/spi-nor/issi.c
@@ -109,7 +109,9 @@ static const struct flash_info issi_nor_parts[] = {
 		.id = SNOR_ID(0x9d, 0x60, 0x1a),
 		.name = "is25lp512",
 		.size = SZ_64M,
-		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES,
+		.fixups = &is25lp256_fixups,
+		.fixup_flags = SPI_NOR_4B_OPCODES,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
 	}, {
 		.id = SNOR_ID(0x9d, 0x70, 0x16),
 		.name = "is25wp032",
