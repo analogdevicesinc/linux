a542de4451093f310d20e3d65d62ab4f4d38241f
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2025-12-05 15:36:09 +0000

    ARM: move FSR fault status definitions before fsr_fs()
    The FSR's fault status bits depend on whether LPAE is enabled. Rather
    than always exposing both LPAE and non-LPAE to all code, move them
    inside the ifdef blocks dependent on LPAE to restrict their visibility.
    No code other than fsr_fs() makes use of these.
    
    Reviewed-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>

--- arch/arm/mm/fault.h
+++ arch/arm/mm/fault.h
@@ -8,15 +8,14 @@
 #define FSR_LNX_PF		BIT(31)
 #define FSR_CM			BIT(13)
 #define FSR_WRITE		BIT(11)
-#define FSR_FS4			BIT(10)
-#define FSR_FS3_0		GENMASK(3, 0)
-#define FSR_FS5_0		GENMASK(5, 0)
 
 #ifdef CONFIG_ARM_LPAE
 #define FSR_FS_AEA		17
 #define FS_TRANS_NOLL		0x4
 #define FS_PERM_NOLL		0xC
 #define FS_MMU_NOLL_MASK	0x3C
+
+#define FSR_FS5_0		GENMASK(5, 0)
 
 static inline int fsr_fs(unsigned int fsr)
 {
@@ -28,6 +27,9 @@ static inline int fsr_fs(unsigned int fsr)
 #define FS_L2_TRANS		0x7
 #define FS_L1_PERM		0xD
 #define FS_L2_PERM		0xF
+
+#define FSR_FS4			BIT(10)
+#define FSR_FS3_0		GENMASK(3, 0)
 
 static inline int fsr_fs(unsigned int fsr)
 {
