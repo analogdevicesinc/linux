3cd5f8e5745647a2d930d797f2e7cb418111d971
Kim Holdt <kim.holdt@analog.com>
2025-12-12 09:53:09 +0100

    misc: adi-tru: fix spdx header
    Signed-off-by: Kim Holdt <kim.holdt@analog.com>

--- include/linux/adi-tru.h
+++ include/linux/adi-tru.h
@@ -1,8 +1,6 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+/* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * Analog Devices Trigger Routing Unit (TRU) driver
- *
- * Copyright 2022 Analog Devices Inc.
+ * Copyright (c) 2022, Analog Devices Incorporated, All Rights Reserved
  */
 
 #ifndef ADI_TRU_H
