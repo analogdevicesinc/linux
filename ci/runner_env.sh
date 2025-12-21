8e01b16548365e439e84e03deea81c46f3018053
Jorge Marques <jorge.marques@analog.com>
2025-12-21 18:55:09 +0100

    .github: Add macros export|assert_job_summary
    Adds export_job_summary assert_job_summary macros to replace
    export_labels and assert_labels. Uses a single json-formatted variable
    to store job/steps results.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- ci/runner_env.sh
+++ ci/runner_env.sh
@@ -1,9 +1,50 @@
+if [[ "$GITHUB_ACTIONS" == "true" ]]; then
+	export _n='%0A'
+else
+	export _n=$'\n'
+fi
+
 export_labels () {
+	# DEPRECATED: use export_job_summary
 	echo "warn=$(printenv | grep ^step_warn_ | grep -v =$ | tr '\n' ',' | sed 's/,$//')" >> "$GITHUB_OUTPUT"
 	echo "fail=$(printenv | grep ^step_fail_ | grep -v =$ | tr '\n' ',' | sed 's/,$//')" >> "$GITHUB_OUTPUT"
 }
 
+export_job_summary () {
+	json=$(printenv | jq -R -n '
+		[inputs] | map(select(index("="))) | map(split("=") | {(.[0]): .[1]}) | add
+		| to_entries
+		| group_by(
+				if .key | startswith("step_fail_") then
+					"fail"
+				elif .key | startswith("step_warn_") then
+					"warn"
+				else
+					"other"
+				end
+			)
+		| map(
+				if .[0].key | startswith("step_fail_") then
+					{"fail": map({ (.key|sub("^step_fail_";"")): .value }) | add }
+				elif .[0].key | startswith("step_warn_") then
+					{"warn": map({ (.key|sub("^step_warn_";"")): .value }) | add }
+				else
+					empty
+				end
+			)
+		| add
+	')
+	if [[ "$json" == "null" ]]; then json="" ; fi
+
+	echo "summary<<EOF" >> "$GITHUB_OUTPUT"
+	echo "$json" >> $GITHUB_OUTPUT
+	echo "EOF" >> "$GITHUB_OUTPUT"
+
+	export_labels # DEPRECATED
+}
+
 assert_labels () {
+	# DEPRECATED: use assert_job_summary with ${{ needs.*.outputs.summary }}
 	local msg="Errors and warnings are annotated on the changed files, in each job step, as well as aggregated on the Summary page.%0ASome messages may not cause the step to fail, but still need to be checked."
 	warn=$(printenv | grep ^job_warn_ | grep -v =$ | tr '\n' ';'  | \
 	       sed -E -e 's/;/%0A/g' -e 's/\=true(,)?/%0A  /g' -e 's/=/:%0A  /g' -e 's/(job|step)_(fail|warn)_//g')
@@ -22,5 +63,51 @@ assert_labels () {
 	    warn=%0A$(echo $warn | tr '\n' ';' | sed 's/;/%0A/g')
 	    echo "::warning::Some jobs didn't pass non-strict checks:%0A$warn"
 	  fi
+	fi
+}
+
+assert_job_summary () {
+	local msg="Errors and warnings are annotated on the changed files, in each job step, as well as aggregated on the Summary page."$_n"Some messages may not cause the step to fail, but still need to be checked."$_n
+	local fail=""
+	local warn=""
+
+	for job_var_name in $(compgen -A variable job_); do
+		local job_name="${job_var_name#job_}"
+		local json="${!job_var_name}"
+
+		local fail_=$(
+			echo "$json" | jq -r '
+				.fail | to_entries |
+				map(
+					"    step \(.key)"
+				) |
+				.[]
+			' 2>/dev/null
+		)
+		[[ -n "$fail_" ]] && fail+=$_n"  job $job_name:"$_n"$fail_"
+
+		local warn_=$(
+			echo "$json" | jq -r '
+				.warn | to_entries |
+				map(
+					"    step \(.key)"
+				) |
+				.[]
+			' 2>/dev/null
+		)
+		[[ -n "$warn_" ]] && warn+=$_n"  job $job_name:"$_n"$warn_"
+	done
+
+	if [[ -n "$fail" ]]; then
+		if [[ -n "$warn" ]]; then
+			echo "::error::Some jobs didn't pass strict checks:$fail"$_n$_n"And non-strict checks:$warn"$_n$_n"$msg"
+		else
+			echo "::error::Some jobs didn't pass strict checks:$fail"$_n$_n"$msg"
+		fi
+		exit 1
+	else
+		if [[ -n "$warn" ]]; then
+			echo "::warning::Some jobs didn't pass non-strict checks:$warn"$_n$_n"$msg"
+		fi
 	fi
 }
