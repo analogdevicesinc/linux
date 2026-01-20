e5813ee03b67b6e8f574a6f7d0674a4d9b352b2f
Andrew Morton <akpm@linux-foundation.org>
2026-01-20 08:54:09 -0800

    mm-pagewalk-use-min-to-simplify-the-code-fix
    add newline, per Lorenzo
    
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Hongling Zeng <zenghongling@kylinos.cn>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/pagewalk.c
+++ mm/pagewalk.c
@@ -313,6 +313,7 @@ static unsigned long hugetlb_entry_end(struct hstate *h, unsigned long addr,
 				       unsigned long end)
 {
 	unsigned long boundary = (addr & huge_page_mask(h)) + huge_page_size(h);
+
 	return min(boundary, end);
 }
 
