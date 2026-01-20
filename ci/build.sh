analogdevicesinc/linux

commit b29abaf62cac6439b93f2c4873d3a18a39ec22ef

check_checkpatch: Ignore /boot/dts/, reset ignored
If the path matches /boot/dts/, allow macros with complex values
without parenthesis. Also reset ignored value per checkpatch call.

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

ci/build.sh
@@ -95,6 +95,7 @@ check_checkpatch() {
 			--git $commit \
 			$flags )
 
+		ignored=0
 		found=0
 		msg=
 
@@ -134,7 +135,8 @@ check_checkpatch() {
 							# Ignore some cases:
 							prefix=
 							if [[ "$msg" =~ ^"Macros with complex values should be enclosed in parentheses" ]] &&
-							   [[ "$file" =~ "/dt-bindings/" ]]; then
+							   [[ "$file" =~ "/dt-bindings/" ]] ||
+							   [[ "$file" =~ "/boot/dts/" ]]; then
 								prefix=ignored
 								((ignored++))
 							else

commit 1a6b1f4b044b7c9326f2cd3f2aba8ff7b91a64e6

check_checkpatch: Fix if evaluation
Wrong precedence caused errors/warnings to be ignored.
Fixes: b29abaf62cac ("check_checkpatch: Ignore /boot/dts/, reset ignored")

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

ci/build.sh
@@ -134,9 +134,9 @@ check_checkpatch() {
 						else
 							# Ignore some cases:
 							prefix=
-							if [[ "$msg" =~ ^"Macros with complex values should be enclosed in parentheses" ]] &&
-							   [[ "$file" =~ "/dt-bindings/" ]] ||
-							   [[ "$file" =~ "/boot/dts/" ]]; then
+							if [[ "$msg" =~ ^"Macros with complex values should be enclosed in parentheses" &&
+							    ( "$file" =~ "/dt-bindings/" ||
+							      "$file" =~ "/boot/dts/" ) ]]; then
 								prefix=ignored
 								((ignored++))
 							else