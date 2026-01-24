8bd0de4941c625c4725299bdb81b7381bf6a47f6
Andrew Morton <akpm@linux-foundation.org>
2026-01-24 15:20:32 -0800

    mm-add-wq_percpu-to-alloc_workqueue-users-fix-fix
    fix kmem_cache_init_late() properly, per Sebastian
    
    Cc: Marco Crivellari <marco.crivellari@suse.com>
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/slub.c
+++ mm/slub.c
@@ -8542,11 +8542,9 @@ void __init kmem_cache_init(void)
 
 void __init kmem_cache_init_late(void)
 {
-#ifndef CONFIG_SLUB_TINY
 	flushwq = alloc_workqueue("slub_flushwq", WQ_MEM_RECLAIM | WQ_PERCPU,
 				  0);
 	WARN_ON(!flushwq);
-#endif
 }
 
 struct kmem_cache *
