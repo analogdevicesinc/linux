af43a3c3424b302673bef1b912606398f09f82d2
Qasim Ijaz <qasim.ijaz@analog.com>
2026-01-27 11:44:50 +0000

    Add intial cpufreq driver with licensing declarations and driver documentation

---
+++ drivers/cpufreq/adsp-sc589-cpufreq.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Analog Devices Inc. ADSP-SC589 CPUFreq Driver
+ * 
+ * Copyright (C) 2026 Analog Devices Inc.
+ *
+ * Author: Qasim Ijaz <Qasim.Ijaz@analog.com>
+ */
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Qasim Ijaz <Qasim.Ijaz@analog.com>");
+MODULE_DESCRIPTION("CPUFreq driver for Analog Devices ADSP-SC589");
