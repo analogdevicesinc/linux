ddb90c02e7e28f93306404e3338c0dc391426efb
Michael Hennerich <michael.hennerich@analog.com>
2026-01-28 17:30:46 +0100

    jesd204: fsm: Add NULL checks for dev_data to prevent NULL pointer dereference
    During overlay removal, devices are unregistered which sets dev_data to
    NULL. However, the FSM may still iterate over devices in the topology
    and attempt to access dev_data callbacks, causing a NULL pointer
    dereference:
    
      Unable to handle kernel NULL pointer dereference at virtual address 0000000000000008
      pc : jesd204_fsm_run_finished_cb_cb+0x4/0x40
    
    Add NULL checks for jdev->dev_data before accessing its members in:
    - jesd204_fsm_run_finished_cb_cb()
    - jesd204_fsm_table_entry_cb()
    - jesd204_fsm_table_entry_done()
    - jesd204_fsm_table_single()
    - jesd204_init_sysref_cb()
    - jesd204_init_secondary_sysref_cb()
    
    Fixes: c9061c65f590 ("jesd204: add link init state transition")
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    (cherry picked from commit 9b135ea64b38d3b3729eaa7093b9b1513f3ff628)

--- drivers/jesd204/jesd204-fsm.c
+++ drivers/jesd204/jesd204-fsm.c
@@ -975,7 +975,7 @@ static int jesd204_init_secondary_sysref_cb(struct jesd204_dev *jdev,
 	if (!jdev->is_sec_sysref_provider)
 		return 0;
 
-	if (!jdev->dev_data->sysref_cb) {
+	if (!jdev->dev_data || !jdev->dev_data->sysref_cb) {
 		jesd204_err(jdev, "Configured as SYSREF, but no SYSREF cb\n");
 		return -EINVAL;
 	}
@@ -1007,7 +1007,7 @@ static int jesd204_init_sysref_cb(struct jesd204_dev *jdev,
 	if (!jdev->is_sysref_provider)
 		return jesd204_init_secondary_sysref_cb(jdev, jdev_top);
 
-	if (!jdev->dev_data->sysref_cb) {
+	if (!jdev->dev_data || !jdev->dev_data->sysref_cb) {
 		jesd204_err(jdev, "Configured as SYSREF, but no SYSREF cb\n");
 		return -EINVAL;
 	}
@@ -1233,7 +1233,7 @@ static int jesd204_fsm_table_entry_done(struct jesd204_dev *jdev,
 			return ret;
 	}
 
-	if (!fsm_data->rollback) {
+	if (!fsm_data->rollback && jdev->dev_data) {
 		state_op = &jdev->dev_data->state_ops[it->table[0].op];
 		if (state_op->post_state_sysref && jesd204_dev_is_top(jdev))
 			jesd204_sysref_async(jdev);
@@ -1316,6 +1316,9 @@ static int jesd204_fsm_table_single(struct jesd204_dev *jdev,
 	while (!jesd204_fsm_table_end(&it->table[0], rollback, jdev->fsm_rb_to_init)) {
 		it->table = table;
 
+		if (!jdev->dev_data)
+			break;
+
 		state_op = &jdev->dev_data->state_ops[table[0].op];
 
 		data->completed = false;
@@ -1368,7 +1371,7 @@ static int jesd204_fsm_run_finished_cb_cb(struct jesd204_dev *jdev,
 {
 	const struct jesd204_link * const *links = fsm_data->cb_data;
 
-	if (!jdev->dev_data->fsm_finished_cb)
+	if (!jdev->dev_data || !jdev->dev_data->fsm_finished_cb)
 		return JESD204_STATE_CHANGE_DONE;
 
 	jdev->dev_data->fsm_finished_cb(jdev, links,
