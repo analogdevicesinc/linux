6f57293abb8d087de830dd3f02e66d94b3e59973
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2026-02-04 21:21:51 +0100

    sunrpc: Fix compilation error (`make W=1`) when dprintk() is no-op
    Clang compiler is not happy about set but unused variables:
    
    .../flexfilelayout/flexfilelayoutdev.c:56:9: error: variable 'ret' set but not used [-Werror,-Wunused-but-set-variable]
    .../flexfilelayout/flexfilelayout.c:1505:6: error: variable 'err' set but not used [-Werror,-Wunused-but-set-variable]
    .../nfs4proc.c:9244:12: error: variable 'ptr' set but not used [-Werror,-Wunused-but-set-variable]
    
    Fix these by forwarding parameters of dprintk() to no_printk().
    The positive side-effect is a format-string checker enabled even for the cases
    when dprintk() is no-op.
    
    Fixes: d67ae825a59d ("pnfs/flexfiles: Add the FlexFile Layout Driver")
    Fixes: fc931582c260 ("nfs41: create_session operation")
    Acked-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- fs/lockd/svclock.c
+++ fs/lockd/svclock.c
@@ -74,6 +74,11 @@ static const char *nlmdbg_cookie2a(const struct nlm_cookie *cookie)
 
 	return buf;
 }
+#else
+static inline const char *nlmdbg_cookie2a(const struct nlm_cookie *cookie)
+{
+	return "???";
+}
 #endif
 
 /*
