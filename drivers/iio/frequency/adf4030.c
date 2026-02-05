99ae868581687bda2fa8aec18d774be88dc49ef6
Michael Hennerich <michael.hennerich@analog.com>
2026-02-05 12:51:47 +0100

    iio: frequency: adf4030: add ODIV minimum value validation
    The output divider (ODIV) valid range is 10 to 4095 per the datasheet.
    The driver checked only the upper bound. Add the lower bound check
    against ADI_ADF4030_O_DIV_MIN (10) to reject configurations that would
    produce an out-of-range divider value.
    
    Fixes: 54bbd40c03ca ("iio: frequency: support the adf4030 Synchronizer")
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/frequency/adf4030.c
+++ drivers/iio/frequency/adf4030.c
@@ -281,7 +281,7 @@ static int adf4030_compute_odiv(u32 vco_freq, u64 bsync_out_freq_uhz, u32 *odiv)
 		return -EINVAL;
 	}
 
-	if (*odiv > ADI_ADF4030_O_DIV_MAX)
+	if (*odiv < ADI_ADF4030_O_DIV_MIN || *odiv > ADI_ADF4030_O_DIV_MAX)
 		return -EINVAL;
 
 	return 0;
