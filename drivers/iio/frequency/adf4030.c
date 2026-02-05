162fcadca14994ebd67cda839d14ed99e81074e4
Michael Hennerich <michael.hennerich@analog.com>
2026-02-05 12:51:18 +0100

    iio: frequency: adf4030: remove unsupported AVGEXP values from avail list
    The datasheet states that AVGEXP values 0, 1, and 2 are not supported,
    corresponding to averaging counts of 64, 128, and 256. Remove these
    from the available oversampling ratio list so userspace cannot select
    them. The minimum supported value is 512 (AVGEXP=3).
    
    Fixes: 54bbd40c03ca ("iio: frequency: support the adf4030 Synchronizer")
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/frequency/adf4030.c
+++ drivers/iio/frequency/adf4030.c
@@ -1003,9 +1003,9 @@ static int adf4030_reg_access(struct iio_dev *indio_dev, unsigned int reg,
 	return regmap_write(st->regmap, reg, writeval);
 }
 
-static const int adf4030_avg_range[] = {64, 128, 256, 512, 1024, 2048, 4096,
-					8192, 16384, 32768, 65536, 131072,
-					262144, 524288, 1048576, 2097152};
+static const int adf4030_avg_range[] = {512, 1024, 2048, 4096, 8192, 16384,
+					32768, 65536, 131072, 262144, 524288,
+					1048576, 2097152};
 
 static int adf4030_read_avail(struct iio_dev *indio_dev,
 			      struct iio_chan_spec const *chan,
