8358169ebb04a4e60c5730682ea5e8706bca485a
Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
2026-02-05 05:12:10 -0800

    iio: sca3000: make stop_all_interrupts() return void
    sca3000_stop_all_interrupts() is called only from the driver remove
    path and its return value is discarded, so convert the helper to return
    void.
    
    No functional change.
    
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@intel.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/accel/sca3000.c
+++ drivers/iio/accel/sca3000.c
@@ -1427,7 +1427,7 @@ static const struct iio_info sca3000_info = {
 	.write_event_config = &sca3000_write_event_config,
 };
 
-static int sca3000_stop_all_interrupts(struct sca3000_state *st)
+static void sca3000_stop_all_interrupts(struct sca3000_state *st)
 {
 	int ret;
 
@@ -1435,14 +1435,13 @@ static int sca3000_stop_all_interrupts(struct sca3000_state *st)
 	ret = sca3000_read_data_short(st, SCA3000_REG_INT_MASK_ADDR, 1);
 	if (ret)
 		goto error_ret;
-	ret = sca3000_write_reg(st, SCA3000_REG_INT_MASK_ADDR,
-				(st->rx[0] &
-				 ~(SCA3000_REG_INT_MASK_RING_THREE_QUARTER |
-				   SCA3000_REG_INT_MASK_RING_HALF |
-				   SCA3000_REG_INT_MASK_ALL_INTS)));
+	sca3000_write_reg(st, SCA3000_REG_INT_MASK_ADDR,
+			  (st->rx[0] &
+			   ~(SCA3000_REG_INT_MASK_RING_THREE_QUARTER |
+			     SCA3000_REG_INT_MASK_RING_HALF |
+			     SCA3000_REG_INT_MASK_ALL_INTS)));
 error_ret:
 	mutex_unlock(&st->lock);
-	return ret;
 }
 
 static int sca3000_probe(struct spi_device *spi)
