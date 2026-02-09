ea8e6bb2d0ab5426a5de59699eedfac6ea9b0a63
Jorge Marques <jorge.marques@analog.com>
2026-02-09 11:32:17 +0100

    Containerfile: Install sparse from source
    Last release is 4 years old, checkout master at
    fbdde3127b83e6d09e0ba808d7925dd84407f3c6 and compile.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- container/install-extra.sh
+++ container/install-extra.sh
@@ -9,14 +9,27 @@ zypper install -y --no-recommends \
 
 # linux checks
 zypper install -y --no-recommends \
-	sparse coccinelle ocaml ocaml-findlib cppcheck \
+	coccinelle ocaml ocaml-findlib cppcheck \
 	sqlite3-devel
 
+zypper install -y --no-recommends \
+	llvm19-devel
+git clone https://git.kernel.org/pub/scm/devel/sparse/sparse.git sparse --depth=50
+pushd sparse
+git switch -d fbdde3127b83e6d09e0ba808d7925dd84407f3c6
+make -j$(nproc)
+
+make install PREFIX=/usr/local install
+popd
+zypper remove -y --clean-deps llvm19-devel
+
 git clone https://repo.or.cz/smatch.git smatch --depth=1
-cd smatch
+pushd smatch
 make -j$(nproc)
 
 ln -s $(realpath smatch) /usr/local/bin/smatch
+
+popd
 
 # user
 zypper install -y --no-recommends \
