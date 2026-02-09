94c125bafa00042daf6d63b4fdd78384abc121fc
Igor Pylypiv <ipylypiv@google.com>
2026-02-09 13:21:51 -0800

    scsi: core: Add 'serial' sysfs attribute for SCSI/SATA
    Add a 'serial' sysfs attribute for SCSI and SATA devices. This attribute
    exposes the Unit Serial Number, which is derived from the Device
    Identification Vital Product Data (VPD) page 0x80.
    
    Whitespace is stripped from the retrieved serial number to handle the
    different alignment (right-aligned for SCSI, potentially left-aligned for
    SATA). As noted in SAT-5 10.5.3, "Although SPC-5 defines the PRODUCT SERIAL
    NUMBER field as right-aligned, ACS-5 does not require its SERIAL NUMBER
    field to be right-aligned. Therefore, right-alignment of the PRODUCT SERIAL
    NUMBER field for the translation is not assured."
    
    This attribute is used by tools such as lsblk to display the serial number
    of block devices.
    
    [mkp: length adjustment]
    
    Signed-off-by: Igor Pylypiv <ipylypiv@google.com>
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Link: https://patch.msgid.link/20260209212151.342151-1-ipylypiv@google.com
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- include/scsi/scsi_device.h
+++ include/scsi/scsi_device.h
@@ -571,6 +571,7 @@ void scsi_put_internal_cmd(struct scsi_cmnd *scmd);
 extern void sdev_disable_disk_events(struct scsi_device *sdev);
 extern void sdev_enable_disk_events(struct scsi_device *sdev);
 extern int scsi_vpd_lun_id(struct scsi_device *, char *, size_t);
+extern int scsi_vpd_lun_serial(struct scsi_device *, char *, size_t);
 extern int scsi_vpd_tpg_id(struct scsi_device *, int *);
 
 #ifdef CONFIG_PM
