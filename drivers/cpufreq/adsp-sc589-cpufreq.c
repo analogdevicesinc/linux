7564e0893e2e5785aae79f4b3fa5f5237000ab5b
Qasim Ijaz <Qasim.Ijaz@analog.com>
2026-02-10 16:54:42 +0000

    Add cgu device tree mapping function
    Add a function to find device tree node associated with adi,sc58x-clocks compatible
    property and map the cgu0 control register via of_iomap.
    
    Signed-off-by: Qasim Ijaz <Qasim.Ijaz@analog.com>

--- drivers/cpufreq/adsp-sc589-cpufreq.c
+++ drivers/cpufreq/adsp-sc589-cpufreq.c
@@ -10,10 +10,17 @@
 #include <linux/module.h>
 #include <linux/cpufreq.h>
 #include <linux/init.h>
+#include <linux/of_address.h>
 
 #define CGU0_DIV_REG 0x3108D00C
 #define GCU0_CONTROL_REG 0x3108D000
 #define CGU0_STAT_REG 0x3108D008
+
+#define CGU0_CLKSALGN BIT(3) 
+#define CGU0_CSEL_MASK 0x1F
+#define CGU0_UPDT BIT(10)
+
+void __iomem *cgu0_ctl;
 
 static struct cpufreq_frequency_table sc589_freq_table[] = {
 	{ .driver_data = 0, .frequency = 450000 },
@@ -38,6 +45,37 @@ static unsigned int sc589_get(unsigned int cpu)
 	return 450000;
 }
 
+static int sc589_wait_clock_align(void)
+{
+	
+	return -ETIMEDOUT;		
+}
+
+/*static int sc589_set_divider(u32 div)
+{
+	
+	return 0;
+}*/
+
+static int map_cgu_from_dt(void)
+{
+	struct device_node *dn = of_find_compatible_node(NULL, NULL, "adi,sc58x-clocks");
+	if (!dn) {
+		pr_err("failed to find clock node in device tree.\n");
+		return -ENODEV;
+	}
+
+	cgu0_ctl = of_iomap(dn, 0);
+	of_node_put(dn);
+
+	if (!cgu0_ctl) {
+		pr_err("failed to map CGU0 control register.\n");
+		return -ENOMEM;
+	}
+
+	return 0;
+}
+
 static struct cpufreq_driver sc589_cpufreq_driver = {
 	.name = "sc589-cpufreq",
 	.init = sc589_init,
@@ -49,6 +87,7 @@ static struct cpufreq_driver sc589_cpufreq_driver = {
 static int __init sc589_cpufreq_init(void)
 {
 	pr_info("sc589-cpufreq loading...\n");
+	map_cgu_from_dt();
 	return cpufreq_register_driver(&sc589_cpufreq_driver);
 }
 module_init(sc589_cpufreq_init);
