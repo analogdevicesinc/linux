41fc3006fd30c08ac01e2330c5d326b8f22f5e09
Qasim Ijaz <Qasim.Ijaz@analog.com>
2026-02-16 14:24:42 +0000

    Clean up macros and call set_divider function
    Signed-off-by: Qasim Ijaz <Qasim.Ijaz@analog.com>

--- drivers/cpufreq/adsp-sc589-cpufreq.c
+++ drivers/cpufreq/adsp-sc589-cpufreq.c
@@ -13,17 +13,19 @@
 #include <linux/of_address.h>
 #include <linux/iopoll.h>
 
-#define CGU0_STAT_OFFSET 0x08
-#define CGU0_DIV_OFFSET	 0x0C
+#define STAT_POLL_SLEEP    10
+#define STAT_POLL_TIMEOUT  10000 
 
-#define CGU0_CLKSALGN	BIT(3) 
-#define CGU0_CSEL_MASK	0x1F
-#define CGU0_UPDT	BIT(30)
+#define CGU0_STAT_OFFSET   0x08
+#define CGU0_DIV_OFFSET	   0x0C
+#define CGU0_CSEL_MASK	   0x1F
 
-#define STAT_POLL_SLEEP   10
-#define STAT_POLL_TIMEOUT 10000 
+#define CGU0_CLKSALGN	   BIT(3) 
+#define CGU0_UPDT	   BIT(30)
 
-void __iomem *cgu0_ctl;
+#define TRANSITION_LATENCY 50000 /* nanoseconds, TODO: refine with timing/testing */
+
+static void __iomem *cgu0_ctl;
 
 static struct cpufreq_frequency_table sc589_freq_table[] = {
 	{ .driver_data = 0, .frequency = 450000 },
@@ -33,6 +35,14 @@ static struct cpufreq_frequency_table sc589_freq_table[] = {
 
 static int sc589_init(struct cpufreq_policy *policy)
 {
+       	//policy->policy = 
+	//policy->governors = 	
+	//policy->cpus = 
+	policy->cpuinfo.transition_latency = TRANSITION_LATENCY;
+	//policy->cpuinfo.min_freq =  
+	//policy->cpuinfo.max_freq = 
+	//policy->min = 
+       	//policy->max = 
 	policy->freq_table = sc589_freq_table;
 	policy->cur = 450000;
 	return 0;
@@ -107,6 +117,7 @@ static int __init sc589_cpufreq_init(void)
 {
 	pr_info("sc589-cpufreq: loading...\n");
 	map_cgu_from_dt();
+	sc589_set_divider(8);
 	return cpufreq_register_driver(&sc589_cpufreq_driver);
 }
 module_init(sc589_cpufreq_init);
