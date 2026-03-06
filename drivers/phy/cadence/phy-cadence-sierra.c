955e68eb3067842067b7b5395a3f8c278428802a
Gregory CLEMENT <gregory.clement@bootlin.com>
2026-03-06 11:23:58 +0100

    phy: cadence: Sierra: Do not modify register when getting parent clock
    The get_parent() callback for the PLL_CMNLC1 clock was incorrectly
    writing to the register while determining the parent clock index. This
    unintended register access forces the PHY back into training mode. If
    the PHY is already configured, this unexpected change prevents it from
    exiting training mode.
    
    Remove the register write operation to ensure the PHY remains stable
    during the get_parent() callback.
    
    Fixes: da08aab940092 ("phy: cadence: Sierra: Fix to get correct parent for mux clocks")
    Signed-off-by: Gregory CLEMENT <gregory.clement@bootlin.com>
    Reviewed-by: Manivannan Sadhasivam <manivannan.sadhasivam@oss.qualcomm.com>
    Reviewed-by: Neil Armstrong <neil.armstrong@linaro.org>
    Link: https://patch.msgid.link/20260306-fix_sierra_get_parent-v2-1-cafe89ee5382@bootlin.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/phy/cadence/phy-cadence-sierra.c
+++ drivers/phy/cadence/phy-cadence-sierra.c
@@ -698,23 +698,16 @@ static const struct phy_ops noop_ops = {
 static u8 cdns_sierra_pll_mux_get_parent(struct clk_hw *hw)
 {
 	struct cdns_sierra_pll_mux *mux = to_cdns_sierra_pll_mux(hw);
-	struct regmap_field *plllc1en_field = mux->plllc1en_field;
-	struct regmap_field *termen_field = mux->termen_field;
 	struct regmap_field *field = mux->pfdclk_sel_preg;
 	unsigned int val;
 	int index;
 
 	regmap_field_read(field, &val);
 
-	if (strstr(clk_hw_get_name(hw), clk_names[CDNS_SIERRA_PLL_CMNLC1])) {
+	if (strstr(clk_hw_get_name(hw), clk_names[CDNS_SIERRA_PLL_CMNLC1]))
 		index = clk_mux_val_to_index(hw, cdns_sierra_pll_mux_table[CMN_PLLLC1], 0, val);
-		if (index == 1) {
-			regmap_field_write(plllc1en_field, 1);
-			regmap_field_write(termen_field, 1);
-		}
-	} else {
+	else
 		index = clk_mux_val_to_index(hw, cdns_sierra_pll_mux_table[CMN_PLLLC], 0, val);
-	}
 
 	return index;
 }
