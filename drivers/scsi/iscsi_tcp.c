5b44c3757ca36e49959cf6819b77d7079de75705
Kexin Sun <kexinsun@smail.nju.edu.cn>
2026-03-21 18:59:04 +0800

    scsi: iscsi_tcp: update outdated comment for renamed iscsi_conn_set_callbacks()
    The function iscsi_conn_set_callbacks() was renamed to
    iscsi_sw_tcp_conn_set_callbacks() by commit 38e1a8f5479d ("[SCSI]
    iscsi_tcp: hook iscsi_tcp into new libiscsi_tcp module").  Update the
    stale reference in iscsi_sw_tcp_conn_restore_callbacks().
    
    Assisted-by: unnamed:deepseek-v3.2 coccinelle
    Signed-off-by: Kexin Sun <kexinsun@smail.nju.edu.cn>
    Link: https://patch.msgid.link/20260321105904.7726-1-kexinsun@smail.nju.edu.cn
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/iscsi_tcp.c
+++ drivers/scsi/iscsi_tcp.c
@@ -267,7 +267,7 @@ iscsi_sw_tcp_conn_restore_callbacks(struct iscsi_conn *conn)
 	struct iscsi_sw_tcp_conn *tcp_sw_conn = tcp_conn->dd_data;
 	struct sock *sk = tcp_sw_conn->sock->sk;
 
-	/* restore socket callbacks, see also: iscsi_conn_set_callbacks() */
+	/* restore socket callbacks, see also: iscsi_sw_tcp_conn_set_callbacks() */
 	write_lock_bh(&sk->sk_callback_lock);
 	sk->sk_user_data    = NULL;
 	sk->sk_data_ready   = tcp_sw_conn->old_data_ready;
