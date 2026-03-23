8abf158b84ca34d5a37566b8d9a641f8a06f7eae
Neel Bullywon <neelb2403@gmail.com>
2026-03-23 19:33:16 -0400

    iio: frequency: adf4350: replace TODO with NOTE in adf4350_set_freq()
    Replace the TODO comment in adf4350_set_freq() with a NOTE explaining
    that a constant-time approach using fls_long() was attempted but deemed
    more complex without meaningful benefit for initialization code.
    
    Signed-off-by: Neel Bullywon <neelb2403@gmail.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@intel.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/frequency/adf4350.c
+++ drivers/iio/frequency/adf4350.c
@@ -152,10 +152,10 @@ static int adf4350_set_freq(struct adf4350_state *st, unsigned long long freq)
 	st->r4_rf_div_sel = 0;
 
 	/*
-	 * !\TODO: The below computation is making sure we get a power of 2
-	 * shift (st->r4_rf_div_sel) so that freq becomes higher or equal to
-	 * ADF4350_MIN_VCO_FREQ. This might be simplified with fls()/fls_long()
-	 * and friends.
+	 * NOTE: This iteratively shifts freq by a power of 2
+	 * (st->r4_rf_div_sel) to meet or exceed ADF4350_MIN_VCO_FREQ.
+	 * A constant-time approach using fls_long() was attempted but
+	 * deemed more complex without meaningful benefit for init code.
 	 */
 	while (freq < ADF4350_MIN_VCO_FREQ) {
 		freq <<= 1;
