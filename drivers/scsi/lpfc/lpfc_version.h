7f1e2c1cce1cad097d14f384c2461c1ff6cac0d0
Justin Tee <justin.tee@broadcom.com>
2026-03-31 13:59:28 -0700

    scsi: lpfc: Update lpfc version to 15.0.0.0
    Update lpfc version to 15.0.0.0
    
    Signed-off-by: Justin Tee <justin.tee@broadcom.com>
    Link: https://patch.msgid.link/20260331205928.119833-11-justintee8345@gmail.com
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/lpfc/lpfc_version.h
+++ drivers/scsi/lpfc/lpfc_version.h
@@ -20,7 +20,7 @@
  * included with this package.                                     *
  *******************************************************************/
 
-#define LPFC_DRIVER_VERSION "14.4.0.14"
+#define LPFC_DRIVER_VERSION "15.0.0.0"
 #define LPFC_DRIVER_NAME		"lpfc"
 
 /* Used for SLI 2/3 */
