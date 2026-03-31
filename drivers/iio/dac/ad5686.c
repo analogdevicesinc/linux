720f236b1ca77789d3a8d8facbce76b7b0873b9c
Rodrigo Alencar <rodrigo.alencar@analog.com>
2026-03-31 15:15:47 +0100

    iio: dac: ad5686: do not ignore errors in devm_pwm_get()
    Since PWM is an optional feature, any other error (notably -EPROBE_DEFER
    if the PWM controller hasn't probed yet) falls through to dev_err_probe(),
    Otherwise, NULL is assigned to the pwm pointer:
    - ENOENT: No pwms property available, as it is intentionally not
      configured.
    - ENODEV: kernel build does not have CONFIG_PWM support.
    Also, "pwm-trigger" name is dropped, as there is no other pwm to consider.
    
    Signed-off-by: Rodrigo Alencar <rodrigo.alencar@analog.com>

--- drivers/iio/dac/ad5686.c
+++ drivers/iio/dac/ad5686.c
@@ -579,8 +579,15 @@ int ad5686_probe(struct device *dev,
 	st->vref_mv = has_external_vref ? ret / 1000 : st->chip_info->int_vref_mv;
 
 	/* PWM configuration */
-	st->pwm = devm_pwm_get(dev, "pwm-trigger");
-	if (!IS_ERR(st->pwm)) {
+	st->pwm = devm_pwm_get(dev, NULL);
+	if (IS_ERR(st->pwm)) {
+		ret = PTR_ERR(st->pwm);
+		if (ret != -ENOENT && ret != -ENODEV)
+			return dev_err_probe(dev, ret, "failed to get pwm\n");
+		st->pwm = NULL;
+	}
+
+	if (st->pwm) {
 		/* Set a default pwm frequency of 1kHz and 50% duty cycle */
 		pwm_init_state(st->pwm, &state);
 		state.enabled = false;
@@ -589,8 +596,6 @@ int ad5686_probe(struct device *dev,
 		ret = pwm_apply_might_sleep(st->pwm, &state);
 		if (ret < 0)
 			return ret;
-	} else {
-		st->pwm = NULL;
 	}
 
 	/* Configure IRQ */
