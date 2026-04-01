5b66890a0c2154093b1c7e299c9533f253b4ace5
Jorge Marques <jorge.marques@analog.com>
2026-04-01 13:04:29 +0200

    Containerfile: normalize shell calling
    Call as 'RUN bash ./script.sh && rm ./script.sh'.
    To avoid continue on error and permission issue.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- container/set-alternatives.sh
+++ container/set-alternatives.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 50
 update-alternatives --install /usr/bin/cpp cpp /usr/bin/cpp-13 50
 update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 50
