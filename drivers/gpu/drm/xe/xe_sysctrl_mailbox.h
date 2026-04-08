45334d5c364498629b314391c7d1eac0b64c882f
Anoop Vijay <anoop.c.vijay@intel.com>
2026-04-08 04:07:59 -0700

    drm/xe/xe_sysctrl: Drop redundant endian conversions in mailbox header macros
    Remove unnecessary le32_to_cpu() conversions from XE_SYSCTRL_APP_HDR_*
    macros.
    
    Fixes: 37ace5254a2b ("drm/xe/xe_sysctrl: Add ABI and mailbox interface headers")
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202604010228.Z20DhK4g-lkp@intel.com/
    Signed-off-by: Anoop Vijay <anoop.c.vijay@intel.com>
    Signed-off-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
    Reviewed-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
    Reviewed-by: Riana Tauro <riana.tauro@intel.com>
    Link: https://patch.msgid.link/20260408110759.1407342-2-anoop.c.vijay@intel.com

--- drivers/gpu/drm/xe/xe_sysctrl_mailbox.h
+++ drivers/gpu/drm/xe/xe_sysctrl_mailbox.h
@@ -15,13 +15,13 @@ struct xe_sysctrl;
 struct xe_sysctrl_mailbox_command;
 
 #define XE_SYSCTRL_APP_HDR_GROUP_ID(hdr) \
-	FIELD_GET(APP_HDR_GROUP_ID_MASK, le32_to_cpu((hdr)->data))
+	FIELD_GET(APP_HDR_GROUP_ID_MASK, (hdr)->data)
 
 #define XE_SYSCTRL_APP_HDR_COMMAND(hdr) \
-	FIELD_GET(APP_HDR_COMMAND_MASK, le32_to_cpu((hdr)->data))
+	FIELD_GET(APP_HDR_COMMAND_MASK, (hdr)->data)
 
 #define XE_SYSCTRL_APP_HDR_VERSION(hdr) \
-	FIELD_GET(APP_HDR_VERSION_MASK, le32_to_cpu((hdr)->data))
+	FIELD_GET(APP_HDR_VERSION_MASK, (hdr)->data)
 
 void xe_sysctrl_mailbox_init(struct xe_sysctrl *sc);
 int xe_sysctrl_send_command(struct xe_sysctrl *sc,
