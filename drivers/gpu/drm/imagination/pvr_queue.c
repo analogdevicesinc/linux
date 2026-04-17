f84d73d2a08498174d950ba5935930dd94df7d3c
Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
2026-04-17 11:37:35 +0100

    drm/imagination: Remove drm_sched_init_args->num_rqs usage
    Remove member no longer used by the scheduler core.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
    Cc: Frank Binns <frank.binns@imgtec.com>
    Cc: Matt Coster <matt.coster@imgtec.com>
    Cc: dri-devel@lists.freedesktop.org
    Reviewed-by: Matt Coster <matt.coster@imgtec.com>
    Signed-off-by: Philipp Stanner <phasta@kernel.org>
    Link: https://patch.msgid.link/20260417103744.76020-21-tvrtko.ursulin@igalia.com

--- drivers/gpu/drm/imagination/pvr_queue.c
+++ drivers/gpu/drm/imagination/pvr_queue.c
@@ -1272,7 +1272,6 @@ struct pvr_queue *pvr_queue_create(struct pvr_context *ctx,
 	const struct drm_sched_init_args sched_args = {
 		.ops = &pvr_queue_sched_ops,
 		.submit_wq = pvr_dev->sched_wq,
-		.num_rqs = 1,
 		.credit_limit = 64 * 1024,
 		.hang_limit = 1,
 		.timeout = msecs_to_jiffies(500),
