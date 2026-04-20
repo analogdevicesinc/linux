6e8bc03349fe4f09567fa76235abf52bdaf83082
Erni Sri Satya Vennela <ernis@linux.microsoft.com>
2026-04-20 05:47:36 -0700

    net: mana: Init gf_stats_work before potential error paths in probe
    Move INIT_DELAYED_WORK(gf_stats_work) to before mana_create_eq(),
    while keeping schedule_delayed_work() at its original location.
    
    Previously, if any function between mana_create_eq() and the
    INIT_DELAYED_WORK call failed, mana_probe() would call mana_remove()
    which unconditionally calls cancel_delayed_work_sync(gf_stats_work)
    in __flush_work() or debug object warnings with
    CONFIG_DEBUG_OBJECTS_WORK enabled.
    
    Fixes: be4f1d67ec56 ("net: mana: Add standard counter rx_missed_errors")
    Signed-off-by: Erni Sri Satya Vennela <ernis@linux.microsoft.com>
    Link: https://patch.msgid.link/20260420124741.1056179-3-ernis@linux.microsoft.com
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- drivers/net/ethernet/microsoft/mana/mana_en.c
+++ drivers/net/ethernet/microsoft/mana/mana_en.c
@@ -3635,6 +3635,8 @@ int mana_probe(struct gdma_dev *gd, bool resuming)
 		INIT_WORK(&ac->link_change_work, mana_link_state_handle);
 	}
 
+	INIT_DELAYED_WORK(&ac->gf_stats_work, mana_gf_stats_work_handler);
+
 	err = mana_create_eq(ac);
 	if (err) {
 		dev_err(dev, "Failed to create EQs: %d\n", err);
@@ -3709,7 +3711,6 @@ int mana_probe(struct gdma_dev *gd, bool resuming)
 
 	err = add_adev(gd, "eth");
 
-	INIT_DELAYED_WORK(&ac->gf_stats_work, mana_gf_stats_work_handler);
 	schedule_delayed_work(&ac->gf_stats_work, MANA_GF_STATS_PERIOD);
 
 out:
