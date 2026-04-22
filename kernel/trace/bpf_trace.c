439ebd5b5708f236f7a4a9784194f7ecb77cd814
Mykyta Yatsenko <yatsenko@meta.com>
2026-04-22 12:41:06 -0700

    bpf: Add sleepable support for raw tracepoint programs
    Rework __bpf_trace_run() to support sleepable BPF programs by using
    explicit RCU flavor selection, following the uprobe_prog_run() pattern.
    
    For sleepable programs, use rcu_read_lock_tasks_trace() for lifetime
    protection with migrate_disable(). For non-sleepable programs, use the
    regular rcu_read_lock_dont_migrate().
    
    Remove the preempt_disable_notrace/preempt_enable_notrace pair from
    the faultable tracepoint BPF probe wrapper in bpf_probe.h, since
    migration protection and RCU locking are now handled per-program
    inside __bpf_trace_run().
    
    Adapt bpf_prog_test_run_raw_tp() for sleepable programs: reject
    BPF_F_TEST_RUN_ON_CPU since sleepable programs cannot run in hardirq
    or preempt-disabled context, and call __bpf_prog_test_run_raw_tp()
    directly instead of via smp_call_function_single(). Rework
    __bpf_prog_test_run_raw_tp() to select RCU flavor per-program and
    add per-program recursion context guard for private stack safety.
    
    Signed-off-by: Mykyta Yatsenko <yatsenko@meta.com>
    Acked-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Link: https://lore.kernel.org/bpf/20260422-sleepable_tracepoints-v13-1-99005dff21ef@meta.com
    Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -2072,11 +2072,19 @@ void bpf_put_raw_tracepoint(struct bpf_raw_event_map *btp)
 static __always_inline
 void __bpf_trace_run(struct bpf_raw_tp_link *link, u64 *args)
 {
+	struct srcu_ctr __percpu *scp = NULL;
 	struct bpf_prog *prog = link->link.prog;
+	bool sleepable = prog->sleepable;
 	struct bpf_run_ctx *old_run_ctx;
 	struct bpf_trace_run_ctx run_ctx;
 
-	rcu_read_lock_dont_migrate();
+	if (sleepable) {
+		scp = rcu_read_lock_tasks_trace();
+		migrate_disable();
+	} else {
+		rcu_read_lock_dont_migrate();
+	}
+
 	if (unlikely(!bpf_prog_get_recursion_context(prog))) {
 		bpf_prog_inc_misses_counter(prog);
 		goto out;
@@ -2085,12 +2093,18 @@ void __bpf_trace_run(struct bpf_raw_tp_link *link, u64 *args)
 	run_ctx.bpf_cookie = link->cookie;
 	old_run_ctx = bpf_set_run_ctx(&run_ctx.run_ctx);
 
-	(void) bpf_prog_run(prog, args);
+	(void)bpf_prog_run(prog, args);
 
 	bpf_reset_run_ctx(old_run_ctx);
 out:
 	bpf_prog_put_recursion_context(prog);
-	rcu_read_unlock_migrate();
+
+	if (sleepable) {
+		migrate_enable();
+		rcu_read_unlock_tasks_trace(scp);
+	} else {
+		rcu_read_unlock_migrate();
+	}
 }
 
 #define UNPACK(...)			__VA_ARGS__
