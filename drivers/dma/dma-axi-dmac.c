5945eccdbf9db662902f22c775b98d70f85df7dd
Nuno Sá <nuno.sa@analog.com>
2026-04-24 18:40:16 +0100

    dmaengine: dma-axi-dmac: Drop struct clk from main struct
    There's no reason to keep struct clk in struct axi_dmac. Hence, use a
    local clk variable in .probe() and drop it from struct axi_dmac.
    
    Signed-off-by: Nuno Sá <nuno.sa@analog.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Link: https://patch.msgid.link/20260424-dma-dmac-handle-vunmap-v4-3-90f43412fdc0@analog.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/dma-axi-dmac.c
+++ drivers/dma/dma-axi-dmac.c
@@ -175,8 +175,6 @@ struct axi_dmac {
 	void __iomem *base;
 	int irq;
 
-	struct clk *clk;
-
 	struct dma_device dma_dev;
 	struct axi_dmac_chan chan;
 };
@@ -1245,6 +1243,7 @@ static int axi_dmac_probe(struct platform_device *pdev)
 {
 	struct dma_device *dma_dev;
 	struct axi_dmac *dmac;
+	struct clk *clk;
 	struct regmap *regmap;
 	unsigned int version;
 	u32 irq_mask = 0;
@@ -1264,9 +1263,9 @@ static int axi_dmac_probe(struct platform_device *pdev)
 	if (IS_ERR(dmac->base))
 		return PTR_ERR(dmac->base);
 
-	dmac->clk = devm_clk_get_enabled(&pdev->dev, NULL);
-	if (IS_ERR(dmac->clk))
-		return PTR_ERR(dmac->clk);
+	clk = devm_clk_get_enabled(&pdev->dev, NULL);
+	if (IS_ERR(clk))
+		return PTR_ERR(clk);
 
 	version = axi_dmac_read(dmac, ADI_AXI_REG_VERSION);
 
