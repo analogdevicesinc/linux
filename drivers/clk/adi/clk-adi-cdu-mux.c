badcd288043bf4ae61202f682ce06bf6c6013d7a
Qasim Ijaz <Qasim.Ijaz@analog.com>
2026-04-27 09:04:33 +0100

    inital work

---
+++ drivers/clk/adi/clk-adi-cdu-mux.c
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * CDU mux clock driver for ADSP-SC5xx processors
+ *
+ * Copyright (C) 2026 Analog Devices Inc.
+ */
+
+#include <linux/clk-provider.h>
+#include <linux/iopoll.h>
+#include <linux/slab.h>
+
+#define CDU_CFG_SEL_NUM_INPUTS  4 /* selectable inputs per CDU_CFG[n] */
+
+#define CDU_CFG_EN		BIT(0)
+#define CDU_CFG_LOCK            BIT(31)
+#define CDU_CFG_SEL_MASK	GENMASK(2, 1)
+#define CDU_CFG_SEL_SHIFT 	1
+
+#define CDU_CFG_POLL_DELAY_US   1
+#define CDU_CFG_POLL_TIMEOUT_US 100
+
+struct clk_adi_cdu_mux {
+	struct clk_hw hw;
+	void __iomem *cdu_cfg;
+	void __iomem *cdu_stat;
+	spinlock_t *cdu_lock;
+	u8 clko_stat_bit;
+};
+
+static inline struct clk_adi_cdu_mux *to_clk_adi_cdu_mux(struct clk_hw *hw)
+{
+	return container_of(hw, struct clk_adi_cdu_mux, hw);
+}
+
+static int sc5xx_cdu_wait_ready(struct clk_adi_cdu_mux *cdu_mux)
+{
+	u32 stat;
+
+	return readl_poll_timeout_atomic(cdu_mux->cdu_stat, stat,
+					 !(stat & BIT(cdu_mux->clko_stat_bit)),
+					 CDU_CFG_POLL_DELAY_US,
+					 CDU_CFG_POLL_TIMEOUT_US);
+}
+
+static int sc5xx_cdu_set_parent(struct clk_hw *hw, u8 index)
+{
+	struct clk_adi_cdu_mux *cdu_mux = to_clk_adi_cdu_mux(hw);
+
+	
+}
+
+static u8 sc5xx_cdu_get_parent(struct clk_hw *hw) 
+{
+}
+
+static int sc5xx_cdu_enable(struct clk_hw *hw)
+{
+}
+
+static void sc5xx_cdu_disable(struct clk_hw *hw)
+{
+}
+
+static int sc5xx_cdu_is_enabled(struct clk_hw *hw)
+{
+}
+
+static const struct clk_ops adi_cdu_mux_ops = {
+	.set_parent = sc5xx_cdu_set_parent,
+	.get_parent = sc5xx_cdu_get_parent,
+	.enable     = sc5xx_cdu_enable,
+	.disable    = sc5xx_cdu_disable,
+	.is_enabled = sc5xx_cdu_is_enabled,
+};
+
+struct clk *sc5xx_adi_cdu_mux(const char *clock_name, void __iomem *cdu_cfg,
+			      const char * const *parent_names,
+			      unsigned long clock_flags, spinlock_t *cdu_lock)
+{
+	struct clk_adi_cdu_mux *cdu_mux;
+	struct clk_init_data init = {};
+	struct clk *clk;
+
+	cdu_mux = kzalloc(sizeof(*cdu_mux), GFP_KERNEL);
+	if (!cdu_mux)
+		return ERR_PTR(-ENOMEM);
+
+	init.name = clock_name;
+	init.ops = &adi_cdu_mux_ops;
+	init.parent_names = parent_names;
+	init.num_parents = CDU_CFG_SEL_NUM_INPUTS;
+	init.flags = clock_flags;
+
+	cdu_mux->hw.init = &init;
+	cdu_mux->cdu_cfg = cdu_cfg;
+	cdu_mux->cdu_lock = cdu_lock;
+
+	clk = clk_register(NULL, &cdu_mux->hw);
+	if (IS_ERR(clk)) {
+		kfree(cdu_mux);
+		return clk;
+	}
+
+	return clk;
+}
+
+MODULE_AUTHOR("Qasim Ijaz <qasim.ijaz@analog.com>");
+MODULE_DESCRIPTION("Analog Devices Inc ADSP-SC5XX Clock Distribution Unit (CDU) driver");
+MODULE_LICENSE("GPL v2");
