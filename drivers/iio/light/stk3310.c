867b0c10aff4b6805b428dea4d58b2055f571ebf
Rafael G. Dias <rafael.guimaraes.dias@usp.br>
2026-04-28 13:13:38 -0300

    iio: light: stk3310: Sort headers alphabetically
    Sort the included headers alphabetically and group the <linux/iio/*>
    headers separately from the generic <linux/*> headers.
    
    Co-developed-by: Felipe Khoury Dayoub <felipedayoub@usp.br>
    Signed-off-by: Felipe Khoury Dayoub <felipedayoub@usp.br>
    Signed-off-by: Rafael G. Dias <rafael.guimaraes.dias@usp.br>
    Reviewed-by: Joshua Crofts <joshua.crofts1@gmail.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/light/stk3310.c
+++ drivers/iio/light/stk3310.c
@@ -10,9 +10,10 @@
 #include <linux/i2c.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
-#include <linux/module.h>
 #include <linux/mod_devicetable.h>
+#include <linux/module.h>
 #include <linux/regmap.h>
+
 #include <linux/iio/events.h>
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
