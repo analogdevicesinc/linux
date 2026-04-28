d7415b3bbc5f30658b208f95f8c6f9d58e66858e
Rafael G. Dias <rafael.guimaraes.dias@usp.br>
2026-04-28 13:13:39 -0300

    iio: light: stk3310: Update includes to match IWYU
    Clean up the included headers in stk3310.c according to the
    Include-What-You-Use (IWYU) tool. Remove the generic <linux/kernel.h>
    header and add explicit dependencies to improve compilation accuracy.
    
    Co-developed-by: Felipe Khoury Dayoub <felipedayoub@usp.br>
    Signed-off-by: Felipe Khoury Dayoub <felipedayoub@usp.br>
    Signed-off-by: Rafael G. Dias <rafael.guimaraes.dias@usp.br>
    Reviewed-by: Joshua Crofts <joshua.crofts1@gmail.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/light/stk3310.c
+++ drivers/iio/light/stk3310.c
@@ -7,16 +7,28 @@
  * IIO driver for STK3310/STK3311. 7-bit I2C address: 0x48.
  */
 
+#include <linux/array_size.h>
+#include <linux/bits.h>
+#include <linux/dev_printk.h>
+#include <linux/err.h>
 #include <linux/i2c.h>
 #include <linux/interrupt.h>
-#include <linux/kernel.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/pm.h>
+#include <linux/property.h>
 #include <linux/regmap.h>
+#include <linux/sprintf.h>
+#include <linux/sysfs.h>
+#include <linux/types.h>
 
 #include <linux/iio/events.h>
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
+#include <linux/iio/types.h>
+
+#include <asm/byteorder.h>
 
 #define STK3310_REG_STATE			0x00
 #define STK3310_REG_PSCTRL			0x01
