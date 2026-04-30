2408f8b9e87c0b3527a6497b82041dba880d62b6
Geert Uytterhoeven <geert+renesas@glider.be>
2026-04-30 17:33:14 +0200

    pinctrl: ingenic: Fix type in .pin_config_group_get() callback
    On 64-bit platforms, "unsigned long" is 64-bit.  Hence checking if all
    "unsigned long" configuration values are equal should be done using an
    "unsigned long" temporary.
    
    While Ingenic is a 32-bit platform, it is still better to use the
    correct type, to serve as an example.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Acked-by: Paul Cercueil <paul@crapouillou.net>
    Signed-off-by: Linus Walleij <linusw@kernel.org>

--- drivers/pinctrl/pinctrl-ingenic.c
+++ drivers/pinctrl/pinctrl-ingenic.c
@@ -4334,7 +4334,8 @@ static int ingenic_pinconf_group_get(struct pinctrl_dev *pctldev,
 		unsigned int group, unsigned long *config)
 {
 	const unsigned int *pins;
-	unsigned int i, npins, old = 0;
+	unsigned int i, npins;
+	unsigned long old = 0;
 	int ret;
 
 	ret = pinctrl_generic_get_group_pins(pctldev, group, &pins, &npins);
