16f57bd260add9ff51bd7ff17e4c88985e4d1abb
Jann Horn <jannh@google.com>
2026-04-30 16:15:33 +0200

    kcov: refactor common handle ID into kcov_common_handle_id
    Store common handle IDs in "struct kcov_common_handle_id", which consumes
    no space in non-KCOV builds.
    
    This cleanup removes #ifdef boilerplate code from subsystems that
    integrate with KCOV (in particular in usbip_common.h and skbuff.h, see the
    diffstat).
    
    This should also make it easier to add KCOV remote coverage to more
    subsystems in the future.
    
    Link: https://lore.kernel.org/20260430-kcov-refactor-common-handle-v1-1-23a0c7a0ba38@google.com
    Signed-off-by: Jann Horn <jannh@google.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-by: Dmitry Vyukov <dvyukov@google.com>
    Acked-by: Jakub Kicinski <kuba@kernel.org>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Andrey Konovalov <andreyknvl@gmail.com>
    Cc: Eugenio Pérez <eperezma@redhat.com>
    Cc: Hongren (Zenithal) Zheng <i@zenithal.me>
    Cc: Jann Horn <jannh@google.com>
    Cc: Jason Wang <jasowang@redhat.com>
    Cc: "Michael S. Tsirkin" <mst@redhat.com>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Valentina Manea <valentina.manea.m@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/kcov.c
+++ kernel/kcov.c
@@ -1083,11 +1083,11 @@ void kcov_remote_stop(void)
 EXPORT_SYMBOL(kcov_remote_stop);
 
 /* See the comment before kcov_remote_start() for usage details. */
-u64 kcov_common_handle(void)
+struct kcov_common_handle_id kcov_common_handle(void)
 {
 	if (!in_task())
-		return 0;
-	return current->kcov_handle;
+		return (struct kcov_common_handle_id){ .val = 0 };
+	return (struct kcov_common_handle_id){ .val = current->kcov_handle };
 }
 EXPORT_SYMBOL(kcov_common_handle);
 
