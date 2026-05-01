359b626d362127451dbe8a687ac5c240f896ae2e
Cássio Gabriel <cassiogabrielcontato@gmail.com>
2026-05-01 14:45:14 -0300

    ALSA: pcmtest: Return -EFAULT on pattern read copy failure
    pattern_write() reports -EFAULT when copy_from_user() fails, but
    pattern_read() converts copy_to_user() failures into a zero-length read.
    That makes a userspace buffer fault look like EOF instead of reporting the
    actual error.
    
    Return -EFAULT from pattern_read() when copying the pattern data to
    userspace fails, and update the file offset only after a successful copy.
    
    Fixes: 315a3d57c64c ("ALSA: Implement the new Virtual PCM Test Driver")
    Signed-off-by: Cássio Gabriel <cassiogabrielcontato@gmail.com>
    Link: https://patch.msgid.link/20260501-alsa-pcmtest-pattern-read-efault-v1-1-53e1e8c11dda@gmail.com
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/drivers/pcmtest.c
+++ sound/drivers/pcmtest.c
@@ -679,9 +679,9 @@ static ssize_t pattern_read(struct file *file, char __user *u_buff, size_t len, 
 		return 0;
 
 	if (copy_to_user(u_buff, patt_buf->buf + *off, to_read))
-		to_read = 0;
-	else
-		*off += to_read;
+		return -EFAULT;
+
+	*off += to_read;
 
 	return to_read;
 }
