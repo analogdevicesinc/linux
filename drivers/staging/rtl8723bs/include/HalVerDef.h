4874c05c4732abb9ea70a371eac7dced1f0022f7
Nikolay Kulikov <nikolayof23@gmail.com>
2026-05-03 00:55:42 +0300

    staging: rtl8723bs: remove unused macros from include/HalVerDef.h
    Remove macros that are wrappers over the fields of 'struct hal_version'
    and are never used in driver code.
    
    Signed-off-by: Nikolay Kulikov <nikolayof23@gmail.com>
    Link: https://patch.msgid.link/20260502220056.59815-4-nikolayof23@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/include/HalVerDef.h
+++ drivers/staging/rtl8723bs/include/HalVerDef.h
@@ -51,35 +51,5 @@ struct hal_version { /* tag_HAL_VERSION */
 
 /* hal_version			VersionID; */
 
-/*  Get element */
-#define GET_CVID_IC_TYPE(version)			((enum hal_ic_type_e)((version).ICType))
-#define GET_CVID_CHIP_TYPE(version) ((enum hal_chip_type_e)((version).chip_type))
-#define GET_CVID_MANUFACTUER(version)		((enum hal_vendor_e)((version).VendorType))
-#define GET_CVID_CUT_VERSION(version)		((enum hal_cut_version_e)((version).CUTVersion))
-#define GET_CVID_ROM_VERSION(version)		(((version).ROMVer) & ROM_VERSION_MASK)
-
-/*  */
-/* Common Macro. -- */
-/*  */
-/* hal_version VersionID */
-
-/* hal_chip_type_e */
-#define IS_TEST_CHIP(version)			((GET_CVID_CHIP_TYPE(version) == TEST_CHIP) ? true : false)
-#define IS_NORMAL_CHIP(version)			((GET_CVID_CHIP_TYPE(version) == NORMAL_CHIP) ? true : false)
-
-/* hal_cut_version_e */
-#define IS_A_CUT(version)				((GET_CVID_CUT_VERSION(version) == A_CUT_VERSION) ? true : false)
-#define IS_B_CUT(version)				((GET_CVID_CUT_VERSION(version) == B_CUT_VERSION) ? true : false)
-#define IS_C_CUT(version)				((GET_CVID_CUT_VERSION(version) == C_CUT_VERSION) ? true : false)
-#define IS_D_CUT(version)				((GET_CVID_CUT_VERSION(version) == D_CUT_VERSION) ? true : false)
-#define IS_E_CUT(version)				((GET_CVID_CUT_VERSION(version) == E_CUT_VERSION) ? true : false)
-#define IS_I_CUT(version)				((GET_CVID_CUT_VERSION(version) == I_CUT_VERSION) ? true : false)
-#define IS_J_CUT(version)				((GET_CVID_CUT_VERSION(version) == J_CUT_VERSION) ? true : false)
-#define IS_K_CUT(version)				((GET_CVID_CUT_VERSION(version) == K_CUT_VERSION) ? true : false)
-
-/* hal_vendor_e */
-#define IS_CHIP_VENDOR_TSMC(version)	((GET_CVID_MANUFACTUER(version) == CHIP_VENDOR_TSMC) ? true : false)
-#define IS_CHIP_VENDOR_UMC(version)	((GET_CVID_MANUFACTUER(version) == CHIP_VENDOR_UMC) ? true : false)
-#define IS_CHIP_VENDOR_SMIC(version)	((GET_CVID_MANUFACTUER(version) == CHIP_VENDOR_SMIC) ? true : false)
 
 #endif
