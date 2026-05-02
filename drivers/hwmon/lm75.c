4ab59263fd781ddd68da71d95f1fe6eef03344f5
Markus Stockhausen <markus.stockhausen@gmx.de>
2026-05-02 19:32:06 +0200

    hwmon: (lm75) Fix AS6200 setup and alarm handling
    The initialization of the AS6200 has two shortcomings
    
    - The device-add-commit states "Conversion mode: continuous" but the
      the lm75_params structure uses set_mask = 0x94c0. This activates
      single shot mode (bit 15). According to the datasheet "The device
      features a single shot measurement mode if the device is in sleep
      mode (SM=1)". This is quite contradictionary.
    - It is the only device that activates polarity active-high (bit 10)
    
    All this is paired with a undefined clear mask bug in function
    lm75_write_config() that was introduced with a later refactoring
    commit.
    
    [as6200] = {
    	.config_reg_16bits = true,
    	.set_mask = 0x94C0,
            -> .clr_mask not defined here
    	.default_resolution = 12,
    ...
    static inline int lm75_write_config(struct lm75_data *data, u16 set_mask,
    				    u16 clr_mask)
    {
    	return regmap_update_bits(data->regmap, LM75_REG_CONF,
    				  clr_mask | LM75_SHUTDOWN, set_mask);
    }
    
    regmap_update_bits() requires clr_mask to be a superset of set_mask.
    So basically all sensors with "wrong" masks like the AS6200 are not
    initialized as intended.
    
    Fix that by
    
    - Change the set_mask to 0x10c0 to reflect the current active-low
      setup properly and to drive the sensor in continous mode. This
      takes into account that the config register is little endian and
      the first byte sent to the chip is the LSB.
    - Adapt the alarm handling so it can report the alarm correctly
      even if it is high active. This is done by comparing config register
      bit 5 and 10 (translated to 2 and 13).
    
    This commit does not introduce any ABI breakage as the mutliple bugs
    effectly drive the AS6200 in standard active-low mode.
    
    Fixes: 4b6358e1fe46 ("hwmon: (lm75) Add AMS AS6200 temperature sensor")
    Suggested-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Markus Stockhausen <markus.stockhausen@gmx.de>
    Link: https://lore.kernel.org/r/20260502173207.3567876-2-markus.stockhausen@gmx.de
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/lm75.c
+++ drivers/hwmon/lm75.c
@@ -137,7 +137,7 @@ static const struct lm75_params device_params[] = {
 	},
 	[as6200] = {
 		.config_reg_16bits = true,
-		.set_mask = 0x94C0,	/* 8 sample/s, 4 CF, positive polarity */
+		.set_mask = 0x10C0,	/* 8 sample/s, 4 CF */
 		.default_resolution = 12,
 		.default_sample_time = 125,
 		.num_sample_times = 4,
@@ -416,7 +416,7 @@ static int lm75_read(struct device *dev, enum hwmon_sensor_types type,
 			switch (data->kind) {
 			case as6200:
 			case tmp112:
-				*val = (regval >> 13) & 0x1;
+				*val = !!(regval & BIT(13)) == !!(regval & BIT(2));
 				break;
 			default:
 				return -EINVAL;
