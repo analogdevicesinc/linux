371e5a0db4ab3d40671011cc073f41417bf5baab
Thorsten Blum <thorsten.blum@linux.dev>
2026-05-04 10:18:05 +0200

    drivers: dio: replace deprecated strcpy with strscpy in dio_init
    strcpy() has been deprecated [1] because it performs no bounds checking
    on the destination buffer, which can lead to buffer overflows. While the
    current code works correctly, replace strcpy() with the safer strscpy()
    to follow secure coding best practices.
    
    [1] https://www.kernel.org/doc/html/latest/process/deprecated.html#strcpy
    
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>
    Reviewed-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Link: https://patch.msgid.link/20260504081804.3260-3-thorsten.blum@linux.dev
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- drivers/dio/dio.c
+++ drivers/dio/dio.c
@@ -247,7 +247,7 @@ static int __init dio_init(void)
 			dev->id = prid;
 
 		dev->ipl = DIO_IPL(va);
-		strcpy(dev->name, dio_getname(dev->id));
+		strscpy(dev->name, dio_getname(dev->id));
                 printk(KERN_INFO "select code %3d: ipl %d: ID %02X", dev->scode, dev->ipl, prid);
 		if (DIO_NEEDSSECID(prid))
                         printk(":%02X", secid);
