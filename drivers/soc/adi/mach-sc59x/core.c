09060c329c21219d0631f49d52ac897252eed938
Jorge Marques <jorge.marques@analog.com>
2026-05-21 15:04:57 +0200

    soc: adi: sc59x: remove leftover SPU stub from core.c
    Drop the inline set_spu_securep_msec() placeholder that was left as
    a TODO in drivers/soc/adi/mach-sc59x/core.c. The Security Processing
    Unit is inaccessible from EL1, so no stub is needed in kernel space.
    
    Fixes: c91c17796b10 ("soc: adi: Add initial support for SC5xx SoCs")
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- drivers/soc/adi/mach-sc59x/core.c
+++ drivers/soc/adi/mach-sc59x/core.c
@@ -35,20 +35,3 @@
 #include <linux/soc/adi/cpu.h>
 
 #include "core.h"
-
-/** @todo spu stuff move to sec.c and make it a real driver */
-static void __iomem *spu_base;
-
-void set_spu_securep_msec(u16 n, bool msec)
-{
-	/*
-	 * This throws a data abort right now.
-	 * I assume the SPU is inaccessible from EL1.
-	 * If we need to adjust this from kernel-space,
-	 * this will have to be a secure monitor call (optee?)
-	 */
-	spu_base = NULL;
-}
-
-EXPORT_SYMBOL(set_spu_securep_msec);
-/** end @todo spu stuff */
