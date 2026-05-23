5e764f1cf9169b077cee4d0820c59db555411895
Qasim <Qasim.Ijaz@analog.com>
2026-05-23 23:13:54 +0100

    Update clk-adi-mux.c

--- drivers/clk/adi/clk-adi-mux.c
+++ drivers/clk/adi/clk-adi-mux.c
@@ -153,6 +153,7 @@ static int sc5xx_cdu_set_parent(struct clk_hw *clk_hw, u8 index)
 
 	ret = sc5xx_cdu_wait_ready(cdu_clk);
 
+	/* Verify the mux update before returning success to CCF. */
 	readback = sc5xx_cdu_read(cdu_clk, sc5xx_cdu_cfg(cdu_clk));
 	if (FIELD_GET(SC5XX_CDU_CFG_SEL, readback) != input_sel)
 		ret = -EIO;
