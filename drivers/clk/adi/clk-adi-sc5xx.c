97dea5e786258ae86079e05f3f40ea325eb15196
Qasim Ijaz <Qasim.Ijaz@analog.com>
2026-05-29 12:00:12 +0100

    updt

---
+++ drivers/clk/adi/clk-adi-sc5xx.c
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Clock driver for ADSP-SC5xx SoCs
+ *
+ * Copyright (C) 2026 Analog Devices Inc.
+ *
+ * Author: Qasim Ijaz <qasim.ijaz@analog.com>
+ * Contact: linux@analog.com
+ */
+
+#include "clk.h"
+
+static int __init sc5xx_clock_probe(struct platform_device *pdev)
+{
+	return 0;
+}
+
+static void sc5xx_clock_remove(struct platform_device *pdev)
+{
+	return 0;
+}
+
+static const struct of_device_id sc5xx_clock_of_match[] = {
+	{
+		/* sc598 clocks */
+		.compatible = "adi,sc598-cgu0",
+		.data = &sc598_cgu0_info,
+	}, {
+		.compatible = "adi,sc598-cgu1",
+		.data = &sc598_cgu1_info,
+	}, {
+		.compatible = "adi,sc598-cgu2",
+		.data = &sc598_cgu2_info,
+	}, {
+		.compatible = "adi,sc598-cdu",
+		.data = &sc598_cdu_info,
+	}, {
+		/* sc589 clocks */
+		.compatible = "adi,sc589-cgu0",
+		.data = &sc589_cgu0_info,
+	}, {
+		.compatible = "adi,sc589-cgu1",
+		.data = &sc589_cgu1_info,
+	}, {
+		.compatible = "adi,sc589-cdu",
+		.data = &sc589_cdu_info,
+	}, {
+		/* sc573 clocks */
+		.compatible = "adi,sc573-cgu0",
+		.data = &sc573_cgu0_info,
+	}, {
+		.compatible = "adi,sc573-cgu1",
+		.data = &sc573_cgu1_info,
+	}, {
+		.compatible = "adi,sc573-cdu",
+		.data = &sc573_cdu_info,
+	}, {
+		/* sc594 clocks */
+		.compatible = "adi,sc594-cgu0",
+		.data = &sc594_cgu0_info,
+	}, {
+		.compatible = "adi,sc594-cgu1",
+		.data = &sc594_cgu1_info,
+	}, {
+		.compatible = "adi,sc594-cdu",
+		.data = &sc594_cdu_info,
+	}, {
+	},
+};
+
+static struct platform_driver sc5xx_clock_driver = {
+	.driver	= {
+		.name = "sc5xx_clock",
+		.of_match_table = sc5xx_clock_of_match,
+	},
+	.probe = sc5xx_clock_probe,
+	.remove = sc5xx_clock_remove,
+};
+
+MODULE_AUTHOR("Qasim Ijaz <qasim.ijaz@analog.com>");
+MODULE_DESCRIPTION("Clock driver for ADSP-SC5xx SoCs");
+MODULE_LICENSE("GPLv2");
