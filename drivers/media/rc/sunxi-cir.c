f9b33ec404200c03dd490ec133b8f9bf599c5444
Sean Young <sean@mess.org>
2026-07-06 12:37:04 +0100

    media: sunxi-cir: Ensure no more interrupts can occur before free
    Only call rc_free_device() once the hardware has been stopped.
    
    Fixes: dccc0c3ddf8f ("media: rc: fix race between unregister and urb/irq callbacks")
    Signed-off-by: Sean Young <sean@mess.org>
    Cc: stable@vger.kernel.org
    Reviewed-by: Hans Verkuil <hverkuil+cisco@kernel.org>

--- drivers/media/rc/sunxi-cir.c
+++ drivers/media/rc/sunxi-cir.c
@@ -374,8 +374,8 @@ static void sunxi_ir_remove(struct platform_device *pdev)
 	struct sunxi_ir *ir = platform_get_drvdata(pdev);
 
 	rc_unregister_device(ir->rc);
-	rc_free_device(ir->rc);
 	sunxi_ir_hw_exit(&pdev->dev);
+	rc_free_device(ir->rc);
 }
 
 static void sunxi_ir_shutdown(struct platform_device *pdev)
