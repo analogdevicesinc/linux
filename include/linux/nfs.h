32ef14709b3900c874460221a9f756122d3253d7
Chuck Lever <cel@kernel.org>
2026-07-20 10:14:40 -0400

    NFS: Add linux/nfs_fh.h
    Plenty of spots around the kernel need the full definition of struct
    nfs_fh but not the cred, sunrpc, and uapi dependencies that
    linux/nfs.h pulls in along with it.
    
    Relocate struct nfs_fh to its own header, and include that header in
    linux/nfs.h so existing consumers keep building. Over time, consumers
    can then replace
    
      #include <linux/nfs.h>
    
    with
    
      #include <linux/nfs_fh.h>
    
    While relocating the code, add kernel-doc comments for the FH
    operations and convert nfs_compare_fh() to return bool.
    
    Link: https://patch.msgid.link/20260720141442.783935-2-cel@kernel.org
    Signed-off-by: Chuck Lever <cel@kernel.org>

--- include/linux/nfs.h
+++ include/linux/nfs.h
@@ -11,8 +11,8 @@
 #include <linux/cred.h>
 #include <linux/sunrpc/auth.h>
 #include <linux/sunrpc/msg_prot.h>
-#include <linux/string.h>
-#include <linux/crc32.h>
+#include <linux/nfs_fh.h>
+
 #include <uapi/linux/nfs.h>
 
 /* The LOCALIO program is entirely private to Linux and is
@@ -21,30 +21,6 @@
 #define NFS_LOCALIO_PROGRAM		400122
 #define LOCALIOPROC_NULL		0
 #define LOCALIOPROC_UUID_IS_LOCAL	1
-
-/*
- * This is the kernel NFS client file handle representation
- */
-#define NFS_MAXFHSIZE		128
-struct nfs_fh {
-	unsigned short		size;
-	unsigned char		data[NFS_MAXFHSIZE];
-};
-
-/*
- * Returns a zero iff the size and data fields match.
- * Checks only "size" bytes in the data field.
- */
-static inline int nfs_compare_fh(const struct nfs_fh *a, const struct nfs_fh *b)
-{
-	return a->size != b->size || memcmp(a->data, b->data, a->size) != 0;
-}
-
-static inline void nfs_copy_fh(struct nfs_fh *target, const struct nfs_fh *source)
-{
-	target->size = source->size;
-	memcpy(target->data, source->data, source->size);
-}
 
 enum nfs3_stable_how {
 	NFS_UNSTABLE = 0,
@@ -55,15 +31,4 @@ enum nfs3_stable_how {
 	NFS_INVALID_STABLE_HOW = -1
 };
 
-/**
- * nfs_fhandle_hash - calculate the crc32 hash for the filehandle
- * @fh - pointer to filehandle
- *
- * returns a crc32 hash for the filehandle that is compatible with
- * the one displayed by "wireshark".
- */
-static inline u32 nfs_fhandle_hash(const struct nfs_fh *fh)
-{
-	return ~crc32_le(0xFFFFFFFF, &fh->data[0], fh->size);
-}
 #endif /* _LINUX_NFS_H */
