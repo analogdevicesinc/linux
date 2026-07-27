f472919f4c0650d7fb22bb230eacaada0c6ee951
Hongling Zeng <zenghongling@kylinos.cn>
2026-07-27 14:11:42 +0800

    dmaengine: sun6i-dma: Fix use-after-free in error handling paths
    In error handling paths, the for loop frees v_lli in the loop body,
    then accesses v_lli->v_lli_next and v_lli->p_lli_next in the
    increment expression, which is use-after-free.
    
    Fix by refactoring the cleanup into a helper function sun6i_dma_free_desc()
    that saves both the next virtual and physical pointers before freeing the
    current node, preventing the use-after-free.
    
    Fixes: 555859308723 ("dmaengine: sun6i: Add driver for the Allwinner A31 DMA controller")
    Signed-off-by: Hongling Zeng <zenghongling@kylinos.cn>
    Suggested-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Reviewed-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Link: https://patch.msgid.link/20260727061142.44195-1-zenghongling@kylinos.cn
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/sun6i-dma.c
+++ drivers/dma/sun6i-dma.c
@@ -406,15 +406,11 @@ static inline void sun6i_dma_dump_lli(struct sun6i_vchan *vchan,
 		v_lli->len, v_lli->para, v_lli->p_lli_next);
 }
 
-static void sun6i_dma_free_desc(struct virt_dma_desc *vd)
+static void sun6i_dma_free_desc(struct sun6i_dma_dev *sdev,
+				struct sun6i_desc *txd)
 {
-	struct sun6i_desc *txd = to_sun6i_desc(&vd->tx);
-	struct sun6i_dma_dev *sdev = to_sun6i_dma_dev(vd->tx.chan->device);
 	struct sun6i_dma_lli *v_lli, *v_next;
 	dma_addr_t p_lli, p_next;
-
-	if (unlikely(!txd))
-		return;
 
 	p_lli = txd->p_lli;
 	v_lli = txd->v_lli;
@@ -430,6 +426,17 @@ static void sun6i_dma_free_desc(struct virt_dma_desc *vd)
 	}
 
 	kfree(txd);
+}
+
+static void sun6i_dma_free_desc_virt(struct virt_dma_desc *vd)
+{
+	struct sun6i_desc *txd = to_sun6i_desc(&vd->tx);
+	struct sun6i_dma_dev *sdev = to_sun6i_dma_dev(vd->tx.chan->device);
+
+	if (unlikely(!txd))
+		return;
+
+	sun6i_dma_free_desc(sdev, txd);
 }
 
 static int sun6i_dma_start_desc(struct sun6i_vchan *vchan)
@@ -788,10 +795,7 @@ static struct dma_async_tx_descriptor *sun6i_dma_prep_slave_sg(
 	return vchan_tx_prep(&vchan->vc, &txd->vd, flags);
 
 err_lli_free:
-	for (p_lli = txd->p_lli, v_lli = txd->v_lli; v_lli;
-	     p_lli = v_lli->p_lli_next, v_lli = v_lli->v_lli_next)
-		dma_pool_free(sdev->pool, v_lli, p_lli);
-	kfree(txd);
+	sun6i_dma_free_desc(sdev, txd);
 	return NULL;
 }
 
@@ -869,10 +873,7 @@ static struct dma_async_tx_descriptor *sun6i_dma_prep_dma_cyclic(
 	return vchan_tx_prep(&vchan->vc, &txd->vd, flags);
 
 err_lli_free:
-	for (p_lli = txd->p_lli, v_lli = txd->v_lli; v_lli;
-	     p_lli = v_lli->p_lli_next, v_lli = v_lli->v_lli_next)
-		dma_pool_free(sdev->pool, v_lli, p_lli);
-	kfree(txd);
+	sun6i_dma_free_desc(sdev, txd);
 	return NULL;
 }
 
@@ -1428,7 +1429,7 @@ static int sun6i_dma_probe(struct platform_device *pdev)
 		struct sun6i_vchan *vchan = &sdc->vchans[i];
 
 		INIT_LIST_HEAD(&vchan->node);
-		vchan->vc.desc_free = sun6i_dma_free_desc;
+		vchan->vc.desc_free = sun6i_dma_free_desc_virt;
 		vchan_init(&vchan->vc, &sdc->slave);
 	}
 
