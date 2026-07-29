cffeb4ef6f2867df5cc4811b59930dc0fe3ea7e9
Ashutosh Dixit <ashutosh.dixit@intel.com>
2026-07-29 16:06:45 -0700

    drm/xe/oa: Extend OAM unit hwe assignment for new dGfx
    dGfx vecs hwe's for media version 35.00 and later follow the same
    assignment to OAM units as vcs engines.
    
    Bspec: 77977
    
    Signed-off-by: Ashutosh Dixit <ashutosh.dixit@intel.com>
    Reviewed-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
    Link: https://patch.msgid.link/20260729230645.3052178-3-ashutosh.dixit@intel.com

--- drivers/gpu/drm/xe/xe_oa.c
+++ drivers/gpu/drm/xe/xe_oa.c
@@ -2577,10 +2577,11 @@ static u32 __hwe_oam_unit(struct xe_hw_engine *hwe)
 		return XE_OA_UNIT_INVALID;
 	else if (!IS_DGFX(gt_to_xe(hwe->gt)))
 		return XE_OAM_UNIT_SCMI_0;
-	else if (hwe->class == XE_ENGINE_CLASS_VIDEO_DECODE)
-		return (hwe->instance / 2 & 0x1) + 1;
-	else if (hwe->class == XE_ENGINE_CLASS_VIDEO_ENHANCE)
+	else if (hwe->class == XE_ENGINE_CLASS_VIDEO_ENHANCE &&
+		 MEDIA_VERx100(gt_to_xe(hwe->gt)) < 3500)
 		return (hwe->instance & 0x1) + 1;
+	else
+		return (hwe->instance / 2 & 0x1) + 1;
 
 	return XE_OA_UNIT_INVALID;
 }
