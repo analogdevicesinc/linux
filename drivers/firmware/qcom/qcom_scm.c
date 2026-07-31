068ce7396a2253a88e162393adf625d450b34080
Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
2026-07-31 19:59:46 +0200

    firmware: qcom: scm: Allow QSEECOM on Yoga Slim 7x Gen11
    Allow the use of QSEECOM on the Glymur-based Lenovo Yoga Slim 7x Gen11,
    as there seem to be no issues with the firmware that would mandate
    disabling it.
    
    Reviewed-by: Abel Vesa <abel.vesa@oss.qualcomm.com>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Signed-off-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
    Tested-by: Oleg Keri <okerixx@gmail.com>
    Link: https://lore.kernel.org/r/20260731-topic-yoga_submission-v2-3-f1887031da4f@oss.qualcomm.com
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -2320,6 +2320,7 @@ static const struct of_device_id qcom_scm_qseecom_allowlist[] __maybe_unused = {
 	{ .compatible = "lenovo,thinkpad-t14s" },
 	{ .compatible = "lenovo,thinkpad-x13s", },
 	{ .compatible = "lenovo,yoga-slim7x" },
+	{ .compatible = "lenovo,yoga-slim7x-gen11" },
 	{ .compatible = "medion,sprchrgd14s1" },
 	{ .compatible = "microsoft,arcata", },
 	{ .compatible = "microsoft,surface-pro-12in", },
