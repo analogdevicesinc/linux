2dc4ff4957a2cd4f17edef5da8ab87c70c353409
Dalvin-Ehinoma Noah Aiguobas <fliegbert2@gmail.com>
2026-07-31 10:41:33 +0000

    staging: rtl8723bs: renaming variable bHwSupportBeamformer in rtw_mlme.c
    Renaming occurrences of bHwSupportBeamformer to hw_support_beamformer
    since the prefix b is an old naming convention to indicate a boolean
    variable.
    
    Signed-off-by: Dalvin-Ehinoma Noah Aiguobas <fliegbert2@gmail.com>
    Link: https://patch.msgid.link/20260731104134.12754-11-fliegbert2@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/core/rtw_mlme.c
+++ drivers/staging/rtl8723bs/core/rtw_mlme.c
@@ -2184,7 +2184,7 @@ void rtw_ht_use_default_setting(struct adapter *adapter)
 	struct ht_priv *ht_priv = &mlme_priv->htpriv;
 	struct registry_priv *registry_priv = &adapter->registrypriv;
 	bool hw_ldpc_support = false, hw_stbc_support = false;
-	bool bHwSupportBeamformer = false, bHwSupportBeamformee = false;
+	bool hw_support_beamformer = false, bHwSupportBeamformee = false;
 
 	if (registry_priv->wifi_spec)
 		ht_priv->bss_coexist = 1;
@@ -2221,10 +2221,10 @@ void rtw_ht_use_default_setting(struct adapter *adapter)
 	}
 
 	/*  Beamforming setting */
-	rtw_hal_get_def_var(adapter, HAL_DEF_EXPLICIT_BEAMFORMER, (u8 *)&bHwSupportBeamformer);
+	rtw_hal_get_def_var(adapter, HAL_DEF_EXPLICIT_BEAMFORMER, (u8 *)&hw_support_beamformer);
 	rtw_hal_get_def_var(adapter, HAL_DEF_EXPLICIT_BEAMFORMEE, (u8 *)&bHwSupportBeamformee);
 	CLEAR_FLAGS(ht_priv->beamform_cap);
-	if (TEST_FLAG(registry_priv->beamform_cap, BIT(4)) && bHwSupportBeamformer)
+	if (TEST_FLAG(registry_priv->beamform_cap, BIT(4)) && hw_support_beamformer)
 		SET_FLAG(ht_priv->beamform_cap, BEAMFORMING_HT_BEAMFORMER_ENABLE);
 
 	if (TEST_FLAG(registry_priv->beamform_cap, BIT(5)) && bHwSupportBeamformee)
