3dd95f077371f5def65a91c4bb13bd574a62bc3a
Vasily Gorbik <gor@linux.ibm.com>
2026-08-07 03:52:08 +0000

    sched/core: Don't steal a proxy-exec donor
    try_steal_cookie() avoids stealing src->core_pick and src->curr before
    moving a task with the same cookie via move_queued_task_locked().
    
    With proxy-exec, src->donor is the current scheduling context and may
    differ from src->curr. Stealing it migrates a task that the source rq
    still treats as current, leaving src's scheduler state for that task
    stale. For CFS this means cfs_rq->curr points at the stolen entity,
    and the next pick on the source rq hits the WARN_ON_ONCE in
    put_prev_entity().
    
    Commit 7de9d4f94638 ("sched: Start blocked_on chain processing in
    find_proxy_task()") tweaked the fair class logic so that the donor task
    isn't migrated away while we're running the proxy. Do it similarly for
    try_steal_cookie() and skip src->donor as well.
    
    Fixes: 7de9d4f94638 ("sched: Start blocked_on chain processing in find_proxy_task()")
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>
    Signed-off-by: John Stultz <jstultz@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://patch.msgid.link/20260807035232.1881495-3-jstultz@google.com

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6509,7 +6509,7 @@ static bool try_steal_cookie(int this, int that)
 		return false;
 
 	do {
-		if (p == src->core_pick || p == src->curr)
+		if (p == src->core_pick || p == src->curr || p == src->donor)
 			goto next;
 
 		if (!is_cpu_allowed(p, this))
