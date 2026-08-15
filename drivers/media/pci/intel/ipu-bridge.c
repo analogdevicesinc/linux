df36316fdd9753799304395b69fc70ddbb45e1a9
Steve Kaznak <skaznak@protonmail.com>
2026-08-15 02:23:50 +0000

    media: ipu-bridge: Add OV01A1B sensor config
    Add the Omnivision OV01A1B (ACPI HID OVTI01AB) to the
    ipu_supported_sensors[] table, with the same single 400 MHz link
    frequency as the OV01A10 and OV01A1S entries it shares a comment with.
    
    The sensor is the IR camera in the Dell XPS 13 Plus 9320, behind an
    Intel Vision Sensing Controller (IVSC). Commit 36eab90d3f4f ("media:
    i2c: ov01a10: Add ov01a1b support") added driver support for it, but
    without a bridge entry no fwnode graph endpoint is created, so the
    driver never binds and the camera is not enumerated.
    
    Tested on an XPS 13 Plus 9320: the sensor binds and streams 1296x816
    Y10 at 30 fps.
    
    Signed-off-by: Steve Kaznak <skaznak@protonmail.com>
    Reviewed-by: Mehdi Djait <mehdi.djait@linux.intel.com>
    Reviewed-by: Hans de Goede <johannes.goede@oss.qualcomm.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- drivers/media/pci/intel/ipu-bridge.c
+++ drivers/media/pci/intel/ipu-bridge.c
@@ -88,8 +88,9 @@ static const struct ipu_sensor_config ipu_supported_sensors[] = {
 	IPU_SENSOR_CONFIG("INT3537", 1, 437000000),
 	/* Lontium lt6911uxe */
 	IPU_SENSOR_CONFIG("INTC10C5", 0),
-	/* Omnivision OV01A10 / OV01A1S */
+	/* Omnivision OV01A10 / OV01A1B / OV01A1S */
 	IPU_SENSOR_CONFIG("OVTI01A0", 1, 400000000),
+	IPU_SENSOR_CONFIG("OVTI01AB", 1, 400000000),
 	IPU_SENSOR_CONFIG("OVTI01AS", 1, 400000000),
 	/* Omnivision OV02C10 */
 	IPU_SENSOR_CONFIG("OVTI02C1", 1, 400000000),
