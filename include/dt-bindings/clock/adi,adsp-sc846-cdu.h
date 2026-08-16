8396b9bd3f09450f3a6a72ac625c23f38d6ccc9e
Qasim Ijaz <Qasim.Ijaz@analog.com>
2026-08-16 11:31:39 +0100

    dt-bindings: clock: adi: add ADSP SoC CDU clock IDs
    Add separate CDU clock binding headers for the ADSP-SC573, ADSP-SC589,
    ADSP-SC594, ADSP-SC598 and ADSP-SC846 SoCs. Define the clock
    IDs exported by each SoC's CDU controller in its
    corresponding header.
    
    Signed-off-by: Qasim Ijaz <Qasim.Ijaz@analog.com>

---
+++ include/dt-bindings/clock/adi,adsp-sc846-cdu.h
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * (C) Copyright 2026 - Analog Devices Inc.
+ *
+ * ADSP-SC846 SoC clock device tree bindings
+ *
+ * This header defines the clocks that the ADSP-SC846
+ * CDU clock controller provides.
+ *
+ */
+
+#ifndef DT_BINDINGS_CLK_ADSP_SC846_CDU_H
+#define DT_BINDINGS_CLK_ADSP_SC846_CDU_H
+
+#define ADSP_SC846_CLK_CDU_SHARC	0
+#define ADSP_SC846_CLK_CDU_ARM		1
+#define ADSP_SC846_CLK_CDU_DDR		2
+#define ADSP_SC846_CLK_CDU_CAN		3
+#define ADSP_SC846_CLK_CDU_SPDIF	4
+#define ADSP_SC846_CLK_CDU_SPI		5
+#define ADSP_SC846_CLK_CDU_GIGE		6
+#define ADSP_SC846_CLK_CDU_XSPI1	7
+#define ADSP_SC846_CLK_CDU_LP		8
+#define ADSP_SC846_CLK_CDU_XSPI0	9
+#define ADSP_SC846_CLK_CDU_TRACE	10
+#define ADSP_SC846_CLK_CDU_PWM		11
+#define ADSP_SC846_CLK_CDU_MSHC		12
+
+#endif
