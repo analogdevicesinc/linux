e81ee06308379a5f2ededf997bcf17551bce5db7
Eric Kim <seohyun.kim@outlook.kr>
2026-08-18 12:48:29 +0900

    sched/fair: Reset NUMA fault locality after scan period update
    When updating the task scan period for NUMA locality checks,
    update_task_scan_period() checks whether there were no faults or failed
    migration on the last scan window, at which it prolongs the scan period.
    
    However, p->numa_faults_locality, which is used to check for migration
    failure and number of faults at the previous scan window is not cleared
    after changing the scan period, which unintentionally increases
    numa_scan_period up to numa_scan_period_max even when there were no
    migration failures or no faults at previous scan.
    
    Fix this by jumping to the out label on early exit at this case to
    ensure p->numa_faults_locality is always cleared before returning.
    
    Closes: https://lore.kernel.org/all/20250404095354.311156-1-qlsdnjs236@chungbuk.ac.kr/
    Reported-by: Binwon Song <qlsdnjs236@chungbuk.ac.kr>
    Signed-off-by: Eric Kim <seohyun.kim@outlook.kr>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: stable@vger.kernel.org
    Link: https://patch.msgid.link/Message-ID:

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3514,7 +3514,7 @@ static void update_task_scan_period(struct task_struct *p,
 		p->mm->numa_next_scan = jiffies +
 			msecs_to_jiffies(p->numa_scan_period);
 
-		return;
+		goto out;
 	}
 
 	/*
@@ -3558,7 +3558,10 @@ static void update_task_scan_period(struct task_struct *p,
 
 	p->numa_scan_period = clamp(p->numa_scan_period + diff,
 			task_scan_min(p), task_scan_max(p));
-	memset(p->numa_faults_locality, 0, sizeof(p->numa_faults_locality));
+
+out:
+	memset(p->numa_faults_locality, 0,
+	       sizeof(p->numa_faults_locality));
 }
 
 /*
