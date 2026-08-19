531aba79af698002c68b8128a0a1ccc068419a1b
Qu Wenruo <wqu@suse.com>
2026-08-19 10:36:19 +0930

    btrfs: remove on-stack paddrs[] array usage
    Since the bs > ps support, we have to handle cases where a data block is
    inside several discontiguous pages.
    
    Thus we need a local paddrs[] array to assemble a data block for bs > ps
    cases.
    
    However to handle all possible bs/ps combinations, we have to declare
    such array using the max block size vs page size, no matter the current
    block size and page size.
    
    This adds 128 bytes on-stack memory usage for several call sites, and
    also introduced several duplicated helpers to calculate checksum for a
    data block:
    
    - btrfs_calculate_block_csum_folio()
    - btrfs_calculate_block_csum_pages()
    - btrfs_check_block_csum()
    
    The differences are mostly in how the data is passed.
    The first one accepts a contiguous paddr range.
    The second one accepts an array of paddrs[].
    The last one is just a simple wrapper of the first one.
    
    However the most common interface to iterate a data block is through
    bio, and we have already converted most callers to use the bio based
    interface, e.g. btrfs_bio_data_csum_ok() and btrfs_csum_one_bio_block().
    
    Convert the remaining two call sites to address the remaining paddrs[]
    usage:
    
    - btrfs_calculate_block_csum_pages() inside verify_bio_data_sectors()
      This can be switched to btrfs_csum_one_bio_block().
    
      This removes the 128 bytes on-stack memory usage.
    
    - btrfs_calculate_block_csum_pages() inside verify_one_sector()
      This call site doesn't use on-stack memory for paddrs[], but reuses
      the existing btrfs_raid_bio::bio_paddrs[] or
      btrfs_raid_bio::stripe_paddrs[].
    
      So implement a local version called calculate_block_csum_paddrs().
    
    Now there is no fixed on-stack paddrs[] usage anymore.
    
    Reviewed-by: Boris Burkov <boris@bur.io>
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/btrfs_inode.h
+++ fs/btrfs/btrfs_inode.h
@@ -507,12 +507,6 @@ static inline void btrfs_set_inode_mapping_order(struct btrfs_inode *inode)
 				      inode->root->fs_info->block_max_order);
 }
 
-void btrfs_calculate_block_csum_folio(struct btrfs_fs_info *fs_info,
-				      const phys_addr_t paddr, u8 *dest);
-void btrfs_calculate_block_csum_pages(struct btrfs_fs_info *fs_info,
-				      const phys_addr_t paddrs[], u8 *dest);
-int btrfs_check_block_csum(struct btrfs_fs_info *fs_info, phys_addr_t paddr, u8 *csum,
-			   const u8 * const csum_expected);
 bool btrfs_bio_data_csum_ok(struct btrfs_bio *bbio, const struct bvec_iter *orig_iter,
 			    struct btrfs_device *dev);
 void btrfs_csum_one_bio_block(struct btrfs_fs_info *fs_info, struct bio *bio,
