a5576ebce9201e2abc820348a494e666de8efe64
Hongyan Xia <hongyan.xia@transsion.com>
2026-08-19 08:12:09 +0000

    sched: Convert paravirt_steal to new static key APIs
    paravirt_steal_rq_enabled and paravirt_steal_enabled use raw static_key
    APIs which are now deprecated. Use the new API instead.
    
    No functional change.
    
    Signed-off-by: Hongyan Xia <hongyan.xia@transsion.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Juergen Gross <jgross@suse.com>
    Link: https://patch.msgid.link/20260819081207.12150-1-hongyan.xia@transsion.com

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -805,7 +805,7 @@ struct rq *_task_rq_lock(struct task_struct *p, struct rq_flags *rf)
 
 /* Use CONFIG_PARAVIRT as this will avoid more #ifdef in arch code. */
 #ifdef CONFIG_PARAVIRT
-struct static_key paravirt_steal_rq_enabled;
+DEFINE_STATIC_KEY_FALSE(paravirt_steal_rq_enabled);
 #endif
 
 static void update_rq_clock_task(struct rq *rq, s64 delta)
@@ -844,7 +844,7 @@ static void update_rq_clock_task(struct rq *rq, s64 delta)
 	}
 #endif
 #ifdef CONFIG_PARAVIRT_TIME_ACCOUNTING
-	if (static_key_false((&paravirt_steal_rq_enabled))) {
+	if (static_branch_unlikely(&paravirt_steal_rq_enabled)) {
 		u64 prev_steal;
 
 		steal = prev_steal = paravirt_steal_clock(cpu_of(rq));
