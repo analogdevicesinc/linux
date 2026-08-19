780ab12fc70d15ddb570bf6dba793b81c48f5fe0
Christian Brauner <brauner@kernel.org>
2026-08-20 01:09:22 +0200

    selftests/coredump: collapse the expected request check into the helper
    All nine callers spell the expected flags. So every new feature bit the
    kernel learns has to be cargo culted.
    
    The callers also all pass COREDUMP_ACK_SIZE_VER0 as the minimum request
    size although what is being validated is coredump_req->size. And
    read_coredump_req() makes the same mixup twice more.
    
    Clean this all up.
    
    Link: https://patch.msgid.link/20260820-work-coredump-sparse-v2-5-ba32dd718c51@kernel.org
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>

--- tools/testing/selftests/coredump/coredump_test.h
+++ tools/testing/selftests/coredump/coredump_test.h
@@ -51,8 +51,7 @@ bool read_marker(int fd, enum coredump_mark mark);
 bool read_coredump_req(int fd, struct coredump_req *req);
 bool send_coredump_ack(int fd, const struct coredump_req *req,
 		       __u64 mask, size_t size_ack);
-bool check_coredump_req(const struct coredump_req *req, size_t min_size,
-			__u64 required_mask);
+bool check_coredump_req(const struct coredump_req *req);
 int open_coredump_tmpfile(int fd_tmpfs_detached);
 void process_coredump_worker(int fd_coredump, int fd_peer_pidfd, int fd_core_file);
 
