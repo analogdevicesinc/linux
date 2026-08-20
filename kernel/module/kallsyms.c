a22f1497700e1b4b1d6285b7884cc1da3ffbba43
Tiezhu Yang <yangtiezhu@loongson.cn>
2026-08-20 20:50:07 +0800

    module/kallsyms: Ignore local labels and mapping symbols during module load
    The compiler toolchains may generate local labels and mapping symbols on
    certain architectures like LoongArch for optimizations and relocations.
    
    While these local labels and mapping symbols are already ignored during
    runtime lookups via find_kallsyms_symbol(), they still leak into kallsyms
    for loaded modules because layout_symtab() and add_kallsyms() don't ignore
    them during load time.
    
    Consequently, tracing tools like bpftrace (which do not perform internal
    filtering, unlike perf) resolve identical addresses into confusing local
    labels instead of actual clear C function names.
    
    Fix this by integrating the is_ignored_kernel_symbol() check directly into
    is_core_symbol(). This ensures these unneeded symbols are stripped during
    module load time, keeping /proc/kallsyms clean and resulting in accurate
    call stacks.
    
    Signed-off-by: Tiezhu Yang <yangtiezhu@loongson.cn>
    Reviewed-by: Huacai Chen <chenhuacai@loongson.cn>
    Reviewed-by: Petr Pavlu <petr.pavlu@suse.com>
    Signed-off-by: Petr Pavlu <petr.pavlu@suse.com>

--- kernel/module/kallsyms.c
+++ kernel/module/kallsyms.c
@@ -89,6 +89,9 @@ static bool is_core_symbol(struct module *mod, const struct load_info *info,
 	    !src->st_name)
 		return false;
 
+	if (is_ignored_kernel_symbol(&info->strtab[src->st_name]))
+		return false;
+
 #ifdef CONFIG_KALLSYMS_ALL
 	if (src->st_shndx == info->index.pcpu)
 		return true;
