e31bd1eb17f927d067e40916abb17c4ce5f95ece
Alain Volmat <alain.volmat@foss.st.com>
2026-08-21 08:38:12 +0200

    media: uapi: Add controls for ST DCMIPP Driver
    Add definitions and documentation for the custom control provided
    by the DCMIPP driver in order to control the pixelproc gamma
    correction.
    
    Signed-off-by: Alain Volmat <alain.volmat@foss.st.com>
    Signed-off-by: Hans Verkuil <hverkuil+cisco@kernel.org>

--- include/uapi/linux/v4l2-controls.h
+++ include/uapi/linux/v4l2-controls.h
@@ -234,6 +234,12 @@ enum v4l2_colorfx {
  */
 #define V4L2_CID_USER_MALI_C55_BASE		(V4L2_CID_USER_BASE + 0x1230)
 
+/*
+ * The base for the ST DCMIPP driver controls.
+ * We reserve 16 controls for this driver
+ */
+#define V4L2_CID_USER_DCMIPP_BASE		(V4L2_CID_USER_BASE + 0x1240)
+
 /* MPEG-class control IDs */
 /* The MPEG controls are applicable to all codec controls
  * and the 'MPEG' part of the define is historical */
