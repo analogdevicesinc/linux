a231ed57976e32d2f9c8d93904803023a65824e9
Alain Volmat <alain.volmat@foss.st.com>
2026-08-21 08:38:11 +0200

    media: stm32: dcmipp: addition of a dcmipp-isp subdev
    The ISP subdev is the first element after input of the main pipeline.
    Part (static configuration) of this block is done via this subdev while
    other configuration done on a per-frame basis will be done via a output
    metadata device attached to this subdev.
    This subdev handled the following features of the ISP block:
      - statistic removal (top / bottom of the frame)
      - decimation
      - demosaicing
      - control of frame export to the aux pipeline
    
    Signed-off-by: Alain Volmat <alain.volmat@foss.st.com>
    Signed-off-by: Hans Verkuil <hverkuil+cisco@kernel.org>

--- drivers/media/platform/st/stm32/stm32-dcmipp/dcmipp-common.h
+++ drivers/media/platform/st/stm32/stm32-dcmipp/dcmipp-common.h
@@ -283,5 +283,8 @@ void dcmipp_byteproc_ent_release(struct dcmipp_ent_device *ved);
 struct dcmipp_ent_device *dcmipp_bytecap_ent_init(const char *entity_name,
 						  struct dcmipp_device *dcmipp);
 void dcmipp_bytecap_ent_release(struct dcmipp_ent_device *ved);
+struct dcmipp_ent_device *dcmipp_isp_ent_init(const char *entity_name,
+					      struct dcmipp_device *dcmipp);
+void dcmipp_isp_ent_release(struct dcmipp_ent_device *ved);
 
 #endif
