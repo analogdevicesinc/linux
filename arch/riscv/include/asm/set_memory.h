0745f2642dd930204c9bf51471a41cf237075671
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-08-23 14:46:17 +0300

    Revert "arch: introduce set_direct_map_valid_noflush()"
    Commit 0c6378a71574 ("arch: introduce set_direct_map_valid_noflush()")
    added set_direct_map_valid_noflush() to allow updating the direct map for
    a physically contiguous range in execmem.
    
    As Brendan recently pointed out [1], this API is confusing because on
    arm64 it means that is sets VALID bit in ptes, while on other
    architectures it is an analog of set_direct_map_default_noflush().
    
    The only user of set_direct_map_valid_noflush() was execmem's ROX cache
    freeing path and it was switched to utilize VM_FLUSH_RESET_PERMS for
    resetting permissions of the direct map alias.
    
    With the last user gone and with
    set_direct_map_{invalid,default}_noflush() accepting number of pages as a
    parameter, set_direct_map_valid_noflush() become a copy of
    set_memory_valid() on arm64 and a duplicate of
    set_direct_map_{invalid,default}_noflush() on other architecture, it is
    safe to remove set_direct_map_valid_noflush().
    
    Also drop a stale comment in arm64::__kernel_map_pages() that Linus
    bothered to add when merging changes containing
    set_direct_map_valid_noflush() to his tree.
    
    This reverts commit 0c6378a71574daa6cd1534ad42a956e3262756c7.
    
    Link: https://lore.kernel.org/20260823-execmem-set-vm-perms-v0-2-v2-6-b013a37d84b3@kernel.org
    Link: https://lore.kernel.org/all/DJ69RCVRBO0Y.3JCYSW50IC4RC@linux.dev [1]
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Reviewed-by: Brendan Jackman <brendan.jackman@linux.dev>
    Cc: Albert Ou <aou@eecs.berkeley.edu>
    Cc: Alexander Gordeev <agordeev@linux.ibm.com>
    Cc: Alexandre Ghiti <alex@ghiti.fr>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: "Borislav Petkov (AMD)" <bp@alien8.de>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Christian Borntraeger <borntraeger@linux.ibm.com>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Gerald Schaefer <gerald.schaefer@linux.ibm.com>
    Cc: Heiko Carstens <hca@linux.ibm.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Huacai Chen <chenhuacai@kernel.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Len Brown <lenb@kernel.org>
    Cc: Palmer Dabbelt <palmer@dabbelt.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: "Rafael J. Wysocki" <rafael@kernel.org>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Sven Schnelle <svens@linux.ibm.com>
    Cc: "Uladzislau Rezki (Sony)" <urezki@gmail.com>
    Cc: Vasily Gorbik <gor@linux.ibm.com>
    Cc: WANG Xuerui <kernel@xen0n.name>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/riscv/include/asm/set_memory.h
+++ arch/riscv/include/asm/set_memory.h
@@ -42,7 +42,6 @@ static inline int set_kernel_memory(char *startp, char *endp,
 
 int set_direct_map_invalid_noflush(struct page *page, unsigned int nr);
 int set_direct_map_default_noflush(struct page *page, unsigned int nr);
-int set_direct_map_valid_noflush(struct page *page, unsigned nr, bool valid);
 bool kernel_page_present(struct page *page);
 
 #endif /* __ASSEMBLER__ */
