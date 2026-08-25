cdee6cae0c64b45e14b80cf9be11aa237d2ce2d2
Zain Aboobacker <zainaboobacker33@gmail.com>
2026-08-24 20:22:40 -0400

    media: saa7164: remove register dump debugging helper
    saa7164_dumpregs() dumps device register contents during initialization
    for debugging purposes. The helper has a single caller and is explicitly
    marked for removal.
    
    Remove the call, helper, and declaration.
    
    Signed-off-by: Zain Aboobacker <zainaboobacker33@gmail.com>
    Signed-off-by: Hans Verkuil <hverkuil+cisco@kernel.org>

--- drivers/media/pci/saa7164/saa7164-core.c
+++ drivers/media/pci/saa7164/saa7164-core.c
@@ -742,35 +742,6 @@ u32 saa7164_getcurrentfirmwareversion(struct saa7164_dev *dev)
 	return reg;
 }
 
-/* TODO: Debugging func, remove */
-void saa7164_dumpregs(struct saa7164_dev *dev, u32 addr)
-{
-	int i;
-
-	dprintk(1, "--------------------> 00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f\n");
-
-	for (i = 0; i < 0x100; i += 16)
-		dprintk(1, "region0[0x%08x] = %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x\n",
-			i,
-			(u8)saa7164_readb(addr + i + 0),
-			(u8)saa7164_readb(addr + i + 1),
-			(u8)saa7164_readb(addr + i + 2),
-			(u8)saa7164_readb(addr + i + 3),
-			(u8)saa7164_readb(addr + i + 4),
-			(u8)saa7164_readb(addr + i + 5),
-			(u8)saa7164_readb(addr + i + 6),
-			(u8)saa7164_readb(addr + i + 7),
-			(u8)saa7164_readb(addr + i + 8),
-			(u8)saa7164_readb(addr + i + 9),
-			(u8)saa7164_readb(addr + i + 10),
-			(u8)saa7164_readb(addr + i + 11),
-			(u8)saa7164_readb(addr + i + 12),
-			(u8)saa7164_readb(addr + i + 13),
-			(u8)saa7164_readb(addr + i + 14),
-			(u8)saa7164_readb(addr + i + 15)
-			);
-}
-
 static void saa7164_dump_hwdesc(struct saa7164_dev *dev)
 {
 	dprintk(1, "@0x%p hwdesc sizeof(struct tmComResHWDescr) = %d bytes\n",
@@ -1344,7 +1315,6 @@ static int saa7164_initdev(struct pci_dev *pci_dev,
 		}
 
 		saa7164_get_descriptors(dev);
-		saa7164_dumpregs(dev, 0);
 		saa7164_getcurrentfirmwareversion(dev);
 		saa7164_getfirmwarestatus(dev);
 		err = saa7164_bus_setup(dev);
