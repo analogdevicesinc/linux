4664d8d77317d4d35d796523a6f796e3ce8eed47
Muchun Song <songmuchun@bytedance.com>
2026-08-25 16:46:04 +0800

    mm/sparse: remove section_map_size()
    section_map_size() no longer provides any shared logic.
    
    After the sparse-vmemmap changes, its only remaining user is the
    !CONFIG_SPARSEMEM_VMEMMAP path in __populate_section_memmap(), which can
    compute the size inline with PAGE_ALIGN(sizeof(struct page) *
    PAGES_PER_SECTION).
    
    Remove section_map_size() and inline the remaining calculation.
    
    Link: https://lore.kernel.org/20260825084608.47437-14-songmuchun@bytedance.com
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Acked-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: David Laight <david.laight.linux@gmail.com>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Qi Zheng <qi.zheng@linux.dev>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/sparse.c
+++ mm/sparse.c
@@ -209,23 +209,12 @@ void __init sparse_sections_init(void)
 		memory_present(nid, start, end);
 }
 
-#ifdef CONFIG_SPARSEMEM_VMEMMAP
-unsigned long __init section_map_size(void)
-{
-	return ALIGN(sizeof(struct page) * PAGES_PER_SECTION, PMD_SIZE);
-}
-
-#else
-unsigned long __init section_map_size(void)
-{
-	return PAGE_ALIGN(sizeof(struct page) * PAGES_PER_SECTION);
-}
-
+#ifndef CONFIG_SPARSEMEM_VMEMMAP
 struct page __init *__populate_section_memmap(unsigned long pfn,
 		unsigned long nr_pages, int nid, struct vmem_altmap *altmap,
 		struct dev_pagemap *pgmap)
 {
-	unsigned long size = section_map_size();
+	unsigned long size = PAGE_ALIGN(sizeof(struct page) * PAGES_PER_SECTION);
 
 	return memmap_alloc(size, size, __pa(MAX_DMA_ADDRESS), nid, false);
 }
