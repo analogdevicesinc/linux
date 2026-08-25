7fea128f6b829ad834f21834d2605d28a45b903d
Chandrakanth Patil <chandrakanth.patil@broadcom.com>
2026-08-26 02:34:01 +0530

    scsi: mpi3mr: Fix out-of-bounds sense buffer access
    The sense buffer address reported on completion is turned into a virtual
    address with no range check, so an address outside the pool resolves to
    memory that does not belong to it.
    
    Check that it lies within the pool and is correctly aligned.
    
    Fixes: 824a156633df ("scsi: mpi3mr: Base driver code")
    Signed-off-by: Chandrakanth Patil <chandrakanth.patil@broadcom.com>
    Link: https://patch.msgid.link/20260825210411.301535-8-chandrakanth.patil@broadcom.com
    Signed-off-by: Martin K. Petersen (Oracle) <mkp@kernel.org>

--- drivers/scsi/mpi3mr/mpi3mr_fw.c
+++ drivers/scsi/mpi3mr/mpi3mr_fw.c
@@ -142,7 +142,17 @@ void *mpi3mr_get_reply_virt_addr(struct mpi3mr_ioc *mrioc,
 void *mpi3mr_get_sensebuf_virt_addr(struct mpi3mr_ioc *mrioc,
 	dma_addr_t phys_addr)
 {
+	u64 offset;
+
 	if (!phys_addr)
+		return NULL;
+
+	offset = phys_addr - mrioc->sense_buf_dma;
+
+	if (phys_addr < mrioc->sense_buf_dma ||
+	    (phys_addr > mrioc->sense_buf_dma +
+	     (mrioc->num_sense_bufs * MPI3MR_SENSE_BUF_SZ) - MPI3MR_SENSE_BUF_SZ) ||
+	    do_div(offset, MPI3MR_SENSE_BUF_SZ))
 		return NULL;
 
 	return mrioc->sense_buf + (phys_addr - mrioc->sense_buf_dma);
