53c34396c19e45cd45e6b20c47ef9f47f289698b
Sean Christopherson <seanjc@google.com>
2026-08-26 09:56:47 -0700

    KVM: guest_memfd: Elaborate on how release() vs. get_pfn() is safe against UAF
    Add more context and information to the comment in kvm_gmem_release() that
    explains why there's no synchronization on RCU _or_ kvm->srcu.  Point (b)
    from commit 67b43038ce14 ("KVM: guest_memfd: Remove RCU-protected attribute
    from slot->gmem.file")
    
      b) kvm->srcu ensures that kvm_gmem_unbind() and freeing of a memslot
         occur after the memslot is no longer visible to kvm_gmem_get_pfn().
    
    is especially difficult to fully grok, particularly in light of commit
    ae431059e75d ("KVM: guest_memfd: Remove bindings on memslot deletion when
    gmem is dying"), which addressed a race between unbind() and release().
    
    See the extended on-list discussion[*] for more details about exactly what
    KVM guards against, and how.
    
    Opportunistically update the comment in kvm_gmem_unbind() to clarify the
    rules for unbinding, i.e. to be more explicit about why it's ok to nullify
    the file pointer even though kvm_gmem_get_pfn() doesn't take the invalidate
    lock.
    
    No functional change intended.
    
    Link: https://lore.kernel.org/all/CAEvNRgGmyd1yqQXsnz5hWRZpBZUs%3DpiEWbEaqP9%2Bcz9ZqEMQ6g@mail.gmail.com [*]
    Cc: Yan Zhao <yan.y.zhao@intel.com>
    Cc: Vishal Annapurve <vannapurve@google.com>
    Reviewed-by: Yan Zhao <yan.y.zhao@intel.com>
    Link: https://patch.msgid.link/20260826165647.769231-1-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- virt/kvm/guest_memfd.c
+++ virt/kvm/guest_memfd.c
@@ -300,17 +300,56 @@ static int kvm_gmem_release(struct inode *inode, struct file *file)
 	 * dereferencing the slot for existing bindings needs to be protected
 	 * against memslot updates, specifically so that unbind doesn't race
 	 * and free the memslot (kvm_gmem_get_file() will return NULL).
-	 *
-	 * Since .release is called only when the reference count is zero,
-	 * after which file_ref_get() and get_file_active() fail,
-	 * kvm_gmem_get_pfn() cannot be using the file concurrently.
-	 * file_ref_put() provides a full barrier, and get_file_active() the
-	 * matching acquire barrier.
 	 */
 	mutex_lock(&kvm->slots_lock);
 
 	filemap_invalidate_lock(inode->i_mapping);
 
+	/*
+	 * Note!  synchronize_srcu() is _not_ needed after nullifying memslot
+	 * bindings as slot->gmem.file cannot be set back to a non-null value
+	 * without the memslot first being deleted.  I.e. this relies on the
+	 * synchronize_srcu_expedited() in kvm_swap_active_memslots() to ensure
+	 * kvm_gmem_get_pfn() (which runs with kvm->srcu held for read) can't
+	 * grab a reference to slot->gmem.file even if the struct file object
+	 * is reallocated (for use as a different file).
+	 *
+	 * file_ref_put() provides a full barrier, and __get_file_rcu() the
+	 * matching acquire barrier, to ensure that kvm_gmem_get_file() (via
+	 * __get_file_rcu()) sees refcount==0 or fails the "file reloaded"
+	 * check (file != NULL due to nullifying the file pointer here).
+	 *
+	 * Unlike most other users of get_file_rcu(), where callers don't care
+	 * if they race with a write, only that they have a reference to _a_
+	 * live file, kvm_gmem_get_pfn() needs to get the exact file that is
+	 * associated with the memslot.  Without the aforementioned SRCU
+	 * synchronization, the following could happen:
+	 *
+	 *  CPU0				CPU1
+	 *  kvm_gmem_get_pfn()
+	 *    slot->gmem.file == A
+	 *					kvm_gmem_release())
+	 *					  slot->gmem.file = NULL
+	 *
+	 *					kvm_set_memory_region()
+	 *					  slot deleted
+	 *
+	 *					kvm_set_memory_region()
+	 *					  slot created
+	 *					  slot->gmem.file = B (alloc the same object)
+	 *
+	 *  get_file_active()
+	 *    file = B
+	 *    file_reloaded = B
+	 *
+	 * <KVM does weird things with an old memslot + new file>
+	 *
+	 * Obviously KVM would be broken in many places if the synchronization
+	 * were omitted, but it's important to note that get_file_active() does
+	 * NOT guarantee a reference to the "original" file was obtained, only
+	 * that it grabbed a reference for the returned file, i.e. didn't grab
+	 * a reference for file A, but then returned a pointer to file B.
+	 */
 	xa_for_each(&f->bindings, index, slot)
 		WRITE_ONCE(slot->gmem.file, NULL);
 
@@ -698,8 +737,9 @@ void kvm_gmem_unbind(struct kvm_memory_slot *slot)
 	xa_store_range(&f->bindings, start, end - 1, NULL, GFP_KERNEL);
 
 	/*
-	 * synchronize_srcu(&kvm->srcu) ensured that kvm_gmem_get_pfn()
-	 * cannot see this memslot.
+	 * Note, the caller is responsible for ensuring the slot is unreachable
+	 * before unbinding, e.g. by synchronizing SRCU after deleting the slot,
+	 * to guarantee kvm_gmem_get_pfn() can't see the slot+file.
 	 */
 	WRITE_ONCE(slot->gmem.file, NULL);
 	filemap_invalidate_unlock(file->f_mapping);
