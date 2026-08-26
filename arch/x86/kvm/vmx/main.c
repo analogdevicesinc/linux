1a66112cbc0214743c0d6d52427609379ac858f7
Nikolay Borisov <nik.borisov@suse.com>
2026-08-26 13:03:21 +0300

    KVM: TDX: Remove always true ifdef
    CONFIG_KVM_INTEL_TDX depends on KVM_TDX_HOST, which in turn depends on
    CONFIG_X86_64. So it's guaranteed that any code under ifdef
    CONFIG_KVM_INTEL_TDX is also running under CONFIG_X86_64 being true,
    remove pointless ifdef guard.
    
    Signed-off-by: Nikolay Borisov <nik.borisov@suse.com>
    Link: https://patch.msgid.link/20260826100321.1309685-1-nik.borisov@suse.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/vmx/main.c
+++ arch/x86/kvm/vmx/main.c
@@ -803,7 +803,6 @@ static void vt_write_tsc_multiplier(struct kvm_vcpu *vcpu)
 	vmx_write_tsc_multiplier(vcpu);
 }
 
-#ifdef CONFIG_X86_64
 static int vt_set_hv_timer(struct kvm_vcpu *vcpu, u64 guest_deadline_tsc,
 			      bool *expired)
 {
@@ -822,7 +821,6 @@ static void vt_cancel_hv_timer(struct kvm_vcpu *vcpu)
 
 	vmx_cancel_hv_timer(vcpu);
 }
-#endif
 
 static void vt_setup_mce(struct kvm_vcpu *vcpu)
 {
