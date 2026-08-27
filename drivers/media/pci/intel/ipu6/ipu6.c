b3c78e2d3e79ae1f6d5ae641b05aedba27904f00
Antti Laakso <antti.laakso@linux.intel.com>
2026-08-27 15:33:26 +0300

    media: ipu6: The VC arbitration mechanism is ipu6 only
    The ipu7 doesn't support the VC arbitration mechanism that exist in
    ipu6.
    
    Signed-off-by: Antti Laakso <antti.laakso@linux.intel.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- drivers/media/pci/intel/ipu6/ipu6.c
+++ drivers/media/pci/intel/ipu6/ipu6.c
@@ -447,7 +447,12 @@ static int ipu6_pci_config_setup(struct pci_dev *dev)
 
 static void ipu6_configure_vc_mechanism(struct ipu6_device *isp)
 {
-	u32 val = readl(isp->base + BUTTRESS_REG_BTRS_CTRL);
+	u32 val;
+
+	if (IS_IPU7(isp))
+		return;
+
+	val = readl(isp->base + BUTTRESS_REG_BTRS_CTRL);
 
 	if (IPU6_BTRS_ARB_STALL_MODE_VC0 == IPU6_BTRS_ARB_MODE_TYPE_STALL)
 		val |= BUTTRESS_REG_BTRS_CTRL_STALL_MODE_VC0;
