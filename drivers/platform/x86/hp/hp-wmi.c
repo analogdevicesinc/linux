162053ad8885a351140b2128e67fb5a53ab1c2d5
Suryansh Singh <technosfan14@gmail.com>
2026-08-27 20:52:26 +0530

    platform/x86: hp-wmi: Add OMEN board 8D40 thermal profile support
    The HP OMEN Slim 16t-an000 (board ID: 8D40) uses the existing OMEN v1
    WMI interface but does not use the standard EC thermal profile
    parameters.
    
    Furthermore, it is the closest sibling of board 8D41 and 8D42, which
    are already included in the table with omen_v1_no_ec_board_params.
    
    Add the DMI board name to hp_wmi_feature_boards[] and map it to
    omen_v1_no_ec_board_params.
    
    This enables the existing board-specific handling for 8D40, including
    platform profile and fan control support.
    
    (Tested-by tag not included due to invalid email address, see the
    github link below.)
    
    Link: https://github.com/CachyOS/kernel-patches/issues/157
    Signed-off-by: Suryansh Singh <technosfan14@gmail.com>
    Link: https://patch.msgid.link/20260827152226.35186-1-technosfan14@gmail.com
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

--- drivers/platform/x86/hp/hp-wmi.c
+++ drivers/platform/x86/hp/hp-wmi.c
@@ -324,6 +324,10 @@ static const struct dmi_system_id hp_wmi_feature_boards[] __initconst = {
 		.driver_data = (void *)&omen_v1_legacy_board_params,
 	},
 	{
+		.matches = { DMI_MATCH(DMI_BOARD_NAME, "8D40") },
+		.driver_data = (void *)&omen_v1_no_ec_board_params,
+	},
+	{
 		.matches = { DMI_MATCH(DMI_BOARD_NAME, "8D41") },
 		.driver_data = (void *)&omen_v1_no_ec_board_params,
 	},
