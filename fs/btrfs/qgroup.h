6232f4e2a0ba34c5f9db5b727d06033c026decc1
Qu Wenruo <wqu@suse.com>
2026-08-27 16:25:30 +0930

    btrfs: avoid long stall when dropping a non-shared large subvolume
    Commit 011b46c30476 ("btrfs: skip subtree scan if it's too high to avoid
    low stall in btrfs_commit_transaction()") introduced a mechanism to skip
    large subtree during snapshot dropping.
    
    But even for a subvolume without any shared tree blocks, we can still
    queue quite a lot of qgroup records into one transaction, and cause a
    long qgroup related stall.
    
    So also add a check against the subvolume root level, to determine if we
    need to mark qgroup inconsistent.
    
    Reviewed-by: Boris Burkov <boris@bur.io>
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/qgroup.h
+++ fs/btrfs/qgroup.h
@@ -376,6 +376,7 @@ int btrfs_qgroup_trace_leaf_items(struct btrfs_trans_handle *trans,
 int btrfs_qgroup_trace_subtree(struct btrfs_trans_handle *trans,
 			       struct extent_buffer *root_eb,
 			       u64 root_gen, int root_level);
+void btrfs_qgroup_check_tree_drop(struct btrfs_fs_info *fs_info, u64 rootid, u8 level);
 int btrfs_qgroup_account_extent(struct btrfs_trans_handle *trans, u64 bytenr,
 				u64 num_bytes, struct ulist *old_roots,
 				struct ulist *new_roots);
