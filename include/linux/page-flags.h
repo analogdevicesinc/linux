c1202fabee300cb43843b1f55ae6d6d4c0987ff8
Tal Zussman <tz2294@columbia.edu>
2026-08-29 15:09:01 -0400

    mm: remove PageReclaim
    This flag is now only used on folios, so we can remove all the page
    accessors.  folio_test_clear_reclaim() is not used, so don't add
    FOLIO_TEST_CLEAR_FLAG() for it.
    
    Link: https://lore.kernel.org/20260829-b4-page_io-folios-v2-2-649728091117@columbia.edu
    Signed-off-by: Tal Zussman <tz2294@columbia.edu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Reviewed-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: Lorenzo Stoakes (ARM) <ljs@kernel.org>
    Acked-by: David Hildenbrand (Arm) <david@kernel.org>

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -593,8 +593,7 @@ TESTPAGEFLAG(Writeback, writeback, PF_NO_TAIL)
 FOLIO_FLAG(mappedtodisk, FOLIO_HEAD_PAGE)
 
 /* PG_readahead is only used for reads; PG_reclaim is only for writes */
-PAGEFLAG(Reclaim, reclaim, PF_NO_TAIL)
-	TESTCLEARFLAG(Reclaim, reclaim, PF_NO_TAIL)
+FOLIO_FLAG(reclaim, FOLIO_HEAD_PAGE)
 FOLIO_FLAG(readahead, FOLIO_HEAD_PAGE)
 	FOLIO_TEST_CLEAR_FLAG(readahead, FOLIO_HEAD_PAGE)
 
