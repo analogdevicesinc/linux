ed2050bc671b44b2c948dbd4347cc534d44ccc9f
Martin Rys <martin@archlinux.org>
2026-08-30 10:02:49 +0200

    USB: Improve "New device found" printout message
    Use a colon instead of a comma, like the debug message after this one
    does.
    
    Do not print a space after `bcdDevice=`. This was introduced as the
    formatting was copied from `lsusb -v` printout, but lsusb output has
    preceding spaces, this message does not, so it ends up looking odd.
    
    Previously the output ended up looking like
    `idProduct=a000, bcdDevice= 1.00` if the first hex character of
    bcdDevice was 0.
    
    Signed-off-by: Martin Rys <martin@archlinux.org>
    Link: https://patch.msgid.link/94a023c9-80de-4c84-85bf-7846de97c240@archlinux.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -2411,7 +2411,7 @@ static void announce_device_ids(struct usb_device *udev)
 	u16 bcdDevice = le16_to_cpu(udev->descriptor.bcdDevice);
 
 	dev_info(&udev->dev,
-		"New USB device found, idVendor=%04x, idProduct=%04x, bcdDevice=%2x.%02x\n",
+		"New USB device found: idVendor=%04x, idProduct=%04x, bcdDevice=%x.%02x\n",
 		le16_to_cpu(udev->descriptor.idVendor),
 		le16_to_cpu(udev->descriptor.idProduct),
 		bcdDevice >> 8, bcdDevice & 0xff);
