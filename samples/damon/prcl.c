6d710a5ac1d26f57a5687e8dd6a8eb31b1e3d8d7
Enze Li <lienze@kylinos.cn>
2026-08-31 07:26:04 -0700

    mm/damon/core: introduce damon_set_target_pid()
    The logic that finds the struct pid for a given pid number and assigns it
    to a damon_target is duplicated in multiple places.  Including
    damon_sysfs_add_target() of mm/damon/sysfs.c and the start functions of
    the two sample modules, samples/damon/wsse.c and samples/damon/prcl.c. 
    Add a function that does the work, and replace the duplicated code in the
    places with calls to the function.
    
    Link: https://lore.kernel.org/20260831142611.77572-3-sj@kernel.org
    Signed-off-by: Enze Li <lienze@kylinos.cn>
    Signed-off-by: SJ Park <sj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: SJ Park <sj@kernel.org>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Hari Mishal <harimishal1@gmail.com>
    Cc: Jaeyeon Lee <jaeyeon.lee.dev@gmail.com>
    Cc: Li Youhong <liyouhong@kylinos.cn>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: "Zenghui Yu (Huawei)" <zenghui.yu@linux.dev>
    Cc: zhaozhengzhuo <zhaozhengzhuo@uniontech.com>

--- samples/damon/prcl.c
+++ samples/damon/prcl.c
@@ -32,7 +32,6 @@ module_param_cb(enabled, &enabled_param_ops, &enabled, 0600);
 MODULE_PARM_DESC(enabled, "Enable or disable DAMON_SAMPLE_PRCL");
 
 static struct damon_ctx *ctx;
-static struct pid *target_pidp;
 
 static int damon_sample_prcl_repeat_call_fn(void *data)
 {
@@ -79,12 +78,10 @@ static int damon_sample_prcl_start(void)
 		return -ENOMEM;
 	}
 	damon_add_target(ctx, target);
-	target_pidp = find_get_pid(target_pid);
-	if (!target_pidp) {
+	if (damon_set_target_pid(target, target_pid)) {
 		damon_destroy_ctx(ctx);
 		return -EINVAL;
 	}
-	target->pid = target_pidp;
 
 	scheme = damon_new_scheme(
 			&(struct damos_access_pattern) {
