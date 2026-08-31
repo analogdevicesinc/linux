d7742f106542bac3d3697ad358816219cbd5b6dc
Joshua Crofts <joshua.crofts1@gmail.com>
2026-08-31 23:13:40 +0200

    iio: temperature: tsys02d: reorder headers
    Sort the headers alphabetically and group the IIO specific headers
    separately.
    
    No functional change.
    
    Signed-off-by: Joshua Crofts <joshua.crofts1@gmail.com>
    Signed-off-by: Jonathan Cameron <jonathan.cameron@oss.qualcomm.com>

--- drivers/iio/temperature/tsys02d.c
+++ drivers/iio/temperature/tsys02d.c
@@ -10,11 +10,12 @@
  *  http://www.meas-spec.com/downloads/Digital_Sensor_TSYS02D.pdf
  */
 
-#include <linux/init.h>
 #include <linux/device.h>
+#include <linux/init.h>
 #include <linux/kernel.h>
-#include <linux/stat.h>
 #include <linux/module.h>
+#include <linux/stat.h>
+
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
 
