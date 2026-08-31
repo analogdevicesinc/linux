d1c4ff5e14e6ac5ba2a77be67d04a553b0934335
Cheng-Han Wu <hank20010209@gmail.com>
2026-08-31 07:57:21 -0700

    mm/damon/core: remove unused damon_targets_empty()
    Patch series "mm/damon/core: remove unused helper functions", v2.
    
    Both damon_targets_empty() and damon_nr_running_ctxs() have had no in-tree
    users since commit 5ec4333b1967 ("mm/damon: remove DAMON debugfs
    interface") removed their remaining callers.
    
    Remove the unused declarations and definitions.
    
    
    This patch (of 2):
    
    damon_targets_empty() has had no in-tree users since commit 5ec4333b1967
    ("mm/damon: remove DAMON debugfs interface") removed its last caller.
    
    Remove the unused declaration and definition.
    
    Link: https://lore.kernel.org/20260831145724.82387-1-sj@kernel.org
    Link: https://lore.kernel.org/20260831145724.82387-2-sj@kernel.org
    Signed-off-by: Cheng-Han Wu <hank20010209@gmail.com>
    Signed-off-by: SJ Park <sj@kernel.org>
    Reviewed-by: SJ Park <sj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/damon.h
+++ include/linux/damon.h
@@ -1055,7 +1055,6 @@ int damos_commit_quota_goals(struct damos_quota *dst, struct damos_quota *src);
 
 struct damon_target *damon_new_target(void);
 void damon_add_target(struct damon_ctx *ctx, struct damon_target *t);
-bool damon_targets_empty(struct damon_ctx *ctx);
 void damon_free_target(struct damon_target *t);
 void damon_destroy_target(struct damon_target *t, struct damon_ctx *ctx);
 unsigned int damon_nr_regions(struct damon_target *t);
