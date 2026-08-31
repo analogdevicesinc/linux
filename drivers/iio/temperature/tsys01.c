ac85c27e16fc7ab7f852428d69d138584106671f
Joshua Crofts <joshua.crofts1@gmail.com>
2026-08-31 23:13:38 +0200

    iio: temperature: tsys01: reorder headers
    Sort the headers alphabetically and group the IIO headers and vendor
    specific header separately.
    
    No functional change.
    
    Signed-off-by: Joshua Crofts <joshua.crofts1@gmail.com>
    Signed-off-by: Jonathan Cameron <jonathan.cameron@oss.qualcomm.com>

--- drivers/iio/temperature/tsys01.c
+++ drivers/iio/temperature/tsys01.c
@@ -8,14 +8,16 @@
  *  http://www.meas-spec.com/downloads/TSYS01_Digital_Temperature_Sensor.pdf
  */
 
-#include <linux/iio/iio.h>
-#include <linux/iio/sysfs.h>
 #include <linux/device.h>
-#include <linux/mutex.h>
-#include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/stat.h>
+
+#include <linux/iio/iio.h>
+#include <linux/iio/sysfs.h>
+
 #include "../common/ms_sensors/ms_sensors_i2c.h"
 
 /* TSYS01 Commands */
