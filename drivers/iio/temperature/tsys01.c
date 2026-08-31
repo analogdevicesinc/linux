988bbca6448476a304ad7e1c799d4c13d9ffa491
Joshua Crofts <joshua.crofts1@gmail.com>
2026-08-31 23:13:39 +0200

    iio: temperature: tsys01: make headers conform to IWYU
    Remove the catch-all kernel.h header, add missing headers and remove
    unused headers.
    
    Signed-off-by: Joshua Crofts <joshua.crofts1@gmail.com>
    Signed-off-by: Jonathan Cameron <jonathan.cameron@oss.qualcomm.com>

--- drivers/iio/temperature/tsys01.c
+++ drivers/iio/temperature/tsys01.c
@@ -8,12 +8,15 @@
  *  http://www.meas-spec.com/downloads/TSYS01_Digital_Temperature_Sensor.pdf
  */
 
+#include <linux/array_size.h>
+#include <linux/bits.h>
 #include <linux/device.h>
-#include <linux/init.h>
-#include <linux/kernel.h>
+#include <linux/i2c.h>
+#include <linux/math64.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
-#include <linux/stat.h>
+#include <linux/sprintf.h>
+#include <linux/types.h>
 
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
