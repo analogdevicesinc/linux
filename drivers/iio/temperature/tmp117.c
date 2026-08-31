be2e8c4e3778a33544f5fd7524993b47714ad613
Joshua Crofts <joshua.crofts1@gmail.com>
2026-08-31 23:13:37 +0200

    iio: temperature: tmp117: make headers conform to IWYU
    Remove the catch-all kernel.h header and add the missing
    <linux/dev_printk.h> header to enforce IWYU.
    
    Signed-off-by: Joshua Crofts <joshua.crofts1@gmail.com>
    Signed-off-by: Jonathan Cameron <jonathan.cameron@oss.qualcomm.com>

--- drivers/iio/temperature/tmp117.c
+++ drivers/iio/temperature/tmp117.c
@@ -12,10 +12,11 @@
 #include <linux/array_size.h>
 #include <linux/bitops.h>
 #include <linux/delay.h>
+#include <linux/dev_printk.h>
 #include <linux/err.h>
 #include <linux/i2c.h>
-#include <linux/kernel.h>
 #include <linux/limits.h>
+#include <linux/minmax.h>
 #include <linux/module.h>
 #include <linux/property.h>
 #include <linux/regulator/consumer.h>
