d2b4e324fc914bb00e08e080c943b4f54026b1cf
Anshuman Khandual <anshuman.khandual@arm.com>
2026-08-31 11:13:28 +0530

    sh/mm: stop using [p4d|pud|pmd]_ERROR()
    Stop using [p4d|pud|pmd]_ERROR() in __get_pte_phys() as the pgtable entries
    are known to be NULL and hence could not really be accessed.
    
    Link: https://lore.kernel.org/20260831054331.625505-7-anshuman.khandual@arm.com
    Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: David Hildenbrand (Arm) <david@kernel.org>
    Acked-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Cc: Yoshinori Sato <ysato@users.sourceforge.jp>
    Cc: Rich Felker <dalias@libc.org>
    Cc: John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Helge Deller <deller@gmx.de>
    Cc: Huacai Chen <chenhuacai@kernel.org>
    Cc: James Bottomley <james.bottomley@HansenPartnership.com>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Samuel Holland <samuel.holland@sifive.com>
    Cc: WANG Xuerui <kernel@xen0n.name>

--- arch/sh/mm/init.c
+++ arch/sh/mm/init.c
@@ -59,19 +59,19 @@ static pte_t *__get_pte_phys(unsigned long addr)
 
 	p4d = p4d_alloc(NULL, pgd, addr);
 	if (unlikely(!p4d)) {
-		p4d_ERROR(*p4d);
+		pr_err("allocating p4d table failed\n");
 		return NULL;
 	}
 
 	pud = pud_alloc(NULL, p4d, addr);
 	if (unlikely(!pud)) {
-		pud_ERROR(*pud);
+		pr_err("allocating pud table failed\n");
 		return NULL;
 	}
 
 	pmd = pmd_alloc(NULL, pud, addr);
 	if (unlikely(!pmd)) {
-		pmd_ERROR(*pmd);
+		pr_err("allocating pmd table failed\n");
 		return NULL;
 	}
 
