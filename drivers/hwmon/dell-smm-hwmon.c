328cb1381cb5ea460da132fbfe7abcced709a318
Armin Wolf <W_Armin@gmx.de>
2026-09-01 20:18:48 +0200

    hwmon: (dell-smm) Add Dell OptiPlex 7090 to fan control whitelist
    A user reported that the Dell OptiPlex 7090 needs to be whitelisted
    for the special SMM calls necessary for globally enabling/disabling
    BIOS fan control.
    
    Closes: https://github.com/Wer-Wolf/i8kutils/issues/18
    Signed-off-by: Armin Wolf <W_Armin@gmx.de>
    Link: https://patch.msgid.link/20260901181849.241776-1-W_Armin@gmx.de
    Acked-by: Pali Rohár <pali@kernel.org>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/dell-smm-hwmon.c
+++ drivers/hwmon/dell-smm-hwmon.c
@@ -1632,6 +1632,14 @@ static const struct dmi_system_id i8k_whitelist_fan_control[] __initconst = {
 		.driver_data = (void *)&i8k_fan_control_data[I8K_FAN_34A3_35A3],
 	},
 	{
+		.ident = "Dell Optiplex 7090",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Dell Inc."),
+			DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "OptiPlex 7090"),
+		},
+		.driver_data = (void *)&i8k_fan_control_data[I8K_FAN_30A3_31A3],
+	},
+	{
 		.ident = "Dell XPS 9315",
 		.matches = {
 			DMI_MATCH(DMI_SYS_VENDOR, "Dell Inc."),
