7a4deb3ba1b92c327012bd0dd2232811e1e0d905
Sean Young <sean@mess.org>
2026-09-01 15:12:02 +0100

    media: ir_toy: Remove unused struct field
    Never assigned or read.
    
    Signed-off-by: Sean Young <sean@mess.org>
    Acked-by: Hans Verkuil <hverkuil+cisco@kernel.org>

--- drivers/media/rc/ir_toy.c
+++ drivers/media/rc/ir_toy.c
@@ -55,7 +55,6 @@ enum state {
 
 struct irtoy {
 	struct device *dev;
-	struct usb_device *usbdev;
 
 	struct rc_dev *rc;
 	struct urb *urb_in, *urb_out;
@@ -441,7 +440,6 @@ static int irtoy_probe(struct usb_interface *intf,
 			  irtoy_out_callback, irtoy);
 
 	irtoy->dev = &intf->dev;
-	irtoy->usbdev = usbdev;
 	irtoy->rc = rc;
 	irtoy->urb_out = urb;
 	irtoy->pulse = true;
