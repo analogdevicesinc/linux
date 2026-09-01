4fe4d3dc5c20c62789c718946bdb4f72a7cf363d
Waqar Hameed <waqar.hameed@axis.com>
2026-09-01 21:35:08 +0200

    power: supply: Add sysfs entry for system load control
    There are devices that can control the connection from power to system
    load. For example, with a field-effect transistor between a battery and
    the system load (BATFET). Drivers for these devices are currently
    enrolling their own custom `sysfs` property to control this.
    
    In order to unify this, add a new `sysfs` entry for controlling such
    switch and corresponding `power_supply_property` with `enum` values. The
    obvious states are "on" and "off", i.e. there is a connection or not,
    respectively. However, many devices can also enter special modes such as
    "low-power", "shipping" or "deep sleep".
    
    Since the members in `struct power_supply_desc` already are ordered,
    adding `load_switches` only increases the size with 4 bytes, as can be
    seen from `pahole`:
    
      struct power_supply_desc {
              const char  *              name;                 /*     0     4 */
              enum power_supply_type     type;                 /*     4     4 */
              u32                        charge_types;         /*     8     4 */
              u32                        usb_types;            /*    12     4 */
              u32                        load_switches;        /*    16     4 */
              const enum power_supply_property  * properties;  /*    20     4 */
              size_t                     num_properties;       /*    24     4 */
              int                        (*get_property)(...); /*    28     4 */
              int                        (*set_property)(...); /*    32     4 */
              int                        (*property_is_writeable)(...);  /*    36     4 */
              void                       (*external_power_changed)(...); /*    40     4 */
              int                        (*init)(struct power_supply *); /*    44     4 */
              int                        use_for_apm;          /*    48     4 */
              bool                       no_thermal;           /*    52     1 */
              u8                         charge_behaviours;    /*    53     1 */
    
              /* size: 56, cachelines: 1, members: 15 */
              /* padding: 2 */
              /* last cacheline: 56 bytes */
      };
    
    Signed-off-by: Waqar Hameed <waqar.hameed@axis.com>
    Link: https://patch.msgid.link/d86f528de9a058091f806d9676639239cfeaf805.1788290289.git.waqar.hameed@axis.com
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -104,6 +104,14 @@ enum {
 	POWER_SUPPLY_SCOPE_DEVICE,
 };
 
+enum {
+	POWER_SUPPLY_LOAD_SWITCH_UNKNOWN = 0,
+	POWER_SUPPLY_LOAD_SWITCH_ON,
+	POWER_SUPPLY_LOAD_SWITCH_OFF,
+	POWER_SUPPLY_LOAD_SWITCH_STANDBY,
+	POWER_SUPPLY_LOAD_SWITCH_SHIP,
+};
+
 enum power_supply_property {
 	/* Properties of type `int' */
 	POWER_SUPPLY_PROP_STATUS = 0,
@@ -182,6 +190,7 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_MANUFACTURE_DAY,
 	POWER_SUPPLY_PROP_INTERNAL_RESISTANCE,
 	POWER_SUPPLY_PROP_STATE_OF_HEALTH,
+	POWER_SUPPLY_PROP_LOAD_SWITCH,
 	/* Properties of type `const char *' */
 	POWER_SUPPLY_PROP_MODEL_NAME,
 	POWER_SUPPLY_PROP_MANUFACTURER,
@@ -261,6 +270,7 @@ struct power_supply_desc {
 	enum power_supply_type type;
 	u32 charge_types;
 	u32 usb_types;
+	u32 load_switches;
 	const enum power_supply_property *properties;
 	size_t num_properties;
 
