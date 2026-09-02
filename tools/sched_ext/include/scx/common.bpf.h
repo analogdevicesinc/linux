a8dbd76180bb000d74be06521e06ed4d4170f405
Kefeng Wang <wangkefeng.wang@huawei.com>
2026-09-02 21:16:53 +0800

    mm: replace PF_KCOMPACTD flag with kthread_func() check
    PF_KCOMPACTD was introduced by commit ce6d9c1c2b5c ("NFS: fix
    nfs_release_folio() to not deadlock via kcompactd writeback") so
    nfs_release_folio() could detect kcompactd context and skip writeback. 
    The flag is only consumed by current_is_kcompactd(), whose sole caller is
    nfs_release_folio().
    
    Replace the flag-based check with kthread_func(current) == kcompactd,
    freeing the 0x00010000 PF flag bit.
    
    Link: https://lore.kernel.org/20260902131653.1338227-5-wangkefeng.wang@huawei.com
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: Zi Yan <ziy@nvidia.com>
    Acked-by: Vlastimil Babka (SUSE) <vbabka@kernel.org>
    Cc: Brendan Jackman <brendan.jackman@linux.dev>
    Cc: Carlos Maiolino <cem@kernel.org>
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: "Darrick J. Wong" <djwong@kernel.org>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- tools/sched_ext/include/scx/common.bpf.h
+++ tools/sched_ext/include/scx/common.bpf.h
@@ -31,7 +31,6 @@
 #define PF_IDLE				0x00000002	/* I am an IDLE thread */
 #define PF_IO_WORKER			0x00000010	/* Task is an IO worker */
 #define PF_WQ_WORKER			0x00000020	/* I'm a workqueue worker */
-#define PF_KCOMPACTD			0x00010000      /* I am kcompactd */
 #define PF_KTHREAD			0x00200000	/* I am a kernel thread */
 #define PF_EXITING			0x00000004
 #define CLOCK_MONOTONIC			1
