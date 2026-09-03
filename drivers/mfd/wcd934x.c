5a5f978cf3cd2f50b9e123f851f5c6577519ee89
David Heidelberg <david@ixit.cz>
2026-09-03 16:42:05 +0200

    mfd: wcd934x: set DMA mask on parent to silence "DMA mask not set"
    The wcd934x MFD parent (a slim_device) never initializes its
    dma_mask/coherent_dma_mask. When the DT-aware children pass through
    of_dma_configure() this triggers the "DMA mask not set" warning for
    each child.
    
    Cc: stable@vger.kernel.org
    Fixes: f959dcd6ddfd ("dma-direct: Fix potential NULL pointer dereference")
    Assisted-by: tencent:hy3
    Signed-off-by: David Heidelberg <david@ixit.cz>
    Link: https://patch.msgid.link/20260903-wcd-dma-v2-1-189cff560a28@ixit.cz
    Signed-off-by: Lee Jones <lee@kernel.org>

--- drivers/mfd/wcd934x.c
+++ drivers/mfd/wcd934x.c
@@ -2,6 +2,7 @@
 // Copyright (c) 2019, Linaro Limited
 
 #include <linux/clk.h>
+#include <linux/dma-mapping.h>
 #include <linux/gpio/consumer.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
@@ -216,6 +217,11 @@ static int wcd934x_slim_probe(struct slim_device *sdev)
 	struct wcd934x_ddata *ddata;
 	struct gpio_desc *reset_gpio;
 	int ret;
+
+	ret = dma_coerce_mask_and_coherent(dev, DMA_BIT_MASK(32));
+	if (ret)
+		return dev_err_probe(dev, ret,
+				     "Failed to set DMA mask\n");
 
 	ddata = devm_kzalloc(dev, sizeof(*ddata), GFP_KERNEL);
 	if (!ddata)
