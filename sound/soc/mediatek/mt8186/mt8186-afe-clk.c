cb552c6b9abe02f254d44d8c29d0dce1ae21f69f
bui duc phuc <phucduc.bui@gmail.com>
2026-09-03 12:11:46 +0700

    ASoC: mediatek: mt8186: Fix APLL enable error handling
    Currently, the mt8186_apll*_enable() functions call mux_setting(afe, true)
    but do not check its return value to handle failures.
    
    In addition, the cleanup paths of mt8186_apll*_enable() do not call
    mux_setting(afe, false) when the enable operation fails, while the
    mt8186_apll*_disable() functions do.
    
    Add error handling for apll*_mux_setting() and call mux_setting(afe, false)
    in the cleanup paths when mt8186_apll*_enable() fails.
    
    Fixes: 55b423d5623c ("ASoC: mediatek: mt8186: support audio clock control in platform driver")
    Signed-off-by: bui duc phuc <phucduc.bui@gmail.com>
    Link: https://patch.msgid.link/20260903051150.178057-5-phucduc.bui@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/mediatek/mt8186/mt8186-afe-clk.c
+++ sound/soc/mediatek/mt8186/mt8186-afe-clk.c
@@ -353,7 +353,9 @@ int mt8186_apll1_enable(struct mtk_base_afe *afe)
 	int ret;
 
 	/* setting for APLL */
-	apll1_mux_setting(afe, true);
+	ret = apll1_mux_setting(afe, true);
+	if (ret)
+		return ret;
 
 	ret = clk_prepare_enable(afe_priv->clk[CLK_APLL22M]);
 	if (ret) {
@@ -378,9 +380,9 @@ int mt8186_apll1_enable(struct mtk_base_afe *afe)
 	return 0;
 
 err_clk_apll1_tuner:
-	clk_disable_unprepare(afe_priv->clk[CLK_APLL1_TUNER]);
-err_clk_apll22m:
 	clk_disable_unprepare(afe_priv->clk[CLK_APLL22M]);
+err_clk_apll22m:
+	apll1_mux_setting(afe, false);
 
 	return ret;
 }
@@ -406,7 +408,9 @@ int mt8186_apll2_enable(struct mtk_base_afe *afe)
 	int ret;
 
 	/* setting for APLL */
-	apll2_mux_setting(afe, true);
+	ret = apll2_mux_setting(afe, true);
+	if (ret)
+		return ret;
 
 	ret = clk_prepare_enable(afe_priv->clk[CLK_APLL24M]);
 	if (ret) {
@@ -431,9 +435,9 @@ int mt8186_apll2_enable(struct mtk_base_afe *afe)
 	return 0;
 
 err_clk_apll2_tuner:
-	clk_disable_unprepare(afe_priv->clk[CLK_APLL2_TUNER]);
-err_clk_apll24m:
 	clk_disable_unprepare(afe_priv->clk[CLK_APLL24M]);
+err_clk_apll24m:
+	apll2_mux_setting(afe, false);
 
 	return ret;
 }
