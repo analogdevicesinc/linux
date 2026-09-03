356892abad7751501cbe4d0b9e1bd61647279f3c
Pawel Zalewski (The Capable Hub) <pzalewski@thegoodpenguin.co.uk>
2026-09-03 16:42:16 +0100

    crypto: ccp - use named initializers for acpi_device_id
    Use a named initializer for the acpi_device_id fields which
    makes the code more readable and consistent with how lists
    are initialized in the rest of the kernel code base.
    
    While we are at it - unify the list terminator to have
    a single space between the brackets and no trailing
    comma.
    
    Signed-off-by: Pawel Zalewski (The Capable Hub) <pzalewski@thegoodpenguin.co.uk>
    Acked-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ccp/sp-platform.c
+++ drivers/crypto/ccp/sp-platform.c
@@ -40,8 +40,8 @@ static const struct sp_dev_vdata dev_vdata[] = {
 };
 
 static const struct acpi_device_id sp_acpi_match[] = {
-	{ "AMDI0C00", (kernel_ulong_t)&dev_vdata[0] },
-	{ },
+	{ .id = "AMDI0C00", .driver_data = (kernel_ulong_t)&dev_vdata[0] },
+	{ }
 };
 MODULE_DEVICE_TABLE(acpi, sp_acpi_match);
 
