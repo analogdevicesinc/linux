64adb67e4568261e1eddc1fd4df47ad4a98758b3
Geert Uytterhoeven <geert+renesas@glider.be>
2026-09-03 11:00:41 +0200

    net: phy: air_en8811h: Make sure clk_init_data is fully initialized
    The clk_init_data structure contains several mutually-exclusive members
    for different methods to specify the possible parents of a clock,
    prompting drivers to initialize only the members they need.  However,
    not initializing all members may cause subtle issues, which are only
    exposed when CONFIG_INIT_STACK_ALL_PATTERN or CONFIG_INIT_STACK_NONE is
    enabled.
    
    Make sure all members are fully initialized, to avoid such bugs, and to
    prevent future breakage when converting drivers to a different method
    for specifying the parents.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Brian Masney <bmasney@redhat.com>
    Link: https://patch.msgid.link/0e869f49c910ce054aa22ae84480aab69420bb92.1788425654.git.geert+renesas@glider.be
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- drivers/net/phy/air_en8811h.c
+++ drivers/net/phy/air_en8811h.c
@@ -938,7 +938,7 @@ static const struct clk_ops an8811hb_clk_ops = {
 
 static int an8811hb_clk_provider_setup(struct device *dev, struct clk_hw *hw)
 {
-	struct clk_init_data init;
+	struct clk_init_data init = {};
 	int ret;
 
 	if (!IS_ENABLED(CONFIG_COMMON_CLK))
@@ -1037,7 +1037,7 @@ static const struct clk_ops en8811h_clk_ops = {
 
 static int en8811h_clk_provider_setup(struct device *dev, struct clk_hw *hw)
 {
-	struct clk_init_data init;
+	struct clk_init_data init = {};
 	int ret;
 
 	if (!IS_ENABLED(CONFIG_COMMON_CLK))
