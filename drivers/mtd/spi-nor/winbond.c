6397f5f8f8754218dd3a059d39eba6b57d04dc3d
Miquel Raynal <miquel.raynal@bootlin.com>
2026-09-04 20:12:59 +0200

    mtd: spi-nor: winbond: Add support for W25Q51RV-M
    There is an ID collision with the chip of same density from the JV
    family. Both chips are very similar in practice, it is mostly a matter
    of electrical differences (mostly power consumption being lower).
    
    As a significant difference, RV chips identify themselves as supporting
    the new SFDP (rev F) field which forces an alternate write SR2
    opcode (0x31).
    
    Reviewed-by: Takahiro Kuwano <takahiro.kuwano@infineon.com>
    Reviewed-by: Michael Walle <mwalle@kernel.org>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Signed-off-by: Michael Walle <mwalle@kernel.org>

--- drivers/mtd/spi-nor/winbond.c
+++ drivers/mtd/spi-nor/winbond.c
@@ -393,7 +393,7 @@ static const struct flash_info winbond_nor_parts[] = {
 		.flags = SPI_NOR_QUAD_PP | SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB |
 			 SPI_NOR_TB_SR_BIT6 | SPI_NOR_4BIT_BP | SPI_NOR_HAS_CMP,
 	}, {
-		/* W25Q512JV-M */
+		/* W25Q512JV-M, W25Q51RV-M */
 		.id = SNOR_ID(0xef, 0x70, 0x20),
 		.flags = SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB | SPI_NOR_TB_SR_BIT6 |
 			 SPI_NOR_4BIT_BP | SPI_NOR_HAS_CMP,
