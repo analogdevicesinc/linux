fc9f75c1457b9ea8f80db77c8292b2ed84116589
Miquel Raynal <miquel.raynal@bootlin.com>
2026-09-04 19:19:04 +0200

    mtd: spi-nor: winbond: W25Q32JV-M: Add quad page program capability
    This chip does not advertise it correctly but it supports 1-1-4 page
    programs.
    
    Before:
    
    $ flash_speed /dev/mtd0 -d -c10
    eraseblock write speed is 2612 KiB/s
    page write speed is 2509 KiB/s
    2 page write speed is 2560 KiB/s
    
    After:
    
    $ flash_speed /dev/mtd0 -c10 -d
    eraseblock write speed is 3033 KiB/s
    page write speed is 2895 KiB/s
    2 page write speed is 2976 KiB/s
    
    The QUAD_PP capability was historically not supported by non-SFDP chips,
    so a Winbond wide late hook already makes sure the capability remains
    disabled for them, for backward compatibility reasons.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Signed-off-by: Michael Walle <mwalle@kernel.org>

--- drivers/mtd/spi-nor/winbond.c
+++ drivers/mtd/spi-nor/winbond.c
@@ -307,8 +307,8 @@ static const struct flash_info winbond_nor_parts[] = {
 		.id = SNOR_ID(0xef, 0x70, 0x16),
 		.name = "w25q32jv",
 		.size = SZ_4M,
-		.flags = SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB,
 		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
+		.flags = SPI_NOR_QUAD_PP | SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB,
 	}, {
 		/* W25Q64JV-M */
 		.id = SNOR_ID(0xef, 0x70, 0x17),
