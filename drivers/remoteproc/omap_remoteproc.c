fa62b6f795c4091bd6a81c8f3ad99c43edce316b
Andrew Davis <afd@ti.com>
2026-09-04 14:56:33 -0500

    remoteproc: k3-dsp: Fix usage of omap_mbox_message and mbox_msg_t
    The type of message sent using omap-mailbox is always u32. The definition
    of mbox_msg_t is uintptr_t which is wrong as that type changes based on
    the architecture (32bit vs 64bit). Make the type fixed to u32.
    
    This then means we need to fix the helper macro omap_mbox_message so it
    doesn't cast to u32 twice. What this macro should be doing is converting
    from the message type returned from the mailbox framework into the
    omap-mailbox type. When fixing this we should add a macro for the other
    direction (from omap-mailbox type to something that can be used with
    mbox_send_message).
    
    After these changes, make use of the new macros as appropriate.
    
    Signed-off-by: Andrew Davis <afd@ti.com>
    Reviewed-by: Beleswar Padhi <b-padhi@ti.com>
    Tested-by: Beleswar Padhi <b-padhi@ti.com>
    Link: https://lore.kernel.org/r/20260904195634.2275867-2-afd@ti.com
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- drivers/remoteproc/omap_remoteproc.c
+++ drivers/remoteproc/omap_remoteproc.c
@@ -499,7 +499,7 @@ static void omap_rproc_mbox_callback(struct mbox_client *client, void *data)
 						client);
 	struct device *dev = oproc->rproc->dev.parent;
 	const char *name = oproc->rproc->name;
-	u32 msg = (u32)data;
+	mbox_msg_t msg = omap_mbox_from_message(data);
 
 	dev_dbg(dev, "mbox msg: 0x%x\n", msg);
 
@@ -550,7 +550,7 @@ static void omap_rproc_kick(struct rproc *rproc, int vqid)
 	}
 
 	/* send the index of the triggered virtqueue in the mailbox payload */
-	ret = mbox_send_message(oproc->mbox, (void *)vqid);
+	ret = mbox_send_message(oproc->mbox, omap_mbox_to_message(vqid));
 	if (ret < 0)
 		dev_err(dev, "failed to send mailbox message, status = %d\n",
 			ret);
@@ -628,7 +628,7 @@ static int omap_rproc_start(struct rproc *rproc)
 	 * Note that the reply will _not_ arrive immediately: this message
 	 * will wait in the mailbox fifo until the remote processor is booted.
 	 */
-	ret = mbox_send_message(oproc->mbox, (void *)RP_MBOX_ECHO_REQUEST);
+	ret = mbox_send_message(oproc->mbox, omap_mbox_to_message(RP_MBOX_ECHO_REQUEST));
 	if (ret < 0) {
 		dev_err(dev, "mbox_send_message failed: %d\n", ret);
 		goto put_mbox;
@@ -777,13 +777,13 @@ static int _omap_rproc_suspend(struct rproc *rproc, bool auto_suspend)
 	struct omap_rproc *oproc = rproc->priv;
 	unsigned long to = msecs_to_jiffies(DEF_SUSPEND_TIMEOUT);
 	unsigned long ta = jiffies + to;
-	u32 suspend_msg = auto_suspend ?
+	mbox_msg_t suspend_msg = auto_suspend ?
 				RP_MBOX_SUSPEND_AUTO : RP_MBOX_SUSPEND_SYSTEM;
 	int ret;
 
 	reinit_completion(&oproc->pm_comp);
 	oproc->suspend_acked = false;
-	ret = mbox_send_message(oproc->mbox, (void *)suspend_msg);
+	ret = mbox_send_message(oproc->mbox, omap_mbox_to_message(suspend_msg));
 	if (ret < 0) {
 		dev_err(dev, "PM mbox_send_message failed: %d\n", ret);
 		return ret;
