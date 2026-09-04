4a6aab1f7335dd8a54b768fdbcbcce0ad454d2fa
Miquel Raynal <miquel.raynal@bootlin.com>
2026-09-04 19:19:10 +0200

    mtd: spi-nor: winbond: W25Q128JV-Q/N: Fill locking information
    The SFDP table does not advertize the CMP locking capabilities of the
    device, flag it.
    
    The CMP capability was historically not supported by non-SFDP chips, so
    a Winbond wide late hook already makes sure the bit remains disabled for
    them, for backward compatibility reasons.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Signed-off-by: Michael Walle <mwalle@kernel.org>

--- drivers/mtd/spi-nor/winbond.c
+++ drivers/mtd/spi-nor/winbond.c
@@ -217,8 +217,8 @@ static const struct flash_info winbond_nor_parts[] = {
 		/* Flavors w/ and w/o SFDP. */
 		.name = "w25q128",
 		.size = SZ_16M,
-		.flags = SPI_NOR_QUAD_PP | SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB,
 		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
+		.flags = SPI_NOR_QUAD_PP | SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB | SPI_NOR_HAS_CMP,
 		.fixups = &w25q128_fixups,
 	}, {
 		/* W25Q256JV-Q/N */
