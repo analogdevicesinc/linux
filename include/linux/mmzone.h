a45be2e95b8246bb67804c49312b4997e8050487
Kairui Song <kasong@tencent.com>
2026-09-06 00:51:07 +0800

    mm/mglru: introduce helpers for manipulating gen and refs flags
    Instead of doing bit ops on folio->flags.f, introduce helpers for
    adjusting a folio's refs and generation info, making the code easier to
    debug and understand.
    
    No functional change is intended: some combined atomic operations are
    split into two, which only creates harmless transient states.  There is no
    measurable performance impact, and some paths even look slightly better in
    the generated assembly.
    
    Link: https://lore.kernel.org/20260906-mglru-flags-cleanup-v6-2-9aacbd77d4ca@tencent.com
    Signed-off-by: Kairui Song <kasong@tencent.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Qi Zheng <qi.zheng@linux.dev>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Barry Song <baohua@kernel.org>
    Cc: Axel Rasmussen <axelrasmussen@google.com>
    Cc: Baoquan He <baoquan.he@linux.dev>
    Cc: Chris Li <chrisl@kernel.org>
    Cc: David Hildenbrand (Arm) <david@kernel.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Kairui Song <ryncsn@gmail.com>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Lian Wang <lianux.mm@gmail.com>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Ridong Chen <ridong.chen@linux.dev>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Cc: Wei Xu <weixugc@google.com>
    Cc: Yuanchu Xie <yuanchu@google.com>
    Cc: Yu Zhao <yuzhao@google.com>
    Cc: Zi Yan <ziy@nvidia.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -500,6 +500,7 @@ enum lruvec_flags {
 
 #define LRU_GEN_MASK		((BIT(LRU_GEN_WIDTH) - 1) << LRU_GEN_PGOFF)
 #define LRU_REFS_MASK		((BIT(LRU_REFS_WIDTH) - 1) << LRU_REFS_PGOFF)
+#define LRU_REFS_MAX		BIT(LRU_REFS_WIDTH)
 
 /*
  * For folios accessed multiple times through file descriptors,
