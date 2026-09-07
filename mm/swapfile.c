f8910410d1105ded0b0845186cef6ccda00bca24
Kemeng Shi <shikemeng@huaweicloud.com>
2026-09-07 17:13:54 +0800

    mm, swap: move setup_swap_clusters_info() after SWP_SOLIDSTATE initialization
    In setup_swap_clusters_info(), SWP_SOLIDSTATE is used to decide
    global_cluster allocation.  Move setup_swap_clusters_info() after
    SWP_SOLIDSTATE initialization to avoid unneeded global_cluster allocation.
    
    Link: https://lore.kernel.org/20260907091356.53026-3-shikemeng@huaweicloud.com
    Fixes: 451c6326105b ("mm, swap: clean up swapon process and locking")
    Signed-off-by: Kemeng Shi <shikemeng@huaweicloud.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Luiz Capitulino <luizcap@redhat.com>
    Acked-by: Kairui Song <kasong@tencent.com>
    Cc: Baoquan He <baoquan.he@linux.dev>
    Cc: Barry Song <baohua@kernel.org>
    Cc: Chris Li <chrisl@kernel.org>
    Cc: Nhat Pham <nphamcs@gmail.com>
    Cc: Youngjun Park <youngjun.park@lge.com>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -3763,11 +3763,6 @@ SYSCALL_DEFINE2(swapon, const char __user *, specialfile, int, swap_flags)
 
 	maxpages = si->max;
 
-	/* Set up the swap cluster info */
-	error = setup_swap_clusters_info(si, swap_header, maxpages);
-	if (error)
-		goto bad_swap_unlock_inode;
-
 	if (si->bdev && bdev_stable_writes(si->bdev))
 		si->flags |= SWP_STABLE_WRITES;
 
@@ -3780,6 +3775,14 @@ SYSCALL_DEFINE2(swapon, const char __user *, specialfile, int, swap_flags)
 		atomic_inc(&nr_rotate_swap);
 		inced_nr_rotate_swap = true;
 	}
+
+	/*
+	 * Set up the swap cluster info after SWP_ flags handling as
+	 * setup_swap_clusters_info() checks SWP_SOLIDSTATE.
+	 */
+	error = setup_swap_clusters_info(si, swap_header, maxpages);
+	if (error)
+		goto bad_swap_unlock_inode;
 
 	if ((swap_flags & SWAP_FLAG_DISCARD) &&
 	    si->bdev && bdev_max_discard_sectors(si->bdev)) {
