72c54bd0cbb2e0c35f4acd056e91c596469edc5f
Damien Le Moal <dlemoal@kernel.org>
2026-09-08 18:02:29 +0900

    scsi: core: Define all additional sense codes and their qualifiers
    Introduce the file include/scsi/scsi_sense.h to define all existing
    additional sense codes and their combinations with additional sense code
    qualifiers, according to the most current T10 list available at
    https://www.t10.org/lists/asc-num.txt.
    
    Additional sense codes (ASCs) are individually defined as an 8-bit value
    with the ASC_ name prefix. The combination of all ASCs with T10 defined
    possible code qualifiers are defined as 16-bit values without any name
    prefix. The 16-bit value for ASCs with a zero ASCQ are defined together
    with the ASC 8-code using the ASC() macro. For names that may potentially
    create conflicts with other subsystems (e.g. WARNING or WRITE_ERROR), the
    SSC_ prefix is added to the sense code name.
    
    The sense key macro definitions in include/scsi/scsi_proto.h are also moved
    into this new file and scsi_sense.h is included in scsi_proto.h.  The
    helper function scsi_sense_code() is defined to facilitate generating a
    16-bit sense code from the 8-bit ASC and ASCQ. The accessors
    scsi_sense_code_asc() and scsi_sense_code_ascq() are defined for the
    reverse operations, that is, to respectively extract an ASC and an ASCQ
    from a 16-bit sense code.
    
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Reviewed-by: Hannes Reinecke <hare@kernel.org>
    Link: https://patch.msgid.link/20260908090308.1085097-2-dlemoal@kernel.org
    Signed-off-by: Martin K. Petersen (Oracle) <mkp@kernel.org>

--- include/scsi/scsi_proto.h
+++ include/scsi/scsi_proto.h
@@ -12,6 +12,7 @@
 
 #include <linux/build_bug.h>
 #include <linux/types.h>
+#include <scsi/scsi_sense.h>
 
 /*
  *      SCSI opcodes
@@ -217,25 +218,6 @@ enum sam_status {
 };
 
 #define STATUS_MASK         0xfe
-
-/*
- *  SENSE KEYS
- */
-#define NO_SENSE            0x00
-#define RECOVERED_ERROR     0x01
-#define NOT_READY           0x02
-#define MEDIUM_ERROR        0x03
-#define HARDWARE_ERROR      0x04
-#define ILLEGAL_REQUEST     0x05
-#define UNIT_ATTENTION      0x06
-#define DATA_PROTECT        0x07
-#define BLANK_CHECK         0x08
-#define VENDOR_SPECIFIC     0x09
-#define COPY_ABORTED        0x0a
-#define ABORTED_COMMAND     0x0b
-#define VOLUME_OVERFLOW     0x0d
-#define MISCOMPARE          0x0e
-#define COMPLETED	    0x0f
 
 /*
  * Additional Sense Codes (ASC).
