6fd889036b729f43b43489205248ba180ee3e43f
Aaron Tomlin <atomlin@atomlin.com>
2026-09-08 16:32:30 -0400

    module: rename module_blacklist to module_denylist
    To preserve the existing user-space ABI, "module_blacklist=" is kept as a
    legacy alias pointing to the same module_denylist variable.
    
    This patch addresses the documentation by marking "module_blacklist=" as
    deprecated in admin-guide/kernel-parameters.txt, and documents the new
    "module_denylist=" parameter.  All internal symbols, such as
    module_is_blacklisted(), have been renamed to use "denylist" and all log
    messages now use "denylisted".
    
    Link: https://lore.kernel.org/20260908203230.401020-4-atomlin@atomlin.com
    Signed-off-by: Aaron Tomlin <atomlin@atomlin.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Luis Chamberalin <mcgrof@kernel.org>
    Cc: "Masami Hiramatsu (Google)" <mhiramat@kernel.org>
    Cc: Miguel Ojeda <ojeda@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Petr Pavlu <petr.pavlu@suse.com>
    Cc: Sami Tolvanen <samitolvanen@google.com>

--- kernel/module/main.c
+++ kernel/module/main.c
@@ -3382,8 +3382,8 @@ static int early_mod_check(struct load_info *info, int flags)
 	 * Now that we know we have the correct module name, check
 	 * if it's blacklisted.
 	 */
-	if (module_is_blacklisted(info->name)) {
-		pr_err("Module %s is blacklisted\n", info->name);
+	if (module_is_denylisted(info->name)) {
+		pr_err("Module %s is denylisted\n", info->name);
 		return -EPERM;
 	}
 
