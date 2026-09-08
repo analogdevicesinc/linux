b5a252adbe4a7c611c8a131383f278852a2773d2
Eric Dumazet <edumazet@google.com>
2026-09-08 09:13:27 +0000

    selftests: icmp_redirect: remove xfail support
    Commit 0a36a75c6818 ("selftests: icmp_redirect: support expected failures")
    added xfail support to icmp_redirect.sh because the test "IPv6: mtu
    exception plus redirect" was failing at the time due to a perceived
    kernel bug described in commit 99513cfa16c6 ("selftest: Fixes for
    icmp_redirect test").
    
    However, commit 0e02bf5de46a ("selftests: icmp_redirect: IPv6 PMTU info
    should be cleared after redirect") subsequently fixed the test logic:
    after redirecting to a new path, the old PMTU information should indeed
    be cleared, so the test was adjusted to verify the redirect without the
    old PMTU.
    
    Since that fix, the test has been passing and is no longer expected to
    fail.
    
    Leaving xfail=1 in log_test() for all IPv6 tests was not only obsolete,
    but had the undesirable side effect of masking any future IPv6 failure
    as XFAIL instead of FAIL. Furthermore, commit 3748939bce3f ("selftests:
    icmp_redirect: pass xfail=0 to log_test()") had to work around a bash
    syntax error caused by the empty xfail argument on IPv4 tests.
    
    Remove the obsolete xfail support from log_test() and restore standard
    pass/fail reporting across all tests.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Ido Schimmel <idosch@nvidia.com>
    Link: https://patch.msgid.link/20260908091327.349091-1-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- tools/testing/selftests/net/icmp_redirect.sh
+++ tools/testing/selftests/net/icmp_redirect.sh
@@ -64,14 +64,10 @@ log_test()
 	local rc=$1
 	local expected=$2
 	local msg="$3"
-	local xfail=$4
 
 	if [ ${rc} -eq ${expected} ]; then
 		printf "TEST: %-60s  [ OK ]\n" "${msg}"
 		nsuccess=$((nsuccess+1))
-	elif [ ${rc} -eq ${xfail} ]; then
-		printf "TEST: %-60s  [XFAIL]\n" "${msg}"
-		nxfail=$((nxfail+1))
 	else
 		ret=1
 		nfail=$((nfail+1))
@@ -303,7 +299,7 @@ check_exception()
 		ip -netns $h1 ro get ${H1_VRF_ARG} ${H2_N2_IP} | \
 		grep -E -v 'mtu|redirected' | grep -q "cache"
 	fi
-	log_test $? 0 "IPv4: ${desc}" 0
+	log_test $? 0 "IPv4: ${desc}"
 
 	# No PMTU info for test "redirect" and "mtu exception plus redirect"
 	if [ "$with_redirect" = "yes" ] && [ "$desc" != "redirect exception plus mtu" ]; then
@@ -319,7 +315,7 @@ check_exception()
 		ip -netns $h1 -6 ro get ${H1_VRF_ARG} ${H2_N2_IP6} | \
 		grep -v "mtu" | grep -q "${R1_LLADDR}"
 	fi
-	log_test $? 0 "IPv6: ${desc}" 1
+	log_test $? 0 "IPv6: ${desc}"
 }
 
 run_ping()
@@ -485,7 +481,6 @@ which ping6 > /dev/null 2>&1 && ping6=$(which ping6) || ping6=$(which ping)
 ret=0
 nsuccess=0
 nfail=0
-nxfail=0
 
 while getopts :pv o
 do
@@ -530,6 +525,5 @@ fi
 
 printf "\nTests passed: %3d\n" ${nsuccess}
 printf "Tests failed: %3d\n"   ${nfail}
-printf "Tests xfailed: %3d\n"  ${nxfail}
 
 exit $ret
