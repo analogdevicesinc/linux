3c06b0b0f3c963c954f775c836d2206a64da61b6
Alex Elder <elder@riscstar.com>
2026-09-09 16:55:54 +0200

    gpio: tc9563: Add support for the embedded GPIO controller
    Add a driver for the GPIO controller embedded in the Toshiba TC9563 PCIe
    switch (and the Qualcomm QPS615). The device implements 37 GPIOs using two
    register banks: three registers control the first 32 GPIOs and three more
    control GPIOs 32-36.  GPIOs 20 and 21 are reserved while GPIOs 22-24,
    27-28, 31, and 34 are input-only.
    
    Register the driver as an auxiliary device driver. The TC9563 power
    controller creates the auxiliary device and provides a regmap that gives
    access to the GPIO registers, so use the gpio-regmap helpers to implement
    the GPIO chip.
    
    Signed-off-by: Alex Elder <elder@riscstar.com>
    Co-developed-by: Daniel Thompson <daniel@riscstar.com>
    Signed-off-by: Daniel Thompson <daniel@riscstar.com>
    Co-developed-by: Lorenzo Bianconi <lorenzo.bianconi@oss.qualcomm.com>
    Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@oss.qualcomm.com>
    [bhelgaas: commit log per https://lore.kernel.org/arQ2mDjqHv4gmoOK@lore-desk]
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Linus Walleij <linusw@kernel.org>
    Reviewed-by: Manivannan Sadhasivam <manivannan.sadhasivam@oss.qualcomm.com>
    Acked-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260909-pci-tc9563-aux-v5-2-c9b33f56c8d3@oss.qualcomm.com

---
+++ include/linux/soc/qcom/tc9563.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2026 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Author: Lorenzo Bianconi <lorenzo.bianconi@oss.qualcomm.com>
+ */
+
+#ifndef __QCOM_TC9563_H
+#define __QCOM_TC9563_H
+
+#define TC9563_GPIO_DEV_NAME	"tc9563-gpio"
+
+#define TC9563_GPIO_IN0_OFFSET		0x801200
+#define TC9563_GPIO_EN0_OFFSET		0x801208
+#define TC9563_GPIO_OUT0_OFFSET		0x801210
+
+#endif /* __QCOM_TC9563_H */
