616bb7ec764ac63af09a64a472565e84b46f27ef
Ackerley Tng <ackerleytng@google.com>
2026-09-09 10:11:00 -0700

    mm: hugetlb: return -ENOSPC on memcg charge failure
    Patch series "Fix bugs in HugeTLB allocation when
    mem_cgroup_charge_hugetlb() fails", v2.
    
    In hugetlb_alloc_folio(), when mem_cgroup_charge_hugetlb() fails, there
    are 2 issues:
    
    1. free_huge_folio() expects a non-refcounted folio and will
       VM_BUG_ON_FOLIO().
    
    2. -ENOMEM is returned, causing an infinite loop retrying the fault.
    
    
    This patch (of 2):
    
    When mem_cgroup_charge_hugetlb() fails with -ENOMEM, alloc_hugetlb_folio()
    currently propagates this error.  This results in the page fault handler
    returning VM_FAULT_OOM.
    
    Because HugeTLB allocations are high-order and use __GFP_RETRY_MAYFAIL,
    they bypass the OOM killer.  Returning VM_FAULT_OOM to the #PF handler
    without triggering the OOM killer (or having it make progress) leads to an
    infinite loop of retrying the fault.
    
    Avoid this loop by returning -ENOSPC when charging fails, which maps to
    VM_FAULT_SIGBUS, terminating the process cleanly.
    
    Make mem_cgroup_charge_hugetlb() fault handling use a common error
    handling path, the same handling used for
    hugetlb_cgroup_uncharge_cgroup{,_rsvd}(), which also don't trigger the OOM
    killer and hence opt to terminate the process with a SIGBUS.
    
    Link: https://lore.kernel.org/20260909-hugetlb-alloc-folio-memcg-charge-error-handling-v2-1-4b4a8a19a7f7@google.com
    Fixes: 991135774c0e ("memcg/hugetlb: introduce mem_cgroup_charge_hugetlb")
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Reviewed-by: Muchun Song <muchun.song@linux.dev>
    Reviewed-by: Joshua Hahn <joshua.hahnjy@gmail.com>
    Cc: Alex Shi <alexs@kernel.org>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Dongliang Mu <dzm91@hust.edu.cn>
    Cc: Frank van der Linden <fvdl@google.com>
    Cc: Hongxiang Lou <louhongxiang@huawei.com>
    Cc: James Houghton <jthoughton@google.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Ma Wupeng <mawupeng1@huawei.com>
    Cc: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Nhat Pham <nphamcs@gmail.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vishal Annapurve <vannapurve@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Cc: Yanteng Si <si.yanteng@linux.dev>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -2851,7 +2851,6 @@ void wait_for_freed_hugetlb_folios(void)
  *
  * Return: A pointer to the allocated folio, or an ERR_PTR on failure.
  *         -ENOSPC if cgroup charging fails or no folio is available.
- *         -ENOMEM if mem cgroup charging fails.
  */
 struct folio *hugetlb_alloc_folio(struct hstate *h,
 		struct mempolicy_interpreted *mpoli, u8 alloc_flags)
@@ -2924,7 +2923,11 @@ struct folio *hugetlb_alloc_folio(struct hstate *h,
 		 * were committed to the folio and freeing the folio
 		 * would have cleared those up.
 		 */
-		return ERR_PTR(ret);
+		/*
+		 * Return -ENOSPC, since retrying the fault is futile:
+		 * the OOM killer is not triggered for HugeTLB.
+		 */
+		return ERR_PTR(-ENOSPC);
 	}
 
 	return folio;
