e398da0b9131583e0f52bca733eaefc3b3aeae95
Daeho Jeong <daehojeong@google.com>
2026-09-09 12:06:36 -0700

    f2fs: fix livelock in syncing dirty inodes
    During checkpoint, f2fs_sync_dirty_inodes() and f2fs_sync_inode_meta()
    iterate over dirty inodes in their respective lists. If igrab() fails on
    an inode (e.g. because it is in the freeing state), the loop continues
    without moving the current inode to the tail of the list. As a result,
    subsequent iterations pick the same inode repeatedly, preventing other
    ready dirty inodes in the list from making forward progress and leading
    to a livelock.
    
    Fix this by moving the current inode to the tail of the list
    (list_move_tail(&fi->{dirty_list,gdirty_list}, head)) before attempting
    igrab() in both f2fs_sync_dirty_inodes() and f2fs_sync_inode_meta().
    
    Additionally, if igrab() fails, yield the CPU with cond_resched() to
    allow the evicting thread to finish eviction. Remove the redundant
    f2fs_submit_merged_write() call, since .writepages already submits cached
    bios via f2fs_submit_merged_write_cond().
    
    v2:
    - Also apply list_move_tail() to f2fs_sync_dirty_inodes().
    - Remove redundant f2fs_submit_merged_write() calls from both functions,
      keeping only cond_resched().
    - Update commit title and description.
    
    Signed-off-by: Daeho Jeong <daehojeong@google.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/checkpoint.c
+++ fs/f2fs/checkpoint.c
@@ -1402,6 +1402,7 @@ retry:
 		return 0;
 	}
 	fi = list_first_entry(head, struct f2fs_inode_info, dirty_list);
+	list_move_tail(&fi->dirty_list, head);
 	inode = igrab(&fi->vfs_inode);
 	spin_unlock(&sbi->inode_lock[type]);
 	if (inode) {
@@ -1424,11 +1425,6 @@ retry:
 		else
 			ino = cur_ino;
 	} else {
-		/*
-		 * We should submit bio, since it exists several
-		 * writebacking dentry pages in the freeing inode.
-		 */
-		f2fs_submit_merged_write(sbi, DATA);
 		cond_resched();
 	}
 	goto retry;
@@ -1452,6 +1448,7 @@ static int f2fs_sync_inode_meta(struct f2fs_sb_info *sbi)
 		}
 		fi = list_first_entry(head, struct f2fs_inode_info,
 							gdirty_list);
+		list_move_tail(&fi->gdirty_list, head);
 		inode = igrab(&fi->vfs_inode);
 		spin_unlock(&sbi->inode_lock[DIRTY_META]);
 		if (inode) {
@@ -1461,6 +1458,8 @@ static int f2fs_sync_inode_meta(struct f2fs_sb_info *sbi)
 			if (is_inode_flag_set(inode, FI_DIRTY_INODE))
 				f2fs_update_inode_page(inode);
 			iput(inode);
+		} else {
+			cond_resched();
 		}
 	}
 	return 0;
