21637a1badc953bb548bd9f16b2848def97850ae
Muchun Song <songmuchun@bytedance.com>
2026-09-10 14:32:52 +0800

    mm/sparse: remove section_map_size()
    section_map_size() no longer provides any shared logic.
    
    After the sparse-vmemmap changes, its only remaining user is the
    !CONFIG_SPARSEMEM_VMEMMAP path in __populate_section_memmap(), which can
    compute the size inline with PAGE_ALIGN(sizeof(struct page) *
    PAGES_PER_SECTION).
    
    Remove section_map_size() and inline the remaining calculation.
    
    Link: https://lore.kernel.org/20260910063256.64386-14-songmuchun@bytedance.com
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Acked-by: David Hildenbrand (Arm) <david@kernel.org>
    Cc: David Laight <david.laight.linux@gmail.com>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Qi Zheng <qi.zheng@linux.dev>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -5140,7 +5140,6 @@ static inline void print_vma_addr(char *prefix, unsigned long rip)
 }
 #endif
 
-unsigned long section_map_size(void);
 struct page * __populate_section_memmap(unsigned long pfn,
 		unsigned long nr_pages, int nid, struct vmem_altmap *altmap,
 		struct dev_pagemap *pgmap);
