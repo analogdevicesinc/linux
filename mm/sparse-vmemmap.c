0d33d4d936c6960fdad30f8a51dc380c49843b27
Muchun Song <songmuchun@bytedance.com>
2026-09-10 14:32:50 +0800

    mm/sparse-vmemmap: remove SPARSEMEM_VMEMMAP_PREINIT support
    SPARSEMEM_VMEMMAP_PREINIT existed only to support HugeTLB's early vmemmap
    optimization setup.
    
    Now that HugeTLB bootmem vmemmap optimization uses the common
    section-based sparse-vmemmap path, the sparse initialization code no
    longer needs a separate pre-initialization mechanism for vmemmap
    population.
    
    Remove the related Kconfig symbols, section flag, and empty early hook, so
    present sections always go through the normal sparse setup path.
    
    Link: https://lore.kernel.org/20260910063256.64386-12-songmuchun@bytedance.com
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Acked-by: Qi Zheng <qi.zheng@linux.dev>
    Acked-by: David Hildenbrand (Arm) <david@kernel.org>
    Cc: David Laight <david.laight.linux@gmail.com>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>

--- mm/sparse-vmemmap.c
+++ mm/sparse-vmemmap.c
@@ -594,19 +594,6 @@ struct page * __meminit __populate_section_memmap(unsigned long pfn,
 	return pfn_to_page(pfn);
 }
 
-#ifdef CONFIG_SPARSEMEM_VMEMMAP_PREINIT
-/*
- * This is called just before initializing sections for a NUMA node.
- * Any special initialization that needs to be done before the
- * generic initialization can be done from here. Sections that
- * are initialized in hooks called from here will be skipped by
- * the generic initialization.
- */
-void __init sparse_vmemmap_init_nid_early(int nid)
-{
-}
-#endif
-
 static void subsection_mask_set(unsigned long *map, unsigned long pfn,
 		unsigned long nr_pages)
 {
