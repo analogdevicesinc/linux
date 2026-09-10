35a407870d38bd6c1392eb5d7015a18e6e292359
Muchun Song <songmuchun@bytedance.com>
2026-09-10 14:32:47 +0800

    mm/sparse-vmemmap: support section-based vmemmap optimization
    Teach sparse-vmemmap population code to use the compound page order when
    deciding whether a vmemmap page can be optimized.
    
    With this information, the common sparse-vmemmap population path can
    allocate or reuse shared tail vmemmap pages directly instead of relying on
    HugeTLB-specific handling.
    
    This centralizes vmemmap optimization logic in the sparse-vmemmap code,
    based on section metadata, and prepares for sharing the same mechanism
    across different users of vmemmap optimization, including HugeTLB and DAX.
    
    Link: https://lore.kernel.org/20260910063256.64386-9-songmuchun@bytedance.com
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Acked-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Acked-by: Qi Zheng <qi.zheng@linux.dev>
    Cc: David Hildenbrand (Arm) <david@kernel.org>
    Cc: David Laight <david.laight.linux@gmail.com>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/sparse.h
+++ mm/sparse.h
@@ -111,8 +111,15 @@ static inline void sparse_init(void) {}
  */
 #ifdef CONFIG_SPARSEMEM_VMEMMAP
 void sparse_init_subsection_map(void);
+int section_nr_vmemmap_pages(unsigned long pfn, unsigned long nr_pages,
+		struct vmem_altmap *altmap, struct dev_pagemap *pgmap);
 #else
 static inline void sparse_init_subsection_map(void) {}
+static inline int section_nr_vmemmap_pages(unsigned long pfn, unsigned long nr_pages,
+		struct vmem_altmap *altmap, struct dev_pagemap *pgmap)
+{
+	return DIV_ROUND_UP(nr_pages * sizeof(struct page), PAGE_SIZE);
+}
 #endif /* CONFIG_SPARSEMEM_VMEMMAP */
 
 #endif /* __MM_SPARSE_H */
