dbe68462fa656078eac3a5a09039038dc47e08da
bui duc phuc <phucduc.bui@gmail.com>
2026-09-11 12:10:43 +0700

    usb: chipidea: ci_hdrc_imx: Fix error handling when creating IRQ name
    devm_kasprintf() can fail when allocating the wakeup IRQ name. In this
    case, the probe error path should shut down the PHY before returning.
    
    Use the phy_shutdown error path instead of err_clk so that the PHY is
    properly shut down when creating the IRQ name or requesting the wakeup
    IRQ fails.
    
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Acked-by: Peter Chen <peter.chen@kernel.org>
    Reported-by: Sashiko Bot <sashiko-bot@kernel.org>
    Link: https://lore.kernel.org/all/20260806065755.C9F361F000E9@smtp.kernel.org/
    Signed-off-by: bui duc phuc <phucduc.bui@gmail.com>
    Link: https://patch.msgid.link/20260911051043.15962-1-phucduc.bui@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/chipidea/ci_hdrc_imx.c
+++ drivers/usb/chipidea/ci_hdrc_imx.c
@@ -533,7 +533,7 @@ static int ci_hdrc_imx_probe(struct platform_device *pdev)
 		irq_name = devm_kasprintf(dev, GFP_KERNEL, "%s:wakeup", pdata.name);
 		if (!irq_name) {
 			ret = dev_err_probe(dev, -ENOMEM, "failed to create irq_name\n");
-			goto err_clk;
+			goto phy_shutdown;
 		}
 
 		ret = devm_request_threaded_irq(dev, data->wakeup_irq,
@@ -541,7 +541,7 @@ static int ci_hdrc_imx_probe(struct platform_device *pdev)
 						IRQF_ONESHOT | IRQF_NO_AUTOEN,
 						irq_name, data);
 		if (ret)
-			goto err_clk;
+			goto phy_shutdown;
 	}
 
 	ret = imx_usbmisc_init(data->usbmisc_data);
