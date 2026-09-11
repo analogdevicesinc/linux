65221358b6a6ea5128accbaa6117565ab8e24aa1
Karl Mehltretter <kmehltretter@gmail.com>
2026-09-12 00:11:00 +0200

    dma-fence: Fix dma_fence_enable_signaling() reference in kernel-doc
    The kernel-doc comment of dma_fence_is_signaled_locked() refers to
    dma_fence_enable_sw_signaling, which was renamed to
    dma_fence_enable_signaling by commit 9370a5c664e8 ("dma-buf: rename
    dma_fence_enable_sw_signaling"). Say dma_fence_enable_signaling.
    
    Fixes: 9370a5c664e8 ("dma-buf: rename dma_fence_enable_sw_signaling")
    Assisted-by: LLM
    Signed-off-by: Karl Mehltretter <kmehltretter@gmail.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Link: https://lore.kernel.org/r/20260911221100.7079-1-kmehltretter@gmail.com

--- include/linux/dma-fence.h
+++ include/linux/dma-fence.h
@@ -501,7 +501,7 @@ dma_fence_test_signaled_flag(struct dma_fence *fence)
  * Returns true if the fence was already signaled, false if not. Since this
  * function doesn't enable signaling, it is not guaranteed to ever return
  * true if dma_fence_add_callback(), dma_fence_wait() or
- * dma_fence_enable_sw_signaling() haven't been called before.
+ * dma_fence_enable_signaling() haven't been called before.
  *
  * This function requires &dma_fence.lock to be held.
  *
