4c7e6130e21827c26247885a5f09353f0d528708
Srish Srinivasan <ssrish@linux.ibm.com>
2026-09-12 11:59:50 +0530

    keys/trusted_keys: move TPM-specific fields into struct trusted_key_tpm
    The trusted_key_options struct contains TPM-specific fields (keyhandle,
    keyauth, blobauth_len, blobauth, pcrinfo_len, pcrinfo, pcrlock, hash,
    policydigest_len, policydigest, and policyhandle). This leads to the
    accumulation of backend-specific fields in the generic options structure.
    
    Define struct trusted_key_tpm and move the TPM-specific fields there. Store
    a pointer to it in the private member of struct trusted_key_options.
    
    Signed-off-by: Srish Srinivasan <ssrish@linux.ibm.com>
    Reviewed-by: Stefan Berger <stefanb@linux.ibm.com>
    Reviewed-by: Jarkko Sakkinen <jarkko@kernel.org>
    Link: https://lore.kernel.org/r/20260912062950.279104-3-ssrish@linux.ibm.com
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>

--- include/keys/trusted_tpm.h
+++ include/keys/trusted_tpm.h
@@ -6,6 +6,20 @@
 
 extern struct trusted_key_ops trusted_key_tpm_ops;
 
+struct trusted_key_tpm {
+	uint32_t keyhandle;
+	unsigned char keyauth[TPM_DIGEST_SIZE];
+	uint32_t blobauth_len;
+	unsigned char blobauth[TPM_DIGEST_SIZE];
+	uint32_t pcrinfo_len;
+	unsigned char pcrinfo[MAX_PCRINFO_SIZE];
+	int pcrlock;
+	uint32_t hash;
+	uint32_t policydigest_len;
+	unsigned char policydigest[MAX_DIGEST_SIZE];
+	uint32_t policyhandle;
+};
+
 int tpm2_seal_trusted(struct tpm_chip *chip,
 		      struct trusted_key_payload *payload,
 		      struct trusted_key_options *options);
