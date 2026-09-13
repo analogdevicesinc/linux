69bd71e55b71c024bf5c105cf10ccec380861886
Tapio Reijonen <tapio.reijonen@vaisala.com>
2026-09-13 11:40:17 +0000

    regulator: mpq4210: Use the -ohms feedback divider property
    Follow the binding rename of mps,fb-voltage-divider to
    mps,fb-voltage-divider-ohms.
    
    The old name was never in a release and this driver is its only reader,
    so it is dropped rather than kept as a fallback.
    
    Fixes: 61879d561e91 ("regulator: Add MPS MPQ4210 buck-boost regulator driver")
    Suggested-by: Krzysztof Kozlowski <krzk@kernel.org>
    Link: https://lore.kernel.org/r/20260911-gaur-of-satisfying-action-1a3b0f@quoll
    Signed-off-by: Tapio Reijonen <tapio.reijonen@vaisala.com>
    Reviewed-by: Krzysztof Kozlowski <krzysztof.kozlowski@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260913-mpq4210-ohms-fixup-v1-2-dcff627001ff@vaisala.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/mpq4210.c
+++ drivers/regulator/mpq4210.c
@@ -144,11 +144,12 @@ static int mpq4210_i2c_probe(struct i2c_client *client)
 	u32 r[2];
 	int ret;
 
-	ret = of_property_read_u32_array(dev->of_node, "mps,fb-voltage-divider",
+	ret = of_property_read_u32_array(dev->of_node,
+					 "mps,fb-voltage-divider-ohms",
 					 r, ARRAY_SIZE(r));
 	if (ret)
 		return dev_err_probe(dev, ret,
-				     "failed to read mps,fb-voltage-divider\n");
+				     "failed to read mps,fb-voltage-divider-ohms\n");
 
 	if (!r[1])
 		return dev_err_probe(dev, -EINVAL,
