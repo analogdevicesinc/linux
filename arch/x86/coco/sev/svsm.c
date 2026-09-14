6c43c72748fffd29dec15cd1f31e9a32949bc437
Melody Wang <huibo.wang@amd.com>
2026-09-14 00:57:17 +0000

    x86/sev: Make vTPM SVSM calls preemption-safe
    Two functions in the SVSM vTPM guest implementation do not disable
    preemption when fetching the SVSM Calling Area Address (CAA).
    
    The SVSM CAA is a per-CPU structure. When a thread is preempted and migrated
    to a different CPU after fetching the per-CPU CAA, the SVSM call will execute
    on the new CPU with the original CPU's CAA. Which is wrong.
    
    Move the CAA fetching operation inside svsm_perform_call_protocol() which
    disables interrupts around the SVSM call and thus runs preemption-safe.
    
    Fixes: 770de678bc28 ("x86/sev: Add SVSM vTPM probe/send_command functions")
    Signed-off-by: Melody Wang <huibo.wang@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Stefano Garzarella <sgarzare@redhat.com>
    Cc: stable@vger.kernel.org
    Link: https://patch.msgid.link/a5bc0d4a2c462a0089109e145c21626b244b2ff0.1789345277.git.huibo.wang@amd.com

--- arch/x86/coco/sev/svsm.c
+++ arch/x86/coco/sev/svsm.c
@@ -74,6 +74,14 @@ int svsm_perform_call_protocol(struct svsm_call *call)
 
 	flags = native_local_irq_save();
 
+	/*
+	 * 'caa' is a per-CPU variable. To avoid using a stale or incorrect
+	 * 'caa' if the task is preempted or migrated to another CPU after it
+	 * is fetched, always fetch 'caa' and then issue the SVSM call with
+	 * interrupts disabled. This ensures the correct 'caa' is used.
+	 */
+	call->caa = svsm_get_caa();
+
 	ghcb = __sev_get_ghcb(&state);
 
 	do {
@@ -321,7 +329,6 @@ int snp_svsm_vtpm_send_command(u8 *buffer)
 {
 	struct svsm_call call = {};
 
-	call.caa = svsm_get_caa();
 	call.rax = SVSM_VTPM_CALL(SVSM_VTPM_CMD);
 	call.rcx = __pa(buffer);
 
@@ -345,7 +352,6 @@ bool snp_svsm_vtpm_probe(void)
 	if (!snp_vmpl)
 		return false;
 
-	call.caa = svsm_get_caa();
 	call.rax = SVSM_VTPM_CALL(SVSM_VTPM_QUERY);
 
 	if (svsm_perform_call_protocol(&call))
