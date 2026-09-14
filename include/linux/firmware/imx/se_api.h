400e503ecc6186f626535d0755892f55a2572122
Pankaj Gupta <pankaj.gupta@nxp.com>
2026-09-15 01:10:00 +0530

    firmware: imx: add driver for NXP EdgeLock Enclave
    Add MU-based communication interface for secure enclave.
    
    NXP hardware IP(s) for secure-enclaves like Edgelock Enclave(ELE), are
    embedded in the SoC to support the features like HSM, SHE & V2X, using
    message based communication interface.
    
    The secure enclave FW communicates with Linux over single or multiple
    dedicated messaging unit(MU) based interface(s).
    Exists on i.MX SoC(s) like i.MX8ULP, i.MX93, i.MX95 etc.
    
    For i.MX9x SoC(s) there is at least one dedicated ELE MU(s) for each
    world - Linux(one or more) and OPTEE-OS (one or more).
    
    Other dependent kernel drivers will be:
    - NVMEM: that supports non-volatile devices like EFUSES,
      managed by NXP's secure-enclave.
    
    Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Signed-off-by: Frank Li <Frank.Li@nxp.com>

---
+++ include/linux/firmware/imx/se_api.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2025 NXP
+ */
+
+#ifndef __SE_API_H__
+#define __SE_API_H__
+
+#include <linux/types.h>
+
+#define SOC_ID_OF_IMX8ULP		0x084d
+#define SOC_ID_OF_IMX93			0x9300
+
+#endif /* __SE_API_H__ */
