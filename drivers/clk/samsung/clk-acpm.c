802a1a97c41dff82a557223f7a43655fc02920bd
Alexey Klimov <alexey.klimov@linaro.org>
2026-09-15 00:05:43 +0100

    clk: samsung: acpm: introduce driver data for SoC-specific clocks
    Currently, the ACPM clock driver hardcodes the GS101 clock variant
    arrays and counts directly in probe() routine. To support additional
    other SoCs (for instance Exynos850 SoC) that use the same ACPM protocol
    but have different clock trees, the driver needs to be more flexible.
    
    Introduce driver data attached to the platform_device_id table.
    Update the probe function to dynamically extract the clock lists,
    number of clocks, its names and mailbox channel id based on the matching
    device ID.
    
    Reviewed-by: Tudor Ambarus <tudor.ambarus@linaro.org>
    Signed-off-by: Alexey Klimov <alexey.klimov@linaro.org>
    Link: https://patch.msgid.link/20260915-exynos850-acpm-clk-v2-1-ffad4a6b3fb1@linaro.org
    [krzk: Drop unneeded mod_devicetable.h]
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- drivers/clk/samsung/clk-acpm.c
+++ drivers/clk/samsung/clk-acpm.c
@@ -101,6 +101,8 @@ static int acpm_clk_register(struct device *dev, struct acpm_clk *aclk,
 
 static int acpm_clk_probe(struct platform_device *pdev)
 {
+	const struct acpm_clk_driver_data *drv_data;
+	const struct platform_device_id *id;
 	struct acpm_handle *acpm_handle;
 	struct clk_hw_onecell_data *clk_data;
 	struct clk_hw **hws;
@@ -114,8 +116,14 @@ static int acpm_clk_probe(struct platform_device *pdev)
 		return dev_err_probe(dev, PTR_ERR(acpm_handle),
 				     "Failed to get acpm handle\n");
 
-	count = acpm_clk_gs101.nr_clks;
-	mbox_chan_id = acpm_clk_gs101.mbox_chan_id;
+	id = platform_get_device_id(pdev);
+	if (!id || !id->driver_data)
+		return -ENODEV;
+
+	drv_data  = (const struct acpm_clk_driver_data *)id->driver_data;
+
+	count = drv_data->nr_clks;
+	mbox_chan_id = drv_data->mbox_chan_id;
 
 	clk_data = devm_kzalloc(dev, struct_size(clk_data, hws, count),
 				GFP_KERNEL);
@@ -142,8 +150,7 @@ static int acpm_clk_probe(struct platform_device *pdev)
 
 		hws[i] = &aclk->hw;
 
-		err = acpm_clk_register(dev, aclk,
-					acpm_clk_gs101.clks[i].name);
+		err = acpm_clk_register(dev, aclk, drv_data->clks[i].name);
 		if (err)
 			return dev_err_probe(dev, err,
 					     "Failed to register clock\n");
@@ -154,7 +161,7 @@ static int acpm_clk_probe(struct platform_device *pdev)
 }
 
 static const struct platform_device_id acpm_clk_id[] = {
-	{ .name = "gs101-acpm-clk" },
+	{ .name = "gs101-acpm-clk", (kernel_ulong_t)&acpm_clk_gs101 },
 	{ }
 };
 MODULE_DEVICE_TABLE(platform, acpm_clk_id);
