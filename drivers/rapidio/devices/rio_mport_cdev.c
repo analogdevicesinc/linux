6c40e8b094229ff7f05c461822d60a1cd611ae09
James Kim <james010kim@gmail.com>
2026-09-14 13:23:27 +0900

    rapidio: mport_cdev: fix use-after-free in mport_mm_close()
    A use-after-free vulnerability was identified in mport_mm_close() in
    drivers/rapidio/devices/rio_mport_cdev.c, identical to the pattern
    previously addressed in dma_req_free().
    
    This is observable from userspace when an application creates an mmap
    mapping via the RapidIO character device and subsequently unmaps it (or
    terminates, triggering exit_mmap()).  During munmap, mport_mm_close() is
    invoked and drops the mapping reference via kref_put().
    
    If kref_put() drops the last reference, mport_release_mapping() is called,
    which frees the underlying rio_mport_mapping structure.  The subsequent
    mutex_unlock() then dereferences map->md to unlock buf_mutex, leading to a
    use-after-free:
    
       mport_mm_close()
         -> mutex_lock(&map->md->buf_mutex);
         ...
         -> kref_put(&map->ref, mport_release_mapping); /* map is freed */
         -> mutex_unlock(&map->md->buf_mutex);          /* UAF: map used */
    
    Fix this by caching map->md before kref_put() and using the cached pointer
    for mutex unlocking, ensuring that freed memory is not accessed.
    
    Link: https://lore.kernel.org/20260914042327.49798-1-james010kim@gmail.com
    Fixes: e8de370188d0 ("rapidio: add mport char device driver")
    Signed-off-by: James Kim <james010kim@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Alexandre Bounine <alex.bou9@gmail.com>
    Cc: Dan Carpenter <dan.carpenter@linaro.org>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Matt Porter <mporter@kernel.crashing.org>
    Cc: <stable@vger.kernel.org>

--- drivers/rapidio/devices/rio_mport_cdev.c
+++ drivers/rapidio/devices/rio_mport_cdev.c
@@ -2167,11 +2167,12 @@ static void mport_mm_open(struct vm_area_struct *vma)
 static void mport_mm_close(struct vm_area_struct *vma)
 {
 	struct rio_mport_mapping *map = vma->vm_private_data;
+	struct mport_dev *md = map->md;
 
 	rmcd_debug(MMAP, "%pad", &map->phys_addr);
-	mutex_lock(&map->md->buf_mutex);
+	mutex_lock(&md->buf_mutex);
 	kref_put(&map->ref, mport_release_mapping);
-	mutex_unlock(&map->md->buf_mutex);
+	mutex_unlock(&md->buf_mutex);
 }
 
 static const struct vm_operations_struct vm_ops = {
