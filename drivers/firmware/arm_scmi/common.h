8bac2642a97ae68f737e40823b87f015c67cd58e
Sudeep Holla <sudeep.holla@kernel.org>
2026-09-15 18:03:36 +0100

    firmware: arm_scmi: Extend transport driver macro to support ACPI
    Extend the SCMI transport driver helper to support ACPI-based systems.
    Introduce an internal helper macro that accepts both OF and ACPI match
    tables, and expose two wrappers:
    
      - DEFINE_SCMI_TRANSPORT_DRIVER(...) for DT/OF transports
      - DEFINE_SCMI_ACPI_TRANSPORT_DRIVER(...) for ACPI transports
    
    For ACPI, set the generated platform_driver .acpi_match_table via
    ACPI_PTR(). The ACPI wrapper relies on the firmware-node propagation
    provided by the preceding change so fwnode lookups on the spawned
    platform device see the correct firmware description.
    
    Keep existing DT users unchanged while allowing transports to be probed
    using struct acpi_device_id tables on ACPI platforms.
    
    Reviewed-by: Jonathan Cameron <jonathan.cameron@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260915-acpi_scmi_pcc-v5-2-298579e9f359@kernel.org
    Signed-off-by: Sudeep Holla <sudeep.holla@kernel.org>

--- drivers/firmware/arm_scmi/common.h
+++ drivers/firmware/arm_scmi/common.h
@@ -9,6 +9,7 @@
 #ifndef _SCMI_COMMON_H
 #define _SCMI_COMMON_H
 
+#include <linux/acpi.h>
 #include <linux/bitfield.h>
 #include <linux/completion.h>
 #include <linux/device.h>
@@ -637,7 +638,8 @@ struct scmi_transport_supplier __supplier = {			\
 	.th.supplier_put = scmi_transport_supplier_put,		\
 }
 
-#define DEFINE_SCMI_TRANSPORT_DRIVER(__tag, __drv, __desc, __match, __core_ops)\
+#define __DEFINE_SCMI_TRANSPORT_DRIVER(__tag, __drv, __desc, __of_match,       \
+					__acpi_match, __core_ops)	       \
 static void __tag##_dev_free(void *data)				       \
 {									       \
 	struct platform_device *spdev = data;				       \
@@ -703,10 +705,17 @@ err_mem:								       \
 static struct platform_driver __drv = {					       \
 	.driver = {							       \
 		   .name = #__tag "_transport",				       \
-		   .of_match_table = __match,				       \
+		   .of_match_table = __of_match,			       \
+		   .acpi_match_table = ACPI_PTR(__acpi_match),		       \
 		   },							       \
 	.probe = __tag##_probe,						       \
 }
+
+#define DEFINE_SCMI_TRANSPORT_DRIVER(__tag, __drv, __desc, __match, __core_ops)\
+	__DEFINE_SCMI_TRANSPORT_DRIVER(__tag, __drv, __desc, __match, NULL, __core_ops)
+
+#define DEFINE_SCMI_ACPI_TRANSPORT_DRIVER(__tag, __drv, __desc, __match, __core_ops)\
+	__DEFINE_SCMI_TRANSPORT_DRIVER(__tag, __drv, __desc, NULL, __match, __core_ops)
 
 void scmi_notification_instance_data_set(const struct scmi_handle *handle,
 					 void *priv);
