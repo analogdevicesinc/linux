3afefbfe55c2a8a0c4bdf6f4cc1f773da120027c
Benjamin Tissoires <bentiss@kernel.org>
2026-09-15 17:46:57 +0200

    HID: bpf: cast size to ssize_t when checking hid_bpf_hw_request
    As reported by Sashiko:
    If a transport driver encounters a hardware error and returns a negative
    error code such as -EPIPE, ret is implicitly promoted to size_t when
    compared against size. This causes the negative error code to evaluate
    as a large positive number, making the (ret > size) condition true.
    
    This silently converts the hardware error into a success return value
    and copies the unmodified buffer back, which could leave BPF programs
    operating on uninitialized or stale data.
    
    Fix this by casting size into ssize_t to return the actual negative
    error code.
    
    Link: https://lore.kernel.org/all/20260904130354.A79471F00A3D@smtp.kernel.org/
    Fixes: 2b658c1c442e ("HID: bpf: prevent buffer overflow in hid_hw_request")
    Cc: stable@vger.kernel.org
    Acked-by: Jiri Kosina <jkosina@suse.com>
    Signed-off-by: Benjamin Tissoires <bentiss@kernel.org>

--- drivers/hid/bpf/hid_bpf_dispatch.c
+++ drivers/hid/bpf/hid_bpf_dispatch.c
@@ -453,7 +453,7 @@ hid_bpf_hw_request(struct hid_bpf_ctx *ctx, __u8 *buf, size_t buf__sz,
 					      (u64)(long)ctx,
 					      true); /* prevent infinite recursions */
 
-	if (ret > size)
+	if (ret > (ssize_t)size)
 		ret = size;
 	if (ret > 0)
 		memcpy(buf, dma_data, ret);
