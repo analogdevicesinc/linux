c4967fd8b43e5b44386e8a3feecab89eacb398d9
Maxime Ripard <mripard@kernel.org>
2026-09-15 16:24:49 +0200

    drm/xen: Switch to drm_atomic_helper_crtc_create_state
    The xen crtc implementation uses the deprecated
    drm_atomic_helper_crtc_reset() as its reset hook.
    
    Switch to drm_atomic_helper_crtc_create_state() instead.
    
    Reviewed-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: https://patch.msgid.link/20260915-drm-no-more-crtc-reset-v5-8-bda007735748@kernel.org
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- drivers/gpu/drm/xen/xen_drm_front_kms.c
+++ drivers/gpu/drm/xen/xen_drm_front_kms.c
@@ -358,7 +358,7 @@ static const struct drm_crtc_helper_funcs display_crtc_helper_funcs = {
 };
 
 static const struct drm_crtc_funcs display_crtc_funcs = {
-	.reset			= drm_atomic_helper_crtc_reset,
+	.atomic_create_state	= drm_atomic_helper_crtc_create_state,
 	.destroy		= drm_crtc_cleanup,
 	.set_config		= drm_atomic_helper_set_config,
 	.page_flip		= drm_atomic_helper_page_flip,
