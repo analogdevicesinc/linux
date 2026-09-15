d7f9bde13c54070ec5b091baf361009dd884f8c7
Maxime Ripard <mripard@kernel.org>
2026-09-15 16:24:45 +0200

    drm/mcde: Switch to drm_atomic_helper_crtc_create_state
    The mcde crtc implementation uses the deprecated
    drm_atomic_helper_crtc_reset() as its reset hook.
    
    Switch to drm_atomic_helper_crtc_create_state() instead.
    
    Reviewed-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: https://patch.msgid.link/20260915-drm-no-more-crtc-reset-v5-4-bda007735748@kernel.org
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- drivers/gpu/drm/mcde/mcde_display.c
+++ drivers/gpu/drm/mcde/mcde_display.c
@@ -1508,7 +1508,7 @@ static int mcde_crtc_helper_atomic_check(struct drm_crtc *crtc, struct drm_atomi
 }
 
 static const struct drm_crtc_funcs mcde_crtc_funcs = {
-	.reset			= drm_atomic_helper_crtc_reset,
+	.atomic_create_state	= drm_atomic_helper_crtc_create_state,
 	.destroy		= drm_crtc_cleanup,
 	.set_config		= drm_atomic_helper_set_config,
 	.page_flip		= drm_atomic_helper_page_flip,
