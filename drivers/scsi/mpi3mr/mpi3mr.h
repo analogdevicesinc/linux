e6687d574bbe142751bcfa6dd0f42d5abd1c2756
Ranjan Kumar <ranjan.kumar@broadcom.com>
2026-09-16 13:57:05 +0530

    scsi: mpi3mr: Driver version update to 8.18.0.8.50
    Update driver version to 8.18.0.8.50
    
    Signed-off-by: Ranjan Kumar <ranjan.kumar@broadcom.com>
    Link: https://patch.msgid.link/20260916082705.44712-11-ranjan.kumar@broadcom.com
    Signed-off-by: Martin K. Petersen (Oracle) <mkp@kernel.org>

--- drivers/scsi/mpi3mr/mpi3mr.h
+++ drivers/scsi/mpi3mr/mpi3mr.h
@@ -56,8 +56,8 @@ extern struct list_head mrioc_list;
 extern int prot_mask;
 extern atomic64_t event_counter;
 
-#define MPI3MR_DRIVER_VERSION	"8.17.0.3.50"
-#define MPI3MR_DRIVER_RELDATE	"09-January-2026"
+#define MPI3MR_DRIVER_VERSION	"8.18.0.8.50"
+#define MPI3MR_DRIVER_RELDATE	"26-June-2026"
 
 #define MPI3MR_DRIVER_NAME	"mpi3mr"
 #define MPI3MR_DRIVER_LICENSE	"GPL"
