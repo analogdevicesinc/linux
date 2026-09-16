7f93ca31f7fd9b42e7d692f59514436d448ac908
Felix Fietkau <nbd@nbd.name>
2026-09-16 11:48:32 +0200

    wifi: mac80211: prevent AP VLAN tx from other interfaces
    Since the station lookup was factored out of ieee80211_build_hdr(),
    ieee80211_lookup_ra_sta() uses sta_info_get_bss() for all AP and AP VLAN
    frames, not only for control port frames. A unicast frame sent on one
    interface thus matches a station of a different VLAN of the same BSS and
    is transmitted to it.
    
    Only match stations of the interface the frame is sent on, except for
    control port frames, since those may be sent for VLAN stations directly
    on the BSS interface.
    
    Fixes: 97ffe75791b3 ("mac80211: factor out station lookup from ieee80211_build_hdr()")
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    Link: https://patch.msgid.link/20260916094833.164219-1-nbd@nbd.name
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/ieee80211_i.h
+++ net/mac80211/ieee80211_i.h
@@ -2475,7 +2475,7 @@ static inline bool ieee80211_require_encrypted_assoc(__le16 fc,
 /* sta_out needs to be checked for ERR_PTR() before using */
 int ieee80211_lookup_ra_sta(struct ieee80211_sub_if_data *sdata,
 			    struct sk_buff *skb,
-			    struct sta_info **sta_out);
+			    struct sta_info **sta_out, bool bss);
 
 static inline void
 ieee80211_tx_skb_tid_band(struct ieee80211_sub_if_data *sdata,
