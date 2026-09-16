18f26958544783b0f30210c41fc0c0da11b8d9a4
Guixin Liu <kanie@linux.alibaba.com>
2026-09-16 10:54:52 +0800

    driver core: Add conditional guard support for device_trylock()
    Introduce a conditional guard version of device_trylock() for scenarios
    that must not block on the device lock. device_trylock() returns 1 on
    success like mutex_trylock(), so the default binary condition applies and
    ACQUIRE_ERR() reports -EBUSY on contention.
    
    Suggested-by: Shaikh Kamaluddin <shaikhkamal2012@gmail.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@oss.qualcomm.com>
    Signed-off-by: Guixin Liu <kanie@linux.alibaba.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Link: https://patch.msgid.link/20260916025453.3532614-2-kanie@linux.alibaba.com
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>

--- include/linux/device.h
+++ include/linux/device.h
@@ -1121,6 +1121,7 @@ static inline void device_unlock(struct device *dev)
 
 DEFINE_GUARD(device, struct device *, device_lock(_T), device_unlock(_T))
 DEFINE_GUARD_COND(device, _intr, device_lock_interruptible(_T), _RET == 0)
+DEFINE_GUARD_COND(device, _try, device_trylock(_T))
 
 static inline void device_lock_assert(struct device *dev)
 {
