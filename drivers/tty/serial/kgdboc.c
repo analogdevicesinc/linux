ef3134cd0d4831ae2137aee2883987859b80b2c6
Wentao Liang <vulab@iscas.ac.cn>
2026-09-17 15:55:23 +0000

    kgdboc: Fix tty driver reference leak in configure_kgdboc()
    tty_find_polling_driver() returns the tty driver with an extra
    reference taken by tty_driver_kref_get(). configure_kgdboc() stores it
    in kgdb_tty_driver but the reference is never dropped: it is leaked when
    kgdb_register_io_module() fails, when the configuration is cleared, and
    on every reconfigure.
    
    Release the reference both when unconfiguring and on the error path.
    
    Fixes: 7d7b93c1452f ("tty: kref the tty driver object")
    Cc: stable@kernel.org
    Signed-off-by: Wentao Liang <vulab@iscas.ac.cn>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Link: https://patch.msgid.link/20260917155523.2161202-1-vulab@iscas.ac.cn
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/kgdboc.c
+++ drivers/tty/serial/kgdboc.c
@@ -188,6 +188,11 @@ static void cleanup_kgdboc(void)
 
 	kgdboc_unregister_kbd();
 	kgdb_unregister_io_module(&kgdboc_io_ops);
+
+	if (kgdb_tty_driver) {
+		tty_driver_kref_put(kgdb_tty_driver);
+		kgdb_tty_driver = NULL;
+	}
 }
 
 static int configure_kgdboc(void)
@@ -254,6 +259,10 @@ do_register:
 
 noconfig:
 	kgdboc_unregister_kbd();
+	if (kgdb_tty_driver) {
+		tty_driver_kref_put(kgdb_tty_driver);
+		kgdb_tty_driver = NULL;
+	}
 	configured = 0;
 
 	return err;
