2cf20fb2bad1d1e4057c21cc8c963221bb503aed
SJ Park <sj@kernel.org>
2026-09-17 07:21:59 -0700

    mm/damon/api: remove NR_DAMOS_FILTER_TYPES
    Patch series "mm/damon: improve readability, clarity and test coverage".
    
    Yet another batch of miscellaneous DAMON minor improvements.  Mostly
    focused on readability and clarity of code and document, and unit/self
    test coverage.  No user-visible behavioral change is intended.
    
    
    This patch (of 10):
    
    Nobody uses NR_DAMOS_FILTER_TYPES.  Remove it.
    
    Link: https://lore.kernel.org/20260917142210.90829-1-sj@kernel.org
    Link: https://lore.kernel.org/20260917142210.90829-2-sj@kernel.org
    Signed-off-by: SJ Park <sj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Asier Gutierrez <gutierrez.asier@huawei-partners.com>
    Cc: Brendan Higgins <brendan.higgins@linux.dev>
    Cc: David Gow <davidgow@davidgow.net>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>

--- include/linux/damon.h
+++ include/linux/damon.h
@@ -400,7 +400,6 @@ struct damos_stat {
  * @DAMOS_FILTER_TYPE_ADDR:	Address range.
  * @DAMOS_FILTER_TYPE_TARGET:	Data Access Monitoring target.
  * @DAMOS_FILTER_TYPE_PROBE_HITS_WSUM:	probe_hits weighted sum range.
- * @NR_DAMOS_FILTER_TYPES:	Number of filter types.
  *
  * All types except &DAMOS_FILTER_TYPE_ADDR, &DAMOS_FILTER_TYPE_TARGET and
  * &DAMOS_FILTER_TYPE_PROBE_HITS_WSUM are handled by the underlying &struct
@@ -422,7 +421,6 @@ enum damos_filter_type {
 	DAMOS_FILTER_TYPE_ADDR,
 	DAMOS_FILTER_TYPE_TARGET,
 	DAMOS_FILTER_TYPE_PROBE_HITS_WSUM,
-	NR_DAMOS_FILTER_TYPES,
 };
 
 /**
