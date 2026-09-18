b318a0b79a8a865200738e9d9d09beaed459a93c
Dale Whinham <daleyo@gmail.com>
2026-09-18 18:49:30 +0100

    platform/surface: aggregator_registry: Fix tablet-mode detection on Surface Pro 11
    Replace ssam_node_kip_tablet_switch with ssam_node_pos_tablet_switch, as
    Surface Pro 11 reports tablet-mode via the POS ('posture') subsystem and
    not the KIP subsystem.
    
    The KIP-based driver would remain stuck in a 'folded-back' state after
    suspend/resume, whereas the POS-based device reports its state correctly
    even after suspend/resume.
    
    This fixes the type cover keyboard/mouse being ignored by desktop
    environments after resume from sleep due to the erroneous 'folded-back'
    state.
    
    Fixes: c4a069095395 ("platform/surface: aggregator_registry: Add Surface Pro 11 (QCOM)")
    Signed-off-by: Dale Whinham <daleyo@gmail.com>
    Link: https://patch.msgid.link/20260918-surface-pro-11-aggregator-registry-fixes-v2-2-c4ed9335814a@gmail.com
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

--- drivers/platform/surface/surface_aggregator_registry.c
+++ drivers/platform/surface/surface_aggregator_registry.c
@@ -415,7 +415,7 @@ static const struct software_node *ssam_node_group_sp11[] = {
 	&ssam_node_hid_kip_penstash,
 	&ssam_node_hid_kip_touchpad,
 	&ssam_node_hid_kip_fwupd,
-	&ssam_node_kip_tablet_switch,
+	&ssam_node_pos_tablet_switch,
 	NULL,
 };
 
