4ade3ea4320003c209d36e6a1e0002762b0ffdb4
Dale Whinham <daleyo@gmail.com>
2026-09-18 18:49:29 +0100

    platform/surface: aggregator_registry: Remove sensors device for Surface Pro 11
    Remove ssam_node_hid_sam_sensors as this device is not present on
    Surface Pro 11. This fixes a probe failure in dmesg:
    
    surface_hid 01:15:01:06:00: unexpected descriptor length: got 0, expected 9
    surface_hid 01:15:01:06:00: probe with driver surface_hid failed with error -71
    
    Fixes: c4a069095395 ("platform/surface: aggregator_registry: Add Surface Pro 11 (QCOM)")
    Signed-off-by: Dale Whinham <daleyo@gmail.com>
    Link: https://patch.msgid.link/20260918-surface-pro-11-aggregator-registry-fixes-v2-1-c4ed9335814a@gmail.com
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

--- drivers/platform/surface/surface_aggregator_registry.c
+++ drivers/platform/surface/surface_aggregator_registry.c
@@ -415,7 +415,6 @@ static const struct software_node *ssam_node_group_sp11[] = {
 	&ssam_node_hid_kip_penstash,
 	&ssam_node_hid_kip_touchpad,
 	&ssam_node_hid_kip_fwupd,
-	&ssam_node_hid_sam_sensors,
 	&ssam_node_kip_tablet_switch,
 	NULL,
 };
