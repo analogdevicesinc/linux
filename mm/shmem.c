ee76e780c05da6ae2051a4e1f2013fb513f673e9
Breno Leitao <leitao@debian.org>
2026-09-18 04:24:37 -0700

    mm/shmem: report RCU-tasks quiescent states while undoing a range
    This shows up in the Meta fleet on ftruncate() of large tmpfs files:
    
      INFO: rcu_tasks detected stalls on tasks:
      00000000752fd185: .. nvcsw: 59455/59455 holdout: 1 idle_cpu: -1/0
      task:rocksdb:bottom  state:R  running task
        __folio_split
        find_get_entry
        find_get_entries
        truncate_inode_partial_folio
        shmem_undo_range
        shmem_setattr
        notify_change
        do_ftruncate
        __x64_sys_ftruncate
        do_syscall_64
    
    shmem_undo_range() walks the whole of the requested range in folio_batch
    sized steps, twice, and its two cond_resched() calls are the only
    reschedule points in that walk.
    
    cond_resched() is not an RCU-tasks quiescent state.  Use
    cond_resched_tasks_rcu_qs() at both points so the walk reports an
    RCU-tasks quiescent state as it proceeds.
    
    Link: https://lore.kernel.org/20260918-shmem-tasks-rcu-v1-1-79acf91a2569@debian.org
    Fixes: 8315f42295d2 ("rcu: Add call_rcu_tasks()")
    Signed-off-by: Breno Leitao <leitao@debian.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: SJ Park <sj@kernel.org>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: "Paul E . McKenney" <paulmck@kernel.org>
    Cc: <stable@vger.kernel.org>

--- mm/shmem.c
+++ mm/shmem.c
@@ -1367,7 +1367,7 @@ static void shmem_undo_range(struct inode *inode, loff_t lstart, uoff_t lend,
 		}
 		folio_batch_remove_exceptionals(&fbatch);
 		folio_batch_release(&fbatch);
-		cond_resched();
+		cond_resched_tasks_rcu_qs();
 	}
 
 	/*
@@ -1408,7 +1408,7 @@ whole_folios:
 
 	index = start;
 	while (index < end) {
-		cond_resched();
+		cond_resched_tasks_rcu_qs();
 
 		if (!find_get_entries(mapping, &index, end - 1, &fbatch,
 				indices)) {
