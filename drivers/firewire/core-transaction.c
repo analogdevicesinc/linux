d34653cbe03fb090325a61566348cabc2d9c83df
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2026-09-19 20:50:17 +0900

    firewire: core: use spinlock without irqsave for card topology_map member
    Asynchronous packet callbacks are now always invoked in process context.
    Therefore, the topology_map member is not accessed from IRQ context.
    
    Use spin_lock() for the topology_map member since disabling local IRQs is
    unnecessary.
    
    Link: https://lore.kernel.org/r/20260919115017.859169-6-o-takashi@sakamocchi.jp
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- drivers/firewire/core-transaction.c
+++ drivers/firewire/core-transaction.c
@@ -1256,9 +1256,7 @@ static void handle_topology_map(struct fw_card *card, struct fw_request *request
 
 	start = (offset - topology_map_region.start) / 4;
 
-	// NOTE: This can be without irqsave when we can guarantee that fw_send_request() for local
-	// destination never runs in any type of IRQ context.
-	scoped_guard(spinlock_irqsave, &card->topology_map.lock)
+	scoped_guard(spinlock, &card->topology_map.lock)
 		memcpy(payload, &card->topology_map.buffer[start], length);
 
 	fw_send_response(card, request, RCODE_COMPLETE);
