8d37f20173a2760718bb02b58bafc5a7cac93d67
Mateo Borda <mateob226@gmail.com>
2026-09-20 14:39:04 -0300

    gpiolib: acpi: Add quirk for Lenovo IdeaPad Slim 3 15ABR8
    On the Lenovo IdeaPad Slim 3 15ABR8 (82XM), the touchscreen
    (GTCH7503:00) GPIO interrupt on pin 10 is armed as a wake source. As
    soon as the system enters s2idle suspend the interrupt fires, causing
    an immediate spurious resume, so suspend is unusable.
    
    Disabling wakeup for the device via sysfs or booting with
    gpiolib_acpi.ignore_wake=GTCH7503:00@10 avoids the problem, so add
    a DMI quirk that ignores wakeup on that pin.
    
    Signed-off-by: Mateo Borda <mateob226@gmail.com>
    Acked-by: Mika Westerberg <westeri@kernel.org>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/gpio/gpiolib-acpi-quirks.c
+++ drivers/gpio/gpiolib-acpi-quirks.c
@@ -392,6 +392,19 @@ static const struct dmi_system_id gpiolib_acpi_quirks[] __initconst = {
 			.ignore_wake = "VEN_0488:00@355",
 		},
 	},
+	{
+		/*
+		 * Lenovo IdeaPad Slim 3 15ABR8: the touchscreen wake IRQ fires as soon
+		 * as the system enters suspend, causing an immediate spurious resume.
+		 */
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "82XM"),
+		},
+		.driver_data = &(struct acpi_gpiolib_dmi_quirk) {
+			.ignore_wake = "GTCH7503:00@10",
+		},
+	},
 	{} /* Terminating entry */
 };
 
