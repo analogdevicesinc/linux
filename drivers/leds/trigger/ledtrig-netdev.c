072e0e799bc6b3909afc8fd7fac750b31e37b438
Rong Zhang <i@rong.moe>
2026-09-21 03:40:22 +0800

    leds: trigger: netdev: Implement hw_offloaded() callback
    "netdev" can run in hardware control according to hardware capabilities
    and trigger options.
    
    Implement hw_offloaded() callback to provide its hardware control state
    to the LED core, and document the relation between the custom
    "offloaded" attribute and the generic "trigger_may_offload_to_hw"
    attribute.
    
    The callback mimics how the existing "offloaded" attribute does, i.e.,
    locklessly reads hw_control, as it's just a hint and don't need to be
    accurate.
    
    Acked-by: Ike Panhc <ikepanhc@gmail.com>
    Signed-off-by: Rong Zhang <i@rong.moe>
    Link: https://patch.msgid.link/20260921-leds-trigger-hw-changed-v7-8-fe3cdb6dec51@rong.moe
    Signed-off-by: Lee Jones <lee@kernel.org>

--- drivers/leds/trigger/ledtrig-netdev.c
+++ drivers/leds/trigger/ledtrig-netdev.c
@@ -795,10 +795,18 @@ static void netdev_trig_deactivate(struct led_classdev *led_cdev)
 	kfree(trigger_data);
 }
 
+static bool netdev_trig_hw_offloaded(struct led_classdev *led_cdev)
+{
+	struct led_netdev_data *trigger_data = led_get_trigger_data(led_cdev);
+
+	return trigger_data->hw_control;
+}
+
 static struct led_trigger netdev_led_trigger = {
 	.name = "netdev",
 	.activate = netdev_trig_activate,
 	.deactivate = netdev_trig_deactivate,
+	.hw_offloaded = netdev_trig_hw_offloaded,
 	.groups = netdev_trig_groups,
 };
 
