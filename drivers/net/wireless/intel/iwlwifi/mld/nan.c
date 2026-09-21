99b0860b18fc142846cf437142e3acf79f900c1c
Avraham Stern <avraham.stern@intel.com>
2026-09-21 23:41:13 +0300

    wifi: iwlwifi: mld: config TLC only after attaching station to a link
    When a NAN link is activated, the TLC config command was sent for all
    the NAN stations. The TLC config was sent on all phys used by NAN.
    However, when a new NAN channel is added when there is already a NAN
    station (e.g. during schedule update), the station is not yet
    attached to the link of this phy (until the new schedule is
    applied by firmware). Sending a TLC command for the station on the
    new phy at this stage results in an assert because in firmware the
    station is not yet attached with the phy of this channel.
    Fix it by sending the TLC command only after configuring the station
    for the new phy.
    
    Signed-off-by: Avraham Stern <avraham.stern@intel.com>
    Link: https://patch.msgid.link/20260921233730.24cefdd72e7d.Ib32c29716c805531f881c61d13931f5b3d7084a9@changeid
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>

--- drivers/net/wireless/intel/iwlwifi/mld/nan.c
+++ drivers/net/wireless/intel/iwlwifi/mld/nan.c
@@ -576,20 +576,10 @@ static int iwl_mld_nan_link_set_active(struct iwl_mld *mld,
 				       bool active)
 {
 	struct iwl_link_config_cmd cmd;
-	struct ieee80211_sta *sta;
 	int ret;
 
 	if (nan_link->active == active)
 		return 0;
-
-	if (active) {
-		for_each_station(sta, mld->hw) {
-			struct iwl_mld_sta *mld_sta = iwl_mld_sta_from_mac80211(sta);
-
-			if (mld_sta->sta_type == STATION_TYPE_NAN_PEER_NDI)
-				iwl_mld_config_tlc(mld, mld_sta->vif, sta);
-		}
-	}
 
 	nan_link->active = active;
 
@@ -602,10 +592,8 @@ static int iwl_mld_nan_link_set_active(struct iwl_mld *mld,
 		return ret;
 	}
 
-	if (!active) {
+	if (!active)
 		nan_link->chanctx = NULL;
-		/* TODO: when FW is ready, Update phy in TLC to invalid after */
-	}
 
 	return 0;
 }
@@ -847,6 +835,10 @@ void iwl_mld_nan_vif_cfg_changed(struct iwl_mld *mld,
 			if (mld_sta->sta_type == STATION_TYPE_NAN_PEER_NMI ||
 			    mld_sta->sta_type == STATION_TYPE_NAN_PEER_NDI)
 				iwl_mld_add_modify_sta_cmd(mld, &sta->deflink);
+
+			if (added_links &&
+			    mld_sta->sta_type == STATION_TYPE_NAN_PEER_NDI)
+				iwl_mld_config_tlc(mld, mld_sta->vif, sta);
 		}
 
 		/*
