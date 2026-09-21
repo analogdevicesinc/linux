a13c9bf62caa62a4cb96d7b85394346cabb73496
Miquel Raynal <miquel.raynal@bootlin.com>
2026-09-21 08:43:35 +0200

    mtd: spi-nor: winbond: W25Q64xW-Q/N: Fill locking information
    The SFDP table does not advertize the locking capabilities of the
    device, flag the missing capabilities.
    
    This feature is identical across the various datasheets exposing the
    same ID, however I could only test it on the JW-Q variant.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Signed-off-by: Michael Walle <mwalle@kernel.org>

--- drivers/mtd/spi-nor/winbond.c
+++ drivers/mtd/spi-nor/winbond.c
@@ -334,7 +334,7 @@ static const struct flash_info winbond_nor_parts[] = {
 		.id = SNOR_ID(0xef, 0x60, 0x17),
 		.name = "w25q64dw",
 		.size = SZ_8M,
-		.flags = SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB,
+		.flags = SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB | SPI_NOR_HAS_CMP,
 		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
 	}, {
 		/* W25Q128FW-G/Q, W25Q128JW-Q/N */
