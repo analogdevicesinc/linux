136588c583adc2c73934bb0e89877fd1a8b91305
Miquel Raynal <miquel.raynal@bootlin.com>
2026-09-21 08:43:51 +0200

    mtd: spi-nor: winbond: Add support for W25Q25PW-Q/N
    There is an ID collision with chips of same density from the JW family
    (which it self conflicted with yet another family). Chips are very
    similar in practice, it is mostly a matter of electrical differences
    (mostly power consumption being lower) as well as the addition of inline
    ECC capability.
    
    16 bytes chunks are protected against single errors. In case a
    non-aligned write happens, ECC is locally disabled until the next erase.
    
    Another significant difference is that PW chips identify themselves as
    supporting the new SFDP (rev F) QER field which forces an alternate
    write SR2 opcode (0x31).
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Signed-off-by: Michael Walle <mwalle@kernel.org>

--- drivers/mtd/spi-nor/winbond.c
+++ drivers/mtd/spi-nor/winbond.c
@@ -395,7 +395,7 @@ static const struct flash_info winbond_nor_parts[] = {
 		.flags = SPI_NOR_QUAD_PP | SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB | SPI_NOR_HAS_CMP,
 		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
 	}, {
-		/* W25Q256JW-Q/N */
+		/* W25Q256JW-Q/N, W25Q25PW-Q/N */
 		.id = SNOR_ID(0xef, 0x60, 0x19),
 		.name = "w25q256jw",
 		.size = SZ_32M,
