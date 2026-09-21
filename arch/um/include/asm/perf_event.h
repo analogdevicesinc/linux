109cf3ac397e05156925f5455b19a6dfcea78b95
Johannes Berg <johannes.berg@intel.com>
2026-09-22 01:07:40 +0200

    um: add perf-event support
    Enable HAVE_PERF_EVENTS without any hardware events (since
    there's no hardware), that just needs to select the symbol
    and provide an empty asm/perf_event.h.
    
    Link: https://patch.msgid.link/20260921230739.587996-2-johannes@sipsolutions.net
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

---
+++ arch/um/include/asm/perf_event.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __UM_PERF_EVENT_H
+#define __UM_PERF_EVENT_H
+
+/* nothing here - clearly no real hardware support */
+
+#endif /* __UM_PERF_EVENT_H */
