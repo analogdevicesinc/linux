a1cdd72371270640bd357bd3e3b2ec0be17c9536
Even Xu <even.xu@intel.com>
2026-09-22 10:46:38 +0800

    HID: Intel-thc-hid: Intel-quickspi: Fix buffer overflow
    The input buffers are used to store complete HID-over-SPI packets,
    including their protocol headers. However, their sizes are
    currently calculated from the payload length only.
    
    Allocate enough space for both the payload and the packet header to
    prevent a buffer overflow when the payload reaches its maximum size.
    
    Signed-off-by: Even Xu <even.xu@intel.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.com>

--- drivers/hid/intel-thc-hid/intel-quickspi/pci-quickspi.c
+++ drivers/hid/intel-thc-hid/intel-quickspi/pci-quickspi.c
@@ -552,7 +552,8 @@ static int quickspi_alloc_report_buf(struct quickspi_device *qsdev)
 	max_input_len = max(le16_to_cpu(qsdev->dev_desc.rep_desc_len),
 			    le16_to_cpu(qsdev->dev_desc.max_input_len));
 
-	qsdev->input_buf = devm_kzalloc(qsdev->dev, max_input_len, GFP_KERNEL);
+	qsdev->input_buf = devm_kzalloc(qsdev->dev,
+					HIDSPI_INPUT_BODY_SIZE(max_input_len), GFP_KERNEL);
 	if (!qsdev->input_buf)
 		return -ENOMEM;
 
