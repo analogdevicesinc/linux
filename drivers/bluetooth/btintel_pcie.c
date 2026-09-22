8f23c427531486caaf53ae03eebc740ef3cb4a8e
Sai Teja Aluvala <aluvala.sai.teja@intel.com>
2026-09-22 11:47:44 +0530

    Bluetooth: btintel_pcie: Add support for Intel Draco
    Add Device ID for Intel Draco support
    
    From lspci -v -d 8086:2731
    
    02: 00.0 Bluetooth: Intel Corporation Device 2731
            Subsystem: Intel Corporation Device 0012
            Flags: bus master, fast devsel, latency 0, IRQ 16
            Memory at fe000000 (32-bit, non-prefetchable) [size=16K]
            Capabilities: [c8] Power Management version 3
            Capabilities: [d0] MSI: Enable- Count=1/1 Maskable- 64bit+
            Capabilities: [40] Express Root Complex Integrated Endpoint, MSI 00
            Capabilities: [80] MSI-X: Enable+ Count=5 Masked-
            Kernel driver in use: btintel_pcie
            Kernel modules: btintel_pcie
    
    Signed-off-by: Sai Teja Aluvala <aluvala.sai.teja@intel.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- drivers/bluetooth/btintel_pcie.c
+++ drivers/bluetooth/btintel_pcie.c
@@ -55,6 +55,8 @@ static const struct pci_device_id btintel_pcie_table[] = {
 	{ BTINTEL_PCI_DEVICE(0xD346, PCI_ANY_ID) },
 	 /* Scorpious2, Nova Lake-PCD-S */
 	{ BTINTEL_PCI_DEVICE(0x6E74, PCI_ANY_ID) },
+	 /* Draco */
+	{ BTINTEL_PCI_DEVICE(0x2731, PCI_ANY_ID) },
 	{ 0 }
 };
 MODULE_DEVICE_TABLE(pci, btintel_pcie_table);
