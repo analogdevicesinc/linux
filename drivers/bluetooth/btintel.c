4145a13c1a044d413de9486c8d99a1a6229ad2b6
Chandrashekar Devegowda <chandrashekar.devegowda@intel.com>
2026-09-22 12:10:20 +0530

    Bluetooth: btintel: fix buffer over-read in btintel_hw_error()
    The "Exception info %s" print uses skb->data + 1, but the 12-byte
    payload is not guaranteed to be NUL-terminated and can be read past
    its end.
    
    Bound the print with "%.*s" and skb->len - 1.
    
    Fixes: 973bb97e5aee ("Bluetooth: btintel: Add generic function for handling hardware errors")
    Cc: stable@vger.kernel.org
    Signed-off-by: Chandrashekar Devegowda <chandrashekar.devegowda@intel.com>

--- drivers/bluetooth/btintel.c
+++ drivers/bluetooth/btintel.c
@@ -289,7 +289,8 @@ void btintel_hw_error(struct hci_dev *hdev, u8 code)
 		goto unlock;
 	}
 
-	bt_dev_err(hdev, "Exception info %s", (char *)(skb->data + 1));
+	bt_dev_err(hdev, "Exception info %.*s", (int)(skb->len - 1),
+		   (char *)(skb->data + 1));
 
 	kfree_skb(skb);
 
