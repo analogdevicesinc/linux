f255e2055701285d6b5f11d2a6facc6614c51ed7
Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
2026-09-23 02:37:33 +0530

    soc: qcom: socinfo: Add SM8975 SoC ID
    Add SoC ID for Qualcomm SM8975 which represents the Hawi platform.
    
    Signed-off-by: Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260923-hawi-socinfo-v1-2-f0d1aaee9323@oss.qualcomm.com
    Signed-off-by: Bjorn Andersson <bjorn.andersson@oss.qualcomm.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -542,6 +542,7 @@ static const struct soc_id soc_id[] = {
 	{ qcom_board_id(QCS9075) },
 	{ qcom_board_id(QCS615) },
 	{ qcom_board_id(SA8797P) },
+	{ qcom_board_id(SM8975) },
 	{ qcom_board_id(CQ7790M) },
 	{ qcom_board_id(CQ7790S) },
 	{ qcom_board_id(CQ2390M) },
