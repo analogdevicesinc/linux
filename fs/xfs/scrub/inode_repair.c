469b4879f75273a94f972677150aefe4c639e130
Darrick J. Wong <djwong@kernel.org>
2026-09-22 22:59:11 -0700

    xfs: fix ondisk symlink target validation in xrep_dinode_check_dfork
    LOLLM noticed that online repair of a broken symlink file could fail
    unnecessarily if a local-format symlink target isn't null terminated.
    The ondisk target isn't required to be null terminated, but repair
    enforces that anyway because it uses the validator for the incore
    symlink target.  (The incore buffer is always null-terminated).  Fix
    this by reverting the changes to xfs_symlink_shortform_verify and adding
    an ondisk-specific helper in inode_repair.c.
    
    Cc: stable@vger.kernel.org # v6.8
    Fixes: e744cef2060559 ("xfs: zap broken inode forks")
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Assisted-by: LOLLM # finding obvious bugs
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Dave Chinner <dgc@kernel.org>
    Signed-off-by: Carlos Maiolino <cem@kernel.org>

--- fs/xfs/scrub/inode_repair.c
+++ fs/xfs/scrub/inode_repair.c
@@ -1024,6 +1024,30 @@ xrep_dinode_bad_metabt_fork(
 	return false;
 }
 
+static xfs_failaddr_t
+xrep_symlink_shortform_verify(
+	void			*sfp,
+	int64_t			size)
+{
+	/*
+	 * Zero length symlinks should never occur in memory as they are
+	 * never allowed to exist on disk.
+	 */
+	if (!size)
+		return __this_address;
+
+	/* No negative sizes or overly long symlink targets. */
+	if (size < 0 || size > XFS_SYMLINK_MAXLEN)
+		return __this_address;
+
+	/* No NULLs in the target either. */
+	if (memchr(sfp, 0, size))
+		return __this_address;
+
+	/* ondisk symlink target isn't null terminated, unlike incore */
+	return NULL;
+}
+
 /*
  * Check the data fork for things that will fail the ifork verifiers or the
  * ifork formatters.
@@ -1099,7 +1123,7 @@ xrep_dinode_check_dfork(
 			return true;
 		/* symlink structure must pass verification. */
 		if (S_ISLNK(mode) &&
-		    xfs_symlink_shortform_verify(dfork_ptr, data_size) != NULL)
+		    xrep_symlink_shortform_verify(dfork_ptr, data_size) != NULL)
 			return true;
 		break;
 	case XFS_DINODE_FMT_EXTENTS:
